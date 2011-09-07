b05136d70673eeadbe025b737697c841f40d343a
Simon Kornblith <simon@simonster.com>
2011-09-07 01:25:14 -0400

    Connector compatibility

--- Library Catalog (InnoPAC).js
+++ Library Catalog (InnoPAC).js
@@ -3,12 +3,13 @@
 	"label": "Library Catalog (InnoPAC)",
 	"creator": "Simon Kornblith and Michael Berkowitz",
 	"target": "(search~|\\/search\\?|(a|X|t|Y|w)\\?|\\?(searchtype|searchscope)|frameset&FF|record=b[0-9]+(~S[0-9])?|/search/q\\?)",
-	"minVersion": "2.1.8",
+	"minVersion": "2.1.9",
 	"maxVersion": "",
+	"browserSupport":"gcs",
 	"priority": 200,
 	"inRepository": true,
 	"translatorType": 4,
-	"lastUpdated": "2011-09-03 11:00:21"
+	"lastUpdated": "2011-09-07 05:24:15"
 }
 
 function detectWeb(doc, url) {
@@ -42,7 +43,7 @@ function detectWeb(doc, url) {
 	if(elmt) {
 		return "book";
 	}
-
+	
 	// Regular expression to reduce false positives
 	if (!url.match(/SEARCH=/) && !url.match(/searchargs?=/) && !url.match(/&FF/) && !url.match(/search~S[0-9]/) && !url.match(/\/search\/q\?/) && !url.match(/record=/)) return false;
 	// First, check to see if the URL alone reveals InnoPAC, since some sites don't reveal the MARC button
@@ -59,7 +60,7 @@ function detectWeb(doc, url) {
 		return "book";
 	}
 	// Also, check for links to an item display page
-	var tags = doc.getElementsByTagName("a");
+	var tags = ZU.xpath(doc, '//a[@href]');
 	for(var i=0; i<tags.length; i++) {
 		if(matchRegexp.test(tags[i].href) || tags[i].href.match(/^https?:\/\/([^/]+\/(?:search\??\/|record=?|search%7e\/)|frameset&FF=)/)) {
 			return "multiple";
@@ -157,72 +158,72 @@ function doWeb(doc, url) {
 	// load translator for MARC
 	var translator = Zotero.loadTranslator("import");
 	translator.setTranslator("a6ee60df-1ddc-4aae-bb25-45e0537be973");
-	var marc = translator.getTranslatorObject();
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
-	if (detectWeb(doc, url) == "book") {
-		var matchRegexp = new RegExp('^(.*)frameset(.+)$');
-		var m = matchRegexp.exec(uri);
-		if (m) {
-			newUri = uri.replace(/frameset/, "marc");
-		} else {
-			var xpath = '//a[img[@src="/screens/marc_display.gif" or @src="/screens/ico_marc.gif" or @src="/screens/marcdisp.gif" or starts-with(@alt, "MARC ") or @src="/screens/regdisp.gif" or @alt="REGULAR RECORD DISPLAY"]] | //a[span/img[@src="/screens/marc_display.gif" or @src="/screens/ico_marc.gif" or @src="/screens/marcdisp.gif" or starts-with(@alt, "MARC ") or @src="/screens/regdisp.gif" or @alt="REGULAR RECORD DISPLAY"]]';
-			newUri = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().href.replace(/frameset/, "marc");
-		}
-		pageByPage(marc, [newUri]);
-	} else {	// Search results page
-		// Require link to match this
-		var tagRegexp = new RegExp();
-		tagRegexp.compile('^https?://[^/]+/search\\??/[^/]+/[^/]+/[0-9]+\%2C[^/]+/frameset');
+	translator.getTranslatorObject(function(marc) {
+		var namespace = doc.documentElement.namespaceURI;
+		var nsResolver = namespace ? function(prefix) {
+			if (prefix == 'x') return namespace; else return null;
+		} : null;
 		
-		var urls = new Array();
-		var availableItems = new Array();
-		var firstURL = false;
-		
-		var tableRows = doc.evaluate('//table//tr[@class="browseEntry" or @class="briefCitRow" or td/input[@type="checkbox"] or td[contains(@class,"briefCitRow") or contains(@class,"briefcitCell")]]',
-									 doc, nsResolver, XPathResult.ANY_TYPE, null);
-		// Go through table rows
-		var i = 0;
-		while(tableRow = tableRows.iterateNext()) {
-			// get link
-			var links = doc.evaluate('.//*[@class="briefcitTitle"]/a', tableRow, nsResolver, XPathResult.ANY_TYPE, null);
-			var link = links.iterateNext();
-			if(!link) {
-				var links = doc.evaluate(".//a", tableRow, nsResolver, XPathResult.ANY_TYPE, null);
-				link = links.iterateNext();
+		if (detectWeb(doc, url) == "book") {
+			var matchRegexp = new RegExp('^(.*)frameset(.+)$');
+			var m = matchRegexp.exec(uri);
+			if (m) {
+				newUri = uri.replace(/frameset/, "marc");
+			} else {
+				var xpath = '//a[img[@src="/screens/marc_display.gif" or @src="/screens/ico_marc.gif" or @src="/screens/marcdisp.gif" or starts-with(@alt, "MARC ") or @src="/screens/regdisp.gif" or @alt="REGULAR RECORD DISPLAY"]] | //a[span/img[@src="/screens/marc_display.gif" or @src="/screens/ico_marc.gif" or @src="/screens/marcdisp.gif" or starts-with(@alt, "MARC ") or @src="/screens/regdisp.gif" or @alt="REGULAR RECORD DISPLAY"]]';
+				newUri = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().href.replace(/frameset/, "marc");
 			}
+			pageByPage(marc, [newUri]);
+		} else {	// Search results page
+			// Require link to match this
+			var tagRegexp = new RegExp();
+			tagRegexp.compile('^https?://[^/]+/search\\??/[^/]+/[^/]+/[0-9]+\%2C[^/]+/frameset');
 			
-			if(link) {
-				if(availableItems[link.href]) {
-					continue;
-				}
-					
-				
-				// Go through links
-				while(link) {
-					if (link.textContent.match(/\w+/)) availableItems[link.href] = link.textContent;
+			var urls = new Array();
+			var availableItems = new Array();
+			var firstURL = false;
+			
+			var tableRows = doc.evaluate('//table//tr[@class="browseEntry" or @class="briefCitRow" or td/input[@type="checkbox"] or td[contains(@class,"briefCitRow") or contains(@class,"briefcitCell")]]',
+										 doc, nsResolver, XPathResult.ANY_TYPE, null);
+			// Go through table rows
+			var i = 0;
+			while(tableRow = tableRows.iterateNext()) {
+				// get link
+				var links = doc.evaluate('.//*[@class="briefcitTitle"]/a', tableRow, nsResolver, XPathResult.ANY_TYPE, null);
+				var link = links.iterateNext();
+				if(!link) {
+					var links = doc.evaluate(".//a[@href]", tableRow, nsResolver, XPathResult.ANY_TYPE, null);
 					link = links.iterateNext();
 				}
-				i++;
+				
+				if(link) {
+					if(availableItems[link.href]) {
+						continue;
+					}
+					
+					// Go through links
+					while(link) {
+						if (link.textContent.match(/\w+/)) availableItems[link.href] = link.textContent;
+						link = links.iterateNext();
+					}
+					i++;
+				}
+			};
+			
+			var items = Zotero.selectItems(availableItems);
+			
+			if(!items) {
+				return true;
 			}
-		};
-		
-		var items = Zotero.selectItems(availableItems);
-		
-		if(!items) {
-			return true;
+			
+			var newUrls = new Array();
+			for(var itemURL in items) {
+				newUrls.push(itemURL.replace("frameset", "marc"));
+			}
+			pageByPage(marc, newUrls);
 		}
-		
-		var newUrls = new Array();
-		for(var url in items) {
-			newUrls.push(url.replace("frameset", "marc"));
-		}
-		pageByPage(marc, newUrls);
-	}
-
+	});
+	
 	Zotero.wait();
 }
 
