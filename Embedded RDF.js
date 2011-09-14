9f967997a314cf16a2b639c76597e354d6edfe58
Avram Lyon <ajlyon@gmail.com>
2011-09-14 09:13:39 -0700

    Expanding Embedded RDF's range of data, per https://groups.google.com/d/msg/zotero-dev/-/y-7WLxIq4JcJ

--- Embedded RDF.js
+++ Embedded RDF.js
@@ -9,44 +9,72 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcs",
-	"lastUpdated": "2011-07-04 00:32:05"
+	"lastUpdated": "2011-09-14 09:10:10"
 }
 
-var _prefix;
-var _dc = "http://purl.org/dc/elements/1.1/";
 
-function getDCPrefix(doc) {
-	if(_prefix) {
-		return _prefix;
+// Known formats
+// Reverse
+var _n = {
+	"http://purl.org/net/biblio#":"bib",
+	"http://purl.org/dc/elements/1.1/":"dc",
+	"http://purl.org/dc/terms/":"dcterms",
+	"http://prismstandard.org/namespaces/1.2/basic/":"prism",
+	"http://xmlns.com/foaf/0.1/":"foaf",
+	"http://nwalsh.com/rdf/vCard#":"vcard",
+	"http://www.w3.org/2006/vcard/ns#":"vcard2",	// currently used only for NSF, but is probably
+							// very similar to the nwalsh vcard ontology in a
+							// different namespace
+	"http://purl.org/rss/1.0/modules/link/":"link",
+	"http://www.zotero.org/namespaces/export#":"z",
+	"http://purl.org/eprint/terms/":"eprint"
+};
+
+// Maps actual prefix in use to URI
+var _prefixes = {};
+
+// These are the ones that we will read without a declared schema
+var _dcDefined = false;
+
+function getPrefixes(doc) {
+	if(_prefixes.length) {
+		return _prefixes;
 	}
-	
+
 	var links = doc.getElementsByTagName("link");
 	for(var i=0; i<links.length; i++) {
-		if(links[i].getAttribute("href") == _dc) {
+		// Look for the schema's URI in our known schemata
+		if(links[i].getAttribute("href") in _n) {
 			var rel = links[i].getAttribute("rel");
 			if(rel) {
 				var matches = rel.match(/^schema\.([a-zA-Z]+)/);
 				if(matches) {
-					_prefix = matches[1].toLowerCase() + ".";
+					_prefixes[matches[1].toLowerCase()] = links[i].getAttribute("href");
+					if (_n[links[i].getAttribute("href")] == "dc") {
+						_dcDefined = true;	
+					}
 				}
 			}
-			break;
 		}
 	}
-	if(!_prefix) {
-		_prefix = "dc.";
+	// We allow prefix-less DC
+	if (!_dcDefined && !_prefixes["dc"]) {
+		_prefixes["dc"] = "http://purl.org/dc/elements/1.1/";
 	}
-	return _prefix;
+	return _prefixes;
 }
 
 function detectWeb(doc, url) {
-	var prefix = getDCPrefix(doc);
+	var prefixes = getPrefixes(doc);
 	
+	// XXX What is this blacklisting?
 	if (url.indexOf("reprint") != -1) return false;
 	var metaTags = doc.getElementsByTagName("meta");
 	for(var i=0; i<metaTags.length; i++) {
 		var tag = metaTags[i].getAttribute("name");
-		if(tag && tag.substr(0, prefix.length).toLowerCase() == prefix) {
+		// See if the supposed prefix is there, split by period or underscore
+		if(tag && (prefixes[tag.split('.')[0].toLowerCase()] 
+				|| prefixes[tag.split('_')[0].toLowerCase()])) {
 			return "webpage";
 		}
 	}
@@ -55,42 +83,39 @@ function detectWeb(doc, url) {
 }
 
 function doWeb(doc, url) {
-	var prefix = getDCPrefix(doc);
+	var prefixes = getPrefixes(doc);
 	
 	// load RDF translator, so that we don't need to replicate import code
 	var translator = Zotero.loadTranslator("import");
 	translator.setTranslator("5e3ad958-ac79-463d-812b-a86a9235c28f");
 	translator.setHandler("itemDone", function(obj, newItem) {
+		if (!newItem.url) newItem.url = doc.location.href;
+		if (!newItem.title) newItem.title = doc.title;
 		// add attachment
 		newItem.attachments.push({document:doc});
-		// add access date and url
+		// add access date
 		newItem.accessDate = 'CURRENT_TIMESTAMP';
-		newItem.url = doc.location.href;
-		newItem.repository = false;
 		newItem.complete();
 	});
 	
 	translator.getTranslatorObject(function(rdf) {
 		var metaTags = doc.getElementsByTagName("meta");
-		var foundTitle = false;		// We can use the page title if necessary
 		for(var i=0; i<metaTags.length; i++) {
 			var tag = metaTags[i].getAttribute("name");
 			var value = metaTags[i].getAttribute("content");
-			if(tag && value && tag.substr(0, prefix.length).toLowerCase() == prefix) {
-				if(tag == "dc.title") {
-					foundTitle = true;
-				}
-				rdf.Zotero.RDF.addStatement(url, _dc + tag.substr(3).toLowerCase(), value, true);
-			} else if(tag && value && (tag == "author" || tag == "author-personal")) {
-				rdf.Zotero.RDF.addStatement(url, _dc + "creator", value, true);
-			} else if(tag && value && tag == "author-corporate") {
-				rdf.Zotero.RDF.addStatement(url, _dc + "creator", value, true);
+
+			// See if the supposed prefix is there, split by period or underscore
+			if(tag && (prefixes[tag.split('.')[0].toLowerCase()] 
+					|| prefixes[tag.split('_')[0].toLowerCase()])) {
+				// Set the delimiter for this tag
+				var delim = (prefixes[tag.split('_')[0].toLowerCase()]) ? '_' : '.';
+				var pieces = tag.split(delim);
+				var prefix = pieces.shift().toLowerCase();
+
+				rdf.Zotero.RDF.addStatement(url, prefixes[prefix] + pieces.join(delim).toLowerCase(), value, true);
 			}
 		}
-		
-		if (!foundTitle) {
-			rdf.Zotero.RDF.addStatement(url, _dc + "title", doc.title, true);
-		}
+
 		rdf.defaultUnknownType = "webpage";
 		rdf.doImport();
 	});
@@ -106,7 +131,8 @@ var testCases = [
 				"itemType": "webpage",
 				"creators": [
 					{
-						"lastName": "Diane Hillmann",
+						"firstName": "Diane",
+						"lastName": "Hillmann",
 						"creatorType": "author"
 					}
 				],
@@ -126,7 +152,10 @@ var testCases = [
 				"label": "Dublin Core Metadata Initiative",
 				"distributor": "Dublin Core Metadata Initiative",
 				"extra": "This document is intended as an entry point for users of Dublin Core. For non-specialists, it will assist them in creating simple descriptive records for information resources (for example, electronic documents). Specialists may find the document a useful point of reference to the documentation of Dublin Core, as it changes and grows.",
-				"url": "http://dublincore.org/documents/usageguide/"
+				"url": "http://dublincore.org/documents/usageguide/",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "Embedded RDF",
+				"checkFields": "title"
 			}
 		]
 	}
