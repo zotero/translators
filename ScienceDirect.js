c08f31c6df7e2bd1058c32bc285ddd80f0defef1
Dan Stillman <dstillman@zotero.org>
2011-10-09 16:25:24 -0300

    Update ScienceDirect timestamp to retrigger push

--- ScienceDirect.js
+++ ScienceDirect.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsb",
-	"lastUpdated": "2011-09-27 20:00:05"
+	"lastUpdated": "2011-10-09 19:20:00"
 }
 
 function detectWeb(doc, url) {
