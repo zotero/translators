e438c2357343efcb0f3bcbe5ea8d62c0d515637f
adam3smith <karcher@u.northwestern.edu>
2011-10-19 20:44:36 -0600

    add tests for ACL and Ab Imperio

--- ACLWeb.js
+++ ACLWeb.js
@@ -1,14 +1,15 @@
 {
-	"translatorID":"f4a5876a-3e53-40e2-9032-d99a30d7a6fc",
-	"translatorType":4,
-	"label":"ACL",
-	"creator":"Nathan Schneider",
-	"target":"^http://(www[.])?aclweb.org/anthology-new/[^#]+",
-	"minVersion":"1.0.7",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":false,
-	"lastUpdated":"2008-12-06 10:21:05"
+	"translatorID": "f4a5876a-3e53-40e2-9032-d99a30d7a6fc",
+	"label": "ACL",
+	"creator": "Nathan Schneider",
+	"target": "^https?://(www[.])?aclweb\\.org/anthology-new/[^#]+",
+	"minVersion": "1.0.7",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2011-10-19 19:41:03"
 }
 
 // based on ACM translator
@@ -17,7 +18,7 @@ function detectWeb(doc, url) {
 	var nsResolver = namespace ? function(prefix) {
 		if (prefix == 'x') return prefix; else return null;
 	} : namespace;
-	
+
 	var bibXpath = "//a[./text() = 'bib']"
 	if(doc.evaluate(bibXpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
 	  return "multiple"
@@ -40,21 +41,21 @@ function scrapeIndex(doc, items) {
 	}
 	else {
 		bibFileNodes = doc.evaluate('//a[substring(@href, string-length(@href)-3, 4) = ".bib"]', doc, null, XPathResult.ANY_TYPE, null);
-		
+
 		results = new Array();
 		doImport = false;
 
 		var bibFileNode = bibFileNodes.iterateNext();
-		
+
 		while (bibFileNode) {
 			var bibFileName = bibFileNode.getAttribute("href");
 			var bibFile = bibFileName.substring(0, bibFileName.length-4);
 
 			var bNodes = doc.evaluate('//a[@href="' + bibFileName + '"]/following-sibling::b[position()=1]', doc, null, XPathResult.ANY_TYPE, null);	// These nodes contain author information
-			
+
 			// Extract authors' last names
 			var authorLasts = new Array();
-			
+
 			var bNode = bNodes.iterateNext();
 			var authorsS = bNode.innerHTML;	// may include markup: potentially <author>, <first>, <von>, and/or <last> tags
 			authorsS = authorsS.replace(/[<][/]?author[>]/g, "");
@@ -88,7 +89,7 @@ function scrapeIndex(doc, items) {
 			// Prepare result for this item, which consists of the relative path to the .bib file (minus the extension)
 			// followed by a space and the authors' last names (abbreviated format)
 			var result = bibFile + " ";
-			
+
 			if (authorLasts.length<3)
 				result += authorLasts.join(" & ");
 			else
@@ -109,7 +110,7 @@ function scrapeIndex(doc, items) {
 		var authorsShort = results[i].substring(ii+1);
 		var fileName = fileRelPath.substring(fileRelPath.lastIndexOf("/")+1);
 		var bibFile = fileRelPath + ".bib";
-	
+
 		var pageurl = doc.location.href;
 		var lastSlash = pageurl.lastIndexOf("/");
 		var dirInUrl = pageurl.substring(0, lastSlash+1);
@@ -127,21 +128,21 @@ function scrapeIndex(doc, items) {
 		var attachments = new Array();
 		attachments.push({title:authorsShort + " " + year + ".pdf", mimeType:"application/pdf", url:pdf});
 
-		var type = "";	
+		var type = "";
 		if (pageurl.indexOf("/anthology-new/J/")>-1)
 			type = "journalArticle";
 		else
 			type = "conferencePaper";
-		
+
 		if (doImport)
 			callTranslator(bib, type, attachments);
-	
+
 	}
 }
 
 function callTranslator(bibFileURL, type, attachments) {
 	Zotero.Utilities.HTTP.doGet(bibFileURL, function(text) {
-		
+
 		// load BibTex translator
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
@@ -153,7 +154,7 @@ function callTranslator(bibFileURL, type, attachments) {
 			item.complete();
 		});
 		translator.translate();
-		
+
 	});
 }
 
@@ -161,16 +162,25 @@ function doWeb(doc, url) {
 	var searchResult = true;
 	if(searchResult) {
 		var possibleItems = scrapeIndex(doc, null);	// items to present to user
-		
+
 		items = Zotero.selectItems(possibleItems);	// items selected by the user
 		if(!items) return true;
-		
+
 		scrapeIndex(doc, items);
-		
+
 	} else {
 	  //not implemented yet
 		scrape(doc);
 	}
-	
+
 	Zotero.wait();
 }
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://aclweb.org/anthology-new/P/P93/",
+		"items": "multiple"
+	}
+]
+/** END TEST CASES **/
