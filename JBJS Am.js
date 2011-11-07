bf38b32855c86ef1f2a0813105d82e6857d39888
adam3smith <karcher@u.northwestern.edu>
2011-11-07 16:14:19 -0700

    Tests for JBJS, JMLR. Disable Journal of Electronig Publishing so that COinS will work.

--- JBJS Am.js
+++ JBJS Am.js
@@ -2,14 +2,14 @@
 	"translatorID": "8a325571-c2a8-417a-8a25-b1dca65154c3",
 	"label": "JBJS Am",
 	"creator": "Max Gordon and Avram Lyon",
-	"target": "^https?://(?:www\\.)?jbjs.org[^\\/]*/(?:searchresults|issue|article)\\.aspx",
+	"target": "^https?://(?:www\\.)?jbjs\\.org[^\\/]*/(?:searchresults|issue|article)\\.aspx",
 	"minVersion": "1.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsib",
-	"lastUpdated": "2011-09-05 06:38:30"
+	"lastUpdated": "2011-11-06 23:37:43"
 }
 
 /*
@@ -108,7 +108,7 @@ function scrape(doc,url)
 			case "citation_journal_title": if (!newItem.publicationTitle) newItem.publicationTitle = value; break;
 			case "citation_journal_abbrev": if (!newItem.journalAbbreviation) newItem.journalAbbreviation = value; break;
 			case "citation_author":    			
-				newItem.creators.push(Zotero.Utilities.cleanAuthor(value, "author", true));
+				newItem.creators.push(Zotero.Utilities.cleanAuthor(value, "author", false));
 			case "citation_title": if (!newItem.title) newItem.title = value; break;
 			case "citation_publisher": if (!newItem.publisher) newItem.publisher = value; break;
 			case "citation_date": if (!newItem.date && value != "NaN" && value != "") newItem.date = value; break;
@@ -122,7 +122,7 @@ function scrape(doc,url)
 			case "citation_doi": if (!newItem.DOI) newItem.DOI = value; break;
 			case "citation_reference": break; // These are citations in the paper-- Z doesn't use them
 			default:
-				Zotero.debug("Ignoring meta tag: " + tag + " => " + value);
+				//Zotero.debug("Ignoring meta tag: " + tag + " => " + value);
 		}
 	}
 		
@@ -136,43 +136,34 @@ function scrape(doc,url)
 	newItem.complete();
 }
 
+
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://www.jbjs.org/article.aspx?articleid=35426",
+		"url": "http://jbjs.org/issue.aspx?journalid=12&issueid=4324",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://jbjs.org/article.aspx?articleid=178197",
 		"items": [
 			{
 				"itemType": "journalArticle",
 				"creators": [
 					{
-						"firstName": "Delamarter",
-						"lastName": "Rick",
+						"firstName": "Kevin L.",
+						"lastName": "Ju",
 						"creatorType": "author"
 					},
 					{
-						"firstName": "Zigler",
-						"lastName": "Jack E. ",
+						"firstName": "David",
+						"lastName": "Zurakowski",
 						"creatorType": "author"
 					},
 					{
-						"firstName": "Balderston",
-						"lastName": "Richard A. ",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Cammisa",
-						"lastName": "Frank P. ",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Goldstein",
-						"lastName": "Jeffrey A. ",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Spivak",
-						"lastName": "Jeffrey M. ",
+						"firstName": "Mininder S.",
+						"lastName": "Kocher",
 						"creatorType": "author"
 					}
 				],
@@ -180,20 +171,21 @@ var testCases = [
 				"tags": [],
 				"seeAlso": [],
 				"attachments": [],
-				"url": "http://www.jbjs.org/article.aspx?articleid=35426",
+				"url": "http://jbjs.org/article.aspx?articleid=178197",
 				"publicationTitle": "The Journal of Bone and Joint Surgery (American)",
 				"journalAbbreviation": "JBJS",
-				"title": "Prospective, Randomized, Multicenter Food and Drug Administration Investigational Device Exemption Study of the ProDisc-L Total Disc Replacement Compared with Circumferential Arthrodesis for the Treatment of Two-Level Lumbar Degenerative Disc Disease: Results at Twenty-four Months",
+				"title": "Differentiating Between Methicillin-Resistant and Methicillin-Sensitive Staphylococcus aureus Osteomyelitis in Children: An Evidence-Based Clinical Prediction Algorithm",
 				"volume": "93",
-				"issue": "8",
-				"date": "4/20/2011 12:00:00 AM",
+				"issue": "18",
+				"date": "2011/09/21",
 				"ISSN": "0021-9355",
-				"DOI": "10.2106/JBJS.I.00680",
+				"DOI": "10.2106/JBJS.J.01154",
 				"publisher": "The Journal of Bone and Joint Surgery",
-				"pages": "705-715",
-				"abstractNote": "Background: \n  Disc replacement arthroplasty previously has been shown to be an effective alternative to spine fusion for the treatment of single-level lumbar degenerative disc disease. The purpose of the present study was to determine the twenty-four-month results of a clinical trial of the ProDisc-L total disc replacement as compared with spinal fusion for the treatment of degenerative disc disease at two contiguous vertebral levels from L3 to S1.Methods: \n  A total of 237 patients were treated in a randomized controlled trial designed as a non-inferiority study for regulatory application purposes. Blocked randomization was performed with use of a 2:1 ratio of total disc arthroplasty to circumferential arthrodesis. Evaluations, including patient self-assessments, physical and neurological examinations, and radiographic examinations, were performed preoperatively, six weeks postoperatively, and three, six, twelve, eighteen, and twenty-four months postoperatively.Results: \n  At twenty-four months, 58.8% (eighty-seven) of 148 patients in the total disc replacement group were classified as a statistical success, compared with 47.8% (thirty-two) of sixty-seven patients in the arthrodesis group; non-inferiority was demonstrated. The mean Oswestry Disability Index in both groups significantly improved from baseline (p < 0.0001); the mean percentage improvement for the total disc replacement group was significantly better than that for the arthrodesis group (p = 0.0282). An established clinical criterion for success, a =15-point improvement in the Oswestry Disability Index from baseline, occurred in 73.2% (109) of 149 patients in the total disc replacement group and 59.7% (thirty-seven) of sixty-two patients in the arthrodesis group. The Short Form-36 physical component scores were significantly better for the total disc replacement group as compared with the arthrodesis group (p = 0.0141 at twenty-four months). Visual analog scale scores for satisfaction significantly favored total disc replacement from three to twenty-four months. At twenty-four months, 78.2% (111) of 142 patients in the total disc replacement group and 62.1% (thirty-six) of fifty-eight patients in the arthrodesis group responded “yes” when asked if they would have the same surgery again. Lumbar spine range of motion on radiographs averaged 7.8° at the superior disc and 6.2° at the inferior disc in patients with total disc replacement. Reduction in narcotics usage significantly favored the total disc replacement group at twenty-four months after surgery (p = 0.0020).Conclusions: \n  Despite the relatively short duration of follow-up and design limitations, the present study suggests that two-level lumbar disc arthroplasty is an alternative to and offers clinical advantages in terms of pain relief and functional recovery in comparison with arthrodesis. Longer-term follow-up is needed to determine the risks for implant wear and/or degenerative segment changes.Level of Evidence: \n  Therapeutic Level I. See Instructions to Authors for a complete description of levels of evidence.",
+				"pages": "1693-1701",
+				"abstractNote": "Background: \n  Although osteomyelitis was once commonly due to methicillin-sensitive Staphylococcus aureus (MSSA), methicillin-resistant Staphylococcus aureus (MRSA)—which causes more virulent and invasive infections—has emerged as an increasingly important cause. Differentiating clinically between MRSA and MSSA can be challenging, but is necessary in order to promptly administer appropriate antibiotics and maintain vigilance against possible sequelae of MRSA osteomyelitis. The purpose of our study was to develop a clinical prediction algorithm to distinguish between MRSA and MSSA osteomyelitis in children.Methods: \n  A retrospective review of 129 children presenting with culture-proven Staphylococcus aureus osteomyelitis between 2000 and 2009 was performed. The demographics, symptoms, vital signs, and laboratory test values in the MSSA group (n = 118) and the MRSA group (n = 11) were compared with use of univariate analysis. Multivariate logistic regression with backward stepwise selection was then used to identify independent multivariate predictors of MRSA osteomyelitis, and each of these predictors was subjected to receiver operating characteristic curve analysis to determine the optimal cutoff value. Finally, a prediction algorithm for differentiating between MRSA and MSSA osteomyelitis on the basis of these independent predictors was constructed.Results: \n  Patients with MRSA osteomyelitis differed significantly from those with MSSA osteomyelitis with regard to non-weight-bearing status, antibiotic use at presentation, body temperature, hematocrit value, heart rate, white blood-cell count, platelet count, C-reactive protein level, and erythrocyte sedimentation rate. Four significant independent multivariate predictors were identified: a temperature of >38°C, a hematocrit value of <34%, a white blood-cell count of >12,000 cells/μL, and a C-reactive protein level of >13 mg/L. The predicted probability of MRSA osteomyelitis, determined on the basis of the number of these predictors that a child satisfied, was 92% for all four predictors, 45% for three, 10% for two, 1% for one, and 0% for zero predictors. Receiver operating characteristic curve analysis was used to evaluate the predictive accuracy of the number of multivariate predictors, and this analysis revealed a steep shoulder and an area under the curve of 0.94 (95% confidence interval, 0.88 to 1.00).Conclusions: \n  Our proposed set of four predictors provided excellent diagnostic performance in differentiating between MRSA and MSSA osteomyelitis in children, and thus would be able to guide patient management and facilitate timely antibiotic selection.Level of Evidence: \n  Diagnostic Level IV. See Instructions to Authors for a complete description of levels of evidence.",
 				"libraryCatalog": "JBJS Am",
-				"shortTitle": "Prospective, Randomized, Multicenter Food and Drug Administration Investigational Device Exemption Study of the ProDisc-L Total Disc Replacement Compared with Circumferential Arthrodesis for the Treatment of Two-Level Lumbar Degenerative Disc Disease"
+				"accessDate": "CURRENT_TIMESTAMP",
+				"shortTitle": "Differentiating Between Methicillin-Resistant and Methicillin-Sensitive Staphylococcus aureus Osteomyelitis in Children"
 			}
 		]
 	}
