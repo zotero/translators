562df681fff480145d326f3a84f5aff6e4615c21
adam3smith <karcher@u.northwestern.edu>
2011-11-09 00:14:48 -0700

    Fixes - Louver multiples, MIT weird attachment data. Tests: MIT, Matbugat, MPI for History of Science, MetaPress, Louvre

--- MetaPress.js
+++ MetaPress.js
@@ -1,14 +1,15 @@
 {
-	"translatorID":"62c0e36a-ee2f-4aa0-b111-5e2cbd7bb5ba",
-	"translatorType":4,
-	"label":"MetaPress",
-	"creator":"Michael Berkowitz",
-	"target":"https?://(.*).metapress.com/",
-	"minVersion":"1.0.0b4.r5",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2008-03-13 22:30:00"
+	"translatorID": "62c0e36a-ee2f-4aa0-b111-5e2cbd7bb5ba",
+	"label": "MetaPress",
+	"creator": "Michael Berkowitz",
+	"target": "https?://(.*)metapress.com/",
+	"minVersion": "1.0.0b4.r5",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2011-11-08 21:36:28"
 }
 
 function detectWeb(doc, url) {
@@ -45,7 +46,7 @@ function doWeb(doc, url) {
 						{url:item.url, title:"MetaPress Snapshot", mimeType:"text/html"},
 						{url:pdfurl, title:"MetaPress Full Text PDF", mimeType:"application/pdf"}
 					];
-					if (tags) item.tags = tags;
+					//if (tags) item.tags = tags;
 					if (item.abstractNote.substr(0, 8) == "Abstract") item.abstractNote = Zotero.Utilities.trimInternal(item.abstractNote.substr(8));
 					item.complete();
 				});
@@ -54,4 +55,59 @@ function doWeb(doc, url) {
 			});
 		}, function() {});
 	}
-}
+}/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://metapress.com/content/hh83064822430454/?p=0cf49d882cc547049b49cfba468cc263&pi=0",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"creators": [
+					{
+						"lastName": "Kawasaki",
+						"firstName": "K.",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Madachi-Yamamoto",
+						"firstName": "S.",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Yonemura",
+						"firstName": "D.",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"url": "http://dx.doi.org/10.1007/BF00143081",
+						"title": "MetaPress Snapshot",
+						"mimeType": "text/html"
+					},
+					{
+						"url": "http://metapress.com/content/hh83064822430454/fulltext.pdf",
+						"title": "MetaPress Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"publicationTitle": "Documenta Ophthalmologica",
+				"title": "Hyperosmolarity response of ocular standing potential as a clinical test for retinal pigment epithelium activity rhegmatogenous retinal detachment",
+				"volume": "57",
+				"issue": "3",
+				"pages": "175-180",
+				"date": "May 01, 1984",
+				"url": "http://dx.doi.org/10.1007/BF00143081",
+				"DOI": "10.1007/BF00143081",
+				"abstractNote": "The hyperosmolarity response of the ocular standing potential was recorded in unilateral rhegmatogenous retinal detachment (8 eyes) and in the fellow ‘healthy’ eye (8 eyes). The hyperosmolarity response was greatly suppressed (M-4 SD: M and SD indicate respectively the mean and the standard deviation in normal subjects) in all affected eyes (p < 0.005), and slightly abnormal in 2 fellow eyes. The L/D ratio was normal in 2 affected eyes and in all fellow eyes. The hyperosmolarity response in the affected eyes was still greatly suppressed 14 months after successful surgical treatment.",
+				"libraryCatalog": "MetaPress",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	}
+]
+/** END TEST CASES **/
