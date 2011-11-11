ec238a2cb105c1131406e4374c9c68f753331259
adam3smith <karcher@u.northwestern.edu>
2011-11-10 23:25:19 -0700

    Patents USPTO - fix multiples, disable references (broken). Add tests for Paris Review and Papers Past

--- Papers Past.js
+++ Papers Past.js
@@ -1,57 +1,58 @@
 {
-        "translatorID":"1b052690-16dd-431d-9828-9dc675eb55f6",
-        "label":"Papers Past",
-        "creator":"staplegun",
-        "target":"^http://paperspast\\.natlib\\.govt\\.nz",
-        "minVersion":"1.0",
-        "maxVersion":"",
-        "priority":100,
-        "inRepository":"1",
-        "translatorType":4,
-        "lastUpdated":"2010-09-14 19:04:32"
+	"translatorID": "1b052690-16dd-431d-9828-9dc675eb55f6",
+	"label": "Papers Past",
+	"creator": "staplegun",
+	"target": "^http://paperspast\\.natlib\\.govt\\.nz",
+	"minVersion": "1.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2011-11-10 23:06:21"
 }
 
 /*
-    Papers Past Translator - Parses historic digitised newspaper articles and creates Zotero-based metadata
-    Copyright (C) 2010 staplegun
+	Papers Past Translator - Parses historic digitised newspaper articles and creates Zotero-based metadata
+	Copyright (C) 2010 staplegun
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 function detectWeb(doc, url) {
 
   // a results parameter in URL means search hitlist
   if (url.match(/results=/) ) {
-    return "multiple";
+	return "multiple";
 
   } else {
 
-    // init variables
-    var namespace = doc.documentElement.namespaceURI;
-    var nsResolver = namespace ? function(prefix) {
-      if (prefix == "x" ) return namespace; else return null;
-    } : null;
-    var myXPath;
-    var myXPathObject;
+	// init variables
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? function(prefix) {
+	  if (prefix == "x" ) return namespace; else return null;
+	} : null;
+	var myXPath;
+	var myXPathObject;
 
-    // publication title in meta tags means have an article
-    myXPath          = '//meta[@name="newsarticle_publication"]/@content';
-    myXPathObject    = doc.evaluate(myXPath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-    var meta = myXPathObject.iterateNext().textContent;
-    if (meta.length > 0) {
-      return "newspaperArticle";
-    }
+	// publication title in meta tags means have an article
+	myXPath          = '//meta[@name="newsarticle_publication"]/@content';
+	myXPathObject    = doc.evaluate(myXPath, doc, nsResolver, XPathResult.ANY_TYPE, null);
+	var meta = myXPathObject.iterateNext().textContent;
+	if (meta.length > 0) {
+	  return "newspaperArticle";
+	}
   }
 }
 
@@ -60,30 +61,30 @@ function doWeb(doc, url) {
   // init variables
   var namespace = doc.documentElement.namespaceURI;
   var nsResolver = namespace ? function(prefix) {
-    if (prefix == "x" ) return namespace; else return null;
+	if (prefix == "x" ) return namespace; else return null;
   } : null;
 
   // hitlist page: compile hitlist titles, user selects which are wanted 
   // (add &zto=1 to URL for usage tracking)
   var articles = new Array();
   if (detectWeb(doc, url) == "multiple") {
-    var titlesXPath = '//div[@class="search-results"]/p/a';
-    var titles      = doc.evaluate(titlesXPath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-    var nextTitle;
-    var items       = new Array();
-    while (nextTitle = titles.iterateNext()) {
-      items[nextTitle.href+"&zto=1"] = nextTitle.textContent;
-    }
-    // presented to user - who reduces list to those selected
-    items = Zotero.selectItems(items);
-    // transfer this list to articles array
-    for (var i in items) {
-      articles.push(i);
-    }
+	var titlesXPath = '//div[@class="search-results"]/p/a';
+	var titles      = doc.evaluate(titlesXPath, doc, nsResolver, XPathResult.ANY_TYPE, null);
+	var nextTitle;
+	var items       = new Array();
+	while (nextTitle = titles.iterateNext()) {
+	  items[nextTitle.href+"&zto=1"] = nextTitle.textContent;
+	}
+	// presented to user - who reduces list to those selected
+	items = Zotero.selectItems(items);
+	// transfer this list to articles array
+	for (var i in items) {
+	  articles.push(i);
+	}
 
   // article page: just continue with single (current) page URL
   } else {
-    articles = [url+"&zto=1"];
+	articles = [url+"&zto=1"];
   }
 
   // process each selected article page URL
@@ -96,7 +97,7 @@ function scrape(doc) {
   // init variables
   var namespace = doc.documentElement.namespaceURI;
   var nsResolver = namespace ? function(prefix) {
-    if (prefix == "x" ) return namespace; else return null;
+	if (prefix == "x" ) return namespace; else return null;
   } : null;
   var myXPath;
   var myXPathObject;
@@ -145,9 +146,9 @@ function scrape(doc) {
   // save copy of entire web page as attachment
 	var attachments = new Array();
   attachments.push({
-    title:titleFixed + " : Article webpage",
-    mimeType:"text/html",
-    url:doc.location.href
+	title:titleFixed + " : Article webpage",
+	mimeType:"text/html",
+	url:doc.location.href
   });
 
   // find image scans and add as attachments
@@ -162,10 +163,52 @@ function scrape(doc) {
 		  title: titleFixed + " : Scan image part " + ++imgNo,
 		  mimeType: "image/gif",
 		  url: imgUrl
-    });
+	});
   }
   newItem.attachments = attachments;
 
   // finish
   newItem.complete();
 }
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://paperspast.natlib.govt.nz/cgi-bin/paperspast?a=q&hs=1&r=1&results=1&dafdq=&dafmq=&dafyq=&datdq=&datmq=&datyq=&pbq=&sf=&ssnip=&tyq=&t=0&txq=argentina&x=11&y=3&e=-------10--1----0--",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://paperspast.natlib.govt.nz/cgi-bin/paperspast?a=d&cl=search&d=EP19440218.2.61&srpos=7&e=-------10--1----0argentina--",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "Coup In Argentina : Article webpage",
+						"mimeType": "text/html",
+						"url": "http://paperspast.natlib.govt.nz/cgi-bin/paperspast?a=d&cl=search&d=EP19440218.2.61&srpos=7&e=-------10--1----0argentina--&zto=1"
+					},
+					{
+						"title": "Coup In Argentina : Scan image part 1",
+						"mimeType": "image/gif",
+						"url": "http://paperspast.natlib.govt.nz/cgi-bin/imageserver/imageserver.pl?oid=EP19440218.2.61&area=1&width=350&color=32&ext=gif&key="
+					}
+				],
+				"url": "http://paperspast.natlib.govt.nz/cgi-bin/paperspast?a=d&cl=search&d=EP19440218.2.61&srpos=7&e=-------10--1----0argentina--&zto=1",
+				"archive": "Papers Past",
+				"publicationTitle": "Evening Post",
+				"title": "Coup In Argentina",
+				"date": "1944-02-18",
+				"pages": "5",
+				"libraryCatalog": "Papers Past",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	}
+]
+/** END TEST CASES **/
--- Patents - USPTO.js
+++ Patents - USPTO.js
@@ -1,20 +1,24 @@
 {
-	"translatorID":"232e24fe-2f68-44fc-9366-ecd45720ee9e",
-	"translatorType":4,
-	"label":"Patents - USPTO",
-	"creator":"Bill McKinney",
-	"target":"^http://patft\\.uspto\\.gov/netacgi/nph-Parser.+",
-	"minVersion":"1.0.0b4.r1",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2011-01-11 04:31:00"
+	"translatorID": "232e24fe-2f68-44fc-9366-ecd45720ee9e",
+	"label": "Patents - USPTO",
+	"creator": "Bill McKinney",
+	"target": "^http://patft\\.uspto\\.gov/netacgi/nph-Parser.+",
+	"minVersion": "1.0.0b4.r1",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2011-11-10 23:24:16"
 }
 
 function detectWeb(doc, url) {
 	var re = new RegExp("^http://patft\.uspto\.gov/netacgi/nph-Parser");
-	if(re.test(doc.location.href)) {
-		return "book";
+	if (doc.title.match(/Search Results:/)){
+		return "multiple"
+	}
+	else if(re.test(doc.location.href)) {
+		return "patent";
 	} else {
 		return "multiple";
 	}
@@ -114,14 +118,13 @@ function scrape(doc) {
 		}
 	
 	}
-
-	newItem.references = tmpRefs;
+//References currenlty broken
+	//newItem.references = tmpRefs;
 	newItem.complete();
 }
 
 function doWeb(doc, url) {
-	var re = new RegExp("^http://patft\.uspto\.gov/netacgi/nph-Parser.+");
-	if(re.test(doc.location.href)) {
+if(detectWeb(doc, url) == "patent") {
 		scrape(doc);
 	} else {
 		var items = Zotero.Utilities.getItemArray(doc, doc, "^http://patft\.uspto\.gov/netacgi/nph-Parser.+");
@@ -141,4 +144,56 @@ function doWeb(doc, url) {
 		
 		Zotero.wait();
 	}
-}
+}/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PTXT&s1=krypto&OS=krypto&RS=krypto",
+		"items": [
+			{
+				"itemType": "patent",
+				"creators": [
+					{
+						"lastName": "Misev",
+						"firstName": "Ljubomir",
+						"creatorType": "inventor"
+					},
+					{
+						"lastName": "Valet",
+						"firstName": "Andreas",
+						"creatorType": "inventor"
+					},
+					{
+						"lastName": "Simmendinger",
+						"firstName": "Peter",
+						"creatorType": "inventor"
+					},
+					{
+						"lastName": "Jung",
+						"firstName": "Tunja",
+						"creatorType": "inventor"
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [],
+				"url": "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PTXT&s1=krypto&OS=krypto&RS=krypto",
+				"title": "United States Patent: 8003169 - Curing of coating induced by plasma",
+				"patentNumber": "8003169",
+				"issueDate": "August 23, 2011",
+				"assignee": "BASF SE\n (Ludwigshafen, \nDE)",
+				"abstractNote": "The Application relates to a method of curing various polymerisable\n     compositions, comprising a suitable photoinitiator, the curing being\n     effected by means of a plasma in plasma discharge chamber.",
+				"libraryCatalog": "Patents - USPTO",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"shortTitle": "United States Patent"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=0&f=S&l=50&TERM1=krypto&FIELD1=&co1=AND&TERM2=&FIELD2=&d=PTXT",
+		"items": "multiple"
+	}
+]
+/** END TEST CASES **/
