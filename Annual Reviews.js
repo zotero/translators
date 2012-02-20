403da6fb373e1098078fb8f46209681cc3f87d7e
Simon Kornblith <simon@simonster.com>
2012-02-20 18:16:36 -0500

    Add license and don't mark bookmarklet compatible (yet)

--- Annual Reviews.js
+++ Annual Reviews.js
@@ -8,9 +8,27 @@
 	"priority": 200,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "gcsib",
+	"browserSupport": "gcs",
 	"lastUpdated": "2012-02-20 03:54:19"
 }
+
+/**
+	Copyright (c) 2012 Aurimas Vinckevicius
+
+	This program is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License
+	as published by the Free Software Foundation, either version 3 of
+	the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public
+	License along with this program.  If not, see
+	<http://www.gnu.org/licenses/>.
+*/
 
 //add using BibTex
 function addByBibTex(doi) {
