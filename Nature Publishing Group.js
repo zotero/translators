8bb46b61db60d02d35900cc9c66614aa15986fa2
aurimasv <aurimas.dev@gmail.com>
2012-02-20 20:29:26 -0600

    Added license information Marked as "gcs" compatible

--- Nature Publishing Group.js
+++ Nature Publishing Group.js
@@ -8,9 +8,27 @@
 	"priority": 200,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "gcsib",
-	"lastUpdated": "2012-02-16 15:46:00"
+	"browserSupport": "gcs",
+	"lastUpdated": "2012-02-21 02:28:01"
 }
+
+/**
+	Copyright (c) 2012 Aurimas Vinckevicius
+	
+	This program is free software: you can redistribute it and/or
+	modify it under the terms of the GNU Affero General Public License
+	as published by the Free Software Foundation, either version 3 of
+	the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Affero General Public License for more details.
+	
+	You should have received a copy of the GNU Affero General Public
+	License along with this program. If not, see
+	<http://www.gnu.org/licenses/>.
+*/
 
 //get abstract
 function getAbstract(doc) {
