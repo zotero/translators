7ddf9fe3ee2bf926142b313020e63518c4daed64
adam3smith <karcher@u.northwestern.edu>
2012-03-12 21:00:25 -0600

    fix pdf title

--- ILO Labordoc.js
+++ ILO Labordoc.js
@@ -9,30 +9,8 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "g",
-	"lastUpdated": "2012-03-12 14:29:04"
+	"lastUpdated": "2012-03-12 20:59:36"
 }
-
-/*
-	***** BEGIN LICENSE BLOCK *****
-	
-	ILO Labordoc translator, Copyright © 2012 Sebastian Karcher
-	This file is part of Zotero.
-	
-	Zotero is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	Zotero is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-	
-	You should have received a copy of the GNU Affero General Public License
-	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
-	
-	***** END LICENSE BLOCK *****
-*/
 
 function detectWeb(doc, url) {
 	if (url.match(/\/search\?/)) return "multiple";
@@ -40,7 +18,7 @@ function detectWeb(doc, url) {
 }
 
 function doWeb(doc, url) {
-	if (detectWeb(doc, url) == "multiple") {
+	if (detectWeb(doc, url) == "multiple"){
 		var articles = [];
 		var items = {};
 		var titles = doc.evaluate('//tr[contains(@class, "tablesearchresults")]/td/a[@class="detailsTitle"]', doc, null, XPathResult.ANY_TYPE, null);
@@ -58,115 +36,180 @@ function doWeb(doc, url) {
 			Zotero.Utilities.processDocuments(articles, scrape, function () {
 				Zotero.done();
 			});
-			Zotero.wait();
+			Zotero.wait();	
 		});
-
-
-	} else {
-		scrape(doc, url);
+		
+		
 	}
+	
+	else{
+scrape(doc, url);
+}
 }
 
 
 
-function scrape(doc, url) {
-	var pdflink = ZU.xpath(doc, '//td[@class="detailsLinks"]/a[contains(@href, "pdf")]/@href')
+function scrape(doc, url){
+var pdflink = ZU.xpath(doc, '//td[@class="detailsLinks"]/a[contains(@href, "pdf")]/@href')
 
-	var newURL = url.replace(/\?/, "/export/xm?");
-	Zotero.Utilities.HTTP.doGet(newURL, function (text) {
+var newURL = url.replace(/\?/, "/export/xm?");
+Zotero.Utilities.HTTP.doGet(newURL, function(text) {
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("edd87d07-9194-42f8-b2ad-997c4c7deefd");
 		translator.setString(text);
-		translator.setHandler("itemDone", function (obj, item) {
-			if (pdflink != null) {
-				for (i in pdflink) {
-					item.attachments[i] = ({
-						url: pdflink[i].textContent,
-						title: "title",
-						mimeType: "application/pdf"
-					});
-				}
+		translator.setHandler("itemDone", function(obj, item) {
+		if (pdflink != null){
+			for (i in pdflink){
+					item.attachments[i]= ({url:pdflink[i].textContent, title: "ILO Labordoc Full Text PDF", mimeType: "application/pdf"});
 			}
-			item.complete();
-		});
+		}			
+item.complete();
+});	
 		translator.translate();
 
-	}) //doGet end
-
-} 
+}) //doGet end
+		
+	
+}//scrape end
 
 /** BEGIN TEST CASES **/
-var testCases = [{
-	"type": "web",
-	"url": "http://labordoc.ilo.org/record/440523?ln=en",
-	"items": [{
-		"itemType": "book",
-		"creators": [{
-			"lastName": "International Labour Organization",
-			"fieldMode": true
-		}, {
-			"lastName": "International Labour Organization",
-			"fieldMode": true
-		}],
-		"notes": [{
-			"note": "Second item on the agenda"
-		}, {
-			"note": "The following items are proposed for the agenda of the 103rd Session (2014) of the International Labour Conference: a recurrent discussion on the strategic objective of employment, the consolidation of three Recommendations on the right to information and consultation (as a follow-up to the conclusions of the Cartier Working Party), as well as the proposals for the agenda of the International Labour Conference contained in GB.312/INS/2/1 not selected for the agenda of the 102nd Session (2013) of the Conference"
-		}],
-		"tags": ["International Labour Conference", "agenda", "Conférence internationale du Travail", "ordre du jour", "Conferencia Internacional del Trabajo", "agenda", "ILO pub", "pub OIT", "pub OIT"],
-		"seeAlso": [],
-		"attachments": [{
-			"title": "title",
-			"mimeType": "application/pdf"
-		}, {
-			"title": "title",
-			"mimeType": "application/pdf"
-		}, {
-			"title": "title",
-			"mimeType": "application/pdf"
-		}, {
-			"title": "title",
-			"mimeType": "application/pdf"
-		}],
-		"title": "Agenda of the International Labour Conference: Proposals for the agenda of the 103rd Session (2014) of the Conference",
-		"place": "Geneva",
-		"publisher": "ILO",
-		"date": "2011",
-		"numPages": "9",
-		"callNumber": "GB.312/INS/2/2",
-		"libraryCatalog": "ILO Labordoc",
-		"shortTitle": "Agenda of the International Labour Conference"
-	}]
-}, {
-	"type": "web",
-	"url": "http://labordoc.ilo.org/record/441828?ln=en",
-	"items": [{
-		"itemType": "book",
-		"creators": [{
-			"lastName": "Statistical Office of the European Communities",
-			"fieldMode": true
-		}],
-		"notes": [{
-			"note": "Theme: Population and social conditions Mode of access : World Wide Web (available in electronic format only) Description based on the Internet version on the World Wide Web"
-		}, {
-			"note": "Provides details in relation to population ageing and setting the scene as regards the dynamics of demographic change, and details the past, present and projected future structure of the EU's population. Presents information in relation to the demand for healthcare services, as well as the budgetary implications facing governments as their populations continue to age. Contains information relating to the active participation of older generations within society, with a particular focus on inter-generational issues and also includes information on the leisure pursuits and social activities undertaken by older persons"
-		}],
-		"tags": ["older people", "older worker", "retired worker", "ageing population", "employment opportunity", "social security", "quality of life", "EU countries", "personnes âgées", "travailleur âgé", "travailleur retraité", "vieillissement de la population", "possibilités d'emploi", "sécurité sociale", "qualité de la vie", "pays de l'UE", "personas de edad avanzada", "trabajador de edad avanzada", "jubilado", "envejecimiento de la población", "oportunidades de empleo", "seguridad social", "calidad de la vida", "países de la UE", "statistical table", "EU pub", "tableau statistique", "pub UE", "cuadros estadísticos", "pub UE"],
-		"seeAlso": [],
-		"attachments": [{
-			"title": "title",
-			"mimeType": "application/pdf"
-		}],
-		"ISBN": "9789279215070",
-		"title": "Active ageing and solidarity between generations a statistical portrait of the European Union 2012",
-		"edition": "2012 ed",
-		"place": "Luxembourg",
-		"publisher": "Publications Office of the European Union",
-		"date": "2012",
-		"numPages": "141",
-		"series": "Statistical books",
-		"callNumber": "WWW ACCESS ONLY",
-		"libraryCatalog": "ILO Labordoc"
-	}]
-}]
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://labordoc.ilo.org/record/440523?ln=en",
+		"items": [
+			{
+				"itemType": "book",
+				"creators": [
+					{
+						"lastName": "International Labour Organization",
+						"fieldMode": true
+					},
+					{
+						"lastName": "International Labour Organization",
+						"fieldMode": true
+					}
+				],
+				"notes": [
+					{
+						"note": "Second item on the agenda"
+					},
+					{
+						"note": "The following items are proposed for the agenda of the 103rd Session (2014) of the International Labour Conference: a recurrent discussion on the strategic objective of employment, the consolidation of three Recommendations on the right to information and consultation (as a follow-up to the conclusions of the Cartier Working Party), as well as the proposals for the agenda of the International Labour Conference contained in GB.312/INS/2/1 not selected for the agenda of the 102nd Session (2013) of the Conference"
+					}
+				],
+				"tags": [
+					"International Labour Conference",
+					"agenda",
+					"Conférence internationale du Travail",
+					"ordre du jour",
+					"Conferencia Internacional del Trabajo",
+					"agenda",
+					"ILO pub",
+					"pub OIT",
+					"pub OIT"
+				],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "ILO Labordoc Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "ILO Labordoc Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "ILO Labordoc Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "ILO Labordoc Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"title": "Agenda of the International Labour Conference: Proposals for the agenda of the 103rd Session (2014) of the Conference",
+				"place": "Geneva",
+				"publisher": "ILO",
+				"date": "2011",
+				"numPages": "9",
+				"callNumber": "GB.312/INS/2/2",
+				"libraryCatalog": "ILO Labordoc",
+				"shortTitle": "Agenda of the International Labour Conference"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://labordoc.ilo.org/record/441828?ln=en",
+		"items": [
+			{
+				"itemType": "book",
+				"creators": [
+					{
+						"lastName": "Statistical Office of the European Communities",
+						"fieldMode": true
+					}
+				],
+				"notes": [
+					{
+						"note": "Theme: Population and social conditions Mode of access : World Wide Web (available in electronic format only) Description based on the Internet version on the World Wide Web"
+					},
+					{
+						"note": "Provides details in relation to population ageing and setting the scene as regards the dynamics of demographic change, and details the past, present and projected future structure of the EU's population. Presents information in relation to the demand for healthcare services, as well as the budgetary implications facing governments as their populations continue to age. Contains information relating to the active participation of older generations within society, with a particular focus on inter-generational issues and also includes information on the leisure pursuits and social activities undertaken by older persons"
+					}
+				],
+				"tags": [
+					"older people",
+					"older worker",
+					"retired worker",
+					"ageing population",
+					"employment opportunity",
+					"social security",
+					"quality of life",
+					"EU countries",
+					"personnes âgées",
+					"travailleur âgé",
+					"travailleur retraité",
+					"vieillissement de la population",
+					"possibilités d'emploi",
+					"sécurité sociale",
+					"qualité de la vie",
+					"pays de l'UE",
+					"personas de edad avanzada",
+					"trabajador de edad avanzada",
+					"jubilado",
+					"envejecimiento de la población",
+					"oportunidades de empleo",
+					"seguridad social",
+					"calidad de la vida",
+					"países de la UE",
+					"statistical table",
+					"EU pub",
+					"tableau statistique",
+					"pub UE",
+					"cuadros estadísticos",
+					"pub UE"
+				],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "ILO Labordoc Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"ISBN": "9789279215070",
+				"title": "Active ageing and solidarity between generations a statistical portrait of the European Union 2012",
+				"edition": "2012 ed",
+				"place": "Luxembourg",
+				"publisher": "Publications Office of the European Union",
+				"date": "2012",
+				"numPages": "141",
+				"series": "Statistical books",
+				"callNumber": "WWW ACCESS ONLY",
+				"libraryCatalog": "ILO Labordoc"
+			}
+		]
+	}
+]
 /** END TEST CASES **/
