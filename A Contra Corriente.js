7d177a41578d3e8855b80315a125e2434fc74f75
adam3smith <karcher@u.northwestern.edu>
2012-03-15 16:57:59 -0600

    run the 2 new translators through js-beautify

--- A Contra Corriente.js
+++ A Contra Corriente.js
@@ -31,13 +31,13 @@
 */
 
 
-function detectWeb(doc,url) {
-	var xpath='//meta[@name="citation_journal_title"]';
-		
+function detectWeb(doc, url) {
+	var xpath = '//meta[@name="citation_journal_title"]';
+
 	if (ZU.xpath(doc, xpath).length > 0) {
 		return "journalArticle";
 	}
-			
+
 	if (url.match(/issue\/view|search\/results/)) {
 		return "multiple";
 	}
@@ -45,38 +45,39 @@ function detectWeb(doc,url) {
 	return false;
 }
 
-function doWeb(doc,url)
-{
+function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
 		var hits = {};
 		var urls = [];
-		
+
 		//distinguish between search results and ToCs, which require different logic
-		if (url.match(/search\/results/)){
-		var rows = ZU.xpath(doc,"//table[@class='listing']//tr[@valign='top']");
-		if (!rows || rows.length == 0) results = ZU.xpath(doc,"//form[@name='search']/table[3]/tbody/tr/td[2]/a[@class='hiddenlink']");
-		
-		for (var i in rows) {
-			var titles = ZU.xpathText(rows[i], './td[2]');
-			var URL = ZU.xpathText(rows[i], './td[3]/a/@href');
-			hits[URL] = titles;
+		if (url.match(/search\/results/)) {
+			var rows = ZU.xpath(doc, "//table[@class='listing']//tr[@valign='top']");
+			if (!rows || rows.length == 0) results = ZU.xpath(doc, "//form[@name='search']/table[3]/tbody/tr/td[2]/a[@class='hiddenlink']");
+
+			for (var i in rows) {
+				var titles = ZU.xpathText(rows[i], './td[2]');
+				var URL = ZU.xpathText(rows[i], './td[3]/a/@href');
+				hits[URL] = titles;
+			}
+		} else {
+			var results = ZU.xpath(doc, "//td[@class='tocTitle']/a");
+			for (var i in results) {
+				hits[results[i].href] = results[i].textContent;
+			}
 		}
-		}
-		else {					
-		var results = ZU.xpath(doc,"//td[@class='tocTitle']/a");
-		for (var i in results) {
-			hits[results[i].href] = results[i].textContent;
-		}
-					}
-				
+
 		//Z.debug(hits);
-		Z.selectItems(hits, function(items) {
+		Z.selectItems(hits, function (items) {
 			if (items == null) return true;
 			for (var j in items) {
 				urls.push(j);
 			}
-			ZU.processDocuments(urls, function (myDoc) { 
-				doWeb(myDoc, myDoc.location.href) }, function () {Z.done()});
+			ZU.processDocuments(urls, function (myDoc) {
+				doWeb(myDoc, myDoc.location.href)
+			}, function () {
+				Z.done()
+			});
 
 			Z.wait();
 		});
@@ -84,78 +85,64 @@ function doWeb(doc,url)
 		// We call the Embedded Metadata translator to do the actual work
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
-		translator.setHandler("itemDone", function(obj, item) {
-				item.abstractNote = item.extra;
-				item.extra = '';
-				item.rights='';
-				item.complete();
-				});
+		translator.setHandler("itemDone", function (obj, item) {
+			item.abstractNote = item.extra;
+			item.extra = '';
+			item.rights = '';
+			item.complete();
+		});
 		translator.getTranslatorObject(function (obj) {
-				obj.doWeb(doc, url);
-				});
+			obj.doWeb(doc, url);
+		});
 	}
 }
 /** BEGIN TEST CASES **/
-var testCases = [
-	{
-		"type": "web",
+var testCases = [{
+	"type": "web",
+	"url": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/article/view/102",
+	"items": [{
+		"itemType": "journalArticle",
+		"creators": [{
+			"firstName": "José Luis",
+			"lastName": "Rénique",
+			"creatorType": "author"
+		}],
+		"notes": [],
+		"tags": ["Carlos Iván degregori", "historia", "Perú"],
+		"seeAlso": [],
+		"attachments": [{
+			"title": "Full Text PDF",
+			"mimeType": "application/pdf"
+		}, {
+			"title": "Snapshot"
+		}],
+		"itemID": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/article/view/102",
+		"title": "Carlos Iván Degregori: antropólogo del alma",
+		"publicationTitle": "A Contracorriente",
+		"date": "02/06/2011",
+		"reportType": "Text.Serial.Journal",
+		"letterType": "Text.Serial.Journal",
+		"manuscriptType": "Text.Serial.Journal",
+		"mapType": "Text.Serial.Journal",
+		"thesisType": "Text.Serial.Journal",
+		"websiteType": "Text.Serial.Journal",
+		"presentationType": "Text.Serial.Journal",
+		"postType": "Text.Serial.Journal",
+		"audioFileType": "Text.Serial.Journal",
+		"language": "en",
+		"volume": "8",
+		"issue": "3",
+		"pages": "i-vii",
+		"ISSN": "1548-7083",
 		"url": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/article/view/102",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"creators": [
-					{
-						"firstName": "José Luis",
-						"lastName": "Rénique",
-						"creatorType": "author"
-					}
-				],
-				"notes": [],
-				"tags": [
-					"Carlos Iván degregori",
-					"historia",
-					"Perú"
-				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot"
-					}
-				],
-				"itemID": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/article/view/102",
-				"title": "Carlos Iván Degregori: antropólogo del alma",
-				"publicationTitle": "A Contracorriente",
-				"date": "02/06/2011",
-				"reportType": "Text.Serial.Journal",
-				"letterType": "Text.Serial.Journal",
-				"manuscriptType": "Text.Serial.Journal",
-				"mapType": "Text.Serial.Journal",
-				"thesisType": "Text.Serial.Journal",
-				"websiteType": "Text.Serial.Journal",
-				"presentationType": "Text.Serial.Journal",
-				"postType": "Text.Serial.Journal",
-				"audioFileType": "Text.Serial.Journal",
-				"language": "en",
-				"volume": "8",
-				"issue": "3",
-				"pages": "i-vii",
-				"ISSN": "1548-7083",
-				"url": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/article/view/102",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"libraryCatalog": "tools.chass.ncsu.edu",
-				"abstractNote": "Remembranza de la reciente muerte (18 de Mayo de 2011) en Lima de Carlos&nbsp;Iv&aacute;n Degregori, uno de los intelectuales peruanos m&aacute;s&nbsp;importantes de las &uacute;ltimas d&eacute;cadas y uno de los estudiosos&nbsp;internacionales m&aacute;s destacados de la violencia pol&iacute;tica. A&nbsp;Contracorriente se suma a los innumerables homenajes que se&nbsp;tributan a su memoria. Su colega y amigo a lo largo de muchos&nbsp;a&ntilde;os, el historiador Jos&eacute; Luis R&eacute;nique, traza en esta nota el perfil&nbsp;humano, intelectual y pol&iacute;tico de Degregori. Al final incluimos&nbsp;tambi&eacute;n una lista de sus principales publicaciones.",
-				"shortTitle": "Carlos Iván Degregori"
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/issue/view/16/showToc",
-		"items": "multiple"
-	}
-]
+		"accessDate": "CURRENT_TIMESTAMP",
+		"libraryCatalog": "tools.chass.ncsu.edu",
+		"abstractNote": "Remembranza de la reciente muerte (18 de Mayo de 2011) en Lima de Carlos&nbsp;Iv&aacute;n Degregori, uno de los intelectuales peruanos m&aacute;s&nbsp;importantes de las &uacute;ltimas d&eacute;cadas y uno de los estudiosos&nbsp;internacionales m&aacute;s destacados de la violencia pol&iacute;tica. A&nbsp;Contracorriente se suma a los innumerables homenajes que se&nbsp;tributan a su memoria. Su colega y amigo a lo largo de muchos&nbsp;a&ntilde;os, el historiador Jos&eacute; Luis R&eacute;nique, traza en esta nota el perfil&nbsp;humano, intelectual y pol&iacute;tico de Degregori. Al final incluimos&nbsp;tambi&eacute;n una lista de sus principales publicaciones.",
+		"shortTitle": "Carlos Iván Degregori"
+	}]
+}, {
+	"type": "web",
+	"url": "http://tools.chass.ncsu.edu/open_journal/index.php/acontracorriente/issue/view/16/showToc",
+	"items": "multiple"
+}]
 /** END TEST CASES **/
--- African Journals Online.js
+++ African Journals Online.js
@@ -30,13 +30,13 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-function detectWeb(doc,url) {
-	var xpath='//meta[@name="citation_journal_title"]';
-		
+function detectWeb(doc, url) {
+	var xpath = '//meta[@name="citation_journal_title"]';
+
 	if (ZU.xpath(doc, xpath).length > 0) {
 		return "journalArticle";
 	}
-			
+
 	if (url.match(/index\/search|index.php\/[a-z]+$/)) {
 		return "multiple";
 	}
@@ -45,36 +45,37 @@ function detectWeb(doc,url) {
 }
 
 
-function doWeb(doc,url)
-{
+function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
 		var hits = {};
 		var urls = [];
-	//distinguish between search results and ToCs, which require different logic
-	if (url.match(/index\/search/)){
-		var rows = ZU.xpath(doc,"//table[@class='listing']//tr[@valign='top']");
-		if (!rows || rows.length == 0) results = ZU.xpath(doc,"//form[@name='search']/table[3]/tbody/tr/td[2]/a[@class='hiddenlink']");
-		
-		for (var i in rows) {
-			var titles = ZU.xpathText(rows[i], './td[3]');
-			var URL = ZU.xpathText(rows[i], './td[4]/a[1]/@href');
-			hits[URL] = titles;
+		//distinguish between search results and ToCs, which require different logic
+		if (url.match(/index\/search/)) {
+			var rows = ZU.xpath(doc, "//table[@class='listing']//tr[@valign='top']");
+			if (!rows || rows.length == 0) results = ZU.xpath(doc, "//form[@name='search']/table[3]/tbody/tr/td[2]/a[@class='hiddenlink']");
+
+			for (var i in rows) {
+				var titles = ZU.xpathText(rows[i], './td[3]');
+				var URL = ZU.xpathText(rows[i], './td[4]/a[1]/@href');
+				hits[URL] = titles;
+			}
+		} else {
+			var results = ZU.xpath(doc, "//td[@class='tocTitle']/a");
+			for (var i in results) {
+				hits[results[i].href] = results[i].textContent;
+			}
 		}
-		}
-		else {					
-		var results = ZU.xpath(doc,"//td[@class='tocTitle']/a");
-		for (var i in results) {
-			hits[results[i].href] = results[i].textContent;
-		}
-					}
-	
-		Z.selectItems(hits, function(items) {
+
+		Z.selectItems(hits, function (items) {
 			if (items == null) return true;
 			for (var j in items) {
 				urls.push(j);
 			}
-			ZU.processDocuments(urls, function (myDoc) { 
-				doWeb(myDoc, myDoc.location.href) }, function () {Z.done()});
+			ZU.processDocuments(urls, function (myDoc) {
+				doWeb(myDoc, myDoc.location.href)
+			}, function () {
+				Z.done()
+			});
 
 			Z.wait();
 		});
@@ -82,111 +83,88 @@ function doWeb(doc,url)
 		// We call the Embedded Metadata translator to do the actual work
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
-		translator.setHandler("itemDone", function(obj, item) {
-				item.abstractNote = item.extra;
-				item.extra = '';
-				item.complete();
-				});
-				
+		translator.setHandler("itemDone", function (obj, item) {
+			item.abstractNote = item.extra;
+			item.extra = '';
+			item.complete();
+		});
+
 		translator.getTranslatorObject(function (obj) {
-				obj.doWeb(doc, url);
-				});
+			obj.doWeb(doc, url);
+		});
 	}
 }
 /** BEGIN TEST CASES **/
-var testCases = [
-	{
-		"type": "web",
-		"url": "http://www.ajol.info/index.php/actat",
-		"items": "multiple"
-	},
-	{
-		"type": "web",
+var testCases = [{
+	"type": "web",
+	"url": "http://www.ajol.info/index.php/actat",
+	"items": "multiple"
+}, {
+	"type": "web",
+	"url": "http://www.ajol.info/index.php/thrb/article/view/63347",
+	"items": [{
+		"itemType": "journalArticle",
+		"creators": [{
+			"firstName": "Akinwumi A.",
+			"lastName": "Akinyede",
+			"creatorType": "author"
+		}, {
+			"firstName": "Alade",
+			"lastName": "Akintonwa",
+			"creatorType": "author"
+		}, {
+			"firstName": "Charles",
+			"lastName": "Okany",
+			"creatorType": "author"
+		}, {
+			"firstName": "Olufunsho",
+			"lastName": "Awodele",
+			"creatorType": "author"
+		}, {
+			"firstName": "Duro C.",
+			"lastName": "Dolapo",
+			"creatorType": "author"
+		}, {
+			"firstName": "Adebimpe",
+			"lastName": "Adeyinka",
+			"creatorType": "author"
+		}, {
+			"firstName": "Ademola",
+			"lastName": "Yusuf",
+			"creatorType": "author"
+		}],
+		"notes": [],
+		"tags": ["malaria", "knowledge", "treatment", "prevention", "HIV patients", "Nigeria"],
+		"seeAlso": [],
+		"attachments": [{
+			"title": "Full Text PDF",
+			"mimeType": "application/pdf"
+		}, {
+			"title": "Snapshot"
+		}],
+		"itemID": "http://www.ajol.info/index.php/thrb/article/view/63347",
+		"title": "Knowledge, treatment seeking and preventive practices in respect of malaria among patients with HIV at the Lagos University Teaching Hospital",
+		"publicationTitle": "Tanzania Journal of Health Research",
+		"rights": "Copyright for articles published in this journal is retained by the journal.",
+		"date": "17/10/2011",
+		"reportType": "Text.Serial.Journal",
+		"letterType": "Text.Serial.Journal",
+		"manuscriptType": "Text.Serial.Journal",
+		"mapType": "Text.Serial.Journal",
+		"thesisType": "Text.Serial.Journal",
+		"websiteType": "Text.Serial.Journal",
+		"presentationType": "Text.Serial.Journal",
+		"postType": "Text.Serial.Journal",
+		"audioFileType": "Text.Serial.Journal",
+		"language": "en",
+		"volume": "13",
+		"issue": "4",
+		"DOI": "10.4314/thrb.v13i4.63347",
+		"ISSN": "0856-6496",
 		"url": "http://www.ajol.info/index.php/thrb/article/view/63347",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"creators": [
-					{
-						"firstName": "Akinwumi A.",
-						"lastName": "Akinyede",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Alade",
-						"lastName": "Akintonwa",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Charles",
-						"lastName": "Okany",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Olufunsho",
-						"lastName": "Awodele",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Duro C.",
-						"lastName": "Dolapo",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Adebimpe",
-						"lastName": "Adeyinka",
-						"creatorType": "author"
-					},
-					{
-						"firstName": "Ademola",
-						"lastName": "Yusuf",
-						"creatorType": "author"
-					}
-				],
-				"notes": [],
-				"tags": [
-					"malaria",
-					"knowledge",
-					"treatment",
-					"prevention",
-					"HIV patients",
-					"Nigeria"
-				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot"
-					}
-				],
-				"itemID": "http://www.ajol.info/index.php/thrb/article/view/63347",
-				"title": "Knowledge, treatment seeking and preventive practices in respect of malaria among patients with HIV at the Lagos University Teaching Hospital",
-				"publicationTitle": "Tanzania Journal of Health Research",
-				"rights": "Copyright for articles published in this journal is retained by the journal.",
-				"date": "17/10/2011",
-				"reportType": "Text.Serial.Journal",
-				"letterType": "Text.Serial.Journal",
-				"manuscriptType": "Text.Serial.Journal",
-				"mapType": "Text.Serial.Journal",
-				"thesisType": "Text.Serial.Journal",
-				"websiteType": "Text.Serial.Journal",
-				"presentationType": "Text.Serial.Journal",
-				"postType": "Text.Serial.Journal",
-				"audioFileType": "Text.Serial.Journal",
-				"language": "en",
-				"volume": "13",
-				"issue": "4",
-				"DOI": "10.4314/thrb.v13i4.63347",
-				"ISSN": "0856-6496",
-				"url": "http://www.ajol.info/index.php/thrb/article/view/63347",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"libraryCatalog": "www.ajol.info",
-				"abstractNote": "The synergistic interaction between Human Immunodeficiency virus (HIV) disease and Malaria makes it mandatory for patients with HIV to respond appropriately in preventing and treating malaria. Such response will help to control the two diseases. This study assessed the knowledge of 495 patients attending the HIV clinic, in Lagos University Teaching Hospital, Nigeria.&nbsp; Their treatment seeking, preventive practices with regards to malaria, as well as the impact of socio &ndash; demographic / socio - economic status were assessed. Out of these patients, 245 (49.5 %) used insecticide treated bed nets; this practice was not influenced by socio &ndash; demographic or socio &ndash; economic factors.&nbsp; However, knowledge of the cause, knowledge of prevention of malaria, appropriate use of antimalarial drugs and seeking treatment from the right source increased with increasing level of education (p &lt; 0.05). A greater proportion of the patients, 321 (64.9 %) utilized hospitals, pharmacy outlets or health centres when they perceived an attack of malaria. Educational intervention may result in these patients seeking treatment from the right place when an attack of malaria fever is perceived."
-			}
-		]
-	}
-]
+		"accessDate": "CURRENT_TIMESTAMP",
+		"libraryCatalog": "www.ajol.info",
+		"abstractNote": "The synergistic interaction between Human Immunodeficiency virus (HIV) disease and Malaria makes it mandatory for patients with HIV to respond appropriately in preventing and treating malaria. Such response will help to control the two diseases. This study assessed the knowledge of 495 patients attending the HIV clinic, in Lagos University Teaching Hospital, Nigeria.&nbsp; Their treatment seeking, preventive practices with regards to malaria, as well as the impact of socio &ndash; demographic / socio - economic status were assessed. Out of these patients, 245 (49.5 %) used insecticide treated bed nets; this practice was not influenced by socio &ndash; demographic or socio &ndash; economic factors.&nbsp; However, knowledge of the cause, knowledge of prevention of malaria, appropriate use of antimalarial drugs and seeking treatment from the right source increased with increasing level of education (p &lt; 0.05). A greater proportion of the patients, 321 (64.9 %) utilized hospitals, pharmacy outlets or health centres when they perceived an attack of malaria. Educational intervention may result in these patients seeking treatment from the right place when an attack of malaria fever is perceived."
+	}]
+}]
 /** END TEST CASES **/
