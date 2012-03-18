ff329e495a788ba68c26292929ee640dafab0560
adam3smith <karcher@u.northwestern.edu>
2012-03-18 11:57:59 -0600

    update creator

--- CiteSeer.js
+++ CiteSeer.js
@@ -1,7 +1,7 @@
 {
 	"translatorID": "fa396dd4-7d04-4f99-95e1-93d6f355441d",
 	"label": "CiteSeer",
-	"creator": "Simon Kornblith",
+	"creator": "Sebastian Karcher",
 	"target": "^https?://citeseerx?\\.ist\\.psu\\.edu",
 	"minVersion": "2.1.9",
 	"maxVersion": "",
@@ -14,23 +14,23 @@
 
 /*
 	***** BEGIN LICENSE BLOCK *****
-	
-	Copyright © 2012 Sebastian Karcher 
+
+	Copyright © 2012 Sebastian Karcher
 	This file is part of Zotero.
-	
+
 	Zotero is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	Zotero is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
-	
+
 	***** END LICENSE BLOCK *****
 */
 
