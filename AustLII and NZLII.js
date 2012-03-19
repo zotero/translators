295d56be3a8766ded4cdeb3c77c04da8c8b7549f
adam3smith <karcher@u.northwestern.edu>
2012-03-19 00:57:05 -0600

    AustLII fix - this restores basic functionality. Due to the nature of the site it won't work perfectly and it will fail occasionally - but this is as much work as I'm willing to put into this and it works decently enough.

--- AustLII and NZLII.js
+++ AustLII and NZLII.js
@@ -1,29 +1,25 @@
 {
-	"translatorID":"5ed5ab01-899f-4a3b-a74c-290fb2a1c9a4",
-	"translatorType":4,
-	"label":"AustLII and NZLII",
-	"creator":"Bill McKinney",
-	"target":"http:\\/\\/www\\.(?:austlii\\.edu\\.au|nzlii\\.org)\\/(?:\\/cgi-bin\\/disp\\.pl\\/)?(?:au|nz)\\/cases\\/.+",
-	"minVersion":"1.0.0b4.r1",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2007-06-18 18:15:00"
+	"translatorID": "5ed5ab01-899f-4a3b-a74c-290fb2a1c9a4",
+	"label": "AustLII and NZLII",
+	"creator": "Bill McKinney and Sebastian Karcher",
+	"target": "^https?://www\\.(?:austlii\\.edu\\.au|nzlii\\.org)",
+	"minVersion": "1.0.0b4.r1",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2012-03-19 00:44:41"
 }
 
 function detectWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
-	var austliiRegexp = /^http:\/\/www\.(?:austlii\.edu\.au|nzlii\.org)\/(?:\/cgi-bin\/disp\.pl\/)?(?:au|nz)\/cases\/.+/
+	var austliiRegexp = /\/cases\/.+\d\.html/
 	if(austliiRegexp.test(url)) {
-		return "book";
+		return "case";
 	} else {
 		var aTags = doc.getElementsByTagName("a");
 		for(var i=0; i<aTags.length; i++) {
-			if(articleRegexp.test(aTags[i].href)) {
+			if(austliiRegexp.test(aTags[i].href)) {
 				return "multiple";
 			}
 		}
@@ -32,49 +28,137 @@ function detectWeb(doc, url) {
 
 
 function scrape(doc) {
-
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
 	var newItem = new Zotero.Item("case");
-	newItem.title = doc.title;
+	var voliss = ZU.xpathText(doc, '//h2');
+	var title = voliss.match(/.+?\[/)[0].replace(/\[/, "");
+	newItem.title =  newItem.caseName = title;
+	//ZU.capitalizeTitle(title.toLowerCase(), true);
 	newItem.url = doc.location.href;
-
-	var titleRegexp = /^(.+)\s+\[(\d+)\]\s+(\w+)\s(\d+)\s+\((\d+)\s+(\w+)\s+(\d+)\)/
-	var titleMatch = titleRegexp .exec(doc.title);
-	if (titleMatch ) {
-		newItem.caseName = titleMatch[1] + " [" + titleMatch[2] + "] " + titleMatch[3] + " " + titleMatch[4];
-		newItem.dateDecided = titleMatch[7] + " " + titleMatch[6] + " " + titleMatch[5];
-		newItem.court = titleMatch[3];	
-	} else {
-		newItem.caseName = doc.title;
-		newItem.dateDecided = "not found";
-	}
-	
+	var court = ZU.trim(voliss.match(/\].+?[\(\[]/)[0].replace(/[\]\(\[]/g, ""))
+	newItem.court = court.match(/[^0-9]+/)[0]
+	newItem.docketNumber=court.match(/\d+/)[0]
+	newItem.dateDecided = voliss.match(/\(\d[^\)]+\d{4}\)/)[0].replace(/[\(\)]/g, "");
+	newItem.attachments = [{ object:doc, title:"AustLII/NZLII snapshort", mimeType:"text/html"}];
 	newItem.complete();
 }
 
 function doWeb(doc, url) {
-	var austliiRegexp = /^http:\/\/www\.(?:austlii\.edu\.au|nzlii\.org)\/(?:\/cgi-bin\/disp\.pl\/)?(?:au|nz)\/cases\/.+/
+	var austliiRegexp = /\/cases\/.+\d\.html/;
 	if(austliiRegexp.test(url)) {
 		scrape(doc);
 	} else {
-		
 		var items = Zotero.Utilities.getItemArray(doc, doc, austliiRegexp);
-		items = Zotero.selectItems(items);
-		
-		if(!items) {
-			return true;
-		}
-		
 		var urls = new Array();
-		for(var i in items) {
-			urls.push(i);
-		}
-		
-		Zotero.Utilities.processDocuments(urls, scrape, function() { Zotero.done(); });
-		Zotero.wait();
+		Zotero.selectItems(items, function (items) {
+			if (!items) {
+				return true;
+			}
+			for (var i in items) {
+				urls.push(i);
+			}
+			Zotero.Utilities.processDocuments(urls, scrape, function () {
+				Zotero.done();
+			});
+			Zotero.wait();	
+		});
 	}
 }
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://www.austlii.edu.au/au/cases/cth/FamCA/2006/212.html",
+		"items": [
+			{
+				"itemType": "case",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"object": {
+							"location": {}
+						},
+						"title": "AustLII/NZLII snapshort",
+						"mimeType": "text/html"
+					}
+				],
+				"caseName": "C & M",
+				"title": "C & M",
+				"url": "http://www.austlii.edu.au/au/cases/cth/FamCA/2006/212.html",
+				"court": "FamCA",
+				"docketNumber": "212",
+				"dateDecided": "20 January 2006",
+				"libraryCatalog": "AustLII and NZLII",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.austlii.edu.au/au/cases/cth/FCA/2010/1.html",
+		"items": [
+			{
+				"itemType": "case",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"object": {
+							"location": {}
+						},
+						"title": "AustLII/NZLII snapshort",
+						"mimeType": "text/html"
+					}
+				],
+				"caseName": "Yeo, in the matter of AES Services (Aust) Pty Ltd (ACN 111 306 543) (Administrators Appointed)",
+				"title": "Yeo, in the matter of AES Services (Aust) Pty Ltd (ACN 111 306 543) (Administrators Appointed)",
+				"url": "http://www.austlii.edu.au/au/cases/cth/FCA/2010/1.html",
+				"court": "FCA",
+				"docketNumber": "1",
+				"dateDecided": "5 January 2010",
+				"libraryCatalog": "AustLII and NZLII",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.nzlii.org/nz/cases/NZSC/2008/1.html",
+		"items": [
+			{
+				"itemType": "case",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"object": {
+							"location": {}
+						},
+						"title": "AustLII/NZLII snapshort",
+						"mimeType": "text/html"
+					}
+				],
+				"caseName": "Bronwyn Estate Ltd and ors v Gareth Hoole and others",
+				"title": "Bronwyn Estate Ltd and ors v Gareth Hoole and others",
+				"url": "http://www.nzlii.org/nz/cases/NZSC/2008/1.html",
+				"court": "NZSC",
+				"docketNumber": "1",
+				"dateDecided": "8 February 2008",
+				"libraryCatalog": "AustLII and NZLII",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.austlii.edu.au/au/cases/act/ACTSC/2010/",
+		"items": "multiple"
+	}
+]
+/** END TEST CASES **/
