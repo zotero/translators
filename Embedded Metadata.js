935ef2e8a1c6427b97868ff62b0fbc661d93d499
aurimasv <aurimas.dev@gmail.com>
2012-03-20 05:00:39 -0500

    Fixed item type detection to look at RDF data when mixed with highwire. Added more detection code for PRISM and EPrints Added test

--- Embedded Metadata.js
+++ Embedded Metadata.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcs",
-	"lastUpdated": "2012-03-17 02:52:44"
+	"lastUpdated": "2012-03-20 04:53:11"
 }
 
 /*
@@ -142,6 +142,9 @@ function detectWeb(doc, url) {
 	getPrefixes(doc);
 
 	var metaTags = doc.getElementsByTagName("meta");
+	//itemTypes as determined from various schemas
+	var dcType, dcTypeGuess, eprintsType, eprintsTypeGuess, prismType,
+		prismTypeGuess, ogType, ogTypeGuess, hwType, hwTypeGuess;
 	for(var i=0; i<metaTags.length; i++) {
 		// Two formats allowed:
 		// 	<meta name="..." content="..." />
@@ -152,74 +155,316 @@ function detectWeb(doc, url) {
 		if (!tag) tag = metaTags[i].getAttribute("property");
 		var value = metaTags[i].getAttribute("content");
 		if(!tag || !value) continue;
+
 		// We allow three delimiters between the namespace and the property
 		var delimIndex = tag.indexOf('.');
 		if(delimIndex === -1) delimIndex = tag.indexOf(':');
 		if(delimIndex === -1) delimIndex = tag.indexOf('_');
 		if(delimIndex === -1) continue;
 
-		var prefix = tag.substr(0, delimIndex).toLowerCase();
+		//process everything in lower case
 		tag = tag.toLowerCase();
-		var prop = tag[delimIndex+1].toLowerCase()+tag.substr(delimIndex+2);
+		value = value.toLowerCase();
+
+		var prefix = tag.substr(0, delimIndex);
+		var prop = tag.substr(delimIndex+1);
 
 		var schema = _prefixes[prefix];
 		if(schema) {
 			_rdfPresent = true;
-			// If we have PRISM or eprints data, don't use the generic webpage icon
-			if (!_itemType && schema === _prefixes.prism || schema === _prefixes.eprints) {
-				return (_itemType = "journalArticle");
-			}
 
-			if (!_itemType && schema === _prefixes.og && prop === "type") {
-				switch (metaTags[i].getAttribute("content")) {
+			if (!ogType && prop === 'type' && schema === _prefixes.og) {
+				switch (value) {
 					case "video.movie":
 					case "video.episode":
 					case "video.tv_show":
 					case "video.other":
-						return "videoRecording";
+						ogType = "videoRecording";
+						break;
 					case "article":
-						return "newspaperArticle";
+						ogType = "newspaperArticle";
+						break;
 					case "book":
-						return "book";
+						ogType = "book";
+						break;
 					case "music.song":
 					case "music.album":
-						return "audioRecording";
+						ogType = "audioRecording";
+						break;
 					case "website":
-						return "webpage";
+						ogType = "webpage";
+						break;
+				}
+
+			} else if(!prismType && schema === _prefixes.prism) {
+				// see http://www.idealliance.org/specifications/prism/specifications/prism-controlled-vocabularies/prism-version-20-controlled-vocabu
+				switch(prop) {
+					//try to guess based on container
+					case 'aggregationtype':
+						switch(value) {
+							case 'book':
+								prismType = 'bookSection';
+								break;
+							case 'feed':
+								//could also be email
+								prismTypeGuess = 'blogPost';
+								break;
+							case 'journal':
+								prismType = 'journalArticle';
+								break;
+							case 'magazine':
+								prismType = 'magazineArticle';
+								break;
+							case 'newsletter':
+								prismType = 'newsArticle';
+								break;
+							//case 'pamphlet':
+							//case 'other':
+							//case 'manual':
+							//case 'catalog':
+						}
+						break;
+					/**This should tell us exactly what it is.
+					 * Very long list. only including things we can handle.
+					 * Also, multiple can be specified. Spec. recommends listing 
+					 * from most to least inclusive. We should probably only 
+					 * handle the latter.
+					 */
+					case 'genre':
+						switch(value) {
+							case 'abstract':
+							case 'acknowledgements':
+							case 'authorbio':
+							case 'bibliography':
+							case 'index':
+							case 'tableofcontents':
+								prismType = 'bookSection';
+								break;
+							case 'autobiography':
+							case 'biography':
+								prismType = 'book';
+								break;
+							case 'blogentry':
+								prismType = 'blogPost';
+								break;
+							case 'homepage':
+							case 'webliography':
+								prismType = 'webpage';
+								break;
+							case 'interview':
+								prismType = 'interview';
+								break;
+							case 'letters':
+								prismType = 'letter';
+								break;
+							case 'adaptation':
+							case 'analysis':
+								prismTypeGuess = 'journalArticle';
+								break;
+							case 'column':
+							case 'newsbulletin':
+							case 'opinion':
+								//magazine or newspaper
+								prismTypeGuess = 'newspaperArticle';
+								break;
+							case 'coverstory':
+							case 'essay':
+							case 'feature':
+							case 'insidecover':
+								//journal or magazine
+								prismTypeGuess = 'magazineArticle';
+								break;
+							//case 'advertorial':
+							//case 'advertisement':
+							//case 'brief':
+							//case 'chronology':
+							//case 'classifiedad':
+							//case 'correction':
+							//case 'cover':
+							//case 'coverpackage':
+							//case 'electionresults':
+							//case 'eventscalendar':
+							//case 'excerpt':
+							//case 'photoshoot':
+							//case 'featurepackage':
+							//case 'financialstatement':
+							//case 'interactivecontent':
+							//case 'legaldocument':
+							//case 'masthead':
+							//case 'notice':
+							//case 'obituary':
+							//case 'photoessay':
+							//case 'poem':
+							//case 'poll':
+							//case 'pressrelease':
+							//case 'productdescription':
+							//case 'profile':
+							//case 'quotation':
+							//case 'ranking':
+							//case 'recipe':
+							//case 'reprint':
+							//case 'response':
+							//case 'review':
+							//case 'schedule':
+							//case 'sidebar':
+							//case 'stockquote':
+							//case 'sectiontableofcontents':
+							//case 'transcript':
+							//case 'wirestory':
+						}
+						break;
+					case 'platform':
+						switch(value) {
+							case 'broadcast':
+								prismTypeGuess = 'tvBroadcast';
+								break;
+							case 'web':
+								prismTypeGuess = 'website';
+								break;
+						}
+						break;
+				}
+				//at least pretend we're a journalArticle if we have prism data
+				if(!prismTypeGuess) prismTypeGuess = 'journalArticle';
+
+			/** EPrints and PRISM use dc:type tags, so we handle those here as well */
+			} else if (schema === _prefixes.dc) {
+				/** It may be desirable in the future to try to determine
+				 * itemType by 'format', but for now we just look at 'type'
+				 * http://dublincore.org/documents/dcmi-terms/#terms-format
+				 * mimeType http://www.iana.org/assignments/media-types/
+				 */
+				if(prop === 'type') {
+					switch (value) {
+						//from http://www.ukoln.ac.uk/repositories/digirep/index/Eprints_Type_Vocabulary_Encoding_Scheme
+						case 'book':
+						case 'patent':
+						case 'report':
+						case 'thesis':
+							eprintsType = value;
+							break;
+						case 'bookitem':
+							eprintsType = 'bookSection';
+							break;
+						//case 'bookreview':
+						case 'conferenceitem':
+						case 'conferencepaper':
+						case 'conferenceposter':
+							eprintsType = 'conferencePaper';
+							break;
+						case 'journalitem':
+						case 'journalarticle':
+						case 'submittedjournalarticle':
+						case 'workingpaper':	//should this be here?
+							eprintsType = 'journalArticle';
+							break;
+						case 'newsitem':
+							eprintsType = 'newspaperArticle';
+							break;
+						case 'scholarlytext':
+							eprintsTypeGuess = 'journalArticle';
+							break;
+
+						//from http://www.idealliance.org/specifications/prism/specifications/prism-controlled-vocabularies/prism-12-controlled-vocabularies
+						//some are the same as eprints and are handled above
+						case 'article':
+							prismTypeGuess = 'journalArticle';
+							break;
+						case 'electronicbook':
+							prismType = 'book';
+							break;
+						case 'homepage':
+						case 'webpage':
+							prismType = 'webpage';
+							break;
+						case 'illustration':
+							prismType = 'artwork';
+							break;
+						case 'map':
+							prismType = 'map';
+							break;
+
+						//from http://dublincore.org/documents/dcmi-type-vocabulary/
+						//this vocabulary is much broader
+						case 'event':
+							//very broad, but has an associated location
+							dcTypeGuess = 'presentation';
+						case 'image':
+							//this includes almost any graphic, moving or not
+							dcTypeGuess = 'artwork';
+							break;
+						case 'movingimage':
+							//could be either film, tvBroadcast, or videoRecording
+							dcTypeGuess = 'videoRecording';
+							break;
+						case 'software':
+							dcTypeGuess = 'computerProgram';
+							break;
+						case 'sound':
+							//could be podcast, radioBroadcast, or audioRecording
+							dcTypeGuess = 'audioRecording';
+							break;
+						case 'stillimage':
+							//could be map or artwork
+							dcTypeGuess = 'artwork';
+							break;
+						case 'text':
+							//very broad
+							dcTypeGuess = 'journalArticle';
+							break;
+						//case 'collection':
+						//case 'dataset':
+						//case 'interactiveresource':
+						//case 'physicalobject':
+						//case 'service':
+					}
 				}
 			}
+			//at least pretend we're a journalArticle if we have eprints data
+			if(schema === _prefixes.eprint && !eprintTypeGuess) eprintTypeGuess = 'journalArticle';
 
-			if(_itemType) return _itemType;
-		} else {
+		//try to find some highwire metadata
+		} else if(!hwType) {
 			var shortTag = tag.slice(tag.lastIndexOf('citation_'));
 			switch(shortTag) {
 				case "citation_journal_title":
-					_itemType = "journalArticle";
+					hwType = "journalArticle";
 					break;
 				case "citation_technical_report_institution":
-					_itemType = "report";
+					hwType = "report";
 					break;
 				case "citation_conference_title":
 				case "citation_conference":
-					_itemType = "conferencePaper";
+					hwType = "conferencePaper";
 					break;
 				case "citation_book_title":
-					_itemType = "bookSection";
+					hwType = "bookSection";
 					break;
 				case "citation_dissertation_institution":
-					_itemType = "thesis";
+					hwType = "thesis";
 					break;
 				case "citation_title":		//fall back to journalArticle, since this is quite common
 				case "citation_series_title":	//possibly journal article, though it could be book
-					if(!_itemType) _itemType = "journalArticle";
+					hwTypeGuess = "journalArticle";
 					break;
 			}
 		}
+
+		/**try to terminate loop early if we have an item type we are sure about
+		 * but keep trying until we encounter some RDF data (if any), so we know
+		 * if we need to call the RDF translator
+		 */
+		 //we're confident with HW, OG, PRISM, and EPrints types
+		if(_rdfPresent && (hwType || ogType || prismType || eprintsType)) break;
 	}
 
-	if(!_rdfPresent && !_itemType) return false;
+	//set item type in order of confidence in data
+	_itemType = hwType || ogType || eprintsType || prismType || dcType ||
+				hwTypeGuess || ogTypeGuess || eprintsTypeGuess ||
+				prismTypeGuess || dcTypeGuess ||
+				//if we have RDF data, but nothing, then pretend it's webpage
+				(_rdfPresent? 'webpage' : false);
 
-	if(!_itemType) _itemType = "webpage";
 	return _itemType;
 }
 
@@ -703,6 +948,89 @@ var testCases = [
 				"url": "http://scholarworks.umass.edu/open_access_dissertations/508/",
 				"accessDate": "CURRENT_TIMESTAMP",
 				"libraryCatalog": "scholarworks.umass.edu"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.cabdirect.org/abstracts/20113303600.html;jsessionid=EAC5D88C979E0FF87338E0EDF2DCB90A",
+		"items": [
+			{
+				"itemType": "conferencePaper",
+				"creators": [
+					{
+						"firstName": "H. J.",
+						"lastName": "Bertschinger",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "C.",
+						"lastName": "Herbert",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "M. A. de B. V.",
+						"lastName": "Guimarães",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "T. E.",
+						"lastName": "Trigg",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "A.",
+						"lastName": "Human",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "V.",
+						"lastName": "Roberts",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [
+					"adverse effects",
+					"anoestrus",
+					"contraception",
+					"contraceptive properties",
+					"contraceptives",
+					"dinoprost",
+					"estradiol",
+					"FSH",
+					"GnRH",
+					"inbreeding",
+					"LH",
+					"oestrous cycle",
+					"ovaries",
+					"progesterone",
+					"reproduction",
+					"testes",
+					"wild animals",
+					"zoo animals"
+				],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"itemID": "http://www.cabdirect.org/abstracts/20113303600.html;jsessionid=EAC5D88C979E0FF87338E0EDF2DCB90A",
+				"title": "Managing the reproductive rate of some large carnivores using the GnRH super-agonist implant Suprelorin®.",
+				"publisher": "British Veterinary Zoological Society",
+				"institution": "British Veterinary Zoological Society",
+				"company": "British Veterinary Zoological Society",
+				"label": "British Veterinary Zoological Society",
+				"distributor": "British Veterinary Zoological Society",
+				"date": "2009",
+				"conferenceName": "British Veterinary Zoological Society Proceedings of the Autumn Meeting 2009, Park Inn, York, 7th and 8th November, 2009. Sex-aspects affecting exotics and wildlife.",
+				"language": "English",
+				"abstractNote": "CAB Direct is the most thorough and extensive source of reference in the applied life sciences, incorporating the leading bibliographic databases CAB Abstracts and Global Health",
+				"pages": "12-17",
+				"url": "http://www.cabdirect.org/abstracts/20113303600.html;jsessionid=EAC5D88C979E0FF87338E0EDF2DCB90A",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "www.cabdirect.org"
 			}
 		]
 	}
