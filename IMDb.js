6ad1665c59219278d52b316d2ea0e33fbf6c6a5b
Simon Kornblith <simon@simonster.com>
2012-06-09 14:14:59 -0400

    Update browserSupport

--- IMDb.js
+++ IMDb.js
@@ -8,7 +8,7 @@
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "gcsv",
+	"browserSupport": "gcsbv",
 	"lastUpdated": "2012-04-28 00:33:00"
 }
 
--- Wiley Online Library.js
+++ Wiley Online Library.js
@@ -8,7 +8,7 @@
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "gcs",
+	"browserSupport": "gcsb",
 	"lastUpdated": "2012-05-31 16:32:07"
 }
 
