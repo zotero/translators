5f3923be85e5e2a8fdef307f1171560f558ee721
adam3smith <karcher@u.northwestern.edu>
2012-06-16 12:37:54 -0600

    fix indenting and small error. Use break;

--- Library Catalog (Koha).js
+++ Library Catalog (Koha).js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "g",
-	"lastUpdated": "2012-06-14 19:02:36"
+	"lastUpdated": "2012-06-16 12:37:00"
 }
 
 /*
@@ -72,65 +72,130 @@ function scrape(marcurl) {
 		translator.setTranslator("a6ee60df-1ddc-4aae-bb25-45e0537be973");
 		translator.setString(text);
 		translator.setHandler("itemDone", function (obj, item) {
-		//editors get mapped as contributors - but so do many others who should be
-		// --> for books that don't have an author, turn contributors into editors.
-		if (newItem.itemType=="book"){
-		    var hasAuthor = false;
-		    for (var i in newItem.creators) {
-			if (newItem.creators[i].creatorType=="author") {
-				hasAuthor = true;
+			//editors get mapped as contributors - but so do many others who should be
+			// --> for books that don't have an author, turn contributors into editors.
+			if (item.itemType=="book"){
+		    	var hasAuthor = false;
+		    	for (var i in item.creators) {
+					if (item.creators[i].creatorType=="author") {
+						hasAuthor = true;
+						break;
+					}
+		    	}
+		    	if (!hasAuthor) {
+					for (var i in item.creators) {
+			    		if (item.creators[i].creatorType=="contributor") {
+						item.creators[i].creatorType="editor";
+			    		}
+					}
+		    	}
 			}
-		    }
-		    if (!hasAuthor) {
-			for (var i in newItem.creators) {
-			    if (newItem.creators[i].creatorType=="contributor") {
-				newItem.creators[i].creatorType="editor";
-			    }
-			}
-		    }
-		}
 			item.complete();
 		});
 		translator.translate();
-	});
-}
+	}) //doGet end
+} 
 
-function marcURL(url) {
+function marcURL(url){
 	var bibnumber = url.match(/(biblionumber=)(\d+)/)[2];
 	var host = url.match(/^.+cgi-bin\/koha\//)[0];
-	var marcURL = host + "opac-export.pl?format=utf8&op=export&bib=" + bibnumber + "save=Go";
+	var marcURL = host + "opac-export.pl?format=utf8&op=export&bib=" + bibnumber +"save=Go";
 	return marcURL;
 }
 /** BEGIN TEST CASES **/
-var testCases = [{
-	"type": "web",
-	"url": "https://ahuntsic.koha.ccsr.qc.ca/cgi-bin/koha/opac-detail.pl?biblionumber=145770",
-	"items": [{
-		"itemType": "book",
-		"creators": [{
-			"lastName": "Collège Ahuntsic",
-			"fieldMode": true
-		}],
-		"notes": [{
-			"note": "Les finissants et finissantes du Collège Ahuntsic soulignent les connaissances acquises au sein du profil Cinéma et médias en faisant découvrir leurs coups de coeur, « des films qui, à leur façon, les ont marqués par une recherche formelle, une originalité thématique, une authenticité sociologique et/ou une valeur historique. » -- P. 5"
-		}],
-		"tags": ["Cinéma"],
-		"seeAlso": [],
-		"attachments": [],
-		"title": "Synopsis: revue de cinéma",
-		"place": "Montréal",
-		"publisher": "Collège Ahuntsic",
-		"date": "2011",
-		"libraryCatalog": "KOHA",
-		"shortTitle": "Synopsis"
-	}]
-}, {
-	"type": "web",
-	"url": "https://catalogue.univ-lyon3.fr/cgi-bin/koha/opac-search.pl?q=thelen",
-	"items": "multiple"
-}, {
-	"type": "web",
-	"url": "http://nmwa.kohalibrary.com/cgi-bin/koha/opac-search.pl?q=image&idx=&submit=Search+Catalog",
-	"items": "multiple"
-}]
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://ahuntsic.koha.ccsr.qc.ca/cgi-bin/koha/opac-detail.pl?biblionumber=145770",
+		"items": [
+			{
+				"itemType": "book",
+				"creators": [
+					{
+						"lastName": "Collège Ahuntsic",
+						"fieldMode": true
+					}
+				],
+				"notes": [
+					{
+						"note": "Les finissants et finissantes du Collège Ahuntsic soulignent les connaissances acquises au sein du profil Cinéma et médias en faisant découvrir leurs coups de coeur, « des films qui, à leur façon, les ont marqués par une recherche formelle, une originalité thématique, une authenticité sociologique et/ou une valeur historique. » -- P. 5"
+					}
+				],
+				"tags": [
+					"Cinéma"
+				],
+				"seeAlso": [],
+				"attachments": [],
+				"title": "Synopsis: revue de cinéma",
+				"place": "Montréal",
+				"publisher": "Collège Ahuntsic",
+				"date": "2011",
+				"libraryCatalog": "Library Catalog (Koha)",
+				"shortTitle": "Synopsis"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://catalogue.univ-lyon3.fr/cgi-bin/koha/opac-search.pl?q=thelen",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://nmwa.kohalibrary.com/cgi-bin/koha/opac-search.pl?q=image&idx=&submit=Search+Catalog",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://ahuntsic.koha.ccsr.qc.ca/cgi-bin/koha/opac-detail.pl?biblionumber=71166",
+		"items": [
+			{
+				"itemType": "book",
+				"creators": [
+					{
+						"firstName": "Véronique",
+						"lastName": "Blanchard",
+						"creatorType": "editor"
+					},
+					{
+						"firstName": "Régis",
+						"lastName": "Revenin",
+						"creatorType": "editor"
+					},
+					{
+						"firstName": "Jean-Jacques",
+						"lastName": "Yvorel",
+						"creatorType": "editor"
+					}
+				],
+				"notes": [
+					{
+						"note": "« Le sujet préoccupe les jeunes eux-mêmes, bien entendu, mais aussi les adultes qui les entourent, parents, enseignants, travailleurs sociaux, médecins, juges, responsables politiques... Il inquiète, il paraît sulfureux, ou tabou. L'émotion suscitée dans les médias en témoigne : tournantes, tourisme sexuel, \"dédipix\" échangés, pilule du lendemain, difficulté d'assumer son homosexualité, etc. Sur les plateaux de télévision, à la radio ou dans les magazines, la parole est donnée aux \"psy\" ou aux \"témoins\", qui renforcent bien souvent les normes et les idées reçues. En contrepoint, ici sont réunis des historiens, des sociologues, des philosophes : trente-cinq chercheurs qui ont dépouillé les archives et mené l'enquête sur le terrain, en France, en Espagne, aux États-Unis, mais aussi en Argentine, au Cameroun, à Madagascar, en Iran, en Thaïlande, en Nouvelle-Zélande... Ce vaste panorama en trois volets, initiations, interdits, identités, permet de s'interroger sur notre actualité sexuelle : peu réprimée jusqu'à une époque récente, pourquoi la pédophilie est-elle aujourd'hui considérée comme le crime le plus abominable? Quel rôle joue la pornographie dans l'éducation sexuelle? La violence sexuelle des jeunes est-elle plus marquée aujourd'hui? Comment se construisent les identités de genre et les orientations sexuelles chez les jeunes? Faut-il avoir peur des jeunes et de la sexualité? Voilà de quoi alimenter la réflexion individuelle et les débats publics tout à la fois... » -- P. 4 de la couv"
+					}
+				],
+				"tags": [
+					"Adolescents",
+					"Sexualité",
+					"Abus sexuels à l'égard des enfants",
+					"Tourisme sexuel",
+					"Éducation sexuelle des jeunes",
+					"Prostitution juvénile"
+				],
+				"seeAlso": [],
+				"attachments": [],
+				"ISBN": "9782746713666",
+				"title": "Les jeunes et la sexualité: initiations, interdits, identités (XIXe-XXIe siècle)",
+				"place": "Paris",
+				"publisher": "Éditions Autrement",
+				"date": "2010",
+				"numPages": "407",
+				"series": "Mutations",
+				"seriesNumber": "262",
+				"callNumber": "HQ 27 J485",
+				"libraryCatalog": "Library Catalog (Koha)",
+				"shortTitle": "Les jeunes et la sexualité"
+			}
+		]
+	}
+]
 /** END TEST CASES **/
