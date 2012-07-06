406b5d88eff6201a5a08edea02a60bf885a885ee
adam3smith <karcher@u.northwestern.edu>
2012-07-05 22:55:28 -0600

    rewrite AGU Journals, mainly based on embedded metadata

--- AGU Journals.js
+++ AGU Journals.js
@@ -1,263 +1,118 @@
 {
 	"translatorID": "9220fa99-b936-430e-a8ea-43ca6cb04145",
 	"label": "AGU Journals",
-	"creator": "Ben Parr",
-	"target": "^https?://(?:www\\.|europa\\.)?agu.org",
-	"minVersion": "1.0.0b4.r1",
+	"creator": "Sebastian Karcher and Ben Parr",
+	"target": "^https?://(?:www\\.|europa\\.)?agu\\.org",
+	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "gcsibv",
-	"lastUpdated": "2012-02-21 15:26:30"
+	"browserSupport": "gcs",
+	"lastUpdated": "2012-07-05 21:00:25"
 }
 
-function detectWeb(doc,url)
-{
-	 var namespace = doc.documentElement.namespaceURI;
-	 var nsResolver = namespace ? function(prefix) {
-	 if (prefix == 'x') return namespace; else return null;
-	 } : null;
+/*
+   AGU Translator
+   Copyright (C) 2012 Sebastian Karcher and Ben Parr
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-	   var xpath;
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
 
-	   //abstract
-	   xpath='//p[@id="citation"]';
-	   if(doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext())
-			   { return "journalArticle"; }
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-	   //full text
-	   xpath='//frameset[@rows="98, *"]';
-	   if(doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext())
-			   { return "journalArticle"; }
+function detectWeb(doc, url) {
+	var xpath = '//meta[@name="citation_journal_title"]';
 
-	   //issue page
-	   xpath='//tr/td/p[@class="title"]';
-	   if(doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext())
-			   { return "multiple"; }
+	if (ZU.xpath(doc, xpath).length > 0) {
+		return "journalArticle";
+	}
 
-	   //Search  Page
-	   if(doc.title.indexOf("Query Results")>-1)
-			   {return "multiple";}
+	xpath = '//tr/td/p[@class="title"]';
+	if (ZU.xpath(doc, xpath).length > 0) {
+		return "multiple";
+	}
+
+	//Search  Page
+	if (url.match(/\?view=results/)) {
+		return "multiple";
+	}
+
+	return false;
 }
 
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) == "multiple") {
+		var citerow;
+		var linkrow;
+		var items = {};
+		var xpath;
+		var urls = [];
 
-function fixCaps(s)
-{
-	   if(s!='')
-	   {
-			   words=Zotero.Utilities.trimInternal(s).toLowerCase().split(" ");
-			   for (var j = 0 ; j < words.length ; j++)
-			   {
-					   if (j==0||(words[j][0] ==words[j][0].toLowerCase()&&words[j]!="or"&&words[j]!="and"&&words[j]!="of"&&words[j]!="in"))
-							   {   words[j]= words[j][0].toUpperCase() +words[j].substr(1);   }
-			   }
-			   return words.join(" ");
-	   }
-	   return '';
-}
-
-function scrape(doc,url)
-{
-	   var namespace = doc.documentElement.namespaceURI;
-	   var nsResolver = namespace ? function(prefix) {
-	   if (prefix == 'x') return namespace; else return null;
-	   } : null;
-
-	   var newItem=new Zotero.Item("journalArticle");
-	   var temp;
-	   var xpath;
-	   var row;
-	   var rows;
-
-	   newItem.url = doc.location.href;
-
-	   xpath='//p[@id="title"]';
-	   temp=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext();
-	   if(temp)
-			   {newItem.title=temp.textContent;}
-
-	   xpath='//span[@id="published"]';
-	   temp=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext();
-	   if(temp)
-	   {
-			   temp=Zotero.Utilities.trimInternal(temp.textContent).split(" ");;
-			   newItem.date=temp[1]+" "+temp[0]+", "+temp[2];
-	   }
-
-	   xpath='//p[@class="author"]';
-	   rows=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-	   var count=0;
-	   while(row=rows.iterateNext())
-			   {newItem.creators.push(Zotero.Utilities.cleanAuthor(row.textContent,"author"));
-			   count++;}
-
-	   xpath='//tr/td/p';
-	   temp=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-	   var temp2=temp.iterateNext();
-	   if(temp2)
-	   {
-			   for(var n=0;n<(3+2*count);n++)
-					   {temp2=temp.iterateNext();}
-			   newItem.abstractNote=Zotero.Utilities.trimInternal(temp2.textContent);
-	   }
-
-	   xpath='//p[@id="runhead"]';
-	   temp=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext();
-	   if(temp)
-	   {
-			   temp=Zotero.Utilities.trimInternal(temp.textContent).split(", ");
-			   newItem.publicationTitle=fixCaps(temp[0]);
-			   for(var n=1;temp[n];n++)
-			   {
-					   if(temp[n].indexOf("VOL")>-1)
-							   {newItem.volume=temp[n].replace('VOL. ','');}
-					   else if(temp[n].indexOf("NO.")>-1)
-							   {newItem.issue=temp[n].replace('NO. ','');}
-					   else if(temp[n].indexOf("doi:")>-1)
-							   {newItem.DOI=temp[n].replace('doi:','');}
-					   else if(temp[n+1])
-							   {newItem.pages=temp[n];}
-			   }
-	   }
-
-	   xpath='//p[@id="keywords"]';
-	   temp=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext();
-	   if(temp)
-	   {
-			   temp=Zotero.Utilities.trimInternal(temp.textContent.replace('Keywords:',''));
-			   newItem.tags=temp.replace('.','').split('; ');
-	   }
-	   xpath='//p[@id="citation"]/span[@id="journal"]';
-	   temp=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext();
-	   if(temp)
-			   {newItem.journalAbbreviation=temp.textContent;}
-
-	   newItem.complete();
-}
+		//issue page
+		xpath = '//tr/td/p[@class="title"]';
+		if (doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+			var titlerows = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+			xpath = '//tr/td/p[@class="pubdate"]/a';
+			var linkrows = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+			while (titlerow = titlerows.iterateNext()) {
+				linkrow = linkrows.iterateNext();
+				while (linkrow.textContent.indexOf("Abstract") < 0) {
+					linkrow = linkrows.iterateNext();
+				}
+				items[linkrow.href] = titlerow.textContent;
+			}
+		}
+		//search results
+		if (url.match(/\?view=results/)) {
+			var results = ZU.xpath(doc, '//p[@class="citation"]/a[1]');
+			for (var i in results) {
+				items[results[i].href] = results[i].textContent;
+			}
+		}
 
 
-function processList(items)
-{
-			   items = Zotero.selectItems(items);
-			   var uris=new Array();
+		Z.selectItems(items, function (items) {
+			if (items == null) return true;
+			for (var j in items) {
+				urls.push(j);
+			}
+			ZU.processDocuments(urls, function (myDoc) {
+				doWeb(myDoc, myDoc.location.href)
+			}, function () {});
+		});
 
-			  if (!items)
-					   {return true;}
-
-			  for (var i in items)
-					   {uris.push(i);}
-
-			 Zotero.Utilities.processDocuments(uris, scrape,function() {Zotero.done(); });
-			 Zotero.wait();
-
-			 return true;
-}
-
-function doWeb(doc,url)
-{
-	 var namespace = doc.documentElement.namespaceURI;
-	 var nsResolver = namespace ? function(prefix) {
-	 if (prefix == 'x') return namespace; else return null;
-	 } : null;
-
-	   //abstract
-	   var xpath='//p[@id="citation"]';
-	   if(doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext())
-	   {
-			   scrape(doc,url);
-			   return true;
-	   }
-
-	   //full text
-	   xpath='//frameset[@rows="98, *"]';
-	   if(doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext())
-	   {
-			   Zotero.Utilities.processDocuments(url+"0.shtml", scrape, function(){ Zotero.done(); });
-			   Zotero.wait();
-
-			   return true;
-	   }
-
-	   //issue page
-	   xpath='//tr/td/p[@class="title"]';
-	   if(doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext())
-	   {
-			   var titlerows=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-			   xpath='//tr/td/p[@class="pubdate"]/a';
-			   var linkrows=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-
-			   var titlerow;
-			   var linkrow;
-			   var items=new Array();
-
-			   while(titlerow=titlerows.iterateNext())
-			   {
-					   linkrow=linkrows.iterateNext();
-					   while(linkrow.textContent.indexOf("Abstract")<0)
-							   {linkrow=linkrows.iterateNext();}
-					   items[linkrow.href]=titlerow.textContent;
-			   }
-
-			   return processList(items);
-	   }
-
-
-	   //Search page
-	   if(doc.title.indexOf("Query Results")>-1)
-	   {
-			   //FASTFind Search
-
-			   xpath='//tr/td/h2';
-			   var tt=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE,null).iterateNext().textContent;
-			   if(tt.indexOf("FASTFIND")>-1)
-			   {
-					   xpath='//tr/td[1]/font';
-					   var citerows=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-					   xpath='//tr/td[2]/font/a';
-					   var linkrows=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-
-					   var citerow;
-					   var linkrow;
-					   var items=new Array();
-					   var temp;
-					   var title;
-
-					   while(citerow=citerows.iterateNext())
-					   {
-							   linkrow=linkrows.iterateNext();
-							   items[linkrow.href]=Zotero.Utilities.trimInternal(citerow.textContent);
-					   }
-					   return processList(items);
-			   }
-			   else
-			   {
-					   //Advanced Search
-
-					   xpath='//tr/td[1]/font/a';
-					   var titlerows=doc.evaluate(xpath, doc,nsResolver,XPathResult.ANY_TYPE, null);
-					   xpath='//tr/td[2]/font/a';
-					   var linkrows=doc.evaluate(xpath, doc, nsResolver,XPathResult.ANY_TYPE, null);
-
-					   var titlerow;
-					   var linkrow;
-					   var items=new Array();
-					   var temp;
-
-					   while(titlerow=titlerows.iterateNext())
-					   {
-							   linkrow=linkrows.iterateNext();
-							   while(linkrow.textContent.indexOf("Abstract")<0)
-									   {linkrow=linkrows.iterateNext();}
-
-							   items[linkrow.href]=titlerow.textContent;
-					   }
-					   return processList(items);
-			   }
-	   }
-
-}
-/** BEGIN TEST CASES **/
+	} else {
+		// We call the Embedded Metadata translator to do the actual work
+		var year = ZU.xpathText(doc, '//meta[@name="citation_year"]/@content')
+		var translator = Zotero.loadTranslator("import");
+		translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
+		translator.setHandler("itemDone", function (obj, item) {
+			//for older translators AGU put NaN into the date fiel, but has the year
+			if (item.title == item.title.toUpperCase()) {
+				Z.debug("here")
+				item.title = ZU.capitalizeTitle(item.title.toLowerCase(), true);
+			}
+			if (item.date.indexOf("NaN") != -1  && year) {
+				item.date = year;
+			}
+			//the keywords are nonsense
+			item.tags = [];
+			item.complete();
+		});
+		translator.getTranslatorObject(function (obj) {
+			obj.doWeb(doc, url);
+		});
+	}
+}/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
@@ -285,17 +140,37 @@ var testCases = [
 				"notes": [],
 				"tags": [],
 				"seeAlso": [],
-				"attachments": [],
-				"url": "http://www.agu.org/pubs/crossref/1950/JZ055i003p00235.shtml",
-				"title": "THEORETICAL CONSIDERATIONS REGARDING THE ALTITUDE OF THE LAYER RESPONSIBLE FOR THE NOCTURNAL EMISSION OF THE SODIUM D-LINES",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot"
+					}
+				],
+				"itemID": "http://www.agu.org/pubs/crossref/1950/JZ055i003p00235.shtml",
+				"title": "Theoretical Considerations Regarding the Altitude of the Layer Responsible for the Nocturnal Emission of the Sodium D-Lines",
 				"publicationTitle": "Journal of Geophysical Research",
+				"rights": "© 2008 American Geophysical Union",
 				"volume": "55",
 				"issue": "3",
-				"pages": "PP. 235-239",
-				"DOI": "195010.1029/JZ055i003p00235",
-				"journalAbbreviation": "J. Geophys. Res.",
-				"libraryCatalog": "AGU Journals",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"number": "3",
+				"patentNumber": "3",
+				"pages": "235-239",
+				"publisher": "American Geophysical Union",
+				"institution": "American Geophysical Union",
+				"company": "American Geophysical Union",
+				"label": "American Geophysical Union",
+				"distributor": "American Geophysical Union",
+				"date": "1950",
+				"ISSN": "0148-0227",
+				"language": "English",
+				"abstractNote": "The altitude of the layer responsible for the nocturnal emission of the D-lines is discussed theoretically. General arguments are used to show that if the luminosity originates directly from atmospheric sodium, its location must be far lower than is indicated by the recent observations of Barbier and Roach. Should these prove to be even approximately correct, some other source would apparently have to be invoked.",
+				"DOI": "10.1029/JZ055i003p00235",
+				"url": "http://www.agu.org/pubs/crossref/1950/JZ055i003p00235.shtml",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "www.agu.org"
 			}
 		]
 	},
@@ -320,19 +195,45 @@ var testCases = [
 				"notes": [],
 				"tags": [],
 				"seeAlso": [],
-				"attachments": [],
-				"url": "http://www.agu.org/pubs/crossref/1997/97EO00127.shtml",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot"
+					}
+				],
+				"itemID": "http://www.agu.org/pubs/crossref/1997/97EO00127.shtml",
 				"title": "Meier receives the Horton Medal",
-				"publicationTitle": "Eos",
-				"pages": "PAGE 188",
+				"publicationTitle": "Eos, Transactions American Geophysical Union",
+				"rights": "© 2008 American Geophysical Union",
 				"volume": "78",
 				"issue": "18",
-				"DOI": "199710.1029/97EO00127",
+				"number": "18",
+				"patentNumber": "18",
+				"pages": "188-189",
+				"publisher": "American Geophysical Union",
+				"institution": "American Geophysical Union",
+				"company": "American Geophysical Union",
+				"label": "American Geophysical Union",
+				"distributor": "American Geophysical Union",
+				"date": "1997",
+				"ISSN": "0096-3941",
+				"language": "English",
 				"journalAbbreviation": "Eos Trans. AGU",
-				"libraryCatalog": "AGU Journals",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"abstractNote": "Mark F. Meier received the Roger E. Horton Medal on December 17, 1996, at the AGU Fall Meeting Honor Ceremony in San Francisco, Calif. The Horton Medal acknowledges outstanding contributions to the geophysical aspects of hydrology. The award citation and Meier's response are given here.",
+				"DOI": "10.1029/97EO00127",
+				"url": "http://www.agu.org/pubs/crossref/1997/97EO00127.shtml",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "www.agu.org"
 			}
 		]
+	},
+	{
+		"type": "web",
+		"url": "http://europa.agu.org/?view=results&simp=1&q=test",
+		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
