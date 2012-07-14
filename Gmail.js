dc8a061ad738104368d59f8ca2a64800f99fafa4
aurimasv <aurimas.dev@gmail.com>
2012-07-13 21:12:06 -0500

    Use gmail's print page to fetch metadata.

--- Gmail.js
+++ Gmail.js
@@ -1,48 +1,58 @@
 {
-	"translatorID":"58a778cc-25e2-4884-95b3-6b22d7571183",
-	"translatorType":4,
-	"label":"Gmail",
-	"creator":"Michael Berkowitz",
-	"target":"http://mail.google.com/",
-	"minVersion":"1.0.0b4.r5",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2008-06-17 19:30:00"
+	"translatorID": "58a778cc-25e2-4884-95b3-6b22d7571183",
+	"label": "Gmail",
+	"creator": "Michael Berkowitz",
+	"target": "https?://mail.google.com/",
+	"minVersion": "1.0.0b4.r5",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2012-07-13 21:07:27"
 }
 
 function detectWeb(doc, url) {
-	if (url.match(/#inbox\/[\w\d]+/)) {
-		return "document";
+	//only trigger on print pages
+	var docOnLoad = doc.body.attributes.onload;
+	if(docOnLoad && docOnLoad.textContent == 'Print()') {
+		return 'email';
 	}
 }
 
 function doWeb(doc, url) {
-	var n = doc.documentElement.namespaceURI;
-	var ns = n ? function(prefix) {
-		if (prefix == 'x') return n; else return null;
-	} : null;
-	
-	var scripts = doc.evaluate('//script', doc, ns, XPathResult.ANY_TYPE, null);
-	var script;
-	var text = "";
-	while (script = scripts.iterateNext()) {
-		text += script.textContent;
+	var item = new Zotero.Item("email");
+	item.title = ZU.xpathText(doc,'(//div[@class="maincontent"]/table[1]//font)[1]');
+//	item.subject = item.title;
+
+	//use "to" and "from" from the first message in the thread
+	var from = ZU.xpathText(doc,
+		'(//div[@class="maincontent"]/table[@class="message"][1]//tr)[1]/td[1]//b');
+	if(from) item.creators.push(ZU.cleanAuthor(ZU.trimInternal(from), "author"));
+
+	//To, CC, and BCC(?) fields
+	var to = ZU.xpath(doc,
+		'//div[@class="maincontent"]/table[@class="message"][1]\
+			//font[@class="recipient"]/div');
+	for(var j=0, m=to.length; j<m; j++) {
+		var rec = to[j].textContent
+			.replace(/^[\s\S]+?:\s*/,'')	// remove "To:", "CC:", etc.,
+											//   but it could be something else in other languages
+			.replace(/\s*<.+?>\s*/g,'')		// remove email addresses if name exists
+			.split(/\s*,\s*/);				// There can be more than one email
+		for(var i=0, n=rec.length; i<n; i++) {
+			item.creators.push(ZU.cleanAuthor(ZU.trimInternal(rec[i]), "recipient"));
+		}
 	}
-	var ik = text.match(/ID_KEY:\"([^"]+)\"/)[1]
-	var th = url.match(/#inbox\/(.*)$/)[1];
-	var newurl = 'http://mail.google.com/mail/?ui=2&ik=' + ik + '&view=om&th=' + th;
-	Zotero.Utilities.HTTP.doGet(newurl, function(text) {
-		var item = new Zotero.Item("email");
-		var to = text.match(/\nTo:\s+([^\n]+)\n/)[1];
-		item.creators.push(Zotero.Utilities.cleanAuthor(Zotero.Utilities.trimInternal(to.replace(/<.*>/g, "")), "recipient"));
-		var from = text.match(/\nFrom:\s+([^\n]+)\n/)[1];
-		item.creators.push(Zotero.Utilities.cleanAuthor(Zotero.Utilities.trimInternal(from.replace(/<.*>/g, "")), "author"));
-		item.date = text.match(/\nDate:\s+(.*,\s+\d+\s+\w+\s+\d{4})/)[1];
-		item.subject = text.match(/\nSubject:\s+([^\n]+)/)[1];
-		if (item.subject == "") item.subject = "<No Subject>";
-		item.title = item.subject;
-		item.complete();
+
+	item.date = ZU.xpathText(doc,
+		'//div[@class="maincontent"]/table[@class="message"][1]//tr[1]/td[2]');
+	if(item.date) item.date = ZU.trimInternal(item.date);
+
+	item.attachments.push({
+		title:"Email Snapshot",
+		document: doc
 	});
-	Zotero.wait();
+
+	item.complete();
 }
