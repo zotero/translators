33133f9c6d0c11babe2a3dd4f0a94415142242e3
adam3smith <karcher@u.northwestern.edu>
2012-07-18 12:50:54 -0600

    adds PDF attachments, clean-up and multiple callback. Closes https://github.com/zotero/translators/issues/278

--- NASA ADS.js
+++ NASA ADS.js
@@ -9,21 +9,16 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2012-01-30 22:46:37"
+	"lastUpdated": "2012-07-18 12:24:51"
 }
 
 function detectWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
 	var singXpath = '//input[@name="bibcode"][@type="hidden"]';
 	var multXpath = '//input[@name="bibcode"][@type="checkbox"]';
 
-	if (doc.evaluate(multXpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
+	if (doc.evaluate(multXpath, doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
 		return "multiple";
-	} else if (doc.evaluate(singXpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()){
+	} else if (doc.evaluate(singXpath, doc, null, XPathResult.ANY_TYPE, null).iterateNext()){
 		return "journalArticle";
 	}
 }
@@ -31,29 +26,31 @@ function detectWeb(doc, url) {
 function parseRIS(bibcodes, hostname){
 	var getURL = "http://" + hostname + "/cgi-bin/nph-bib_query?"
 		+ bibcodes + "data_type=REFMAN&nocookieset=1";
-	Zotero.Utilities.HTTP.doGet(getURL, function(text){	
+	var pdfURL = "http://" + hostname + "/cgi-bin/nph-data_query?"
+		+ bibcodes + "link_type=ARTICLE";
+		//Z.debug(getURL)
+		//Z.debug(pdfURL)
+		Zotero.Utilities.HTTP.doGet(getURL, function(text){	
 		// load translator for RIS
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
 		translator.setString(text);
+		translator.setHandler("itemDone", function(obj, item) {
+			item.attachments = [{url:pdfURL, title: "NASA/ADS Full Text PDF", mimeType: "application/pdf"}];
+			item.complete();
+		});	
 		translator.translate();
-		Zotero.done();
 	}, function() {});
-	Zotero.wait();
 }
 
 function doWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
 
 	var singXpath = '//input[@name="bibcode"][@type="hidden"]';
 	var multXpath = '//input[@name="bibcode"][@type="checkbox"]';
 	var titleXpath = '//table/tbody/tr/td[4]'; //will find scores and titles
 	var hostname = doc.location.host
-	var bibElmts = doc.evaluate(multXpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-	var titleElmts = doc.evaluate(titleXpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
+	var bibElmts = doc.evaluate(multXpath, doc, null, XPathResult.ANY_TYPE, null);
+	var titleElmts = doc.evaluate(titleXpath, doc, null, XPathResult.ANY_TYPE, null);
 	var titleElmt;
 	var bibElmt;
 
@@ -65,16 +62,18 @@ function doWeb(doc, url) {
 			titleElmt = titleElmts.iterateNext(); //iterate a second time to avoid score
 			items[bibElmt.value] = Zotero.Utilities.trimInternal(titleElmt.textContent);
 		} while((bibElmt = bibElmts.iterateNext()) && (titleElmt = titleElmts.iterateNext()));
-		items = Zotero.selectItems(items);
-		if(!items) return true;
-
+	
+	Zotero.selectItems(items, function (items) {
+		if (!items) {
+			return true;
+		}	
 		var bibcodes="";
 		for(var bibcode in items) {
 			bibcodes = bibcodes + "bibcode="+encodeURIComponent(bibcode) + "&";
 		}
 		parseRIS(bibcodes, hostname);		
-				
-	} else if (bibElmt = doc.evaluate(singXpath, doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()){
+	});			
+	} else if (bibElmt = doc.evaluate(singXpath, doc, null, XPathResult.ANY_TYPE, null).iterateNext()){
 		var bibcode = bibElmt.value;
 		var bibcodes = "bibcode="+encodeURIComponent(bibcode) + "&";
 		parseRIS(bibcodes, hostname);
@@ -103,17 +102,21 @@ var testCases = [
 				"tags": [],
 				"seeAlso": [],
 				"attachments": [
-					{}
+					{
+						"title": "NASA/ADS Full Text PDF",
+						"mimeType": "application/pdf"
+					}
 				],
-				"url": "http://adsabs.harvard.edu/abs/1955ApJ...121..161S",
-				"abstractNote": "Abstract image available at: \nhttp://adsabs.harvard.edu/abs/1955ApJ...121..161S",
-				"DOI": "DOI: 10.1086/145971",
-				"libraryCatalog": "NASA ADS",
 				"title": "The Luminosity Function and Stellar Evolution.",
 				"publicationTitle": "The Astrophysical Journal",
 				"volume": "121",
 				"date": "January 1, 1955",
-				"pages": "161"
+				"pages": "161",
+				"url": "http://adsabs.harvard.edu/abs/1955ApJ...121..161S",
+				"abstractNote": "Abstract image available at: \nhttp://adsabs.harvard.edu/abs/1955ApJ...121..161S",
+				"DOI": "DOI: 10.1086/145971",
+				"libraryCatalog": "NASA ADS",
+				"accessDate": "CURRENT_TIMESTAMP"
 			}
 		]
 	}
