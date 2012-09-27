13c5546cc05c86695049717c2752ecc51623a359
wingfay <wingfay@gmail.com>
2012-09-27 12:28:53 +0800

    Update Insignia opac.js

--- Insignia opac.js
+++ Insignia opac.js
@@ -30,56 +30,38 @@ function doWeb(doc, url) {
 	if(detectWeb(doc,url)!="book"){
 		return;
 	}
-var xPathTitle = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Title"]';
-var xpathSeries = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Series"]';
-var xpathCallNumber = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="CallNo"]';
-var xpathEdition = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Edition"]';
-var xpathAuthor = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Author"]/a';
+	var xPathTitle = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Title"]';
+	var xpathSeries = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Series"]';
+	var xpathCallNumber = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="CallNo"]';
+	var xpathEdition = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Edition"]';
+	var xpathAuthor = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Author"]/a';
+	
+	var xpathISBN = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="ISBN"]';
+	var xpathPublisher = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="Publication"]';
+	var xpathDescription = '//table[@id="tbDetailInfo_Summary"]/tbody/tr/td/label[@name="Summary"]';
 
-var xpathISBN = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="ISBN"]';
-var xpathPublisher = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="Publication"]';
-var xpathDescription = '//table[@id="tbDetailInfo_Summary"]/tbody/tr/td/label[@name="Summary"]';
-
-var xpathPageNumber = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="Collation"]';
+	var xpathPageNumber = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="Collation"]';
 
 	var title = ZU.xpathText(doc, xPathTitle);
+	
 	if(title!=null && title.length>0){
 		
 		var item  = new Zotero.Item();
-				
-		var ISBN = ZU.xpathText(doc, xpathISBN);
-		
-
-		
-		var Series = ZU.xpathText(doc, xpathSeries);
-		
-		
-		var CallNumber = ZU.xpathText(doc, xpathCallNumber);
-		
-		var Edition = ZU.xpathText(doc, xpathEdition);
-
-		SaveAuthor(item,xpathAuthor,doc);
-		
-		var ISBN = ZU.xpathText(doc, xpathISBN);
-		
-		var Publisher = ZU.xpathText(doc, xpathPublisher);
-		
-		var Description = ZU.xpathText(doc, xpathDescription);
-		
-
-		item.itemType ="book";
+		item.itemType ="book";		
 		item.title = title;
-		item.ISBN = ISBN==null?"":ISBN;
-		item.series = Series==null?"":Series;
-		item.callNumber = CallNumber==null?"":CallNumber;
-		item.publisher = Publisher==null?"":Publisher;
-		item.edition= Edition==null?"":Edition;
-		item.notes=Description;
+		item.ISBN = ZU.xpathText(doc, xpathISBN);
+		item.series = ZU.xpathText(doc, xpathSeries);
+		item.callNumber = ZU.xpathText(doc, xpathCallNumber);
+		item.edition = ZU.xpathText(doc, xpathEdition);
+		item.publisher = ZU.xpathText(doc, xpathPublisher);
+		tem.notes = ZU.xpathText(doc, xpathDescription);
+		
+		
 		var textContent = ZU.xpathText(doc, xpathPageNumber);
 		if(textContent.match(/\w/) && textContent.match(" p.")){
 			item.numPages= textContent.split("p.")[0]+"p";
 		}
-
+		saveAuthor(item,xpathAuthor,doc);
 		item.complete();
 		
 	}
@@ -89,7 +71,7 @@ var xpathPageNumber = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label
 }
 
 
-function SaveAuthor(item,xpathAuthor,doc)
+function saveAuthor(item,xpathAuthor,doc)
 {
 	var contents = doc.evaluate(xpathAuthor, doc, null, XPathResult.ANY_TYPE, null);
 
@@ -99,8 +81,8 @@ function SaveAuthor(item,xpathAuthor,doc)
 	var author;
 	
 	while (author = contents.iterateNext()) {
-		
-		var newAuthor =Zotero.Utilities.cleanAuthor(author.textContent, "author");
+		// author like :Howe, James, 1946-    Donner, Andrea K., 1967-   Grant, Joan, 1931-curtis neitl, 1950-
+		var newAuthor = Zotero.Utilities.cleanAuthor(author.textContent, "author");
 		if(newAuthor.lastName.match(/^[0-9]/))
 		{
 			item.creators.push(Zotero.Utilities.cleanAuthor(newAuthor.firstName, "author"));
