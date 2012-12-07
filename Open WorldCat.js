dcf1215f4805c926a9fc6dec57233a1a3ff549d6
adam3smith <karcher@u.northwestern.edu>
2012-12-07 00:51:19 -0700

    fix ISBNs with X in lookup

--- Open WorldCat.js
+++ Open WorldCat.js
@@ -157,7 +157,7 @@ function doWeb(doc, url) {
 }
 
 function doSearch(item) {
-	ZU.processDocuments("http://www.worldcat.org/search?q=isbn%3A" + item.ISBN.replace(/[^0-9]/g, "") + "&=Search&qt=results_page", function (doc, url) {
+	ZU.processDocuments("http://www.worldcat.org/search?q=isbn%3A" + item.ISBN.replace(/[^0-9X]/g, "") + "&=Search&qt=results_page", function (doc, url) {
 		//we take the first search result and run scrape on it
 		if (doc.evaluate('//div[@class="name"]/a', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) { //search results view
 			var title = doc.evaluate('//div[@class="name"]/a[1]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
