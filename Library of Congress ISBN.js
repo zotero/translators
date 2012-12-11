9963de1c958d06721901e84d644097352f5cfbd5
adam3smith <karcher@u.northwestern.edu>
2012-12-11 16:33:08 -0700

    add ISBN search translator through LoC that precedes WorldCat. Works great in my tests. @aurimasv @simonster - any opinions on whether we should remove the call number or not?

---
+++ Library of Congress ISBN.js
@@ -0,0 +1,31 @@
+{
+    "translatorID": "c070e5a2-4bfd-44bb-9b3c-4be20c50d0d9",
+    "label": "Library of Congress ISBN",
+    "creator": "Simon Kornblith, Sebastian Karcher",
+    "target": "",
+    "minVersion": "3.0",
+    "maxVersion": "",
+    "priority": 99,
+    "inRepository": true,
+    "translatorType": 8,
+    "browserSupport": "gcsb",
+    "lastUpdated": "2012-12-11 16:10:34"
+}
+
+
+function detectSearch(item) {
+    return !!item.ISBN;
+}
+
+
+function doSearch(item) {
+    //Sends an SRU formatted as CQL to the library of Congress asking for marcXML back
+    //http://www.loc.gov/standards/sru/
+    ZU.doGet("http://z3950.loc.gov:7090/voyager?version=1.1&operation=searchRetrieve&query=dc.resourceIdentifier=" + item.ISBN.replace(/[^0-9X]/g, "") + "&maximumRecords=1", function (text) {
+        Z.debug(text);
+        var translator = Zotero.loadTranslator("import");
+        translator.setTranslator("edd87d07-9194-42f8-b2ad-997c4c7deefd");
+        translator.setString(text);
+        translator.translate();
+    });
+}
