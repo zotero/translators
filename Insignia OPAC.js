b45ca72dbe414686b1fe642fa1cc833c7de82826
aurimasv <aurimas.dev@gmail.com>
2013-01-04 02:33:43 -0600

    [Insignia OPAC] Some field cleanup. Closes #458

--- Insignia OPAC.js
+++ Insignia OPAC.js
@@ -1,6 +1,6 @@
 {
 	"translatorID": "abd7c626-6913-42d4-a05f-acc6683c69da",
-	"label": "Insignia opac",
+	"label": "Insignia OPAC",
 	"creator": "Niko",
 	"target": "https?://[^/]+/library/[^/?#]+\\.aspx",
 	"minVersion": "3.0",
@@ -9,11 +9,13 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcs",
-	"lastUpdated": "2012-11-26 15:49:00"
+	"lastUpdated": "2013-01-04 08:32:45"
 }
 
-
-
+/**
+ * No tests, but translator can be tested at
+ * http://www.insigniasoftware.com/library/WhatIsNew.aspx
+ **/
 
 function detectWeb(doc, url) {
 	var type =  ZU.xpathText(doc, '//input[@id="__ZoteroType"]/@value');
@@ -22,18 +24,14 @@ function detectWeb(doc, url) {
 		
 		var xPathTitle = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Title"]';
 		var title = ZU.xpathText(doc, xPathTitle);
-		if(title!=null && title.length>0){
+		if(title){
 			return "book";
 		}
 	}
-	return false;
 }
 
 
 function doWeb(doc, url) {
-	if(detectWeb(doc,url)!="book"){
-		return;
-	}
 	var xPathTitle = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Title"]';
 	var xpathSeries = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="Series"]';
 	var xpathCallNumber = '//table[@id="tbDetailInfo_Basic"]/tbody/tr/td/label[@name="CallNo"]';
@@ -47,38 +45,53 @@ function doWeb(doc, url) {
 
 	var xpathPageNumber = '//table[@id="tbDetailInfo_Publication"]/tbody/tr/td/label[@name="Collation"]';
 
-	var title = ZU.xpathText(doc, xPathTitle);
-	
-	if(title!=null && title.length>0){
-		
-		var item  = new Zotero.Item();
-		item.itemType ="book";		
-		item.title = title;
-		item.ISBN = ZU.xpathText(doc, xpathISBN);
-		item.series = ZU.xpathText(doc, xpathSeries);
-		item.callNumber = ZU.xpathText(doc, xpathCallNumber);
-		item.edition = ZU.xpathText(doc, xpathEdition);
-		item.publisher = ZU.xpathText(doc, xpathPublisher);
-		var note = ZU.xpathText(doc, xpathDescription);
-		if(note)
-			item.notes.push(note);
-		note = ZU.xpathText(doc, xpathDescription2);
-		if(note)
-			item.notes.push(note);
-		
-		//112 p. : col. ill. ; 15 cm..
-		var textContent = ZU.xpathText(doc, xpathPageNumber);
-		
-		if(textContent.match(/\w/) && textContent.indexOf(" p") != -1 ){
-			item.numPages= textContent.split(" p")[0];
-		}
-		saveAuthor(item,xpathAuthor,doc);
-		item.complete();
-		
-	}
-	
+	var item  = new Zotero.Item();
+	item.itemType ="book";		
+	item.title = ZU.xpathText(doc, xPathTitle);
+	var isbns = ZU.xpathText(doc, xpathISBN);
 
+	if(isbns) {
+		isbns = isbns.split(';');
+		for(var i=0, n=isbns.length; i<n; i++) {
+			isbns[i] = ZU.cleanISBN(isbns[i]);
+			if(!isbns[i]) {
+				isbns.splice(i,1);
+			}
+		}
+		item.ISBN = isbns.join('; ');
+	}
+
+	item.series = ZU.xpathText(doc, xpathSeries);
+	item.callNumber = ZU.xpathText(doc, xpathCallNumber);
+	item.edition = ZU.xpathText(doc, xpathEdition);
+
+	var publisher = ZU.xpathText(doc, xpathPublisher);
+	if(publisher) {
+		var dateRE = /[\s,[]+c?(\d{4})[\]\s.]*$/gi;
+		var date;
+		while(date = dateRE.exec(publisher)) {
+			if(!item.date) item.date = date[1];
+			publisher = publisher.substring(0, publisher.length - date[0].length);
+			dateRE.lastIndex = 0;
+		}
+		item.publisher = publisher;
+	}
+
+	var note = ZU.xpathText(doc, xpathDescription);
+	if(note)
+		item.notes.push(note);
+	note = ZU.xpathText(doc, xpathDescription2);
+	if(note)
+		item.notes.push(note);
 	
+	//112 p. : col. ill. ; 15 cm..
+	var textContent = ZU.xpathText(doc, xpathPageNumber);
+	
+	if(textContent){
+		item.numPages = textContent.split(" p")[0];
+	}
+	saveAuthor(item,xpathAuthor,doc);
+	item.complete();
 }
 
 
