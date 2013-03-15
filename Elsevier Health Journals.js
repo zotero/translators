40f9dcdc579c0b87335c0af7d959492a4e1c5d45
adam3smith <karcher@u.northwestern.edu>
2013-03-14 23:23:36 -0600

    update with Aurimas's suggestions

--- Elsevier Health Journals.js
+++ Elsevier Health Journals.js
@@ -5,15 +5,15 @@
 	"target": "/search/(quick|results)$|/article/[^/]+/(abstract|fulltext|references|images)$",
 	"minVersion": "1.0",
 	"maxVersion": "",
-	"priority": 100,
+	"priority": 250,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "g",
-	"lastUpdated": "2013-03-12 00:24:23"
+	"lastUpdated": "2013-03-14 23:22:35"
 }
 
 /*
-    Translator
+	Translator
    Copyright (C) 2013 Sebastian Karcher
 
    This program is free software: you can redistribute it and/or modify
@@ -31,6 +31,11 @@
 */
 
 function detectWeb(doc,url) {
+	
+	var footer = doc.getElementById('footer');
+	if(!footer) return;
+	var elsevierLink = footer.getElementsByTagName('a')[0];
+	if(!elsevierLink || elsevierLink.textContent.trim() != 'Elsevier') return;
 	var xpath='//meta[@name="citation_journal_title"]';
 		
 	if (ZU.xpath(doc, xpath).length > 0) {
@@ -38,36 +43,40 @@ function detectWeb(doc,url) {
 	}
 			
 	if (url.match(/\/search\/(results|quick)/)) {
-		return "multiple";
+		if (getMultiples(doc).length>0) return "multiple";
 	}
 	return false;
 }
 
+function getMultiples(doc) {
+	var table = doc.getElementById('searchResult');
+	return ZU.xpath(table, './tbody/tr/td[.//a[@class="viewoption" and @onclick and (normalize-space(text()) = "Full Text"\
+					or normalize-space(text()) = "Abstract") and (contains(@onclick,"/fulltext") or contains(@onclick, "/abstract"))]]');
+}
 
+ 
 function doWeb(doc,url)
 {
 	if (detectWeb(doc, url) == "multiple") {
 		var hits = {};
 		var urls = [];
-		var resultsrow = ZU.xpath(doc,"//table[@id='searchResult']/tbody/tr");
+		var results = getMultiples(doc)
 		var link;
-		for (var i in resultsrow) {
-			link = ZU.xpathText(resultsrow[i], './/a[@class="viewoption" and contains(text(), "Full Text")]/@onclick');
-			if (!link) link = ZU.xpathText(resultsrow[i], './/a[@class="viewoption" and contains(text(), "Abstract")]/@onclick');
+		for (var i in results) {
+			link = ZU.xpathText(results[i], './/a[@class="viewoption" and contains(text(), "Full Text")]/@onclick');
+			if (!link) link = ZU.xpathText(results[i], './/a[@class="viewoption" and contains(text(), "Abstract")]/@onclick');
 			link = link.match(/http:\/\/.+(fulltext|abstract)/)[0];
-			hits[link] = ZU.xpathText(resultsrow[i], './td[@class="focus"]/text()').trim();
+			hits[link] = results[i].textContent.trim();
 		}
 		Z.selectItems(hits, function(items) {
 			if (items == null) return true;
 			for (var j in items) {
 				urls.push(j);
 			}
-			ZU.processDocuments(urls, function (myDoc) { 
-				doWeb(myDoc, myDoc.location.href) });
-
+		ZU.processDocuments(urls, doWeb);
 		});
 	} else {
-		var abstract = ZU.xpathText(doc, '//div[@class="tContent"]/*[self::h3 or (self::p and not(self::p[@class="note"]))]')
+		var abstract = ZU.xpathText(doc, '//div[@class="tContent"]/*[self::h3 or (self::p and not(self::p[@class="note"]))]', null, '\n')
 		//Z.debug(abstract)
 		var keywords = ZU.xpath(doc, '//div[@class="tContent"]/p/span[contains(@class, "keyword")]')
 		// We call the Embedded Metadata translator to do the actual work
@@ -76,15 +85,18 @@ function doWeb(doc,url)
 		translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
 		translator.setDocument(doc);
 		translator.setHandler('itemDone', function(obj, item) {
+			var m;
+			if(item.publicationTitle && (m = item.publicationTitle.match(/^(.+), (the)$/i) )){
+				item.publicationTitle = m[2] + ' ' + m[1];
+			}
 			for (var i in keywords){
-				if (keywords[i].textContent.length>0){
-					item.tags.push(keywords[i].textContent.trim())
-				}			
+				var kw = keywords[i].textContent.trim();
+				if(kw) item.tags.push(kw);		
 			}
 			item.abstractNote = abstract;
 			item.complete();
 		});
-	translator.translate();
+		translator.translate();
 	}
 }
 /** BEGIN TEST CASES **/
@@ -152,7 +164,7 @@ var testCases = [
 				],
 				"title": "Risk of Falls and Major Bleeds in Patients on Oral Anticoagulation Therapy",
 				"date": "August 2012",
-				"publicationTitle": "American Journal of Medicine, The",
+				"publicationTitle": "The American Journal of Medicine",
 				"volume": "125",
 				"issue": "8",
 				"publisher": "Elsevier",
@@ -162,7 +174,7 @@ var testCases = [
 				"url": "http://www.amjmed.com/article/S0002-9343(12)00352-X/abstract",
 				"accessDate": "CURRENT_TIMESTAMP",
 				"libraryCatalog": "www.amjmed.com",
-				"abstractNote": "Background, The risk of falls is the most commonly cited reason for not providing oral anticoagulation, although the risk of bleeding associated with falls on oral anticoagulants is still debated. We aimed to evaluate whether patients on oral anticoagulation with high falls risk have an increased risk of major bleeding., Methods, We prospectively studied consecutive adult medical patients who were discharged on oral anticoagulants. The outcome was the time to a first major bleed within a 12-month follow-up period adjusted for age, sex, alcohol abuse, number of drugs, concomitant treatment with antiplatelet agents, and history of stroke or transient ischemic attack., Results, Among the 515 enrolled patients, 35 patients had a first major bleed during follow-up (incidence rate: 7.5 per 100 patient-years). Overall, 308 patients (59.8%) were at high risk of falls, and these patients had a nonsignificantly higher crude incidence rate of major bleeding than patients at low risk of falls (8.0 vs 6.8 per 100 patient-years, P=.64). In multivariate analysis, a high falls risk was not statistically significantly associated with the risk of a major bleed (hazard ratio 1.09; 95% confidence interval, 0.54-2.21). Overall, only 3 major bleeds occurred directly after a fall (incidence rate: 0.6 per 100 patient-years)., Conclusions, In this prospective cohort, patients on oral anticoagulants at high risk of falls did not have a significantly increased risk of major bleeds. These findings suggest that being at risk of falls is not a valid reason to avoid oral anticoagulants in medical patients."
+				"abstractNote": "Background\nThe risk of falls is the most commonly cited reason for not providing oral anticoagulation, although the risk of bleeding associated with falls on oral anticoagulants is still debated. We aimed to evaluate whether patients on oral anticoagulation with high falls risk have an increased risk of major bleeding.\nMethods\nWe prospectively studied consecutive adult medical patients who were discharged on oral anticoagulants. The outcome was the time to a first major bleed within a 12-month follow-up period adjusted for age, sex, alcohol abuse, number of drugs, concomitant treatment with antiplatelet agents, and history of stroke or transient ischemic attack.\nResults\nAmong the 515 enrolled patients, 35 patients had a first major bleed during follow-up (incidence rate: 7.5 per 100 patient-years). Overall, 308 patients (59.8%) were at high risk of falls, and these patients had a nonsignificantly higher crude incidence rate of major bleeding than patients at low risk of falls (8.0 vs 6.8 per 100 patient-years, P=.64). In multivariate analysis, a high falls risk was not statistically significantly associated with the risk of a major bleed (hazard ratio 1.09; 95% confidence interval, 0.54-2.21). Overall, only 3 major bleeds occurred directly after a fall (incidence rate: 0.6 per 100 patient-years).\nConclusions\nIn this prospective cohort, patients on oral anticoagulants at high risk of falls did not have a significantly increased risk of major bleeds. These findings suggest that being at risk of falls is not a valid reason to avoid oral anticoagulants in medical patients."
 			}
 		]
 	},
@@ -274,7 +286,7 @@ var testCases = [
 				"url": "http://www.celltherapyjournal.org/article/S1465-3249(12)70632-1/abstract",
 				"accessDate": "CURRENT_TIMESTAMP",
 				"libraryCatalog": "www.celltherapyjournal.org",
-				"abstractNote": "Background aims, Umbilical cord blood (UCB) is a source of hematopoietic stem cells that initially was used exclusively for the hematopoietic reconstitution of pediatric patients. It is now suggested for use for adults as well, a fact that increases the pressure to obtain units with high cellularity. Therefore, the optimization of UCB processing is a priority., Methods, The present study focused on parameters influencing total nucleated cell (TNC), mononucleated cell (MNC) and CD34 + cell (CD34C) recovery after routine volume reduction of 1553 UCB units using hydroxyethyl starch-induced sedimentation with an automated device, under routine laboratory conditions., Results, We show that the unit volume rather than the TNC count significantly affects TNC, MNC and CD34C processing efficiency (PEf), and this in a non-linear fashion: when units were sampled according to the collection volume, including pre-loaded anticoagulant (gross volume), PEf increased up to a unit volume of 110–150mL and decreased thereafter. Thus units with initial gross volumes < 90mL and > 170mL similarly exhibited a poor PEf., Conclusions, These data identify unit gross volume as a major parameter influencing PEf and suggest that fractionation of large units should be contemplated only when the resulting volume of split units is > 90mL."
+				"abstractNote": "Background aims\nUmbilical cord blood (UCB) is a source of hematopoietic stem cells that initially was used exclusively for the hematopoietic reconstitution of pediatric patients. It is now suggested for use for adults as well, a fact that increases the pressure to obtain units with high cellularity. Therefore, the optimization of UCB processing is a priority.\nMethods\nThe present study focused on parameters influencing total nucleated cell (TNC), mononucleated cell (MNC) and CD34 + cell (CD34C) recovery after routine volume reduction of 1553 UCB units using hydroxyethyl starch-induced sedimentation with an automated device, under routine laboratory conditions.\nResults\nWe show that the unit volume rather than the TNC count significantly affects TNC, MNC and CD34C processing efficiency (PEf), and this in a non-linear fashion: when units were sampled according to the collection volume, including pre-loaded anticoagulant (gross volume), PEf increased up to a unit volume of 110–150mL and decreased thereafter. Thus units with initial gross volumes < 90mL and > 170mL similarly exhibited a poor PEf.\nConclusions\nThese data identify unit gross volume as a major parameter influencing PEf and suggest that fractionation of large units should be contemplated only when the resulting volume of split units is > 90mL."
 			}
 		]
 	}
