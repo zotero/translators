4aa7936b82bae8a3e6b0fde3fabeb7341b2d4fc1
adam3smith <karcher@u.northwestern.edu>
2013-04-18 00:33:45 -0600

    fix VTLS for French and Arabic

--- Library Catalog (VTLS).js
+++ Library Catalog (VTLS).js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2012-11-06 10:55:28"
+	"lastUpdated": "2013-04-18 00:31:33"
 }
 
 function detectWeb(doc, url) {
@@ -17,7 +17,7 @@ function detectWeb(doc, url) {
 	if (node) {
 		return "multiple";
 	}
-	var node = doc.evaluate('//a[text()="marc"]|//a[text()="marc view"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
+	var node = doc.evaluate('//a[text()="marc" or text()="marc view" or contains(text(), "UNIMARC") or contains(text(), "مارك")]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
 	if (node) {
 		return "book";
 	}
@@ -26,7 +26,7 @@ function detectWeb(doc, url) {
 function doWeb(doc, url) {
 	var uri = doc.location.href;
 	var newUris = new Array();
-	var marcs = doc.evaluate('//a[text()="marc"]|//a[text()="marc view"]', doc, null, XPathResult.ANY_TYPE, null);
+	var marcs = doc.evaluate('//a[text()="marc" or text()="marc view" or contains(text(), "UNIMARC") or contains(text(), "مارك")]', doc, null, XPathResult.ANY_TYPE, null);
 	var record = marcs.iterateNext();
 
 	if (record && !marcs.iterateNext()) {
@@ -36,7 +36,7 @@ function doWeb(doc, url) {
 		var tagRegexp = new RegExp();
 		tagRegexp.compile("/chameleon\?.*function=CARDSCR");
 
-		var items = new Array();
+		var items = {};
 
 		var tableRows = doc.evaluate('//tr[@class="intrRow"]', doc, null, XPathResult.ANY_TYPE, null);
 		var tableRow;
@@ -58,7 +58,7 @@ function doWeb(doc, url) {
 				var field;
 				while (field = fields.iterateNext()) {
 					var header = doc.evaluate('./th/text()', field, null, XPathResult.ANY_TYPE, null).iterateNext();
-					if (header.nodeValue == "Title"|| header.nodeValue == "Tytuł") {
+					if (header.nodeValue == "Title"|| header.nodeValue == "Tytuł" || header.nodeValue == "Titre" || header.nodeValue == "العنوان") {
 						var value = doc.evaluate('./td', field, null, XPathResult.ANY_TYPE, null).iterateNext();
 						if (value) {
 							items[url] = Zotero.Utilities.trimInternal(value.textContent);
@@ -77,10 +77,7 @@ function doWeb(doc, url) {
 				newUris.push(i.replace(/function=[A-Z]{7}/, "function=MARCSCR"));
 			}
 			//Z.debug(newUris);
-			Zotero.Utilities.processDocuments(newUris, scrape, function () {
-				Zotero.done();
-			});
-			Zotero.wait();
+			Zotero.Utilities.processDocuments(newUris, scrape);
 		});
 	}
 }
@@ -91,7 +88,6 @@ function scrape(doc, newUris) {
 	var translator = Zotero.loadTranslator("import");
 	translator.setTranslator("a6ee60df-1ddc-4aae-bb25-45e0537be973");
 	translator.getTranslatorObject(function (marc) {
-
 		Zotero.Utilities.processDocuments(newUris, function (newDoc) {
 			var uri = newDoc.location.href
 			var record = new marc.record();
@@ -109,25 +105,20 @@ function scrape(doc, newUris) {
 				var ind1 = newDoc.evaluate('./TD[2]/text()[1]', elmt, null, XPathResult.ANY_TYPE, null).iterateNext().nodeValue;
 				var ind2 = newDoc.evaluate('./TD[3]/text()[1]', elmt, null, XPathResult.ANY_TYPE, null).iterateNext().nodeValue;
 				var value = newDoc.evaluate('./TD[4]/text()[1]', elmt, null, XPathResult.ANY_TYPE, null).iterateNext().nodeValue;
-				value = value.replace(/\\([a-z]) /g, marc.subfieldDelimiter + "$1");
-
+				//French and English delimiters differ (sigh...)
+				value = value.replace(/\\([a-z0-9])\s?/g, marc.subfieldDelimiter + "$1").replace(/\$ ([a-z0-9]) /g, marc.subfieldDelimiter + "$1");
+				//Z.debug(field+": " + value)
 				record.addField(field, ind1 + ind2, value);
 			}
 
 			var newItem = new Zotero.Item();
 			record.translate(newItem);
-
 			var domain = uri.match(/https?:\/\/([^/]+)/);
 			newItem.repository = domain[1] + " Library Catalog";
 
 			newItem.complete();
-		}, function () {
-			Zotero.done();
-		}, null);
-
+		});
 	});
-
-	Zotero.wait();
 } 
 /** BEGIN TEST CASES **/
 var testCases = [
