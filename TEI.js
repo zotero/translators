7c30d2064644d58088cc00164ae8929bc8a63c38
Simon Kornblith <simon@simonster.com>
2013-05-10 12:40:13 -0400

    Bump TEI

--- TEI.js
+++ TEI.js
@@ -19,7 +19,7 @@
 		"Full TEI Document": false,
 		"Export Collections": false
 	},
-	"lastUpdated": "2013-05-10 15:30:00"
+	"lastUpdated": "2013-05-10 16:39:00"
 }
 
 // ********************************************************************
