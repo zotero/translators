b22d8b56df7c42f7c740f7ae5863e16fde6c7344
adam3smith <karcher@u.northwestern.edu>
2013-06-06 00:23:37 -0600

    fix 19th. Century Periodicals in Infotrac. The whole thing is a mess and I'm not sure how much Infotrac sites are actually still around. Most Gale content has been moved to the much smoother Gale sites (which have a translator)

--- InfoTrac.js
+++ InfoTrac.js
@@ -1,27 +1,25 @@
 {
-	"translatorID":"6773a9af-5375-3224-d148-d32793884dec",
-	"translatorType":4,
-	"label":"InfoTrac",
-	"creator":"Simon Kornblith",
-	"target":"^https?://[^/]+/itw/infomark/",
-	"minVersion":"1.0.0b3.r1",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2011-01-11 04:31:00"
+	"translatorID": "6773a9af-5375-3224-d148-d32793884dec",
+	"label": "InfoTrac",
+	"creator": "Simon Kornblith",
+	"target": "^https?://[^/]+/itw/infomark/",
+	"minVersion": "1.0.0b3.r1",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "g",
+	"lastUpdated": "2013-06-06 00:19:59"
 }
 
 function detectWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
 	
 	// ensure that there is an InfoTrac logo
-	if(!doc.evaluate('//img[substring(@alt, 1, 8) = "InfoTrac"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) return false;
+	if(!doc.evaluate('//img[substring(@alt, 1, 8) = "InfoTrac"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) return false;
 	
 	if(doc.title.substring(0, 8) == "Article ") {
-		var genre = doc.evaluate('//comment()[substring(., 1, 6) = " Genre"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
+		if (ZU.xpathText(doc, '//td//img[contains(@src, "ncnp_logo.gif")]/@title')) return "newspaperArticle";
+		var genre = doc.evaluate('//comment()[substring(., 1, 6) = " Genre"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
 		
 		if(genre) {
 			var value = Zotero.Utilities.trimInternal(genre.nodeValue.substr(7));
@@ -42,13 +40,50 @@ function detectWeb(doc, url) {
 	}
 }
 
+function scrape(doc, url){
+	var newItem = new Zotero.Item();
+	var xpath = '/html/body//comment()';
+	var elmts = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+	var citation = ZU.xpath(doc, '//p/table/tbody//td/table/tbody[not(./script)]');
+	newItem.title = ZU.xpathText(citation, './/font/b');
+	newItem.itemType = "newspaperArticle";
+	var author = ZU.xpathText(citation, './/td/i');
+	if (author) newItem.creators.push(ZU.cleanAuthor(author, "author`"));
+	var date = ZU.xpathText(citation, './/td/text()');
+	if (date) date = date.match(/[A-Z][a-z]+\s\d+,\s\d{4}/);
+	if (date) newItem.date = date[0];
+	var pdfurl = ZU.xpathText(doc, '//blockquote/a[contains(@href, "!pdf")][1]/@href');
+	if  (pdfurl){
+		newItem.attachments.push({url: pdfurl, title: "Infotrac Full Text PDF", mimeType: "application/pdf"})
+	}
+	newItem.attachments.push({document: doc, title: "Infotrac Snapshot", mimeType: "text/html"});
+	while(elmt = elmts.iterateNext()) {
+		var colon = elmt.nodeValue.indexOf(":");
+		var field = elmt.nodeValue.substring(1, colon).toLowerCase();
+		var value = elmt.nodeValue.substring(colon+1, elmt.nodeValue.length-1);
+		if(field == "journal") {
+			newItem.publicationTitle = value;
+		}
+	}
+	if (newItem.publicationTitle.search(/\(.+\)/)){
+		newItem.place = newItem.publicationTitle.match(/\((.+)\)/)[1];
+		newItem.publicationTitle = newItem.publicationTitle.replace(/\(.+\).*/, "");
+	}
+	newItem.complete();
+}
+
 function extractCitation(url, elmts, title, doc) {
 	var newItem = new Zotero.Item();
 	newItem.url = url;
-	
 	if(title) {
 		newItem.title = Zotero.Utilities.superCleanString(title);
 	}
+	newItem.title = ZU.xpathText(citation, './/font/b');
+	newItem.itemType = "newspaperArticle";
+	var date = ZU.xpathText(citation, './/td/text()');
+	if (date) date = date.match(/[A-Z][a-z]+\s\d+,\s\d{4}/);
+	if (date) newItem.date = date[0];
+
 	while(elmt = elmts.iterateNext()) {
 		var colon = elmt.nodeValue.indexOf(":");
 		var field = elmt.nodeValue.substring(1, colon).toLowerCase();
@@ -155,60 +190,87 @@ function extractCitation(url, elmts, title, doc) {
 		newItem.attachments.push({document:doc, title:"InfoTrac Snapshot"});
 	} else {
 		newItem.attachments.push({url:url, title:"InfoTrac Snapshot",
-		                         mimeType:"text/html"});
+								 mimeType:"text/html"});
 	}
 	
 	newItem.complete();
 }
 
 function doWeb(doc, url) {	
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
+	var ncnp;
+	if (ZU.xpathText(doc, '//td//img[contains(@src, "ncnp_logo.gif")]/@title')) ncnp = true;
+	/*the only Infotrac Site that's still up & I'm aware of is 19th Century Newspapers. 
+	But there may well be others, so I'm leaving a lot of legacy code in just in case */
 
 	var uri = doc.location.href;
 	if(doc.title.substring(0, 8) == "Article ") {	// article
-		var xpath = '/html/body//comment()';
-		var elmts = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-		extractCitation(uri, elmts);
+		if(ncnp) scrape(doc, url);
+		else{
+			var xpath = '/html/body//comment()';
+			var elmts = doc.evaluate(xpath, doc, null, XPathResult.ANY_TYPE, null);
+			extractCitation(uri, elmts);
+		}
 	} else {										// search results
-		var items = new Array();
+		var items = new Object();
 		var uris = new Array();
 		var elmts = new Array();
 		
 		var host = doc.location.href.match(/^https?:\/\/[^\/]+/)[0];
-		
-		var tableRows = doc.evaluate('/html/body//table/tbody/tr/td[a/b]', doc, nsResolver,
-		                             XPathResult.ANY_TYPE, null);
+		var baseurl = doc.location.href.match(/(.+)\/purl=/);
+		var institution = url.match(/\?sw_aep=.+/)[0];
+		var tableRows = doc.evaluate('/html/body//table/tbody/tr/td[b or strong]', doc, null,
+									 XPathResult.ANY_TYPE, null);
 		var tableRow;
 		var javaScriptRe = /'([^']*)' *, *'([^']*)'/
 		var i = 0;
 		// Go through table rows
-		while(tableRow = tableRows.iterateNext()) {
-			var link = doc.evaluate('./a', tableRow, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-			var m = javaScriptRe.exec(link.href);
-			if(m) {
-				uris[i] = host+"/itw/infomark/192/215/90714844w6"+m[1]+"?sw_aep=olr_wad"+m[2];
+		if(ncnp){
+			while(tableRow = tableRows.iterateNext()) {
+				var title = ZU.trimInternal(ZU.xpathText(tableRow, './strong'));
+				var link = ZU.xpathText(tableRow, './a[1]/@href');
+				link = link.match(/\(\'(\/.+)\',\'/)[1];
+				link = baseurl[1] + link + institution;
+				//Z.debug(link)
+				items[link] = title;
 			}
-			var article = doc.evaluate('./b/text()', link, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-			items[i] = article.nodeValue;
-			// Chop off final period
-			if(items[i].substr(items[i].length-1) == ".") {
-				items[i] = items[i].substr(0, items[i].length-1);
+			Zotero.selectItems(items, function (items) {
+			if (!items) {
+				return true;
 			}
-			elmts[i] = doc.evaluate(".//comment()", tableRow, nsResolver, XPathResult.ANY_TYPE, null);
-			i++;
+			for (var i in items) {
+
+				uris.push(i);
+			}
+			Zotero.Utilities.processDocuments(uris, scrape)
+		});
 		}
-		
-		items = Zotero.selectItems(items);
-		
-		if(!items) {
-			return true;
-		}
-		
-		for(var i in items) {
-			extractCitation(uris[i], elmts[i], items[i]);
-		}
+		else{
+			while(tableRow = tableRows.iterateNext()) {
+				var link = doc.evaluate('./a', tableRow, null, XPathResult.ANY_TYPE, null).iterateNext();
+				var m = javaScriptRe.exec(link.href);
+				if(m) {
+					uris[i] = host+"/itw/infomark/192/215/90714844w6"+m[1]+"?sw_aep=olr_wad"+m[2];
+				}
+				var article = doc.evaluate('./b/text()|./strong/text', link, null, XPathResult.ANY_TYPE, null).iterateNext();
+				items[i] = article.nodeValue;
+				// Chop off final period
+				if(items[i].substr(items[i].length-1) == ".") {
+					items[i] = items[i].substr(0, items[i].length-1);
+				}
+				elmts[i] = doc.evaluate(".//comment()", tableRow, null, XPathResult.ANY_TYPE, null);
+				citation[i] = ZU.xpath(tableRow, '//')
+				i++;
+			}
+			
+			items = Zotero.selectItems(items);
+			
+			if(!items) {
+				return true;
+			}
+			
+			for(var i in items) {
+				extractCitation(uris[i], elmts[i], items[i]);
+			}
+		}	
 	}
 }
