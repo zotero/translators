8e039034780e2a415999f117d183847ea167de37
Maxime Escourbiac <maxime.escourbiac@gmail.com>
2013-06-10 09:54:17 +0200

    Fix license terms.

--- Intellixir.js
+++ Intellixir.js
@@ -14,10 +14,8 @@
 
 
 /**
- * Licensed to Rhapsodia and Maxime Escourbiac under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. ElasticSearch licenses this
+ * Licensed to Intellixir under one
+ * or more contributor license agreements. Intellixir licenses this
  * file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
