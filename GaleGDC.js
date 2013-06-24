988988376e80ac770bbd355f07d4c1babd331ee0
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2013-06-24 14:40:17 -0500

    [GaleGDC] Mark Chrome/Safari compatible

--- GaleGDC.js
+++ GaleGDC.js
@@ -8,8 +8,8 @@
 	"priority": 200,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "g",
-	"lastUpdated": "2013-06-23 00:25:01"
+	"browserSupport": "gcs",
+	"lastUpdated": "2013-06-23 00:39:01"
 }
 /*
  * Gale GDC Copyright (C) 2011 Gale GDC
