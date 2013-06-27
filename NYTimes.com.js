0e7e071d4696630de8ccd0b2c3b6a6f697bd4c69
adam3smith <karcher@u.northwestern.edu>
2013-06-26 23:27:20 -0600

    add blogs for LA Times and NYTimes

--- NYTimes.com.js
+++ NYTimes.com.js
@@ -2,14 +2,14 @@
 	"translatorID": "ce7a3727-d184-407f-ac12-52837f3361ff",
 	"label": "NYTimes.com",
 	"creator": "Simon Kornblith",
-	"target": "^https?://(?:query\\.nytimes\\.com/(?:search|gst)/(?:alternate/)?|(?:select\\.|www\\.)?nytimes\\.com/.)",
+	"target": "^https?://(?:query\\.nytimes\\.com/(?:search|gst)/(?:alternate/)?|(?:select\\.|www\\.)?|[^\\.]+\\.blogs\\.nytimes\\.com/.)",
 	"minVersion": "2.1.9",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsv",
-	"lastUpdated": "2012-07-12 23:21:02"
+	"lastUpdated": "2013-06-26 13:02:02"
 }
 
 function detectWeb(doc, url) {
@@ -21,13 +21,18 @@ function detectWeb(doc, url) {
 	var metaTags = doc.getElementsByTagName("meta");
 	var haveHdl = false;
 	var haveByl = false;
+	var blogPost = false;
 	for (var i in metaTags) {
 		if (metaTags[i].name === "hdl") {
 			haveHdl = true;
 		} else if (metaTags[i].name == "byl") {
 			haveByl = true;
 		}
+		else if (metaTags[i].name == "PST") {
+			blogPost = true;
+		}
 		if (haveHdl && haveByl) return "newspaperArticle";
+		else if (haveByl && blogPost) return "blogPost"
 	}
 	return false;
 }
@@ -39,23 +44,19 @@ function associateMeta(newItem, metaTags, field, zoteroField) {
 }
 
 function scrape(doc, url) {
-	var namespace = null;
-	var nsResolver = namespace ?
-	function (prefix) {
-		if (prefix == 'x') return namespace;
-		else return null;
-	} : null;
 
+	
 	var newItem = new Zotero.Item("newspaperArticle");
 	newItem.publicationTitle = "The New York Times";
 	newItem.ISSN = "0362-4331";
-
 	var metaTags = new Object();
+	var metaTagsProperty = new Object();
 	if (url != undefined) {
-
+		if(url.indexOf("blogs.nytimes.com")!= -1) newItem.itemType="blogPost";
 		newItem.url = url;
 		var metaTagRe = /<meta[^>]*>/gi;
 		var nameRe = /name="([^"]+)"/i;
+		var propertyRe = /property="([^"]+)"/i;
 		var contentRe = /content="([^"]+)"/i;
 		var m = doc.match(metaTagRe);
 
@@ -66,14 +67,18 @@ function scrape(doc, url) {
 		for (var i = 0; i < m.length; i++) {
 			var name = nameRe.exec(m[i]);
 			var content = contentRe.exec(m[i]);
+			var property = propertyRe.exec(m[i]);
+			var content = contentRe.exec(m[i]);
 			if (name && content) {
 				metaTags[name[1]] = content[1];
 			}
+			if (property && content) {	
+				metaTagsProperty[property[1]] = content[1];
+			}
 		}
-
-		if (!metaTags["hdl"]) {
+	/*	if (!metaTags["hdl"]) {
 			return;
-		}
+		}*/
 		// We want to get everything on one page
 		newItem.attachments.push({
 			url: url.replace(/\.html\??([^/]*)(pagewanted=[^&]*)?([^/]*)$/, ".html?pagewanted=all&$1$2"),
@@ -81,18 +86,25 @@ function scrape(doc, url) {
 			mimeType: "text/html"
 		});
 	} else {
+		var type = detectWeb(doc, url);
+		newItem.itemType = type;
 		newItem.url = doc.location.href;
 		var metaTagHTML = doc.getElementsByTagName("meta");
 		for (var i = 0; i < metaTagHTML.length; i++) {
 			var key = metaTagHTML[i].getAttribute("name");
 			var value = metaTagHTML[i].getAttribute("content");
+			var prop = metaTagHTML[i].getAttribute("property");
 			if (key && value) {
 				metaTags[key] = value;
 			}
+			if (prop && value) {
+				metaTagsProperty[prop] = value;
+			}
+			
 		}
 		// Get everything on one page is possible
 		var singlePage = false;
-		if (!newItem.url.match(/\?pagewanted=all/) && (singlePage = doc.evaluate('//ul[@id="toolsList"]/li[@class="singlePage"]/a', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext())) {
+		if (!newItem.url.match(/\?pagewanted=all/) && (singlePage = doc.evaluate('//ul[@id="toolsList"]/li[@class="singlePage"]/a', doc, null, XPathResult.ANY_TYPE, null).iterateNext())) {
 			newItem.attachments.push({
 				url: singlePage.href,
 				title: "New York Times Snapshot",
@@ -109,9 +121,16 @@ function scrape(doc, url) {
 
 	associateMeta(newItem, metaTags, "dat", "date");
 	associateMeta(newItem, metaTags, "hdl", "title");
+	associateMeta(newItem, metaTags, "description", "abstractNote");
+	if (type === "blogPost"){
+		associateMeta(newItem, metaTagsProperty, "og:title", "title");
+		associateMeta(newItem, metaTagsProperty, "og:site_name", "blogTitle");
+	}
 	associateMeta(newItem, metaTags, "dsk", "section");
 	associateMeta(newItem, metaTags, "articleid", "accessionNumber");
-
+	
+	if (newItem.blogTitle) newItem.blogTitle = "The New York Times - " + newItem.blogTitle;
+	
 	if (metaTags["pdate"]) {
 		newItem.date = metaTags["pdate"].replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
 	}
@@ -150,13 +169,13 @@ function scrape(doc, url) {
 			newItem.tags[i] = newItem.tags[i].replace("  ", ", ");
 		}
 	}
-
+	
+	
 	// Remove everything after .html from the URL - we want the canonical version
 	//but not for historical abstracts, where it's needed
 	if (!newItem.url.match(/abstract\.html/)) {
 		newItem.url = newItem.url.replace(/\?.+/, '');
 	}
-
 	//	get pdf for archive articles - make sure we don't go here if we're getting multiples or regular items
 	var pdfxpath = '//div[@class="articleAccess"]/p[@class="button"]/a[contains(@href, "/pdf")]/@href'
 	if (!m && ZU.xpathText(doc, pdfxpath) != null) {
@@ -194,9 +213,7 @@ function doWeb(doc, url) {
 			Zotero.Utilities.HTTP.doGet(urls, function (text, response, url) {
 				scrape(text, url)
 			}, function () {
-				Zotero.done();
 			}, null);
-			Zotero.wait();
 		});
 	} else {
 		scrape(doc);
@@ -223,13 +240,9 @@ var testCases = [
 				"seeAlso": [],
 				"attachments": [
 					{
-						"document": {
-							"location": {}
-						},
 						"title": "New York Times Snapshot"
 					},
 					{
-						"url": "http://article.archive.nytimes.com/1912/03/05/100523320.pdf?AWSAccessKeyId=AKIAJBTN455PTTBQQNRQ&Expires=1327279930&Signature=AmK21Cp20qcYualg%2FLvaT4t7Ypw%3D",
 						"title": "NY Times Archive PDF",
 						"mimeType": "application/pdf"
 					}
@@ -239,6 +252,7 @@ var testCases = [
 				"url": "http://query.nytimes.com/gst/abstract.html?res=F30D15FD3F5813738DDDAC0894DB405B828DF1D3",
 				"date": "1912-03-05",
 				"title": "TWO MONEY INQUIRIES.; Hearings of Trust Charges and Aldrich Plan at the Same Time.",
+				"abstractNote": "WASHINGTON, March 4. -- The Money Trust inquiry and consideration of the proposed Aldrich monetary legislation will probably be handled side by side by the House Banking and Currency Committee. The present tentative plan is to divide the committee into two parts, one of which, acting as a sub-committee, will investigate as far as it can those allegations of the Henry Money Trust resolution which fall within the jurisdiction of the Banking and Currency Committee.",
 				"accessionNumber": "100523320",
 				"libraryCatalog": "NYTimes.com",
 				"accessDate": "CURRENT_TIMESTAMP"
@@ -277,6 +291,7 @@ var testCases = [
 				"url": "http://www.nytimes.com/2010/08/21/education/21harvard.html",
 				"date": "2010-08-20",
 				"title": "Harvard Finds Marc Hauser Guilty of Scientific Misconduct",
+				"abstractNote": "The university has found Marc Hauser “solely responsible” for eight instances of scientific misconduct.",
 				"section": "Education",
 				"accessionNumber": "1248068890906",
 				"libraryCatalog": "NYTimes.com",
@@ -289,6 +304,45 @@ var testCases = [
 		"defer": true,
 		"url": "http://query.nytimes.com/search/sitesearch/#/marc+hauser",
 		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://opinionator.blogs.nytimes.com/2013/06/19/our-broken-social-contract/",
+		"items": [
+			{
+				"itemType": "blogPost",
+				"creators": [
+					{
+						"firstName": "Thomas B.",
+						"lastName": "Edsall",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [
+					"Economic Conditions and Trends",
+					"Income Inequality",
+					"Social Conditions and Trends",
+					"United States Economy",
+					"United States"
+				],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "New York Times Snapshot"
+					}
+				],
+				"publicationTitle": "The New York Times",
+				"ISSN": "0362-4331",
+				"url": "http://opinionator.blogs.nytimes.com/2013/06/19/our-broken-social-contract/",
+				"abstractNote": "At their core, are America&#8217;s problems primarily economic or moral?",
+				"title": "Our Broken Social Contract",
+				"blogTitle": "The New York Times - Opinionator",
+				"date": "2013-06-19",
+				"libraryCatalog": "NYTimes.com",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
 	}
 ]
 /** END TEST CASES **/
