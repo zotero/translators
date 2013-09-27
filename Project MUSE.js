88363dca2cea868f34d25ea90804aafe72586253
adam3smith <karcher@u.northwestern.edu>
2013-09-27 00:58:23 -0600

    HuffPo dates, ProjectMuse clean up abstracts

--- Project MUSE.js
+++ Project MUSE.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2013-06-23 23:00:08"
+	"lastUpdated": "2013-09-26 23:40:33"
 }
 
 function detectWeb(doc, url) {
@@ -85,7 +85,7 @@ function scrapeOne(doc) {
 			translator.setHandler("itemDone", function(obj, item) {
 			item.attachments.push({document:doc, title:"Project MUSE Snapshot"});
 			if (abstract){
-				item.abstractNote=abstract;
+				item.abstractNote=abstract.replace(/\n/g, " ").replace(/\s\s+/g, " ");
 			}
 			item.complete();
 			});
@@ -123,7 +123,6 @@ function scrapeOne(doc) {
 				//Muse has authors wrong in the RIS - we get the names from google/highwire metadata and use them
 				// they're also inconsistent about comma use, so we're using the code from the Embedded Metadata translator to distinguish
 				if(authorNodes.length){
-					Z.debug("here")
 					item.creators = [];
 						for(var i=0, n=authorNodes.length; i<n; i++) {
 							//make sure there are no empty authors
@@ -164,7 +163,7 @@ function scrapeOne(doc) {
 					item.DOI = DOI.textContent.replace(/^DOI: /,"");
 				}
 				if(abstract) {
-					item.abstractNote = abstract.textContent;
+					item.abstractNote = abstract.textContent.replace(/\n/g, " ").replace(/\s\s+/g, " ");
 				}
 				item.complete();
 			});
@@ -182,8 +181,8 @@ var testCases = [
 				"itemType": "journalArticle",
 				"creators": [
 					{
-						"lastName": "Higonnet",
 						"firstName": "Patrice L. R.",
+						"lastName": "Higonnet",
 						"creatorType": "author"
 					}
 				],
@@ -208,7 +207,7 @@ var testCases = [
 				"url": "http://muse.jhu.edu/journals/past_and_present/v191/191.1higonnet.html",
 				"date": "2006",
 				"extra": "<p>Number 191, May 2006</p>",
-				"abstractNote": "In lieu of an abstract, here is a brief excerpt of the content:\n         Past & Present 191.1 (2006) 121-164    Patrice Higonnet   Harvard University    Tocqueville in the 1850s wrote of France in the 1780s that never had tolerance been more accepted, authority been more mild, or benevolence been so widely practised. Nonetheless, he went on, 'from the bosom of such mild mores would spring the most inhuman of revolutions'. And even for those of us who deeply admire the French Revolution's message of civic equality, the Terror of the Year II (1793–4) seems not just ominous and horrendous, but also out of place. Auschwitz, Dresden and Hiroshima — after the Great War of 1914–18 and the Great Depression of the 1930s: we can see why these wartime tragedies happened, given the awful events that preceded them. But what of the Terror after the Enlightenment — after Voltaire, Boucher, and Madame de Pompadour? Isser Woloch has rightly described the 'sequence' from 1789 to 1793, from liberalism to terror, as an eternally fascinating 'enigma'. Why the French Revolution occurred is something of a mystery. And why it failed so dramatically is also deeply perplexing.   Historians have pored over the cause and nature of the Terror of the Year II ever since it occurred. And yet the many valuable (though often conflicting) explanations which have been offered to account for it have somehow fallen short. Many of them are too narrow or too vague. They are seldom wrong in any simple sense, but they need to be reconceived. I propose that there is an extant and even ancient frame, the 'young Marx explanation', which, if rejuvenated by reference to the theory of collective trauma, can enable us to renew these accounts, first by identifying new ground for research, but principally by making it possible to integrate the seemingly irreconcilable points of view  about why the unanimity of 1789 gave way by 1792–4 to a divisive and self-destructive intolerance.   I   A first step is to review explanations, past and present, that range from culture and ideology (Rousseauian Jacobinism was bound to end as it did) to circumstance (where the circumstances might be social, political or institutional). For many reasons, it is important to have these various answers in mind — however biased or incomplete they may have been: first, because explanations of Revolutionary terrorism are suggestive in their own right since they indirectly chronicle two centuries of historiographic effort; second, because their very number speaks to the importance of the Terror as a historical event, and of course any understanding of the French Revolution must subsume an understanding of the Terror; and third, because the variety and incompatibility of extant accounts underscore the interest of any explanation that aims to reconcile opposing points of view.   As regards larger ideological explanations, a good place to begin is with François Furet, since his is the dominant mode of explanation today. This brilliant historian was unambiguous: for him, the 'revolutionary government' 'was written in the logic of Montagnard policy'. The Terror, taken as government policy by the Convention, does not originate in September 1793 at all, but in the past (both recent and distant):   The circumstances surrounding this celebrated vote indicate that before becoming a set of repressive institutions used by the Republic to liquidate its adversaries and establish its domination on a basis of fear, the Terror was a demand based on political conviction or beliefs, a characteristic feature of the mentality of revolutionary activism.   From this perspective, the Terror is the illegitimate child of the Enlightenment — begotten, perhaps, on the Enlightenment by Jean-Jacques Rousseau. The year 1793 is embedded in 1789. Furet's first and most talented disciple, Keith Baker, gives us a new (and, it must be said, extreme) version of this same ideological  causal perspective. What, he asks, was the implication of...",
+				"abstractNote": "In lieu of an abstract, here is a brief excerpt of the content: Past & Present 191.1 (2006) 121-164 Patrice Higonnet Harvard University Tocqueville in the 1850s wrote of France in the 1780s that never had tolerance been more accepted, authority been more mild, or benevolence been so widely practised. Nonetheless, he went on, 'from the bosom of such mild mores would spring the most inhuman of revolutions'. And even for those of us who deeply admire the French Revolution's message of civic equality, the Terror of the Year II (1793–4) seems not just ominous and horrendous, but also out of place. Auschwitz, Dresden and Hiroshima — after the Great War of 1914–18 and the Great Depression of the 1930s: we can see why these wartime tragedies happened, given the awful events that preceded them. But what of the Terror after the Enlightenment — after Voltaire, Boucher, and Madame de Pompadour? Isser Woloch has rightly described the 'sequence' from 1789 to 1793, from liberalism to terror, as an eternally fascinating 'enigma'. Why the French Revolution occurred is something of a mystery. And why it failed so dramatically is also deeply perplexing. Historians have pored over the cause and nature of the Terror of the Year II ever since it occurred. And yet the many valuable (though often conflicting) explanations which have been offered to account for it have somehow fallen short. Many of them are too narrow or too vague. They are seldom wrong in any simple sense, but they need to be reconceived. I propose that there is an extant and even ancient frame, the 'young Marx explanation', which, if rejuvenated by reference to the theory of collective trauma, can enable us to renew these accounts, first by identifying new ground for research, but principally by making it possible to integrate the seemingly irreconcilable points of view about why the unanimity of 1789 gave way by 1792–4 to a divisive and self-destructive intolerance. I A first step is to review explanations, past and present, that range from culture and ideology (Rousseauian Jacobinism was bound to end as it did) to circumstance (where the circumstances might be social, political or institutional). For many reasons, it is important to have these various answers in mind — however biased or incomplete they may have been: first, because explanations of Revolutionary terrorism are suggestive in their own right since they indirectly chronicle two centuries of historiographic effort; second, because their very number speaks to the importance of the Terror as a historical event, and of course any understanding of the French Revolution must subsume an understanding of the Terror; and third, because the variety and incompatibility of extant accounts underscore the interest of any explanation that aims to reconcile opposing points of view. As regards larger ideological explanations, a good place to begin is with François Furet, since his is the dominant mode of explanation today. This brilliant historian was unambiguous: for him, the 'revolutionary government' 'was written in the logic of Montagnard policy'. The Terror, taken as government policy by the Convention, does not originate in September 1793 at all, but in the past (both recent and distant): The circumstances surrounding this celebrated vote indicate that before becoming a set of repressive institutions used by the Republic to liquidate its adversaries and establish its domination on a basis of fear, the Terror was a demand based on political conviction or beliefs, a characteristic feature of the mentality of revolutionary activism. From this perspective, the Terror is the illegitimate child of the Enlightenment — begotten, perhaps, on the Enlightenment by Jean-Jacques Rousseau. The year 1793 is embedded in 1789. Furet's first and most talented disciple, Keith Baker, gives us a new (and, it must be said, extreme) version of this same ideological causal perspective. What, he asks, was the implication of the path chosen by the National Assembly in September 1789 over the issue of the king's suspensive veto? At stake here, he argues in a historical version of 'path determinacy', was the setting aside of a discourse of the social, grounded on the notion of the differential distribution of reason, functions, and interests in modern civil society, in favor of a discourse of the political...",
 				"libraryCatalog": "Project MUSE",
 				"accessDate": "CURRENT_TIMESTAMP"
 			}
@@ -245,13 +244,14 @@ var testCases = [
 						"title": "Project MUSE Snapshot"
 					}
 				],
-				"title": "Writing the Forest in Early Modern England",
+				"title": "Writing the Forest in Early Modern England: A Sylvan Pastoral Nation",
 				"publisher": "Duquesne University Press",
 				"place": "Pittsburgh, PA",
 				"ISBN": "9780820705057",
 				"date": "2009",
 				"abstractNote": "In Writing the Forest in Early Modern England: A Sylvan Pastoral Nation, Jeffrey S. Theis focuses on pastoral literature in early modern England as an emerging form of nature writing. In particular, Theis analyzes what happens when pastoral writing is set in forests — what he terms “sylvan pastoral.”\nDuring the sixteenth and seventeenth centuries, forests and woodlands played an instrumental role in the formation of individual and national identities in England. Although environmentalism as we know it did not yet exist, persistent fears of timber shortages led to a larger anxiety about the status of forests. Perhaps more important, forests were dynamic and contested sites of largely undeveloped spaces where the poor would migrate in a time of rising population when land became scarce. And in addition to being a place where the poor would go, the forest also was a playground for monarchs and aristocrats where they indulged in the symbolically rich sport of hunting.\nConventional pastoral literature, then, transforms when writers use it to represent and define forests and the multiple ways in which English society saw these places. In exploring these themes, authors expose national concerns regarding deforestation and forest law and present views relating to land ownership, nationhood, and the individual’s relationship to nature. Of particular interest are the ways in which cultures turn confusing spaces into known places and how this process is shaped by nature, history, gender, and class.\nTheis examines the playing out of these issues in familiar works by Shakespeare, such as A Midsummer Night’s Dream, The Merry Wives of Windsor, and As You Like It, Andrew Marvell’s “Upon Appleton House,” John Milton’s Mask and Paradise Lost, as well as in lesser known prose works of the English Revolution, such as James Howell’s Dendrologia>/i> and John Evelyn’s Sylva.\nAs a unique ecocritical study of forests in early modern English literature, Writing the Forest makes an important contribution to the growing field of the history of environmentalism, and will be of interest to those working in literary and cultural history as well as philosophers concerned with nature and space theory.\n       show less, In Writing the Forest in Early Modern England: A Sylvan Pastoral Nation, Jeffrey S. Theis focuses on pastoral literature in early modern England as an emerging form of nature writing. In particular, Theis analyzes what happens when pastoral writing is set in forests — what he terms “sylvan pastoral.” During the sixteenth and seventeenth centuries, forests and woodlands played an instrumental role in the formation of individual and national identities in England. Although environmentalism as we know it did not yet exist, persistent fears of timber shortages led to a larger anxiety about the status of forests. Perhaps more important . . .  show more",
-				"libraryCatalog": "Project MUSE"
+				"libraryCatalog": "Project MUSE",
+				"shortTitle": "Writing the Forest in Early Modern England"
 			}
 		]
 	}
