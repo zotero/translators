0c2e0b2457b474b771d3dcabe503877866d08b08
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2013-10-05 13:50:46 -0500

    [Galegroup] Re-commit. Now supports the following: * Gale Virtual Reference Library * Academic OneFile, Academic ASAP, maybe other Academic DBs too? * LegalTrac * Literature Resource Center * Gale NewsVault * Eighteenth Cnetury Collection Online (no PDF attachments)

--- Galegroup.js
+++ Galegroup.js
@@ -1,15 +1,15 @@
 {
 	"translatorID": "4ea89035-3dc4-4ae3-b22d-726bc0d83a64",
 	"label": "Galegroup",
-	"creator": "Sebastian Karcher",
+	"creator": "Sebastian Karcher and Aurimas Vinckevicius",
 	"target": "https?://(find|go)\\.galegroup\\.com",
-	"minVersion": "2.1.9",
+	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsib",
-	"lastUpdated": "2013-10-04 18:53:25"
+	"lastUpdated": "2013-10-05 13:47:51"
 }
 
 /*
@@ -33,200 +33,222 @@
 	
 	***** END LICENSE BLOCK *****
 */
+function getSearchResults(doc) {
+	//Gale Virtual Reference Library
+	var results = ZU.xpath(doc, '//*[@id="SearchResults"]//section[@class="resultsBody"]/ul/li');
+	if(results.length) {
+		results.linkXPath = './p[@class="subTitle"]/a';
+		Z.debug("Using GVRL");
+		composeAttachment = composeAttachmentGVRL;
+		composeRisUrl = composeRisUrlGVRL;
+		return results;
+	}
+	
+	//Academic OneFile
+	//Academic ASAP
+	results = ZU.xpath(doc, '//div[@id="resultsBox"]//li[@class="resrow"]');
+	if(results.length) {
+		results.linkXPath = './/div[@class="pic_Title"]/a';
+		Z.debug("Academic, but using GVRL");
+		composeAttachment = composeAttachmentGVRL;
+		composeRisUrl = composeRisUrlGVRL;
+		return results;
+	}
+	
+	//LegalTrac
+	results = ZU.xpath(doc, '//*[@id="sr_ul"]/li');
+	if(results.length) {
+		results.linkXPath = './/span[@class="title"]/a';
+		Z.debug("LegalTrac, but using GVRL");
+		composeAttachment = composeAttachmentGVRL;
+		composeRisUrl = composeRisUrlGVRL;
+		return results;
+	}
+	
+	//Literature Resource Center
+	results = ZU.xpath(doc, '//div[@id="resultsTable"]/div');
+	if(results.length) {
+		results.linkXPath = './/span[@class="title"]/a';
+		Z.debug("LRC, but using GVRL");
+		composeAttachment = composeAttachmentGVRL;
+		composeRisUrl = composeRisUrlGVRL;
+		return results;
+	}
+	
+	//Gale NewsVault
+	results = ZU.xpath(doc, '//*[@id="results_list"]/div[contains(@class,"resultList")]');
+	if(results.length) {
+		results.linkXPath = './div[@class="pub_details"]//li[@class="resultInfo"]/p//a';
+		Z.debug("Using GNV");
+		composeAttachment = composeAttachmentGNV;
+		composeRisUrl = composeRisUrlGNV;
+		return results;
+	}
+	
+	/** TODO: **/
+//	//19th century UK periodicals
+//	results = ZU.xpath(doc, '//*[@id="content"]//table[@class="resultstable"]//tr[@class="selectedRow" or @class="unselectedRow"]');
+//	if(results.length) {
+//		results.linkXPath = './/b/a[contains(@href, "retrieve.do")]';
+//		composeAttachment = composeAttachmentUKPC;
+//		composeRisUrl = composeRisUrlUKPC;
+//		return results;
+//	}
+
+	//Declassified Documents Reference System
+	
+	//"Full Citation" metadata:
+	//  The Making of Modern Law (multiple)
+	//  Sabin Americana 1500-1926
+	
+	//British Newspapers
+	//Burney Collection Newspapers
+	
+	//Eighteenth Cnetury Collection Online
+	//  works, but no PDFs
+	
+	//Biography in Context
+	
+	//Old InfoTrac ?? (various)
+	
+	return [];
+}
 
 function detectWeb(doc, url) {
-	if (url.match(/\/retrieve\.do|\/i\.do|\/infomark\.do|newspaperRetrieve\.do/)) {
-		if (url.match(/\/ecco\//)) return "book"
-		else if (url.indexOf("newspaperRetrieve.do")!= -1) return "newspaperArticle"
-		else return "journalArticle";
+	if(url.indexOf('/newspaperRetrieve.do') != -1) {
+		return "newspaperArticle";
+	}
+	
+	if(url.indexOf('/retrieve.do') != -1
+		|| url.indexOf('/i.do') != -1
+		|| url.indexOf('/infomark.do') != -1) {
+		
+		if(url.indexOf('/ecco/') != -1) return "book";
+		
+		return "journalArticle";
+	}
+	
+	if(getSearchResults(doc).length) return "multiple";
+}
 
-	} else if (url.match(/\/basicSearch\.do|\/advancedSearch\.do|\/subjectguide\.do|\/limitExpandSearchResults\.do/)) {
-		return "multiple";
+var composeRisUrl;
+
+function composeRisUrlGNV(url) {
+	return url.replace(/#.*/,'').replace(/\/[^\/?]+(?=\?|$)/, '/centralizedGenerateCitation.do')
+		.replace(/\bactionString=[^&]*/g, '').replace(/\bcitationFormat=[^&]*&?/g, '')
+		+ '&actionString=FormatCitation&citationFormat=ENDNOTE';
+}
+
+function composeRisUrlGVRL(url) {
+	return url.replace(/#.*/,'').replace(/\/[^\/?]+(?=\?|$)/, '/generateCitation.do')
+		.replace(/\bactionString=[^&]*/g, '').replace(/\bcitationFormat=[^&]*&?/g, '')
+		+ '&actionString=FormatCitation&citationFormat=ENDNOTE';
+}
+
+var composeAttachment;
+
+function composeAttachmentGVRL(doc, url) {
+	var pdf = !!(doc.getElementById('pdfLink') || doc.getElementById('docTools-pdf'));
+	var attachment = ZU.xpath(doc, '//*[@id="docTools-download"]/a[./@href]')[0];
+	if(attachment && pdf /* HTML currently pops up a download dialog for HTML attachments */) {
+		url = attachment.href;
+		return {
+			url: url.replace(/#.*/, '').replace(/\/[^\/?]+(?=\?|$)/, '/downloadDocument.do')
+				.replace(/\b(?:actionCmd|downloadFormat)=[^&]*&?/g, '')
+				+ '&actionCmd=DO_DOWNLOAD_DOCUMENT&downloadFormat=' + (pdf?'PDF':'HTML'),
+			title: "Full Text " + (pdf?'PDF':'HTML'),
+			mimeType: pdf?'application/pdf':'text/html'
+		};
+	} else {
+		return {document: doc, title: "Snapshot"};
 	}
 }
 
-function composeURL(url) {
-	//Z.debug("url: " + url)
-	//"host" here includes the database abbreviation
-	var host = url.match(/^https?:\/\/.+?\/.+?\//)[0];
-	var prodID = url.match(/prodId=.+?&/)[0];
-	if (url.match(/userGroupName=.+?&/)) var usergroup = url.match(/userGroupName=.+?&/)[0];
-	else if (!url.match(/userGroupName=.+?&/)) var usergroup = "";
-	var tabID = url.match(/tabID=.+?&/)[0];
-	var docID = url.match(/docId=.*?(&|$)/)[0];
-	//Z.debug(docID)
-	if (docID == "docId=&"){
-		Z.debug("here")
-		var pageBatch= url.match(/relevancePageBatch=(.+?(&|$))/);
-		if (pageBatch) docID = "docId=" + pageBatch[1];
-	}
-	var contentSet = url.match(/contentSet=.+?(&|$)/)[0];
-	if (!contentSet.match(/&/)) contentSet = contentSet + "&";
-	var RISurl = host + "generateCitation.do?actionString=FormatCitation&inPS=true&" + prodID + tabID + usergroup + docID + contentSet + "citationFormat=REFMGR";
-	//Z.debug(RISurl)
-	return RISurl;
+function composeAttachmentGNV(doc, url) {
+	var lowerLimit = ZU.xpathText(doc, '//form[@id="resultsForm"]/input[@name="pdfLowerLimit"]/@value') || '1';
+	var upperLimit = ZU.xpathText(doc, '//form[@id="resultsForm"]/input[@name="pdfHigherLimit"]/@value') || lowerLimit;
+	var numPages = ZU.xpathText(doc, '//form[@id="resultsForm"]/input[@name="noOfPages"]/@value') || (upperLimit - lowerLimit + 1);
+	return {
+		url: url.replace(/#.*/,'').replace(/\/[^\/?]+(?=\?|$)/, '/downloadDocument.do')
+			.replace(/\b(?:scale|orientation|docType|pageIndex|relatedDocId|isIllustration|imageId|aCmnd|recNum|pageRange|noOfPages)=[^&]*&?/g, '')
+			+ '&scale=&orientation=&docType=&pageIndex=1&relatedDocId=&isIllustration=false'
+			+ '&imageId=&aCmnd=PDFFormat&recNum=&' + 'noOfPages=' + numPages + '&pageRange=' + lowerLimit + '-' + upperLimit,
+		title: 'Full Text PDF',
+		mimeType: 'application/pdf'
+	};
 }
 
+function parseRis(text, attachment) {
+	text = text.trim();
+	//gale puts issue numbers in M1
+	text = text.replace(/M1\s*\-/g, "IS  -");
+	//L2 is probably meant to be UR, but we can ignore it altogether
+	text = text.replace(/^L2\s+-.+\n/gm, '');
+	//we can map copyright notes via CR
+	text = text.replace(/^N1(?=\s+-\s+copyright)/igm, 'CR');
+	//Z.debug(text);
+	
+	var translator = Zotero.loadTranslator("import");
+	translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+	translator.setString(text);
+	translator.setHandler("itemDone", function (obj, item) {
+		if(attachment) item.attachments.push(attachment);
+		item.complete();
+	});
+	translator.translate();
+}
 
-function parseRIS(url) {
-	//we get the host so that proxies will work later
-	if (typeof url == "string") {
-		var host = url.match(/^https?:\/\/.+?\//)[0];
-	} else if (typeof url == "object") {
-		var host = url[0].match(/^https?:\/\/.+?\//)[0];
-	}
-
-	Zotero.Utilities.HTTP.doGet(url, function (text) {
-		text = text.trim();
-		//gale puts issue numbers in M1
-		text = text.replace(/M1\s*\-/, "IS  -");
-		//Z.debug(text)
-		var translator = Zotero.loadTranslator("import");
-		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
-		translator.setString(text);
-		translator.setHandler("itemDone", function (obj, item) {
-			//make sure the attachment URL gets proxied
-			item.attachments.push({url: item.url, title: "Full Text (HTML)", mimeType: "text/html",})
-			for (i in item.attachments) {
-				item.attachments[i].url = item.attachments[i].url.replace(/^https?:\/\/.+?\//, host);
-			}
-			//remove empty date clutter
-			if (item.date) item.date = item.date.replace(/[\/\.]+$/, "")
-			item.complete();
+function processArticles(articles) {
+	var article;
+	while(article = articles.shift()) {
+		ZU.processDocuments(article, function(doc, url) {
+			processPage(doc, url);
+			processArticles(articles);
 		});
-		translator.translate();
+	}
+}
+
+function processPage(doc, url) {
+	var attachment = composeAttachment(doc, url);
+	ZU.doGet(composeRisUrl(url), function(text) {
+		parseRis(text, attachment);
 	});
 }
 
 function doWeb(doc, url) {
-	var articles = new Array();
-	if (detectWeb(doc, url) == "multiple") {
-		var items = new Object();
-		var titles = doc.evaluate('//span[@class="title"]/a|//div[contains(@class, "Title")]/a|//p[@class="articleTitle"]//a|//li[@class="resultInfo"]/p/b/a|//li[@class="resultsInstance"]/p[@class="subTitle"]/a', doc, null, XPathResult.ANY_TYPE, null);
-		var next_title;
-		while (next_title = titles.iterateNext()) {
-			var textContent = next_title.textContent;
-			// Strip trailing comma on some sites
-			textContent = textContent.replace(/\,$/, '');
-			items[next_title.href] = textContent;
+	if(detectWeb(doc, url) == "multiple") {
+		var results = getSearchResults(doc);
+		var items = {};
+		for(var i=0, n=results.length; i<n; i++) {
+			var link = ZU.xpath(results[i], results.linkXPath)[0];
+			if(!link) continue;
+			
+			items[link.href] = ZU.trimInternal(link.textContent);
 		}
-
+		
 		Zotero.selectItems(items, function (items) {
-			if (!items) {
-				return true;
-			}
-			for (var i in items) {
-				i = composeURL(i);
+			if(!items) return true;
+			
+			var articles = [];
+			for(var i in items) {
 				articles.push(i);
 			}
-			parseRIS(articles, function () {
-				Zotero.done();
-			});
+			processArticles(articles);
 		});
-
 	} else {
-		//get a full URL for permalinks
-		if (url.match(/\/i\.do/)) {
-			var host = url.match(/^https?:\/\/.+?\/.+?\//)[0];
-			url = host + ZU.xpathText(doc, '//li/a[contains(@title, "Download")]/@href');
+		if(doc.title.indexOf('NewsVault') != -1) {
+			Z.debug("Using GNV");
+			composeAttachment = composeAttachmentGNV;
+			composeRisUrl = composeRisUrlGNV;
+		} else {
+			Z.debug("Using GVRL");
+			composeAttachment = composeAttachmentGVRL;
+			composeRisUrl = composeRisUrlGVRL;
 		}
-		var RISurl = composeURL(url);
-		//Z.debug(RISurl)
-		parseRIS(RISurl);
+		
+		processPage(doc, url);
 	}
 }
 /** BEGIN TEST CASES **/
-var testCases = [
-	{
-		"type": "web",
-		"url": "http://go.galegroup.com/ps/i.do?action=interpret&id=GALE%7CH1420025063&v=2.1&u=viva_gmu&it=r&p=LitRG&sw=w&authCount=1",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"creators": [
-					{
-						"lastName": "Lewald",
-						"firstName": "H. Ernest",
-						"creatorType": "author"
-					},
-					{
-						"lastName": "Borges",
-						"firstName": "Jorge Luis",
-						"creatorType": "author"
-					}
-				],
-				"notes": [
-					{
-						"note": "<p>COPYRIGHT 1999 Gale Research, COPYRIGHT 2007 Gale, Cengage Learning</p>"
-					}
-				],
-				"tags": [
-					"Fervor de Buenos Aires (Poetry collection)",
-					"Dulcia Linquimus Arva (Poem)",
-					"El Amenazado (Poem)",
-					"El Centinela (Poem)",
-					"Lo perdido (Poem)",
-					"El Triste (Poem)",
-					"H. O. (Poem)",
-					"Borges, Jorge Luis"
-				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"mimeType": "text/html",
-						"title": "Full Text (HTML)"
-					}
-				],
-				"publicationTitle": "Chasqui",
-				"issue": "1",
-				"extra": "19",
-				"date": "November 1974",
-				"pages": "19-33",
-				"title": "Borges: His Recent Poetry",
-				"volume": "4",
-				"accessDate": "May 7, 2012",
-				"language": "English",
-				"libraryCatalog": "Gale",
-				"archive": "Literature Resources from Gale",
-				"shortTitle": "Borges",
-				"url": "http://go.galegroup.com/ps/i.do?id=GALE%7CH1420025063&v=2.1&u=viva_gmu&it=r&p=LitRG&sw=w"
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "http://find.galegroup.com/ecco/infomark.do?&source=gale&prodId=ECCO&u=viva_gmu&tabID=T001&docId=CW3325179878&type=multipage&contentSet=ECCOArticles&version=1.0&docLevel=FASCIMILE",
-		"items": [
-			{
-				"itemType": "book",
-				"creators": [],
-				"notes": [
-					{
-						"note": "<p>Copyright 2009 Gale, Cengage Learning</p>"
-					}
-				],
-				"tags": [],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"mimeType": "text/html",
-						"title": "Full Text (HTML)"
-					}
-				],
-				"title": "A digest of the law of actions and trials at nisi prius. By Isaac 'espinasse, of Gray's Inn, Esq. Barrister at Law. The third edition, corrected, with considerable additions from printed and manuscript cases. In two volumes. ...",
-				"place": "London",
-				"url": "http://find.galegroup.com/ecco/infomark.do?&source=gale&prodId=ECCO&userGroupName=viva_gmu&tabID=T001&docId=CW3325179878&type=multipage&contentSet=ECCOArticles&version=1.0",
-				"pages": "469",
-				"numPages": "469",
-				"DOI": "Monograph",
-				"date": "1798",
-				"volume": "Volume 1",
-				"accessDate": "2012/05/07",
-				"archive": "Eighteenth Century Collection Online",
-				"numberOfVolumes": "2",
-				"libraryCatalog": "Gale"
-			}
-		]
-	}
-]
+var testCases = []
 /** END TEST CASES **/
