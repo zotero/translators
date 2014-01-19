17ef5575b42bf5e4ce2165014006f30de09895d2
adam3smith <karcher@u.northwestern.edu>
2014-01-18 18:11:23 -0700

    fix Chadwyck for some odd failures, expand to more collections

--- Chadwyck Literature Online.js
+++ Chadwyck Literature Online.js
@@ -2,14 +2,14 @@
 	"translatorID": "e13b0f9d-44ba-4ece-aa22-77993bb26ef2",
 	"label": "Chadwyck Literature Online",
 	"creator": "Sebastian Karcher",
-	"target": "^https?://lion\\.chadwyck\\.com/search",
+	"target": "^https?://(lion|collections)\\.chadwyck\\.com/search",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsib",
-	"lastUpdated": "2013-04-17 03:09:28"
+	"lastUpdated": "2014-01-18 18:08:06"
 }
 
 /*
@@ -36,7 +36,9 @@
 
 function detectWeb(doc, url) {
 	//this may not always get the right item type, but doing that may neither be possible nor terribly important.
-	if (url.indexOf("searchFullrec.do?")!=-1||url.indexOf("searchFulltext.do?")!=-1) return "journalArticle";
+	if (url.indexOf("searchFullrec.do?")!=-1||url.indexOf("searchFulltext.do?")!=-1){ 
+		if(ZU.xpathText(doc, '//a[@class="bold" and contains(text(), "Download citation")]/@href')) return "journalArticle";
+	}
 	//I think these are all possible search results - not sure, though, may need to add
 	if (url.indexOf("searchQuick")!=-1||url.indexOf("searchTexts") != -1) return "multiple";
 }
@@ -66,14 +68,32 @@ function doWeb(doc, url){
 }
 
 function scrape(doc, url){
-	var id = url.match(/id=[^&]+/)[0];
+	var id = url.match(/id=[^&]+/);
+	var downloadurl = ZU.xpathText(doc, '//a[@class="bold" and contains(text(), "Download citation")]/@href')
+	if (id) id = id[0]; else id = downloadurl.match(/id=[^&]+/)[0];
 	var area = url.match(/area=([^&]+)/)[1];
 	var carea = area.toLowerCase();
-	//RIS Link
-	var risurl = "http://lion.chadwyck.com/downloadCitation.do?" + id + "&area=" + area + "&citationArea=" + carea + "&citationFormat=procite&citationExport=direct";
-	Z.debug(risurl)
+	var host = doc.location.host ? "http://" + doc.location.host : url.match(/https?:\/\/[^\/]+/)[0]
+	var risurl = "http://" + doc.location.host + "/downloadCitation.do?" + id + "&area=" + area + "&citationArea=" + carea + "&citationFormat=procite&citationExport=direct";
+	fetchRIS(risurl, downloadurl, 1)
+}
+
+function fetchRIS(risurl, downloadurl, retry){
 	Zotero.Utilities.HTTP.doGet(risurl, function (text) {
-		//Z.debug(text)
+		//Z.debug("RIS: " + text)
+		var foundRIS = (text.indexOf('TY  - ') != -1);
+		//Cookie weirdness - on some, but not all pages, this requires a cookie set by loading the citation page. 
+		//That's what we do when the first try fails (modeled after APA Psycnet)
+		if(!foundRIS && retry) {		
+			//retry after accessing citation download page
+			Z.debug('No RIS data. Retrying (' + retry + ').');
+			ZU.doGet(downloadurl, function(){fetchRIS(risurl, --retry);});
+			return;
+		} 
+		else if (!foundRIS){
+			throw("Can't find RIS data");
+			return;
+		}
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
 		translator.setString(text);
@@ -184,6 +204,39 @@ var testCases = [
 				"ISSN": "00322032",
 				"rights": "Copyright © 1996-2009 ProQuest LLC. All Rights Reserved.",
 				"libraryCatalog": "Chadwyck Literature Online"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://collections.chadwyck.com/searchFullrec.do?id=R04919506&area=abell&forward=critref_fr&DurUrl=Yes",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"creators": [
+					{
+						"lastName": "Connolly",
+						"creatorType": "author",
+						"fieldMode": 1
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "Chadwyck Literature Online Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"title": "'the agitation of the autobiographical': national politics and aesthetic autonomy in Philip Roth's Exit Ghost.",
+				"publicationTitle": "Journal of American Studies",
+				"journalAbbreviation": "Journal of American Studies",
+				"date": "2013",
+				"pages": "637",
+				"rights": "Copyright © 1996-2010 ProQuest LLC. All Rights Reserved.",
+				"libraryCatalog": "Chadwyck Literature Online",
+				"shortTitle": "'the agitation of the autobiographical'"
 			}
 		]
 	}
