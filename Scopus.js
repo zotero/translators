f3f2911e82fec7fdc37ad21cdc97df1c41fd0f1b
adam3smith <karcher@u.northwestern.edu>
2014-02-18 16:47:20 -0700

    hopefully fix http vs. https; also fix indenting

--- Scopus.js
+++ Scopus.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "g",
-	"lastUpdated": "2014-02-07 12:11:38"
+	"lastUpdated": "2014-02-18 16:45:41"
 }
 
 /*
@@ -75,41 +75,41 @@ function scrape(doc, url) {
 	var ISSN = ZU.xpathText(doc, '//div[contains(@class, "formatSourceExtended")]/span[strong[contains(text(), "ISSN:")]]');
 	var ISBN = ZU.xpathText(doc, '//div[contains(@class, "formatSourceExtended")]/span[strong[contains(text(), "ISBN:")]]');
 	var language = ZU.xpathText(doc, '//div[contains(@class, "formatSourceExtended")]/span[strong[contains(text(), "Original language:")]]');
-		var get = 'https://' + doc.location.host + 
+	var prefix= url.match(/^https?:\/\//)[0];
+	var get = prefix + doc.location.host + 
 		'/onclick/export.url?oneClickExport=%7b%22Format%22%3a%22RIS%22%2c%22View%22%3a%22CiteAbsKeyws%22%7d&origin=recordpage&eid=';
 		//this is the encoded version of oneClickExport={"Format":"RIS","View":"CiteAbsKeyws"} but since it's always the same, no need to run encodeURL
-		var eid = getEID(url)
-		var rislink = get + eid;	
-		Z.debug(rislink)
-		Zotero.Utilities.HTTP.doGet(rislink, function(text) {
-			// load translator for RIS
-			//Z.debug(text)
-			if (text.search(/T2  -/)!=-1 && text.search(/JF  -/)!=-1){
-				//SCOPUS RIS mishandles alternate titles and journal titles
-				//if both fields are present, T2 is the alternate title and JF the journal title
-				text = text.replace(/T2  -/, "N1  -" ).replace(/JF  -/, "T2  -");
-				
-			}
-
+	var eid = getEID(url)
+	var rislink = get + eid;	
+	Z.debug(rislink)
+	Zotero.Utilities.HTTP.doGet(rislink, function(text) {
+		// load translator for RIS
+		//Z.debug(text)
+		if (text.search(/T2  -/)!=-1 && text.search(/JF  -/)!=-1){
+			//SCOPUS RIS mishandles alternate titles and journal titles
+			//if both fields are present, T2 is the alternate title and JF the journal title
+			text = text.replace(/T2  -/, "N1  -" ).replace(/JF  -/, "T2  -");
+			
+		}
 			var translator = Zotero.loadTranslator("import");
-			translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
-			translator.setString(text);
-			translator.setHandler("itemDone", function(obj, item) {
-				var notes = [];
-				for (i in item.notes) {
-					if (item.notes[i]['note'].match(/Export Date:|Source:/))
-						continue;
-					notes.push(item.notes[i]);
-				}
-				item.notes = notes;
-				item.url = "";
-				item.attachments.push({document: doc, title: "SCOPUS Snapshot", mimeType: "text/html"});
-				if (doi) item.DOI = doi.replace(/DOI:/, "").trim();
-				if (ISSN) item.ISSN = ZU.cleanISSN(ISSN);
-				if (ISBN) item.ISBN = ZU.cleanISBN(ISBN);
-				if (language) item.language = language.replace(/Original language:/, "").trim();
-				item.complete();
-			});
-			translator.translate();
-		});;
+		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+		translator.setString(text);
+		translator.setHandler("itemDone", function(obj, item) {
+			var notes = [];
+			for (i in item.notes) {
+				if (item.notes[i]['note'].match(/Export Date:|Source:/))
+					continue;
+				notes.push(item.notes[i]);
+			}
+			item.notes = notes;
+			item.url = "";
+			item.attachments.push({document: doc, title: "SCOPUS Snapshot", mimeType: "text/html"});
+			if (doi) item.DOI = doi.replace(/DOI:/, "").trim();
+			if (ISSN) item.ISSN = ZU.cleanISSN(ISSN);
+			if (ISBN) item.ISBN = ZU.cleanISBN(ISBN);
+			if (language) item.language = language.replace(/Original language:/, "").trim();
+			item.complete();
+		});
+		translator.translate();
+	});;
 }
