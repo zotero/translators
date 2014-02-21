563fc00b927ed7c502151d13b173b6f02c6e0e30
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2014-02-21 05:35:42 -0600

    [APS] Rewrite for the new site design. Add support for supplemental

--- APS.js
+++ APS.js
@@ -1,73 +1,136 @@
 {
 	"translatorID": "2c310a37-a4dd-48d2-82c9-bd29c53c1c76",
 	"label": "APS",
-	"creator": "Eugeniy Mikhailov and Michael Berkowitz",
-	"target": "^https?://(?:www\\.)?(prola|prl|pra|prb|prc|prd|pre|prx|prst-ab|prst-per|rmp|publish)\\.aps\\.org[^/]*/(toc|abstract|forward|showrefs|supplemental|search)/?",
-	"minVersion": "1.0.0b3.r1",
+	"creator": "Aurimas Vinckevicius",
+	"target": "https?://journals.aps.org",
+	"minVersion": "3.0.12",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2012-08-31 15:26:06"
+	"lastUpdated": "2014-02-21 05:32:29"
 }
 
-// Works for all APS journals: http://publish.aps.org/
+function getSearchResults(doc) {
+	var articles = doc.getElementsByClassName('article');
+	var results = [];
+	for(var i=0; i<articles.length; i++) {
+		if(articles[i].getElementsByClassName('reveal-export').length) {
+			results.push(articles[i]);
+		}
+	}
+	
+	return results;
+}
 
 function detectWeb(doc, url) {
-	// toc indicates table of contents, forward is a "Citing articles" page
-	if (/\/toc\//.test(url) || (/\/forward\//.test(url)) || (/\/search/.test(url))){
+	if(getSearchResults(doc).length){
 		return "multiple";
-	} else {
+	}
+	
+	var title = doc.getElementById('title');
+	if(title && ZU.xpath(title, './/a[@data-reveal-id="export-article"]').length) {
 		return "journalArticle";
 	}
 }
 
 function doWeb(doc, url) {
-		var arts = new Array();
-		if (detectWeb(doc, url) == "multiple") {
-			var items = Zotero.Utilities.getItemArray(doc, doc, "abstract");
-			items = Zotero.selectItems(items);
-			for (var i in items) {
-				arts.push(i);
-			}
-		} else {
-			arts = [url];
+	if(detectWeb(doc, url) == 'multiple') {
+		var results = getSearchResults(doc);
+		var items = {};
+		for(var i=0; i<results.length; i++) {
+			var title = ZU.xpath(results[i], './/h5[@class="title"]/a')[0];
+			items[title.href] = cleanMath(title.textContent);
 		}
 		
-		Zotero.Utilities.processDocuments(arts, function(newDoc) {
-			Zotero.debug(newDoc.title);
-		// Use abstract only if we have one
-		if (newDoc.evaluate('//div[contains(@class, "aps-abstractbox")]/p', newDoc, null, XPathResult.ANY_TYPE, null).iterateNext()) var abs = Zotero.Utilities.trimInternal(newDoc.evaluate('//div[contains(@class, "aps-abstractbox")]/p', newDoc, null, XPathResult.ANY_TYPE, null).iterateNext().textContent);
-			var urlRIS = newDoc.location.href;
-		urlRIS = urlRIS.replace(/(abstract|forward|showrefs|supplemental)/,"export");
-		var post = "type=ris";
-		var snapurl = newDoc.location.href;
-		var pdfurl = snapurl.replace(/(abstract|forward|showrefs|supplemental)/, "pdf");
-		Zotero.Utilities.HTTP.doPost(urlRIS, post, function(text) {
-			//DOI is stored in ID field. Fix it.
-			text = text.replace(/^ID\s\s?-\s/mg, 'DO  - ');
-			// load translator for RIS
-			var translator = Zotero.loadTranslator("import");
-			translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
-			translator.setString(text);
-			translator.setHandler("itemDone", function(obj, item) {
-				item.attachments = [
-					{url:snapurl, title:"APS Snapshot", mimeType:"text/html"},
-					{url:pdfurl, title:"APS Full Text PDF", mimeType:"application/pdf"}
-				];
-				if (abs) item.abstractNote = abs;
-				item.complete();
+		Z.selectItems(items, function(selectedItems) {
+			if(!selectedItems) return true;
+			
+			var urls = [];
+			for(var i in selectedItems) {
+				urls.push(i);
+			}
+			ZU.processDocuments(urls, scrape);
+		});
+	} else {
+		scrape(doc, url);
+	}
+}
+
+function scrape(doc, url) {
+	url = url.replace(/[?#].*/, '').replace(/\/abstract\//, '/{REPLACE}/');
+	// fetch RIS
+	var risUrl = url.replace('{REPLACE}', 'export')
+	           + '?type=ris&download=true';
+	ZU.doGet(risUrl, function(text) {
+		text = text.replace(/^ID\s+-\s+/mg, 'DO  - ');
+		var trans = Zotero.loadTranslator('import');
+		trans.setTranslator('32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7');
+		trans.setString(text);
+		trans.setHandler('itemDone', function(obj, item) {
+			// scrape abstract from page
+			item.abstractNote = ZU.trimInternal(cleanMath(
+				ZU.xpathText(doc, '//section[contains(@class,"abstract")]/div[@class="content"]/p[1]')
+			));
+			
+			// attach PDF
+			if(ZU.xpath(doc, '//section[@id="title"]//a[text()="PDF"]').length) {
+				item.attachments.push({
+					title: 'Full Text PDF',
+					url: url.replace('{REPLACE}', 'pdf'),
+					mimeType: 'application/pdf'
+				});
+			}
+			
+			item.attachments.push({
+				title: "APS Snapshot",
+				document: doc
 			});
-			translator.translate();
-		 }, null, 'UTF-8');
-	}, function() {Zotero.done();});
-	Zotero.wait();
+			
+			if(Z.getHiddenPref && Z.getHiddenPref('attachSupplementary')) {
+				try {
+					// Fetch supplemental info as JSON
+					ZU.doGet(url.replace('{REPLACE}', 'supplemental'), function(text) {
+						var suppInfo = JSON.parse(text);
+						var asLink = Z.getHiddenPref('supplementaryAsLink');
+						for(var i=0; i<suppInfo.components.length; i++) {
+							var supp = suppInfo.components[i];
+							if(!supp.path) continue;
+							if(asLink) {
+								item.attachments.push({
+									title: supp.filename,
+									url: supp.path,
+									mimeType: 'text/html',
+									snapshot: false
+								});
+							} else {
+								item.attachments.push({
+									title: supp.filename,
+									url: supp.path
+								});
+							}
+						}
+					}, function() { item.complete() });
+				} catch(e) {
+					Z.debug('Could not attach supplemental data. ' + e.message);
+				}
+			} else {
+				item.complete()
+			}
+		});
+		trans.translate();
+	});
+}
+
+function cleanMath(str) {
+	//math tags appear to have duplicate content and are somehow left in even after textContent
+	return str.replace(/<(math|mi)[^<>]*>.*?<\/\1>/g, '');
 }/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://prd.aps.org/abstract/PRD/v84/i7/e077701",
+		"url": "http://journals.aps.org/prd/abstract/10.1103/PhysRevD.84.077701",
 		"items": [
 			{
 				"itemType": "journalArticle",
@@ -88,26 +151,24 @@ var testCases = [
 				"seeAlso": [],
 				"attachments": [
 					{
-						"title": "APS Snapshot",
-						"mimeType": "text/html"
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
 					},
 					{
-						"title": "APS Full Text PDF",
-						"mimeType": "application/pdf"
+						"title": "APS Snapshot"
 					}
 				],
 				"DOI": "10.1103/PhysRevD.84.077701",
-				"title": "Hints for a nonstandard Higgs boson from the LHC",
-				"publicationTitle": "Physical Review D",
-				"journalAbbreviation": "Phys. Rev. D",
-				"volume": "84",
-				"issue": "7",
-				"pages": "077701",
 				"url": "http://link.aps.org/doi/10.1103/PhysRevD.84.077701",
-				"date": "October 21, 2011",
+				"journalAbbreviation": "Phys. Rev. D",
+				"issue": "7",
 				"abstractNote": "We reconsider Higgs boson invisible decays into Dark Matter in the light of recent Higgs searches at the LHC. Present hints in the Compact Muon Solenoid and ATLAS data favor a nonstandard Higgs boson with approximately 50% invisible branching ratio, and mass around 143 GeV. This situation can be realized within the simplest thermal scalar singlet Dark Matter model, predicting a Dark Matter mass around 50 GeV and direct detection cross section just below present bound. The present runs of the Xenon100 and LHC experiments can test this possibility.",
 				"libraryCatalog": "APS",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"title": "Hints for a nonstandard Higgs boson from the LHC",
+				"publicationTitle": "Physical Review D",
+				"volume": "84",
+				"pages": "077701",
+				"date": "October 21, 2011"
 			}
 		]
 	},
