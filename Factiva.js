760b0be523f7f12e61d509f5aacc5c7e38b0d0f1
Philipp Zumstein <zuphilip@gmail.com>
2014-03-08 23:08:28 +0100

    Changed Factiva translator, based on the webpage
    * there seems to be no XML-export from Factiva and before the translator was based on this XML-export
    * now the translator identifies the information directly on the webpage(s)
    * Handles Frame Option well as No-Frame Option (with monitorDOMChanges)
    * handles different display options
    * add stable url, snapshot
    * add normalization of date

--- Factiva.js
+++ Factiva.js
@@ -1,175 +1,203 @@
 {
 	"translatorID": "7bdb79e-a47f-4e3d-b317-ccd5a0a74456",
 	"label": "Factiva",
-	"creator": "Simon Kornblith",
-	"target": "^https?://global\\.factiva\\.com/ha/default\\.aspx",
-	"minVersion": "1.0.0b3.r1",
+	"creator": "Philipp Zumstein",
+	"target": "^https?://global\\.factiva\\.com/(?:[gh]a|redir|np)/default\\.aspx",
+	"minVersion": "4.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "g",
-	"lastUpdated": "2012-07-01 00:32:37"
+	"browserSupport": "gcsv",
+	"lastUpdated": "2014-03-10 21:57:48"
 }
 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Factiva Translator, Copyright © 2014 Philipp Zumstein
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
+
 function detectWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
-	if(doc.evaluate('//tr[@class="headline"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-		if(doc.body.className == 'articleView') {
-			return "newspaperArticle";
-		} else {
-			return "multiple";
-		}
-	}
+	Z.monitorDOMChanges(doc.body, { attributes: true });
+	if(doc.body.className == 'articleView') return "newspaperArticle";
+	if(ZU.xpath(doc, '//tr[@class="headline"] | //tr[td[@class="headline"]]').length>0 ) return "multiple";
+	if(ZU.xpath(doc, '//div[contains(@class, "article")]').length>0) return "newspaperArticle";
 }
 
 function doWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
-	var items = new Array();
-	var singlePage = doc.body.className == 'articleView';
-	
-	var tableRows = doc.evaluate('//tr[@class="headline"]', doc, nsResolver, XPathResult.ANY_TYPE, null);
-	var tableRow;
-	while(tableRow = tableRows.iterateNext()) {
-		var hdl = doc.evaluate('.//input[@name="hdl"]', tableRow, nsResolver, XPathResult.ANY_TYPE,
-			null).iterateNext().value;
-		if(!singlePage){
-			items[hdl] = Zotero.Utilities.trimInternal(tableRow.getElementsByTagName("a")[0].textContent);
-		} else {
-			var m = doc.evaluate('.//td[@class="count"]', tableRow, nsResolver, XPathResult.ANY_TYPE, 
-				null).iterateNext().textContent.match(/[0-9]+/);
-			items[m[0]] = hdl;
+	if (detectWeb(doc, url) == "multiple") {
+		var items = new Object();
+		var articles = new Array();
+		var rows = ZU.xpath(doc, '//tr[contains(@class,"headline")] | //tr[td[@class="headline"]]');
+		for(var i=0; i<rows.length; i++) {
+			var title = ZU.xpathText(rows[i], './td[contains(@class,"count")]') + ZU.xpathText(rows[i], './td/a') + " / " + ZU.xpathText(rows[i], './td/div/a');
+			var link = ZU.xpathText(rows[i], './td/a/@href') + "&dfd=FULR&";
+			items[link] = title;
 		}
-	}
-	
-	if(!singlePage) {
-		items = Zotero.selectItems(items);
-		if(!items) return true;
-		
-		var hdls = new Array();
-		for(var hdl in items) {
-			hdls.push(hdl);
-		}
+		Zotero.selectItems(items, function (items) {
+			if (!items) {
+				return true;
+			}
+			for (var i in items) {
+				articles.push(i);
+			}
+			ZU.processDocuments(articles, scrape);
+		});
 	} else {
-		var m = doc.evaluate('//div[@class="articleHeader"][@id="artHdr1"]/span[substring(text(), 1, 7) = "Article"]',
-			doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent.match(/[0-9]+/);
-		var hdls = [items[m[0]]];
-	}
-	
-	var post = "";
-	
-	var hiddenInputs = doc.evaluate('//form[@name="PageBaseForm"]//input[@type="hidden"]', doc, nsResolver,
-		XPathResult.ANY_TYPE, null);
-	var hiddenInput;
-	while(hiddenInput = hiddenInputs.iterateNext()) {
-		// this is some weird shit, but apparently they're very picky
-		post = post+"&"+hiddenInput.name+"="+escape(hiddenInput.value).replace(/\+/g, "%2B").replace(/\%20/g, "+");
-	}
-	
-	var selects = doc.evaluate('//form[@name="PageBaseForm"]//select', doc, nsResolver,
-		XPathResult.ANY_TYPE, null);
-	var select;
-	while(select = selects.iterateNext()) {
-		post = post+"&"+select.name+"="+escape(select.options[select.selectedIndex].value);
-	}
-	post += "&dfd=FULR";
-	for each(var hdl in hdls) {
-		post += "&hdl="+escape(hdl);
-	}
-	post = post.substr(1);
-	post = post.replace(/\//g,"%2F");
-	
-	Zotero.Utilities.HTTP.doPost("/pps/default.aspx?pp=XML", post, function(text) {
-		// Remove xml parse instruction and doctype
-		text = text.replace(/<!DOCTYPE[^>]*>/, "").replace(/<\?xml[^>]*\?>/, "");
-		// kill the XML namespace, too, because we have no way of knowing what it will be, which presents a problem
-		text = text.replace(/<ppsArticleResponse xmlns="[^"]+">/, "<ppsArticleResponse>");
-		// kill the XML namespace here, too
-		text = text.replace(/<ppsHeadlineResponse xmlns="[^"]+">/, "<ppsHeadlineResponse>");
-		// kill hlt tags; they just make parsing harder
-		text = text.replace(/<\/?hlt>/g, "");
-		// kill &nbsp; which shouldn't even be there, since it's not a legit XML entity
-		text = text.replace(/&nbsp;/g," ");
-		//Z.debug(text);
-		var xml = new XML(text);
-
-		var articles = [];
-		// loop through articles
-		for each(var ppsarticle in xml[0]..ppsarticle) {
-			articles.push(ppsarticle.article);
-		}
-		
-		// Consolidated, de-duped results are <ppsHeadlineResponse>
-		if (xml[1]) {
-			for each(var ppshl in xml[1]..contentHeadline) {
-				articles.push(ppshl);
-			}
-		}
-
-		// loop through articles
-		for each(var article in articles) {
-			var newItem = new Zotero.Item("newspaperArticle");
-			
-			newItem.title = Zotero.Utilities.trimInternal(article.headline.paragraph.text().toString());
-			if(article.sourceName.length()) {
-				newItem.publicationTitle = Zotero.Utilities.trimInternal(article.sourceName.text().toString());
-			}
-			if(article.source.sourceName.length()) {
-				newItem.publicationTitle = Zotero.Utilities.trimInternal(article.source.sourceName.text().toString());
-			}
-			for each(var tag in article..name) {
-				newItem.tags.push(tag.text().toString());
-			}
-			if(article.publicationDate.dateTime.length()) {
-				newItem.date = Zotero.Utilities.formatDate(
-					Zotero.Utilities.strToDate(
-						article.publicationDate.dateTime.text().toString()));
-			}
-			if(article.publicationDate.date.length()) {
-				newItem.date = Zotero.Utilities.formatDate(
-					Zotero.Utilities.strToDate(
-						article.publicationDate.date.text().toString()));
-			}
-			if(article.byline.length()) {
-				var byline = Zotero.Utilities.trimInternal(article.byline.text().toString().replace(/By/i, ""));
-				var authors = byline.split(/ (?:\&|and) /i);
-				for each(var author in authors) {
-					newItem.creators.push(Zotero.Utilities.cleanAuthor(author, "author"));
+		var rows = ZU.xpath(doc, '//div[contains(@class, "article")]//tr');
+		if (rows.length > 0) {//works if the display options are either HLPI or FULLR
+			scrape(doc, url);
+		} else {//works if the search rows are invisible but still there
+			var shareLink = doc.getElementById("shareLinkOtherAcctURLHRef").value;
+			var id =  /[&?]an=([^&#]*)[&#]/.exec(shareLink)[1];
+			//Z.debug(id);
+			if (ZU.xpath(doc, '//tr[@class="headline"]').length>0 ) {
+				var correspondingRow = doc.getElementById(id).parentNode.parentNode;
+				var link = ZU.xpathText(correspondingRow, './td/a/@href') + "&dfd=FULR&";
+				ZU.processDocuments(link, scrape);
+			} else {//otherwise
+				var post = "";
+				var hiddenInputs = ZU.xpath(doc, '//form[@name="PageBaseForm"]//input[@type="hidden"]');
+				for(var i=0; i<hiddenInputs.length; i++) {
+					if (hiddenInputs[i].name != "dfd") {
+						post += "&"+hiddenInputs[i].name+"="+encodeURIComponent(hiddenInputs[i].value);
+					}
 				}
-			}
-			newItem.section = article.sectionName.text().toString();
-			newItem.edition = article.edition.text().toString();
-			if (article.snippet.length())
-				newItem.abstractNote = article.snippet.paragraph.text().toString();
-			else if (article.leadParagraph.length())
-				newItem.abstractNote = article.leadParagraph.paragraph.text().toString();			
-			
-			if(article.pages.length()) {
-				newItem.pages = "";
-				for each(var page in article.pages.page) {
-					newItem.pages += ","+page.text().toString();
+				var selects = ZU.xpath(doc, '//form[@name="PageBaseForm"]//select');
+				for(var i=0; i<selects.length; i++) {
+					post += "&"+selects[i].name+"="+encodeURIComponent(selects[i].value);
 				}
-				newItem.pages = newItem.pages.substr(1);
+				post += "&dfd=FULR";//Full Article/Report, plus Indexing
+				var hdlParameter = doc.getElementById("_hdl").value;
+				post += "&hdl=" + encodeURIComponent(hdlParameter);
+				
+				if(url.indexOf('?') == -1) url += '?';
+				ZU.processDocuments(url + post, scrape);
 			}
-			
-			newItem.callNumber = article.accessionNo.text().toString();
-			
-			var m = article.volume.text().toString().match(/ISSN[:\s]*([\-0-9]{8,9})/i);
-			if(m) newItem.ISSN = m[1];
-			if (newItem.title)
-				newItem.complete();
+
 		}
+	}
+}
+
+function scrape(doc, url) {
+	//Z.debug(url);
+	var element = new Array();
+	var elementInverse = new Array();
+	var rows = ZU.xpath(doc, '//div[contains(@class, "article")]//tr');
+
+	var maxLeft = 0;
+	for(var i=0; i<rows.length; i++) {
+		var fieldShortcut = ZU.trimInternal(ZU.xpathText(rows[i], './/td[1]'));
+		var fieldContent = ZU.trimInternal(ZU.xpathText(rows[i], './/td[2]'));
+		element[fieldShortcut] = fieldContent;
+		elementInverse[fieldContent] = fieldShortcut;
+		maxLeft = Math.max(fieldShortcut.length, maxLeft);
+	}
+	if (maxLeft > 4) {//right-to-left language, e.g. arabic
+		element = elementInverse;
+	}
+	
+	var newItem = new Zotero.Item("newspaperArticle");
+	
+	newItem.title = element["HD"];
+	newItem.publicationTitle = element["SN"];
+	newItem.section = element["SE"];
 		
-		Zotero.done();
-	});
-		
-	Zotero.wait();
+	if (element["PD"]) {
+		dateArray = element["PD"].split(/ |\. ?/);
+		if (dateArray.length == 5) {//in Spanish e.g. [8 de diciembre de 2013
+			dateArray = [dateArray[0], dateArray[2], dateArray[4] ];
+		}
+		if (dateArray.length == 3) {//e.g. [8, December, 2013]
+			//order: German, English, French, Italian, Spanish (no dublicates)
+			var monthsMap = {	"Januar":"01", "January":"01", "janvier":"01", "gennaio":"01", "enero":"01",
+								"Februar":"02", "February":"02", "février":"02", "febbraio":"02", "febrero":"02",
+								"März":"03", "March":"03", "mars":"03", "marzo":"03",
+								"April":"04", "avril":"04", "aprile":"04", "april":"04",
+								"Mai":"05", "May":"05", "mai":"05", "maggio":"05", "mayo":"05",
+								"Juni":"06", "June":"06", "juin":"06", "giugno":"06", "junio":"06",
+								"Juli":"07", "July":"07", "juillet":"07", "luglio":"07", "julio":"07",
+								"August":"08", "août":"08", "agosto":"08",
+								"September":"09", "septembre":"09", "settembre":"09", "septiembre":"09", 
+								"Oktober":"10", "October":"10", "octobre":"10", "ottobre":"10", "octubre":"10",
+								"November":"11", "novembre":"11", "noviembre":"11", 
+								"Dezember":"12", "December":"12", "décembre":"12", "dicembre":"12", "dicembre":"12", "diciembre":"12"
+									   
+			};
+			if (dateArray[1] in monthsMap) dateArray[1] = monthsMap[dateArray[1]];
+			if (dateArray[0].length == 1) dateArray[0] = "0"+dateArray[0];
+			var dateString = dateArray[2]+"-"+dateArray[1]+"-"+dateArray[0];
+			newItem.date = dateString;
+		} else {
+			newItem.date = element["PD"];
+		}
+	}
+
+	newItem.edition = element["ED"];
+	newItem.abstractNote = element["LP"];
+	newItem.pages = element["PG"];
+	newItem.publisher = element["PUB"];
+	newItem.language = element["LA"];
+	newItem.volume = element["VOL"];
+	newItem.rights = element["CY"];
+	
+	var authors = new Array();
+	if (element["AU"]) {
+		authors = element["AU"].split(",");
+	} else if (element["BY"]) {
+		var byline = ZU.trimInternal(element["BY"].replace(/By/i, ""));
+		authors = byline.split(/(?:\&| and |,| et )/i);
+	}
+	for(var i=0; i<authors.length; i++) {
+		newItem.creators.push(ZU.cleanAuthor(authors[i], "author"));
+	}
+	
+	//company: element["CO"] --> seems fine as tags
+	//industry: element["IN"] --> broad but still okay
+	//element["NS"] --> too messy
+	//regions: element["RE"] --> too broad, messy
+	var tagString = element["CO"];
+	if (!tagString) {
+		tagString = element["IN"];
+	} else if (element["IN"]) {
+		tagString += " | "+element["IN"];
+	}
+	if (tagString) {
+		var tagArray = tagString.split("|");
+		for(var i=0; i<tagArray.length; i++) {
+			var tagCodeNamePair = tagArray[i].split(":");
+			newItem.tags.push(ZU.trimInternal(tagCodeNamePair[1]));
+		}
+	}
+	
+	if (element["AN"]) {
+		element["AN"] = element["AN"].split(" ")[1];
+		var exportUrl = 'http://global.factiva.com/redir/default.aspx?P=sa&an=' + encodeURIComponent(element["AN"]) + '&cat=a&ep=ASE';
+		newItem.url = exportUrl;
+	}
+	
+	newItem.attachments = [{
+		title: "Snapshot",
+		document:doc
+	}];
+
+	newItem.complete();
 }
