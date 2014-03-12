e8c45fc49c54d458bf2de4f33a3bf1dfa2e3fe3f
adam3smith <karcher@u.northwestern.edu>
2014-03-11 22:21:17 -0600

    fix Google Books

--- Google Books.js
+++ Google Books.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsb",
-	"lastUpdated": "2014-03-01 10:37:34"
+	"lastUpdated": "2014-03-11 22:18:46"
 }
 
 /*
@@ -163,7 +163,7 @@ function getItemArrayGB (doc, inHere, urlRe, rejectRe) {
 	var availableItems = new Object();	// Technically, associative arrays are objects
 
 	//quick check for new format
-	var bookList = ZU.xpath(doc, '//ol[@id="rso"]/li');
+	var bookList = ZU.xpath(doc, '//ol[@id="rso"]/li|//ol[@id="rso"]/div/li');
 	if(bookList.length) {
 		Z.debug("newFormat")
 		for(var i=0, n=bookList.length; i<n; i++) {
@@ -362,11 +362,12 @@ var testCases = [
 				"numPages": "208",
 				"ISBN": "9780313312601",
 				"publisher": "Greenwood Publishing Group",
-				"title": "Gabriel García Márquez",
+				"title": "Gabriel García Márquez: A Critical Companion",
 				"language": "en",
 				"abstractNote": "Winner of the Nobel Prize for Literature in 1982 for his masterpiece \"One Hundred Years of Solitude,\" Gabriel Garc DEGREESD'ia M DEGREESD'arquez had already earned tremendous respect and popularity in the years leading up to that honor, and remains, to date, an active and prolific writer. Readers are introduced to Garc DEGREESD'ia M DEGREESD'arquez with a vivid account of his fascinating life; from his friendships with poets and presidents, to his distinguished career as a journalist, novelist, and chronicler of the quintessential Latin American experience. This companion also helps students situate Garc DEGREESD'ia M DEGREESD'arquez within the canon of Western literature, exploring his contributions to the modern novel in general, and his forging of literary techniques, particularly magic realism, that have come to distinguish Latin American fiction. Full literary analysis is given for \"One Hundred Years of Solitude,\" as well as \"Chronicle of a Death Foretold\" (1981), \"Love in the Time of Cholera\" (1985), two additional novels, and five of Garc DEGREESD'ia M DEGREESD'arquez's best short stories. Students are given guidance in understanding the historical contexts, as well as the characters and themes that recur in these interrelated works. Narrative technique and alternative critical perspectives are also explored for each work, helping readers fully appreciate the literary accomplishments of Gabriel Garc DEGREESD'ia M DEGREESD'arquez.",
 				"date": "2001-01-01",
-				"libraryCatalog": "Google Books"
+				"libraryCatalog": "Google Books",
+				"shortTitle": "Gabriel García Márquez"
 			}
 		]
 	},
@@ -436,7 +437,7 @@ var testCases = [
 						"mimeType": "text/html"
 					}
 				],
-				"numPages": "332",
+				"numPages": "340",
 				"ISBN": "9780415936446",
 				"publisher": "Psychology Press",
 				"title": "Electronic and Experimental Music: Pioneers in Technology and Composition",
