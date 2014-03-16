52b611822c04aff0d54241b691879f3ec974d593
Philipp Zumstein <zuphilip@gmail.com>
2014-03-16 14:28:09 +0100

    Update LexisNexis translator
    * should save now single items to zotero
    * maybe works over different access urls
    * TODO: multiple items not yet properly working
    * TODO: snapshot, ...

--- LexisNexis.js
+++ LexisNexis.js
@@ -1,159 +1,185 @@
 {
-	"translatorID":"b047a13c-fe5c-6604-c997-bef15e502b09",
-	"translatorType":4,
-	"label":"LexisNexis",
-	"creator":"Sean Takats",
-	"target":"https?://[^/]*lexis-?nexis\\.com[^/]*/us/lnacademic",
-	"minVersion":"1.0.0b3.r1",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":true,
-	"lastUpdated":"2010-04-19 05:35:00"
+	"translatorID": "b047a13c-fe5c-6604-c997-bef15e502b09",
+	"label": "LexisNexis",
+	"creator": "Sean Takats, Philipp Zumstein",
+	"target": "https?://[^/]*lexis-?nexis\\.com",
+	"minVersion": "3",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsv",
+	"lastUpdated": "2014-03-16 14:20:57"
 }
 
 function detectWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
-	if (doc.title.substr(doc.title.length-8, 8)=="Document"){
-		var xpath = '//input[@name="cisb"]';
-		var elmt = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-		if (elmt.iterateNext()){
-			return "newspaperArticle";
-		}
+	//besides deciding whether it is a single item or multiple items
+	//it is also important here to select the correct frame! Zotero
+	//will only focus on one frame and it is possible to work with that
+	//frame further.
+
+	//let's go for the navigation bar (2nd frame from top) to call new urls with hidden variables
+	//(this is maybe not the natural choice, but it seems to work)
+	if (url.indexOf("parent=docview") != -1 && url.indexOf("target=results_listview_resultsNav") != -1 ) {
+		Z.debug("1s url = "+url);
+		return "newspaperArticle";
 	}
-	var xpath = '//input[@name="frm_tagged_documents" and @type="checkbox"]';
-	var elmt = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-	if (elmt.iterateNext()){
+	
+	//uk/nexis/contentRenderer.do?rand=0.6035261066437975&resultFormatId=NewsRslt
+	//uk/nexis/frame.do?tokenKey=rsh-14.764477.4893349548&target=results_ResultsList&returnToKey=20_T19440205643&parent=ptframeset&rand=1394919788099&reloadEntirePage=true
+	if (url.indexOf("contentRenderer.do?") != -1 || url.indexOf("target=results_ResultsList") != -1) {
+		Z.debug("1m url = "+url);
 		return "multiple";
 	}
 }
 
-function doWeb(doc, url) {	
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-
-	// define results navigation frame doc for export buttons and hidden fields
-	//var rfDoc = doc.importNode(doc.defaultView.window.top.frames[1].document, true);
-	var rfDoc = doc;
-	var xpath = '//img[@title="Export Bibliographic References"]';	
-
-	var elmt = doc.evaluate(xpath, rfDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-
-	var hostRe = new RegExp("^http(?:s)?://[^/]+");
-	var m = hostRe.exec(doc.location.href);
-	var host = m[0];
-
-	var risb = doc.evaluate('//input[@name="risb"]', rfDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().value;
-	if (doc.title.substr(doc.title.length-8, 8) == "Document") {
-		var cisb = doc.evaluate('//input[@name="cisb"]', rfDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().value;
-		var uri = host+"/us/lnacademic/results/listview/delPrep.do?cisb="+cisb+"&risb="+risb+"&mode=delivery_refworks";
-		var hiddenInputs = doc.evaluate('//form[@name="results_docview_DocumentForm"]//input[@type="hidden" and not(@name="tagData")]', rfDoc, nsResolver,
-			XPathResult.ANY_TYPE, null);
-		
+function scrape(doc, url) {
+	//base is url containing the host and the first two subdirectories
+	//e.g. http://www.lexisnexis.com/uk/nexis/
+	//or   http://www.lexisnexis.com/us/lnacademic/
+	//or   http://www.lexisnexis.com/lnacui2api/
+	var baseRegExp = new RegExp("^http(?:s)?://[^/]+/[^/]+/[^/]+/");
+	var m = baseRegExp.exec(doc.location.href);//or simply url ??
+	var base;
+	if (m) {
+		base = m[0];
 	} else {
-		var cisb = doc.evaluate('//a[@target="_parent"]', rfDoc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().href.match(/cisb=([^&]+)/)[1];
-		var uri = host+"/us/lnacademic/results/listview/delPrep.do?cisb="+cisb+"&risb="+risb+"&mode=delivery_refworks";
-		var hiddenInputs = doc.evaluate('//input[@type="hidden" and not(@name="tagData") ]', rfDoc, nsResolver,
-			XPathResult.ANY_TYPE, null);
-		
+		baseRegExp = new RegExp("^http(?:s)?://[^/]+/[^/]+/");
+		m = baseRegExp.exec(doc.location.href);
+		base = m[0];
 	}
-	var hiddenInput;
+
+	var permaLink = ZU.xpathText(doc,'//input[@name="bookmarkUrl"]/@value');
+	
+	var risb = ZU.xpathText(doc,'//input[@name="risb"]/@value');//or @name="risbId"
+	
+	var cisb = ZU.xpathText(doc,'//input[@name="cisb"]/@value');//or @name="cisbId"
+	if (cisb.length==0) {
+		/* legacy code ?
+		var cisbTemp = ZU.xpath(doc,'//a[@target="_parent"]');
+		if (cisbTemp.length>0) {
+			cisb = cisbTemp[0].href.match(/cisb=([^&]+)/)[1];
+		} else {*/
+		cisb = "";
+	}
+
+	var urlIntermediateSite = base+"results/listview/delPrep.do?cisb="+cisb+"&risb="+risb+"&mode=delivery_refworks";
+
+	var hiddenInputs = ZU.xpath(doc, '//form[@name="results_docview_DocumentForm"]//input[@type="hidden" and not(@name="tagData")]');
+	if (hiddenInputs.length==0) {
+		hiddenInputs = ZU.xpath(doc, '//input[@type="hidden" and not(@name="tagData")]');
+	}
 	var poststring="";
-	while(hiddenInput = hiddenInputs.iterateNext()) {
-		poststring = poststring+"&"+hiddenInput.name+"="+encodeURIComponent(hiddenInput.value);
-	}
-
-	var xpath = '//input[@name="frm_tagged_documents" and @type="checkbox"]';
-	var elmt = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-	if (doc.title.substr(doc.title.length-8, 8)=="Document"){
-		// single page
-		var delRange = "cur";
-		poststring = poststring + "&hiddensearchfield=Narrow+Search&reloadClassif=&format=GNBFI&focusTerms=&nextSteps=0";
-	} else {
-		// get multiple item titles and tags
-		var xpath = '//tr[td/input[@name="frm_tagged_documents"]]';
-		var rows = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var title;
-		var tagNumber;
-		var items = new Object();
-		while (row = rows.iterateNext()){
-			title = doc.evaluate('.//a', row, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-			tagNumber = doc.evaluate('./td/input[@name="frm_tagged_documents"]', row, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().value;
-			items[tagNumber] = title;
-		}		
-		var tagData = "";
-		items = Zotero.selectItems(items);
-		if (!items) {
-			return true;
-		}
-		for (var i in items) {
-			tagData += "-"+i;
-		}
-		tagData = tagData.substr(1);
-		var delRange = "tag";
-		// this is a violatin of human rights
-		poststring = "followlinks=false&tocLoad=results%2FpubtreeWaitMsg.do&listview=results%2Flistview%2Flistview.do&docview=results%2Fdocview%2Fdocview.do&searchurl=search%2FfocusSearch.do&treeMax=true&treeMod=true&treeWidth=0&pubTreeWidth=&sortId=RELEVANCE&pubTreeMax=false&exit=&startDocNo=1&docNo=&selRCDomainId=4&formatStr=GNBLIST&csi=0&offset=0&ftdividerload=&selTocNodeId=&nextOrPrevious=&curDocLni=null&tocCsiForSeqBrws=null&uri=&viewOptionChanged=false&prevFormat=GNBLIST&prevAction=null&sId=34888793&legVerSupported=false&nodeDisplayName=&unCheckedNodes=&checkedNodes=&grayedNodes=&pisb=&docsInCategory=0&backToBrowseKey=&formatType=LIST&minMaxURI=&docviewAccessed=false&showViewTagged=false&hitNo=0&pageorigin=results_listview_Listview&docTitleFull=&originalDocNo=&dtnAvailable=false&changeLocalePath=&alrtsRqstHndlrPath=&archiveURL=&selectedNodeId=&multipleNodes=&dtnDataHandlerUrl=results%2Fdocview%2FdtnDataHandler.do&totalDocsInResult=1000&fromDocNo=0&cartDocIds=&hiddensearchfield=Search+within+results&reloadClassif=&selDomainID=&urlApiUser=false&editSrchRtUrlPresent=false&showWidget=false&bookmarkUrl=&sourceName=Major+U.S.+and+World+Publications&permalinkNarrowSearchTerms=&prevSortOption=Relevance&format=GNBLIST&inlineLafnNarrow=false&focusTerms=&sort=RELEVANCE&nextSteps=0";
+	for (var i=0; i<hiddenInputs.length; i++) {
+		poststring = poststring+"&"+hiddenInputs[i].name+"="+encodeURIComponent(hiddenInputs[i].value);
+	};
+	
+	poststring += "&focusTerms=&nextSteps=0";//seems still be missing
+	
+	ZU.doPost(urlIntermediateSite, poststring, function(text) {
 		
-		poststring = poststring + "&tagData=" + tagData +"&risId=" + risb + "&cisbId=" + cisb + "&risb=" + risb + "&cisb=" + cisb + "&resultrisbId=" + risb;
-		//Zotero.debug(poststring);
-	} 
-	Zotero.Utilities.HTTP.doPost(uri, poststring, function(text) {
-		uri = host+"/us/lnacademic/delivery/refExport.do";
-		var disb = text.match(/<input type="hidden" name="disb" value="([^"]+)">/);
-		poststring = "delRange="+delRange+"&selDocs=&disb="+disb[1]+"&initializationPage=0";
-		Zotero.Utilities.HTTP.doPost(uri, poststring, function(text) {
-			uri = text.match(/url=([^']+)'/)
-			uri = decodeURIComponent(uri[1]);
-			uri = uri.replace(/http:\/\/[^/]*\//, host+"/");
-			var uris = new Array();
-			uris.push(uri);
-			Zotero.Utilities.processDocuments(uris, function(newDoc){
-				var elmts =newDoc.evaluate('//html', newDoc, nsResolver, XPathResult.ANY_TYPE, null);
-				var elmt;
-				while (elmt = elmts.iterateNext()){
-					var newItem = new Zotero.Item("newspaperArticle");
-					var title = newDoc.evaluate('.//div[@class="HEADLINE"]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-					if (title.textContent){
-						newItem.title = title.textContent;
-					}else{
-						newItem.title = " ";
-					}
-					var date = newDoc.evaluate('.//meta[@name="_lndateissue"]/@content', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-					if (date){
-						date = date.nodeValue;
-						var m = date.match(/([^T]+)T/);
-						date = m[1];
-						if (date.length == 8){
-							date = date.substr(0,4) + "-" + date.substr(4,2) + "-" + date.substr(6,2);
-						} else if (date.length == 6){
-							date = date.substr(0,4) + "-" + date.substr(4,2);
-						}
-						newItem.date = date; 		
-					}
-					var publicationTitle = newDoc.evaluate('.//div[@class="PUB"]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-					if (publicationTitle){
-						newItem.publicationTitle = publicationTitle.textContent;
-					}
-					var section = newDoc.evaluate('.//div[@class="SECTION-INFO"]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-					if (section){
-						newItem.section = section.textContent;				
-					}
-					var author = newDoc.evaluate('.//div[@class="BYLINE"]', elmt, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
-					if (author){
-						newItem.creators.push(Zotero.Utilities.cleanAuthor(author.textContent, "author"));
-					}
-					newItem.repository = "lexisnexis.com";
-					newItem.url = url;
-					newItem.complete()
+		var urlRis = base+"delivery/rwBibiographicDelegate.do";
+		var disb = /<input type="hidden" name="disb" value="([^"]+)">/.exec(text);
+		var initializationPage = /<input type="hidden" name="initializationPage" value="([^"]+)">/.exec(text);
+		
+		var poststring2 = "screenReaderSupported=false&delRange=cur&selDocs=&exportType=dnldBiblio&disb="+disb[1]+"&initializationPage="+initializationPage[1];
+		Z.debug(poststring2);
+
+		ZU.doPost(urlRis, poststring2, function(text) {
+			var risData = text;
+			//most authors are saved in N1 tag, correct that:
+			text = text.replace(/^N1\s+-\s+(\w.*)$/mg, "AU - $1" );
+			Z.debug(text);
+			
+			var trans = Zotero.loadTranslator('import');
+			trans.setTranslator('32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7');//https://github.com/zotero/translators/blob/master/RIS.js
+			trans.setString(text);
+
+			trans.setHandler('itemDone', function (obj, item) {
+				//correct date format in RIS e.g. PY - 2013/05/09/
+				if (item.date) {
+					item.date = item.date.replace(/\/$/,'').replace(/\//g,'-');
 				}
-				Zotero.done();
+				
+				item.url = permaLink;
+				
+				//for debugging TODO: delete later
+				item.notes.push({note:risData});
+				
+				//TODO: make it better than just this frame
+				item.attachments.push( { title: "Snapshot", document:doc } );
+				
+				//TODO: permalink --> problem with redirection, cookies?
+				item.attachments.push( {
+					url: permaLink,
+					title: "LexisNexis Entry",
+					mimeType: "text/html",
+					snapshot: true
+				} );
+				
+				
+				item.complete();
 			});
+		
+			trans.translate();
+			
 		});
 	});
-	Zotero.wait();
+
+}
+
+
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) == "multiple") {//TODO: not properly working...because of iframes
+		//iframes are problematic for the xpath-expressions and they do not contain anything useful
+		//--> tried to delete them BUT couldn't get a grip at them
+		//neither an xpath-expression, getElementsByTagName, nor getElementById could get a handle on them :-(
+		//it seems that with these functions we only have access to the content of the first iframe (and nothing around/besides it)
+		
+		var items = new Object();
+		var articles = new Array();
+		
+		/* Attemp to grab the elements from pure text --> did not work!
+		var docHTML = doc.documentElement.outerHTML;
+		Z.debug(docHTML);// --> not the whole document only the google-translate iframe !!
+		var searchString = 'name="frm_tagged_documents"';
+		var startIndex = 0;
+		var linkBegin; var linkEnd; var titleBegin; var titleEnd; var link; var title;
+		var test = docHTML.indexOf(searchString,startIndex);
+		while (test != -1) {
+			Z.debug("found one");
+			linkBegin = docHTML.indexOf('<a href="',test)+10;
+			linkEnd = docHTML.indexOf('"',linkBegin)-1;
+			titleBegin = docHTML.indexOf('>',linkEnd)+1;
+			titleEnd = docHTML.indexOf('</a>',titleBegin)-1;
+			link = docHTML.substring(linkBegin, linkEnd);
+			title = docHTML.substring(titleBegin, titleEnd);
+			items[link] = title;
+			test = docHTML.indexOf(searchString,titleEnd);
+		}
+		*/
+		
+		//should work in theory, but not in practice
+		var rows = ZU.xpath(doc, '//tr[td/input[@name="frm_tagged_documents"]]');
+		Z.debug("rows.length = " + rows.length);
+		for(var i=0; i<rows.length; i++) {
+			if (ZU.xpath(rows[i], './td/a').length>0 ) {//exclude weblinks
+				var title = ZU.xpathText(rows[i], './td/a');
+				var link = ZU.xpathText(rows[i], './td/a/@href');
+				items[link] = title;
+			}
+		}
+		Zotero.selectItems(items, function (items) {
+			if (!items) {
+				return true;
+			}
+			for (var i in items) {
+				articles.push(i);
+			}
+			ZU.processDocuments(articles, scrape);
+		});
+	} else {
+		scrape(doc, url);
+	}
+	
 }
