8fb729fa771da45c6b5e6260af04786f5c3befff
adam3smith <karcher@u.northwestern.edu>
2014-04-08 11:33:13 -0600

    add carat in LoC ISBN search translator

--- Library of Congress ISBN.js
+++ Library of Congress ISBN.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 8,
 	"browserSupport": "gcsb",
-	"lastUpdated": "2014-02-04 16:10:34"
+	"lastUpdated": "2014-04-08 16:10:34"
 }
 
 
@@ -25,8 +25,8 @@ function detectSearch(item) {
 function doSearch(item) {
 	//Sends an SRU formatted as CQL to the library of Congress asking for marcXML back
 	//http://www.loc.gov/standards/sru/
-	ZU.doGet("http://lx2.loc.gov:210/LCDB?operation=searchRetrieve&version=1.1&query=bath.ISBN=" + ZU.cleanISBN(item.ISBN) + "&maximumRecords=1", function (text) {
-		Z.debug(text);
+	ZU.doGet("http://lx2.loc.gov:210/LCDB?operation=searchRetrieve&version=1.1&query=bath.ISBN=^" + ZU.cleanISBN(item.ISBN) + "&maximumRecords=1", function (text) {
+		//Z.debug(text);
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("edd87d07-9194-42f8-b2ad-997c4c7deefd");
 		translator.setString(text);
