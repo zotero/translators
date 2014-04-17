b05298afbb15b6dfa173258599f9879b7442c582
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2014-04-16 19:58:43 -0500

    [Amazon.com] Don't break translation on search errors

--- Amazon.com.js
+++ Amazon.com.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsbv",
-	"lastUpdated": "2014-04-15 20:51:52"
+	"lastUpdated": "2014-04-16 20:51:52"
 }
 
 var searchRe = new RegExp('^https?://(?:www\.)?amazon\.([^/]+)/(gp/search/|(gp/)?registry/(wishlist|registry)|exec/obidos/search-handle-url/|s/|s\\?|[^/]+/lm/|gp/richpub/)');
@@ -289,6 +289,10 @@ function scrape(doc) {
 				}
 			});
 			search.translate();
+		});
+		search.setHandler("error", function(obj, errror) {
+			// we mostly need this handler to prevent the default one from kicking in
+			Z.debug("ISBN search for " + item.ISBN + " failed: " + error);
 		});
 		search.setHandler("done", function(obj) {
 			item.complete();
