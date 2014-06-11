3c79cea4978b26114608ffbb642d2d595802d31e
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2014-06-10 20:17:00 -0500

    [CanLII] Add French support Closes #748

--- CanLII (English).js
+++ CanLII (English).js
@@ -1,21 +1,20 @@
 {
 	"translatorID": "84799379-7bc5-4e55-9817-baf297d129fe",
-	"label": "CanLII (English)",
+	"label": "CanLII",
 	"creator": "Sebastian Karcher",
-	"target": "^https?://(www\\.)?canlii\\.org.*/en/",
-	"minVersion": "2.1.9",
+	"target": "^https?://(?:www\\.)?canlii\\.org/(?:en|fr)/",
+	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2014-04-28 19:52:25"
+	"lastUpdated": "2014-06-11 01:13:42"
 }
 
-var canLiiRegexp = /https?:\/\/(www\.)?canlii\.org\/.*en\/[^\/]+\/[^\/]+\/doc\/.+/;
+var canLiiRegexp = /https?:\/\/(?:www\.)?canlii\.org[^\/]*\/(?:en|fr)\/[^\/]+\/[^\/]+\/doc\/.+/;
 
 function detectWeb(doc, url) {
-
 	if (canLiiRegexp.test(url)) {
 		return "case";
 	} else {
@@ -32,70 +31,70 @@ function detectWeb(doc, url) {
 function scrape(doc, url) {
 
 	var newItem = new Zotero.Item("case");
-	var voliss = ZU.xpathText(doc, '//td[contains(@class, "canlii-label") and contains(text(), "Citation:")]/following-sibling::td');
+	var voliss = doc.getElementsByClassName('documentMeta-citation')[0].nextElementSibling;
+	voliss = ZU.trimInternal(
+		ZU.xpathText(voliss, './node()[not(self::script)]', null, '')
+	);
 	//Z.debug("voliss: ("+voliss+")")
-	var casename = voliss.match(/.+?,/)[0].replace(/,/, "").trim();
-	//Z.debug("casename: ("+casename+")");
-	var court = voliss.match(/,\s*\d{4}\s*[A-Z]+/);
-	//Z.debug("court: ("+court+")");
-	var reportvl = voliss.match(/\]\s*\d+/);
-	//Z.debug("reportvl: ("+reportvl+")");
-	var reporter = voliss.match(/\]\s*\d+\s*[A-Z]+/);
-	//Z.debug("reporter: ("+reporter+")");
-	var reporterpg = voliss.match(/\]\s*\d+\s*[A-Z]+\s*\d+/);
-	//Z.debug("reporterpg: ("+reporterpg+")");
-	var page = voliss.match(/,\s*\d{4}\s*[A-Z]+\s*\d+/);
-	var dateDocket = ZU.xpathText(doc, '//td[@class="canlii-label" and contains(text(), "Date:")]/following-sibling::td');
-
+	
+	var casename = voliss.match(/.+?(?=\s*,)/)[0];
 	newItem.caseName = newItem.title = casename;
-	if (court) newItem.court = court[0].replace(/,\s*\d{4}\s*/, "").trim();;
-	if (reporter) newItem.reporter = reporter[0].replace(/\]\s*\d+\s*/, "");
-
-	if (dateDocket){
+	//Z.debug("casename: ("+casename+")");
+	
+	var court = voliss.match(/,\s*\d{4}\s*([A-Z]+)/);
+	if (court) newItem.court = court[1];
+	//Z.debug("court: ("+court+")");
+	
+	var reportvl = voliss.match(/\]\s*(\d+)/);
+	if (reportvl) newItem.reporterVolume = reportvl[1];
+	//Z.debug("reportvl: ("+reportvl+")");
+	
+	var reporter = voliss.match(/\]\s*\d+\s*([A-Z]+)/);
+	if (reporter) newItem.reporter = reporter[1];
+	//Z.debug("reporter: ("+reporter+")");
+	
+	var reporterpg = voliss.match(/(?:\]\s*\d+|,\s*\d{4})\s*[A-Z]+\s*(\d+)/);
+	if (reporterpg) newItem.firstPage = reporterpg[1];
+	//Z.debug("reporterpg: ("+reporterpg+")");
+	
+	var dateDocket = doc.getElementsByClassName('canlii-label')[0];
+	if (dateDocket && dateDocket.nextElementSibling) {
+		dateDocket = ZU.trimInternal(dateDocket.nextElementSibling.textContent);
 		var date = dateDocket.match(/\d{4}-\d{2}-\d{2}/);
 		if (date) newItem.dateDecided = date[0];
-		var docket = ZU.trimInternal(dateDocket).match(/\(Docket:(.+?)\)/);
+		var docket = ZU.trimInternal(dateDocket).match(/\(\s*(?:Docket|Dossier)\s*:\s*(.+?)\s*\)/);
 		if (docket) newItem.docketNumber = docket[1];
 	}
-	if (reporterpg) newItem.firstPage = reporterpg[0].replace(/\]\s*\d+\s*[A-Z]+\s*/, "");
-	//
-	if (!reporterpg && page) newItem.firstPage = page[0].replace(/,\s*\d{4}\s*[A-Z]+\s*/, "");
-	if (reportvl) newItem.reporterVolume = reportvl[0].replace(/\]\s*/, "");
 
 	// attach link to pdf version
-	Z.debug(url)
-	var pdfurl = url.replace(/\.html.+/, ".pdf");
-	if (pdfurl) {
-		newItem.attachments = [{
-			url: pdfurl,
-			title: "CanLII Full Text PDF",
-			mimeType: "application/pdf"
-		}];
-	}
+	//Z.debug(url)
+	var pdfurl = url.replace(/\.html(?:[?#].*)?/, ".pdf");
+	newItem.attachments.push({
+		url: pdfurl,
+		title: "CanLII Full Text PDF",
+		mimeType: "application/pdf"
+	});
 	newItem.attachments.push({
 		document: doc,
-		title: "CanLII Snapshot",
-		mimeType: "text/html"
+		title: "CanLII Snapshot"
 	});
 	newItem.complete();
 }
 
 function doWeb(doc, url) {
-
 	if (canLiiRegexp.test(url)) {
 		scrape(doc, url);
 	} else {
-
-		var items = Zotero.Utilities.getItemArray(doc, doc, canLiiRegexp);
-		var articles = [];
+		var items = ZU.getItemArray(doc, doc, canLiiRegexp);
 		Zotero.selectItems(items, function (items) {
 			if (!items) {
 				return true;
 			}
+			var articles = [];
 			for (var i in items) {
 				articles.push(i);
 			}
-			Zotero.Utilities.processDocuments(articles, scrape);
+			ZU.processDocuments(articles, scrape);
 		});
 	}
 }
@@ -108,7 +107,7 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "http://canlii.org/en/ca/scc/doc/2010/2010scc2/2010scc2.html",
+		"url": "http://www.canlii.org/en/ca/scc/doc/2010/2010scc2/2010scc2.html",
 		"items": [
 			{
 				"itemType": "case",
@@ -122,19 +121,16 @@ var testCases = [
 						"mimeType": "application/pdf"
 					},
 					{
-						"title": "CanLII Snapshot",
-						"mimeType": "text/html"
+						"title": "CanLII Snapshot"
 					}
 				],
-				"title": "MiningWatch Canada v. Canada (Fisheries and Oceans)",
 				"caseName": "MiningWatch Canada v. Canada (Fisheries and Oceans)",
 				"court": "SCC",
-				"reporter": "SCR",
-				"dateDecided": "2010-01-21",
-				"docketNumber": "32797",
-				"firstPage": "6",
 				"reporterVolume": "1",
-				"libraryCatalog": "CanLII"
+				"reporter": "SCR",
+				"firstPage": "6",
+				"dateDecided": "2010-01-21",
+				"docketNumber": "32797"
 			}
 		]
 	},
@@ -154,17 +150,75 @@ var testCases = [
 						"mimeType": "application/pdf"
 					},
 					{
-						"title": "CanLII Snapshot",
-						"mimeType": "text/html"
+						"title": "CanLII Snapshot"
 					}
 				],
-				"title": "Suttie v. Canada (Attorney General)",
 				"caseName": "Suttie v. Canada (Attorney General)",
 				"court": "FC",
-				"dateDecided": "2011-02-02",
-				"docketNumber": "T-1089-10",
 				"firstPage": "119",
-				"libraryCatalog": "CanLII"
+				"dateDecided": "2011-02-02",
+				"docketNumber": "T-1089-10"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://canlii.org/fr/ca/csc/nav/date/2010.html",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://www.canlii.org/fr/ca/csc/doc/2010/2010csc2/2010csc2.html",
+		"items": [
+			{
+				"itemType": "case",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "CanLII Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "CanLII Snapshot"
+					}
+				],
+				"caseName": "Mines Alerte Canada c. Canada (Pêches et Océans)",
+				"court": "CSC",
+				"reporterVolume": "1",
+				"reporter": "RCS",
+				"firstPage": "6",
+				"dateDecided": "2010-01-21",
+				"docketNumber": "32797"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.canlii.org/fr/ca/cfpi/doc/2011/2011cf119/2011cf119.html?searchUrlHash=AAAAAQAjU3V0dGllIHYuIENhbmFkYSAoQXR0b3JuZXkgR2VuZXJhbCkAAAAAAQ",
+		"items": [
+			{
+				"itemType": "case",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"title": "CanLII Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "CanLII Snapshot"
+					}
+				],
+				"caseName": "Suttie c. Canada (Procureur Général)",
+				"court": "CF",
+				"firstPage": "119",
+				"dateDecided": "2011-02-02",
+				"docketNumber": "T-1089-10"
 			}
 		]
 	}
