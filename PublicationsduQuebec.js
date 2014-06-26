e1eceaa7314a31df84827119188f102b53bf15f7
Marc Lajoie <marclajoie@me.com>
2014-06-26 08:25:51 -0400

    Update PublicationsduQuebec.js
    Removed extra newlines ok.

--- PublicationsduQuebec.js
+++ PublicationsduQuebec.js
@@ -11,35 +11,6 @@
 	"browserSupport": "gcsib",
 	"lastUpdated": "2014-06-24 10:16:52"
 }
-/*PublicationduQuebec Statutes and Regulations
-
-Copyright (C) 2014 Marc Lajoie
-
-
-This program is free software: you can redistribute it and/or modify
-
-it under the terms of the GNU General Public License as published by
-
-the Free Software Foundation, either version 3 of the License, or
-
-(at your option) any later version.
-
-
-This program is distributed in the hope that it will be useful,
-
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-
-GNU General Public License for more details.
-
-
-
-You should have received a copy of the GNU General Public License
-
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-*/
 
 var pubqcRegexp = /https?:\/\/(?:www2\.)?publicationsduquebec\.gouv\.qc\.ca\/dynamicSearch\/.+/;
 
