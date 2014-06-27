a567c4d842380c1d636de15347c74b6107a4dd42
Marc Lajoie <marclajoie@me.com>
2014-06-26 20:05:35 -0400

    Update PublicationsduQuebec.js
    Added the copyright notice and fixed the extra space in newItem.title.

--- PublicationsduQuebec.js
+++ PublicationsduQuebec.js
@@ -11,6 +11,39 @@
 	"browserSupport": "gcsib",
 	"lastUpdated": "2014-06-24 10:16:52"
 }
+/*
+
+Publications du Québec Translator
+
+Copyright (C) 2014 Marc Lajoie
+
+
+
+This program is free software: you can redistribute it and/or modify
+
+it under the terms of the GNU General Public License as published by
+
+the Free Software Foundation, either version 3 of the License, or
+
+(at your option) any later version.
+
+
+
+This program is distributed in the hope that it will be useful,
+
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+
+GNU General Public License for more details.
+
+
+
+You should have received a copy of the GNU General Public License
+
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
++*/
 
 var pubqcRegexp = /https?:\/\/(?:www2\.)?publicationsduquebec\.gouv\.qc\.ca\/dynamicSearch\/.+/;
 
@@ -36,7 +69,7 @@ function scrape(doc, url) {
 	var codeloi = doc.getElementsByClassName('Alpha')[0] || doc.getElementsByClassName('Libelle')[0];
 	codeloi = ZU.trimInternal(codeloi.textContent);
 	
-	newItem.title=titleloi+" ";
+	newItem.title=titleloi;
 	
 	if (codeloi.indexOf("chapitre")!=-1){
 		newItem.language="french";
