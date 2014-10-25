440bfcb640e6afbaf1e36333b586ce34ed26e382
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2014-10-25 02:20:24 -0500

    [Time.com] Pass through JSBeautifier, minor cleanup, fix detectWeb All tests pass with same data as before. Closes #788 CC @aepheus

--- Time.com.js
+++ Time.com.js
@@ -2,113 +2,121 @@
 	"translatorID": "d9be934c-edb9-490c-a88d-34e2ee106cd7",
 	"label": "Time.com",
 	"creator": "Michael Berkowitz",
-	"target": "^https://([^/]*\\\\.)?time.com/",
+	"target": "^https?://([^/]*\\.)?time.com/",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsv",
-	"lastUpdated": "2014-10-24 21:31:51"
+	"lastUpdated": "2014-10-25 07:16:36"
 }
 
 function detectWeb(doc, url) {
 	if (url.indexOf('results.html') != -1) {
 		return "multiple";
-	}
-	else if (url.search(/\/article\/|\d{4}\/\d{2}\/\d{2}\/./)!=-1
-		|| ZU.xpathText(doc, '//section[@class="article-body"]/div[@class="issue-date"]')) {
+	} else if (url.search(/\/article\/|\d{4}\/\d{2}\/\d{2}\/./) != -1
+		|| ZU.xpath(doc, '//section[@class="article-body"]/div[@class="issue-date"]').length
+		|| doc.getElementsByClassName('active').length
+	) {
 		return "magazineArticle";
-	}//we can get false positives on magazine if the user is scrolling
+	}
+	// TODO: detect new content on scroll, beacause we should not detect on
+	// ads and ToC content
 }
 
 function handleAuthors(authors) {
-	if(authors && authors.trim()) {
+	if (authors && (authors = authors.trim())) {
 		var matches = authors.match(/^\s*([^\/]+?)\s*\/\s*(.+?)\s*$/);
-		if(matches) {
-			if(matches[1] == 'AP' || matches[1] == 'Fortune') {
+		if (matches) {
+			if (matches[1] == 'AP' || matches[1] == 'Fortune') {
 				authors = matches[2];
 			} else {
 				authors = matches[1];
 			}
 		}
+		
 		//x, y and z
-		authors = authors.replace(/^By\s+|\sBy\s+/, "").split(/,| and /i);
-		var authArr = new Array();
-		for(var i=0, n=authors.length; i<n; i++) {
-			authArr.push(ZU.cleanAuthor(ZU.capitalizeTitle(authors[i].replace(/@.+/, "")), 'author'));
+		authors = authors.replace(/^By\s+|\sBy\s+/, "").split(/\s*,\s*|\s+and\s+/i);
+		var authArr = [];
+		for (var i = 0, n = authors.length; i < n; i++) {
+			authArr.push(ZU.cleanAuthor(ZU.capitalizeTitle(authors[i].replace(/\s+@.+/, "")), 'author'));
 		}
-		if(authArr.length)
-			return authArr
+		if (authArr.length) return authArr;
 	}
+	return [];
 }
 
 function handleKeywords(keywords) {
-	if(keywords && keywords.trim()) {
-		return ZU.capitalizeTitle(keywords).split(', ');
+	if (keywords && (keywords = keywords.trim())) {
+		return keywords.split(', ');
 	}
+	return [];
 }
 
 function scrape(doc, url) {
-	var articles = ZU.xpath(doc,'//section/div[@class="wrapper"]/article'),
-		metaUrl = ZU.xpathText(doc, '/html/head/meta[@property="og:url"]/@content'),
-		activeTitle = ZU.xpathText(doc, '//nav/section[@class="article-list"]/article[contains(@class, "active")]');
+	var article = ZU.xpath(doc, '//section/div[@class="wrapper"]/article[contains(@class, "active")]')[0],
+		metaUrl = ZU.xpathText(doc, '/html/head/meta[@property="og:url"]/@content');
+
+	if (article && metaUrl && doc.location.href.indexOf(metaUrl) == -1) {
+		var item = new Zotero.Item("magazineArticle");
+		item.title = ZU.trimInternal(article.getElementsByClassName('article-title')[0].textContent);
+		item.publicationTitle = "Time";
+		item.url = url;
+		item.ISSN = "0040-718X";
 		
-	if(articles && doc.location.href.indexOf(metaUrl) == -1 && activeTitle) {
-		//scrolling down to a new article
-		activeTitle = activeTitle.trim();
-
-		for(var i = 0; i < articles.length; i++){
-			var title = ZU.xpathText(articles[i], 'header//h2[@class="article-title"]');
-			if(activeTitle.indexOf(title) >= 0){
-				var item = new Zotero.Item("magazineArticle");
-				item.publicationTitle = "Time";
-				item.url = url;
-				item.ISSN = "0040-718X";
-				item.title = title;
-				var authors = ZU.xpathText(articles[i], 'header//ul[@class="article-authors"]//span[@class="byline"]/a')
-				if(authors) item.creators = handleAuthors(authors);
-
-				var keywords = ZU.xpathText(articles[i], 'header//a[@class="topic-tag" or @class="section-tag"]');
-				if(keywords) item.tags = handleKeywords(keywords);
-
-				item.abstractNote = ZU.xpathText(articles[i], 'section//h2[@class="article-excerpt"]')
-				item.date = ZU.xpathText(articles[i], 'header//time[@class="publish-date"]/@datetime')
-				item.complete();
-				break;
-			}
+		var authors = article.getElementsByClassName('byline');
+		if (authors.length) {
+			item.creators = handleAuthors(authors
+				.map(function(a) { return ZU.trimInternal(a.textContent) })
+				.join(', ')
+			);
 		}
-	}
-	else{
+
+		var keywords = ZU.xpathText(article, 'header//a[@class="topic-tag" or @class="section-tag"]');
+		if (keywords) item.tags = handleKeywords(keywords);
+
+		item.abstractNote = ZU.xpathText(doc, '//h2[@class="article-excerpt"]');
+		item.date = ZU.xpathText(article, 'header//time[@class="publish-date"]/@datetime');
+		
+		item.complete();
+	} else {
 		var translator = Zotero.loadTranslator('web');
 		translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
 		translator.setDocument(doc);
-	
+		
 		translator.setHandler('itemDone', function(obj, item) {
 			item.itemType = "magazineArticle";
 			item.publicationTitle = "Time";
 			item.url = url;
 			item.ISSN = "0040-718X";
+			
 			var authors = ZU.xpathText(doc, '//meta[@name="byline"]/@content')
 				|| ZU.xpathText(doc, '//span[@class="author vcard"]/a', null, ' and ')
 				|| ZU.xpathText(doc, '//span[@class="entry-byline"]')
-				|| ZU.xpathText(doc, '//header[@class="article-header"]//ul[@class="article-authors"]//span[@class="byline"]/a');
-			if(authors) item.creators = handleAuthors(authors);
+				|| ZU.xpathText(doc, '//header[@class="article-header"]\
+					//ul[@class="article-authors"]//span[@class="byline"]/a');
+			if (authors) item.creators = handleAuthors(authors);
 
 			var title = ZU.xpathText(doc, '//h1[@class="entry-title"]');
-			if(title) item.title = title;
+			if (title) item.title = title;
+			
 			var keywords = ZU.xpathText(doc, '/html/head/meta[@name="keywords"]/@content')
 				|| ZU.xpathText(doc, 'header//a[@class="topic-tag" or @class="section-tag"]');
-			if(keywords) item.tags = handleKeywords(keywords);
-			if(!item.abstractNote) item.abstractNote = ZU.xpathText(doc, '//h2[@class="article-excerpt"]')
-			if(!item.date) item.date = ZU.xpathText(doc, '//time[@class="publish-date"]/@datetime');
+			if (keywords) item.tags = handleKeywords(keywords);
+			
+			if (!item.abstractNote) item.abstractNote = ZU.xpathText(doc, '//h2[@class="article-excerpt"]');
+			if (!item.date) item.date = ZU.xpathText(doc, '//time[@class="publish-date"]/@datetime');
+			
 			item.complete();
 		});
 		
 		translator.getTranslatorObject(function(em) {
-			em.addCustomFields({'date': 'date'});
+			em.addCustomFields({
+				'date': 'date'
+			});
 		});
-		
+
 		translator.translate();
 	}
 }
@@ -123,7 +131,7 @@ function doWeb(doc, url) {
 		
 			var urls = new Array();
 			for (var i in selectedItems) {
-					urls.push(i);
+				urls.push(i);
 			}
 			Z.debug(urls)
 			ZU.processDocuments(urls, scrape);
@@ -139,6 +147,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "The War on Teacher Tenure",
 				"creators": [
 					{
 						"firstName": "Haley Sweetland",
@@ -146,7 +155,18 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
+				"date": "2014-10-23 05:58:37",
+				"ISSN": "0040-718X",
+				"abstractNote": "It’s really difficult to fire a bad teacher. A group of Silicon Valley investors wants to change that",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "time.com",
+				"publicationTitle": "Time",
+				"url": "http://time.com/3533556/the-war-on-teacher-tenure/",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
 				"tags": [
 					"cover story",
 					"education",
@@ -155,20 +175,8 @@ var testCases = [
 					"teahers",
 					"tech"
 				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Snapshot"
-					}
-				],
-				"title": "The War on Teacher Tenure",
-				"publicationTitle": "Time",
-				"url": "http://time.com/3533556/the-war-on-teacher-tenure/",
-				"abstractNote": "It’s really difficult to fire a bad teacher. A group of Silicon Valley investors wants to change that",
-				"libraryCatalog": "time.com",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"ISSN": "0040-718X",
-				"date": "2014-10-23 05:58:37"
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -178,6 +186,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "The New Ebola Protocols",
 				"creators": [
 					{
 						"firstName": "David Von",
@@ -185,26 +194,25 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [
-					"ebola",
-					"medicine"
-				],
-				"seeAlso": [],
+				"date": "2014-10-16 06:26:47",
+				"ISSN": "0040-718X",
+				"abstractNote": "New U.S. cases have health experts rethinking the response and turning to doctors and hospitals that were truly prepared",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "time.com",
+				"publicationTitle": "Time",
+				"url": "http://time.com/3512672/the-new-ebola-protocols/",
 				"attachments": [
 					{
 						"document": "[object]",
 						"title": "Snapshot"
 					}
 				],
-				"title": "The New Ebola Protocols",
-				"publicationTitle": "Time",
-				"url": "http://time.com/3512672/the-new-ebola-protocols/",
-				"abstractNote": "New U.S. cases have health experts rethinking the response and turning to doctors and hospitals that were truly prepared",
-				"libraryCatalog": "time.com",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"ISSN": "0040-718X",
-				"date": "2014-10-16 06:26:47"
+				"tags": [
+					"ebola",
+					"medicine"
+				],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -214,6 +222,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "How the U.S. Postal Service Fell Apart",
 				"creators": [
 					{
 						"firstName": "Josh",
@@ -221,27 +230,26 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
+				"date": "Thursday, Nov. 17, 2011",
+				"ISSN": "0040-718X",
+				"abstractNote": "Battling debilitating congressional mandates and competition online, the USPS is closing thousands of post offices and struggling to find a place in the modern world. But there are people behind the scenes trying to save this American institution",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "content.time.com",
+				"publicationTitle": "Time",
+				"url": "http://content.time.com/time/nation/article/0,8599,2099187,00.html",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
 				"tags": [
 					"Post Offices",
 					"Postal Service",
 					"USPS",
 					"United States Postal Service"
 				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Snapshot"
-					}
-				],
-				"title": "How the U.S. Postal Service Fell Apart",
-				"publicationTitle": "Time",
-				"url": "http://content.time.com/time/nation/article/0,8599,2099187,00.html",
-				"abstractNote": "Battling debilitating congressional mandates and competition online, the USPS is closing thousands of post offices and struggling to find a place in the modern world. But there are people behind the scenes trying to save this American institution",
-				"libraryCatalog": "content.time.com",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"date": "Thursday, Nov. 17, 2011",
-				"ISSN": "0040-718X"
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -251,6 +259,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "On Scene in Indiana and Kentucky: When the Tornadoes Came",
 				"creators": [
 					{
 						"firstName": "Cary",
@@ -258,31 +267,30 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [
-					"weather",
-					"storm",
-					"tornado",
-					"henryville",
-					"indiana",
-					"kentucky",
-					"destruction"
-				],
-				"seeAlso": [],
+				"date": "Sunday, Mar. 04, 2012",
+				"ISSN": "0040-718X",
+				"abstractNote": "The month of March isn't really the heart of the tornado season but they have come fast and with awesome destruction.",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "content.time.com",
+				"publicationTitle": "Time",
+				"shortTitle": "On Scene in Indiana and Kentucky",
+				"url": "http://content.time.com/time/nation/article/0,8599,2108263,00.html",
 				"attachments": [
 					{
 						"title": "Snapshot"
 					}
 				],
-				"title": "On Scene in Indiana and Kentucky: When the Tornadoes Came",
-				"publicationTitle": "Time",
-				"url": "http://content.time.com/time/nation/article/0,8599,2108263,00.html",
-				"abstractNote": "The month of March isn't really the heart of the tornado season but they have come fast and with awesome destruction.",
-				"libraryCatalog": "content.time.com",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"date": "Sunday, Mar. 04, 2012",
-				"ISSN": "0040-718X",
-				"shortTitle": "On Scene in Indiana and Kentucky"
+				"tags": [
+					"destruction",
+					"henryville",
+					"indiana",
+					"kentucky",
+					"storm",
+					"tornado",
+					"weather"
+				],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -292,6 +300,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "Obama Courts AIPAC Before Netanyahu Meeting",
 				"creators": [
 					{
 						"firstName": "Jay",
@@ -299,10 +308,20 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
+				"ISSN": "0040-718X",
+				"abstractNote": "Obama rejected any notion that his administration has not been in Israel's corner. “Over the last three years, as President of the United States, I have kept my commitments to the state of Israel.\" The President then ticked off the number of ways he has supported Israel in the last year.",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "swampland.time.com",
+				"publicationTitle": "Time",
+				"url": "http://swampland.time.com/2012/03/04/obama-courts-aipac-before-netanyahu-meeting/?iid=sl-main-lede",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
 				"tags": [
-					"barack obama",
 					"aipac",
+					"barack obama",
 					"bibi",
 					"iran",
 					"israel",
@@ -312,19 +331,8 @@ var testCases = [
 					"speech",
 					"washington"
 				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Snapshot"
-					}
-				],
-				"title": "Obama Courts AIPAC Before Netanyahu Meeting",
-				"publicationTitle": "Time",
-				"url": "http://swampland.time.com/2012/03/04/obama-courts-aipac-before-netanyahu-meeting/?iid=sl-main-lede",
-				"abstractNote": "Obama rejected any notion that his administration has not been in Israel's corner. “Over the last three years, as President of the United States, I have kept my commitments to the state of Israel.\" The President then ticked off the number of ways he has supported Israel in the last year.",
-				"libraryCatalog": "swampland.time.com",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"ISSN": "0040-718X"
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -334,6 +342,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "Struggling to Stay Afloat: Number of Underwater Homeowners Keeps on Rising",
 				"creators": [
 					{
 						"firstName": "Brad",
@@ -341,41 +350,40 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [
-					"california real estate",
-					"economics & policy",
-					"florida real estate",
-					"mortgages",
-					"personal finance",
-					"real estate & homes",
-					"real estate markets",
-					"the economy",
-					"arizona",
-					"baltimore",
-					"california",
-					"dallas",
-					"florida",
-					"georgia",
-					"nevada",
-					"sunbelt",
-					"underwater",
-					"upside-down"
-				],
-				"seeAlso": [],
+				"ISSN": "0040-718X",
+				"abstractNote": "Despite signs that some housing markets are improving, the overall trend is for home prices (and values) to keep dropping—and dropping. As values shrink, more and more homeowners find themselves underwater, the unfortunate scenario in which one owes more on the mortgage than the home is worth.",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "business.time.com",
+				"publicationTitle": "Time",
+				"shortTitle": "Struggling to Stay Afloat",
+				"url": "http://business.time.com/2012/03/02/struggling-to-stay-afloat-number-of-underwater-homeowners-keeps-on-rising/?iid=pf-main-lede/",
 				"attachments": [
 					{
 						"title": "Snapshot"
 					}
 				],
-				"title": "Struggling to Stay Afloat: Number of Underwater Homeowners Keeps on Rising",
-				"publicationTitle": "Time",
-				"url": "http://business.time.com/2012/03/02/struggling-to-stay-afloat-number-of-underwater-homeowners-keeps-on-rising/?iid=pf-main-lede/",
-				"abstractNote": "Despite signs that some housing markets are improving, the overall trend is for home prices (and values) to keep dropping—and dropping. As values shrink, more and more homeowners find themselves underwater, the unfortunate scenario in which one owes more on the mortgage than the home is worth.",
-				"libraryCatalog": "business.time.com",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"ISSN": "0040-718X",
-				"shortTitle": "Struggling to Stay Afloat"
+				"tags": [
+					"arizona",
+					"baltimore",
+					"california",
+					"california real estate",
+					"dallas",
+					"economics & policy",
+					"florida",
+					"florida real estate",
+					"georgia",
+					"mortgages",
+					"nevada",
+					"personal finance",
+					"real estate & homes",
+					"real estate markets",
+					"sunbelt",
+					"the economy",
+					"underwater",
+					"upside-down"
+				],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
