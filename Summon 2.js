16522de80ee274ba8a0d169b0ecfe71b5b4c661b
Caistarrin Mystical <Caistarrin.Mystical@proquest.com>
2014-11-12 12:43:48 -0800

    Get multiple result sets
    Needed ability to get refs from multiple API queries and then sort
    those refs to keep them in the same order as shown on the webpage

--- Summon 2.js
+++ Summon 2.js
@@ -9,176 +9,214 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsib",
-	"lastUpdated": "2014-11-03 15:42:24"
+	"lastUpdated": "2014-11-12 12:36:57"
 }
 
 function detectWeb(doc, url) {
 	// Make sure the search actually returned something by checking whether the "no results" message is visible
 	var noResultsMsg = ZU.xpath(doc, '//div[contains(@class, "noResults") and not(contains(@class, "ng-hide"))]');
 
-	// Summon always shows a search results page, so it's multiple or nothing
-	if(noResultsMsg.length == 0 || ZU.xpath(noResultsMsg, 'h2[contains(@class, "endOfResults")]').length > 0)
+	if (noResultsMsg.length == 0 || ZU.xpath(noResultsMsg, 'h2[contains(@class, "endOfResults")]').length > 0) {
+		// Summon always shows a search results page, so it's multiple or nothing
 		return "multiple";
-	else
-		return false;
+	}
+
+	return false;
 }
 
 function doWeb(doc, url) {
 	var type = detectWeb(doc, url);
-	if(type == "multiple") {
+	if (type == "multiple") {
 		var results = ZU.xpath(doc, '//li/div[contains(@class, "documentSummary")]//div[contains(@class, "summary")]');
+		var apiUrls = getUrlSet(url, results.length);
+		var counter = apiUrls.length;
+		var docSets = [];
 
-		ZU.HTTP.doGet(getApiUrl(url, results.length), function(text) {
-			var obj = JSON.parse(text);
-			var items = new Object();
-			var documents = [];
-
-			if(obj.rollups.newspaper.documents) {
+		ZU.HTTP.doGet(
+			apiUrls, 
+			function (text, response, url) {
+				var obj = JSON.parse(text);
 				var rollups = obj.rollups.newspaper;
-				var rollupsPosition = rollups.position - 1;
-				documents = obj.documents.slice(0, rollupsPosition).concat(rollups.documents).concat(obj.documents.slice(rollupsPosition));
-			}
-			else {
-				documents = obj.documents;
-			}
+				var docs = [];
+				var pageNumIndex = url.indexOf("pn=") + 3;
+				var index = url.slice(pageNumIndex, url.indexOf("&", pageNumIndex));
 
-			for(var i = 0; i < documents.length; i++) {
-				items[i] = ZU.cleanTags(documents[i].full_title);
-			}
-
-			Zotero.selectItems(items, function(items) {
-				for(item in items) {
-					var ref = documents[item];
-
-					item = new Zotero.Item(getRefType(ref));
-
-					if (ref.abstracts && ref.abstracts.length > 0) {
-						item.abstractNote = ref.abstracts[0].abstract;
-					}
-
-					if (ref.languages && ref.languages.length > 0) {
-						item.language = ref.languages[0];
-					}
-
-					if (ref.copyrights && ref.copyrights.length > 0) {
-						item.rights = ref.copyrights[0];
-					}
-
-					if (ref.uris && ref.uris.length > 0) {
-						item.url = ref.uris[0];
-					}
-
-					item.creators = getAuthors(ref);
-
-					item.title = Zotero.Utilities.cleanTags(ref.full_title);
-
-					if (ref.issns && ref.issns.length > 0){
-						item.ISSN = ref.issns[0];
-					}
-					else if (ref.eissns && ref.eissns.length > 0) {
-						item.ISSN = ref.eissns[0];
-					}
-
-					if (ref.isbn) {
-						item.ISBN = ref.isbn;
-					}
-
-					if (ref.dois && ref.dois.length > 0) {
-						item.DOI = ref.dois[0];
-					}
-
-					if (ref.publisher) {
-						item.publisher = ref.publisher;
-					}
-
-					if (ref.publication_places && ref.publication_places.length > 0) {
-						item.place = ref.publication_places[0];
-					}
-					else if (ref.dissertation_schools && ref.dissertation_schools.length > 0) {
-						item.place = ref.dissertation_schools[0];
-					}
-
-					if (ref.publication_title) {
-						item.publicationTitle = ref.publication_title;
-					}
-
-					if (ref.volumes && ref.volumes.length > 0) {
-						item.volume = ref.volumes[0];
-					}
-
-					if (ref.issues && ref.issues.length > 0) {
-						item.issue = ref.issues[0];
-					}
-
-					if (ref.pages) {
-						item.pages = ref.pages;
-					}
-					else if (ref.start_pages && ref.start_pages.length > 0) {
-						item.pages = ref.start_pages[0] + (ref.end_pages && ref.end_pages.length > 0 ? "-" + ref.end_pages[0] : "");
-					}
-
-					if (ref.page_count) {
-						item.numPages = ref.page_count;
-					}
-
-					if (ref.publication_date) {
-						item.date = ref.publication_date;
-					}
-					else if (ref.publication_years && ref.publication_years.length > 0) {
-						item.date = ref.publication_years[ref.publication_years.length - 1];
-					}
-
-					if (ref.editions && ref.editions.length > 0) {
-						item.edition = ref.editions[0];
-					}
-
-					if (ref.lc_call_numbers && ref.lc_call_numbers.length > 0) {
-						item.callNumber = ref.lc_call_numbers[0];
-					}
-
-					if (ref.publication_series_title) {
-						item.seriesTitle = ref.publication_series_title;
-					}
-
-					if(ref.subject_terms && ref.subject_terms.length > 0) {
-						for(var i = 0; i < ref.subject_terms.length; i++) {
-							item.tags.push(ref.subject_terms[i]);
-						}
-					}
-
-					item.complete();
+				if (rollups.documents) {
+					var rollupsPosition = rollups.position - 1;
+					docs = obj.documents.slice(0, rollupsPosition).concat(rollups.documents).concat(obj.documents.slice(rollupsPosition));
 				}
-			});
-		});
+				else {
+					docs = obj.documents;
+				}
+
+				docSets.push({
+					"index": index,
+					"docs": docs
+				});
+			},
+			function () {
+				orderDocSets(docSets);
+			}
+		);
 	}
 }
 
-function getApiUrl(url, numResults) {
+function orderDocSets(docSets) {
+	var documents = [];
+
+	docSets.sort(function (a, b) {
+		return (a.index < b.index ? -1 : 1)
+	});
+
+	for (var i = 0; i < docSets.length; i++) {
+		documents = documents.concat(docSets[i].docs);
+	}
+
+	getRefs(documents);
+}
+
+function getRefs(documents) {
+	var items = new Object();
+
+	for (var i = 0; i < documents.length; i++) {
+		items[i] = ZU.cleanTags(documents[i].full_title);
+	}
+
+	Zotero.selectItems(items, function(items) {
+		for (item in items) {
+			var ref = documents[item];
+			item = new Zotero.Item(getRefType(ref));
+
+			if (ref.abstracts && ref.abstracts.length > 0) {
+				item.abstractNote = ref.abstracts[0].abstract;
+			}
+
+			if (ref.languages && ref.languages.length > 0) {
+				item.language = ref.languages[0];
+			}
+
+			if (ref.copyrights && ref.copyrights.length > 0) {
+				item.rights = ref.copyrights[0];
+			}
+
+			if (ref.uris && ref.uris.length > 0) {
+				item.url = ref.uris[0];
+			}
+
+			item.creators = getAuthors(ref);
+
+			item.title = Zotero.Utilities.cleanTags(ref.full_title);
+
+			if (ref.issns && ref.issns.length > 0) {
+				item.ISSN = ref.issns[0];
+			}
+			else if (ref.eissns && ref.eissns.length > 0) {
+				item.ISSN = ref.eissns[0];
+			}
+
+			if (ref.isbn) {
+				item.ISBN = ref.isbn;
+			}
+
+			if (ref.dois && ref.dois.length > 0) {
+				item.DOI = ref.dois[0];
+			}
+
+			if (ref.publisher) {
+				item.publisher = ref.publisher;
+			}
+
+			if (ref.publication_places && ref.publication_places.length > 0) {
+				item.place = ref.publication_places[0];
+			}
+			else if (ref.dissertation_schools && ref.dissertation_schools.length > 0) {
+				item.place = ref.dissertation_schools[0];
+			}
+
+			if (ref.publication_title) {
+				item.publicationTitle = ref.publication_title;
+			}
+
+			if (ref.volumes && ref.volumes.length > 0) {
+				item.volume = ref.volumes[0];
+			}
+
+			if (ref.issues && ref.issues.length > 0) {
+				item.issue = ref.issues[0];
+			}
+
+			if (ref.pages) {
+				item.pages = ref.pages;
+			}
+			else if (ref.start_pages && ref.start_pages.length > 0) {
+				item.pages = ref.start_pages[0] + (ref.end_pages && ref.end_pages.length > 0 ? "-" + ref.end_pages[0] : "");
+			}
+
+			if (ref.page_count) {
+				item.numPages = ref.page_count;
+			}
+
+			if (ref.publication_date) {
+				item.date = ref.publication_date;
+			}
+			else if (ref.publication_years && ref.publication_years.length > 0) {
+				item.date = ref.publication_years[ref.publication_years.length - 1];
+			}
+
+			if (ref.editions && ref.editions.length > 0) {
+				item.edition = ref.editions[0];
+			}
+
+			if (ref.lc_call_numbers && ref.lc_call_numbers.length > 0) {
+				item.callNumber = ref.lc_call_numbers[0];
+			}
+
+			if (ref.publication_series_title) {
+				item.seriesTitle = ref.publication_series_title;
+			}
+
+			if (ref.subject_terms && ref.subject_terms.length > 0) {
+				for (var i = 0; i < ref.subject_terms.length; i++) {
+					item.tags.push(ref.subject_terms[i]);
+				}
+			}
+
+			item.complete();
+		}
+	});
+}
+
+function getUrlSet(url, numResults) {
 	var urlArray = decodeURI(url).split('?');
 	var params = urlArray[1].split('&');
-	var apiURL = urlArray[0].replace("#!", "api") + "?pn=1&ps=" + numResults;
+	var apiURL = urlArray[0].replace("#!", "api") + "?";
 	var fvf = "";
+	var urlSet = [];
+	var numPages = Math.floor(numResults / 10);
 
-	for(var i = 0; i < params.length; i++) {
-		if(params[i].indexOf("fvf=") > -1) {
+	for (var i = 0; i < params.length; i++) {
+		if (params[i].indexOf("fvf=") > -1) {
 			fvf = params[i].substring(4);
 		}
 		else {
-			apiURL += "&" + params[i];
+			apiURL += params[i] + "&";
 		}
 	}
 
-	if(fvf.length > 0) {
+	if (fvf.length > 0) {
 		var fvfArray = fvf.split('|');
 
-		for(var i = 0; i < fvfArray.length; i++) {
-			apiURL += "&fvf[]=" + fvfArray[i];
+		for (var i = 0; i < fvfArray.length; i++) {
+			apiURL += "fvf[]=" + fvfArray[i] + "&";
 		}
 	}
 
-	Z.debug(apiURL);
+	for (var i = 1; i <= numPages; i++) {
+		Z.debug(apiURL + "pn=" + i + "&ps=10");
+		urlSet.push(apiURL + "pn=" + i + "&ps=10");
+	}
 
-	return apiURL;
+	return urlSet;
 }
 
 function getAuthors(ref) {
