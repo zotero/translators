9511fc15926d90033381957e6d34c8809d07b8d9
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2014-11-13 09:11:03 -0600

    [ARTstor] Import metadata from the top pop-up window. Plus some data cleanup

--- ARTstor.js
+++ ARTstor.js
@@ -2,14 +2,14 @@
 	"translatorID": "5278b20c-7c2c-4599-a785-12198ea648bf",
 	"label": "ARTstor",
 	"creator": "Sebastian Karcher",
-	"target": "^https?://library\\.artstor.org[^/]*",
+	"target": "^https?://library\\.artstor\\.org",
 	"minVersion": "3.1",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcs",
-	"lastUpdated": "2013-10-30 01:41:51"
+	"lastUpdated": "2014-11-13 15:09:17"
 }
 
 /*
@@ -34,57 +34,98 @@
 	***** END LICENSE BLOCK *****
 */
 
-function getDataRows(doc) {
-	return ZU.xpath(doc, 'html/body/div[@class="MetaDataWidgetRoot"][last()]\
-		//table[@class="headerTable" and .//th[@class="th1"\
-			and contains(text(), "Field")]]\
-		//table[@class="scrollTable" and ./tbody/tr][1]/tbody/tr');
+function getData(doc, checkOnly) {
+	var data = {},
+		widgets = doc.getElementsByClassName('MetaDataWidget'),
+		topWidget = null, topZIndex;
+	
+	// Dtermine the top metadata window
+	for (var i=0; i<widgets.length; i++) {
+		
+		var zindex = doc.defaultView.getComputedStyle(widgets[i], null)
+			.getPropertyValue('z-index');
+		if (!zindex && zindex !== 0) continue;
+		
+		if (!topWidget || topZIndex < zindex) {
+			topWidget = widgets[i];
+			topZIndex = zindex;
+		}
+	}
+	
+	if (!topWidget) return false;
+	
+	var fields = topWidget.getElementsByClassName('field'),
+		found = false;
+	for (var i=0; i<fields.length; i++) {
+		var field = ZU.trimInternal(fields[i].textContent);
+		
+		var value = fields[i].nextElementSibling;
+		if (!value || !value.classList.contains('data')) continue;
+		value = ZU.unescapeHTML(ZU.cleanTags(value.innerHTML));
+		
+		if (!field || !value.trim()) continue;
+		
+		if (checkOnly) return true;
+		
+		found = true;
+		data[field] = value;
+	}
+	
+	return found ? data : false;
 }
 
 function detectWeb(doc, url) {
 	//monitor changes to body's direct children. That's where the metadata popup is added
 	Zotero.monitorDOMChanges(doc.body, {childList: true});
 
-	if (getDataRows(doc).length) {
+	if (getData(doc, true)) {
 		return "artwork";
 	}
 }
 
-function associateData(newItem, dataTags, field, zoteroField) {
-	if (dataTags[field]) {
-		newItem[zoteroField] = dataTags[field];
-	}
-}
-
-function scrape(doc, url) {
-	var dataTags = new Object();
-	var newItem = new Zotero.Item("artwork");
-	var rows = getDataRows(doc);
-	for(var i=0, n=rows.length; i<n; i++){
-		var td = rows[i].getElementsByTagName('td');
-		var field = td[0].textContent;
-		var content = ZU.cleanTags(td[1].innerHTML).replace(/[\r\n]+/g, ' ');
-		if(!field || !content) continue;
-		
-		dataTags[field] = content;
-		if (field == "Creator"){
-		  var artist = content.replace(/\s*\(.*/, '');
-		  if(artist) newItem.creators = ZU.cleanAuthor(artist, "artist", artist.indexOf(',') != -1);
-		}
-		//Z.debug("field: " + field + " content: " + dataTags[field])
-	}
-   //these might not be complete - it's pretty straightforward to add more
-	associateData(newItem, dataTags, "Title", "title");
-	associateData(newItem, dataTags, "Measurements", "artworkSize");
-	associateData(newItem, dataTags, "Rights", "rights");
-	associateData(newItem, dataTags, "Material", "artworkMedium");
-	associateData(newItem, dataTags, "Date", "date");
-	associateData(newItem, dataTags, "Repository", "archive");
-	associateData(newItem, dataTags, "ID Number", "archiveLocation");
-	associateData(newItem, dataTags, "Description", "abstractNote");
-	newItem.complete();
+function associateData(newItem, value, field) {
+	if (!value) return;
+	if (typeof value == 'string') value = ZU.trimInternal(value);
+	if (!value) return;
+	
+	newItem[field] = value;
 }
 
 function doWeb(doc, url) {
-	scrape(doc, url);
-} 
+	var newItem = new Zotero.Item("artwork"),
+		data = getData(doc),
+		content;
+	
+	if (data.Creator) {
+		content = data.Creator.replace(/\s*\(.*/, '');
+		associateData(newItem,
+			ZU.cleanAuthor(content, "artist", artist.indexOf(',') != -1),
+			'creators'
+		);
+	}
+	
+	if (data.Date) {
+		content = data.Date.replace(/^Photographed (?:in )?/, '');
+		associateData(newItem, content, "date");
+	}
+	
+	if (data.Rights) {
+		var m = data.Rights.match(/^.*(©.*)$/m);
+		if (m) associateData(newItem, m[1], "rights");
+	}
+	
+	if (data.Subject) {
+		content = data.Subject.split(/\s*;\s*/);
+		associateData(newItem, content, 'tags');
+	}
+	
+   //these might not be complete - it's pretty straightforward to add more
+	associateData(newItem, data.Title, "title");
+	associateData(newItem, data.Measurements, "artworkSize");
+	associateData(newItem, data.Material, "artworkMedium");
+	associateData(newItem, data.Repository, "archive");
+	associateData(newItem, data['ID Number'], "archiveLocation");
+	associateData(newItem, data.Description, "abstractNote");
+	
+	newItem.complete();
+}
