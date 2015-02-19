573a50ae8b3cff6c1ddd8b0729f8cc949232f58a
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2015-02-18 22:46:51 -0600

    [LC Polaris] Add license and sample catalogs

--- Library Catalog (Polaris).js
+++ Library Catalog (Polaris).js
@@ -12,6 +12,30 @@
 	"lastUpdated": "2015-02-19 04:35:19"
 }
 
+/*
+   Polaris Library Catalog Translator
+   Copyright (C) 2015 Aurimas Vinckevicius
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** Sample catalogs
+ * http://pac.kings.edu/polaris/
+ * http://librarycatalog.eugene-or.gov/Polaris/
+ * http://www.clan.lib.nv.us/polaris/
+ */
+
 function detectWeb(doc, url) {
 	if (url.indexOf('title.aspx') != -1 && getPos(url) !== null) {
 		return getItemType(doc);
