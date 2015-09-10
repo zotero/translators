5944801c6e9311631a027bad07c899a71dc7b5da
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2015-09-10 17:45:32 -0500

    [Engineering Village] Rewrite to clean up and fix multiples Drops notes and attachments (AFAICT, these were links to other catalogs that host full-text) Adds support for pre-selected multiples Re https://forums.zotero.org/discussion/50925/engineering-village-translator-do-not-work-in-chrome/

--- Engineering Village.js
+++ Engineering Village.js
@@ -9,102 +9,112 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsb",
-	"lastUpdated": "2014-02-02 07:20:12"
+	"lastUpdated": "2015-09-10 22:42:33"
 }
 
-function detectWeb(doc, url)
-{
-	var xpath='//span/a[@id="downloadlink"][@href]';
-	if(doc.evaluate(xpath, doc,
-		null,XPathResult.ANY_TYPE,null).iterateNext())
-		{  return "journalArticle";}
+function detectWeb(doc, url) {
+	var downloadLink = doc.getElementById('oneclickDL');
+	if(downloadLink && getDocIDs(downloadLink.href)) {
+		return "journalArticle";
+	}
+	
+	if(getSearchResults(doc, true)) {
+		return "multiple";
+	}
+}
+
+function getDocIDs(url) {
+	var m = url.match(/\bdocidlist=([^&#]+)/);
+	if (!m) return false;
+	
+	return decodeURIComponent(m[1]).split(',');
+}
+
+function getSearchResults(doc, checkOnly) {
+	var rows = doc.getElementsByClassName('result'),
+		items = {},
+		found = false;
+	
+	for (var i=0; i<rows.length; i++) {
+		var checkbox = rows[i].querySelector('input[name="cbresult"]');
+		if (!checkbox) continue;
 		
-	xpath='//div[@id="resultslist"]';
-	if(doc.evaluate(xpath, doc,
-		null,XPathResult.ANY_TYPE,null).iterateNext())
-		{  return "multiple";}		
-	return null; 
-}
-
-function parseRIS(uris)
-{	
-	 Zotero.Utilities.HTTP.doGet(uris, function(text){
-	 	//Z.debug(text)
-			 // load translator for RIS
-			 var translator = Zotero.loadTranslator("import");
-			 translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
-			 translator.setString(text);
-			 translator.translate();
-			 Zotero.done();
-	 }, function() {});
-	 Zotero.wait();
-}
-
-//creates the link to the RIS file
-function createURL(EISESSION,docidlist,curURL)
-{
-	var milli = (new Date()).getTime();
-	var temp = curURL.split('/');		
-	var url = '/delivery/download/submit.url?downloadformat=ris&displayformat=abstract&timestamp=' + milli  + docidlist + '&handlelist=1'
-	//Z.debug("risurl: " + url)
-	return url;
+		var docid = checkbox.getAttribute('docid');
+		if (!docid) continue;
+		
+		var title = rows[i].querySelector('h3.resulttitle');
+		if (!title) continue;
+		
+		if (checkOnly) return true;
+		found = true;
+		
+		items[docid] = {
+			title: ZU.trimInternal(title.textContent),
+			checked: checkbox.checked
+		}
+	}
+	
+	return found ? items : false;
 }
 
 function doWeb(doc, url) {
-		var url;
-		var xpath='//span/a[@id="downloadlink"][@href]';
-	if(doc.evaluate(xpath, doc,
-		null,XPathResult.ANY_TYPE,null).iterateNext())
-	{
-		xpath='//span/a[@id="downloadlink"]/@href';
-		var temp=ZU.xpathText(doc, xpath);
-		Z.debug(temp)
-		var docidlist=temp.match(/\&docidlist=[^&]+/)[0];
-		//Session ID can have a question mark or an ampersand at beginning. If the latter, 
-		//change to question mark
-		var EISESSION;
-		if (temp.match(/(\?|\&)EISESSION=[^&]+/)) var EISESSION=temp.match(/(\?|\&)EISESSION=[^&]+/)[0].replace(/^\&/, "?");
-		url=createURL(EISESSION,docidlist,doc.location.href);
-		parseRIS(url);
-	}
-	else
-	{
-		xpath='//input[@NAME="sessionid"]';
-		var EISESSION=ZU.xpathText(doc, xpath);
-		EISESSION = "?EISESSION=" + EISESSION;
-		xpath='//div[@class="result"]|//div[@class="result odd"]';
-		var articles = new Array();
-		var items=new Object();
-		var rows=doc.evaluate(xpath, doc, null,XPathResult.ANY_TYPE,null);
-		var xpath2='//p[@class="resulttitle"]';
-		xpath2=doc.evaluate(xpath2, doc, null,XPathResult.ANY_TYPE,null);
-		var title;
-		var docidlist;
-		while(row=rows.iterateNext())
-		{
-			docidlist=ZU.xpathText(row, './div/input[@type="checkbox"]/@docid')
-			docidlist= "&docidlist="+ docidlist;
+	if (detectWeb(doc, url) == 'multiple') {
+		Zotero.selectItems(getSearchResults(doc), function (items) {
+			if (!items) return true;
 			
-			url=createURL(EISESSION,docidlist,doc.location.href);
-			
-			title=xpath2.iterateNext();
-			title=title.textContent;
-			
-			items[url]=title;			
-		}
-
-		Zotero.selectItems(items, function (items) {
-			if (!items) {
-				return true;
-			}
+			var ids = [];
 			for (var i in items) {
-				articles.push(i);
+				ids.push(i);
 			}
-			parseRIS(articles, function () {
-				Zotero.done();
-			});
+			
+			fetchRIS(doc, ids);
 		});
+	} else {
+		var downloadLink = doc.getElementById('oneclickDL');
+		fetchRIS(doc, getDocIDs(downloadLink.href));
 	}
+}
+
+function fetchRIS(doc, docIDs) {
+	Z.debug(docIDs);
+	
+	// handlelist to accompany the docidlist. Seems like it just has to be a
+	// list of numbers the same size as the docid list.
+	var handleList = new Array(docIDs.length);
+	for (var i=0; i<docIDs.length; i++) {
+		handleList[i] = i+1;
+	}
+	
+	// The database we're currently using. Compendex is 1
+	// Not tested with multi-database search
+	var db = doc.getElementsByName('database')[0];
+	if (db) db = db.value;
+	if (!db) db = "1";
+	
+	var url = '/delivery/download/submit.url?downloadformat=ris'
+		+ '&filenameprefix=Engineering_Village&displayformat=abstract'
+		+ '&database=' + encodeURIComponent(db)
+		+ '&docidlist=' + encodeURIComponent(docIDs.join(','))
+		+ '&handlelist=' + encodeURIComponent(handleList.join(','));
+	
+	// This is what their web page does. It also sends Content-type and
+	// Content-length parameters in the body, but seems like we can skip that
+	// part
+	ZU.doPost(url, "", function(text) {
+		Z.debug(text);
+		
+		var translator = Zotero.loadTranslator("import");
+		// RIS
+		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+		translator.setString(text);
+		translator.setHandler('itemDone', function(obj, item) {
+			item.attachments = [];
+			item.notes = [];
+			
+			item.complete();
+		})
+		translator.translate();
+	});
 }/** BEGIN TEST CASES **/
 var testCases = []
 /** END TEST CASES **/
