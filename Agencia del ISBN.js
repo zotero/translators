de50bcf68d88df0a636f5cb429be3ed8d15f42d0
Philipp Zumstein <zuphilip@gmail.com>
2015-12-07 17:51:56 +0100

    Add AGPL licences for only usual contributors - A

--- Agencia del ISBN.js
+++ Agencia del ISBN.js
@@ -12,6 +12,29 @@
 	"lastUpdated": "2015-06-10 11:33:38"
 }
 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2008 Michael Berkowitz
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
 function detectWeb(doc, url) {
 	if (doc.evaluate('//div[@class="isbnResultado"]/div[@class="isbnResDescripcion"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
 		return "multiple";
