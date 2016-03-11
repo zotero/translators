24ccfd68c8b87607d9c65a583f9ea1845a5621d4
adam3smith <karcher@u.northwestern.edu>
2016-03-10 23:18:36 -0500

    fix Copernicus translator
    search results are in an iFrame (sigh), that's where the separate target
    is used for

--- Copernicus.js
+++ Copernicus.js
@@ -2,28 +2,67 @@
 	"translatorID": "8082115d-5bc6-4517-a4e8-abed1b2a784a",
 	"label": "Copernicus",
 	"creator": "Michael Berkowitz",
-	"target": "^https?://www\\.(?:adv-sci-res|earth-syst-dynam|adv-geosci|adv-radio-sci|ann-geophys|astrophys-space-sci-trans|atmos-chem-phys|atmos-meas-tech|biogeosciences|clim-past|electronic-earth|hydrol-earth-syst-sci|nat-hazards-earth-syst-sci|nonlin-processes-geophys|ocean-sci|soc-geogr|surv-perspect-integr-environ-soc|the-cryosphere|geosci-model-dev)(?:-discuss)?\\.net/",
+	"target": "^https?://(www\\.(?:adv-sci-res|earth-syst-dynam|adv-geosci|adv-radio-sci|ann-geophys|astrophys-space-sci-trans|atmos-chem-phys|atmos-meas-tech|biogeosciences|clim-past|electronic-earth|hydrol-earth-syst-sci|nat-hazards-earth-syst-sci|nonlin-processes-geophys|ocean-sci|soc-geogr|surv-perspect-integr-environ-soc|the-cryosphere|geosci-model-dev)(?:-discuss)?\\.net/|editor\\.copernicus\\.org/search\\.php)",
 	"minVersion": "2.1",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2014-03-31 13:44:16"
+	"lastUpdated": "2016-03-11 04:16:27"
 }
 
+/*
+    ***** BEGIN LICENSE BLOCK *****
+
+    Copyright © 2016 Sebastian Karcher
+
+    This file is part of Zotero.
+
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+    ***** END LICENSE BLOCK *****
+*/
+
 function detectWeb(doc, url) {
-	if (doc.evaluate('//div[@id="publisher"]/iframe', doc, null, XPathResult.ANY_TYPE, null).iterateNext() || doc.evaluate('//td[*[a[contains(text(), "Abstract")]]]', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
+	if (url.search(/\/search\.php|\.net\/$|issue\d+\.html/)!=-1 && getSearchResults(doc, true)) {
 		return "multiple";
-	} else if (doc.title.match(/Abstract/)) {
+	} else if (doc.title.search(/Abstract|Peer review|Metrics|Relations/) != -1) {
 		return "journalArticle";
 	}
 }
 
+function getSearchResults(doc, checkOnly) {
+    var items = {};
+    var found = false;
+    var rows = doc.getElementsByClassName("article-title");
+    for (var i=0; i<rows.length; i++) {
+        var href = rows[i].href;
+        var title = ZU.trimInternal(rows[i].textContent);
+        if (!href || !title) continue;
+        if (checkOnly) return true;
+        found = true;
+        items[href] = title;
+    }
+    return found ? items : false;
+}
+
 function scrape (doc, url){
 	var abstract = ZU.xpathText(doc, '//span[@class="pb_abstract"]');
-	var link = url.replace(/\.html.*/, ".ris");
+	var link = ZU.xpathText(doc, '//ul[@class="additional_info"]/li/a[contains(text(), "EndNote")]/@href');
 	Zotero.Utilities.HTTP.doGet(link, function(text) {
+		//Z.debug(text)
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
 		translator.setString(text);
@@ -36,13 +75,6 @@ function scrape (doc, url){
 				item.attachments[0].title = item.publicationTitle + " PDF";
 				item.attachments[0].mimeType = "application/pdf";
 			}
-
-			item.attachments.push({
-				title: item.publicationTitle + " Snapshot",
-				url: item.url,
-				mimeType: "text/html",
-				snapshot: true
-			})
 			item.complete();
 		});
 		translator.translate();
@@ -52,45 +84,16 @@ function scrape (doc, url){
 function doWeb(doc, url) {
 	var arts = new Array();
 	if (detectWeb(doc, url) == "multiple") {
-		var items = new Object();
-		if (doc.evaluate('//div[@id="publisher"]/iframe', doc, null, XPathResult.ANY_TYPE, null).iterateNext()) {
-			var link = doc.evaluate('//div[@id="publisher"]/iframe', doc, null, XPathResult.ANY_TYPE, null).iterateNext().src;
-			Zotero.Utilities.HTTP.doGet(link, function(text) {
-				var links = text.match(/<a\s+target=\"_top\"\s+href=\"[^"]+\">[^<]+/g);
-				for (var i =0; i<links.length; i++) {
-					var link = links[i].match(/href=\"([^"]+)\">(.*)/);
-					items[link[1]] = Zotero.Utilities.trimInternal(link[2]) + "...";
-				}
-				Zotero.selectItems(items, function (items) {
-					if (!items) {
-						Zotero.done();
-						return true;
-					}
-					for (var i in items) {
-						arts.push(i);
-					}
-					ZU.processDocuments(arts, scrape);
-				});
-			})
-		} else {
-			var titles = doc.evaluate('//td[*[a[contains(text(), "Abstract")]]]/span[@class="pb_toc_article_title"]', doc, null, XPathResult.ANY_TYPE, null);
-			var links = doc.evaluate('//td[*[a[contains(text(), "Abstract")]]]//a[1]', doc, null, XPathResult.ANY_TYPE, null);
-			var title;
-			var link;
-			while ((title = titles.iterateNext()) && (link = links.iterateNext())) {
-				items[link.href] = title.textContent;
+		var items = getSearchResults(doc);
+		Zotero.selectItems(items, function (items) {
+			if (!items) {
+				return true;
 			}
-			Zotero.selectItems(items, function (items) {
-				if (!items) {
-					Zotero.done();
-					return true;
-				}
-				for (var i in items) {
-					arts.push(i);
-				}
-				ZU.processDocuments(arts, scrape);
-			});
-		}
+			for (var i in items) {
+				arts.push(i);
+			}
+			ZU.processDocuments(arts, scrape);
+		});
 	} else {
 		scrape(doc, url)
 	}
@@ -104,6 +107,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "journalArticle",
+				"title": "Preface ''Precipitation: Measurement, Climatology, Remote Sensing, and Modeling (EGU 2010)''",
 				"creators": [
 					{
 						"lastName": "Michaelides",
@@ -116,35 +120,26 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "May 9, 2011",
+				"DOI": "10.5194/adgeo-30-1-2011",
+				"ISSN": "1680-7359",
+				"abstractNote": "No abstract available.",
+				"journalAbbreviation": "Adv. Geosci.",
+				"libraryCatalog": "Copernicus Online Journals",
+				"pages": "1-2",
+				"publicationTitle": "Adv. Geosci.",
+				"shortTitle": "Preface ''Precipitation",
+				"url": "http://www.adv-geosci.net/30/1/2011/",
+				"volume": "30",
 				"attachments": [
 					{
 						"title": "Adv. Geosci. PDF",
-						"downloadable": true,
 						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Adv. Geosci. Snapshot",
-						"mimeType": "text/html",
-						"snapshot": true
 					}
 				],
-				"title": "Preface ''Precipitation: Measurement, Climatology, Remote Sensing, and Modeling (EGU 2010)''",
-				"journalAbbreviation": "Adv. Geosci.",
-				"volume": "30",
-				"pages": "1-2",
-				"date": "May 9, 2011",
-				"publisher": "Copernicus Publications",
-				"ISSN": "1680-7359",
-				"url": "http://www.adv-geosci.net/30/1/2011/",
-				"DOI": "10.5194/adgeo-30-1-2011",
-				"publicationTitle": "Adv. Geosci.",
-				"abstractNote": "No abstract available.",
-				"libraryCatalog": "Copernicus Online Journals",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"shortTitle": "Preface ''Precipitation"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -154,6 +149,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "journalArticle",
+				"title": "Time domain reflectrometry measurements using a movable obstacle for the determination of dielectric profiles",
 				"creators": [
 					{
 						"lastName": "Will",
@@ -176,48 +172,35 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "May 26, 2008",
+				"DOI": "10.5194/ars-6-1-2008",
+				"ISSN": "1684-9973",
+				"abstractNote": "Microwave techniques for the measurement of the permittivity of soils including the water content of soils and other materials, especially TDR (time domain reflectometry), have become accepted as routine measurement techniques. This summary deals with an advanced use of the TDR principle for the determination of the water content of soil along a probe. The basis of the advanced TDR technique is a waveguide, which is inserted into the soil for obtaining measurements of the effective soil permittivity, from which the water content is estimated, and an obstacle, which can mechanically be moved along the probe and which acts as a reference reflection for the TDR system with an exactly known position. Based on the known mechanical position of the reference reflection, the measured electrical position can be used as a measure for the effective dielectric constant of the environment. Thus, it is possible to determine the effective dielectric constant with a spatial resolution given by the step size of the obstacle displacement.  A conventional industrial TDR-system, operating in the baseband, is used for the signal generation and for the evaluation of the pulse delay time of the obstacle reflection. Thus, a cost effective method for the acquisition of the dielectric measurement data is available.",
+				"journalAbbreviation": "Adv. Radio Sci.",
+				"libraryCatalog": "Copernicus Online Journals",
+				"pages": "1-4",
+				"publicationTitle": "Adv. Radio Sci.",
+				"url": "http://www.adv-radio-sci.net/6/1/2008/",
+				"volume": "6",
 				"attachments": [
 					{
 						"title": "Adv. Radio Sci. PDF",
-						"downloadable": true,
 						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Adv. Radio Sci. Snapshot",
-						"mimeType": "text/html",
-						"snapshot": true
 					}
 				],
-				"title": "Time domain reflectrometry measurements using a movable obstacle for the determination of dielectric profiles",
-				"journalAbbreviation": "Adv. Radio Sci.",
-				"volume": "6",
-				"pages": "1-4",
-				"date": "May 26, 2008",
-				"publisher": "Copernicus Publications",
-				"ISSN": "1684-9973",
-				"url": "http://www.adv-radio-sci.net/6/1/2008/",
-				"DOI": "10.5194/ars-6-1-2008",
-				"publicationTitle": "Adv. Radio Sci.",
-				"abstractNote": "Microwave techniques for the measurement of the permittivity of soils including the water content of soils and other materials, especially TDR (time domain reflectometry), have become accepted as routine measurement techniques. This summary deals with an advanced use of the TDR principle for the determination of the water content of soil along a probe. The basis of the advanced TDR technique is a waveguide, which is inserted into the soil for obtaining measurements of the effective soil permittivity, from which the water content is estimated, and an obstacle, which can mechanically be moved along the probe and which acts as a reference reflection for the TDR system with an exactly known position. Based on the known mechanical position of the reference reflection, the measured electrical position can be used as a measure for the effective dielectric constant of the environment. Thus, it is possible to determine the effective dielectric constant with a spatial resolution given by the step size of the obstacle displacement.\n\n A conventional industrial TDR-system, operating in the baseband, is used for the signal generation and for the evaluation of the pulse delay time of the obstacle reflection. Thus, a cost effective method for the acquisition of the dielectric measurement data is available.",
-				"libraryCatalog": "Copernicus Online Journals",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
-		"url": "http://www.adv-geosci.net/title_and_author_search.html?x=0&y=0&title=measurement",
-		"items": "multiple"
-	},
-	{
-		"type": "web",
-		"url": "http://www.atmos-chem-phys-discuss.net/14/2307/2014/acpd-14-2307-2014.html",
+		"url": "http://www.atmos-chem-phys.net/14/4349/2014/acp-14-4349-2014-metrics.html",
 		"items": [
 			{
 				"itemType": "journalArticle",
+				"title": "TransCom N2O model inter-comparison – Part 1: Assessing the influence of transport and surface fluxes on tropospheric N2O variability",
 				"creators": [
 					{
 						"lastName": "Thompson",
@@ -315,38 +298,38 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "April 30, 2014",
+				"DOI": "10.5194/acp-14-4349-2014",
+				"ISSN": "1680-7324",
+				"issue": "8",
+				"journalAbbreviation": "Atmos. Chem. Phys.",
+				"libraryCatalog": "Copernicus Online Journals",
+				"pages": "4349-4368",
+				"publicationTitle": "Atmos. Chem. Phys.",
+				"shortTitle": "TransCom N2O model inter-comparison – Part 1",
+				"url": "http://www.atmos-chem-phys.net/14/4349/2014/",
+				"volume": "14",
 				"attachments": [
 					{
-						"title": "Atmos. Chem. Phys. Discuss. PDF",
-						"downloadable": true,
+						"title": "Atmos. Chem. Phys. PDF",
 						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Atmos. Chem. Phys. Discuss. Snapshot",
-						"mimeType": "text/html",
-						"snapshot": true
 					}
 				],
-				"title": "TransCom N2O model inter-comparison – Part 1: Assessing the influence of transport and surface fluxes on tropospheric N2O variability",
-				"journalAbbreviation": "Atmos. Chem. Phys. Discuss.",
-				"volume": "14",
-				"issue": "2",
-				"pages": "2307-2362",
-				"date": "January 24, 2014",
-				"publisher": "Copernicus Publications",
-				"ISSN": "1680-7375",
-				"url": "http://www.atmos-chem-phys-discuss.net/14/2307/2014/",
-				"DOI": "10.5194/acpd-14-2307-2014",
-				"publicationTitle": "Atmos. Chem. Phys. Discuss.",
-				"abstractNote": "We present a comparison of chemistry-transport models (TransCom-N2O) to examine the importance of atmospheric transport and surface fluxes on the variability of N2O mixing ratios in the troposphere. Six different models and two model variants participated in the inter-comparison and simulations were made for the period 2006 to 2009. In addition to N2O, simulations of CFC-12 and SF6 were made by a subset of four of the models to provide information on the models proficiency in stratosphere-troposphere exchange (STE) and meridional transport, respectively. The same prior emissions were used by all models to restrict differences among models to transport and chemistry alone. Four different N2O flux scenarios totalling between 14 and 17 Tg N yr−1 (for 2005) globally were also compared. The modelled N2O mixing ratios were assessed against observations from in-situ stations, discrete air sampling networks, and aircraft. All models adequately captured the large-scale patterns of N2O and the vertical gradient from the troposphere to the stratosphere and most models also adequately captured the N2O tropospheric growth rate. However, all models underestimated the inter-hemispheric N2O gradient by at least 0.33 ppb (equivalent to 1.5 Tg N), which, even after accounting for an overestimate of emissions in the Southern Ocean of circa 1.0 Tg N, points to a likely underestimate of the Northern Hemisphere source by up to 0.5 Tg N and/or an overestimate of STE in the Northern Hemisphere. Comparison with aircraft data reveal that the models overestimate the amplitude of the N2O seasonal cycle at Hawaii (21° N, 158° W) below circa 6000 m, suggesting an overestimate of the importance of stratosphere to troposphere transport in the lower troposphere at this latitude. In the Northern Hemisphere, most of the models that provided CFC-12 simulations captured the phase of the CFC-12, seasonal cycle, indicating a reasonable representation of the timing of STE. However, for N2O all models simulated a too early minimum by 2 to 3 months owing to errors in the seasonal cycle in the prior soil emissions, which is still not adequately represented by terrestrial biosphere models. In the Southern Hemisphere, most models failed to capture the N2O and CFC-12 seasonality at Cape Grim, Tasmania, and all failed at the South Pole, whereas for SF6, all models could capture the seasonality at all sites, suggesting that there are large errors in modeled vertical transport in high southern latitudes.",
-				"libraryCatalog": "Copernicus Online Journals",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"shortTitle": "TransCom N2O model inter-comparison – Part 1"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.atmos-chem-phys.net/special_issue15.html",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://www.atmos-chem-phys.net/",
+		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
