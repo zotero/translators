63a93cf5959512464b4a7659013fb743cfae520b
adam3smith <karcher@u.northwestern.edu>
2016-04-05 01:15:46 -0400

    fix Figshare translator
    Significant rewrite; no longer downloading files: I saw a bunch of
    GB-sized ones and other with hundreds of files.

--- Figshare.js
+++ Figshare.js
@@ -2,20 +2,20 @@
 	"translatorID": "ab5983ab-6ad9-4060-aff1-4b455c89a3b3",
 	"label": "Figshare",
 	"creator": "Sebatian Karcher",
-	"target": "^https?://figshare\\.com",
+	"target": "^https?://figshare\\.com/",
 	"minVersion": "1.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsb",
-	"lastUpdated": "2013-12-10 16:57:47"
+	"lastUpdated": "2016-04-05 04:14:23"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 	
-	Figshare translator Copyright © 2013 Sebastian Karcher 
+	Figshare translator Copyright © 2013-2016 Sebastian Karcher
 	This file is part of Zotero.
 	
 	Zotero is free software: you can redistribute it and/or modify
@@ -36,7 +36,7 @@
 
 
 function detectWeb(doc, url) {
-	if (url.indexOf("search?q") != -1 || url.indexOf("/browse") != -1) {
+	if ((url.indexOf("search?q") != -1 || url.indexOf("/browse") != -1 || url.indexOf("/categories/") != -1) && getSearchResults(doc, true)) {
 		return "multiple";
 	} else if (url.indexOf("/article") != -1) {
 		//no great item type here - switch once we have dataset.
@@ -44,33 +44,33 @@ function detectWeb(doc, url) {
 	}
 }
 
-function doWeb(doc, url) {
-	var arts = new Array();
-	if (detectWeb(doc, url) == "multiple") {
+function getSearchResults(doc, checkOnly) {
 		var items = new Object();
-		var title;
-
-		var titles = doc.evaluate('//div[@class="list_elem"]//a[contains(@href, "article")]', doc, null, XPathResult.ANY_TYPE, null);
-		if (titles.iterateNext()) {
-			while (title = titles.iterateNext()) {
-				items[title.href] = title.textContent;
-			}
-		} else {
-			var titles = doc.evaluate('//div[@class="textthumb"]/p', doc, null, XPathResult.ANY_TYPE, null);
-			var links = doc.evaluate('//div[@class="imgthumb"]//a[contains(@href, "article")]', doc, null, XPathResult.ANY_TYPE, null);
-			var link;
-			while ((title = titles.iterateNext()) && (link = links.iterateNext())) {
-				items[link.href] = title.textContent;
-			}
+		var found = false;
+		var titles = ZU.xpath(doc, '//div[@class="item-title"]');
+		//Z.debug(titles.length)
+		for (var i = 0; i<titles.length; i++) {
+			//.href does not work -- can't quite follow why but appears related to path depths
+			var href = ZU.xpathText(titles[i], './@href');
+			var title = titles[i].textContent;
+	 		if (!href || !title) continue;
+			if (checkOnly) return true;
+			found = true;
+			items[href] = title;
 		}
-		Zotero.selectItems(items, function (items) {
+	return found ? items : false;
+}
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
 			if (!items) {
 				return true;
 			}
+			var arts = new Array();
 			for (var i in items) {
 				arts.push(i);
 			}
-			Zotero.Utilities.processDocuments(arts, scrape);
+			ZU.processDocuments(arts, scrape);
 		});
 	} else {
 		scrape(doc, url);
@@ -78,15 +78,14 @@ function doWeb(doc, url) {
 }
 
 function scrape(doc, url) {
-	var id = url.match(/\/(\d+)/);
-	if (id) id = id[1];
-	var get = 'http://figshare.com/articles/exportref?id=' + id;
-	var downloadlink = ZU.xpathText(doc, '//div[@id="download_all"]/a/@href');
-	if (downloadlink.indexOf("javascript:void") != -1) downloadlink = "";
-	var DOI = ZU.xpathText(doc, '//meta[@name="citation_doi"]/@content')
-	ZU.HTTP.doGet(get, function (text) {
+	var risURL = ZU.xpathText(doc, '//div[@class="exports-wrap section"]/div/a[contains(text(), "Ref. manager")]/@href');
+	var DOI = ZU.xpathText(doc, '//meta[@name="DC.identifier"]/@content');
+	//Z.debug(DOI)
+	//Z.debug(risURL)
+	ZU.HTTP.doGet(risURL, function (text) {
 		//Z.debug(text)
 		text = text.trim()
+		text = text.replace(/L4 - .+/g, "");
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
 		translator.setString(text);
@@ -95,7 +94,7 @@ function scrape(doc, url) {
 			for (i in item.creators) {
 				//sometimes there _is_ a comma delimiter
 				if (!item.creators[i].firstName) {
-					item.creators[i] = ZU.cleanAuthor(item.creators[i].lastName, "author")
+					item.creators[i] = ZU.cleanAuthor(item.creators[i].lastName, "author");
 				}
 			}
 			//Remove period at end of title
@@ -105,13 +104,17 @@ function scrape(doc, url) {
 				title: "Figshare Snapshot",
 				mimeType: "text/html"
 			});
-			item.DOI = DOI;
-			if (downloadlink) {
-				item.attachments.push({
-					url: downloadlink,
-					title: "Figshare Download"
-				})
+			//place DOI in DOI field for journal articles, into Extra otherwise
+			if (DOI) {
+				DOI = DOI.replace(/^doi:?\s*/i, "")
+				if (item.itemType == "journalArticle") {
+					item.DOI = DOI;
+				}
+				else if (DOI) {
+					item.extra = "DOI:" + DOI;
+				}
 			}
+
 			item.complete();
 		});
 		translator.translate();
@@ -122,14 +125,16 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "http://figshare.com/articles/browse#thumb",
+		"defer": true,
 		"items": "multiple"
 	},
 	{
 		"type": "web",
-		"url": "http://figshare.com/articles/_Number_of_reported_pertussis_cases_per_week_in_Japan_from_2002_to_2012_/815480",
+		"url": "https://figshare.com/articles/_Number_of_reported_pertussis_cases_per_week_in_Japan_from_2002_to_2012_/815480",
 		"items": [
 			{
 				"itemType": "document",
+				"title": "Number of reported pertussis cases per week in Japan from 2002 to 2012",
 				"creators": [
 					{
 						"firstName": "Yusuke",
@@ -157,46 +162,42 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [
-					"japan",
-					"cases",
-					"2002",
-					"pertussis"
-				],
-				"seeAlso": [],
+				"date": "October 4, 2013",
+				"abstractNote": "Pertussis cases are shown by the black line, with each value representing a week of the year. The percentage of adolescent and adult cases (≥15 years old) per year is shown in red circles. The data were obtained from the Ministry of Health, Labor and Welfare of Japan Infectious Disease Surveillance data. Data regarding the number of adolescent and adult cases in 2012 were not available.",
+				"extra": "DOI: 10.1371/journal.pone.0077165.g001",
+				"libraryCatalog": "Figshare",
+				"url": "https://figshare.com/articles/_Number_of_reported_pertussis_cases_per_week_in_Japan_from_2002_to_2012_/815480",
 				"attachments": [
 					{
 						"title": "Full Text (HTML)",
-						"mimeType": "text/html",
-						"downloadable": true
-					},
-					{
-						"title": "Figure_1",
-						"downloadable": true
+						"mimeType": "text/html"
 					},
 					{
 						"title": "Figshare Snapshot",
 						"mimeType": "text/html"
-					},
-					{
-						"title": "Figshare Download"
 					}
 				],
-				"title": "Number of reported pertussis cases per week in Japan from 2002 to 2012",
-				"url": "http://figshare.com/articles/_Number_of_reported_pertussis_cases_per_week_in_Japan_from_2002_to_2012_/815480",
-				"abstractNote": "Pertussis cases are shown by the black line, with each value representing a week of the year. The percentage of adolescent and adult cases (≥15 years old) per year is shown in red circles. The data were obtained from the Ministry of Health, Labor and Welfare of Japan Infectious Disease Surveillance data. Data regarding the number of adolescent and adult cases in 2012 were not available.",
-				"date": "October 5, 2013",
-				"DOI": "doi:10.6084/m9.figshare.815480",
-				"libraryCatalog": "Figshare",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"tags": [
+					"2002",
+					"cases",
+					"japan",
+					"pertussis"
+				],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
-		"defer": true,
 		"url": "http://figshare.com/articles/search?q=labor&quick=1&x=0&y=0",
+		"defer": true,
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://figshare.com/categories/Biological_Sciences/48",
+		"defer": true,
 		"items": "multiple"
 	}
 ]
