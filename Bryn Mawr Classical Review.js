eedc13996ad82030179d17b2cdbc8f9494f71a1e
John Muccigrosso <Jmuccigr@users.noreply.github.com>
2016-04-09 17:09:04 -0400

    Adding AGPL.

--- Bryn Mawr Classical Review.js
+++ Bryn Mawr Classical Review.js
@@ -9,8 +9,25 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2016-04-07 21:36:13"
+	"lastUpdated": "2016-04-09 17:08:00"
 }
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+	Copyright © 2016 Michael Berkowitz and John Muccigrosso
+	This file is part of Zotero.
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+	***** END LICENSE BLOCK *****
+*/
 
 function detectWeb(doc, url) {
 	if (url.match(/by_reviewer/) || url.match(/by_author/) || url.match(/recent.html/) || url.match(/\/\d{4}\/$/)) {
