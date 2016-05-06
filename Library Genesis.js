3f399905da76bd2d690b059a9a9ef03a16bf25f2
Reverend Wicks Cherrycoke <rev.w.cherrycoke@hmamail.com>
2016-05-06 13:24:02 +0200

    Add license block to Library Genesis translator

--- Library Genesis.js
+++ Library Genesis.js
@@ -9,8 +9,30 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsi",
-	"lastUpdated": "2016-04-22 22:11:28"
+	"lastUpdated": "2016-05-06 13:15:28"
 }
+
+/*
+***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2016 Rev. Wicks Cherrycoke
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+***** END LICENSE BLOCK *****
+*/
 
 var MIME_TYPES = {
 	'pdf': 'application/pdf',
