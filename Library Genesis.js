4f90ebf5bb81e51fab1b63f01e387445b6a40e65
Reverend Wicks Cherrycoke <rev.w.cherrycoke@hmamail.com>
2016-05-07 10:25:41 +0200

    Fix indentation, disable attachment download

--- Library Genesis.js
+++ Library Genesis.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsi",
-	"lastUpdated": "2016-05-06 13:15:28"
+	"lastUpdated": "2016-05-07 08:21:47"
 }
 
 /*
@@ -46,7 +46,7 @@ var AUTHOR_REGEX = /author\s*=\s*{(.*?)}/;
 
 function detectWeb(doc, url) {
 	if (url.indexOf('book/index.php') != -1) {
-	return 'book';
+		return 'book';
 	}
 }
 
@@ -57,48 +57,49 @@ function doWeb(doc, url) {
 	// metadata and use Zotero's built-in bibtex importer
 	var bibtexUrl = "/book/bibtex.php?md5=" + md5Hash;
 	ZU.processDocuments(bibtexUrl, function(bibtexDoc) {
-	var bibtexStr = bibtexDoc.getElementsByTagName("textarea")[0].value
-	var translator = Zotero.loadTranslator('import');
-	translator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4');
-	translator.setString(bibtexStr);
-
-	translator.setHandler('itemDone', function(obj, item) {
-		// The bibtex messes up multiple authors, so we set them ourselves
-		var authorStr = AUTHOR_REGEX.exec(bibtexStr)[1];
-		if (authorStr.indexOf(';') != -1) {
-			item.creators = authorStr.split(";").map(function(author) {
-				// Are we dealing with "last, first" formatting?
-				var useCommas = (author.indexOf(",") != -1);
-				return ZU.cleanAuthor(author, "author", useCommas);
-			});
-		} else {
-			item.creators = authorStr.split(",").map(function(author) {
-			return ZU.cleanAuthor(author, "author", false);
-			});
-		}
-
-		// It also messes up multiple ISBNs, so we just pick the first one
-		if (item.ISBN) {
-			[' ', ','].forEach(function(splitChar) {
-			if (item.ISBN.indexOf(splitChar) != -1) {
-				item.ISBN = item.ISBN.split(splitChar)[0];
+		var bibtexStr = bibtexDoc.getElementsByTagName("textarea")[0].value;
+		var translator = Zotero.loadTranslator('import');
+		translator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4');
+		translator.setString(bibtexStr);
+		translator.setHandler('itemDone', function(obj, item) {
+			// The bibtex messes up multiple authors, so we set them ourselves
+			var authorStr = AUTHOR_REGEX.exec(bibtexStr)[1];
+			if (authorStr.indexOf(';') != -1) {
+				item.creators = authorStr.split(";").map(function(author) {
+					// Are we dealing with "last, first" formatting?
+					var useCommas = (author.indexOf(",") != -1);
+					return ZU.cleanAuthor(author, "author", useCommas);
+				});
+			} else {
+				item.creators = authorStr.split(",").map(function(author) {
+					return ZU.cleanAuthor(author, "author", false);
+				});
 			}
-			});
-		}
-
-		// Add the full text attachment
-		var extension = ZU.xpathText(
-			doc, '//td[contains(./font/text(), "Extension")]/following-sibling::td');
-		var downloadUrl = "/get/" + md5Hash + "/" + md5Hash + "." + extension;
-		item.attachments.push({
-		title: "Full Text",
-		url: downloadUrl,
-		mimeType: MIME_TYPES[extension]});
-		item.complete();
-	});
-	translator.translate();
+			// It also messes up multiple ISBNs, so we just pick the first one
+			if (item.ISBN) {
+				[' ', ','].forEach(function(splitChar) {
+					if (item.ISBN.indexOf(splitChar) != -1) {
+						item.ISBN = item.ISBN.split(splitChar)[0];
+					}
+				});
+			}
+			// Add the full text attachment
+			/* NOTE: For now this is commented out, pending a decision on how to
+					 deal with possibly huge downloads, see issue #1056 for details.
+			var extension = ZU.xpathText(
+				doc, '//td[contains(./font/text(), "Extension")]/following-sibling::td');
+			var downloadUrl = "/get/" + md5Hash + "/" + md5Hash + "." + extension;
+			item.attachments.push({
+				title: "Full Text",
+				url: downloadUrl,
+				mimeType: MIME_TYPES[extension]});
+			*/
+			item.complete();
+		});
+		translator.translate();
 	});
 }
+
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
@@ -131,12 +132,7 @@ var testCases = [
 				"libraryCatalog": "Library Genesis",
 				"publisher": "The University of Georgia Press",
 				"url": "http://gen.lib.rus.ec/book/index.php?md5=3f3ae2e1c6293a4550904e2587ff1441",
-				"attachments": [
-					{
-						"title": "Full Text",
-						"mimeType": "application/pdf"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -164,12 +160,7 @@ var testCases = [
 				"publisher": "Holt Paperbacks",
 				"shortTitle": "Mason & Dixon",
 				"url": "http://gen.lib.rus.ec/book/index.php?md5=8AE55C8664341707B748EADE479C02C1",
-				"attachments": [
-					{
-						"title": "Full Text",
-						"mimeType": "application/epub+zip"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -211,12 +202,7 @@ var testCases = [
 				"libraryCatalog": "Library Genesis",
 				"publisher": "Plume",
 				"url": "http://gen.lib.rus.ec/book/index.php?md5=16C402F4F9B737EA33C4EA5D938331A0",
-				"attachments": [
-					{
-						"title": "Full Text",
-						"mimeType": "application/pdf"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -252,12 +238,7 @@ var testCases = [
 				"libraryCatalog": "Library Genesis",
 				"publisher": "MIT Press",
 				"url": "http://gen.lib.rus.ec/book/index.php?md5=a05be4942325aea362e2aff8c305b0de",
-				"attachments": [
-					{
-						"title": "Full Text",
-						"mimeType": "application/pdf"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
