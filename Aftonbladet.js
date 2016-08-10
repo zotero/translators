28820a45e0340f4babcdf0dd75536ed28994937a
Jonatan Svensson Glad <josve05a@users.noreply.github.com>
2016-08-10 17:50:26 +0200

    Commenting out changes which breaks for normal articles

--- Aftonbladet.js
+++ Aftonbladet.js
@@ -91,8 +91,8 @@ function scrape(doc) {
 		/* TODO: This following line needs to be fixed. The replace's causes erroor on e.g. http://tv.aftonbladet.se/abtv/articles/125960 */
 		newArticle.section = ZU.xpathText(doc, '//div[@class="abBreadcrumbs clearfix"]/span[@class="abLeft"]').replace("Startsidan\n/", "").replace("\n/", " /").replace(" / ", "/").replace("Nyheter/", "").replace("Nyheter", "");
 		/* Following lines are for tv.aftonbladet.se */
-		newArticle.date = ZU.xpathText(doc, '//div[@class="channel-info-metadata abLabelThin"]/span/text()');
-		newArticle.abstractNote = ZU.xpathText(doc, '//div[@class="expandable-info-description"]/text() ');`
+//		newArticle.date = ZU.xpathText(doc, '//div[@class="channel-info-metadata abLabelThin"]/span/text()');
+//		newArticle.abstractNote = ZU.xpathText(doc, '//div[@class="expandable-info-description"]/text() ');`
 	newArticle.complete();
 }
 
