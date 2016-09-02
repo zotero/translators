8a3d5147b171c9a3c49f1338235756156cf4f1b2
febrezo <felixbrezo@gmail.com>
2016-09-02 22:01:58 +0200

    Add Justpaste.it translator.

---
+++ Justpaste.it.js
@@ -0,0 +1,125 @@
+{
+	"translatorID": "1d1a51d4-60bf-44b8-ae28-8b154d1ed721",
+	"label": "Justpaste.it",
+	"creator": "febrezo",
+	"target": "https?://justpaste.it",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 1,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2016-09-02 20:00:29"
+}
+
+/*
+	Justpaste.it Translator
+	Copyright (C) 2016 Félix Brezo, felixbrezo@gmail.com
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the Affero GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the Affero GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+function detectWeb(doc, url) {
+	return "webpage";
+}
+
+// More item types available at: <http://gsl-nagoya-u.net/http/pub/csl-fields/index.html>
+// Sample report found at: https://api.zotero.org/items/new?itemType=webpage
+/* 
+	{
+		"itemType": "webpage",
+		"title": "",
+		"creators": [
+			{
+				"creatorType": "author",
+				"firstName": "",
+				"lastName": ""
+			}
+		],
+		"abstractNote": "",
+		"websiteTitle": "",
+		"websiteType": "",
+		"date": "",
+		"shortTitle": "",
+		"url": "",
+		"accessDate": "",
+		"language": "",
+		"rights": "",
+		"extra": "",
+		"tags": [],
+		"collections": [],
+		"relations": {}
+	}
+*/
+
+function doWeb(doc, url) {
+	resourceType = detectWeb(doc, url);
+
+	// Creating the item
+	var newItem = new Zotero.Item(resourceType);
+
+	//Setting common data:
+	newItem.websiteTitle = "Justpaste.it";
+	newItem.url = url;
+	var auxTitle = ZU.xpathText(doc, '//title');	
+	newItem.title = auxTitle.substring(0, auxTitle.lastIndexOf(" - justpaste.it"));
+	newItem.websiteType = "paste";
+
+
+	var auxDate = ZU.xpathText(doc, '//div[@class="col-md-3 col-xs-4 noteBottomFooter"]');
+	if (auxDate.indexOf("ago")>=0) {
+		var today = new Date(Date.now());
+		newItem.date = today.getFullYear() + "/" + today.getMonth() + "/" + today.getDay() ;		
+	} else {
+		newItem.date = auxDate.substring(9);		
+	}
+
+	// Adding the attachment
+	newItem.attachments.push({
+		//title: newItem.title,
+		title: newItem.title,		
+		mimeType: "text/html",
+		url: url
+	});	
+	
+	newItem.complete();
+}/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://justpaste.it/xyuj",
+		"items": [
+			{
+				"itemType": "webpage",
+				"title": "This is the first line...",
+				"creators": [],
+				"date": "2016/8/5",
+				"url": "https://justpaste.it/xyuj",
+				"websiteTitle": "Justpaste.it",
+				"websiteType": "paste",
+				"attachments": [
+					{
+						"title": "This is the first line...",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
