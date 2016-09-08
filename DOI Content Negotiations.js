3c91d53fef49b41748f321c2e6480e36f8070b71
adam3smith <karcher@u.northwestern.edu>
2016-09-07 23:40:18 -0400

    Some cleanup
    This now works decently well

--- DOI Content Negotiations.js
+++ DOI Content Negotiations.js
@@ -1,9 +1,9 @@
 {
 	"translatorID": "b28d0d42-8549-4c6d-83fc-8382874a5cb9",
 	"label": "DOI Content Negotiation",
-	"creator": "Aurimas Vinckevicius",
+	"creator": "Sebastian Karcher",
 	"target": "",
-	"minVersion": "3.0",
+	"minVersion": "4.0.29.11",
 	"maxVersion": "",
 	"priority": 80,
 	"inRepository": true,
@@ -11,6 +11,29 @@
 	"browserSupport": "gcs",
 	"lastUpdated": "2014-05-29 07:46:16"
 }
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2016 Philipp Zumstein
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
 
 function detectSearch(items) {
 	if(!items) return false;
@@ -47,26 +70,38 @@ function filterQuery(items) {
 function doSearch(items) {
 	var dois = filterQuery(items);
 	if(!dois.length) return;
-
 	processDOIs(dois);
 }
 
 var fixItemType = {
-"journal-article": "article-journal",
-"report-series": "report",
-"misc": "article-journal"
+	"journal-article": "article-journal", //http://doi.org/10.1136%2Fbmj.2.5914.335-a and crossref generally
+	"book-chapter": "chapter", //http://doi.org/10.1017/CBO9781316216453.176
+	"reference-entry": "chapter", //http://doi.org/10.1002/14651858.CD002966.pub3
+	"report-series": "report",
+	"misc": "article-journal" //DataCite
 }
 
 function fixJSON(text) {
 	try {
 		var item = JSON.parse(text);
-		Z.debug("CSL_JSON" + text)
+		//Z.debug("CSL_JSON" + text)
+		Z.debug(item)
 		if(fixItemType[item.type]) item.type = fixItemType[item.type];
-		Z.debug(item);
-		Z.debug(item.type);
+		//	Z.debug(item.container-title)
+		if (item.type == "report") {
+			Z.debug("here")
+			Z.debug(item['container-title'])
+			item.genre = item['container-title']
+		}
+		if (item.type != "article-journal" && item.type != "paper-conference") {
+			item.note = "DOI: " + item.DOI;
+		}
+		//Sometimes date is in created, not issued: 10.1017/CCOL0521858429.016
+		if (!item.issued.length && item.created) {
+			item.issued = item.created;
+		}
 		if(item.issued && item.issued.raw) item.issued.literal = item.issued.raw;
 		if(item.accessed && item.accessed.raw) item.accessed.literal = item.accessed.raw;
-
 		return JSON.stringify([item]);
 	} catch(e) {
 		return false;
@@ -80,13 +115,27 @@ function processDOIs(dois) {
 		if(!text) {
 			return;
 		}
-		Z.debug(text)
+		//Z.debug(text)
 		// use CSL JSON translator
 		var trans = Zotero.loadTranslator('import');
 		trans.setTranslator('bc03b4fe-436d-4a1f-ba59-de4d2d7a63f7');
 		trans.setString(text);
 		trans.setHandler('itemDone', function(obj, item) {
-			//item.libraryCatalog = "DOI.org";
+			//if (!item.libraryCatalog)
+			item.libraryCatalog = "DOI.org";
+			//check if there are potential issues with character encoding and try to fix it
+			//e.g. 10.1057/9780230391116.0016 (en dash in title is presented as escaped unicode)
+			for(var field in item) {
+				if(typeof item[field] != 'string') continue;
+				//check for control characters that should never be in strings from CrossRef
+				if(/[\u007F-\u009F]/.test(item[field])) {
+					item[field] = decodeURIComponent(escape(item[field]));
+				}
+			}
+			Z.debug(item)
+			if (item.itemType == "report") {
+				item.reportType = item.publicationTitle;
+			}
 			item.complete();
 		});
 		trans.translate();
