55a4e19a8190dd371256cf5d7fab2d58917c7b6c
Philipp Zumstein <zuphilip@gmail.com>
2016-09-08 23:03:59 +0200

    BusinessWeek.js --> Bloomberg.js
    The acquisition was in late 2009:
    https://en.wikipedia.org/wiki/Bloomberg_Businessweek#Bloomberg_L.P._acquisition

--- Bloomberg.js
+++ Bloomberg.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2016-09-08 07:04:46"
+	"lastUpdated": "2016-09-08 20:56:54"
 }
 
 /*
@@ -36,8 +36,10 @@
 */
 
 function detectWeb(doc, url) {
-	if (url.indexOf('/articles/')>-1) {
-		return "webpage";
+	if (url.indexOf('/articles/')>-1 || ZU.xpathText(doc, '//meta[@property="og:type"]/@content')=="article") {
+		return "newspaperArticle";
+	} else if (url.indexOf('/videos/')>-1) {
+		return "tvBroadcast";
 	} else if (getSearchResults(doc, true)) {
 		return "multiple";
 	}
@@ -46,7 +48,7 @@ function detectWeb(doc, url) {
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	var rows = ZU.xpath(doc, '//a[@data-tracker-label="headline"]');
+	var rows = ZU.xpath(doc, '//a[@data-resource-id or @data-tracker-label="headline"]');
 	for (var i=0; i<rows.length; i++) {
 		var href = rows[i].href;
 		var title = ZU.trimInternal(rows[i].textContent);
@@ -78,9 +80,11 @@ function doWeb(doc, url) {
 
 function scrape(doc, url) {
 	var translator = Zotero.loadTranslator('web');
+	var type = detectWeb(doc, url);
 	// Embedded Metadata
 	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
 	translator.getTranslatorObject(function(trans) {
+		trans.itemType = type;
 		trans.doWeb(doc, url);
 	});
 }/** BEGIN TEST CASES **/
@@ -95,7 +99,7 @@ var testCases = [
 		"url": "http://www.bloomberg.com/news/articles/2012-01-04/bank-earnings-increase-57-in-analyst-forecasts-which-proved-wrong-in-2011",
 		"items": [
 			{
-				"itemType": "webpage",
+				"itemType": "newspaperArticle",
 				"title": "Bank Earnings Jump 57% in Analyst Forecasts Proved Wrong in 2011",
 				"creators": [
 					{
@@ -110,8 +114,9 @@ var testCases = [
 					}
 				],
 				"abstractNote": "Analysts’ failure to foresee declining earnings per share for the biggest U.S. banks last year hasn’t stopped them from predicting an even bigger profit surge for 2012.",
+				"libraryCatalog": "www.bloomberg.com",
+				"publicationTitle": "Bloomberg.com",
 				"url": "http://www.bloomberg.com/news/articles/2012-01-04/bank-earnings-increase-57-in-analyst-forecasts-which-proved-wrong-in-2011",
-				"websiteTitle": "Bloomberg.com",
 				"attachments": [
 					{
 						"title": "Snapshot"
@@ -136,14 +141,15 @@ var testCases = [
 		"url": "https://www.bloomberg.com/view/articles/2012-01-05/four-economists-come-together-to-say-we-agree-business-class",
 		"items": [
 			{
-				"itemType": "webpage",
+				"itemType": "newspaperArticle",
 				"title": "Four Economists Come Together to Say ‘We Agree’: Business Class",
 				"creators": [],
 				"date": "2012-01-05T00:01:34.000Z",
 				"abstractNote": "Jan. 5 (Bloomberg) -- “If you laid all the economists in",
+				"libraryCatalog": "www.bloomberg.com",
+				"publicationTitle": "Bloomberg View",
 				"shortTitle": "Four Economists Come Together to Say ‘We Agree’",
 				"url": "http://www.bloombergview.com/articles/2012-01-05/four-economists-come-together-to-say-we-agree-business-class",
-				"websiteTitle": "Bloomberg View",
 				"attachments": [
 					{
 						"title": "Snapshot"
@@ -164,6 +170,39 @@ var testCases = [
 				"seeAlso": []
 			}
 		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.bloomberg.com/news/videos/2016-09-08/how-to-capitalize-on-the-rotation-in-u-s-stocks",
+		"items": [
+			{
+				"itemType": "tvBroadcast",
+				"title": "How to Capitalize on the Rotation in U.S. Stocks",
+				"creators": [],
+				"abstractNote": "Tobias Levkovich, Citigroup's chief U.S. equity strategist, discusses the investment opportunities in U.S. stocks with Bloomberg's Vonnie Quinn and David Gura on \"Bloomberg Markets.\" (Source: Bloomberg)",
+				"libraryCatalog": "www.bloomberg.com",
+				"programTitle": "Bloomberg.com",
+				"url": "http://www.bloomberg.com/news/videos/2016-09-08/how-to-capitalize-on-the-rotation-in-u-s-stocks",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [
+					"Citigroup Inc",
+					"Markets",
+					"Money",
+					"Stocks"
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.bloomberg.com/businessweek",
+		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
