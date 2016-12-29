544d175d010f469ee6682100ae10bd34481deff3
JaretK <jaret.karnuta@gmail.com>
2016-12-29 02:24:12 -0500

    Update tests
    Test result update.

--- Access Medicine.js
+++ Access Medicine.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2016-12-29 07:14:02"
+	"lastUpdated": "2016-12-29 07:23:32"
 }
 
 /*
@@ -161,3 +161,137 @@ function risTranslate(doc, link, bookSection){
 		translator.translate();
 	});
 }
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://accesssurgery.mhmedical.com/SearchResults.aspx?q=roux+en+y",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://accessemergencymedicine.mhmedical.com/content.aspx?sectionid=72121336&bookid=1345&Resultclick=2",
+		"items": [
+			{
+				"itemType": "bookSection",
+				"title": "Foreign Bodies",
+				"creators": [
+					{
+						"lastName": "Tenenbein",
+						"firstName": "Milton",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Schafermeyer",
+						"firstName": "Robert",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Tenenbein",
+						"firstName": "Milton",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Macias",
+						"firstName": "Charles G.",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Sharieff",
+						"firstName": "Ghazala Q.",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Yamamoto",
+						"firstName": "Loren G.",
+						"creatorType": "editor"
+					}
+				],
+				"date": "2015",
+				"abstractNote": "Endobronchial and esophageal foreign bodies are marked by three clinical stages—an initial symptomatic stage of coughing, choking, and discomfort; a stage of relative amelioration of symptoms; followed by a stage of complications due to obstruction and infection.The gold standard for the diagnosis of an endobronchial foreign body is bronchoscopy.An asymptomatic or mildly symptomatic child with a coin in the esophagus can be observed for 8 to 16 hours because the coin will spontaneously pass into the stomach in 25% to 30% of these children.A useful method for removal of most intranasal foreign bodies is a positive-pressure technique such as the parent's kiss or a bag-valve-mask resuscitator.A useful initial method for removal of most foreign bodies from the external auditory canal is irrigation. This technique requires very little patient cooperation.An immobile battery, for example, in the esophagus, nose or ear, requires emergent removal to prevent perforation at the site of impaction and subsequent infection.Two or more rare-earth magnets in the gastrointestinal tract or on both sides of the nasal septum require emergent removal because of the potential of erosion and perforation of the tissue between the two adherent magnets.",
+				"bookTitle": "Strange and Schafermeyer's Pediatric Emergency Medicine",
+				"edition": "4",
+				"libraryCatalog": "Access Medicine",
+				"numberOfVolumes": "Book, Section",
+				"place": "New York, NY",
+				"publisher": "McGraw-Hill Education",
+				"url": "http://mhmedical.com/content.aspx?aid=1105680244",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [],
+				"notes": [
+					{
+						"note": "Chapter 11"
+					}
+				],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://accessmedicine.mhmedical.com/content.aspx?bookid=1130&sectionid=63651344",
+		"items": [
+			{
+				"itemType": "bookSection",
+				"title": "The Practice of Medicine",
+				"creators": [
+					{
+						"lastName": "Kasper",
+						"firstName": "Dennis",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Fauci",
+						"firstName": "Anthony",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Hauser",
+						"firstName": "Stephen",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Longo",
+						"firstName": "Dan",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Jameson",
+						"firstName": "J. Larry",
+						"creatorType": "editor"
+					},
+					{
+						"lastName": "Loscalzo",
+						"firstName": "Joseph",
+						"creatorType": "editor"
+					}
+				],
+				"date": "2015",
+				"bookTitle": "Harrison's Principles of Internal Medicine",
+				"edition": "19",
+				"libraryCatalog": "Access Medicine",
+				"numberOfVolumes": "Book, Section",
+				"place": "New York, NY",
+				"publisher": "McGraw-Hill Education",
+				"url": "http://mhmedical.com/content.aspx?aid=1120785046",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [],
+				"notes": [
+					{
+						"note": "Chapter 1"
+					}
+				],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
