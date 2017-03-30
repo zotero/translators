5c011b1d44e7a8684fc1cadcf79ce98a6caec597
adam3smith <karcher@u.northwestern.edu>
2017-03-30 00:31:07 -0400

    Fix RSC; Add book title option to EM

--- Embedded Metadata.js
+++ Embedded Metadata.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2016-11-07 05:17:35"
+	"lastUpdated": "2017-03-30 04:24:04"
 }
 
 /*
@@ -44,6 +44,7 @@ var HIGHWIRE_MAPPINGS = {
 	"citation_date":"date",
 	"citation_journal_title":"publicationTitle",
 	"citation_journal_abbrev":"journalAbbreviation",
+	"citation_inbook_title": "bookTitle", //used on RSC, e.g. http://pubs.rsc.org/en/content/chapter/bk9781849730518-00330/978-1-84973-051-8
 	"citation_book_title":"bookTitle",
 	"citation_volume":"volume",
 	"citation_issue":"issue",
