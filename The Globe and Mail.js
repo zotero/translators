888e939ef7cfe3ea3fb146814fa8f396d57eaf15
sonali0901 <sonaligpt0@gmail.com>
2017-04-14 13:56:40 +0530

    Rewrite, site updated

--- The Globe and Mail.js
+++ The Globe and Mail.js
@@ -8,216 +8,134 @@
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "g",
-	"lastUpdated": "2015-06-02 21:28:35"
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2017-04-14 08:24:05"
 }
 
 function detectWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
-	
-	var metaTags = new Object();
-	var metaTagHTML = doc.getElementsByTagName("meta");
-	for (var i = 0 ; i < metaTagHTML.length ; i++) {
-		metaTags[metaTagHTML[i].getAttribute("name")] = Zotero.Utilities.cleanTags(metaTagHTML[i].getAttribute("content"));
-		
-	}
-		
-	if (doc.evaluate('//div[@id="header"]/h2/a/img', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {	
-		var printEdition1 = doc.evaluate('//div[@id="header"]/h2/a/img', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().src;
-		if (printEdition1.match("printedition")) {
-			return "newspaperArticle";
-		}
-	}
-		
-	if (doc.evaluate('//p[@id="continueReading"]/strong', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-		var payPerView = doc.evaluate('//p[@id="continueReading"]/strong', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-		if (payPerView == "purchase this article") {
-			return "newspaperArticle";
-		}
-	}
-	
-	if (metaTags["article_id"]) {
-		return "newspaperArticle";
-		
-	} else if (doc.title.match('globeandmail.com: Search')) {
+	if (url.indexOf("/search/") != -1 && getSearchResults(doc, true)) {
 		return "multiple";
-	} 
-	
-	if (doc.evaluate('//ul[@id="utility"]/li[@class="email"]/a', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-		var blogCheck = doc.evaluate('//ul[@id="utility"]/li[@class="email"]/a', doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var blogCheck1 = blogCheck.iterateNext().textContent;
-		if (blogCheck1.match("blog")) { 
-			if (doc.location.href.match("story")) {
-			return "blogPost";
-			}
-		}
+	}
+	else if (ZU.xpathText(doc, '//article')) {
+		return "newspaperArticle";
 	}
 }
 
-//Translator for the Globe and Mail newspaper: code by Adam Crymble 
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = ZU.xpath(doc, '//article/h3/a');
+	for (var i=0; i<rows.length; i++) {
+		var href = rows[i].href;
+		var title = ZU.trimInternal(rows[i].textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
 
-function associateMeta (newItem, metaTags, field, zoteroField) {
-	if (metaTags[field]) {
-		newItem[zoteroField] = metaTags[field];
+
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			if (!items) {
+				return true;
+			}
+			var articles = [];
+			for (var i in items) {
+				articles.push(i);
+			}
+			ZU.processDocuments(articles, scrape);
+		});
+	} else {
+		scrape(doc, url);
 	}
 }
 
 function scrape(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
 	
-	if (detectWeb(doc, url) == "blogPost") {
-		var newItem = new Zotero.Item("blogPost");
-		
-		var title = doc.evaluate('//div[@id="headline"]/h2', doc, nsResolver, XPathResult.ANY_TYPE, null);
-		newItem.title = title.iterateNext().textContent;
-		
-		var blogger = doc.evaluate('//div[@id="author"]/p', doc, nsResolver, XPathResult.ANY_TYPE, null); 
-		var bloggerName = blogger.iterateNext().textContent.replace(/^\s*|\s*$/g, '');
-		var bloggerName1 = bloggerName.split(",");
-		newItem.creators.push(Zotero.Utilities.cleanAuthor(bloggerName1[0], "author"));
-	}
-	var noMetaType = 0;
-	
-	if (detectWeb(doc, url) == "newspaperArticle") {
-		var newItem = new Zotero.Item("newspaperArticle");
-		
-	//checks if the article is from the "Print Edition" which doesn't contain meta data.	
-		if (doc.evaluate('//div[@id="header"]/h2/a/img', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {	
-			var printEdition1 = doc.evaluate('//div[@id="header"]/h2/a/img', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().src;
-			if (printEdition1.match("printedition")) {
-				noMetaType = 1;
-				if (doc.evaluate('//div[@id="author"]/p[@class="article-date"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-					newItem.date= doc.evaluate('//div[@id="author"]/p[@class="article-date"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-				}
-			}
-		}
-		
-	//checks if the article is a Pay per view article.	
-		if (doc.evaluate('//p[@id="continueReading"]/strong', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-			var payPerView = doc.evaluate('//p[@id="continueReading"]/strong', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-			if (payPerView == "purchase this article") {
-				noMetaType = 1;
-			}
-		}
-		
-	//format for the "Print Edition" and "Pay per view" articles	
-		if (noMetaType = 1) {
-			noMetaType = 1;
-				if (doc.evaluate('//div[@id="headline"]/h2', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-					newItem.title = doc.evaluate('//div[@id="headline"]/h2', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-				}
-								
-				if (doc.evaluate('//div[@id="author"]/p[@class="byline"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext()) {
-					var author = doc.evaluate('//div[@id="author"]/p[@class="byline"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-				}
-				noMetaType = 0;
-		}
-		
-		var metaTags = new Object();
-		var tagsContent = new Array();
-		
-	//get data
-		var metaTagHTML = doc.getElementsByTagName("meta");
-		for (var i = 0 ; i < metaTagHTML.length ; i++) {
-			metaTags[metaTagHTML[i].getAttribute("name")] = Zotero.Utilities.cleanTags(metaTagHTML[i].getAttribute("content"));
-		}
-		
-	//author	
-		if (metaTags["byline"]) {
-			var author = metaTags["byline"];
-		}
-		
-	//date	
-		if (metaTags["pubdate"]) {
-			var month = metaTags["pubdate"].substr(4, 2);
-			var day = metaTags["pubdate"].substr(6, 2);
-			var year = metaTags["pubdate"].substr(0, 4);
-			
-			newItem.date = (year + "-" + month + "-"+ day);
-		}
-	
-	//tags	
-		if (metaTags["article_keywords"]) {
-			tagsContent = metaTags["article_keywords"].split("; ");
-		}
-		
-		for (var i = 0; i < tagsContent.length; i++) {
-		     	if (tagsContent[i] != (" ") && tagsContent[i] != ("")) {
-			     	newItem.tags[i] = tagsContent[i];
-		     	}
-	     	}	
-		
-		associateMeta (newItem, metaTags, "headline", "title");
-		associateMeta (newItem, metaTags, "summary", "abstractNote");
-		associateMeta (newItem, metaTags, "desk", "section");	
-		associateMeta (newItem, metaTags, "article_id", "callNumber");
-		associateMeta (newItem, metaTags, "credit", "rights");
-	
-	//rest of author (shared between both newspaperArticle types)
-		if (author) {
-			
-		
-			if (author.substr(0,3).toLowerCase() == "by ") {
-				author= author.substr(3);
-			}
-		
-			var authors = author.toLowerCase().split(" and ");
-			for (var i=0; i<authors.length; i++) {
-				var author = authors[i];
-				var words = author.split(" ");
-				
-				for (var j=0; j<words.length; j++) {
-					if (words[j] != "") {
-						words[j] = words[j][0].toUpperCase() + words[j].substr(1).toLowerCase();
-					}
-				}
-				author = words.join(" ");
-				newItem.creators.push(Zotero.Utilities.cleanAuthor(author, "author"));			
-			}
-		}
-	}	
-	
-	newItem.url = doc.location.href;
-	newItem.complete();
-	
-}
+	newItem = new Zotero.Item("newspaperArticle");
+	newItem.url = url;
 
-function doWeb(doc, url) {
-	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
-		if (prefix == 'x') return namespace; else return null;
-	} : null;
+	//get headline
+	var title = ZU.xpathText(doc, '//article/h1');
+	if (!title) title = ZU.xpathText(doc, '//meta[@property="og:title"]/@content');
+	newItem.title = title;
+
+	//get abstract
+	newItem.abstractNote = ZU.xpathText(doc, '//meta[@property="og:description"]/@content');
 	
-	var articles = new Array();
-	
-	if (detectWeb(doc, url) == "multiple") {
-		var items = new Object();
-		var next_title = new Array();
-		
-		var titlesCount = doc.evaluate('count (//h3[@class="storyLink"]/a)', doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var titles = doc.evaluate('//h3[@class="storyLink"]/a', doc, nsResolver, XPathResult.ANY_TYPE, null);		
-		
-		for (i=0; i < titlesCount.numberValue; i++) {
-			next_title = titles.iterateNext();
-			
-			if (next_title.href.match("story")) {
-				items[next_title.href] = next_title.textContent;
-			}
-		}	
-		
-		items = Zotero.selectItems(items);
-		for (var i in items) {
-			articles.push(i);
-		}
-	} else {
-		articles = [url];
+	//get date
+	var xpathdate_author = '//time[@itemprop="datePublished"]';
+	var date = ZU.xpathText(doc, xpathdate_author);
+	if (date) {
+		newItem.date = ZU.strToISO(date);
 	}
-	Zotero.Utilities.processDocuments(articles, scrape, function() {Zotero.done();});
-	Zotero.wait();
-}
+
+	//get author or organization
+	var authors = ZU.xpath(doc, '//meta[@itemprop="author"]/@content');
+	for (var i in authors){
+		newItem.creators.push(ZU.cleanAuthor(authors[i].textContent, "author"));
+	}
+	if(!authors.length){//need to change
+		authors = ZU.xpathText(doc, xpathdate_author);
+		if(authors){
+			authors_org=authors.substring(0,authors.lastIndexOf("|")-1);
+			var regex = /(.*By\s+)(.*)/;
+			authors = authors_org.replace(regex, "$2");
+			newItem.creators.push({lastName:authors, creatorType: "author", fieldMode: 1})
+		}
+	}
+	
+	newItem.language = ZU.xpathText(doc, '//meta[@http-equiv="Content-Language"]/@content');
+	
+	newItem.section = ZU.xpathText(doc, '//meta[@name="article:type"]/@content');
+	
+	newItem.attachments = ({
+		url: url,
+		title: "The Globe and Mail Snapshot",
+		mimeType: "text/html"
+	});
+	newItem.complete();
+}/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://www.theglobeandmail.com/news/national/sharp-differences-in-career-paths-of-phd-grads-across-fields-ubc-study-finds/article34694435/?reqid=443820d7-b187-4ede-a28a-2d075ac4bd80",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"title": "Sharp differences in career paths of PhD grads across fields, UBC study finds",
+				"creators": [
+					{
+						"firstName": "SIMONA",
+						"lastName": "CHIOSE",
+						"creatorType": "author"
+					}
+				],
+				"date": "2017-04-13",
+				"abstractNote": "Multiple studies over the past two years have found that a minority of PhDs become professors",
+				"language": "en-ca",
+				"libraryCatalog": "The Globe and Mail",
+				"section": "news",
+				"url": "http://www.theglobeandmail.com/news/national/sharp-differences-in-career-paths-of-phd-grads-across-fields-ubc-study-finds/article34694435/?reqid=443820d7-b187-4ede-a28a-2d075ac4bd80",
+				"attachments": {
+					"url": "http://www.theglobeandmail.com/news/national/sharp-differences-in-career-paths-of-phd-grads-across-fields-ubc-study-finds/article34694435/?reqid=443820d7-b187-4ede-a28a-2d075ac4bd80",
+					"title": "The Globe and Mail Snapshot",
+					"mimeType": "text/html"
+				},
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.theglobeandmail.com/search/?q=nuclear",
+		"items": "multiple"
+	}
+]
+/** END TEST CASES **/
