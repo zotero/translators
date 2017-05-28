7a603ae1ae57a93398c2c0efdf4748ea75987f5f
Philipp Zumstein <zuphilip@users.noreply.github.com>
2017-05-29 01:21:53 +0200

    Rewrite NYTimes completely, #wikicite (#1305)
    * [NYTimes.com.js] Fix multiple authors, #wikicite
    * [NYTimes.com.js] Rename PDF attachment
    * Add new export option splitTags in EM for NYTimes

--- NYTimes.com.js
+++ NYTimes.com.js
@@ -1,224 +1,136 @@
 {
 	"translatorID": "ce7a3727-d184-407f-ac12-52837f3361ff",
 	"label": "NYTimes.com",
-	"creator": "Simon Kornblith",
+	"creator": "Philipp Zumstein",
 	"target": "^https?://(query\\.nytimes\\.com/(search|gst)/(alternate/)?|(select\\.|www\\.|\\.blogs\\.)?nytimes\\.com/)",
-	"minVersion": "2.1.9",
+	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
-	"browserSupport": "gcsv",
-	"lastUpdated": "2017-05-23 21:27:51"
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2017-05-27 10:52:46"
 }
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2017 Philipp Zumstein
+	
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
 
 function detectWeb(doc, url) {
-	// Check for search results
-	var searchResults = doc.evaluate('//div[@id="search_results"] |//div[@id="searchResults"] |//div[@id="srchContent"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
-	if (searchResults) return "multiple";
-
-	// Check for article meta tags
-	var metaTags = doc.getElementsByTagName("meta");
-	var haveHdl = false;
-	var haveByl = false;
-	var blogPost = false;
-	for (var i in metaTags) {
-		if (metaTags[i].name === "hdl") {
-			haveHdl = true;
-		} else if (metaTags[i].name == "byl") {
-			haveByl = true;
-		}
-		else if (metaTags[i].name == "PST") {
-			blogPost = true;
-		}
-		if (haveHdl && haveByl) return "newspaperArticle";
-		else if (haveByl && blogPost) return "blogPost"
+	if (getSearchResults(doc, true)) {
+		return "multiple";
 	}
-	return false;
-}
-
-function associateMeta(newItem, metaTags, field, zoteroField) {
-	if (metaTags[field]) {
-		newItem[zoteroField] = metaTags[field];
+	if (ZU.xpathText(doc, '//meta[@property="og:type" and @content="article"]/@content')) {
+		if (url.indexOf('blog')>-1) {
+			return "blogPost";
+		} else {
+			return "newspaperArticle";
+		}
 	}
 }
+
 
 function scrape(doc, url) {
-
+	var type = detectWeb(doc, url);
+	var translator = Zotero.loadTranslator('web');
+	// Embedded Metadata
+	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
+	//translator.setDocument(doc);
 	
-	var newItem = new Zotero.Item("newspaperArticle");
-	newItem.publicationTitle = "The New York Times";
-	newItem.ISSN = "0362-4331";
-	newItem.language = "en-US";
-	var metaTags = new Object();
-	var metaTagsProperty = new Object();
-	if (url != undefined) {
-		if(url.indexOf("blogs.nytimes.com")!= -1) newItem.itemType="blogPost";
-		newItem.url = url;
-		var metaTagRe = /<meta[^>]*>/gi;
-		var nameRe = /name="([^"]+)"/i;
-		var propertyRe = /property="([^"]+)"/i;
-		var contentRe = /content="([^"]+)"/i;
-		var m = doc.match(metaTagRe);
-
-		if (!m) {
-			return;
+	translator.setHandler('itemDone', function (obj, item) {
+		item.language = ZU.xpathText(doc, '//meta[@itemprop="inLanguage"]/@content') || "en-US";
+		if (item.date) {
+			item.date = ZU.strToISO(item.date);
 		}
-
-		for (var i = 0; i < m.length; i++) {
-			var name = nameRe.exec(m[i]);
-			var content = contentRe.exec(m[i]);
-			var property = propertyRe.exec(m[i]);
-			var content = contentRe.exec(m[i]);
-			if (name && content) {
-				metaTags[name[1]] = content[1];
-			}
-			if (property && content) {	
-				metaTagsProperty[property[1]] = content[1];
-			}
-		}
-	/*	if (!metaTags["hdl"]) {
-			return;
-		}*/
-		// We want to get everything on one page
-		newItem.attachments.push({
-			url: url.replace(/\.html\??([^/]*)(pagewanted=[^&]*)?([^/]*)$/, ".html?pagewanted=all&$1$2"),
-			title: "New York Times Snapshot",
-			mimeType: "text/html"
-		});
-	} else {
-		var type = detectWeb(doc, url);
-		newItem.itemType = type;
-		newItem.url = doc.location.href;
-		var metaTagHTML = doc.getElementsByTagName("meta");
-		for (var i = 0; i < metaTagHTML.length; i++) {
-			var key = metaTagHTML[i].getAttribute("name");
-			var value = metaTagHTML[i].getAttribute("content");
-			var prop = metaTagHTML[i].getAttribute("property");
-			if (key && value) {
-				metaTags[key] = value;
-			}
-			if (prop && value) {
-				metaTagsProperty[prop] = value;
-			}
-			
-		}
-		// Get everything on one page is possible
-		var singlePage = false;
-		if (!newItem.url.match(/\?pagewanted=all/) && (singlePage = doc.evaluate('//ul[@id="toolsList"]/li[@class="singlePage"]/a', doc, null, XPathResult.ANY_TYPE, null).iterateNext())) {
-			newItem.attachments.push({
-				url: singlePage.href,
-				title: "New York Times Snapshot",
-				mimeType: "text/html"
-			});
+		if (item.itemType == "blogPost") {
+			item.blogTitle = ZU.xpathText(doc, '//meta[@property="og:site_name"]/@content');
 		} else {
-			newItem.attachments.push({
-				document: doc,
-				title: "New York Times Snapshot"
+			item.publicationTitle = "The New York Times";
+			item.ISSN = "0362-4331";
+		}
+		//Multiple authors are just put into the same Metadata field
+		//we have to split this here
+		var authors = ZU.xpathText(doc, '//meta[@name="author"]/@content');
+		if (authors) {
+			item.creators = [];
+			var authorsList = authors.split(/,|\band\b/);
+			for (var i=0; i<authorsList.length; i++) {
+				item.creators.push(ZU.cleanAuthor(authorsList[i], "author"));
+			}
+		}
+		item.url = ZU.xpathText(doc, '//link[@rel="canonical"]/@href') || url;
+		var pdfurl = ZU.xpathText(doc, '//div[@id="articleAccess"]//span[@class="downloadPDF"]/a[contains(@href, "/pdf")]/@href');
+		if (pdfurl) {
+			item.attachments.push({
+				url: pdfurl,
+				title: "NYTimes Archive PDF"
 			});
 		}
-
-	}
-
-	associateMeta(newItem, metaTags, "dat", "date");
-	associateMeta(newItem, metaTags, "hdl", "title");
-	associateMeta(newItem, metaTags, "description", "abstractNote");
-	if (type === "blogPost"){
-		associateMeta(newItem, metaTagsProperty, "og:title", "title");
-		associateMeta(newItem, metaTagsProperty, "og:site_name", "blogTitle");
-	}
-	associateMeta(newItem, metaTags, "dsk", "section");
-	associateMeta(newItem, metaTags, "articleid", "accessionNumber");
+		item.complete();
+	});
 	
-	if (newItem.blogTitle) newItem.blogTitle = "The New York Times - " + newItem.blogTitle;
-	
-	if (metaTags["pdate"]) {
-		newItem.date = metaTags["pdate"].replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
-	}
-
-	if (metaTags["byl"]) {
-		var author = Zotero.Utilities.trimInternal(metaTags["byl"]);
-		if (author.substr(0, 3).toLowerCase() == "by ") {
-			author = author.substr(3);
-		}
-
-		var authors = author.split(" and ");
-		for (var i=0; i<authors.length; i++) {
-			var author = authors[i];
-			// fix capitalization
-			var words = author.split(" ");
-			for (var j=0; j<words.length; j++) {
-				words[j] = words[j][0].toUpperCase() + words[j].substr(1).toLowerCase();
-			}
-			author = words.join(" ");
-
-			if (words[0] == "The") {
-				newItem.creators.push({
-					lastName: author,
-					creatorType: "author",
-					fieldMode: true
-				});
-			} else {
-				newItem.creators.push(Zotero.Utilities.cleanAuthor(author, "author"));
-			}
-		}
-	}
-
-	if (metaTags["keywords"]) {
-		var keywords = metaTags["keywords"];
-		newItem.tags = keywords.split(",");
-		for (var i in newItem.tags) {
-			newItem.tags[i] = newItem.tags[i].replace("  ", ", ");
-		}
-	}
-	
-	
-	// Remove everything after .html from the URL - we want the canonical version
-	//but not for historical abstracts, where it's needed
-	if (!newItem.url.match(/abstract\.html/)) {
-		newItem.url = newItem.url.replace(/\?.+/, '');
-	}
-	//	get pdf for archive articles - make sure we don't go here if we're getting multiples or regular items
-	var pdfxpath = '//div[@id="articleAccess"]//span[@class="downloadPDF"]/a[contains(@href, "/pdf")]/@href'
-	if (!m && ZU.xpathText(doc, pdfxpath) != null) {
-		var pdflink = ZU.xpathText(doc, pdfxpath)
-		Zotero.Utilities.doGet(pdflink, function (text) {
-			var realpdf = text.match(/http\:\/\/article\.archive\.nytimes.+\"/);
-			Z.debug("pdflink: " + realpdf)
-			if (realpdf) {
-				newItem.attachments.push({
-					url: realpdf[0].replace(/\"/, ""),
-					title: "NY Times Archive PDF",
-					mimeType: "application/pdf"
-				});
-			}
-		}, function () {
-			newItem.complete();
+	translator.getTranslatorObject(function(trans) {
+		trans.itemType = type;
+		trans.splitTags = false;
+		trans.addCustomFields({
+			'dat': 'date',
 		});
-	} else {
-		newItem.complete();
-	}
-
+		trans.doWeb(doc, url);
+	});
+	
 }
 
+
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = ZU.xpath(doc, '(//div[@id="search_results"]|//div[@id="searchResults"]|//div[@id="srchContent"])//li');
+	for (var i=0; i<rows.length; i++) {
+		var href = ZU.xpathText(rows[i], './/a/@href');
+		var title = ZU.trimInternal(rows[i].textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
+
+
 function doWeb(doc, url) {
-	var searchResults = doc.evaluate('//div[@id="search_results"] |//div[@id="searchResults"]| //div[@id="srchContent"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
-	if (searchResults) {
-		var items = Zotero.Utilities.getItemArray(doc, searchResults, '^https?://(?:select\.|www\.)nytimes.com/.*\.html(\\?|$)');
-
-		Zotero.selectItems(items, function (items) {
-			if (!items) return true;
-
-			var urls = [];
-			for (var i in items) urls.push(i);
-
-			Zotero.Utilities.HTTP.doGet(urls, function (text, response, url) {
-				scrape(text, url)
-			}, function () {
-			}, null);
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			if (!items) {
+				return true;
+			}
+			var articles = [];
+			for (var i in items) {
+				articles.push(i);
+			}
+			ZU.processDocuments(articles, scrape);
 		});
 	} else {
-		scrape(doc);
+		scrape(doc, url);
 	}
 }
 
@@ -231,27 +143,20 @@ var testCases = [
 			{
 				"itemType": "newspaperArticle",
 				"title": "TWO MONEY INQUIRIES.; Hearings of Trust Charges and Aldrich Plan at the Same Time.",
-				"creators": [
-					{
-						"firstName": "Special To The New York",
-						"lastName": "Times",
-						"creatorType": "author"
-					}
-				],
+				"creators": [],
 				"date": "1912-03-05",
 				"ISSN": "0362-4331",
 				"abstractNote": "WASHINGTON, March 4. -- The Money Trust inquiry and consideration of the proposed Aldrich monetary legislation will probably be handled side by side by the House Banking and Currency Committee. The present tentative plan is to divide the committee into two parts, one of which, acting as a sub-committee, will investigate as far as it can those allegations of the Henry Money Trust resolution which fall within the jurisdiction of the Banking and Currency Committee.",
 				"language": "en-US",
-				"libraryCatalog": "NYTimes.com",
+				"libraryCatalog": "query.nytimes.com",
 				"publicationTitle": "The New York Times",
-				"url": "http://query.nytimes.com/gst/abstract.html?res=9C07E4DC143CE633A25756C0A9659C946396D6CF&legacy=true",
+				"url": "http://query.nytimes.com/gst/abstract.html?res=9C07E4DC143CE633A25756C0A9659C946396D6CF",
 				"attachments": [
 					{
-						"title": "New York Times Snapshot"
+						"title": "Snapshot"
 					},
 					{
-						"title": "NY Times Archive PDF",
-						"mimeType": "application/pdf"
+						"title": "NYTimes Archive PDF"
 					}
 				],
 				"tags": [],
@@ -278,12 +183,13 @@ var testCases = [
 				"ISSN": "0362-4331",
 				"abstractNote": "The university has found Marc Hauser “solely responsible” for eight instances of scientific misconduct.",
 				"language": "en-US",
-				"libraryCatalog": "NYTimes.com",
+				"libraryCatalog": "www.nytimes.com",
 				"publicationTitle": "The New York Times",
-				"url": "http://www.nytimes.com/2010/08/21/education/21harvard.html",
+				"section": "Education",
+				"url": "https://www.nytimes.com/2010/08/21/education/21harvard.html",
 				"attachments": [
 					{
-						"title": "New York Times Snapshot"
+						"title": "Snapshot"
 					}
 				],
 				"tags": [
@@ -320,12 +226,12 @@ var testCases = [
 				],
 				"date": "2013-06-19",
 				"abstractNote": "At their core, are America’s problems primarily economic or moral?",
-				"blogTitle": "The New York Times",
+				"blogTitle": "Opinionator",
 				"language": "en-US",
 				"url": "https://opinionator.blogs.nytimes.com/2013/06/19/our-broken-social-contract/",
 				"attachments": [
 					{
-						"title": "New York Times Snapshot"
+						"title": "Snapshot"
 					}
 				],
 				"tags": [
@@ -335,6 +241,98 @@ var testCases = [
 					"Thomas B. Edsall",
 					"United States",
 					"United States Economy"
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.nytimes.com/2015/05/10/nyregion/manicurists-in-new-york-area-are-underpaid-and-unprotected.html?_r=0",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"title": "반짝이는 매니큐어에 숨겨진 네일 미용사들의 어두운 삶",
+				"creators": [
+					{
+						"firstName": "Sarah Maslin",
+						"lastName": "Nir",
+						"creatorType": "author"
+					}
+				],
+				"date": "2015-05-07",
+				"ISSN": "0362-4331",
+				"abstractNote": "뉴욕타임스는 취재 중 많은 네일숍 직원들이 부당한 대우와 인종차별 및 학대에 흔하게 시달리며 정부 노동자법률기구의 보호도 제대로 받지 못한다는 사실을 발견했다.",
+				"language": "ko-KR",
+				"libraryCatalog": "www.nytimes.com",
+				"publicationTitle": "The New York Times",
+				"section": "N.Y. / Region",
+				"url": "https://www.nytimes.com/2015/05/10/nyregion/manicurists-in-new-york-area-are-underpaid-and-unprotected.html",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [
+					"Beauty Salons",
+					"Discrimination",
+					"Korean-Americans",
+					"Labor and Jobs",
+					"New York City",
+					"Wages and Salaries"
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.nytimes.com/2017/05/24/us/politics/russia-trump-manafort-flynn.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=span-ab-top-region&region=top-news&WT.nav=top-news&_r=0",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"title": "Top Russian Officials Discussed How to Influence Trump Aides Last Summer",
+				"creators": [
+					{
+						"firstName": "Matthew",
+						"lastName": "Rosenberg",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Adam",
+						"lastName": "Goldman",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Matt",
+						"lastName": "Apuzzo",
+						"creatorType": "author"
+					}
+				],
+				"date": "2017-05-24",
+				"ISSN": "0362-4331",
+				"abstractNote": "American spies collected intelligence last summer revealing that Russians were debating how to work with Trump advisers, current and former officials say.",
+				"language": "en-US",
+				"libraryCatalog": "www.nytimes.com",
+				"publicationTitle": "The New York Times",
+				"section": "Politics",
+				"url": "https://www.nytimes.com/2017/05/24/us/politics/russia-trump-manafort-flynn.html",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [
+					"Cyberwarfare and Defense",
+					"Espionage and Intelligence Services",
+					"Flynn, Michael T",
+					"Manafort, Paul J",
+					"Presidential Election of 2016",
+					"Russia",
+					"Trump, Donald J",
+					"United States Politics and Government"
 				],
 				"notes": [],
 				"seeAlso": []
