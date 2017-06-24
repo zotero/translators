cce9c14ed5be800fe735c7a716e6135227951d8a
Philipp Zumstein <zuphilip@gmail.com>
2017-06-24 11:14:30 +0200

    Update SAE Papers.js
    This solves #1342.

--- SAE Papers.js
+++ SAE Papers.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-06-16 19:44:16"
+	"lastUpdated": "2017-06-24 09:09:38"
 }
 
 /*
@@ -79,6 +79,14 @@ function getSearchResults(doc, checkOnly) {
 }
 
 function scrape(doc, url) {
+	//the highwire meta tags are now part of the body and not head
+	//which we have to correct here before calling EM translator
+	var head = doc.getElementsByTagName('head');
+	var metasInBody = ZU.xpath(doc, '//body/meta');
+	for (var i=0; i<metasInBody.length; i++) {
+		head[0].append(metasInBody[i]);
+	}
+	
 	var abstract = ZU.xpathText(doc, '//div[@class="dt-scope" and div/div[contains(text(), "Abstract")]]//div[@class="dt-scope-c-content"]');
 	var translator = Zotero.loadTranslator('web');
 	// use the Embedded Metadata translator
@@ -222,10 +230,6 @@ var testCases = [
 				"reportType": "SAE Technical Paper",
 				"url": "http://papers.sae.org/540090/",
 				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
 					{
 						"title": "Snapshot"
 					}
