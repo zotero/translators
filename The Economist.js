f680dea5e70f1cddb38420ef38003414edc00950
Philipp Zumstein <zuphilip@gmail.com>
2017-06-30 20:11:30 +0200

    Update The Economist.js
    The translator should work now when the page is fully loaded.
    However, the website uses quite extensively some delayed loading
    of contents, such that test sometimes fail.

--- The Economist.js
+++ The Economist.js
@@ -9,15 +9,13 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2014-04-04 09:55:12"
+	"lastUpdated": "2017-06-30 18:09:56"
 }
 
 function detectWeb(doc, url) {
-	if (doc.location.href.indexOf("/search/") != -1) {
-		return "multiple";
-	} else if (ZU.xpathText(doc, '//h3[@class="headline"]')) {
+	if (ZU.xpathText(doc, '//h3[@class="headline"]')) {
 		return "magazineArticle";
-	} else if (ZU.xpathText(doc, '//div[@class="view-content"]//div[@class="article"]') || ZU.xpathText(doc, '//div[@id="column-content"]//section[contains(@class, "news-package")]')){
+	} else if (getSearchResults(doc, url, true)) {
 		return "multiple";
 	} 
 
@@ -61,42 +59,47 @@ function scrape(doc, url) {
 }
 
 
+function getSearchResults(doc, url, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows;
+	if (url.indexOf('/printedition/')>-1) {
+		rows = ZU.xpath(doc, '//li/a[contains(@class, "list__link")]');
+	} else if (url.indexOf('/search?')>-1) {
+		rows = ZU.xpath(doc, '//div[contains(@class, "gs-title")]/a[@class="gs-title" and not(contains(@href, "topics"))]');
+	} else {
+		rows = ZU.xpath(doc, '//article/a[contains(@class, "teaser__link")]');
+	}
+	for (var i=0; i<rows.length; i++) {
+		var href = rows[i].href;
+		var title = ZU.trimInternal(rows[i].textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
+
+
 function doWeb(doc, url) {
-
-	var urls = new Array();
-
 	if (detectWeb(doc, url) == "multiple") {
-
-		var articles = new Array();
-		var items = {};
-		//search results
-		var titles = doc.evaluate('//div[contains(@class, "gs-title")]/a[@class="gs-title" and not(contains(@href, "topics"))]', doc, null, XPathResult.ANY_TYPE, null);
-		//sections
-		if (!titles.iterateNext()){
-			var titles = doc.evaluate('//section[contains(@class, "news-package")]//article/a|//section[contains(@class, "news-package")]//ul/li/div/a[@class="headline"]', doc, null, XPathResult.ANY_TYPE, null);
-		}
-		//print ToC
-		if (!titles.iterateNext()){
-			var titles = doc.evaluate('//div[@class="article"]/a[@class="node-link"]', doc, null, XPathResult.ANY_TYPE, null);
-		}
-		
-		var title;
-		while (title = titles.iterateNext()) {
-			items[title.href] = title.textContent.trim();
-		}
-		Zotero.selectItems(items, function (items) {
+		Zotero.selectItems(getSearchResults(doc, url, false), function (items) {
 			if (!items) {
 				return true;
 			}
+			var articles = [];
 			for (var i in items) {
 				articles.push(i);
 			}
-			Zotero.Utilities.processDocuments(articles, scrape)
-		})
-	} else{
+			ZU.processDocuments(articles, scrape);
+		});
+	} else {
 		scrape(doc, url);
 	}
-}/** BEGIN TEST CASES **/
+}
+
+/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
@@ -104,23 +107,23 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "Dreams and realities",
 				"creators": [],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "Nov 12th 2011",
+				"ISSN": "0013-0613",
+				"abstractNote": "A battle over American-led free trade brews in Asia",
+				"libraryCatalog": "The Economist",
+				"publicationTitle": "The Economist",
+				"url": "http://www.economist.com/node/21538214",
 				"attachments": [
 					{
 						"title": "The Economist Snapshot",
 						"mimeType": "text/html"
 					}
 				],
-				"ISSN": "0013-0613",
-				"url": "http://www.economist.com/node/21538214",
-				"publicationTitle": "The Economist",
-				"title": "Dreams and realities",
-				"abstractNote": "A battle over American-led free trade brews in Asia",
-				"date": "Nov 12th 2011",
-				"libraryCatalog": "The Economist"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -132,6 +135,11 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "http://www.economist.com/world/united-states",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.economist.com/search?q=mannheim",
 		"items": "multiple"
 	}
 ]
