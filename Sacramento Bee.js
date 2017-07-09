727cf9f27a182a11b7b1697da304cd3334a1a1d1
Wolf <owcz@users.noreply.github.com>
2017-07-09 01:06:28 -0400

    misc fixes & fixes per review

--- Sacramento Bee.js
+++ Sacramento Bee.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-07-09 02:19:40"
+	"lastUpdated": "2017-07-09 05:05:18"
 }
 
 /*
@@ -40,30 +40,31 @@
 function attr(doc,selector,attr,index){if(index>0){var elem=doc.querySelectorAll(selector).item(index);return elem?elem.getAttribute(attr):null}var elem=doc.querySelector(selector);return elem?elem.getAttribute(attr):null}function text(doc,selector,index){if(index>0){var elem=doc.querySelectorAll(selector).item(index);return elem?elem.textContent:null}var elem=doc.querySelector(selector);return elem?elem.textContent:null}
 
 function detectWeb(doc, url) {
-	if (url.match(/article\d+/)) {
+	if (url.search(/article\d+/) != -1) {
 		return "newspaperArticle";
-	} else if (url.match(/\/(news|sports|entertainment)\//)) {
+	} else if (url.search(/(\/((news|sports|entertainment)\/)|(search\/\?q=))|sacbee\.com\/?$/) != -1) {
 		return "multiple";
-	} else if (url.indexOf("/search/?q=") != -1) {
-		return "multiple";
-	} else return null;
+	}
 }
 
 function scrape(doc, url) {
 	var item = new Zotero.Item("newspaperArticle");
-	item.websiteTitle = "The Sacramento Bee";
+	item.publicationTitle = "The Sacramento Bee";
 	item.language = "en-US";
 	item.ISSN = "0890-5738";
 	item.url = url;
 	item.title = attr(doc,'[property="og:title"]','content');
 	item.date = text(doc,'.published-date');
+	if (item.date) { // don't perform on empty string
+		item.date = ZU.strToISO(item.date);
+	}
 	item.abstractNote = text(doc,'#content-body- p');
 	var keywords = attr(doc,'meta[name="keywords"]','content');
 	if (keywords) { // so as not to perform a split when keyword string is null
 		item.tags = keywords.split(", ");
 	}
 	item.attachments.push({
-		title: "The Sacramento Bee snapshot",
+		title: "The Sacramento Bee Snapshot",
 		mimeType: "text/html",
 		document: doc
 	});
@@ -87,23 +88,33 @@ function scrape(doc, url) {
 	item.complete();
 }
 
-function doWeb(doc, url) { // news & search pages supported, Feature support to come
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('#site-search-results .title a, .media-body .title a, #story-list .title a, .col-sm-7 .title a');
+	for (var i=0; i<rows.length; i++) {
+		var href = rows[i].href;
+		var title = ZU.trimInternal(rows[i].textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
+
+function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
-		var items = {};
-		articles = [];
-		var titles = doc.querySelectorAll('#site-search-results .title a, .media-body .title a, #story-list .title a, .col-sm-7 .title a');
-		for (var i = 0; i < titles.length; i++) {
-			items[titles[i].href] = titles[i].textContent;
-		}
-		Zotero.selectItems(items, function(items) {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
 			if (!items) {
 				return true;
 			}
+			var articles = [];
 			for (var i in items) {
 				articles.push(i);
 			}
-			Zotero.Utilities.processDocuments(articles, scrape);
-		})
+			ZU.processDocuments(articles, scrape);
+		});
 	} else scrape(doc, url);
 }/** BEGIN TEST CASES **/
 var testCases = [
@@ -126,15 +137,16 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"date": "January 08, 2015 1:09 PM",
+				"date": "2015-01-08",
 				"ISSN": "0890-5738",
 				"abstractNote": "Until this week, the federal government officially considered Eric Taylor McDavid a threat to the nation, a radical eco-terrorist who plotted to bomb or torch the Nimbus Dam, a U.S. Forest Service lab and cellphone towers in the Sacramento region.",
 				"language": "en-US",
 				"libraryCatalog": "Sacramento Bee",
+				"publicationTitle": "The Sacramento Bee",
 				"url": "http://www.sacbee.com/news/local/crime/article5641188.html",
 				"attachments": [
 					{
-						"title": "The Sacramento Bee snapshot",
+						"title": "The Sacramento Bee Snapshot",
 						"mimeType": "text/html"
 					}
 				],
@@ -173,15 +185,16 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"date": "January 17, 2015 4:56 PM",
+				"date": "2015-01-17",
 				"ISSN": "0890-5738",
 				"abstractNote": "Eric McDavid was running late for his interview Wednesday, unavoidably delayed when a detective asked to chat with him after he stopped at the Placer County Sheriff’s Office to register as an arsonist.",
 				"language": "en-US",
 				"libraryCatalog": "Sacramento Bee",
+				"publicationTitle": "The Sacramento Bee",
 				"url": "http://www.sacbee.com/news/local/article7251650.html",
 				"attachments": [
 					{
-						"title": "The Sacramento Bee snapshot",
+						"title": "The Sacramento Bee Snapshot",
 						"mimeType": "text/html"
 					}
 				],
@@ -214,16 +227,17 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"date": "July 07, 2017 2:00 PM",
+				"date": "2017-07-07",
 				"ISSN": "0890-5738",
 				"abstractNote": "Shortly after Independence Day fireworks, it’s time to start thinking about another holiday – Halloween.",
 				"language": "en-US",
 				"libraryCatalog": "Sacramento Bee",
+				"publicationTitle": "The Sacramento Bee",
 				"shortTitle": "Want a giant pumpkin?",
 				"url": "http://www.sacbee.com/entertainment/living/home-garden/article159629689.html",
 				"attachments": [
 					{
-						"title": "The Sacramento Bee snapshot",
+						"title": "The Sacramento Bee Snapshot",
 						"mimeType": "text/html"
 					}
 				],
