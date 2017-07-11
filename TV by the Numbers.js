d8d2e9f8796488ecec462466e38a5e05c217f484
sonali0901 <sonaligpt0@gmail.com>
2017-07-12 00:34:45 +0530

    Fix itemtype, tag, test cases

--- TV by the Numbers.js
+++ TV by the Numbers.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-07-07 14:06:32"
+	"lastUpdated": "2017-07-11 19:01:21"
 }
 
 /*
@@ -39,7 +39,7 @@ function detectWeb(doc, url) {
 	if (url.indexOf('/?s=') > -1 && getSearchResults(doc, true))
 		return "multiple";
 	else if (doc.body.className.indexOf("single-post") > -1)
-		return "webpage";
+		return "blogPost";
 }
 
 function doWeb(doc, url) {
@@ -74,25 +74,6 @@ function getSearchResults(doc, checkOnly) {
 	return found ? items : false;
 }
 
-
-
-function doWeb(doc, url) {
-	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function(items) {
-			if (!items) {
-				return true;
-			}
-			var articles = [];
-			for (var i in items) {
-				articles.push(i);
-			}
-			ZU.processDocuments(articles, scrape);
-		});
-	} else {
-		scrape(doc, url);
-	}
-}
-
 function scrape(doc, url) {
 	var translator = Zotero.loadTranslator('web');
 	// Embedded Metadata
@@ -100,7 +81,8 @@ function scrape(doc, url) {
 	translator.setDocument(doc);
 
 	translator.setHandler('itemDone', function(obj, item) {
-		item.date = ZU.strToISO(item.date);
+		if(item.date)
+			item.date = ZU.strToISO(item.date);
 		var authors = ZU.xpath(doc, '//a[@rel="author"]');
 		for (var i in authors) {
 			if (authors[i].textContent == 'TV By The Numbers')
@@ -112,15 +94,22 @@ function scrape(doc, url) {
 			else
 				item.creators.push(ZU.cleanAuthor(authors[i].textContent, "author"));
 		}
+		item.publicationTitle = "TV By The Numbers";
+		var tags = ZU.xpath(doc, '//a[@rel="tag"]');
+		item.tags = [];
+		for (var i in tags) {
+			item.tags.push(tags[i].textContent);
+		}
 		item.complete();
 	});
 
 	translator.getTranslatorObject(function(trans) {
-		trans.itemType = "webpage";
+		trans.itemType = "blogPost";
 		trans.doWeb(doc, url);
 	});
 } /** BEGIN TEST CASES **/
-var testCases = [{
+var testCases = [
+	{
 		"type": "web",
 		"url": "http://tvbythenumbers.zap2it.com/page/20/?s=harry+potter",
 		"items": "multiple"
@@ -128,81 +117,91 @@ var testCases = [{
 	{
 		"type": "web",
 		"url": "http://tvbythenumbers.zap2it.com/daily-ratings/monday-final-ratings-july-3-2017/",
-		"items": [{
-			"itemType": "webpage",
-			"title": "‘American Ninja Warrior’ adjusts up: Monday final ratings",
-			"creators": [{
-				"firstName": "Rick",
-				"lastName": "Porter",
-				"creatorType": "author"
-			}],
-			"date": "2017-07-06",
-			"abstractNote": "Final broadcast primetime live + same-day ratings for Monday, July 3, 2017 The top show on a rerun-filled Monday night saw its adults 18-49 rating grow from the preliminary numbers to the finals. &…",
-			"shortTitle": "‘American Ninja Warrior’ adjusts up",
-			"url": "http://tvbythenumbers.zap2it.com/daily-ratings/monday-final-ratings-july-3-2017/",
-			"websiteTitle": "TV By The Numbers by zap2it.com",
-			"attachments": [{
-				"title": "Snapshot"
-			}],
-			"tags": [
-				"american ninja warrior ratings",
-				"battle of the network stars ratings",
-				"daily ratings",
-				"kevin can wait ratings",
-				"life in pieces ratings",
-				"man with a plan ratings",
-				"mom ratings",
-				"scorpion ratings",
-				"so you think you can dance ratings",
-				"spartan: ultimate team challenge ratings",
-				"supergirl ratings",
-				"superhuman ratings",
-				"the bachelorette ratings",
-				"whose line is it anyway ratings"
-			],
-			"notes": [],
-			"seeAlso": []
-		}]
+		"items": [
+			{
+				"itemType": "blogPost",
+				"title": "‘American Ninja Warrior’ adjusts up: Monday final ratings",
+				"creators": [
+					{
+						"firstName": "Rick",
+						"lastName": "Porter",
+						"creatorType": "author"
+					}
+				],
+				"date": "2017-07-06",
+				"abstractNote": "Final broadcast primetime live + same-day ratings for Monday, July 3, 2017 The top show on a rerun-filled Monday night saw its adults 18-49 rating grow from the preliminary numbers to the finals. &…",
+				"blogTitle": "TV By The Numbers",
+				"shortTitle": "‘American Ninja Warrior’ adjusts up",
+				"url": "http://tvbythenumbers.zap2it.com/daily-ratings/monday-final-ratings-july-3-2017/",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [
+					"American Ninja Warrior Ratings",
+					"Battle of the Network Stars Ratings",
+					"Kevin Can Wait Ratings",
+					"Life in Pieces Ratings",
+					"Man With a Plan Ratings",
+					"Mom Ratings",
+					"Scorpion Ratings",
+					"So You Think You Can Dance Ratings",
+					"Spartan: Ultimate Team Challenge Ratings",
+					"Supergirl Ratings",
+					"Superhuman Ratings",
+					"The Bachelorette Ratings",
+					"Whose Line is it Anyway Ratings"
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
 	},
 	{
 		"type": "web",
 		"url": "http://tvbythenumbers.zap2it.com/sdsdskdh279882992z1/tv-ratings-friday-grimm-constantine-fall-shark-tank-the-amazing-race-last-man-standing-up-cristela-hawaii-five-0-steady/322517/",
-		"items": [{
-			"itemType": "webpage",
-			"title": "TV Ratings Friday: ‘Grimm’ & ‘Constantine’ Fall, ‘Shark Tank’, ‘The Amazing Race’ & ‘Last Man Standing’ Up, ‘Cristela’ & ‘Hawaii FIve-0’ Steady",
-			"creators": [{
-				"lastName": "TV By The Numbers",
-				"creatorType": "author",
-				"fieldMode": 1
-			}],
-			"date": "2014-11-01",
-			"abstractNote": "ABC was number one in adults 18-49 while CBS won with total viewers.",
-			"shortTitle": "TV Ratings Friday",
-			"url": "http://tvbythenumbers.zap2it.com/sdsdskdh279882992z1/tv-ratings-friday-grimm-constantine-fall-shark-tank-the-amazing-race-last-man-standing-up-cristela-hawaii-five-0-steady/",
-			"websiteTitle": "TV By The Numbers by zap2it.com",
-			"attachments": [{
-				"title": "Snapshot"
-			}],
-			"tags": [
-				"'",
-				"20/20 ratings",
-				"america's next top model ratings",
-				"blue bloods ratings",
-				"constantine ratings",
-				"cristela ratings",
-				"dateline ratings",
-				"gotham ratings",
-				"hawaii five-0 ratings",
-				"last man standing ratings",
-				"shark tank ratings",
-				"the amazing race ratings",
-				"utopia ratings",
-				"whose line is it anyway ratings",
-				"world series ratings"
-			],
-			"notes": [],
-			"seeAlso": []
-		}]
+		"items": [
+			{
+				"itemType": "blogPost",
+				"title": "TV Ratings Friday: ‘Grimm’ & ‘Constantine’ Fall, ‘Shark Tank’, ‘The Amazing Race’ & ‘Last Man Standing’ Up, ‘Cristela’ & ‘Hawaii FIve-0’ Steady",
+				"creators": [
+					{
+						"lastName": "TV By The Numbers",
+						"creatorType": "author",
+						"fieldMode": 1
+					}
+				],
+				"date": "2014-11-01",
+				"abstractNote": "ABC was number one in adults 18-49 while CBS won with total viewers.",
+				"blogTitle": "TV By The Numbers",
+				"shortTitle": "TV Ratings Friday",
+				"url": "http://tvbythenumbers.zap2it.com/sdsdskdh279882992z1/tv-ratings-friday-grimm-constantine-fall-shark-tank-the-amazing-race-last-man-standing-up-cristela-hawaii-five-0-steady/",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
+				"tags": [
+					"20/20 Ratings",
+					"America's Next Top Model Ratings",
+					"Blue Bloods Ratings",
+					"Constantine Ratings",
+					"Cristela Ratings",
+					"Dateline Ratings",
+					"Gotham Ratings",
+					"Hawaii Five-0 Ratings",
+					"Last Man Standing Ratings",
+					"Shark Tank Ratings",
+					"The Amazing Race Ratings",
+					"Utopia Ratings",
+					"Whose Line is it Anyway Ratings",
+					"World Series Ratings"
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
 	}
 ]
 /** END TEST CASES **/
