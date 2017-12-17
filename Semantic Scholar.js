b3512c0ab171e4ad0d40b8b5771a64f83354378c
Guy Aglionby <guy@guyaglionby.com>
2017-12-17 02:21:17 +0000

    Grab PDFs from Semantic Scholar via the raw JSON data; add tests.

--- Semantic Scholar.js
+++ Semantic Scholar.js
@@ -75,39 +75,20 @@ function doWeb(doc, url) {
 		});
 
 		// Attach the PDF
-		var paperLinkElement = ZU.xpath(doc, '//div[@class="flex-container flex-paper-actions"]//a[@data-selenium-selector="paper-link"]')[0];
-		if (paperLinkElement) {
-			var paperLink = paperLinkElement.href;
-			// Sometimes the link is to a website where you can download it elsewhere
-			if (paperLink.endsWith('.pdf')) {
-				item.attachments.push({
-					url: paperLink,
-					title: item.title,
-					mimeType: "application/pdf"
-				});
-			} else {
-				/*
-				// Otherwise this is the div that needs to be clicked (if it exists) to show more paper links
-		var dropdownElement = ZU.xpath(doc, '//div[@class="flex-container flex-paper-actions"]//div[@class="dropdown-menu"]');
-				if (dropdownElement) {
-					// TODO Somehow simulate a click on it
-					// Once that div is clicked, a new div is added (hopefully) containing new paper links
-					// Example: https://www.semanticscholar.org/paper/Foundations-of-Statistical-Natural-Language-Proces-Manning-Sch%C3%BCtze/06fd7d924d499fbc62ccbcc2e458fb6c187bcf6f
-					// The below code should then work
-					var paperLinks = ZU.xpath(doc, '//div[@class="flex-container flex-paper-actions"]//a[@data-selenium-selector="paper-link"]');
-					paperLinks = paperLinks.map(element => element.href)
-										   .filter(link => link.endsWith('.pdf'));
-					var link = paperLinks[0];
-
-					if (link) {
-						item.attachments.push({
-							url: paperLink,
-							title: item.title,
-							mimeType: "application/pdf"
-						});
-					}
-				}*/
-			}
+		var scripts = ZU.xpath(doc, '//script');
+		const DATA_INDICATOR = 'var DATA ='
+		var dataText = scripts.map(function(element) { return element.innerHTML; } )
+						      .filter(function(script) { return script.startsWith(DATA_INDICATOR); })[0]
+						      .replace(DATA_INDICATOR, '')
+						      .slice(0, -1);
+		var rawData = JSON.parse(dataText)[0].resultData.paper;
+		if(rawData.hasPdf) {
+			var paperLink = rawData.links.filter(function(link) { return link.linkType === 's2'; })[0].url;
+			item.attachments.push({
+				url: paperLink,
+				title: item.title,
+				mimeType: 'application/pdf'
+			});
 		}
 
 		// Add the abstract
@@ -135,6 +116,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "conferencePaper",
+				"title": "TectoMT: Modular NLP Framework",
 				"creators": [
 					{
 						"firstName": "Martin",
@@ -147,9 +129,12 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "2010",
+				"abstractNote": "In the present paper we describe TectoMT, a multi-purpose open-source NLP framework. It allows for fast and efficient development of NLP applications by exploiting a wide range of software modules already integrated in TectoMT, such as tools for sentence segmentation, tokenization, morphological analysis, POS tagging, shallow and deep syntax parsing, named entity recognition, anaphora resolution, tree-to-tree translation, natural language generation, word-level alignment of parallel corpora, and other tasks. One of the most complex applications of TectoMT is the English-Czech machine translation system with transfer on deep syntactic (tectogrammatical) layer. Several modules are available also for other languages (German, Russian, Arabic). Where possible, modules are implemented in a language-independent way, so they can be reused in many applications.",
+				"itemID": "Popel2010TectoMTMN",
+				"libraryCatalog": "Semantic Scholar",
+				"proceedingsTitle": "IceTAL",
+				"shortTitle": "TectoMT",
 				"attachments": [
 					{
 						"title": "Semantic Scholar Link",
@@ -161,15 +146,9 @@ var testCases = [
 						"mimeType": "application/pdf"
 					}
 				],
-				"itemID": "Popel2010TectoMTMN",
-				"title": "TectoMT: Modular NLP Framework",
-				"publicationTitle": "IceTAL",
-				"date": "2010",
-				"abstractNote": "In the present paper we describe TectoMT, a multi-purpose open-source NLP framework. It allows for fast and efficient development of NLP applications by exploiting a wide range of software modules already integrated in TectoMT, such as tools for sentence segmentation, tokenization, morphological analysis, POS tagging, shallow and deep syntax parsing, named entity recognition, anaphora resolution, tree-to-tree translation, natural language generation, word-level alignment of parallel corpora, and other tasks. One of the most complex applications of TectoMT is the English-Czech machine translation system with transfer on deep syntactic (tectogrammatical) layer. Several modules are available also for other languages (German, Russian, Arabic). Where possible, modules are implemented in a language-independent way, so they can be reused in many applications.",
-				"doi": "10.1007/978-3-642-14770-8_33",
-				"libraryCatalog": "Semantic Scholar",
-				"shortTitle": "TectoMT",
-				"proceedingsTitle": "IceTAL"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -180,6 +159,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "journalArticle",
+				"title": "The spring in the arch of the human foot.",
 				"creators": [
 					{
 						"firstName": "Robert F.",
@@ -207,9 +187,13 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "1987",
+				"abstractNote": "Large mammals, including humans, save much of the energy needed for running by means of elastic structures in their legs and feet. Kinetic and potential energy removed from the body in the first half of the stance phase is stored briefly as elastic strain energy and then returned in the second half by elastic recoil. Thus the animal runs in an analogous fashion to a rubber ball bouncing along. Among the elastic structures involved, the tendons of distal leg muscles have been shown to be important. Here we show that the elastic properties of the arch of the human foot are also important.",
+				"itemID": "Ker1987TheSI",
+				"libraryCatalog": "Semantic Scholar",
+				"pages": "147-9",
+				"publicationTitle": "Nature",
+				"volume": "325 7000",
 				"attachments": [
 					{
 						"title": "Semantic Scholar Link",
@@ -217,16 +201,163 @@ var testCases = [
 						"snapshot": false
 					}
 				],
-				"itemID": "Ker1987TheSI",
-				"title": "The spring in the arch of the human foot.",
-				"publicationTitle": "Nature",
-				"date": "1987",
-				"volume": "325 7000",
-				"pages": "147-9",
-				"abstractNote": "Large mammals, including humans, save much of the energy needed for running by means of elastic structures in their legs and feet. Kinetic and potential energy removed from the body in the first half of the stance phase is stored briefly as elastic strain energy and then returned in the second half by elastic recoil. Thus the animal runs in an analogous fashion to a rubber ball bouncing along. Among the elastic structures involved, the tendons of distal leg muscles have been shown to be important. Here we show that the elastic properties of the arch of the human foot are also important.",
-				"libraryCatalog": "Semantic Scholar"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.semanticscholar.org/paper/Foundations-of-Statistical-Natural-Language-Proces-Manning-Sch%C3%BCtze/06fd7d924d499fbc62ccbcc2e458fb6c187bcf6f",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Foundations of Statistical Natural Language Processing",
+				"creators": [
+					{
+						"firstName": "Christopher D.",
+						"lastName": "Manning",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Hinrich",
+						"lastName": "Schüze",
+						"creatorType": "author"
+					}
+				],
+				"date": "2001",
+				"abstractNote": "In 1993, Eugene Charniak published a slim volume entitled Statistical Language Learning. At the time, empirical techniques to natural language processing were on the rise — in that year, Computational Linguistics published a special issue on such methods — and Charniak’s text was the first to treat the emerging field. Nowadays, the revolution has become the establishment; for instance, in 1998, nearly half the papers in Computational Linguistics concerned empirical methods (Hirschberg, 1998). Indeed, Christopher Manning and Hinrich Schütze’s new, by-no-means slim textbook on statistical NLP — strangely, the first since Charniak’s — begins, “The need for a thorough textbook for Statistical Natural Language Processing hardly needs to be argued for”. Indubitably so; the question is, is this it? Foundations of Statistical Natural Language Processing (henceforth FSNLP) is certainly ambitious in scope. True to its name, it contains a great deal of preparatory material, including: gentle introductions to probability and information theory; a chapter on linguistic concepts; and (a most welcome addition) discussion of the nitty-gritty of doing empirical work, ranging from lists of available corpora to indepth discussion of the critical issue of smoothing. Scattered throughout are also topics fundamental to doing good experimental work in general, such as hypothesis testing, cross-validation, and baselines. Along with these preliminaries, FSNLP covers traditional tools of the trade: Markov models, probabilistic grammars, supervised and unsupervised classification, and the vector-space model. Finally, several chapters are devoted to specific problems, among them lexicon acquisition, word sense disambiguation, parsing, machine translation, and information retrieval. (The companion website contains further useful material, including links to programs and a list of errata.) In short, this is a Big Book, and this fact alone already confers some benefits. For the researcher, FSNLP offers the convenience of one-stop shopping: at present, there is no other NLP reference in which standard empirical techniques, statistical tables, definitions of linguistics terms, and elements of information retrieval appear together; furthermore, the text also summarizes and critiques many individual research papers. Similarly, someone teaching a course on statistical NLP will appreciate the large number of topics FSNLP covers, allowing the tailoring of a syllabus to individual interests. And for those entering the field, the book records “folklore” knowledge that is typically acquired only by word of mouth",
+				"itemID": "Manning2001FoundationsOS",
+				"libraryCatalog": "Semantic Scholar",
+				"pages": "80-81",
+				"publicationTitle": "Information Retrieval",
+				"volume": "4",
+				"attachments": [
+					{
+						"title": "Semantic Scholar Link",
+						"mimeType": "text/html",
+						"snapshot": false
+					},
+					{
+						"title": "Foundations of Statistical Natural Language Processing",
+						"mimeType": "application/pdf"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.semanticscholar.org/paper/Interleukin-7-mediates-the-homeostasis-of-na%C3%AFve-an-Schluns-Kieper/aee7b854bed51120fe356a5792dfb22fec7cf2ae",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Interleukin-7 mediates the homeostasis of naïe and memory CD8 T cells in vivo",
+				"creators": [
+					{
+						"firstName": "Kimberly S.",
+						"lastName": "Schluns",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "William C.",
+						"lastName": "Kieper",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Stephen C.",
+						"lastName": "Jameson",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Leo",
+						"lastName": "Lefrançois",
+						"creatorType": "author"
+					}
+				],
+				"date": "2000",
+				"abstractNote": "The naïve and memory T lymphocyte pools are maintained through poorly understood homeostatic mechanisms that may include signaling via cytokine receptors. We show that interleukin-7 (IL-7) plays multiple roles in regulating homeostasis of CD8+ T cells. We found that IL-7 was required for homeostatic expansion of naïve CD8+ and CD4+ T cells in lymphopenic hosts and for CD8+ T cell survival in normal hosts. In contrast, IL- 7 was not necessary for growth of CD8+ T cells in response to a virus infection but was critical for generating T cell memory. Up-regulation of Bcl-2 in the absence of IL-7 signaling was impaired after activation in vivo. Homeostatic proliferation of memory cells was also partially dependent on IL-7. These results point to IL-7 as a pivotal cytokine in T cell homeostasis.",
+				"itemID": "Schluns2000Interleukin7MT",
+				"libraryCatalog": "Semantic Scholar",
+				"pages": "426-432",
+				"publicationTitle": "Nature Immunology",
+				"volume": "1",
+				"attachments": [
+					{
+						"title": "Semantic Scholar Link",
+						"mimeType": "text/html",
+						"snapshot": false
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.semanticscholar.org/paper/Prim%C3%A4re-Ziliendyskinesie-in-%C3%96sterreich-Lesic-Maurer/13c67d45a9919f44bbd07fde9bdf5f4a0e9ecc8d",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Primäre Ziliendyskinesie in Öterreich",
+				"creators": [
+					{
+						"firstName": "Irena",
+						"lastName": "Lesic",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Elisabeth",
+						"lastName": "Maurer",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Marie-Pierre F.",
+						"lastName": "Strippoli",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Claudia E.",
+						"lastName": "Kuehni",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Angelo",
+						"lastName": "Barbato",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Thomas",
+						"lastName": "Frischer",
+						"creatorType": "author"
+					}
+				],
+				"date": "2009",
+				"abstractNote": "INTRODUCTION: Primary ciliary dyskinesia (PCD) is a rare hereditary recessive disease with symptoms of recurrent pneumonia, chronic bronchitis, bronchiectasis, and chronic sinusitis. Chronic rhinitis is often the presenting symptom in newborns and infants. Approximately half of the patients show visceral mirror image arrangements (situs inversus). In this study, we aimed 1) to determine the number of paediatric PCD patients in Austria, 2) to show the diagnostic and therapeutic modalities used in the clinical centres and 3) to describe symptoms of children with PCD. PATIENTS, MATERIAL AND METHODS: For the first two aims, we analysed data from a questionnaire survey of the European Respiratory Society (ERS) task force on Primary Ciliary Dyskinesia in children. All paediatric respiratory units in Austria received a questionnaire. Symptoms of PCD patients from Vienna Children's University Hospital (aim 3) were extracted from case histories. RESULTS: In 13 Austrian clinics 48 patients with PCD (36 aged from 0–19 years) were identified. The prevalence of reported cases (aged 0–19 yrs) in Austria was 1:48000. Median age at diagnosis was 4.8 years (IQR 0.3–8.2), lower in children with situs inversus compared to those without (3.1 vs. 8.1 yrs, p = 0.067). In 2005–2006, the saccharine test was still the most commonly used screening test for PCD in Austria (45%). Confirmation of the diagnosis was usually by electron microscopy (73%). All clinics treated exacerbations immediately with antibiotics, 73% prescribed airway clearance therapy routinely to all patients. Other therapies and diagnostic tests were applied very inconsistently across Austrian hospitals. All PCD patients from Vienna (n = 13) had increased upper and lower respiratory secretions, most had recurring airway infections (n = 12), bronchiectasis (n = 7) and bronchitis (n = 7). CONCLUSION: Diagnosis and therapy of PCD in Austria are inhomogeneous. Prospective studies are needed to learn more about the course of the disease and to evaluate benefits and harms of different treatment strategies. EINLEITUNG: Die primäre Ziliendyskinesie (Primary Ciliary Dykinesia, PCD) ist eine seltene, meist autosomal-rezessiv vererbte Erkrankung, mit den typischen Manifestationen rezidivierende Pneumonien, chronische Bronchitis, Bronchiektasien, chronische Sinusitis und, insbesondere bei Neugeborenen und Säuglingen, chronischer Rhinitis. Die Hälfte der Patienten haben einen Situs inversus. Die Ziele dieser Studie waren, 1) die Anzahl pädiatrischer PCD-Patienten in Österreich zu erfassen, 2) die diagnostischen und therapeutischen Modalitäten der behandelnden Zentren darzustellen und 3) die Symptomatik der Patienten zu beschreiben. PATIENTEN, MATERIAL UND METHODEN: Zur Beantwortung der ersten zwei Fragen analysierten wir die österreichischen Resultate einer Fragebogenuntersuchung der pädiatrischen PCD Taskforce der European Respiratory Society (ERS). Die klinischen Charakteristika der PCD-Patienten an der Universitätsklinik für Kinder- und Jugendheilkunde in Wien stellten wir anhand der Krankengeschichten zusammen. ERGEBNISSE: In 13 österreichischen Krankenhäusern wurden 48 Patienten identifiziert (36 im Alter von 0–19 Jahre). Dies ergibt für Österreich eine Prävalenz diagnostizierter PCD-Patienten (0–19 Jahre) von 1:48000. Das mediane Alter bei Diagnose war 4,8 Jahre (IQR 0,3–8,2 Jahre). Patienten mit Situs inversus wurden früher diagsnotiziert (3,1 Jahre versus 8,1 Jahre; p = 0,067). Das gebräuchlichste screening-Verfahren (2005–2006) war der Saccharintest (45%), zur Diagnosesicherung wurde meist die Elektronenmikroskopie eingesetzt (73%). Alle Kliniken behandelten Exazerbationen sofort antibiotisch, Atemphysiotherapie wurde in 73% der Zentren eingesetzt. Insgesamt waren Diagnostik und Therapie der PCD in Österreich uneinheitlich. Alle Patienten der Universitätsklinik Wien (n = 13) hatten eine verstärkte Sekretproduktion, die meisten rezidivierende Atemwegsinfekte (n = 12), Bronchiektasen (n = 7) und Bronchitis (n = 7). KONKLUSION: Diagnostik und Therapie der PCD in Österreich sind uneinheitlich. Prospektive Studien sind notwendig, den Verlauf der Erkrankung zu erforschen sowie Nutzen und Schaden unterschiedlicher Therapie-konzepte darzustellen.",
+				"itemID": "Lesic2009PrimreZI",
+				"libraryCatalog": "Semantic Scholar",
+				"pages": "616-622",
+				"publicationTitle": "Wiener klinische Wochenschrift",
+				"volume": "121",
+				"attachments": [
+					{
+						"title": "Semantic Scholar Link",
+						"mimeType": "text/html",
+						"snapshot": false
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	}
 ]
 /** END TEST CASES **/
+
