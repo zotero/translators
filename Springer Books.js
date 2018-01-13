a201a2e1446b89a60906fec3e2dd8e7f4e684a70
Jonathan <psisquared2@users.noreply.github.com>
2018-01-13 11:41:35 +0100

    various minor issues

--- Springer Books.js
+++ Springer Books.js
@@ -7,15 +7,15 @@
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
-	"translatorType": 12,
-	"browserSupport": "gcsbv",
-	"lastUpdated": "2017-10-23 17:56:11"
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2018-01-13 10:40:54"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2017 YourName
+	Copyright © 2017 Jonathan Schulz
 	
 	This file is part of Zotero.
 
@@ -42,17 +42,21 @@ function detectWeb(doc, url) {
 		case "book":
 			// test if any relevant <meta> information is available
 			if(ZU.xpathText(doc, '//meta[@property="og:title"]/@content')) return "book";
+			break;
+		case "search":
 			//test for relevant search entries
-		case "search": return getSearchResults(doc, true) ? "multiple" : false;
+			return getSearchResults(doc, true) ? "multiple" : false;
+			break;
 	}
 }
 
 function doWeb(doc, url) {
 	var detection_type = detectWeb(doc, url);
 	switch (detection_type) {
-		case "book": scrapeBook(doc, url);
-		case "multiple": {
-			if (!getSearchResults(doc, true)) return;
+		case "book":
+			scrapeBook(doc, url);
+			break;
+		case "multiple":
 			Zotero.selectItems(getSearchResults(doc, false), function (items) {
 				if (!items) return true;
 				var articles = [];
@@ -61,7 +65,7 @@ function doWeb(doc, url) {
 				}
 				ZU.processDocuments(articles, scrapeBook);
 			});
-		}
+			break;
 	}
 	
 }
@@ -82,7 +86,8 @@ function getSearchResults(doc, checkOnly) {
 	for (var i=0; i<search_entries.length; i++) {
 		var item_type = search_entries[i].getAttribute("class");
 		//test if we actually have a usable result
-		if (!item_type || item_type.search(/result-item result-item-\d+ result-type-book.*/) == -1) continue;
+		if (!item_type || item_type.search(/result-item-\d+/)==-1
+			|| !item_type.includes('result-type-book')) continue;
 		var title_link = ZU.xpath(search_entries[i], "h4/a");
 		if (title_link.length == 0) continue;
 		//extract book title and URL
@@ -175,15 +180,15 @@ function scrapeBook(doc, url) {
 				item.date = yearField.match(/^©\s(\d+)/)[1];
 			}
 		}
-		//The abstract note is shortened in the <meta> field; try to load
+		//The abstract note might be shortened in the <meta> field; try to load
 		//the full abstract note
 		var long_abstractNote = ZU.xpath(doc, '//div[@class="product-about"]//div[@class="springer-html"]');
 		if (long_abstractNote.length > 0) {
 			long_abstractNote = long_abstractNote[0].textContent.trim();
 			long_abstractNote = long_abstractNote.replace(/(?:\r\n|\r|\n)/g, '');
-			//Check for consistency with existing shortened abstractNote
-			if (shortenedStringTest(item.abstractNote, long_abstractNote))
-				item.abstractNote = long_abstractNote;
+			//No consistency check here, because the <meta> abstract note
+			//and the long abstract note might be completely different
+			item.abstractNote = long_abstractNote;
 		}
 
 		item.complete();
@@ -209,6 +214,7 @@ var testCases = [
 				"date": "2017",
 				"ISBN": "9783319633237",
 				"abstractNote": "This textbook offers a clear and comprehensive introduction to methods and applications in quantum mechanics, one of the core components of undergraduate physics courses.  It follows on naturally from the previous volumes in this series, thus developing the understanding of quantized states further on. The first part of the book introduces the quantum theory of angular momentum and approximation methods. More complex themes are covered in the second part of the book, which describes multiple particle systems and scattering theory.  Ideally suited to undergraduate students with some grounding in the basics of quantum mechanics, the book is enhanced throughout with learning features such as boxed inserts and chapter summaries, with key mathematical derivations highlighted to aid understanding.  The text is supported by numerous worked examples and end of chapter problem sets. About the Theoretical Physics series Translated from the renowned and highly successful German editions, the eight volumes of this series cover the complete core curriculum of theoretical physics at undergraduate level. Each volume is self-contained and provides all the material necessary for the individual course topic. Numerous problems with detailed solutions support a deeper understanding.  Wolfgang Nolting is famous for his refined didactical style and has been referred to as the \"German Feynman\" in reviews.",
+				"language": "en",
 				"libraryCatalog": "www.springer.com",
 				"publisher": "Springer International Publishing",
 				"shortTitle": "Theoretical Physics 7",
@@ -241,6 +247,7 @@ var testCases = [
 				"date": "2013",
 				"ISBN": "9789462094826",
 				"abstractNote": "This book focuses on the delivery of public examinations offered by the main examining boards in England since Victorian England. The investigation reveals that the provision of examinations was as controversial in the nineteenth century as it is today, particularly since the government is now determined to bring in reform. The issues of grade inflation, the place of coursework in marking, and the introduction of technological change all feature in this book. Educational policy is primarily examined as well as some reference to the global scene. The study analyses archival material from a wide range of sources, including those records stored at the National Archives and the London Metropolitan Archives. An emphasis is placed upon the various institutions that contributed to the process, including the Royal Society of Arts, the London Chamber of Commerce, the City of Guilds of London Institute and the University of London. Attention is given to the findings of the Taunton Commission and the Bryce Commission and shorter reports such as the Northcote-Trevelyn Report which served to radicalise entry and recruitment to the Civil Service. The modern GCSE and the plans for I-levels are considered and key observations are made about the efficacy of those examinations offered by Oxford and Cambridge universities and O-levels, A-levels and NVQs, The reader is given every opportunity to benefit enthusiastically in this account of examinations, and those engaged in education, whether teachers, examiners, students or administrators, will be able to gain useful insights into the workings of the examination system.",
+				"language": "en",
 				"libraryCatalog": "www.springer.com",
 				"publisher": "Sense Publishers",
 				"shortTitle": "Testing Times",
@@ -273,6 +280,7 @@ var testCases = [
 				"date": "2005",
 				"ISBN": "9783540212904",
 				"abstractNote": "Complex geometry studies (compact) complex manifolds. It discusses algebraic as well as metric aspects. The subject is on the crossroad of algebraic and differential geometry. Recent developments in string theory have made it an highly attractive area, both for mathematicians and theoretical physicists.The author’s goal is to provide an easily accessible introduction to the subject. The book contains detailed accounts of the basic concepts and the many exercises illustrate the theory. Appendices to various chapters allow an outlook to recent research directions.Daniel Huybrechts is currently Professor of Mathematics at the University Denis Diderot in Paris.",
+				"language": "en",
 				"libraryCatalog": "www.springer.com",
 				"place": "Berlin Heidelberg",
 				"publisher": "Springer-Verlag",
