b8aa8d2dd87875a518e60f7eeb088ff7634702b2
Justin Warren <justin@eigenmagic.com>
2018-01-20 16:38:27 +1100

    Updated AustLII translater to support the new fancy website layout.

--- AustLII and NZLII.js
+++ AustLII and NZLII.js
@@ -2,14 +2,14 @@
 	"translatorID": "5ed5ab01-899f-4a3b-a74c-290fb2a1c9a4",
 	"label": "AustLII and NZLII",
 	"creator": "Bill McKinney and Sebastian Karcher",
-	"target": "^https?://www\\.(austlii\\.edu\\.au|nzlii\\.org)",
+	"target": "^https?://www\\d?\\.(austlii\\.edu\\.au|nzlii\\.org)",
 	"minVersion": "2.1.9",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2012-05-16 23:48:05"
+	"lastUpdated": "2018-01-20 05:35:28"
 }
 
 function detectWeb(doc, url) {
@@ -29,16 +29,16 @@ function detectWeb(doc, url) {
 
 function scrape(doc) {
 	var newItem = new Zotero.Item("case");
-	var voliss = ZU.xpathText(doc, '//h2');
-	var title = voliss.match(/.+?\[/)[0].replace(/\[/, "");
-	newItem.title =  newItem.caseName = title;
-	//ZU.capitalizeTitle(title.toLowerCase(), true);
+	var voliss = ZU.xpathText(doc, '//head/title');
+	var title = voliss.match(/.+?\[/)[0].replace(/ \[/, "");
+
+	newItem.title = newItem.caseName = title;
 	newItem.url = doc.location.href;
 	var court = ZU.trim(voliss.match(/\].+?[\(\[]/)[0].replace(/[\]\(\[]/g, ""))
 	newItem.court = court.match(/[^0-9]+/)[0]
 	newItem.docketNumber=court.match(/\d+/)[0]
 	newItem.dateDecided = voliss.match(/\(\d[^\)]+\d{4}\)/)[0].replace(/[\(\)]/g, "");
-	newItem.attachments = [{ document:doc, title:"AustLII/NZLII snapshort", mimeType:"text/html"}];
+	newItem.attachments = [{ document:doc, title:"AustLII/NZLII snapshot", mimeType:"text/html"}];
 	newItem.complete();
 }
 
@@ -67,55 +67,49 @@ function doWeb(doc, url) {
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://www.austlii.edu.au/au/cases/cth/FamCA/2006/212.html",
+		"url": "http://www7.austlii.edu.au/cgi-bin/viewdoc/au/cases/cth/FamCA/2006/212.html",
 		"items": [
 			{
 				"itemType": "case",
+				"caseName": "C & M",
 				"creators": [],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"dateDecided": "20 January 2006",
+				"court": "FamCA",
+				"docketNumber": "212",
+				"url": "http://www7.austlii.edu.au/cgi-bin/viewdoc/au/cases/cth/FamCA/2006/212.html",
 				"attachments": [
 					{
-						"title": "AustLII/NZLII snapshort",
+						"title": "AustLII/NZLII snapshot",
 						"mimeType": "text/html"
 					}
 				],
-				"caseName": "C & M",
-				"title": "C & M",
-				"url": "http://www.austlii.edu.au/au/cases/cth/FamCA/2006/212.html",
-				"court": "FamCA",
-				"docketNumber": "212",
-				"dateDecided": "20 January 2006",
-				"libraryCatalog": "AustLII and NZLII",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
-		"url": "http://www.austlii.edu.au/au/cases/cth/FCA/2010/1.html",
+		"url": "http://www8.austlii.edu.au/cgi-bin/viewdoc/au/cases/cth/FCA/2010/1.html",
 		"items": [
 			{
 				"itemType": "case",
+				"caseName": "Yeo, in the matter of AES Services (Aust) Pty Ltd (ACN 111 306 543) (Administrators Appointed)",
 				"creators": [],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"dateDecided": "5 January 2010",
+				"court": "FCA",
+				"docketNumber": "1",
+				"url": "http://www8.austlii.edu.au/cgi-bin/viewdoc/au/cases/cth/FCA/2010/1.html",
 				"attachments": [
 					{
-						"title": "AustLII/NZLII snapshort",
+						"title": "AustLII/NZLII snapshot",
 						"mimeType": "text/html"
 					}
 				],
-				"caseName": "Yeo, in the matter of AES Services (Aust) Pty Ltd (ACN 111 306 543) (Administrators Appointed)",
-				"title": "Yeo, in the matter of AES Services (Aust) Pty Ltd (ACN 111 306 543) (Administrators Appointed)",
-				"url": "http://www.austlii.edu.au/au/cases/cth/FCA/2010/1.html",
-				"court": "FCA",
-				"docketNumber": "1",
-				"dateDecided": "5 January 2010",
-				"libraryCatalog": "AustLII and NZLII",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -125,31 +119,76 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "case",
+				"caseName": "Bronwyn Estate Ltd and ors v Gareth Hoole and others",
 				"creators": [],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"dateDecided": "8 February 2008",
+				"court": "NZSC",
+				"docketNumber": "1",
+				"url": "http://www.nzlii.org/nz/cases/NZSC/2008/1.html",
 				"attachments": [
 					{
-						"title": "AustLII/NZLII snapshort",
+						"title": "AustLII/NZLII snapshot",
 						"mimeType": "text/html"
 					}
 				],
-				"caseName": "Bronwyn Estate Ltd and ors v Gareth Hoole and others",
-				"title": "Bronwyn Estate Ltd and ors v Gareth Hoole and others",
-				"url": "http://www.nzlii.org/nz/cases/NZSC/2008/1.html",
-				"court": "NZSC",
-				"docketNumber": "1",
-				"dateDecided": "8 February 2008",
-				"libraryCatalog": "AustLII and NZLII",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
-		"url": "http://www.austlii.edu.au/au/cases/act/ACTSC/2010/",
+		"url": "http://www.nzlii.org/nz/cases/NZSC/2008/1.html",
+		"items": [
+			{
+				"itemType": "case",
+				"caseName": "Bronwyn Estate Ltd and ors v Gareth Hoole and others",
+				"creators": [],
+				"dateDecided": "8 February 2008",
+				"court": "NZSC",
+				"docketNumber": "1",
+				"url": "http://www.nzlii.org/nz/cases/NZSC/2008/1.html",
+				"attachments": [
+					{
+						"title": "AustLII/NZLII snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www8.austlii.edu.au/cgi-bin/viewtoc/au/cases/act/ACTSC/2010/",
 		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://www8.austlii.edu.au/cgi-bin/viewdoc/au/cases/cth/AICmr/2017/134.html",
+		"items": [
+			{
+				"itemType": "case",
+				"caseName": "'NM' and Department of Human Services (Freedom of information)",
+				"creators": [],
+				"dateDecided": "8 December 2017",
+				"court": "AICmr",
+				"docketNumber": "134",
+				"url": "http://www8.austlii.edu.au/cgi-bin/viewdoc/au/cases/cth/AICmr/2017/134.html",
+				"attachments": [
+					{
+						"title": "AustLII/NZLII snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
 	}
 ]
 /** END TEST CASES **/
