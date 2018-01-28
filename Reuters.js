a42583e74bf42a67234fd6fb338bd212818d9640
Philipp Zumstein <zuphilip@gmail.com>
2018-01-28 22:21:21 +0100

    Cleanup some old code

--- Reuters.js
+++ Reuters.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2015-06-02 21:16:57"
+	"lastUpdated": "2018-01-28 20:10:54"
 }
 
 /*
@@ -36,101 +36,120 @@
 	***** END LICENSE BLOCK *****
 */
 
+
+// attr()/text() v2
+function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}
+
+
 function detectWeb(doc, url) {
 	if (url.match(/^https?:\/\/(www\.)?reuters\.com\/article/)) {
 		return "newspaperArticle";
 	} else if (url.match(/^https?:\/\/blogs\.reuters\.com/)) {
 	  return "blogPost";
-	} else if (url.match(/search\?/)) {
+	} else if (url.includes('/search/') && getSearchResults(doc, true)) {
 	  return "multiple";
 	}
 }
 
-function doWeb(doc, url) {
-	var articles = new Array();
-	if (detectWeb(doc, url) == "multiple") {
-		var items = {};
-		var titles = doc.evaluate('//li[@class="searchHeadline"]/a', doc, null, XPathResult.ANY_TYPE, null);
-		var title;
-		while (title = titles.iterateNext()) {
-			items[title.href] = title.textContent;
-		}
 
-		Zotero.selectItems(items, function (items) {
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('h3.search-result-title>a');
+	for (let i=0; i<rows.length; i++) {
+		let href = rows[i].href;
+		let title = ZU.trimInternal(rows[i].textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
+
+
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
 			if (!items) {
 				return true;
 			}
+			var articles = [];
 			for (var i in items) {
 				articles.push(i);
 			}
-			Zotero.Utilities.processDocuments(articles, scrape, function () {
-				Zotero.done();
-			});
-			Zotero.wait();
+			ZU.processDocuments(articles, scrape);
 		});
 	} else {
 		scrape(doc, url);
 	}
 }
 
+
 function scrape(doc, url) {
-	if (detectWeb(doc, url) == "newspaperArticle") {
-		var item = new Zotero.Item("newspaperArticle");
-
-		item.date = doc.evaluate('//meta[@name="REVISION_DATE"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext().content;
-		var byline = ZU.xpathText(doc, '//div[@id="articleInfo"]//p[@class="byline"]');
-		if (byline) {
-			var authors = byline.substr(3).split(/and |,/);
-			for (var i=0; i<authors.length; i++) {
-				item.creators.push(authorFix(authors[i]));
-			}
-		}
-		item.publicationTitle = "Reuters";
+	var type = detectWeb(doc, url);
+	// copy meta tags in body to head
+	var head = doc.getElementsByTagName('head');
+	var metasInBody = ZU.xpath(doc, '//body/meta');
+	for (var i=0; i<metasInBody.length; i++) {
+		head[0].append(metasInBody[i]);
 	}
-	if (detectWeb(doc, url) == "blogPost") {
-		var item = new Zotero.Item("blogPost");
-
-		item.date = ZU.xpathText(doc, '//div[@id="single"]/div[@class="timestamp"]');
-		var byline = ZU.xpathText(doc, '//div[@class="author"]');
-		if (byline) {
-			var authors = byline.split(/and |,/);
-			for (var i=0; i<authors.length; i++) {
-				item.creators.push(authorFix(authors[i]));
+	var translator = Zotero.loadTranslator('web');
+	// Embedded Metadata
+	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
+	// translator.setDocument(doc);
+	
+	translator.setHandler('itemDone', function (obj, item) {
+		if (detectWeb(doc, url) == "newspaperArticle") {
+			Z.debug(item.date);
+			item.date = doc.evaluate('//meta[@name="REVISION_DATE"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext().content;
+			Z.debug(item.date);
+			var byline = ZU.xpathText(doc, '//div[@id="articleInfo"]//p[@class="byline"]');
+			if (byline) {
+				var authors = byline.substr(3).split(/and |,/);
+				for (var i=0; i<authors.length; i++) {
+					item.creators.push(authorFix(authors[i]));
+				}
 			}
+			item.publicationTitle = "Reuters";
 		}
-
-		var blogtitle = ZU.xpathText(doc, '//h1');
-		if (blogtitle) item.publicationTitle = "Reuters Blogs - " + blogtitle;
-		else item.publicationTitle = "Reuters Blogs";
-	}
-
-	//general fields
-	if(item) {
-		item.title = ZU.xpathText(doc, '//meta[@property="og:title"]/@content');
-
+		if (detectWeb(doc, url) == "blogPost") {
+			item.date = text(doc, '#thebyline .timestamp');
+			var byline = text(doc, 'div.author');
+			if (byline) {
+				var authors = byline.split(/and |,/);
+				for (var i=0; i<authors.length; i++) {
+					item.creators.push(authorFix(authors[i]));
+				}
+			}
+	
+			var blogtitle = text(doc, 'h1');
+			if (blogtitle) item.publicationTitle = "Reuters Blogs - " + blogtitle;
+			else item.publicationTitle = "Reuters Blogs";
+		}
+		
+		if (item.date) {
+			item.date = ZU.strToISO(item.date);
+		}
 		item.place = ZU.xpathText(doc, '//div[@id="articleInfo"]//span[@class="location"]');
 		if (item.place) {
 			if (item.place == item.place.toUpperCase()) item.place = Zotero.Utilities.capitalizeTitle(item.place.toLowerCase(), true);
 		}
 
-		item.url = ZU.xpathText(doc, '//link[@rel="canonical"]/@href');
-		item.abstractNote = ZU.xpathText(doc, '//meta[@name="description"]/@content');
-
-		var tags = ZU.xpathText(doc, '//meta[@name="keywords"]/@content');
-		if(tags)
-			item.tags = tags.trim().split(/\s*,\s*/);
-
-		item.attachments.push({title: 'Snapshot', document: doc});
-
 		item.complete();
-	}
+	});
+
+	translator.getTranslatorObject(function(trans) {
+		trans.itemType = type;
+		trans.doWeb(doc, url);
+	});
 }
+
 
 function authorFix(author) {
 	// Sometimes we have "By Author"
-	if (author.substr(0, 3).toLowerCase() == "by ") {
-		author = author.substr(3);
-	}
+	author = author.replace(/^\s*by/i, '');
+
 	var cleaned = Zotero.Utilities.cleanAuthor(author, "author");
 	// If we have only one name, set the author to one-name mode
 	if (cleaned.firstName == "") {
@@ -152,6 +171,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "newspaperArticle",
+				"title": "Europe could be in worst hour since WW2: Merkel",
 				"creators": [
 					{
 						"firstName": "James",
@@ -164,56 +184,55 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [
-					"Germany",
-					"Greece",
-					"Italy",
-					"Germany",
-					"Greece",
-					"Italy",
-					"Germany",
-					"Angela Merkel",
-					"Antonis Samaras",
-					"George Papandreou",
-					"Harry Papachristou",
-					"Jens Weidmann",
-					"Lucas Papademos",
-					"Mario Monti",
-					"Olli Rehn",
-					"Philip Pullella",
-					"Silvio Berlusconi",
-					"Angela Merkel",
-					"Antonis Samaras",
-					"George Papandreou",
-					"Giorgio Napolitano",
-					"Harry Papachristou",
-					"Jack Ablin",
-					"Jens Weidmann",
-					"Kai Pfaffenbach",
-					"Lucas Papademos",
-					"Mario Monti",
-					"Olli Rehn",
-					"Philip Pullella",
-					"Silvio Berlusconi",
-					"Angela Merkel",
-					"Kai Pfaffenbach"
-				],
-				"seeAlso": [],
+				"date": "Mon Nov 14 21:16:28 UTC 2011",
+				"abstractNote": "ROME (Reuters) - Prime Minister-designate Mario Monti meets the leaders of Italy's biggest two parties on Tuesday to discuss the many sacrifices needed to reverse a collapse in market confidence that is",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "Reuters",
+				"place": "Rome",
+				"publicationTitle": "Reuters",
+				"shortTitle": "Europe could be in worst hour since WW2",
+				"url": "http://www.reuters.com/article/2011/11/14/us-eurozone-idUSTRE7AC15K20111114",
 				"attachments": [
 					{
 						"title": "Snapshot"
 					}
 				],
-				"date": "Mon Nov 14 21:16:28 UTC 2011",
-				"publicationTitle": "Reuters",
-				"title": "Europe could be in worst hour since WW2: Merkel",
-				"place": "Rome",
-				"url": "http://www.reuters.com/article/2011/11/14/us-eurozone-idUSTRE7AC15K20111114",
-				"abstractNote": "ROME (Reuters) - Prime Minister-designate Mario Monti meets the leaders of Italy's biggest two parties on Tuesday to discuss the many sacrifices needed to reverse a collapse in market confidence that is",
-				"libraryCatalog": "Reuters",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"shortTitle": "Europe could be in worst hour since WW2"
+				"tags": [
+					"Angela Merkel",
+					"Angela Merkel",
+					"Angela Merkel",
+					"Antonis Samaras",
+					"Antonis Samaras",
+					"George Papandreou",
+					"George Papandreou",
+					"Germany",
+					"Germany",
+					"Germany",
+					"Giorgio Napolitano",
+					"Greece",
+					"Greece",
+					"Harry Papachristou",
+					"Harry Papachristou",
+					"Italy",
+					"Italy",
+					"Jack Ablin",
+					"Jens Weidmann",
+					"Jens Weidmann",
+					"Kai Pfaffenbach",
+					"Kai Pfaffenbach",
+					"Lucas Papademos",
+					"Lucas Papademos",
+					"Mario Monti",
+					"Mario Monti",
+					"Olli Rehn",
+					"Olli Rehn",
+					"Philip Pullella",
+					"Philip Pullella",
+					"Silvio Berlusconi",
+					"Silvio Berlusconi"
+				],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
@@ -223,6 +242,7 @@ var testCases = [
 		"items": [
 			{
 				"itemType": "blogPost",
+				"title": "It’s too soon to return to normal policies",
 				"creators": [
 					{
 						"firstName": "Lawrence",
@@ -230,7 +250,16 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
+				"abstractNote": "After years when the risks to the consensus modest-growth forecast were to the downside, they are now very much two-sided.",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"libraryCatalog": "Reuters",
+				"publicationTitle": "Reuters Blogs - Lawrence Summers",
+				"url": "http://blogs.reuters.com/lawrencesummers/2012/03/26/its-too-soon-to-return-to-normal-policies/",
+				"attachments": [
+					{
+						"title": "Snapshot"
+					}
+				],
 				"tags": [
 					"deficit",
 					"fiscal policy",
@@ -238,24 +267,19 @@ var testCases = [
 					"recovery",
 					"unemployment"
 				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Snapshot"
-					}
-				],
-				"title": "It’s too soon to return to normal policies",
-				"abstractNote": "After years when the risks to the consensus modest-growth forecast were to the downside, they are now very much two-sided.",
-				"url": "http://blogs.reuters.com/lawrencesummers/2012/03/26/its-too-soon-to-return-to-normal-policies/",
-				"publicationTitle": "Reuters Blogs - Lawrence Summers",
-				"libraryCatalog": "Reuters",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
 		"url": "http://www.reuters.com/search?blob=europe",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.reuters.com/search/news?blob=europe",
 		"items": "multiple"
 	}
 ]
