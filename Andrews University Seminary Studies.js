b7bbce79f55b33ce405221bb441a5cb409fabb6a
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2018-06-11 10:22:36 +0200

    AUSS: Give away my "hard work" to the uni

--- Andrews University Seminary Studies.js
+++ Andrews University Seminary Studies.js
@@ -15,7 +15,7 @@
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2018 Madeesh Kannan
+	Copyright © 2018 Universitätsbibliothek Tübingen.  All rights reserved.
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
