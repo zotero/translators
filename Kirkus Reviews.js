b42342d3a77834b01761ff2f95f0d6360f380cfe
Wolf <owcz@users.noreply.github.com>
2018-07-08 17:56:33 -0400

    rmv FW

--- Kirkus Reviews.js
+++ Kirkus Reviews.js
@@ -9,14 +9,13 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-03-16 07:09:00"
+	"lastUpdated": "2018-07-08 21:56:12"
 }
 
-/* FW LINE 59:b820c6d */ function flatten(t){var e=new Array;for(var i in t){var r=t[i];r instanceof Array?e=e.concat(flatten(r)):e.push(r)}return e}var FW={_scrapers:new Array};FW._Base=function(){this.callHook=function(t,e,i,r){if("object"==typeof this.hooks){var n=this.hooks[t];"function"==typeof n&&n(e,i,r)}},this.evaluateThing=function(t,e,i){var r=typeof t;if("object"===r){if(t instanceof Array){var n=this.evaluateThing,a=t.map(function(t){return n(t,e,i)});return flatten(a)}return t.evaluate(e,i)}return"function"===r?t(e,i):t},this.makeItems=function(t,e,i,r,n){n()}},FW.Scraper=function(t){FW._scrapers.push(new FW._Scraper(t))},FW._Scraper=function(t){for(x in t)this[x]=t[x];this._singleFieldNames=["abstractNote","applicationNumber","archive","archiveLocation","artworkMedium","artworkSize","assignee","audioFileType","audioRecordingType","billNumber","blogTitle","bookTitle","callNumber","caseName","code","codeNumber","codePages","codeVolume","committee","company","conferenceName","country","court","date","dateDecided","dateEnacted","dictionaryTitle","distributor","docketNumber","documentNumber","DOI","edition","encyclopediaTitle","episodeNumber","extra","filingDate","firstPage","forumTitle","genre","history","institution","interviewMedium","ISBN","ISSN","issue","issueDate","issuingAuthority","journalAbbreviation","label","language","legalStatus","legislativeBody","letterType","libraryCatalog","manuscriptType","mapType","medium","meetingName","nameOfAct","network","number","numberOfVolumes","numPages","pages","patentNumber","place","postType","presentationType","priorityNumbers","proceedingsTitle","programTitle","programmingLanguage","publicLawNumber","publicationTitle","publisher","references","reportNumber","reportType","reporter","reporterVolume","rights","runningTime","scale","section","series","seriesNumber","seriesText","seriesTitle","session","shortTitle","studio","subject","system","thesisType","title","type","university","url","version","videoRecordingType","volume","websiteTitle","websiteType"],this._makeAttachments=function(t,e,i,r){if(i instanceof Array)i.forEach(function(i){this._makeAttachments(t,e,i,r)},this);else if("object"==typeof i){var n=i.urls||i.url,a=i.types||i.type,s=i.titles||i.title,o=i.snapshots||i.snapshot,u=this.evaluateThing(n,t,e),l=this.evaluateThing(s,t,e),c=this.evaluateThing(a,t,e),h=this.evaluateThing(o,t,e);u instanceof Array||(u=[u]);for(var f in u){var p,m,v,d=u[f];p=c instanceof Array?c[f]:c,m=l instanceof Array?l[f]:l,v=h instanceof Array?h[f]:h,r.attachments.push({url:d,title:m,mimeType:p,snapshot:v})}}},this.makeItems=function(t,e,i,r,n){var a=new Zotero.Item(this.itemType);a.url=e;for(var s in this._singleFieldNames){var o=this._singleFieldNames[s];if(this[o]){var u=this.evaluateThing(this[o],t,e);u instanceof Array?a[o]=u[0]:a[o]=u}}var l=["creators","tags"];for(var c in l){var h=l[c],f=this.evaluateThing(this[h],t,e);if(f)for(var p in f)a[h].push(f[p])}this._makeAttachments(t,e,this.attachments,a),r(a,this,t,e),n()}},FW._Scraper.prototype=new FW._Base,FW.MultiScraper=function(t){FW._scrapers.push(new FW._MultiScraper(t))},FW._MultiScraper=function(t){for(x in t)this[x]=t[x];this._mkSelectItems=function(t,e){var i=new Object;for(var r in t)i[e[r]]=t[r];return i},this._selectItems=function(t,e,i){var r=new Array;Zotero.selectItems(this._mkSelectItems(t,e),function(t){for(var e in t)r.push(e);i(r)})},this._mkAttachments=function(t,e,i){var r=this.evaluateThing(this.attachments,t,e),n=new Object;if(r)for(var a in i)n[i[a]]=r[a];return n},this._makeChoices=function(t,e,i,r,n){if(t instanceof Array)t.forEach(function(t){this._makeTitlesUrls(t,e,i,r,n)},this);else if("object"==typeof t){var a=t.urls||t.url,s=t.titles||t.title,o=this.evaluateThing(a,e,i),u=this.evaluateThing(s,e,i),l=u instanceof Array;o instanceof Array||(o=[o]);for(var c in o){var h,f=o[c];h=l?u[c]:u,n.push(f),r.push(h)}}},this.makeItems=function(t,e,i,r,n){if(this.beforeFilter){var a=this.beforeFilter(t,e);if(a!=e)return void this.makeItems(t,a,i,r,n)}var s=[],o=[];this._makeChoices(this.choices,t,e,s,o);var u=this._mkAttachments(t,e,o),l=this.itemTrans;this._selectItems(s,o,function(t){if(t){var e=function(t){var e=t.documentURI,i=l;void 0===i&&(i=FW.getScraper(t,e)),void 0===i||i.makeItems(t,e,u[e],r,function(){})};Zotero.Utilities.processDocuments(t,e,n)}else n()})}},FW._MultiScraper.prototype=new FW._Base,FW.WebDelegateTranslator=function(t){return new FW._WebDelegateTranslator(t)},FW._WebDelegateTranslator=function(t){for(x in t)this[x]=t[x];this.makeItems=function(t,e,i,r,n){var a=this,s=Zotero.loadTranslator("web");s.setHandler("itemDone",function(i,n){r(n,a,t,e)}),s.setDocument(t),this.translatorId?(s.setTranslator(this.translatorId),s.translate()):(s.setHandler("translators",function(t,e){e.length&&(s.setTranslator(e[0]),s.translate())}),s.getTranslators()),n()}},FW._WebDelegateTranslator.prototype=new FW._Base,FW._StringMagic=function(){this._filters=new Array,this.addFilter=function(t){return this._filters.push(t),this},this.split=function(t){return this.addFilter(function(e){return e.split(t).filter(function(t){return""!=t})})},this.replace=function(t,e,i){return this.addFilter(function(r){return r.match(t)?r.replace(t,e,i):r})},this.prepend=function(t){return this.replace(/^/,t)},this.append=function(t){return this.replace(/$/,t)},this.remove=function(t,e){return this.replace(t,"",e)},this.trim=function(){return this.addFilter(function(t){return Zotero.Utilities.trim(t)})},this.trimInternal=function(){return this.addFilter(function(t){return Zotero.Utilities.trimInternal(t)})},this.match=function(t,e){return e||(e=0),this.addFilter(function(i){var r=i.match(t);return void 0===r||null===r?void 0:r[e]})},this.cleanAuthor=function(t,e){return this.addFilter(function(i){return Zotero.Utilities.cleanAuthor(i,t,e)})},this.key=function(t){return this.addFilter(function(e){return e[t]})},this.capitalizeTitle=function(){return this.addFilter(function(t){return Zotero.Utilities.capitalizeTitle(t)})},this.unescapeHTML=function(){return this.addFilter(function(t){return Zotero.Utilities.unescapeHTML(t)})},this.unescape=function(){return this.addFilter(function(t){return unescape(t)})},this._applyFilters=function(t,e){for(i in this._filters){t=flatten(t),t=t.filter(function(t){return void 0!==t&&null!==t});for(var r=0;r<t.length;r++)try{if(void 0===t[r]||null===t[r])continue;t[r]=this._filters[i](t[r],e)}catch(n){t[r]=void 0,Zotero.debug("Caught exception "+n+"on filter: "+this._filters[i])}t=t.filter(function(t){return void 0!==t&&null!==t})}return flatten(t)}},FW.PageText=function(){return new FW._PageText},FW._PageText=function(){this._filters=new Array,this.evaluate=function(t){var e=[t.documentElement.innerHTML];return e=this._applyFilters(e,t),0==e.length?!1:e}},FW._PageText.prototype=new FW._StringMagic,FW.Url=function(){return new FW._Url},FW._Url=function(){this._filters=new Array,this.evaluate=function(t,e){var i=[e];return i=this._applyFilters(i,t),0==i.length?!1:i}},FW._Url.prototype=new FW._StringMagic,FW.Xpath=function(t){return new FW._Xpath(t)},FW._Xpath=function(t){this._xpath=t,this._filters=new Array,this.text=function(){var t=function(t){return"object"==typeof t&&t.textContent?t.textContent:t};return this.addFilter(t),this},this.sub=function(t){var e=function(e,i){var r=i.evaluate(t,e,null,XPathResult.ANY_TYPE,null);return r?r.iterateNext():void 0};return this.addFilter(e),this},this.evaluate=function(t){var e=t.evaluate(this._xpath,t,null,XPathResult.ANY_TYPE,null),i=e.resultType,r=new Array;if(i==XPathResult.STRING_TYPE)r.push(e.stringValue);else if(i==XPathResult.BOOLEAN_TYPE)r.push(e.booleanValue);else if(i==XPathResult.NUMBER_TYPE)r.push(e.numberValue);else if(i==XPathResult.ORDERED_NODE_ITERATOR_TYPE||i==XPathResult.UNORDERED_NODE_ITERATOR_TYPE)for(var n;n=e.iterateNext();)r.push(n);return r=this._applyFilters(r,t),0==r.length?!1:r}},FW._Xpath.prototype=new FW._StringMagic,FW.detectWeb=function(t,e){for(var i in FW._scrapers){var r=FW._scrapers[i],n=r.evaluateThing(r.itemType,t,e),a=r.evaluateThing(r.detect,t,e);if(a.length>0&&a[0])return n}},FW.getScraper=function(t,e){var i=FW.detectWeb(t,e);return FW._scrapers.filter(function(r){return r.evaluateThing(r.itemType,t,e)==i&&r.evaluateThing(r.detect,t,e)})[0]},FW.doWeb=function(t,e){var i=FW.getScraper(t,e);i.makeItems(t,e,[],function(t,e,i,r){e.callHook("scraperDone",t,i,r),t.title||(t.title=""),t.complete()},function(){Zotero.done()}),Zotero.wait()};
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2016–2017 czar
+	Copyright © 2018 czar
 	http://en.wikipedia.org/wiki/User_talk:Czar
 
 	This file is part of Zotero.
@@ -37,59 +36,95 @@
 	***** END LICENSE BLOCK *****
 */
 
-function detectWeb(doc, url) { return FW.detectWeb(doc, url); }
-function doWeb(doc, url) { return FW.doWeb(doc, url); }
 
-FW.Scraper({
-itemType         : 'journalArticle',
-detect           : FW.Xpath('//h2[@content="Kirkus Book Review"]'),
-title            : FW.Xpath('//meta[@property="og:title"]/@content').text().split(/\|/),
-attachments      : [{ url: FW.Url(),
-  title:  "Kirkus Reviews snapshot",
-  type: "text/html" }],
-creators         : FW.Xpath('//div[@class="book-author clearfix"]//a/span').text().cleanAuthor("reviewedAuthor"),
-date             : FW.Xpath('//span[@itemprop="datePublished"]/@content').text(),
-publicationTitle : "Kirkus Reviews",
-language         : "en-US",
-abstractNote     : FW.Xpath('//meta[@name="description"]/@content').text(),
-hooks : { "scraperDone": function  (item,doc,url) {
-	// Process title
-	item.title = Zotero.Utilities.capitalizeTitle(item.title.toLowerCase(),true).replace (/^/,'Rev. of ').replace(/\s,/,",");
-}}
-});
+// attr()/text() v2 per https://github.com/zotero/translators/issues/1277
+function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}
 
-FW.Scraper({
-itemType         : 'blogPost',
-detect           : FW.Xpath('//body[@data-slot-scope="blog-post"]'),
-title            : FW.Xpath('//meta[@property="og:title"]/@content').text().split(/\|/),
-attachments      : [{ url: FW.Url(),
-  title:  "Kirkus Reviews snapshot",
-  type: "text/html" }],
-creators         : FW.Xpath('(//div[@class="blog-post-teaser-meta-ctr "])[1]').text().replace(/[\n\s]+/g," ").replace(/.*[Bb]y\s([\w\s]+),?.*\son\s.*/,"$1").cleanAuthor("reviewedAuthor"), // http://regexr.com/3fhho
-date             : FW.Xpath('(//div[@class="blog-post-teaser-meta-ctr "])[1]').text().replace(/[\n\s]+/g," ").replace(/.*\son\s(.*)/,"$1"),
-blogTitle		 : "Kirkus Reviews",
-language         : "en-US",
-abstractNote     : FW.Xpath('//meta[@name="description"]/@content').text()
-});
 
-FW.MultiScraper({
-itemType         : 'multiple',
-detect           : FW.Xpath('//div[@class="page-intro-header"]'), // nav cats
-choices          : {
-  titles  :  FW.Xpath('(//div[@class="row book-item-ctr"]//span[@itemprop="name"])|(//div[@class="row blog-post-teaser-row"]//div[@class="blog-post-header"]/a)').text().trim(),
-  urls    :  FW.Xpath('(//div[@class="row book-item-ctr"]//span[@itemprop="name"]/ancestor::a)|(//div[@class="row blog-post-teaser-row"]//div[@class="blog-post-header"]/a)').key("href")
+function detectWeb(doc, url) {
+	if (doc.querySelector('div[itemprop="review"]')) {
+		return "journalArticle";
+	} else if (doc.querySelector('body[data-slot-scope="blog-post"]')) {
+		return "blogPost"
+	} else if (getSearchResults(doc, true)) {
+		return "multiple";
+	}
 }
-});
 
-FW.MultiScraper({
-itemType         : 'multiple',
-detect           : FW.Url().match(/\/search\/\?q=/), // search
-choices          : {
-  titles  :  FW.Xpath('//div[@class="hidden-xs col-sm-12"]/a[1]').text().trim(),
-  urls    :  FW.Xpath('//div[@class="hidden-xs col-sm-12"]/a[1]').key("href")
+
+function scrape(doc, url) {
+	var type = detectWeb(doc, url);
+	var item = new Zotero.Item(type);
+	item.publicationTitle = "Kirkus Reviews";
+	item.title = attr(doc,'meta[property="og:title"]','content').split('|')[0];
+	item.abstractNote = attr(doc,'meta[property="og:description"]','content');
+	item.language = "en-US";
+	item.url = url;
+	
+	if (type == "journalArticle") {
+		item.ISSN = "1948-7428";
+		item.date = attr(doc,'span[itemprop="datePublished"]','content');
+		// Process title
+		item.title = ZU.capitalizeTitle(item.title.toLowerCase(),true).replace (/^/,'Rev. of ').replace(/\s,/,",");
+		var reviewedauthorMetadata = doc.querySelectorAll('.book-author span[itemprop="name"]');
+		for (let author of reviewedauthorMetadata) {
+			item.creators.push(ZU.cleanAuthor(author.innerText, "reviewedAuthor"));
+		}
+	}
+	if (type == "blogPost") {
+		item.date = ZU.strToISO(text(doc,'.blog-post-teaser-meta-ctr').replace(/[\n\s]+/g," ").replace(/.*\son\s(.*)/,"$1"));
+		var blogauthorMetadata = text(doc,'.blog-post-teaser-meta-ctr').replace(/[\n\s]+/g," ").replace(/.*[Bb]y\s([\w\s]+),?.*\son\s.*/,"$1"); // http://regexr.com/3fhho
+		if (blogauthorMetadata) {
+			item.creators.push(ZU.cleanAuthor(blogauthorMetadata, "author"));
+		}
+	}
+
+	item.attachments.push({
+		title: "Kirkus Reviews snapshot",
+		mimeType: "text/html",
+		document: doc
+	});
+
+	item.complete();
 }
-});
 
+
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('.book-item-ctr span[itemprop="name"], .quilt-item-title a, .list-hero-ctr span[itemprop="name"], .blog-post-header');
+	var links = doc.querySelectorAll('.book-item-ctr a[itemprop="url"], .quilt-item-title a, .list-hero-ctr a[itemprop="url"], .blog-post-header a');
+	for (let i=0; i<rows.length; i++) {
+		let href = links[i].href;
+		let title = ZU.trimInternal(rows[i].textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
+
+
+function doWeb(doc, url) {
+	switch (detectWeb(doc, url)) {
+		case "multiple":
+			Zotero.selectItems(getSearchResults(doc, false), function (items) {
+				if (!items) {
+					return true;
+				}
+				var articles = [];
+				for (var i in items) {
+					articles.push(i);
+				}
+				ZU.processDocuments(articles, scrape);
+			});
+			break;
+		default:
+			scrape(doc, url);
+			break;
+	}
+}
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
@@ -99,13 +134,8 @@ var testCases = [
 			{
 				"itemType": "journalArticle",
 				"title": "Rev. of Like a Conquered Province by Paul Goodman",
-				"creators": [
-					{
-						"firstName": "Paul",
-						"lastName": "Goodman",
-						"creatorType": "reviewedAuthor"
-					}
-				],
+				"creators": [],
+				"ISSN": "1948-7428",
 				"abstractNote": "As a dissenting David to the Goliath of stultifying centralization, Paul Goodman has become a campus hero and a Washington bugaboo.",
 				"language": "en-US",
 				"libraryCatalog": "Kirkus Reviews",
@@ -143,6 +173,7 @@ var testCases = [
 					}
 				],
 				"date": "2012-05-30",
+				"ISSN": "1948-7428",
 				"abstractNote": "The Big Something doesn't end up amounting to much in this lackluster beginning reader.",
 				"language": "en-US",
 				"libraryCatalog": "Kirkus Reviews",
@@ -185,6 +216,7 @@ var testCases = [
 					}
 				],
 				"date": "2017-03-15",
+				"ISSN": "1948-7428",
 				"abstractNote": "A debut collection of essays covering the scope of modern literature, along with a quiet plea for its violent overthrow.",
 				"language": "en-US",
 				"libraryCatalog": "Kirkus Reviews",
@@ -213,10 +245,10 @@ var testCases = [
 					{
 						"firstName": "Megan",
 						"lastName": "Labrise",
-						"creatorType": "reviewedAuthor"
+						"creatorType": "author"
 					}
 				],
-				"date": "March 9, 2017",
+				"date": "2017-03-09",
 				"abstractNote": "Kirkus Reviews talks to Julianne Pachico about her novel, The Lucky Ones.",
 				"blogTitle": "Kirkus Reviews",
 				"language": "en-US",
@@ -244,10 +276,10 @@ var testCases = [
 					{
 						"firstName": "Laurie",
 						"lastName": "Muchnick",
-						"creatorType": "reviewedAuthor"
+						"creatorType": "author"
 					}
 				],
-				"date": "March 9, 2017",
+				"date": "2017-03-09",
 				"abstractNote": "Kirkus Reviews' fiction editor Laurie Muchnick spotlights Judith Flanders' Samantha Clair mystery series.",
 				"blogTitle": "Kirkus Reviews",
 				"language": "en-US",
@@ -272,6 +304,11 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "https://www.kirkusreviews.com/features/interviews/",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.kirkusreviews.com/search/?q=goodman",
 		"items": "multiple"
 	}
 ]
