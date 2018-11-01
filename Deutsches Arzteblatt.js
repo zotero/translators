4860ee8ef6d43d43d243a25684054e7b6d101d2d
Sebastian Fleer <pubalias@posteo.net>
2018-11-01 17:29:48 +0100

    Add AGPLv3 block

--- Deutsches Arzteblatt.js
+++ Deutsches Arzteblatt.js
@@ -12,6 +12,29 @@
 	"lastUpdated": "2018-11-01 15:42:33"
 }
 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2018 Sebastian Fleer
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
 function detectWeb(doc, url) {
 	if (url.match(/archiv\/\d+/) || url.match(/aid\=\d+/)) {
 		return "journalArticle";
