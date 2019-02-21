ea0357d1d244bbe11a0f9e1d58d160374c77e415
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-02-21 13:29:08 +0100

    Atypon Journals: save secondary abstracts as extra data

--- Atypon Journals.js
+++ Atypon Journals.js
@@ -237,12 +237,15 @@ function scrape(doc, url, extras) {
 					item.tags.push(tags[i].textContent);
 				}
 
+				// save the first abstract to the expected field and the others in the notes field for later processing
 				var abstracts = ZU.xpath(doc, '//div[@class="abstractSection"]/p/span');
 				if (abstracts) {
-					item.abstractNote = "";
-					for (var entry in abstracts)
-						item.abstractNote += abstracts[entry].textContent + "\n\n";
-
+					item.abstractNote = abstracts[0].textContent;
+					for (var i = 1; i < abstracts.length; ++i) {
+						item.notes.push({
+							note: "abs:" + abstracts[i].textContent,
+						});
+					}
 					item.abstractNote.trim();
 				}
 
