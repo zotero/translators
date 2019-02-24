f692524bb861d55c8ac45b0f130d891fa49c97c6
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-02-24 02:43:11 +0100

    processors runs first, so show first

--- .ci/eslint-plugin-zotero-translator/index.js
+++ .ci/eslint-plugin-zotero-translator/index.js
@@ -4,10 +4,6 @@
  */
 "use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
 const fs = require('fs');
 const path = require('path');
 
@@ -21,13 +17,46 @@ const deleted = new Set(
 		.filter(id => id && id.indexOf('-') > 0)
 );
 
+module.exports.processors = {
+	'.js': {
+		preprocess: function(text, filename) {
+			filename = path.resolve(filename);
+	
+			headers[filename] = {
+				// capture header
+				raw: text.replace(/\n}\n[\S\s]*/, '\n}'),
+			};
+	
+			try {
+				headers[filename].parsed = JSON.parse(headers[filename].raw);
+	
+			} catch (err) {
+				headers[filename].error = err.message; // leave for rules to pick up
+			}
+	
+			text = `var ${header_var_name} = ` // assign header to variable to make valid JS
+				+ text
+					.replace('\n', ' // eslint-disable-line no-unused-vars \n') // prevent eslint warnings about this variable being unused
+					.replace('\n}\n', '\n};\n'); // add a semicolon after the header to pacify eslint
+	
+			// fs.writeFileSync(header_var_name + '.js', text, 'utf-8')
+	
+			return [text];
+		},
+	
+		// takes a Message[][] and filename
+		postprocess: function(messages, filename) {
+			return messages[0].sort((a, b) => {
+				const la = a.line || 0;
+				const lb = b.line || 0;
+	
+				return (la !== lb) ? la - lb : a.ruleId.localeCompare(b.ruleId);
+			});
+		},
+	},
+};
+	
 
-//------------------------------------------------------------------------------
-// Plugin Definition
-//------------------------------------------------------------------------------
-
-
-// import all rules in lib/rules
 module.exports.rules = {
 	'header-valid-json': function(context) {
 		return {
@@ -150,43 +179,3 @@ module.exports.rules = {
 		};
 	},
 };
-
-module.exports.processors = {
-	'.js': {
-		preprocess: function(text, filename) {
-			filename = path.resolve(filename);
-	
-			headers[filename] = {
-				// capture header
-				raw: text.replace(/\n}\n[\S\s]*/, '\n}'),
-			};
-	
-			try {
-				headers[filename].parsed = JSON.parse(headers[filename].raw);
-	
-			} catch (err) {
-				headers[filename].error = err.message; // leave for rules to pick up
-			}
-	
-			text = `var ${header_var_name} = ` // assign header to variable to make valid JS
-				+ text
-					.replace('\n', ' // eslint-disable-line no-unused-vars \n') // prevent eslint warnings about this variable being unused
-					.replace('\n}\n', '\n};\n'); // add a semicolon after the header to pacify eslint
-	
-			// fs.writeFileSync(header_var_name + '.js', text, 'utf-8')
-	
-			return [text];
-		},
-	
-		// takes a Message[][] and filename
-		postprocess: function(messages, filename) {
-			return messages[0].sort((a, b) => {
-				const la = a.line || 0;
-				const lb = b.line || 0;
-	
-				return (la !== lb) ? la - lb : a.ruleId.localeCompare(b.ruleId);
-			});
-		},
-	},
-};
-	
