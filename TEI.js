0136f68bf56f494c151a70f42c6ab1be09755eb8
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-03-07 15:36:25 +0100

    don't relicense

--- TEI.js
+++ TEI.js
@@ -27,46 +27,25 @@
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2019 Stefan Majewski
+	tei-zotero-translator. Zotero 2 to TEI P5 exporter.
 
-	This file is part of Zotero.
+	Copyright (C) 2010 Stefan Majewski <xml@stefanmajewski.eu>
 
-	Zotero is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published by
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	Zotero is distributed in the hope that it will be useful,
+	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU Affero General Public License for more details.
+	GNU General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 	***** END LICENSE BLOCK *****
 */
-
-
-// ********************************************************************
-//
-// tei-zotero-translator. Zotero 2 to TEI P5 exporter.
-//
-// Copyright (C) 2010 Stefan Majewski <xml@stefanmajewski.eu>
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 
 // *********************************************************************
 //
