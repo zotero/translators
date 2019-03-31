a9755824de257839c557bccffe34fd64ba417d15
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-03-31 16:15:59 +0200

    add eslint-fixer for common import-test errors

--- .ci/eslint-plugin-zotero-translator/lib/rules/test-cases.js
+++ .ci/eslint-plugin-zotero-translator/lib/rules/test-cases.js
@@ -1,6 +1,95 @@
 'use strict';
 
 const translators = require('../translators').cache;
+const astring = require('astring');
+
+function verifyWeb(context, astNode, tc, prefix) {
+	if (tc.items !== 'multiple' && !Array.isArray(tc.items)) {
+		context.report(astNode, `${prefix} needs items`);
+	}
+
+	if (typeof tc.url !== 'string') {
+		context.report(astNode, `${prefix} test needs url`);
+	}
+}
+
+function verifyImport(context, astNode, tc, prefix) {
+	if (!Array.isArray(tc.items)) {
+		context.report(astNode, `${prefix} needs items`);
+	}
+
+	if (typeof tc.input !== 'string') {
+		context.report(astNode, `${prefix} needs a string input`);
+	}
+
+	const items = astNode.properties.find(prop => prop.key.value === 'items').value.elements;
+	for (const itemNode of items) {
+		if (itemNode.type !== 'ObjectExpression') {
+			context.report(itemNode, `${prefix} item must be an object`);
+			continue;
+		}
+
+		const item = JSON.parse(astring.generate(itemNode));
+		const messages = [];
+		for (const prop of ['tags', 'notes', 'attachments', 'seeAlso']) {
+			if (item[prop] && !item[prop].length) {
+				messages.push(`${prefix} remove empty ${prop}`);
+				delete item[prop];
+			}
+		}
+
+		for (const note of (item.notes || [])) {
+			if (note.itemType !== 'note') {
+				messages.push(`${prefix} note must have itemType "note"`);
+				note.itemType = 'note';
+			}
+		}
+
+		for (const creator of (item.creators || [])) {
+			if (creator.fieldMode === 1) {
+				messages.push(`${prefix} fieldMode is obsolete, use "name" to indicate single-field names`);
+				creator.name = creator.lastName;
+				delete creator.lastName;
+				delete creator.fieldMode;
+			}
+		}
+
+		if (item.tags) {
+			item.tags = item.tags.map(function (tag) {
+				if (typeof tag === 'string') {
+					messages.push(`${prefix} tag must be an object, not a string`);
+					return { tag, type: 1 };
+				}
+				else if (typeof tag.type !== 'number') {
+					messages.push(`${prefix} missing "type" on tag`);
+					return { ...tag, type: 1 };
+				}
+				return tag;
+			});
+		}
+
+		// this is a bit cheaty, but I don't feel like walking the entire AST on this, so fix all reported issues in one go
+		let fix = function (fixer) {
+			return fixer.replaceText(itemNode, JSON.stringify(item, null, '\t'));
+		};
+		for (const message of messages) {
+			context.report({ node: itemNode, message, fix });
+			fix = undefined; // the first fixer will take care of all of them at once
+		}
+	}
+}
+
+function verifySearch(context, astNode, tc, prefix) {
+	if (!Array.isArray(tc.items)) {
+		context.report(astNode, `${prefix} needs items`);
+	}
+
+	const term = Object.keys(tc.input || {}).join('/');
+	const expected = ['DOI', 'ISBN', 'PMID', 'identifiers', 'contextObject'];
+	if (!expected.includes(term)) {
+		context.report(astNode, `${prefix} has search term '${term}', expected one of ${expected.join(', ')}`);
+	}
+}
 
 module.exports = {
 	meta: {
@@ -10,6 +99,7 @@ module.exports = {
 			description: 'disallow invalid test input',
 			category: 'Possible Errors',
 		},
+		fixable:	'code',
 	},
 
 	create: function (context) {
@@ -26,56 +116,28 @@ module.exports = {
 
 				if (!translator.testCases || translator.testCases.error) return; // regular js or no test cases
 
-				let caseNo = -1;
-				for (const testCase of translator.testCases.parsed) {
-					caseNo += 1;
-					const prefix = `test case${testCases[caseNo] ? '' : ' ' + (caseNo + 1)}`;
-					const loc = testCases[caseNo] ? testCases[caseNo].loc.start : { start: { line: translator.testCases.start, column: 1 } };
-
-					if (!['web', 'import', 'search'].includes(testCase.type)) {
-						context.report({
-							message: `${prefix} has invalid type "${testCase.type}"`,
-							loc,
-						});
+				for (const astNode of testCases) {
+					if (astNode.type !== 'ObjectExpression') {
+						context.report(astNode, `test case is not a test case object`);
 						continue;
 					}
 
-					if (!(Array.isArray(testCase.items) || (testCase.type === 'web' && testCase.items === 'multiple'))) {
-						context.report({
-							message: `${prefix} of type "${testCase.type}" needs items`,
-							loc,
-						});
-					}
+					const testCase = JSON.parse(astring.generate(astNode));
+					const prefix = `${testCase.type || 'unspecified'} test case`;
 
-					if (testCase.type === 'web' && typeof testCase.url !== 'string') {
-						context.report({
-							message: `${prefix} of type "${testCase.type}" test needs url`,
-							loc,
-						});
-					}
-
-					if (['import', 'search'].includes(testCase.type) && !testCase.input) {
-						context.report({
-							message: `${prefix} of type "${testCase.type}" needs a string input`,
-							loc,
-						});
-					}
-					else if (testCase.type === 'import' && typeof testCase.input !== 'string') {
-						context.report({
-							message: `${prefix} of type "${testCase.type}" needs input`,
-							loc,
-						});
-					}
-					else if (testCase.type === 'search') {
-						// console.log(JSON.stringify(testCase.input))
-						const term = Object.keys(testCase.input).join('/');
-						const expected = ['DOI', 'ISBN', 'PMID', 'identifiers', 'contextObject'];
-						if (!expected.includes(term)) {
-							context.report({
-								message: `${prefix} of type "${testCase.type}" has search term '${term}', expected one of ${expected.join(', ')}`,
-								loc,
-							});
-						}
+					switch (testCase.type) {
+					case 'web':
+						verifyWeb(context, astNode, testCase, prefix);
+						break;
+					case 'import':
+						verifyImport(context, astNode, testCase, prefix);
+						break;
+					case 'search':
+						verifySearch(context, astNode, testCase, prefix);
+						break;
+					default:
+						context.report(astNode, `${prefix} has invalid type "${testCase.type}"`);
+						break;
 					}
 				}
 			}
