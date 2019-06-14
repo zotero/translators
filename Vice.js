accd98b36a7380fc09464ea77826248ef58111e1
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-06-14 11:45:49 +0200

    remove text/attr

--- Vice.js
+++ Vice.js
@@ -37,19 +37,6 @@
 */
 
 
-// attr()/text() v2 per https://github.com/zotero/translators/issues/1277
-function attr(docOrElem, selector, attr, index) {
-	var elem = index ? docOrElem.querySelectorAll(selector).item(index) : docOrElem.querySelector(selector);
-	return elem ? elem.getAttribute(attr) : null;
-}
-
-/*
-function text(docOrElem, selector, index) {
-	var elem = index ? docOrElem.querySelectorAll(selector).item(index) : docOrElem.querySelector(selector);
-  return elem ? elem.textContent : null;
-}
-*/
-
 function scrubLowercaseTags(tags) {
 	for (let tag of tags) {
 		if (tag == tag.toLowerCase()) {
