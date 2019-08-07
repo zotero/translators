e3e133a0f896c49d6a22445c860122b257d956e7
Philipp Zumstein <zuphilip@gmail.com>
2019-08-07 13:10:01 +0200

    :art: Fix linting in Amazon.js

--- Amazon.js
+++ Amazon.js
@@ -19,51 +19,55 @@ function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelec
 function detectWeb(doc, url) {
 	if (getSearchResults(doc, true)) {
 		return (Zotero.isBookmarklet ? "server" : "multiple");
-	} else {
-		if (attr(doc, 'input[name*="ASIN"]', 'value')) {
-			if (Zotero.isBookmarklet) return "server";
+	}
+	else if (attr(doc, 'input[name*="ASIN"]', 'value')) {
+		if (Zotero.isBookmarklet) return "server";
 			
-			var storeID = attr(doc, 'input[name="storeID"]', 'value');
-			if (storeID && storeID == "gateway") {
-				storeID = attr(doc, 'div[id="dp"]', 'class');
-				// delete language code
-				storeID = storeID.replace(/[a-z][a-z]_[A-Z][A-Z]/, "").trim();
+		var storeID = attr(doc, 'input[name="storeID"]', 'value');
+		if (storeID && storeID == "gateway") {
+			storeID = attr(doc, 'div[id="dp"]', 'class');
+			// delete language code
+			storeID = storeID.replace(/[a-z][a-z]_[A-Z][A-Z]/, "").trim();
+		}
+		if (storeID) {
+			if (storeID.includes("book")) {
+				return "book";
 			}
-			if (storeID) {
-				if (storeID.indexOf("book")>-1) {
-					return "book";
-				} else if (storeID=="music"|storeID=="dmusic"){
-					return "audioRecording";
-				} else if (storeID=="dvd"|storeID=="dvd-de"|storeID=="video"|storeID=="movies-tv"){
-					return "videoRecording";
-				} else if (storeID=="videogames"|storeID=="mobile-apps") {
-					return "computerProgram";
-				} else {
-					Z.debug("Items in this store will be ignored by Zotero: " + storeID);
-				}
-			} else {
-				//audio books are purchased as audible abo
-				if (text(doc, 'form[class="a-spacing-none"][action*="/audible/"]')) {
-					return "audioRecording";
-				}
-				var mainCategory = text(doc, '#wayfinding-breadcrumbs_container li a');
-				if (mainCategory && mainCategory.indexOf('Kindle')>-1) {
-					return "book";
-				} else {
-					Z.debug("Items in this category will be ignored by Zotero: " + mainCategory);
-				}
-				
+			else if (storeID == "music" | storeID == "dmusic") {
+				return "audioRecording";
+			}
+			else if (storeID == "dvd" | storeID == "dvd-de" | storeID == "video" | storeID == "movies-tv") {
+				return "videoRecording";
+			}
+			else if (storeID == "videogames" | storeID == "mobile-apps") {
+				return "computerProgram";
+			}
+			else {
+				Z.debug("Items in this store will be ignored by Zotero: " + storeID);
+			}
+		}
+		else {
+			// audio books are purchased as audible abo
+			if (text(doc, 'form[class="a-spacing-none"][action*="/audible/"]')) {
+				return "audioRecording";
+			}
+			var mainCategory = text(doc, '#wayfinding-breadcrumbs_container li a');
+			if (mainCategory && mainCategory.includes('Kindle')) {
+				return "book";
+			}
+			else {
+				Z.debug("Items in this category will be ignored by Zotero: " + mainCategory);
 			}
 		}
 	}
+	return false;
 }
 
 function getSearchResults(doc, checkOnly) {
-	//search results
+	// search results
 	var links = [],
 		container = doc.getElementById('searchTemplate');
 	if (container) {
-		
 		links = container.getElementsByClassName('s-access-detail-page');
 		if (!links.length) {
 			links = ZU.xpath(container, './/div[contains(@class,"results")]/div[starts-with(@id,"result_")]//h3/a');
@@ -71,7 +75,7 @@ function getSearchResults(doc, checkOnly) {
 	}
 	
 	if (!links.length) {
-		//wish lists
+		// wish lists
 		container = doc.getElementById('item-page-wrapper');
 		if (container) {
 			links = ZU.xpath(container, './/a[starts-with(@id, "itemName_")]');
@@ -79,7 +83,7 @@ function getSearchResults(doc, checkOnly) {
 	}
 	
 	if (!links.length) {
-		//author pages
+		// author pages
 		container = doc.getElementById('mainResults');
 		if (container) {
 			links = ZU.xpath(container, './/li[starts-with(@id, "result_")]//a[h2]');
@@ -89,7 +93,7 @@ function getSearchResults(doc, checkOnly) {
 	if (!links.length) return false;
 	var availableItems = {}, found = false,
 		asinRe = /\/(?:dp|product)\/(?:[^?#]+)\//;
-	for (var i=0; i<links.length; i++) {
+	for (var i = 0; i < links.length; i++) {
 		var elmt = links[i];
 		if (asinRe.test(elmt.href)) {
 			if (checkOnly) return true;
@@ -103,33 +107,33 @@ function getSearchResults(doc, checkOnly) {
 
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) == 'multiple') {
-		Zotero.selectItems(getSearchResults(doc), function(items) {
-			if (!items) return true;
+		Zotero.selectItems(getSearchResults(doc), function (items) {
+			if (!items) return;
 			
 			var links = [];
 			for (var i in items) links.push(i);
 			Zotero.Utilities.processDocuments(links, scrape);
 		});
-
-	} else {
+	}
+	else {
 		scrape(doc, url);
 	}
 }
 
 function addLink(doc, item) {
-	item.attachments.push({title:"Amazon.com Link", snapshot:false, mimeType:"text/html", url:doc.location.href});
+	item.attachments.push({ title: "Amazon.com Link", snapshot: false, mimeType: "text/html", url: doc.location.href });
 }
 
 
 var CREATOR = {
-	"Actor":"castMember",
-	"Director":"director",
-	"Producer":"producer",
-	"Writer":"scriptwriter",
-	"Translator":"translator",
-	"Author":"author",
-	"Illustrator":"contributor",
-	"Editor": "editor"
+	Actor: "castMember",
+	Director: "director",
+	Producer: "producer",
+	Writer: "scriptwriter",
+	Translator: "translator",
+	Author: "author",
+	Illustrator: "contributor",
+	Editor: "editor"
 };
 
 var DATE = [
@@ -137,51 +141,53 @@ var DATE = [
 	"DVD Release Date"
 ];
 
-//localization
+// localization
 var i15dFields = {
-	'ISBN' : ['ISBN-13', 'ISBN-10', 'ISBN', '条形码'],
-	'Publisher': ['Publisher', 'Verlag', '出版社'],
-	'Hardcover': ['Hardcover', 'Gebundene Ausgabe', '精装', 'ハードカバー', 'Relié', 'Copertina rigida', 'Tapa dura'],
-	'Paperback' : ['Paperback', 'Taschenbuch', '平装', 'ペーパーバック', 'Broché', 'Copertina flessibile', 'Tapa blanda'],
-	'Print Length' : ['Print Length', 'Seitenzahl der Print-Ausgabe', '紙の本の長さ', "Nombre de pages de l'édition imprimée", "Longueur d'impression", 'Poche', 'Broché', 'Lunghezza stampa', 'Longitud de impresión', 'Número de páginas'],//TODO: Chinese label
-	'Language' : ['Language', 'Sprache', '语种', '言語', 'Langue', 'Lingua', 'Idioma'],
-	'Author' : ['Author', '著', '作者'],
-	'Actor' : ['Actors', 'Actor', 'Darsteller', 'Acteurs', 'Attori', 'Attore', 'Actores', '出演'],
-	'Director' : ['Directors', 'Director', 'Regisseur', 'Regisseur(e)', 'Réalisateurs', 'Regista', 'Directores', '監督'],
-	'Producer' : ['Producers', 'Producer'],
-	'Run Time' : ['Run Time', 'Spieldauer', 'Durée', 'Durata', 'Duración', '時間'],
-	'Studio' : ['Studio', 'Estudio', '販売元'],
-	'Audio CD' : ['Audio CD', 'CD', 'CD de audio'],
-	'Label' : ['Label', 'Etichetta', 'Étiquette', 'Sello', '发行公司', 'レーベル'],
-	'Total Length' : ['Total Length', 'Gesamtlänge', 'Durée totale', 'Lunghezza totale', 'Duración total', '収録時間'],
-	'Translator' : ["Translator", "Übersetzer", "Traduttore", "Traductor", "翻訳"],
-	'Illustrator' : ["Illustrator", "Illustratore", "Ilustrador", "イラスト"],
-	'Writer' : ['Writers'],
-	'Editor' : ['Editor', 'Editora', 'Editeur', 'Éditeur', 'Editore']
+	ISBN: ['ISBN-13', 'ISBN-10', 'ISBN', '条形码'],
+	Publisher: ['Publisher', 'Verlag', '出版社'],
+	Hardcover: ['Hardcover', 'Gebundene Ausgabe', '精装', 'ハードカバー', 'Relié', 'Copertina rigida', 'Tapa dura'],
+	Paperback: ['Paperback', 'Taschenbuch', '平装', 'ペーパーバック', 'Broché', 'Copertina flessibile', 'Tapa blanda'],
+	'Print Length': ['Print Length', 'Seitenzahl der Print-Ausgabe', '紙の本の長さ', "Nombre de pages de l'édition imprimée", "Longueur d'impression", 'Poche', 'Broché', 'Lunghezza stampa', 'Longitud de impresión', 'Número de páginas'], // TODO: Chinese label
+	Language: ['Language', 'Sprache', '语种', '言語', 'Langue', 'Lingua', 'Idioma'],
+	Author: ['Author', '著', '作者'],
+	Actor: ['Actors', 'Actor', 'Darsteller', 'Acteurs', 'Attori', 'Attore', 'Actores', '出演'],
+	Director: ['Directors', 'Director', 'Regisseur', 'Regisseur(e)', 'Réalisateurs', 'Regista', 'Directores', '監督'],
+	Producer: ['Producers', 'Producer'],
+	'Run Time': ['Run Time', 'Spieldauer', 'Durée', 'Durata', 'Duración', '時間'],
+	Studio: ['Studio', 'Estudio', '販売元'],
+	'Audio CD': ['Audio CD', 'CD', 'CD de audio'],
+	Label: ['Label', 'Etichetta', 'Étiquette', 'Sello', '发行公司', 'レーベル'],
+	'Total Length': ['Total Length', 'Gesamtlänge', 'Durée totale', 'Lunghezza totale', 'Duración total', '収録時間'],
+	Translator: ["Translator", "Übersetzer", "Traduttore", "Traductor", "翻訳"],
+	Illustrator: ["Illustrator", "Illustratore", "Ilustrador", "イラスト"],
+	Writer: ['Writers'],
+	Editor: ['Editor', 'Editora', 'Editeur', 'Éditeur', 'Editore']
 };
 
 function getField(info, field) {
-	//returns the value for the key 'field' or any of its
-	//corresponding (language specific) keys of the array 'info'
+	// returns the value for the key 'field' or any of its
+	// corresponding (language specific) keys of the array 'info'
 	
-	if (!i15dFields[field]) return;
+	if (!i15dFields[field]) return false;
 	
-	for (var i=0; i<i15dFields[field].length; i++) {
-		if (info[i15dFields[field][i]] !== undefined) return info[i15dFields[field][i]];	
+	for (var i = 0; i < i15dFields[field].length; i++) {
+		if (info[i15dFields[field][i]] !== undefined) return info[i15dFields[field][i]];
 	}
+	return false;
 }
 
 function translateField(str) {
 	for (var f in i15dFields) {
-		if (i15dFields[f].indexOf(str) != -1) {
+		if (i15dFields[f].includes(str)) {
 			return f;
 		}
 	}
+	return false;
 }
 
 
 function scrape(doc, url) {
-	var isAsian = url.search(/^https?:\/\/[^\/]+\.(?:jp|cn)[:\/]/) != -1;
+	var isAsian = url.search(/^https?:\/\/[^/]+\.(?:jp|cn)[:/]/) != -1;
 	// Scrape HTML for items without ISBNs, because Amazon doesn't provide an easy way for
 	// open source projects like us to use their API
 	Z.debug("Scraping from Page");
@@ -194,14 +200,14 @@ function scrape(doc, url) {
 		|| doc.getElementById('title_feature_div');
 	// get first non-empty text node (other text nodes are things like [Paperback] and dates)
 	item.title = ZU.trimInternal(
-			ZU.xpathText(title, '(.//text()[normalize-space(self::text())])[1]')
-		)
+		ZU.xpathText(title, '(.//text()[normalize-space(self::text())])[1]')
+	)
 		// though sometimes [Paperback] or [DVD] is mushed with the title...
-		.replace(/(?: [(\[].+[)\]])+$/, "");
+		.replace(/(?: [([].+[)\]])+$/, "");
 	
 	var baseNode = title.parentElement, bncl;
-	while (baseNode && (bncl = baseNode.classList) && 
-		!(// ways to identify a node encompasing title and authors
+	while (baseNode && (bncl = baseNode.classList)
+		&& !(// ways to identify a node encompasing title and authors
 			baseNode.id == 'booksTitle'
 			|| baseNode.id == 'ppd-center'
 			|| baseNode.id == 'title_feature_div'
@@ -216,7 +222,7 @@ function scrape(doc, url) {
 	
 	if (baseNode) {
 		var authors = ZU.xpath(baseNode, './/span[@id="artistBlurb"]/a');
-		//if (!authors.length) authors = baseNode.getElementsByClassName('contributorNameID');
+		// if (!authors.length) authors = baseNode.getElementsByClassName('contributorNameID');
 		if (!authors.length) authors = ZU.xpath(baseNode, '(.//*[@id="byline"]/span[contains(@class, "author")] | .//*[@id="byline"]/span[contains(@class, "author")]/span)/a[contains(@class, "a-link-normal")][1]');
 		if (!authors.length) authors = ZU.xpath(baseNode, './/span[@class="contributorNameTrigger"]/a[not(@href="#")]');
 		if (!authors.length) authors = ZU.xpath(baseNode, './/span[contains(@class, "author")]/a|.//span[contains(@class, "author")]/span/a');
@@ -224,7 +230,7 @@ function scrape(doc, url) {
 		if (!authors.length) authors = ZU.xpath(baseNode, './/a[contains(@href, "field-author=")]');
 		if (!authors.length) authors = ZU.xpath(baseNode, './/a[@id="ProductInfoArtistLink"]');
 		if (!authors.length) authors = ZU.xpath(baseNode, './/a[@id="ProductInfoArtistLink"]');
-		for (var i=0; i<authors.length; i++) {
+		for (let i = 0; i < authors.length; i++) {
 			var role = ZU.xpathText(authors[i], '(.//following::text()[normalize-space(self::text())])[1]');
 			if (role) {
 				role = CREATOR[translateField(
@@ -244,11 +250,12 @@ function scrape(doc, url) {
 					creatorType: 'performer',
 					fieldMode: 1
 				});
-			} else {
+			}
+			else {
 				var invertName = isAsian && !(/[A-Za-z]/.test(name));
 				if (invertName) {
 					// Use last character as given name if there is no space
-					if (name.indexOf(' ') == -1) name = name.replace(/.$/, ' $&');
+					if (!name.includes(' ')) name = name.replace(/.$/, ' $&');
 					name = name.replace(/\s+/, ', '); // Surname comes first
 				}
 				item.creators.push(ZU.cleanAuthor(name, role, invertName));
@@ -256,7 +263,7 @@ function scrape(doc, url) {
 		}
 	}
 	
-	//Abstract
+	// Abstract
 	var abstractNode = doc.getElementById('postBodyPS');
 	if (abstractNode) {
 		item.abstractNote = abstractNode.textContent.trim();
@@ -273,18 +280,19 @@ function scrape(doc, url) {
 	var info = {},
 		els = ZU.xpath(doc, '//div[@class="content"]/ul/li[b]');
 	if (els.length) {
-		for (var i=0; i<els.length; i++) {
-			var el = els[i],
+		for (let i = 0; i < els.length; i++) {
+			let el = els[i],
 				key = ZU.xpathText(el, 'b[1]').trim();
 			if (key) {
-				info[key.replace(/\s*:$/, "")] = el.textContent.substr(key.length+1).trim();
+				info[key.replace(/\s*:$/, "")] = el.textContent.substr(key.length + 1).trim();
 			}
 		}
-	} else {
+	}
+	else {
 		// New design encountered 06/30/2013
 		els = ZU.xpath(doc, '//tr[td[@class="a-span3"]][td[@class="a-span9"]]');
-		for (var i=0; i<els.length; i++) {
-			var el = els[i],
+		for (let i = 0; i < els.length; i++) {
+			let el = els[i],
 				key = ZU.xpathText(el, 'td[@class="a-span3"]'),
 				value = ZU.xpathText(el, 'td[@class="a-span9"]');
 			if (key && value) info[key.trim()] = value.trim();
@@ -297,13 +305,13 @@ function scrape(doc, url) {
 	}
 	
 	// Date
-	for (var i=0; i<DATE.length; i++) {
+	for (let i = 0; i < DATE.length; i++) {
 		item.date = info[DATE[i]];
 		if (item.date) break;
 	}
 	if (!item.date) {
-		for (var i in info) {
-			var m = /\(([^)]+ [0-9]{4})\)/.exec(info[i]);
+		for (let i in info) {
+			let m = /\(([^)]+ [0-9]{4})\)/.exec(info[i]);
 			if (m) item.date = m[1];
 		}
 	}
@@ -317,9 +325,9 @@ function scrape(doc, url) {
 		if (m[3] && m[3].search(/\b\d{4}\b/) != -1) item.date = m[3].trim(); // Looks like a date
 	}
 	var pages = getField(info, 'Hardcover') || getField(info, 'Paperback') || getField(info, 'Print Length');
-	if (pages) item.numPages = parseInt(pages, 10);
+	if (pages) item.numPages = parseInt(pages);
 	item.language = getField(info, 'Language');
-	//add publication place from ISBN translator, see at the end
+	// add publication place from ISBN translator, see at the end
 	
 	// Video
 	if (item.itemType == 'videoRecording') {
@@ -332,7 +340,7 @@ function scrape(doc, url) {
 					clearedCreators = true;
 				}
 				var creators = getField(info, i).split(/ *, */);
-				for (var j=0; j<creators.length; j++) {
+				for (var j = 0; j < creators.length; j++) {
 					item.creators.push(ZU.cleanAuthor(creators[j], CREATOR[i]));
 				}
 			}
@@ -347,24 +355,25 @@ function scrape(doc, url) {
 	var department = ZU.xpathText(doc, '//li[contains(@class, "nav-category-button")]/a');
 	if (getField(info, 'Audio CD')) {
 		item.audioRecordingFormat = "Audio CD";
-	} else if (department && department.trim() == "Amazon MP3 Store") {
+	}
+	else if (department && department.trim() == "Amazon MP3 Store") {
 		item.audioRecordingFormat = "MP3";
 	}
 	
 	addLink(doc, item);
 	
-	//we search for translators for a given ISBN
-	//and try to figure out the missing publication place
+	// we search for translators for a given ISBN
+	// and try to figure out the missing publication place
 	if (item.ISBN && !item.place) {
 		Z.debug("Searching for additional metadata by ISBN: " + item.ISBN);
 		var search = Zotero.loadTranslator("search");
-		search.setHandler("translators", function(obj, translators) {
+		search.setHandler("translators", function (obj, translators) {
 			search.setTranslator(translators);
-			search.setHandler("itemDone", function(obj, lookupItem) {
+			search.setHandler("itemDone", function (obj, lookupItem) {
 				Z.debug(lookupItem.libraryCatalog);
 				if (lookupItem.place) {
-					//e.g. [Paris]
-					item.place = lookupItem.place.replace("[","").replace("]","");
+					// e.g. [Paris]
+					item.place = lookupItem.place.replace("[", "").replace("]", "");
 				}
 				
 				if (!item.date && lookupItem.date) {
@@ -373,20 +382,19 @@ function scrape(doc, url) {
 			});
 			search.translate();
 		});
-		search.setHandler("error", function(error) {
+		search.setHandler("error", function (error) {
 			// we mostly need this handler to prevent the default one from kicking in
 			Z.debug("ISBN search for " + item.ISBN + " failed: " + error);
 		});
-		search.setHandler("done", function() {
+		search.setHandler("done", function () {
 			item.complete();
 		});
 		search.setSearch({ ISBN: item.ISBN });
 		search.getTranslators();
-		
-	} else {
+	}
+	else {
 		item.complete();
 	}
-	
 }
 
 
