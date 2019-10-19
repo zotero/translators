ac4c7bda2dae53945bab2db0d172d8ccde19145d
Philipp Zumstein <zuphilip@gmail.com>
2019-10-19 16:39:19 +0200

    :art: Fix linting issues in Springer Books.js

--- Springer Books.js
+++ Springer Books.js
@@ -37,29 +37,30 @@
 
 
 function detectWeb(doc, url) {
-	var action = url.match(/^https?:\/\/[^\/]+\/[^\/]+\/([^\/?#]+)/);
-	if (!action) return;
+	var action = url.match(/^https?:\/\/[^/]+\/[^/]+\/([^/?#]+)/);
+	if (!action) return false;
 	switch (action[1]) {
 		case "book":
 			// test if any relevant <meta> information is available
 			if (ZU.xpathText(doc, '//meta[@property="og:title"]/@content')) return "book";
 			break;
 		case "search":
-			//test for relevant search entries
+			// test for relevant search entries
 			return getSearchResults(doc, true) ? "multiple" : false;
 	}
+	return false;
 }
 
 
 function doWeb(doc, url) {
-	var detection_type = detectWeb(doc, url);
-	switch (detection_type) {
+	var detectionType = detectWeb(doc, url);
+	switch (detectionType) {
 		case "book":
 			scrapeBook(doc, url);
 			break;
 		case "multiple":
 			Zotero.selectItems(getSearchResults(doc, false), function (items) {
-				if (!items) return true;
+				if (!items) return;
 				var articles = [];
 				for (var i in items) {
 					articles.push(i);
@@ -68,24 +69,23 @@ function doWeb(doc, url) {
 			});
 			break;
 	}
-	
 }
 
 
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	var search_entries = ZU.xpath(doc, '//div[@id="result-list"]/div');
-	for (var i=0; i<search_entries.length; i++) {
-		var item_type = search_entries[i].getAttribute("class");
-		//test if we actually have a usable result
-		if (!item_type || item_type.search(/result-item-\d+/)==-1
-			|| !item_type.includes('result-type-book')) continue;
-		var title_link = ZU.xpath(search_entries[i], "h4/a");
-		if (title_link.length === 0) continue;
-		//extract book title and URL
-		var title = title_link[0].textContent;
-		var href = title_link[0].getAttribute("href");
+	var searchEntries = ZU.xpath(doc, '//div[@id="result-list"]/div');
+	for (let i = 0; i < searchEntries.length; i++) {
+		var itemType = searchEntries[i].getAttribute("class");
+		// test if we actually have a usable result
+		if (!itemType || itemType.search(/result-item-\d+/) == -1
+			|| !itemType.includes('result-type-book')) continue;
+		var titleLink = ZU.xpath(searchEntries[i], "h4/a");
+		if (titleLink.length === 0) continue;
+		// extract book title and URL
+		var title = titleLink[0].textContent;
+		var href = titleLink[0].getAttribute("href");
 		if (!title || !href) continue;
 		if (checkOnly) return true;
 		found = true;
@@ -95,13 +95,12 @@ function getSearchResults(doc, checkOnly) {
 }
 
 
-function scrapeBook(doc, url) {
-	//Call the embedded metadata translator
+function scrapeBook(doc, _url) {
+	// Call the embedded metadata translator
 	var translator = Zotero.loadTranslator("web");
 	translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
 	translator.setDocument(doc);
-	translator.setHandler("itemDone", function(obj, item) {
-		
+	translator.setHandler("itemDone", function (obj, item) {
 		// From EM the item.title field looks like this:
 		// item.title = <book title> | <first creator> | Springer
 		item.title = ZU.xpathText(doc, '//h1');
@@ -110,24 +109,22 @@ function scrapeBook(doc, url) {
 		
 		// There are no creators in the Embedded Metadata and thus we need
 		// to add them manually here.
-		editors = ZU.xpathText(doc, '//li[@itemprop="editor"]/span');
-		authors = ZU.xpathText(doc, '//li[@itemprop="author"]/span');
+		var editors = ZU.xpathText(doc, '//li[@itemprop="editor"]/span');
+		var authors = ZU.xpathText(doc, '//li[@itemprop="author"]/span');
 		if (editors) {
 			editors = editors.split(", ");
-			for (let i=0; i<editors.length; i++)
-				item.creators.push(ZU.cleanAuthor(editors[i], "editor", editors[i].includes(',')));
+			for (let i = 0; i < editors.length; i++) item.creators.push(ZU.cleanAuthor(editors[i], "editor", editors[i].includes(',')));
 		}
 		if (authors) {
 			authors = authors.split(", ");
-			for (let i=0; i<authors.length; i++)
-				item.creators.push(ZU.cleanAuthor(authors[i], "author", authors[i].includes(',')));
+			for (let i = 0; i < authors.length; i++) item.creators.push(ZU.cleanAuthor(authors[i], "author", authors[i].includes(',')));
 		}
 		
 		if (!item.publisher) {
 			item.publisher = ZU.xpathText(doc, '//dd[@itemprop="publisher"]/span');
 		}
 		// see if we can seperate "Springer-Verlag <place>" into publisher and place
-		if (item.publisher && item.publisher.search(/^Springer-Verlag\s.+/)!=-1) {
+		if (item.publisher && item.publisher.search(/^Springer-Verlag\s.+/) != -1) {
 			var publisherInfo = item.publisher.match(/^(Springer-Verlag)\s(.+)/);
 			item.publisher = publisherInfo[1];
 			item.place = publisherInfo[2];
@@ -160,11 +157,11 @@ function scrapeBook(doc, url) {
 			}
 		}
 		
-		//The abstract note might be shortened in the <meta> field; try to load
-		//the full abstract note
-		var long_abstractNote = ZU.xpathText(doc, '(//div[@class="product-about"]//div[@class="springer-html"])[1]');
-		if (long_abstractNote) {
-			item.abstractNote = ZU.trimInternal(long_abstractNote);
+		// The abstract note might be shortened in the <meta> field; try to load
+		// the full abstract note
+		var longAbstractNote = ZU.xpathText(doc, '(//div[@class="product-about"]//div[@class="springer-html"])[1]');
+		if (longAbstractNote) {
+			item.abstractNote = ZU.trimInternal(longAbstractNote);
 		}
 
 		item.complete();
