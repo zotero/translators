75664aed276aff8cf563818ccb699ecab7200904
Tim Hostetler <thostetler@users.noreply.github.com>
2019-11-20 16:52:34 -0500

    Replace old NASA ADS translator

--- NASA ADS.js
+++ NASA ADS.js
@@ -1,22 +1,22 @@
 {
 	"translatorID": "7987b420-e8cb-4bea-8ef7-61c2377cd686",
 	"label": "NASA ADS",
-	"creator": "Philipp Zumstein, Asa Kusuma and Ramesh Srigiriraju",
-	"target": "^https?://(ukads|cdsads|ads|adsabs|esoads|adswww|www\\.ads)\\.(inasan|iucaa\\.ernet|nottingham\\.ac|harvard|eso|u-strasbg|nao\\.ac|astro\\.puc|bao\\.ac|on|kasi\\.re|grangenet|lipi\\.go|mao\\.kiev)\\.(edu|org|net|fr|jp|cl|id|uk|cn|ua|in|ru|br|kr)/(cgi-bin|abs)/",
+	"creator": "Philipp Zumstein, Asa Kusuma, Ramesh Srigiriraju and Tim Hostetler",
+	"target": "^https://ui\\.adsabs\\.harvard\\.edu/(search|abs)/",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2018-02-17 17:39:33"
+	"lastUpdated": "2019-11-06 20:31:43"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2018 Philipp Zumstein
-	
+	Copyright © 2019 Philipp Zumstein
+
 	This file is part of Zotero.
 
 	Zotero is free software: you can redistribute it and/or modify
@@ -35,134 +35,157 @@
 	***** END LICENSE BLOCK *****
 */
 
+function getSearchResults(doc) {
+	const results = doc.querySelectorAll("a[href$=abstract]");
+	const entries = {};
+	for (let el of results) {
+		const titleEl = el.querySelector(":scope h3");
+		if (!titleEl) {
+			continue;
+		}
+		const hrefParts = el.getAttribute("href").split("/");
+		if (hrefParts.length > 2) {
+			const identifier = hrefParts[hrefParts.length - 2];
+			entries[identifier] = ZU.trimInternal(titleEl.textContent);
+		}
+	}
+	return entries;
+}
 
-// attr()/text() v2
-function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}
-
+function extractId(url) {
+	return /\/abs\/(.*)\/abstract/.exec(url)[1];
+}
 
 function detectWeb(doc, url) {
-	if (attr(doc, 'input[name="bibcode"][type="checkbox"]', 'value')) {
+	if (url.includes("/search/")) {
 		return "multiple";
-	} else if (attr(doc, 'input[name="bibcode"][type="hidden"]', 'value')){
+	}
+	else if (url.includes("/abs/")) {
+
+		const type = extractId(url).slice(5);
+		if (type.startsWith('MsT')) {
+			return 'Masters thesis';
+		} else if (type.startsWith('PhDT')) {
+			return 'Ph.D. thesis';
+		}
 		return "journalArticle";
 	}
+	return false;
 }
-
-function scrape(bibcodes, url, doc) {
-	// We can retrieve one RIS file containing the bibliographic
-	// information of multiples entries specified by their
-	// bibcode.
-	var hostname = url.replace(/\/(cgi-bin|abs)\/.*/, '');
-	var getURL = hostname + '/cgi-bin/nph-bib_query?'+bibcodes.join('&')+'&data_type=REFMAN&nocookieset=1';
-	// Some articles have a full text link to the PDF, which
-	// we can find by going through all links in <a>-tags. This
-	// works for the detail page as well as for the mulitple page.
-	var atags = doc.querySelectorAll('a');
-	Zotero.Utilities.HTTP.doGet(getURL, function(text){	
-		// load translator for RIS
-		var translator = Zotero.loadTranslator("import");
-		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
-		translator.setString(text);
-		translator.setHandler("itemDone", function(obj, item) {
-			if (item.url && item.url.includes('/abs/')) {
-				let bibcode = item.url.split('/abs/')[1];
-				let pdfUrl;
-				// Go through all a tags and look for one including the
-				// corresponding bibcode and the link_type for full-text PDF.
-				for (let i=0; i<atags.length; i++) {
-					let atagUrl = atags[i].href;
-					if (atagUrl.includes('bibcode='+bibcode) && atagUrl.includes('link_type=ARTICLE')) {
-						pdfUrl = atagUrl;
-					}
-				}
-				if (pdfUrl) {
-					//Z.debug(pdfUrl);
-					item.attachments = [{
-						url: pdfUrl,
-						title: "Full Text PDF",
-						mimeType: "application/pdf"
-					}];
-				}
-			}
-			item.complete();
-		});	
-		translator.translate();
-	});
-}
-
-
-function getSearchResults(doc, checkOnly) {
-	var items = {};
-	var found = false;
-	var rows = doc.querySelectorAll('input[name="bibcode"][type="checkbox"]');
-	for (var i=0; i<rows.length; i++) {
-		var value = rows[i].value;
-		var title = value;
-		if (!value || !title) continue;
-		if (checkOnly) return true;
-		found = true;
-		items[value] = title;
-	}
-	return found ? items : false;
-}
-
 
 function doWeb(doc, url) {
-	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
-			if (!items) {
-				return true;
-			}
-			var bibcodes = [];
-			for (var i in items) {
-				bibcodes.push('bibcode='+i);
-			}
-			scrape(bibcodes, url, doc);
+	if (detectWeb(doc, url) === "multiple") {
+		Zotero.selectItems(getSearchResults(doc), function (items) {
+			if (!items) return true;
+			return scrape(Object.keys(items), doc);
 		});
-	} else {
-		var bibcode = attr(doc, 'input[name="bibcode"][type="hidden"]', 'value');
-		scrape(['bibcode='+bibcode], url, doc);
 	}
+	else {
+		scrape([extractId(url)], url);
+	}
+}
+
+function makePdfUrl(id) {
+	return 'https://ui.adsabs.harvard.edu/link_gateway/' + id + '/ARTICLE';
+}
+
+function scrape(ids, doc) {
+	const exportUrl
+		= "http://adsabs.harvard.edu/cgi-bin/nph-bib_query?"
+		+ ids.join("&")
+		+ "&data_type=REFMAN&nocookieset=1";
+	ZU.doGet(exportUrl, function (text) {
+		const translator = Zotero.loadTranslator("import");
+		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+		translator.setString(text);
+		translator.setHandler("itemDone", function (obj, item) {
+			item.attachments.push({
+				url: makePdfUrl(ids.pop()),
+				title: "Full Text PDF",
+				mimeType: "application/pdf"
+			});
+			item.attachments.push({ title: "Snapshot", document: doc });
+			item.complete();
+		});
+		translator.translate();
+	});
 }
 
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://adsabs.harvard.edu/cgi-bin/basic_connect?qsearch=star&version=1",
+		"url": "https://ui.adsabs.harvard.edu/search/q=star&sort=date%20desc%2C%20bibcode%20desc&p_=0",
 		"items": "multiple"
 	},
 	{
 		"type": "web",
-		"url": "http://adsabs.harvard.edu/abs/1955ApJ...121..161S",
+		"url": "https://ui.adsabs.harvard.edu/abs/2020CNSNS..8205014M/abstract",
 		"items": [
 			{
 				"itemType": "journalArticle",
-				"title": "The Luminosity Function and Stellar Evolution.",
+				"title": "Modeling excitability in cerebellar stellate cells: Temporal changes in threshold, latency and frequency of firing",
 				"creators": [
 					{
-						"lastName": "Salpeter",
-						"firstName": "Edwin E.",
+						"lastName": "Mitry",
+						"firstName": "John",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Alexander",
+						"firstName": "Ryan P. D.",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Farjami",
+						"firstName": "Saeed",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Bowie",
+						"firstName": "Derek",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Khadra",
+						"firstName": "Anmar",
 						"creatorType": "author"
 					}
 				],
-				"date": "January 1, 1955",
-				"DOI": "10.1086/145971",
-				"ISSN": "0004-637X",
-				"abstractNote": "Abstract image available at: \nhttp://adsabs.harvard.edu/abs/1955ApJ...121..161S",
-				"journalAbbreviation": "The Astrophysical Journal",
-				"libraryCatalog": "NASA ADS",
-				"pages": "161",
-				"publicationTitle": "The Astrophysical Journal",
-				"url": "http://adsabs.harvard.edu/abs/1955ApJ...121..161S",
-				"volume": "121",
+				"date": "March 1, 2020",
+				"DOI": "10.1016/j.cnsns.2019.105014",
+				"ISSN": "1007-5704",
+				"abstractNote": "Cerebellar stellate cells are inhibitory molecular interneurons that \nregulate the firing properties of Purkinje cells, the sole output of\ncerebellar cortex. Recent evidence suggests that these cells exhibit\ntemporal increase in excitability during whole-cell patch-clamp\nconfiguration in a phenomenon termed runup. They also exhibit a\nnon-monotonic first-spike latency profile as a function of the holding\npotential in response to a fixed step-current. In this study, we use\nmodeling approaches to unravel the dynamics of runup and categorize the\nfiring behavior of cerebellar stellate cells as either type I or type II\noscillators. We then extend this analysis to investigate how the\nnon-monotonic latency profile manifests itself during runup. We employ a\npreviously developed, but revised, Hodgkin-Huxley type model to show\nthat stellate cells are indeed type I oscillators possessing a saddle\nnode on an invariant cycle (SNIC) bifurcation. The SNIC in the model\nacts as a \"threshold\" for tonic firing and produces a slow region in the\nphase space called the ghost of the SNIC. The model reveals that (i) the\nSNIC gets left-shifted during runup with respect to Iapp\n=Itest in the current-step protocol, and (ii) both the\ndistance from the stable limit cycle along with the slow region produce\nthe non-monotonic latency profile as a function of holding potential.\nUsing the model, we elucidate how latency can be made arbitrarily large\nfor a specific range of holding potentials close to the SNIC during\npre-runup (post-runup). We also demonstrate that the model can produce\ntransient single spikes in response to step-currents entirely below\nISNIC, and that a pair of dynamic inhibitory and excitatory\npost-synaptic inputs can robustly evoke action potentials, provided that\nthe magnitude of the inhibition is either low or high but not\nintermediate. Our results show that the topology of the SNIC is the key\nto explaining such behaviors.",
+				"journalAbbreviation": "Communications in Nonlinear Science and Numerical Simulations",
+				"libraryCatalog": "NASA ADS 2",
+				"pages": "105014",
+				"publicationTitle": "Communications in Nonlinear Science and Numerical Simulations",
+				"shortTitle": "Modeling excitability in cerebellar stellate cells",
+				"url": "http://adsabs.harvard.edu/abs/2020CNSNS..8205014M",
+				"volume": "82",
 				"attachments": [
 					{
 						"title": "Full Text PDF",
 						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot"
 					}
 				],
-				"tags": [],
+				"tags": [
+					{
+						"tag": "Non-monotonic first-spike latency"
+					},
+					{
+						"tag": "Runup"
+					},
+					{
+						"tag": "Transient single spiking"
+					},
+					{
+						"tag": "Type I oscillator with a SNIC"
+					}
+				],
 				"notes": [],
 				"seeAlso": []
 			}
