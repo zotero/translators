1bffd34f26a1cc8d89ad07c95d7a4ecf85a0487b
placardo <istelod@gmail.com>
2019-11-21 20:15:03 +0100

    Fixed errors and warnings (travis)

--- JournalReligionSociety.js
+++ JournalReligionSociety.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2019-11-21 18:58:46"
+	"lastUpdated": "2019-11-21 19:14:26"
 }
 
 /*
@@ -51,24 +51,24 @@ function getSearchResults(doc, checkOnly) {
 	// We have a different querySelector according to the page (respectively, all supplements page, journal issue page, and supplement issue page)
 	var rows = doc.querySelectorAll('div[class^=pubs] p.SuppVolume em,div[class^=pubs] p.title, div[class^=pubs] p.chap');
 	for (let row of rows) {
-	let title = ZU.trimInternal(row.textContent);
-	if (!title) continue;
-	if (checkOnly) return true;
-	found = true;
-	items[title] = title;
+		let title = ZU.trimInternal(row.textContent);
+		if (!title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[title] = title;
 	}
 	return found ? items : false;
 }
 
 function doWeb(doc, url) {
-  if (detectWeb(doc, url) == "multiple") {
-	Zotero.selectItems(getSearchResults(doc, false), function (items) {
-		if(!items) return true;
-		for (var id in items) {
-			scrape(id, doc, url);
-		}
-	});
-  }
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			if (!items) return true;
+			for (var id in items) {
+				scrape(id, doc, url);
+			}
+		});
+	}
 }
 
 function scrape(id, doc, url) {
@@ -84,13 +84,13 @@ function scrape(id, doc, url) {
 		item.volume = doc.querySelector(".heading").textContent.split('(')[0].match(/\d+/)[0];
 		item.url = url;
 		
-		infoBlock = doc.evaluate("//p[contains(., '"+id+"')]", doc, null, XPathResult.ANY_TYPE, null)
+		infoBlock = doc.evaluate("//p[contains(., '" + id + "')]", doc, null, XPathResult.ANY_TYPE, null)
 		.iterateNext()
 		.nextElementSibling;
 		author = infoBlock.textContent.split("\n");
-		item.creators.push(author)
-		for (let auth of author.slice(0,author.length-1)) {
-			item.creators.push(ZU.cleanAuthor(auth.split(", ")[0], "author", false)); 
+		item.creators.push(author);
+		for (let auth of author.slice(0, author.length - 1)) {
+			item.creators.push(ZU.cleanAuthor(auth.split(", ")[0], "author", false));
 		}
 	
 		if (infoBlock.querySelectorAll("a:last-child")[0].textContent.search("PDF") > -1) {
@@ -108,19 +108,18 @@ function scrape(id, doc, url) {
 		}
 	}
 	else if (url.search('/toc/Supplement') > -1) {
-		let root = "http://moses.creighton.edu/JRS/toc/"
+		let root = "http://moses.creighton.edu/JRS/toc/";
 		item = new Zotero.Item("book");
 		item.title = id;
 		item.series = "Supplement of the Journal of Religion & Society";
 		item.publisher = "Journal of Religion & Society Supplement";
 		
-		infoBlock = doc.evaluate("//p[contains(., '"+id+"')]", doc, null, XPathResult.ANY_TYPE, null).iterateNext();
+		infoBlock = doc.evaluate("//p[contains(., '" + id + "')]", doc, null, XPathResult.ANY_TYPE, null).iterateNext();
 		item.date = infoBlock.querySelector("em").nextSibling.textContent.match(/\d+/)[0];
 		item.seriesNumber = infoBlock.querySelector("a").textContent.match(/\d+/)[0];
 		item.url = root + infoBlock.querySelector("a").getAttribute("href");
 		author = infoBlock.nextElementSibling.textContent.split(",")[0].replace("Edited by ", "").split(" and ");
 		for (let auth of author) item.creators.push(ZU.cleanAuthor(auth, "editor", false));
-		
 	}
 	else if (url.search('/toc/SS') > -1) {
 		item = new Zotero.Item("bookSection");
@@ -134,7 +133,7 @@ function scrape(id, doc, url) {
 		for (let auth of author) item.creators.push(ZU.cleanAuthor(auth, "editor", false));
 		item.url = url;
 		
-		infoBlock = doc.evaluate("//p[contains(., '"+id+"')]", doc, null, XPathResult.ANY_TYPE, null).iterateNext();
+		infoBlock = doc.evaluate("//p[contains(., '" + id + "')]", doc, null, XPathResult.ANY_TYPE, null).iterateNext();
 		item.pages = infoBlock.textContent.split("(pp. ")[1].replace(")", "");
 		author = infoBlock.nextElementSibling.textContent.split(",")[0].split(" and ");
 		for (let auth of author) item.creators.push(ZU.cleanAuthor(auth, "author", false));
@@ -155,6 +154,7 @@ function scrape(id, doc, url) {
 	item.libraryCatalog = "Journal of Religion and Society";
 	item.complete();
 }
+
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
