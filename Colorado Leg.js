9d8a40778a2e0e2df35e5c3e0dd4c4d06819d6d5
Andrew Schwartz <andrew@aschwartz.me>
2020-01-21 12:07:16 -0700

    fix lint issues (indentation, curly brace placement)

--- Colorado Leg.js
+++ Colorado Leg.js
@@ -14,92 +14,93 @@
 
 
 /*
-  ***** BEGIN LICENSE BLOCK *****
+	***** BEGIN LICENSE BLOCK *****
 
-  Copyright © 2020 Andrew Schwartz
+	Copyright © 2020 Andrew Schwartz
 
-  This file is part of Zotero.
+	This file is part of Zotero.
 
-  Zotero is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Affero General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-  Zotero is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU Affero General Public License for more details.
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License
-  along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
 
-  ***** END LICENSE BLOCK *****
+	***** END LICENSE BLOCK *****
 */
 
 
 function detectWeb(doc, url) {
-  if (url.includes('/bills/')) {
-    return "bill";
-  } else
-  if (url.includes('/bill-search') && getSearchResults(doc, true)) {
-    return "multiple";
-  }
-  return false;
+	if (url.includes('/bills/')) {
+		return "bill";
+	}
+	else if (url.includes('/bill-search') && getSearchResults(doc, true)) {
+		return "multiple";
+	}
+	return false;
 }
 
 function getSearchResults(doc, checkOnly) {
-  var items = {};
-  var found = false;
-  var rows = doc.querySelectorAll(`header[class='search-result-single-item']>h4>a[href*="/bills/"]`);
-  for (let row of rows) {
-    let href = row.href;
-    let title = ZU.trimInternal(row.textContent);
-    if (!href || !title) continue;
-    if (checkOnly) return true;
-    found = true;
-    items[href] = title;
-  }
-  return found ? items : false;
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll(`header[class='search-result-single-item']>h4>a[href*="/bills/"]`);
+	for (let row of rows) {
+		let href = row.href;
+		let title = ZU.trimInternal(row.textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
 }
 
 function scrape(doc, url) {
-  var item = new Zotero.Item("bill");
+	var item = new Zotero.Item("bill");
 
-  item.title = doc.querySelector(`h1[class='node__title node-title']`).textContent;
+	item.title = doc.querySelector(`h1[class='node__title node-title']`).textContent;
 
-  let billNumber = doc.querySelector(
-    `div[class='field field-name-field-bill-number field-type-text field-label-hidden']`
-  ).textContent;
-  item.billNumber = ZU.trimInternal(billNumber);
+	let billNumber = doc.querySelector(
+		`div[class='field field-name-field-bill-number field-type-text field-label-hidden']`
+	).textContent;
+	item.billNumber = ZU.trimInternal(billNumber);
 
-  let sponsors = doc.querySelectorAll(`div[class='sponsor-item']>div[class='member']>div[class='member-details']>h4`);
+	let sponsors = doc.querySelectorAll(`div[class='sponsor-item']>div[class='member']>div[class='member-details']>h4`);
 
-  for (var i = sponsors.length - 1; i >= 0; i--) {
-    let sponsorName = ZU.trimInternal(sponsors[i].textContent);
-    item.creators.push(ZU.cleanAuthor(sponsorName, "sponsor", false));
-  }
-  item.legislativeBody = 'Colorado General Assembly';
+	for (var i = sponsors.length - 1; i >= 0; i--) {
+		let sponsorName = ZU.trimInternal(sponsors[i].textContent);
+		item.creators.push(ZU.cleanAuthor(sponsorName, "sponsor", false));
+	}
+	item.legislativeBody = 'Colorado General Assembly';
 
-  item.session = doc.querySelector(`div[class='bill-session']>div>div[class='field-items']>div`).textContent;
+	item.session = doc.querySelector(`div[class='bill-session']>div>div[class='field-items']>div`).textContent;
 
-  let tags = doc.querySelectorAll(`div[class='bill-subjects']>div>div[class=field-items]>div[class*='field-item']`);
-  for (let tag of tags) {
-    item.tags.push(tag.textContent);
-  }
+	let tags = doc.querySelectorAll(`div[class='bill-subjects']>div>div[class=field-items]>div[class*='field-item']`);
+	for (let tag of tags) {
+		item.tags.push(tag.textContent);
+	}
 
-  item.complete();
+	item.complete();
 }
 
 function doWeb(doc, url) {
-  if (detectWeb(doc, url) == "multiple") {
-    Zotero.selectItems(getSearchResults(doc, false), function (items) {
-      if (items) ZU.processDocuments(Object.keys(items), scrape);
-    });
-  } else
-  {
-    scrape(doc, url);
-  }
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			if (items) ZU.processDocuments(Object.keys(items), scrape);
+		});
+	}
+	else {
+		scrape(doc, url);
+	}
 }
+
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
