f6f215b70c89e1f3afb1b59754f900494dcac05e
Eon <60341438+eonm-abes@users.noreply.github.com>
2020-03-16 10:21:53 +0000

    Lint code
    Use eslint to fix issues

--- ThesesFR.js
+++ ThesesFR.js
@@ -58,24 +58,27 @@ function getSearchResults(doc, checkOnly) {
 		if (checkOnly) return true;
 		found = true;
 		items[href] = title;
-	})
+		return row;
+	});
 
 	return found ? items : false;
 }
 
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) === 'multiple') {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+		Zotero.selectItems(getSearchResults(doc, false), (items) => {
 			if (!items) return;
 
 			const records = [];
+
 			for (item in items) {
 				records.push(item);
 			}
 			
 			ZU.processDocuments(records, scrape);
 		});
-	} else {
+	}
+	else {
 		scrape(doc, url);
 	}
 }
@@ -84,29 +87,29 @@ function scrape(doc, url) {
 	const xmlDocumentUrl = `${url}.rdf`;
 	
 	// Each thesis record has an underlying .rdf file
-	Zotero.Utilities.HTTP.doGet(xmlDocumentUrl, function (text) {		
+	Zotero.Utilities.HTTP.doGet(xmlDocumentUrl, function (text) {
 		const parser = new DOMParser();
 		const xmlDoc = parser.parseFromString(text, 'application/xml');
 
 		// Skiping invalid or empty RDF files : prevents crashes while importing multiple records
 		if (xmlDoc.getElementsByTagName('parsererror')[0] || xmlDoc.children[0].childElementCount === 0) {
 			Z.debug('Skiping: Invalid or empty RDF file.');
-			return
+			return;
 		}
 		
 		// Importing XML namespaces for parsing purposes
 		const ns = {
-			'bibo': 'http://purl.org/ontology/bibo/',
-			'dc': 'http://purl.org/dc/elements/1.1/',
-			'dcterms': 'http://purl.org/dc/terms/',
-			'foaf': 'http://xmlns.com/foaf/0.1/',
-			'marcrel': 'http://www.loc.gov/loc.terms/relators/',
-			'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+			bibo: 'http://purorg/ontology/bibo/',
+			dc: 'http://purl.org/dc/elements/1.1/',
+			dcterms: 'http://purl.org/dc/terms/',
+			foaf: 'http://xmlns.com/foaf/0.1/',
+			marcrel: 'http://www.loc.gov/loc.terms/relators/',
+			rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
 		};
 	
 		const title = ZU.xpathText(xmlDoc, '//dc:title', ns);
 		
-		// The case should't exists : prevents crashes while importing multiple records
+		// The case should't exists : preve;nts crashes while importing multiple records
 		if (!title) return;
 
 		const newItem = new Zotero.Item();
@@ -125,9 +128,9 @@ function scrape(doc, url) {
 		});
 
 		const abstractNotes = ZU.xpath(xmlDoc, '//dcterms:abstract', ns);
-		newItem.abstractNote = abstractNotes.length > 0 ? abstractNotes[0].textContent : undefined ;
+		newItem.abstractNote = abstractNotes.length > 0 ? abstractNotes[0].textContent : undefined;
 
-		// '/s + digit' in url means thesis in preparation	
+		// '/s + digit' in url means thesis in preparation
 		newItem.thesisType = url.match(/\/s\d+/) ? 'These en préparation' : 'These de doctorat';
 
 		const organizations = ZU.xpath(xmlDoc, '//marcrel:dgg/foaf:Organization/foaf:name', ns);
@@ -141,21 +144,21 @@ function scrape(doc, url) {
 
 		const permalink = ZU.xpath(xmlDoc, '//bibo:Document', ns);
 		newItem.url = permalink.length > 0 ? permalink[0].getAttribute('rdf:about') : url;
-		newItem.libraryCatalog = 'theses.fr'
+		newItem.libraryCatalog = 'theses.fr';
 		newItem.rights = 'Les données de Theses.fr sont sous licence Etalab';
 
 		// Keep extra information such as laboratory, graduate schools, etc.
 		newItem.extra = Array.from(doc.getElementsByClassName('donnees-ombre')).map((description) => {
-			return Array.from(description.getElementsByTagName('p')).map((description) => description.textContent.replace(/\n/g, ' '))
+			return Array.from(description.getElementsByTagName('p')).map(description => description.textContent.replace(/\n/g, ' '));
 		}).join(' ');
 
 		ZU.xpath(xmlDoc, '//dc:subject', ns).forEach((t) => {
-			const tag =  t.textContent;
+			const tag = t.textContent;
 			newItem.tags.push(tag);
 		});
 
-		newItem.complete()
-	})
+		newItem.complete();
+	});
 }
 
 /** BEGIN TEST CASES **/
@@ -251,18 +254,18 @@ var testCases = [
 				 		"creatorType": "contributor"
 					}
 				],
-			"notes": [],
-			"tags": [],
-			"seeAlso": [],
-			"attachments": [],
-			"title": "Les relations bilatérales France-Québec à l'épreuve de l'OMC et de l'UE",
-			"abstractNote": "Champ territorial: les fondements juridiques France/Québec/Canada/Europe, approches croisées européen/international. 1ère partie orientée histoire du droit: analyse relation bilatérale France/Québec: prémices enjeux diplomatiques et culturels pour la France de \"Gesta Dei per Francos\" aux échanges particuliers France/Québec (1910-1860), puis enjeux diplomatiques et culturels pour la France dans les années 1960 de Gaulle, Malraux, Québec et francophonie), Trente Glorieuses et période de guerre froide, tournant sur le plan international influant et restructurant les bases juridiques. Pour le Canada: période de \"crise majeure de son histoire\" avec la Révolution tranquille et remise en cause des rapports/accords diplomatiques avec la France qui existaient depuis Napoléon III, apparition ouverte d'un rapport triangulaire (Paris-Ottawa-Québec). Le Québec s'éveille, s'affirme, rêve d'indépendance.",
-			"thesisType": "These en préparation",
-			"university": "Paris 2",
-			"date": "2014",
-			"url": "http://www.theses.fr/s128743",
-			"libraryCatalog": "theses.fr",
-			"rights": "Les données de Theses.fr sont sous licence Etalab"
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [],
+				"title": "Les relations bilatérales France-Québec à l'épreuve de l'OMC et de l'UE",
+				"abstractNote": "Champ territorial: les fondements juridiques France/Québec/Canada/Europe, approches croisées européen/international. 1ère partie orientée histoire du droit: analyse relation bilatérale France/Québec: prémices enjeux diplomatiques et culturels pour la France de \"Gesta Dei per Francos\" aux échanges particuliers France/Québec (1910-1860), puis enjeux diplomatiques et culturels pour la France dans les années 1960 de Gaulle, Malraux, Québec et francophonie), Trente Glorieuses et période de guerre froide, tournant sur le plan international influant et restructurant les bases juridiques. Pour le Canada: période de \"crise majeure de son histoire\" avec la Révolution tranquille et remise en cause des rapports/accords diplomatiques avec la France qui existaient depuis Napoléon III, apparition ouverte d'un rapport triangulaire (Paris-Ottawa-Québec). Le Québec s'éveille, s'affirme, rêve d'indépendance.",
+				"thesisType": "These en préparation",
+				"university": "Paris 2",
+				"date": "2014",
+				"url": "http://www.theses.fr/s128743",
+				"libraryCatalog": "theses.fr",
+				"rights": "Les données de Theses.fr sont sous licence Etalab"
 		   }
 		]
 	}
