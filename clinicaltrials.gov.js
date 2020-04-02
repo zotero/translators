ff3a1a3e11a2f0db56ad684ac3043b841e16b01d
rdvelazquez <ryan.velazquez@azimuth1.com>
2020-04-01 20:40:01 -0400

    address comments; additional test cases

--- clinicaltrials.gov.js
+++ clinicaltrials.gov.js
@@ -36,8 +36,8 @@
 */
 
 function detectWeb(doc, url) {
-	if (url.includes('https://clinicaltrials.gov/ct2/results')) {
-		throw new Error('clinicaltrials.gov search pages not supported by Zotero, only individual trials');
+	if (url.includes("https://clinicaltrials.gov/ct2/results")) {
+		throw new Error("clinicaltrials.gov search pages not supported by Zotero, only individual trials");
 	}
 	else {
 		return "report";
@@ -67,17 +67,23 @@ function isXmlAPIRequest(url) {
 }
 
 function getClinicalTrialID(url) {
-	// TODO: make sure this handles all the potential URLs
 	if (isXmlAPIRequest(url) || isJsonAPIRequest(url)) {
 		return url.split("expr=")[1].split("&")[0];
 	}
 	else {
-		return url.split('/show/')[1].split("?")[0];
+		return url.split("/show/")[1].split("?")[0];
 	}
 }
 
 function dateTimeToDateString(dateTime) {
 	return dateTime.split(" ")[0].split(":").join("-");
+}
+
+function nameToFirstAndLast(rawName) {
+	const name = rawName.split(",")[0];
+	const firstName = name.split(" ")[0];
+	const lastName = name.split(",")[0].split(" ")[name.split(" ").length - 1];
+	return [firstName, lastName];
 }
 
 function scrape(doc, url) {
@@ -97,33 +103,74 @@ function scrape(doc, url) {
 		item.itemType = "report";
 		item.title = study.ProtocolSection.IdentificationModule.OfficialTitle;
 
-		// Get the creator info
-		const responsibleParty = study.ProtocolSection.SponsorCollaboratorsModule.ResponsibleParty;
-		if (typeof responsibleParty.ResponsiblePartyInvestigatorFullName == "string") {
-			let authorName = responsibleParty.ResponsiblePartyInvestigatorFullName;
-			item.creator = {
-				author: authorName
-			};
-		}
-		else if (study.ProtocolSection.SponsorCollaboratorsModule.ResponsibleParty.ResponsiblePartyType == "Sponsor") {
-			let sponsor = study.ProtocolSection.SponsorCollaboratorsModule.LeadSponsor.LeadSponsorName;
-			item.creator = {
-				author: sponsor
-			};
+		// Start get the creator info
+		let creators = [];
+		let responsiblePartyInvestigator;
+		let sponsor;
+		let collaborators = [];
+		if (study.ProtocolSection.SponsorCollaboratorsModule.hasOwnProperty("ResponsibleParty")) {
+			const responsibleParty = study.ProtocolSection.SponsorCollaboratorsModule.ResponsibleParty;
+			if (typeof responsibleParty.ResponsiblePartyInvestigatorFullName == "string") {
+				responsiblePartyInvestigator = responsibleParty.ResponsiblePartyInvestigatorFullName;
+				const splitResponsiblePartyInvestigator = nameToFirstAndLast(responsiblePartyInvestigator);
+				creators.push({
+					firstName: splitResponsiblePartyInvestigator[0],
+					lastName: splitResponsiblePartyInvestigator[1],
+					creatorType: "author"
+				});
+			}
 		}
 
-		item.date = study.ProtocolSection.StatusModule.LastUpdateSubmitDate; // TODO: is this the date that we want? Would "StudyFirstSubmitDate" be better?
+		if (study.ProtocolSection.SponsorCollaboratorsModule.hasOwnProperty("LeadSponsor")) {
+			sponsor = study.ProtocolSection.SponsorCollaboratorsModule.LeadSponsor.LeadSponsorName;
+			creators.push({
+				firstName: sponsor,
+				creatorType: "author"
+			});
+		}
+		
+		if (study.ProtocolSection.SponsorCollaboratorsModule.hasOwnProperty("CollaboratorList")) {
+			const collaboratorList = study.ProtocolSection.SponsorCollaboratorsModule.CollaboratorList.Collaborator;
+			collaboratorList.forEach((collaborator) => {
+				collaborators.push(
+					{
+						firstName: collaborator.CollaboratorName,
+						creatorType: "author"
+					}
+				);
+			});
+			collaborators.forEach((collaborator) => {
+				creators.push(collaborator);
+			});
+		}
+
+		item.creators = creators;
+		// Done get the creator info
+
+		item.date = study.ProtocolSection.StatusModule.LastUpdateSubmitDate;
 		item.accessDate = dateTimeToDateString(data.FullStudiesResponse.DataVrs);
 		item.libraryCatalog = "clinicaltrials.gov";
 		item.shortTitle = study.ProtocolSection.IdentificationModule.BriefTitle;
 		item.url = "https://clinicaltrials.gov/ct2/show/" + clinicalTrialID;
+		let extras = {
+			submittedDate: study.ProtocolSection.StatusModule.StudyFirstSubmitDate,
+			responsiblePartyInvestigator: responsiblePartyInvestigator,
+			sponsor: sponsor
+		};
+		if (collaborators.length > 0) {
+			extras.collaborators = [];
+			collaborators.forEach((collaborator) => {
+				extras.collaborators.push(collaborator.firstName);
+			});
+		}
+
+		item.extra = extras;
 		item.complete();
 	});
 }
 
 
 /** BEGIN TEST CASES **/
-// TODO: set up more test cases
 var testCases = [ 
 	{
 		"type": "web",
@@ -132,16 +179,87 @@ var testCases = [
 			{
 				"itemType": "report",
 				"title": "A Phase 3 Randomized Study to Evaluate the Safety and Antiviral Activity of Remdesivir (GS-5734™) in Participants With Severe COVID-19",
-				"creator": {
-					"author": "Gilead Sciences"
-				},
+				"creators": [
+					{
+						"firstName": "Gilead Sciences",
+						"creatorType": "author"
+					}
+				],
 				"date": "February 28, 2020",
 				"accessDate": "2020-04-01",
 				"libraryCatalog": "clinicaltrials.gov",
 				"shortTitle": "Study to Evaluate the Safety and Antiviral Activity of Remdesivir (GS-5734™) in Participants With Severe Coronavirus Disease (COVID-19)",
-				"url": "https://clinicaltrials.gov/ct2/show/NCT04292899"
+				"url": "https://clinicaltrials.gov/ct2/show/NCT04292899",
+				"extra": {
+					"submittedDate": "February 6, 2020",
+					"responsiblePartyInvestigator": "undefined",
+					"sponsor": "Gilead Sciences"
+				}
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://clinicaltrials.gov/ct2/show/NCT00287391",
+		"items": [
+			{
+				"itemType": "report",
+				"title": "The Impact of Gastroesophageal Reflux Disease in Sleep Disorders: A Pilot Investigation of Rabeprazole, 20 mg Twice Daily for the Relief of GERD-Related Insomnia.",
+				"creators": [
+					{
+						"firstName": "University of North Carolina",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Janssen Pharmaceutica N.V., Belgium",
+						"creatorType": "author"
+					}
+				],
+				"date": "April 25, 2007",
+				"accessDate": "2020-04-01",
+				"libraryCatalog": "clinicaltrials.gov",
+				"shortTitle": "Sleep Disorders and Gastroesophageal Reflux Disease (GERD)",
+				"url": "https://clinicaltrials.gov/ct2/show/NCT00287391",
+				"extra": {
+					"submittedDate": "February 3, 2006",
+					"responsiblePartyInvestigator": "undefined",
+					"sponsor": "University of North Carolina",
+					"collaborators": ["Janssen Pharmaceutica N.V., Belgium"]
+				}
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://clinicaltrials.gov/ct2/show/NCT04261517?recrs=e&cond=COVID&draw=2&rank=8",
+		"items": [
+			{
+				"itemType": "report",
+				"title": "Efficacy and Safety of Hydroxychloroquine for Treatment of Pneumonia Caused by 2019-nCoV ( HC-nCoV )",
+				"creators": [
+					{
+						"firstName": "Hongzhou",
+						"lastName": "Lu",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Shanghai Public Health Clinical Center",
+						"creatorType": "author"
+					}
+				],
+				"date": "March 22, 2020",
+				"accessDate": "2020-04-01",
+				"libraryCatalog": "clinicaltrials.gov",
+				"shortTitle": "Efficacy and Safety of Hydroxychloroquine for Treatment of Pneumonia Caused by 2019-nCoV ( HC-nCoV )",
+				"url": "https://clinicaltrials.gov/ct2/show/NCT04261517",
+				"extra": {
+					"submittedDate": "February 6, 2020",
+					"responsiblePartyInvestigator": "Hongzhou Lu",
+					"sponsor": "Shanghai Public Health Clinical Center"
+				}
 			}
 		]
 	}
+
 ]
 /** END TEST CASES **/
