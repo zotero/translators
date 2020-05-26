ebd72315135ecdda2db94e41c01302a7b0c4f3c1
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-05-26 15:24:20 +0200

    Update and rename Davarlogos.js to ubtue_Davarlogos.js

--- ubtue_Davarlogos.js
+++ ubtue_Davarlogos.js
@@ -6,7 +6,7 @@
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
-	"inRepository": true,
+	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
 	"lastUpdated": "2020-05-26 09:18:22"
