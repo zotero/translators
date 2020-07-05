20e4614d6fa64281f6fcdd1b0e0634f3ae5b969b
step21 <step21@users.noreply.github.com>
2020-07-05 23:05:25 +0200

    disable eslint on minimzed code
    Co-authored-by: Sebastian Karcher <karcher@u.northwestern.edu>

--- ResearchGate.js
+++ ResearchGate.js
@@ -37,11 +37,8 @@
 
 
 // attr()/text() v2
-function attr(docOrElem, selector, attr, index) {
-	var elem = index ? docOrElem.querySelectorAll(selector).item(index) : docOrElem.querySelector(selector); return elem ? elem.getAttribute(attr) : null;
-} function text(docOrElem, selector, index) {
-	var elem = index ? docOrElem.querySelectorAll(selector).item(index) : docOrElem.querySelector(selector); return elem ? elem.textContent : null;
-}
+// eslint-disable-next-line
+function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null}
 
 function detectWeb(doc, url) {
 	if (url.includes('/publication/')) {
