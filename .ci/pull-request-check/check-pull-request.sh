9a7cca2968a1a5f3a8cb3c6cb5434ac0a0bf49a3
Dan Stillman <dstillman@zotero.org>
2020-07-15 11:23:33 -0400

    CI: Update global schema submodule within Zotero submodule

--- .ci/pull-request-check/check-pull-request.sh
+++ .ci/pull-request-check/check-pull-request.sh
@@ -10,10 +10,12 @@ cd connectors
 if [ -d .git ]; then
 	git pull
 	git submodule update
+	git -C src/zotero/ submodule update -- resource/schema/global
 	npm ci
 else
 	git clone https://github.com/zotero/zotero-connectors.git --depth 1 .
 	git submodule update --init --depth 1
+	git -C src/zotero submodule update --init --depth 1 -- resource/schema/global
   npm ci
 fi
 
