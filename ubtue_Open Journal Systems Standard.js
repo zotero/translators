17f0b9de8e85dc4c10f70c90a73fc601e077bf4a
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-08-27 15:20:11 +0200

    Update and rename Open Journal Systems Standard.js to ubtue_Open Journal Systems Standard.js

--- ubtue_Open Journal Systems Standard.js
+++ ubtue_Open Journal Systems Standard.js
@@ -5,7 +5,7 @@
 	"target": "article|issue/view/",
 	"minVersion": "3.0",
 	"maxVersion": "",
-	"priority": 80,
+	"priority": 100,
 	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
