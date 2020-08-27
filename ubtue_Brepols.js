538cd294cf1f63822dd20b11e2c4dbf126a1621b
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-08-27 10:49:44 +0200

    https://github.com/ubtue/DatenProbleme/issues/643
    https://github.com/ubtue/DatenProbleme/issues/640
    push second abstract in item.notes

--- ubtue_Brepols.js
+++ ubtue_Brepols.js
@@ -9,7 +9,7 @@
 	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-08-26 15:44:24"
+	"lastUpdated": "2020-08-27 10:47:44"
 }
 
 /*
@@ -79,11 +79,62 @@ function invokeEMTranslator(doc, url) {
 	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
 	translator.setDocument(doc);
 	translator.setHandler('itemDone', function (t, i) {
-		let abstracts = text(doc, '.hlFld-Abstract');
-		if (i.abstractNote) i.abstractNote = abstracts.replace(/^Abstract/, '');
+		var rows = doc.querySelectorAll('.hlFld-Abstract');
+		for (let row of rows) {
+			var abstractsEntry = row.innerText//.replace('Abstract', '');
+		}
+		Z.debug(abstractsEntry)
+		let abstractsOneTwo = abstractsEntry.split('\n\n'); //Z.debug(abstractsOneTwo)
+		if (i.abstractNote) i.abstractNote = abstractsOneTwo[1];
+		if (abstractsOneTwo[2]) {
+			i.notes.push({
+				note: "abs:" + abstractsOneTwo[2],
+		});
+	}
 		if (i.reportType === "book-review") i.tags.push('Book Review') && delete i.abstractNote;
 		i.complete();
 	});
 	translator.translate();
-}
-
+}/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://www.brepolsonline.net/doi/abs/10.1484/J.JAAJ.5.119152",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Faith in a Silent God: The Characterization of Hannah in Pseudo-Philo",
+				"creators": [
+					{
+						"firstName": "Tavis A.",
+						"lastName": "Bohlinger",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020-02-18",
+				"DOI": "10.1484/J.JAAJ.5.119152",
+				"abstractNote": "Le caractère biblique d’Anne dans Liber Antiquitatum Biblicarum est un exemple révélateur de la façon dont le Pseudo-Philo transforme ses héros en modèles de foi radicale en Dieu. Pourtant, l’ampleur de la réécriture de son histoire par LAB n’a pas reçu toute l’attention qu’elle mérite. La caractérisation d’Anne dans LAB est remarquable car elle est une femme opprimée dans un récit dominé par des personnages masculins. Sa situation, telle que décrite par le Pseudo-Philo, dépasse de loin la situation désastreuse du personnage biblique d’Anne dans les premiers chapitres de 1 Samuel. En effet, sa situation exige une plus grande foi en Dieu que toutes les autres figures masculines du récit, à l’exception peut-être d’Abraham dans l’épisode de la fournaise ardente (LAB 6). D’un point de vue sociologique dans l’Israël ancien, Anne se trouve à l’opposé des dirigeants masculins dans le LAB : elle est stérile, une seconde femme sans enfant, sans aucun rôle de leadership. Pourtant, les attentions multiples qu’elle reçoit de Dieu, sans jamais entendre une seule parole de sa part, lui donne une place d’honneur inattendue dans la liste des exemples de la vraie foi en Dieu en Israël. Bien que les exégètes aient remarqué l’importance d’Anne dans le récit, l’article offre un compte-rendu complet du modèle de foi qu’Anne incarne. Cet aspect est évident chez les autres dirigeants masculins dans le LAB, mais chez Anne, il est différent : non seulement Dieu ne lui parle jamais directement, mais elle n’a également jamais la possibilité d’entendre les paroles de Dieu rapportés par d’autres. Le récit du Pseudo-Philo est encadré par la parole divine indiquant la gravité théologique de la suppression de la parole divine pour Anne avant la naissance de Samuel. À travers Anne, le Pseudo-Philo livre un message poignant à un public juif en attente d’être délivré de l’oppression romaine : elle se tient aux côtés d’Abraham comme exemple éminent de foi radicale en Dieu qui semble être délibérément silencieux.",
+				"archiveLocation": "Turnhout, Belgium",
+				"language": "en",
+				"libraryCatalog": "www.brepolsonline.net",
+				"publicationTitle": "Judaïsme Ancien - Ancient Judaism",
+				"shortTitle": "Faith in a Silent God",
+				"url": "https://www.brepolsonline.net/doi/abs/10.1484/J.JAAJ.5.119152",
+				"attachments": [
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [
+					{
+						"note": "abs:The biblical character of Hannah in Liber Antiquitatum Biblicarum is a telling example of how Pseudo-Philo reworks his heroes into models of radical trust in God. And yet, the magnitude of LAB’s rewriting of her story has not received the attention it warrants. Hannah’s characterization in LAB is remarkable for the fact that she is an oppressed woman in a narrative dominated by prominent male figures. Her circumstances, as portrayed by Pseudo-Philo, far exceed the dire situation of the biblical Hannah in the opening chapters of 1 Samuel. Indeed, her situation demands greater trust in God than any of the male figures in the narrative, with the possible exception of Abraham at the fiery furnace (LAB 6). In terms of Israel’s social spectrum, Hannah lies at the complete opposite end of the male leaders in LAB; she is sterile, a childless second wife with no leadership role whatsoever. Yet the multifarious vindication she receives from God, without ever hearing a word from him, warrants her a place of unexpected honor on the list of Israel’s exemplars of true faith in God. Whilst previous interpreters have noticed Hannah’s importance to the narrative, my paper offers a comprehensive account of the pattern of faith that Hannah models. This pattern is evident in the other male leaders in LAB, but Hannah’s pattern is different in one significant aspect: God never speaks to her, nor does she hear his words secondhand. Pseudo-Philo’s narrative is framed by divine speech, indicating the theological gravity of the author’s removal of a divine word for Hannah prior to the birth of Samuel. Through Hannah, Pseudo-Philo delivers a poignant message to a Jewish audience awaiting deliverance from Roman oppression: she stands with Abraham as a preeminent example of radical faith in the God who at times seems deliberately silent."
+					}
+				],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
