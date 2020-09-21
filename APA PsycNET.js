cd654f8876ba31ba51c5497b6ff155d69f549137
Dan Stillman <dstillman@zotero.org>
2020-09-21 03:47:18 -0400

    APA PsycNet: Disable eslint for attr/text line

--- APA PsycNET.js
+++ APA PsycNET.js
@@ -47,6 +47,7 @@
 
 
 // attr()/text() v2
+// eslint-disable-next-line
 function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}
 
 
