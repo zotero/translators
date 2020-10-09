bc4033029f5494f70e65b61249d420e1c5a6179e
Sebastian Karcher <karcher@u.northwestern.edu>
2020-10-09 09:19:36 -0400

    clean up some comments

--- OSF Preprints.js
+++ OSF Preprints.js
@@ -144,10 +144,8 @@ function scrape(doc, url) {
 	var translator = Zotero.loadTranslator('web');
 	// Embedded Metadata
 	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
-	// translator.setDocument(doc);
 
 	translator.setHandler('itemDone', function (obj, item) {
-		// TODO adjust if needed:
 		if (item.extra) {
 			item.extra += "\ntype: article";
 		}
