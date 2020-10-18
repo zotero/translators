612921f6aa3c053cebef210068e6101a8dfbccf7
Felix Hui <xuwd86@gmail.com>
2020-10-18 21:34:00 +0800

    Revert "ASCE Rewrite with EM (#2201)"
    This reverts commit 1647aa6af39d983f4790ccced6520caeef6d158a.

--- ASCE.js
+++ ASCE.js
@@ -2,20 +2,23 @@
 	"translatorID": "303bdfc5-11b8-4107-bca1-63ca97701a0f",
 	"label": "ASCE",
 	"creator": "Sebastian Karcher",
-	"target": "^https?://(www\\.)?ascelibrary\\.org/(toc|doi|action)/",
+	"target": "^https?://(www\\.)?ascelibrary\\.org/",
 	"minVersion": "2.1.9",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-06-28 02:29:25"
+	"lastUpdated": "2017-04-01 14:22:53"
 }
+
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2020 Sebastian Karcher
+	ASCE Translator
+	(Based on Taylor and Francis Translator)
+	Copyright © 2012 Sebastian Karcher
 
 	This file is part of Zotero.
 
@@ -36,70 +39,116 @@
 */
 
 
-// attr()/text() v2
-// eslint-disable-next-line
-function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null}
+function getTitles(doc) {
+	return ZU.xpath(doc, '//table[@class="articleEntry"]//td[@valign="top"]/a[1]');
+}
 
 function detectWeb(doc, url) {
-	if (/\/doi\/((abs|full)\/)?10\./.test(url)) {
+/*	if (url.match(/\/doi\/abs\/10\.|\/doi\/full\/10\./)) {
 		return "journalArticle";
-	}
-	else if (getSearchResults(doc, true)) {
+	} else if (url.match(/\/action\/doSearch\?|\/toc\//))
+		{
 		return "multiple";
-	}
-	return false;
+		} */
+    //currently this triggers a massive download that shuts down Zotero for a significant time; turning it off until I have a fix (which should be shortly)
+    return false;
 }
 
-function getSearchResults(doc, checkOnly) {
-	var items = {};
-	var found = false;
-	var rows = doc.querySelectorAll('div[class*="art_title"]>a');
-	for (let row of rows) {
-		let href = row.href;
-		let title = ZU.trimInternal(row.textContent);
-		if (!href || !title) continue;
-		if (checkOnly) return true;
-		found = true;
-		items[href] = title;
-	}
-	return found ? items : false;
-}
 
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
-			if (items) ZU.processDocuments(Object.keys(items), scrape);
+		var items = new Object();
+		var titles = getTitles(doc);
+		var doi;
+		for (var i=0, n=titles.length; i<n; i++) {
+			doi = titles[i].href.match(/\/doi\/(?:abs|full)\/(10\.[^?#]+)/);
+			if (doi) {
+				items[doi[1]] = titles[i].textContent;
+			}
+		}
+
+		Zotero.selectItems(items, function(selectedItems){
+			if (!selectedItems) return true;
+			
+			var dois = new Array();
+			for (var i in selectedItems) {
+				dois.push(i);
+			}
+			scrape(null, url,dois);
+		});
+	} else {
+		var doi = url.match(/\/doi\/(?:abs|full)\/(10\.[^?#]+)/);
+		scrape(doc, url,[doi[1]]);
+	}
+}
+
+function finalizeItem(item, doc, doi, baseUrl) {
+	var pdfurl = baseUrl + '/doi/pdf/';
+	var absurl = baseUrl + '/doi/abs/';
+
+	//add attachments
+	item.attachments = [{
+		title: 'Full Text PDF',
+		url: pdfurl + doi,
+		mimeType: 'application/pdf'
+	}];
+	if (doc) {
+		item.attachments.push({
+			title: 'Snapshot',
+			document: doc
+		});
+	} else {
+		item.attachments.push({
+			title: 'Snapshot',
+			url: item.url || absurl + doi,
+			mimeType: 'text/html'
 		});
 	}
-	else {
-		scrape(doc, url);
+
+	item.complete();
+}
+
+function scrape(doc, url, dois) {
+	var baseUrl = url.match(/https?:\/\/[^\/]+/)[0]
+	var postUrl = baseUrl + '/action/downloadCitation';
+	var postBody = 	'downloadFileName=citation&' +
+					'direct=true&' +
+					'include=abs&' +
+					'doi=';
+	var risFormat = '&format=ris';
+	var bibtexFormat = '&format=bibtex';
+
+	for (var i=0, n=dois.length; i<n; i++) {
+		(function(doi) {
+			ZU.doPost(postUrl, postBody + doi + bibtexFormat, function(text) {
+				var translator = Zotero.loadTranslator("import");
+				// Use BibTeX translator
+				translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
+				translator.setString(text);
+				translator.setHandler("itemDone", function(obj, item) {
+					item.bookTitle = item.publicationTitle;
+
+					//unfortunately, bibtex is missing some data
+					//publisher, ISSN/ISBN
+					ZU.doPost(postUrl, postBody + doi + risFormat, function(text) {
+						risTrans = Zotero.loadTranslator("import");
+						risTrans.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+						risTrans.setString(text);
+						risTrans.setHandler("itemDone", function(obj, risItem) {
+							item.publisher = risItem.publisher;
+							item.ISSN = risItem.ISSN;
+							item.ISBN = risItem.ISBN;
+							finalizeItem(item, doc, doi, baseUrl);
+						});
+						risTrans.translate();
+					});
+				});
+				translator.translate();
+			});
+		})(dois[i]);
 	}
 }
 
-function scrape(doc, url) {
-	// EM only gets a social media preview for the abastract
-	let abstract = text(doc, 'article.article div[class*="Abstract"]>p');
-
-	// Z.debug(abstract);
-	var translator = Zotero.loadTranslator('web');
-	// Embedded Metadata
-	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
-	translator.setDocument(doc);
-
-	translator.setHandler('itemDone', function (obj, item) {
-		item.libraryCatalog = "ASCE";
-		if (abstract) {
-			item.abstractNote = abstract;
-		}
-		// Remove mapping from DC:coverage to archiveLocation
-		item.archiveLocation = "";
-		item.complete();
-	});
-
-	translator.getTranslatorObject(function (trans) {
-		trans.doWeb(doc, url);
-	});
-}
 
 /** BEGIN TEST CASES **/
 var testCases = [
@@ -110,7 +159,7 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "https://ascelibrary.org/doi/abs/10.1061/%28ASCE%290887-381X%282003%2917%3A1%2837%29",
+		"url": "http://ascelibrary.org/doi/abs/10.1061/%28ASCE%290887-381X%282003%2917%3A1%2837%29",
 		"items": [
 			{
 				"itemType": "journalArticle",
@@ -132,17 +181,16 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"date": "2003/03/01",
+				"date": "2003",
 				"DOI": "10.1061/(ASCE)0887-381X(2003)17:1(37)",
 				"ISSN": "0887-381X",
 				"abstractNote": "In seeking to promote cycling in wintertime, it is desirable to understand how important the winter maintenance service level is in people’s decision to cycle or not, and methods to compare different road conditions on cycleways are therefore needed. By measuring friction, an assessment of the service level can be achieved, but methods available often involve the use of large vehicles, which can lead to overloading damage on cycleways, and constitute a safety risk for cyclists and pedestrians. A portable friction tester (PFT), originally designed to measure friction on road markings, was thought to be an appropriate instrument for cycleways and was, therefore, tested on different winter road conditions, and on different cycleway pavement materials. In this study, it was found that the PFT is a valuable tool for measuring friction on cycleways. Different winter road conditions, as well as different pavement materials, can be distinguished from each other through PFT measurements. The PFT provides a good complement to visual inspections of cycleways in winter maintenance evaluation and can, for example, be used to determine if desired service levels have been achieved.",
 				"issue": "1",
-				"language": "EN",
+				"itemID": "doi:10.1061/(ASCE)0887-381X(2003)17:1(37)",
 				"libraryCatalog": "ASCE",
 				"pages": "37-57",
 				"publicationTitle": "Journal of Cold Regions Engineering",
-				"rights": "Copyright © 2003 American Society of Civil Engineers",
-				"url": "https://ascelibrary.org/doi/abs/10.1061/%28ASCE%290887-381X%282003%2917%3A1%2837%29",
+				"url": "http://dx.doi.org/10.1061/(ASCE)0887-381X(2003)17:1(37)",
 				"volume": "17",
 				"attachments": [
 					{
@@ -153,38 +201,7 @@ var testCases = [
 						"title": "Snapshot"
 					}
 				],
-				"tags": [
-					{
-						"tag": "Bicycles"
-					},
-					{
-						"tag": "Friction"
-					},
-					{
-						"tag": "Maintenance"
-					},
-					{
-						"tag": "Measurement"
-					},
-					{
-						"tag": "Roads"
-					},
-					{
-						"tag": "friction"
-					},
-					{
-						"tag": "inspection"
-					},
-					{
-						"tag": "maintenance engineering"
-					},
-					{
-						"tag": "road traffic"
-					},
-					{
-						"tag": "safety"
-					}
-				],
+				"tags": [],
 				"notes": [],
 				"seeAlso": []
 			}
