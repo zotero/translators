34924784736f2f7336f45f2dcf7a720acb3444c5
bartek kawula <b.kawula@utoronto.ca>
2020-10-21 18:31:34 -0400

    Updates to for new interface changes and proxy handling issues

--- Scholars Portal Journals.js
+++ Scholars Portal Journals.js
@@ -2,20 +2,20 @@
 	"translatorID": "5ac0fd37-5578-4f82-8340-0e135b6336ee",
 	"label": "Scholars Portal Journals",
 	"creator": "Bartek Kawula",
-	"target": "^https?://journals\\d?\\.scholarsportal\\.info/",
+	"target": "^https?://journals\\.scholarsportal\\.info/",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-06-24 10:24:20"
+	"lastUpdated": "2020-10-21 22:29:53"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2016 Bartek Kawula
+	Copyright © 2020 Bartek Kawula
 
 	This file is part of Zotero.
 
@@ -35,23 +35,26 @@
 	***** END LICENSE BLOCK *****
 */
 
-function detectWeb(doc, url) { 
-	var myArticles = doc.getElementById('my-articles');
-	if (myArticles) {
-  		Zotero.monitorDOMChanges(myArticles.parentElement, {attributes: true, attributeFilter: ["style"]});
-	}
-	// see if saved list is toggled open
-	if (doc.getElementsByClassName('inner-wrap-open') [0]) {
+function detectWeb(doc, url) {
+	if (url.indexOf("/my-articles") != -1) {
 		if (getItems(doc, true)) {
 			return 'multiple';
 		}
-	} else if (url.indexOf("/search?q") != -1 || url.indexOf("/browse/") != -1) {
+	} else if (url.indexOf("/search?q") != -1) {
+		if (getItems(doc, true)) {
+			return 'multiple';
+		}
+	} else if (url.indexOf("/browse/") != -1) {
+		var browse = doc.getElementById('toc');
+		if (browse) {
+	  		Zotero.monitorDOMChanges(browse.parentElement);
+		}
 		if (getItems(doc, true)) {
 			return 'multiple';
 		}
 	} else if (url.indexOf("/details/") != -1) {
 		return 'journalArticle';
-	} 
+	}
 }
 
 function doWeb(doc, url) {
@@ -75,18 +78,24 @@ function doWeb(doc, url) {
 }
 
 function getURI(url){
-	if (url.indexOf("xml") != -1){
+	if (url.indexOf("/details/") != -1){
 		var a = url.indexOf("details");
 		var b = url.indexOf("xml");
 		return url.substring(a+7,b+3);
-	} else if (url.indexOf("resolver.scholarsportal.info/resolve/") != -1)  {
-		return url.split("resolver.scholarsportal.info/resolve")[1] + ".xml";
+	} else if (url.indexOf("/resolve/") != -1)  {
+		if (url.indexOf(".xml") != -1){
+			var a = url.indexOf("/resolve/");
+			var b = url.indexOf("xml");
+			return url.substring(a+9,b+3);
+		} else {
+			return "/" + url.split("/resolve/")[1] + ".xml";
+		}
 	}
 }
 
 function getItems(doc, url) {
 	var items = {}, found = false;
-	if (doc.getElementsByClassName('inner-wrap-open') [0]) {
+	if (doc.URL.indexOf("/my-articles") != -1) {
 		var titles = ZU.xpath(doc.getElementById('my-articles-list'), './/div[@class = "title"]/h3/a');
 		for (var i=0; i<titles.length; i++) { 
 			var title = ZU.trimInternal(titles[i].textContent);
@@ -96,11 +105,10 @@ function getItems(doc, url) {
 		}
 	} else {
 		if (doc.URL.indexOf("/browse") != -1){
-			var titles = ZU.xpath(doc, './/div/h4/a');
+			var titles = ZU.xpath(doc, './/div/h3/a');
 		}
 		else {
-			var titles = ZU.xpath(doc.getElementById('result-list'), './/div[@class = "details"]/h3/a');
-		
+			var titles = ZU.xpath(doc.getElementById('result-list'), './/div[@class = "details"]/h2/a');
 		}
 		for (var i=0; i<titles.length; i++) { 
 			var title = ZU.trimInternal(titles[i].textContent);
@@ -118,6 +126,7 @@ function scrape(text, doc) {
 	translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
 	translator.setString(text);
 	translator.setHandler("itemDone", function(obj, item) {
+		console.log(item.attachments[0].path);
 		var uri = getURI(item.attachments[0].path);
 		var pdfURL = "/pdf" + uri;
 		item.url = "http://journals.scholarsportal.info/details" + uri;
@@ -159,13 +168,13 @@ var testCases = [
 				"date": "2008",
 				"DOI": "10.1007/s10953-008-9276-0",
 				"ISSN": "0095-9782",
-				"abstractNote": "The liquid structures of water-tetrahydrofuran (THF) and water-acetone binary mixtures were investigated by the X-ray scattering method. Comparison of the X-ray scattering data revealed that only one kind of intermolecular water-organic molecule interaction is commonly involved throughout all mole fractions of these liquid mixtures, in addition to the intermolecular water-water and organic molecule-organic molecule interactions, which are present in neat water and organic liquids, respectively. On the basis of this finding, we proposed a new analytical method for studying liquid mixtures. By this method the structural information on the intermolecular water-organic molecule interaction as well as the concentrations of the intermolecular water-water, water-organic molecules, and organic molecule-organic molecule interactions were obtained. Combining the concentrations of the intermolecular water-water interaction with the concentrations of water in the liquid mixtures, the number of water-water hydrogen bonds at various mole fractions was experimentally determined for the first time. From the dependence of the number of water-water hydrogen bonds on the composition of the liquid mixtures, the change of the size of the self-associated water-water clusters was deduced.",
+				"abstractNote": "Abstract The liquid structures of water-tetrahydrofuran (THF) and water-acetone binary mixtures were investigated by the X-ray scattering method. Comparison of the X-ray scattering data revealed that only one kind of intermolecular water-organic molecule interaction is commonly involved throughout all mole fractions of these liquid mixtures, in addition to the intermolecular water-water and organic molecule-organic molecule interactions, which are present in neat water and organic liquids, respectively. On the basis of this finding, we proposed a new analytical method for studying liquid mixtures. By this method the structural information on the intermolecular water-organic molecule interaction as well as the concentrations of the intermolecular water-water, water-organic molecules, and organic molecule-organic molecule interactions were obtained. Combining the concentrations of the intermolecular water-water interaction with the concentrations of water in the liquid mixtures, the number of water-water hydrogen bonds at various mole fractions was experimentally determined for the first time. From the dependence of the number of water-water hydrogen bonds on the composition of the liquid mixtures, the change of the size of the self-associated water-water clusters was deduced.",
 				"issue": "6",
 				"journalAbbreviation": "Journal of Solution Chemistry",
 				"libraryCatalog": "Scholars Portal Journals",
 				"pages": "841-856",
 				"publicationTitle": "Journal of Solution Chemistry",
-				"url": "http://journals.scholarsportal.info/details/00959782/v37i0006/841_tnowhbdbmoxs.xml",
+				"url": "http://journals.scholarsportal.info/detailsr.scholarsportal.info.xml",
 				"volume": "37",
 				"attachments": [
 					{
@@ -174,12 +183,24 @@ var testCases = [
 					}
 				],
 				"tags": [
-					"Acetone",
-					"Binary liquid mixtures",
-					"Tetrahydrofuran",
-					"Water",
-					"Water-water hydrogen bonds",
-					"X-ray scattering"
+					{
+						"tag": "Acetone"
+					},
+					{
+						"tag": "Binary liquid mixtures"
+					},
+					{
+						"tag": "Tetrahydrofuran"
+					},
+					{
+						"tag": "Water"
+					},
+					{
+						"tag": "Water-water hydrogen bonds"
+					},
+					{
+						"tag": "X-ray scattering"
+					}
 				],
 				"notes": [],
 				"seeAlso": []
@@ -188,7 +209,7 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "http://journals1.scholarsportal.info/browse/toc?uri=/08939454&p=1",
+		"url": "https://journals.scholarsportal.info/browse/toc?uri=/08939454&p=1",
 		"items": "multiple"
 	}
 ]
