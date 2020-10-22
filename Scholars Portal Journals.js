c26ec4b8b4d8c371f094b5d8e0f2a95f8ca8e557
bartek kawula <b.kawula@utoronto.ca>
2020-10-22 18:41:11 -0400

    Even more linting fixes!

--- Scholars Portal Journals.js
+++ Scholars Portal Journals.js
@@ -35,16 +35,18 @@
 	***** END LICENSE BLOCK *****
 */
 
-function detectWeb(doc, url) {
+function detectWeb (doc, url) {
 	if (url.includes('/my-articles')) {
 		if (getItems(doc)) {
 			return 'multiple';
 		}
-	} else if (url.includes('/search?q')) {
+	} 
+	else if (url.includes('/search?q')) {
 		if (getItems(doc)) {
 			return 'multiple';
 		}
-	} else if (url.includes('/browse/')) {
+	} 
+	else if (url.includes('/browse/')) {
 		let browse = doc.getElementById('toc');
 		if (browse) {
 			Zotero.monitorDOMChanges(browse.parentElement);
@@ -52,13 +54,14 @@ function detectWeb(doc, url) {
 		if (getItems(doc)) {
 			return 'multiple';
 		}
-	} else if (url.includes('/details/')) {
+	} 
+	else if (url.includes('/details/')) {
 		return 'journalArticle';
 	}
 	return false;
 }
 
-function doWeb(doc, url) {
+function doWeb (doc, url) {
 	let type = detectWeb(doc, url);
 	if (type == 'multiple') {
 		let list = getItems(doc);
@@ -70,9 +73,11 @@ function doWeb(doc, url) {
 				articles.push(article);
 			}
 			ZU.doGet(articles, scrape);
+			return false;
 		}
 		);
-	} else {
+	} 
+	else {
 		let uri = getURI(url);
 		let article = '/ris?uri=' + uri;
 		ZU.doGet(article, scrape);
@@ -80,37 +85,40 @@ function doWeb(doc, url) {
 	return false;
 }
 
-function getURI(url){
+function getURI (url){
 	var a = '';
 	var b = '';
 	if (url.includes('/details/')) {
 		a = url.indexOf('details');
 		b = url.indexOf('xml');
-		return url.substring(a + 7,b + 3);
-	} else if (url.includes('/resolve/')) {
+		return url.substring(a + 7, b + 3);
+	} 
+	else if (url.includes('/resolve/')) {
 		if (url.includes('.xml')){
 			a = url.indexOf('/resolve/');
 			b = url.indexOf('xml');
-			return url.substring(a + 9,b + 3);
-		} else {
+			return url.substring(a + 9, b + 3);
+		} 
+		else {
 			return '/' + url.split('/resolve/')[1] + '.xml';
 		}
 	}
 	return false;
 }
 
-function getItems(doc) {
+function getItems (doc) {
 	var items = {}, found = false;
 	var titles = '';
 	if (doc.URL.includes('/my-articles')) {
 		titles = ZU.xpath(doc.getElementById('my-articles-list'), './/div[@class = "title"]/h3/a');
-		for (let i = 0; i < titles.length; i++) { 
+		for (let i = 0; i < titles.length; i++) {
 			let title = ZU.trimInternal(titles[i].textContent);
 			let uri = getURI(titles[i].href);
 			items[uri] = title;
 			found = true;
 		}
-	} else {
+	} 
+	else {
 		if (doc.URL.includes('/browse')){
 			titles = ZU.xpath(doc, './/div/h3/a');
 		}
@@ -127,7 +135,7 @@ function getItems(doc) {
 	return found ? items : false;
 }
 
-function scrape(text) {
+function scrape (text) {
 	// loading RIS transformer.
 	let translator = Zotero.loadTranslator('import');
 	translator.setTranslator('32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7');
