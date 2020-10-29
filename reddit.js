8472d7e96ac5730d8ede648613797b855a886dda
Lukas Kawerau <lukas@kawerau.org>
2020-10-29 09:27:00 +0100

    Add search, multiples and individual comments

--- reddit.js
+++ reddit.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-10-28 15:41:29"
+	"lastUpdated": "2020-10-29 08:25:52"
 }
 
 /*
@@ -30,49 +30,42 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-function attr(docOrElem, selector, attr, index) {
-	var elem = index ? docOrElem.querySelectorAll(selector).item(index) : docOrElem.querySelector(selector);
-	return elem ? elem.getAttribute(attr) : null;
-}
-
-function text(docOrElem, selector, index) {
-	var elem = index ? docOrElem.querySelectorAll(selector).item(index) : docOrElem.querySelector(selector);
-	return elem ? elem.textContent : null;
-}
+// attr()/text() v2
+// eslint-disable-next-line
+function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null}
 
 function detectWeb(doc, url) {
-	// Adjust the inspection of url as required
-	if (url.indexOf('search') != -1 && getSearchResults(doc, true)) {
+	if (url.includes('search') && getSearchResults(doc, true)) {
+		return 'multiple';
+	} 
+	else if (url.match(/\/r\/[a-z\d]+\/comments\//g)) {
+		return 'forumPost';
+	}  else {
 		return 'multiple';
 	}
-	// Adjust the inspection of url as required
-	else {
-		return 'forumPost';
-	}
-	// Add other cases if needed
 }
 
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	// Adjust the CSS Selectors
-	var rows = doc.querySelectorAll('.mw-search-result-heading a');
-	for (var i=0; i<rows.length; i++) {
-		// Adjust if required, use Zotero.debug(rows) to check
-		var href = rows[i].href;
-		// Adjust if required, use Zotero.debug(rows) to check
-		var title = ZU.trimInternal(rows[i].textContent);
+	var rows = doc.querySelectorAll('.SQnoC3ObvgnGjWt90zD9Z._2INHSNB8V5eaWp4P0rY_mE');
+	for (let row of rows) {
+		var href = row.href + '.json';
+		var title = ZU.trimInternal(row.textContent);
 		if (!href || !title) continue;
 		if (checkOnly) return true;
 		found = true;
 		items[href] = title;
 	}
+
 	return found ? items : false;
 }
 
 function doWeb(doc, url) {
+	var json_url = url + '.json';
 	if (detectWeb(doc, url) == "multiple") {
 		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			Zotero.debug(items);
 			if (!items) {
 				return true;
 			}
@@ -80,55 +73,127 @@ function doWeb(doc, url) {
 			for (var i in items) {
 				articles.push(i);
 			}
-			ZU.processDocuments(articles, scrape);
+			ZU.doGet(articles, scrape);
 		});
+	} else if (detectWeb(doc, url) == "forumPost" && url.match(/\/r\/[a-z\d]+\/comments\/[a-z\d]{6}\/[a-z\d_]+\/[a-z\d]{7}\//g)) {
+		ZU.doGet(json_url, scrape_comment);
 	} else {
-		scrape(doc, url);
+		ZU.doGet(json_url, scrape);
 	}
 }
 
 
-function scrape(doc, url) {
-	var json_url = url + '.json'
-	Zotero.Utilities.HTTP.doGet(json_url, function(text) {
-		var newItem = new Zotero.Item("forumPost");
-		var reddit_data = JSON.parse(text);
-		newItem.title = reddit_data[0]["data"]["children"][0]["data"]["title"];
-		newItem.creators.push(ZU.cleanAuthor(reddit_data[0]["data"]["children"][0]["data"]["author"], "author", false));
-		newItem.url = 'www.reddit.com' + reddit_data[0]["data"]["children"][0]["data"]["permalink"];
-		var post_date = new Date(reddit_data[0]["data"]["children"][0]["data"]["created_utc"]*1000);
-		newItem.date = post_date.toISOString();
-		// Zotero.debug(reddit_data);
-		newItem.postType = "Reddit Post";
-		newItem.forumTitle = 'r/'+reddit_data[0]["data"]["children"][0]["data"]["subreddit"];
-		newItem.websiteTitle = "reddit.com";
-		newItem.complete();
-		//Zotero.debug(newItem);
-		Zotero.done();
-	}, function() {});
+function scrape(text) {
+	var newItem = new Zotero.Item("forumPost");
+	var reddit_json = JSON.parse(text);
+	var reddit_data = reddit_json[0]["data"]["children"][0]["data"];
+	newItem.title = reddit_data["title"];
+	newItem.creators.push(ZU.cleanAuthor(reddit_data["author"], "author", true));
+	newItem.url = 'www.reddit.com' + reddit_data["permalink"];
+	var post_date = new Date(reddit_data["created_utc"]*1000);
+	newItem.date = post_date.toISOString();
+	newItem.postType = "Reddit Post";
+	newItem.forumTitle = 'r/'+reddit_data["subreddit"];
+	newItem.websiteTitle = "reddit.com";
+	newItem.attachments.push({
+    	url : 'https://www.reddit.com' + reddit_data["permalink"],
+		title : "Reddit Post Snapshot",
+		type : "text/html"
+	});
+	newItem.complete();
+}
 
+function scrape_comment(text) {
+	var newItem = new Zotero.Item("forumPost");
+	var reddit_json = JSON.parse(text);
+	var parent_data = reddit_json[0]["data"]["children"][0]["data"];
+	var reddit_data = reddit_json[1]["data"]["children"][0]["data"];
+	newItem.title = ZU.ellipsize(reddit_data["body"], 20);
+	newItem.creators.push(ZU.cleanAuthor(reddit_data["author"], "author", true));
+	newItem.url = 'www.reddit.com' + reddit_data["permalink"];
+	var post_date = new Date(reddit_data["created_utc"]*1000);
+	newItem.date = post_date.toISOString();
+	newItem.postType = "Reddit Comment";
+	newItem.forumTitle = 'r/'+reddit_data["subreddit"];
+	newItem.websiteTitle = "reddit.com";
+	newItem.extra = 'Post URL: www.reddit.com' + parent_data["permalink"]; 
+	newItem.attachments.push({
+    	url : 'https://www.reddit.com' + reddit_data["permalink"],
+		title : "Reddit Comment Snapshot",
+		type : "text/html"
+	});
+	newItem.complete();
 }
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "https://www.reddit.com/r/spacex/comments/jj7arf/spacex_partners_with_leolabs_to_track_starlink/",
+		"url": "https://www.reddit.com/search/?q=zotero",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.reddit.com/r/zotero/",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.reddit.com/",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.reddit.com/r/zotero/comments/j7ityb/zotero_ipad_bookmarklet_not_working/",
 		"items": [
 			{
 				"itemType": "forumPost",
-				"title": "SpaceX Partners with LeoLabs to Track Starlink Satellites",
+				"title": "Zotero iPad bookmarklet not working",
 				"creators": [
 					{
-						"firstName": "",
-						"lastName": "SpikePlayz",
+						"lastName": "cegmondiale",
 						"creatorType": "author"
 					}
 				],
-				"date": "2020-10-27T18:54:37.000Z",
-				"forumTitle": "r/spacex",
+				"date": "2020-10-08T18:43:48.000Z",
+				"forumTitle": "r/zotero",
 				"postType": "Reddit Post",
-				"url": "www.reddit.com/r/spacex/comments/jj7arf/spacex_partners_with_leolabs_to_track_starlink/",
-				"attachments": [],
+				"url": "www.reddit.com/r/zotero/comments/j7ityb/zotero_ipad_bookmarklet_not_working/",
+				"attachments": [
+					{
+						"title": "Reddit Post Snapshot",
+						"type": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.reddit.com/r/zotero/comments/j7ityb/zotero_ipad_bookmarklet_not_working/g88zfcp/",
+		"items": [
+			{
+				"itemType": "forumPost",
+				"title": "I use the exact same…",
+				"creators": [
+					{
+						"lastName": "lukelbd93",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020-10-09T20:07:37.000Z",
+				"extra": "Post URL: www.reddit.com/r/zotero/comments/j7ityb/zotero_ipad_bookmarklet_not_working/",
+				"forumTitle": "r/zotero",
+				"postType": "Reddit Comment",
+				"url": "www.reddit.com/r/zotero/comments/j7ityb/zotero_ipad_bookmarklet_not_working/g88zfcp/",
+				"attachments": [
+					{
+						"title": "Reddit Comment Snapshot",
+						"type": "text/html"
+					}
+				],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
