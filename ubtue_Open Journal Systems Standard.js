1fd8f65e0ae0c76509f10a9b9e0afd15c0d752f5
socheres <kim.changhe@googlemail.com>
2020-11-04 18:00:25 +0100

    merge from ztk

--- ubtue_Open Journal Systems Standard.js
+++ ubtue_Open Journal Systems Standard.js
@@ -1,15 +1,15 @@
 {
 	"translatorID": "a5d5ca83-b975-4abe-86c9-d956d7b9c8fa",
-	"label": "Open Journal Systems Standard",
+	"label": "ubtue_Open Journal Systems Standard",
 	"creator": "Timotheus Kim",
-	"target": "article|issue/view/",
+	"target": "/(article|issue)/(view)?",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
-	"inRepository": false,
+	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-08-27 11:23:02"
+	"lastUpdated": "2020-11-04 17:35:40"
 }
 
 /*
@@ -29,18 +29,17 @@
 */
 
 function detectWeb(doc, url) {
-	if (url.match(/article/)) return "journalArticle";
-		else if (url.match(/\/issue\/view/) && getSearchResults(doc)) return "multiple";
-	else return false;
+	if (url.match(/\/article\/view/)) return "journalArticle"
+	else if (url.match(/\/issue\/view/) && getSearchResults(doc)) return "multiple";
 }
 
 function getSearchResults(doc) {
 	var items = {};
 	var found = false;
-	var rows = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "media-heading", " " ))]//a | //*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]//a | //a[contains(@href, "/article/view/") and not(contains(@href, "/pdf"))]');
+	var rows = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "media-heading", " " ))]//a | //*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]//a | //*[(@id = "content")]//a');
 	for (let row of rows) {
 		let href = row.href;
-		let title = ZU.trimInternal(row.textContent);
+		let title = ZU.trimInternal(row.textContent).replace(/pdf/i, '');
 		if (!href || !title) continue;
 		found = true;
 		items[href] = title;
@@ -53,12 +52,15 @@ function invokeEMTranslator(doc) {
 	translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
 	translator.setDocument(doc);
 	translator.setHandler("itemDone", function (t, i) {
-		if (i.pages.match(/^\d{1,3}–\d{1,3}-\d{1,3}–\d{1,3}/)) {
-			let firstandlastpages = i.pages.split('–');
-			i.pages = firstandlastpages[0] + '-' + firstandlastpages[2] ; // Z.debug(item.pages)
-		}
+		var firstPage = ZU.xpathText(doc, '//meta[@name="citation_firstpage"]/@content');
+		var lastPage = ZU.xpathText(doc, '//meta[@name="citation_lastpage"]/@content');
+		var firstandlastPages = i.pages.split('-');//Z.debug(firstandlastPages)
+		if (firstandlastPages[0] === firstandlastPages[1]) i.pages = firstandlastPages[0];
 		if (i.issue === "0") delete i.issue;
-		if (i.abstractNote.match(/No abstract available/)) delete i.abstractNote;
+		if (i.abstractNote !== undefined) {
+			if (i.abstractNote.match(/No abstract available/)) delete i.abstractNote;
+		}
+		if (i.tags[0] === "book review") i.tags.push('RezensionstagPica') && delete i.tags[0];
 		i.complete();
 	});
 	translator.translate();
@@ -83,11 +85,6 @@ function doWeb(doc, url) {
 var testCases = [
 	{
 		"type": "web",
-		"url": "https://ojs.reformedjournals.co.za/stj/issue/view/70",
-		"items": "multiple"
-	},
-	{
-		"type": "web",
 		"url": "https://ojs.reformedjournals.co.za/stj/article/view/1969",
 		"items": [
 			{
@@ -108,7 +105,7 @@ var testCases = [
 				"journalAbbreviation": "STJ",
 				"language": "en",
 				"libraryCatalog": "ojs.reformedjournals.co.za",
-				"pages": "13-40",
+				"pages": "13–40",
 				"publicationTitle": "STJ | Stellenbosch Theological Journal",
 				"rights": "Copyright (c) 2020 Pieter de Waal Neethling Trust, Stellenbosch",
 				"shortTitle": "“The message to the people of South Africa” in contemporary context",
@@ -160,6 +157,225 @@ var testCases = [
 					}
 				],
 				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://jps.library.utoronto.ca/index.php/renref/article/view/34078",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Becoming “Indians”: The Jesuit Missionary Path from Italy to Asia",
+				"creators": [
+					{
+						"firstName": "Camilla",
+						"lastName": "Russell",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020/04/30",
+				"DOI": "10.33137/rr.v43i1.34078",
+				"ISSN": "2293-7374",
+				"abstractNote": "The Jesuit missions in Asia were among the most audacious undertakings by Europeans in the early modern period. This article focuses on a still relatively little understood aspect of the enterprise: its appointment process. It draws together disparate archival documents to recreate the steps to becoming a Jesuit missionary, specifically the Litterae indipetae (petitions for the “Indies”), provincial reports about missionary candidates, and replies to applicants from the Jesuit superior general. Focusing on candidates from the Italian provinces of the Society of Jesus, the article outlines not just how Jesuit missionaries were appointed but also the priorities, motivations, and attitudes that informed their assessment and selection. Missionaries were made, the study shows, through a specific “way of proceeding” that was negotiated between all parties and seen in both organizational and spiritual terms, beginning with the vocation itself, which, whether the applicant departed or not, earned him the name indiano.",
+				"issue": "1",
+				"journalAbbreviation": "1",
+				"language": "en",
+				"libraryCatalog": "jps.library.utoronto.ca",
+				"pages": "9-50",
+				"publicationTitle": "Renaissance and Reformation",
+				"rights": "Copyright (c)",
+				"shortTitle": "Becoming “Indians”",
+				"url": "https://jps.library.utoronto.ca/index.php/renref/article/view/34078",
+				"volume": "43",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://jps.library.utoronto.ca/index.php/renref/article/view/34078",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Becoming “Indians”: The Jesuit Missionary Path from Italy to Asia",
+				"creators": [
+					{
+						"firstName": "Camilla",
+						"lastName": "Russell",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020/04/30",
+				"DOI": "10.33137/rr.v43i1.34078",
+				"ISSN": "2293-7374",
+				"abstractNote": "The Jesuit missions in Asia were among the most audacious undertakings by Europeans in the early modern period. This article focuses on a still relatively little understood aspect of the enterprise: its appointment process. It draws together disparate archival documents to recreate the steps to becoming a Jesuit missionary, specifically the Litterae indipetae (petitions for the “Indies”), provincial reports about missionary candidates, and replies to applicants from the Jesuit superior general. Focusing on candidates from the Italian provinces of the Society of Jesus, the article outlines not just how Jesuit missionaries were appointed but also the priorities, motivations, and attitudes that informed their assessment and selection. Missionaries were made, the study shows, through a specific “way of proceeding” that was negotiated between all parties and seen in both organizational and spiritual terms, beginning with the vocation itself, which, whether the applicant departed or not, earned him the name indiano.",
+				"issue": "1",
+				"journalAbbreviation": "1",
+				"language": "en",
+				"libraryCatalog": "jps.library.utoronto.ca",
+				"pages": "9-50",
+				"publicationTitle": "Renaissance and Reformation",
+				"rights": "Copyright (c)",
+				"shortTitle": "Becoming “Indians”",
+				"url": "https://jps.library.utoronto.ca/index.php/renref/article/view/34078",
+				"volume": "43",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://jsri.ro/ojs/index.php/jsri/article/view/1194",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Time as a Basic Factor of the Development of Family Relationships in Slovakia",
+				"creators": [
+					{
+						"firstName": "Ladislav",
+						"lastName": "Csontos",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Rastislav",
+						"lastName": "Bednarik",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Jozef",
+						"lastName": "Žuffa",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020/06/26",
+				"ISSN": "1583-0039",
+				"abstractNote": "In the search for factors affecting the stability of marriage and family, support for the family in changing conditions of adult access to children is based on findings of its empirical research that identified selected value and religious aspects of the family. These were enriched by sociological studies of religiosity and scientific studies from the field of psychology and pedagogy. This made it possible to identify family time spent in building relationships as one of the key factors of its stability. The study also includes some aspects of religious beliefs and their implications on declared values, as well as suggestions for creation of specific pastoral plans.",
+				"issue": "56",
+				"language": "en",
+				"libraryCatalog": "jsri.ro",
+				"pages": "3-16",
+				"publicationTitle": "Journal for the Study of Religions and Ideologies",
+				"rights": "Both JSRI and the authors holds the copyright of all published materials. In addition, authors have the right to use all or part of their texts and abstracts for their own personal use and for their teaching purposes.   Authors have the right to use all or part of the text and abstract, in the preparation of derivative works, extension of the article into book-length or in other works, and the right to include the article in full or in part in a thesis or dissertation or books. Authors are kindly asked to provide acknowledgement of the original publication in JSRI, including the title of the article, the journal name, volume, issue number, page numbers, and year of publication.   For use in non-commercial situations there is no need for authors to apply for written permission from JSRI in advance.",
+				"url": "http://jsri.ro/ojs/index.php/jsri/article/view/1194",
+				"volume": "19",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Marriage"
+					},
+					{
+						"tag": "communication"
+					},
+					{
+						"tag": "counseling"
+					},
+					{
+						"tag": "family"
+					},
+					{
+						"tag": "relationship"
+					},
+					{
+						"tag": "trust"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://jsri.ro/ojs/index.php/jsri/article/view/1212",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Eco-Religious Approach to Deforestation by Indonesian Istighosa Community",
+				"creators": [
+					{
+						"firstName": "Wildana",
+						"lastName": "Wargadinata",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Iffat",
+						"lastName": "Maimunah",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Rohmani Nur",
+						"lastName": "Indah",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020/06/25",
+				"ISSN": "1583-0039",
+				"abstractNote": "This paper aims to explain the involvement of an istighosah community in environmental conservation in Indonesia. The data were obtained through the method of observing religious activities and conservation actions, interviewing the community leaders, and documenting the existence and activity of worshipers. The finding confirmed three aspects. First, the implementation of Sufism teachings is an alternative in nature conservation, not only aiming to meet the spiritual needs of pilgrims, but also fostering awareness of pilgrims to prevent damage to nature. Second, what is shown by pilgrims is very closely related to the human urge to always realize its basic capacity as a leader for nature. The forest is considered as a brother that the sustainability must be guarded, preserved, and guaranteed. Third, the teachings of Sufism become the basis for the group to act to be involved in caring for nature. This doctrine is inherited through formal media such as recitation and carried out in the form of direct action. Therefore, further studies are needed to explore the involvement of other religious organizations in the effort to conserve the environment in a sustainable manner",
+				"issue": "56",
+				"language": "en",
+				"libraryCatalog": "jsri.ro",
+				"pages": "166-178",
+				"publicationTitle": "Journal for the Study of Religions and Ideologies",
+				"rights": "Both JSRI and the authors holds the copyright of all published materials. In addition, authors have the right to use all or part of their texts and abstracts for their own personal use and for their teaching purposes.   Authors have the right to use all or part of the text and abstract, in the preparation of derivative works, extension of the article into book-length or in other works, and the right to include the article in full or in part in a thesis or dissertation or books. Authors are kindly asked to provide acknowledgement of the original publication in JSRI, including the title of the article, the journal name, volume, issue number, page numbers, and year of publication.   For use in non-commercial situations there is no need for authors to apply for written permission from JSRI in advance.",
+				"url": "http://jsri.ro/ojs/index.php/jsri/article/view/1212",
+				"volume": "19",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "conservation"
+					},
+					{
+						"tag": "deforestation"
+					},
+					{
+						"tag": "eco-religious approach"
+					}
+				],
 				"notes": [],
 				"seeAlso": []
 			}
