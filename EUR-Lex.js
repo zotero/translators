91f9c18ffeacfb41d8b1968ec8e5c2fcfa367e5a
pjrwees <pjrwees@gmail.com>
2021-01-19 13:46:07 +0100

    rollback Eur-lex in branch pending pull request

--- EUR-Lex.js
+++ EUR-Lex.js
@@ -1,21 +1,21 @@
 {
 	"translatorID": "bf053edc-a8c3-458c-93db-6d04ead2e636",
 	"label": "EUR-Lex",
-	"creator": "Philipp Zumstein, Pieter van der Wees",
-	"target": "^https?://(www\\.)?eur-lex\\.europa\\.eu/(legal-content/[A-Z][A-Z]/(TXT|ALL)/|search.html\\?)",
+	"creator": "Philipp Zumstein",
+	"target": "^https?://(www\\.)?eur-lex\\.europa\\.eu/(legal-content/[A-Z][A-Z]/TXT/|search.html\\?)",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-01-13 13:04:33"
+	"lastUpdated": "2017-12-21 20:27:07"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2017-2020 Philipp Zumstein, Pieter van der Wees
+	Copyright © 2017 Philipp Zumstein
 	
 	This file is part of Zotero.
 
@@ -36,42 +36,32 @@
 */
 
 
-// attr() v2
-function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}
+// attr()/text() v2
+function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}
 
 
 // the eli resource types are described at:
-// https://op.europa.eu/en/web/eu-vocabularies/at-dataset/-/resource/dataset/resource-type
-// all types not mapped below are saved as bills
+// http://publications.europa.eu/mdr/resource/authority/resource-type/html/resourcetypes-eng.html
 var typeMapping = {
-	"DIR": "bill", // directive
-	"REG": "statute", // regulation
-	"DEC": "statute", // decision
-	"RECO": "report", // recommendation
-	"OPI": "report", // opinion
-	"CONS": "statute", // consolidated text
-	"TREATY": "statute", // treaty
+	'DIR': 'bill', // directive
+	'REG': 'statute', // regulation
+	'DEC': 'statute', // decision
+	'RECO': 'report', // recommodation
+	'OPI': 'report' // opinion
 };
 
 
 function detectWeb(doc, url) {
-	var docSector = attr(doc, 'meta[name="WT.z_docSector"]', 'content');
 	var eliTypeURI = attr(doc, 'meta[property="eli:type_document"]', 'resource');
 	if (eliTypeURI) {
-		var eliType = eliTypeURI.split("/").pop();
-		var eliCategory = eliType.split("_")[0];
+		var eliType = eliTypeURI.split('/').pop();
+		var eliCategory = eliType.split('_')[0];
 		var type = typeMapping[eliCategory];
 		if (type) {
 			return type;
-		}
-		else {
+		} else {
 			Z.debug("Unknown eliType: " + eliType);
-			return "bill";
 		}
-	} else if (docSector == "6") {
-			return "case";
-	} else if (docSector && docSector !== "other") { 
-			return "bill";
 	} else if (getSearchResults(doc, true)) {
 		return "multiple";
 	}
@@ -81,7 +71,7 @@ function detectWeb(doc, url) {
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	var rows = doc.querySelectorAll("a.title");
+	var rows = doc.querySelectorAll('a.title');
 	for (let i=0; i<rows.length; i++) {
 		let href = rows[i].href;
 		let title = ZU.trimInternal(rows[i].textContent);
@@ -115,175 +105,112 @@ function doWeb(doc, url) {
 			}
 			ZU.processDocuments(articles, scrape);
 		});
-	} 
-	else {
+	} else {
 		scrape(doc, url);
 	}
 }
 
 
-// this maps language codes from ISO 639-1 to 639-3 and adds court names
-// see https://op.europa.eu/en/web/eu-vocabularies/at-dataset/-/resource/dataset/court-type
+// this maps language codes from ISO 639-1 to 639-3
 var languageMapping = {
-	"BG": ["BUL", "Съд", "Общ съд", "Съд на публичната служба"],
-	"CS": ["CES", "Soudní dvůr", "Tribunál", "Soud pro veřejnou službu"],
-	"DA": ["DAN", "EU-Domstolen", "EU-Retten", "EU-Personalretten"],
-	"DE": ["DEU", "EuGH", "Gerichtshof", "Gericht für den öffentlichen Dienst"],
-	"EL": ["ELL", "Δικαστήριο", "Γενικό Δικαστήριο", "Δικαστήριο Δημόσιας Διοίκησης"],
-	"EN": ["ENG", "ECJ", "GC", "CST"],
-	"ES": ["SPA", "TJUE", "Tribunal General", "Tribunal de la Función Pública"],
-	"ET": ["EST", "Euroopa Kohus", "Üldkohus", "Avaliku Teenistuse Kohus"],
-	"FI": ["FIN", "Unionin tuomioistuin", "Unionin yleinen tuomioistuin", "Virkamiestuomioistuin"],
-	"FR": ["FRA", "Cour de justice", "Tribunal", "Tribunal de la fonction publique"],
-	"GA": ["GLE", "An Chúirt Bhreithiúnais", "Cúirt Ghinearálta", "An Binse um Sheirbhís Shibhialta"],
-	"HR": ["HRV", "Europski sud", "Opći sud", "Službenički sud"],
-	"HU": ["HUN", "A Bíróság", "Törvényszék", "Közszolgálati Törvényszék"],
-	"IT": ["ITA", "Corte di giustizia", "Tribunale", "Tribunale della funzione pubblica"],
-	"LV": ["LAV", "Tiesa", "Vispārējā tiesa", "Civildienesta tiesa"],
-	"LT": ["LIT", "Teisingumo Teismas", "Bendrasis Teismas", "Tarnautojų teismas"],
-	"MT": ["MLT", "Il-Qorti tal-Ġustizzja", "Il-Qorti Ġenerali", "Il-Tribunal għas-Servizz Pubbliku"],
-	"NL": ["NLD", "HvJ EU", "Gerecht EU", "GvA EU"],
-	"PL": ["POL", "Trybunał Sprawiedliwości", "Sąd", "Sąd do spraw Służby Publicznej"],
-	"PT": ["POR", "Tribunal Europeu de Justiça", "Tribunal Geral", "Tribunal da Função Pública"],
-	"RO": ["RON", "Curtea Europeană de Justiție", "Tribunalul", "Tribunalul Funcţiei Publice"],
-	"SK": ["SLK", "Súdny dvor", "Všeobecný súd", "Súd pre verejnú službu"],
-	"SL": ["SLV", "Sodišče", "Splošno sodišče", "Sodišče za uslužbence"],
-	"SV": ["SWE", "Domstolen", "Tribunalen", "Personaldomstolen"]
+	'BG': 'bul',
+	'CS': 'ces',
+	'DA': 'dan',
+	'DE': 'deu',
+	'EL': 'ell',
+	'EN': 'eng',
+	'ES': 'spa',
+	'ET': 'est',
+	'FI': 'fin',
+	'FR': 'fra',
+	'GA': 'gle',
+	'HR': 'hrv',
+	'HU': 'hun',
+	'IT': 'ita',
+	'LV': 'lav',
+	'LT': 'lit',
+	'MT': 'mlt',
+	'NL': 'nld',
+	'PL': 'pol',
+	'PT': 'por',
+	'RO': 'ron',
+	'SK': 'slk',
+	'SL': 'slv',
+	'SV': 'swe'
 };
 
 
 function scrape(doc, url) {
-	// declare meta variables present for all sectors
-	var docSector = attr(doc, 'meta[name="WT.z_docSector"]', 'content');
-	var docType = attr(doc, 'meta[name="WT.z_docType"]', 'content');
-	var celex = attr(doc, 'meta[name="WT.z_docID"]', 'content');
-	
-	var eliTypeUri = (attr(doc, 'meta[property="eli:type_document"]', "resource"));
-	
 	var type = detectWeb(doc, url);
 	var item = new Zotero.Item(type);
+	
 	// determine the language we are currently looking the document at
-	var languageUrl = url.split("/")[4].toUpperCase();
-	if (languageUrl == "AUTO") {
+	var languageUrl = url.split('/')[4];
+	if (languageUrl=="AUTO") {
 		languageUrl = autoLanguage || "EN";
 	}
-	var language = languageMapping[languageUrl][0] || "eng";
-	// Cases only return language; discard everything else
+	var language = languageMapping[languageUrl] || "eng";
+	
+	item.title = attr(doc, 'meta[property="eli:title"][lang=' + languageUrl.toLowerCase() + ']', 'content');
 	item.language = languageUrl.toLowerCase();
 	
-
-	if (eliTypeUri) {
-		// type: everything with ELI (see var typeMapping: bill, statute, report)
-		item.title = attr(doc, 'meta[property="eli:title"][lang=' + languageUrl.toLowerCase() + "]", "content");
-		var uri = attr(doc, "#format_language_table_digital_sign_act_" + languageUrl.toUpperCase(), "href");
-		if (uri) {
-			var uriParts = uri.split("/").pop().replace("?uri=", "").split(":");
-			// e.g. uriParts =  ["OJ", "L", "1995", "281", "TOC"]
-			// e.g. uriParts = ["DD", "03", "061", "TOC", "FI"]
-			if (uriParts.length >= 4) {
-				if (/\d+/.test(uriParts[1])) {
-					item.code = uriParts[0];
-					item.codeNumber = uriParts[1] + ", " + uriParts[2];
-				} 
-				else {
-					item.code = uriParts[0] + " " + uriParts[1];
-					item.codeNumber = uriParts[3];
-				}
-				if (type == "bill") {
-					item.codeVolume = item.codeNumber;
-					item.codeNumber = null;
-				}
+	var uri = attr(doc, '#format_language_table_digital_sign_act_' + languageUrl.toUpperCase(), 'href');
+	if (uri) {
+		var uriParts = uri.split('/').pop().replace('?uri=', '').split(':');
+		// e.g. uriParts =  ["OJ", "L", "1995", "281", "TOC"]
+		// e.g. uriParts = ["DD", "03", "061", "TOC", "FI"]
+		if (uriParts.length>=4) {
+			if (/\d+/.test(uriParts[1])) {
+				item.code = uriParts[0];
+				item.codeNumber = uriParts[1] + ', ' + uriParts[2];
+			} else {
+				item.code = uriParts[0] + ' ' + uriParts[1];
+				item.codeNumber = uriParts[3];
+			}
+			if (type=="bill") {
+				item.codeVolume = item.code;
+				item.code = item.codeNumber;
 			}
 		}
-
-		// item.number = attr(doc, 'meta[property="eli:id_local"]', 'content');
-
-		item.date = attr(doc, 'meta[property="eli:date_document"]', "content");
-		if (!item.date) {item.date = attr(doc, 'meta[property="eli:date_publication"]', "content")}
-		
-		var passedBy = doc.querySelectorAll('meta[property="eli:passed_by"]');
-		var passedByArray = [];
-		for (let i = 0; i < passedBy.length; i++) {
-			passedByArray.push(passedBy[i].getAttribute("resource").split("/").pop());
-		}
-		item.legislativeBody = passedByArray.join(", ");
-		
-		item.url = attr(doc, 'meta[typeOf="eli:LegalResource"]', "about") + "/" + language.toLowerCase();
-		// eli:is_about -> eurovoc -> tags
 	}
-	else if (docSector == "6") {
-		// type: case
-		// pretty hacky stuff, as there's little metadata available
-		var docCourt = docType.substr(0, 1);
-		if (docCourt == "C") {
-			item.court = languageMapping[languageUrl][1] || languageMapping["EN"][1];
-		}
-		else if (docCourt == "T") {
-			item.court = languageMapping[languageUrl][2] || languageMapping["EN"][2];
-		}
-		else if (docCourt == "F") {
-			item.court = languageMapping[languageUrl][3] || languageMapping["EN"][3];
-		}
-		item.url = url;
+	
+	item.number = attr(doc, 'meta[property="eli:id_local"]', 'content');
+	
+	item.date = attr(doc, 'meta[property="eli:date_publication"]', 'content');
+	// attr(doc, 'meta[property="eli:date_document"]', 'content');
 
-		if (docType.substr(1) == "J") { // Judgments
-			var titleParts = attr(doc, 'meta[name="WT.z_docTitle"]', "content").replace(/\./g,"").split("#");
-			item.caseName = titleParts[1];
-			item.abstractNote = titleParts[titleParts.length - 2];
-			item.docketNumber = titleParts.pop();
-			item.dateDecided = titleParts[0].substr(titleParts[0].search(/[0-9]/g));
-		}
-		else { // Orders, summaries, etc.
-			item.caseName = attr(doc, 'meta[name="WT.z_docTitle"]', "content").replace(/#/g," ");
-			item.dateDecided = celex.substr(1, 4);
-		}
+	var passedBy = doc.querySelectorAll('meta[property="eli:passed_by"]');
+	var passedByArray = [];
+	for (let i=0; i<passedBy.length; i++) {
+		passedByArray.push(passedBy[i].getAttribute('resource').split('/').pop());
 	}
-	else {
-		// type: bill
-		item.title = attr(doc, 'meta[name="WT.z_docTitle"]', "content");
-		item.date = celex.substr(1, 4);
-		item.url = url;
-		if (docType == "C") {
-			var celexCParts = celex.substr(1).split("/");
-			item.code = "OJ C";
-			item.codeVolume = celexCParts[1];
-			item.codePages = celexCParts[2];
-		}
-	}
-	// attachments
-	// type: all
-	var pdfurl = "https://eur-lex.europa.eu/legal-content/" + languageUrl + "/TXT/PDF/?uri=CELEX:" + celex;
-	var htmlurl = "https://eur-lex.europa.eu/legal-content/" + languageUrl + "/TXT/HTML/?uri=CELEX:" + celex;
-	item.attachments = [{ url: pdfurl, title: "EUR-Lex PDF (" + languageUrl + ")", mimeType: "application/pdf" }];
-	item.attachments.push({ url: htmlurl, title: "EUR-Lex HTML (" + languageUrl + ")", mimeType: "text/html" });
+	item.legislativeBody = passedByArray.join(', ');
+	
+	item.url = attr(doc, 'meta[typeOf="eli:LegalResource"]', 'about') + '/' + language;
+	
+	// eli:is_about -> eurovoc -> tags
 	
 	item.complete();
-}/** BEGIN TEST CASES **/
+}
+
+/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:31995L0046",
+		"url": "http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:31995L0046",
 		"items": [
 			{
 				"itemType": "bill",
 				"title": "Directive 95/46/EC of the European Parliament and of the Council of 24 October 1995 on the protection of individuals with regard to the processing of personal data and on the free movement of such data",
 				"creators": [],
-				"date": "1995-10-24",
-				"code": "OJ L",
-				"codeVolume": "281",
+				"date": "1995-11-23",
+				"billNumber": "31995L0046",
+				"code": "281",
+				"codeVolume": "OJ L",
 				"language": "en",
 				"legislativeBody": "EP, CONSIL",
 				"url": "http://data.europa.eu/eli/dir/1995/46/oj/eng",
-				"attachments": [
-					{
-						"title": "EUR-Lex PDF (EN)",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "EUR-Lex HTML (EN)",
-						"mimeType": "text/html"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -292,27 +219,63 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "https://eur-lex.europa.eu/legal-content/FR/TXT/?uri=CELEX:31994R2257",
+		"url": "http://eur-lex.europa.eu/legal-content/CS/TXT/?uri=CELEX:31995L0046&from=DE",
+		"items": [
+			{
+				"itemType": "bill",
+				"title": "Směrnice Evropského parlamentu a Rady 95/46/ES ze dne 24. října 1995 o ochraně fyzických osob v souvislosti se zpracováním osobních údajů a o volném pohybu těchto údajů",
+				"creators": [],
+				"date": "1995-11-23",
+				"billNumber": "31995L0046",
+				"code": "13, 015",
+				"codeVolume": "DD",
+				"language": "cs",
+				"legislativeBody": "EP, CONSIL",
+				"url": "http://data.europa.eu/eli/dir/1995/46/oj/ces",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://eur-lex.europa.eu/legal-content/DE/TXT/?uri=CELEX:31995L0046",
+		"items": [
+			{
+				"itemType": "bill",
+				"title": "Richtlinie 95/46/EG des Europäischen Parlaments und des Rates vom 24. Oktober 1995 zum Schutz natürlicher Personen bei der Verarbeitung personenbezogener Daten und zum freien Datenverkehr",
+				"creators": [],
+				"date": "1995-11-23",
+				"billNumber": "31995L0046",
+				"code": "281",
+				"codeVolume": "OJ L",
+				"language": "de",
+				"legislativeBody": "EP, CONSIL",
+				"url": "http://data.europa.eu/eli/dir/1995/46/oj/deu",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://eur-lex.europa.eu/legal-content/FR/TXT/?uri=CELEX:31994R2257",
 		"items": [
 			{
 				"itemType": "statute",
 				"nameOfAct": "Règlement (CE) n° 2257/94 de la Commission, du 16 septembre 1994, fixant des normes de qualité pour les bananes (Texte présentant de l'intérêt pour l'EEE)",
 				"creators": [],
-				"dateEnacted": "1994-09-16",
+				"dateEnacted": "1994-09-20",
 				"code": "OJ L",
 				"codeNumber": "245",
 				"language": "fr",
+				"publicLawNumber": "31994R2257",
 				"url": "http://data.europa.eu/eli/reg/1994/2257/oj/fra",
-				"attachments": [
-					{
-						"title": "EUR-Lex PDF (FR)",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "EUR-Lex HTML (FR)",
-						"mimeType": "text/html"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -321,95 +284,8 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "https://eur-lex.europa.eu/search.html?lang=en&text=%22open+access%22&qid=1513887127793&type=quick&scope=EURLEX&locale=nl",
+		"url": "http://eur-lex.europa.eu/search.html?lang=en&text=%22open+access%22&qid=1513887127793&type=quick&scope=EURLEX&locale=nl",
 		"items": "multiple"
-	},
-	{
-		"type": "web",
-		"url": "https://eur-lex.europa.eu/legal-content/NL/TXT/?uri=CELEX:62019CJ0245",
-		"items": [
-			{
-				"itemType": "case",
-				"caseName": "État luxembourgeois tegen B en État luxembourgeois tegen B ea",
-				"creators": [],
-				"dateDecided": "6 oktober 2020",
-				"abstractNote": "Prejudiciële verwijzing – Richtlijn 2011/16/EU – Administratieve samenwerking op het gebied van de belastingen – Artikelen 1 en 5 – Bevel tot het verstrekken van inlichtingen aan de bevoegde autoriteit van een lidstaat, die handelt naar aanleiding van een verzoek tot uitwisseling van inlichtingen van de bevoegde autoriteit van een andere lidstaat – Persoon die in het bezit is van de informatie waarvan de bevoegde autoriteit van eerstbedoelde lidstaat heeft bevolen dat deze moet worden verstrekt – Belastingplichtige tegen wie het onderzoek loopt dat aanleiding heeft gegeven tot het verzoek van de bevoegde autoriteit van de tweede lidstaat – Derden met wie deze belastingplichtige juridische, bancaire, financiële of, meer in het algemeen, economische banden onderhoudt – Rechtsbescherming – Handvest van de grondrechten van de Europese Unie – Artikel 47 – Recht op een doeltreffende voorziening in rechte – Artikel 52, lid 1 – Beperking – Rechtsgrondslag – Eerbiediging van de wezenlijke inhoud van het recht op een doeltreffende voorziening in rechte – Bestaan van een rechtsmiddel dat de betrokken justitiabelen de mogelijkheid biedt alle relevante feitelijke en juridische kwesties doeltreffend te laten toetsen en een daadwerkelijke rechtsbescherming van hun door het Unierecht gewaarborgde rechten te genieten – Door de Unie erkende doelstelling van algemeen belang – Bestrijding van internationale belastingfraude en ‑ontwijking – Evenredigheid – Criterium dat de informatie waarop het bevel tot het verstrekken van inlichtingen betrekking heeft ‚naar verwachting van belang is’ – Rechterlijke toetsing – Omvang – In aanmerking te nemen persoonlijke, temporele en materiële gegevens",
-				"court": "HvJ EU",
-				"docketNumber": "Gevoegde zaken C-245/19 en C-246/19",
-				"language": "nl",
-				"url": "https://eur-lex.europa.eu/legal-content/NL/TXT/?uri=CELEX:62019CJ0245",
-				"attachments": [
-					{
-						"title": "EUR-Lex PDF (NL)",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "EUR-Lex HTML (NL)",
-						"mimeType": "text/html"
-					}
-				],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "https://eur-lex.europa.eu/legal-content/CS/TXT/?uri=uriserv%3AOJ.C_.2021.014.01.0001.01.CES&toc=OJ%3AC%3A2021%3A014%3ATOC",
-		"items": [
-			{
-				"itemType": "bill",
-				"title": "Bez námitek k navrhovanému spojení (Věc M.10068 — Brookfield/Mansa/Polenergia) (Text s významem pro EHP) 2021/C 14/01",
-				"creators": [],
-				"date": "2021",
-				"language": "cs",
-				"url": "https://eur-lex.europa.eu/legal-content/CS/TXT/?uri=uriserv%3AOJ.C_.2021.014.01.0001.01.CES&toc=OJ%3AC%3A2021%3A014%3ATOC",
-				"attachments": [
-					{
-						"title": "EUR-Lex PDF (CS)",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "EUR-Lex HTML (CS)",
-						"mimeType": "text/html"
-					}
-				],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "https://eur-lex.europa.eu/legal-content/MT/TXT/?uri=CELEX%3A62009TJ0201",
-		"items": [
-			{
-				"itemType": "case",
-				"caseName": "Rügen Fisch AG vs l-Uffiċċju għall-Armonizzazzjoni fis-Suq Intern (trademarks u disinni) (UASI)",
-				"creators": [],
-				"dateDecided": "21 ta' Settembru 2011",
-				"abstractNote": "Trade mark Komunitarja - Proċedimenti għal dikjarazzjoni ta’ invalidità - Trade mark Komunitarja verbali SCOMBER MIX - Raġuni assoluta għal rifjut - Karattru deskrittiv - Artikolu 7(1)(b) u (ċ) tar-Regolament (KE) Nru 40/94 [li sar l-Artikolu 7(1)(b) u (c) tar-Regolament (KE) Nru 207/2009]",
-				"court": "Il-Qorti Ġenerali",
-				"docketNumber": "Kawża T-201/09",
-				"language": "mt",
-				"url": "https://eur-lex.europa.eu/legal-content/MT/TXT/?uri=CELEX%3A62009TJ0201",
-				"attachments": [
-					{
-						"title": "EUR-Lex PDF (MT)",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "EUR-Lex HTML (MT)",
-						"mimeType": "text/html"
-					}
-				],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
 	}
 ]
 /** END TEST CASES **/
