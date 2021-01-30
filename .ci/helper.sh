d594dcf098825d4e4b62f2675003045989dd6299
Dan Stillman <dstillman@zotero.org>
2021-01-30 06:36:02 -0500

    Create helper function with translators-to-check logic
    And create script to use that for linting as well

--- .ci/helper.sh
+++ .ci/helper.sh
@@ -8,7 +8,7 @@ if [[ -t 1 && "$(tput colors)" -gt 0 ]]; then
     export color_reset=$'\e[0m'
 fi
 
-grepTranslatorId () {
+get_translator_id() {
     if [[ -n "$1" ]];then
         grep -r '"translatorID"' "$@" | sed -e 's/[" ,]//g' -e 's/^.*://g'
     else
@@ -18,4 +18,31 @@ grepTranslatorId () {
     fi
 }
 
-# vim: ft=sh
+get_translators_to_check() {
+	# If a PR branch has no conflicts with the master then git
+	# creates a custom merge commit where it merges PR into master.
+	# Travis-CI tests on that commit instead of the HEAD of the PR branch.
+	#
+	# Thus below we first determine if HEAD is a merge commit by checking how
+	# many parents the current HEAD has. If number of parents > 1, then it's a merge commit
+	# in which case we need to diff translator names between HEAD^2 and PR split commit from master.
+	# The above will generally only be the case in CI or if using a custom PR pulling script which
+	# pulls the merge PR commit instead of just the PR branch.
+	#
+	# If the HEAD commit is not a merge then we diff HEAD with PR split commit from master. This is the case
+	# when running from a local development PR branch
+	#
+	# The branching point hash retrieval logic is based on https://stackoverflow.com/a/12185115/3199106
+	
+	TRANSLATORS_TO_CHECK=""
+	
+	# Gets parent commits. Either one or two hashes
+	local parent_commits=($(git show --no-patch --format="%P" HEAD))
+	# Size of $parent_commits array
+	local num_parent_commits=${#parent_commits[@]}
+	if [ $num_parent_commits -gt 1 ]; then
+		TRANSLATORS_TO_CHECK=$(git diff HEAD^2 $(git rev-list "$(git rev-list --first-parent ^master HEAD^2 | tail -n1)^^!") --name-only | grep -e "^[^/]*.js$")
+	else
+		TRANSLATORS_TO_CHECK=$(git diff $(git rev-list "$(git rev-list --first-parent ^master HEAD | tail -n1)^^!") --name-only | grep -e "^[^/]*.js$")
+	fi
+}
