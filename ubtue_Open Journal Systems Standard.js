44f60f29ecb3f1a6930fe80ef5adc045136afaf9
jriedl <johannes.riedl@uni-tuebingen.de>
2021-02-01 17:35:21 +0100

    Replace true by false...

--- ubtue_Open Journal Systems Standard.js
+++ ubtue_Open Journal Systems Standard.js
@@ -6,7 +6,7 @@
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
-	"inRepository": true,
+	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
 	"lastUpdated": "2021-02-01 16:29:58"
