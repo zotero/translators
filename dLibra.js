d06abe3f17ca348b4fdb9b8b87aa914be8cc4c10
Kamil Gronowski <kgronowski@man.poznan.pl>
2021-02-26 12:59:13 +0100

    Update dLibra translator. Add dLibra 6 support.

--- dLibra.js
+++ dLibra.js
@@ -1,7 +1,7 @@
 {
 	"translatorID": "915e3ae2-afa9-4b1d-9780-28ed3defe0ab",
 	"label": "dLibra",
-	"creator": "Pawel Kolodziej <p.kolodziej@gmail.com>",
+	"creator": "Pawel Kolodziej <p.kolodziej@gmail.com> and Kamil Gronowski <kgronowski@man.poznan.pl>",
 	"target": "/.*dlibra/(doccontent|docmetadata|collectiondescription|results)|/dlibra/?",
 	"minVersion": "2.1.9",
 	"maxVersion": "",
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2016-09-12 06:02:53"
+	"lastUpdated": "2021-02-26 11:45:18"
 }
 
 /*
@@ -30,61 +30,67 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//multiple test URL: http://bcul.lib.uni.lodz.pl/dlibra/results?action=SearchAction&skipSearch=true&mdirids=&server%3Atype=both&tempQueryType=-3&encode=false&isExpandable=on&isRemote=off&roleId=-3&queryType=-3&dirids=1&rootid=&query=Karte&localQueryType=-3&remoteQueryType=-2
-
-
 function detectWeb(doc, url) {
-
-	var singleRe = /.*dlibra\/(doccontent|docmetadata|publication).*/;
-	var multipleRe = /.*dlibra\/(collectiondescription|results).*|.*\/dlibra\/?/;
+	let singleRe = /.*dlibra\/(doccontent|docmetadata|publication).*/;
+	let multipleRe = /.*dlibra\/(collectiondescription|results|planned).*|.*\/dlibra\/?/;
 	if (singleRe.test(url))
-		return "book";
+		return "document";
 	if (multipleRe.test(url))
 		return "multiple";
 }
 
-
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) == "multiple") {
+		let items = {};
 
-		var articles = new Array();
-		var itemsXPath = '//ol[@class="itemlist"]/li/a | //td[@class="searchhit"]/b/a | //p[@class="resultTitle"]/b/a[@class="dLSearchResultTitle"]';
-		var titles = doc.evaluate(itemsXPath, doc, null, XPathResult.ANY_TYPE, null);
-		var title;
-		var items = {};
-		while (title = titles.iterateNext()) {
-			items[title.href] = title.textContent;
+		let dlibra5ItemsXPath = '//ol[@class="itemlist"]/li/a | //td[@class="searchhit"]/b/a | //p[@class="resultTitle"]/b/a[@class="dLSearchResultTitle"]';
+		let titles = doc.evaluate(dlibra5ItemsXPath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+		if (titles.snapshotLength > 0) {
+			for (let i = 0, length = titles.snapshotLength; i < length; ++i) {
+				let item = titles.snapshotItem(i);
+				items[item.href] = item.textContent;
+			}
+		} else {
+			let dlibra6ItemsXPath = '//h2[@class="objectbox__text--title"]';
+			titles = doc.evaluate(dlibra6ItemsXPath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+			for (let i = 0, length = titles.snapshotLength; i < length; ++i) {
+				let item = titles.snapshotItem(i);
+				if (item.getAttribute('title')) { //skip Similar in FBC
+					items[item.firstElementChild.getAttribute('href')] = item.getAttribute('title');
+				}
+			}
 		}
 
 		Zotero.selectItems(items, function (items) {
 			if (!items) {
 				return true;
 			}
-			for (var i in items) {
-
-				articles.push(i);
+			let objects = [];
+			for (let i in items) {
+				objects.push(i);
 			}
-			Zotero.Utilities.processDocuments(articles, scrape);
+			ZU.processDocuments(objects, scrape);
 		});
-	} else
+	} else {
 		scrape(doc, url);
-
+	}
 }
 
 function scrape(doc, url) {
-	var reSingle = new RegExp("(.*/dlibra)/(?:doccontent|docmetadata|publication).*[?&]id=([0-9]*).*");
-	var m = reSingle.exec(url);
-	if (!m)
-		return "";
-	var baseUrl = m[1];
-	var id = m[2];
-	var isPIA = baseUrl.match("lib.pia.org.pl|cyfrowaetnografia.pl");
-	Zotero.Utilities.HTTP.doGet(baseUrl + "/rdf.xml?type=e&id=" + id, function (rdf) {
+	let reDlibra5Single = new RegExp("(.*/dlibra)/(?:doccontent|docmetadata|publication).*[?&]id=([0-9]*).*");
+	let m = reDlibra5Single.exec(url);
+	if (m) {
+		dlibra5Scrape(m[1], m[2]);
+	} else {
+		dlibra6Scrape(doc, url);
+	}
+}
 
+function dlibra5Scrape(baseUrl, id) {
+	ZU.HTTP.doGet(baseUrl + "/rdf.xml?type=e&id=" + id, function (rdf) {
 		rdf = rdf.replace(/<\?xml[^>]*\?>/, "");
-		//Z.debug(rdf)
-		var translator = Zotero.loadTranslator("import");
-		translator.setTranslator("5e3ad958-ac79-463d-812b-a86a9235c28f");
+		let translator = Zotero.loadTranslator("import");
+		translator.setTranslator("5e3ad958-ac79-463d-812b-a86a9235c28f"); // RDF importer
 		translator.setString(rdf);
 		translator.setHandler("itemDone", function (obj, item) {
 			if (item.extra) item.notes.push(item.extra);
@@ -93,38 +99,115 @@ function scrape(doc, url) {
 			item.complete();
 		});
 		translator.getTranslatorObject(function (trans) {
-			trans.defaultUnknownType = 'book';
+			trans.defaultUnknownType = 'document';
 			trans.doImport();
 		});
+	});
+}
 
-	})
+function dlibra6Scrape(doc, url) {
+	let translator = Zotero.loadTranslator('web');
+	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48'); // Embedded Metadata
+	translator.setDocument(doc);
+	translator.getTranslatorObject(function (trans) {
+		trans.doWeb(doc, url);
+	});
 }
 
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://bcul.lib.uni.lodz.pl/dlibra/docmetadata?id=1247&from=&dirids=1&ver_id=&lp=2&QI=",
+		"url": "http://cyfrowa.bibliotekakolbuszowa.pl/dlibra/docmetadata?id=945&from=&dirids=1&ver_id=&lp=4&QI=",
 		"items": [
 			{
-				"itemType": "book",
-				"title": "D2. Special Karte von Südpreussen : mit Allergrösster Erlaubniss aus der Königlichen grossen topographischen Vermessungs-Karte, unter Mitwürkung des Directors Langner",
+				"itemType": "document",
+				"title": "Gazeta Świerczowska. Nr 5 (2020)",
 				"creators": [
 					{
-						"firstName": "David",
-						"lastName": "Gilly",
+						"firstName": "Janusz",
+						"lastName": "Tokarz",
 						"creatorType": "author"
 					}
 				],
-				"date": "1802-1803",
-				"abstractNote": "Mapy topograficzne Prus Południowych.13 arkuszy o wymiarach 62 x 82 cm. Skala [ca 1:150000]. Miedzioryt, ręcznie kolorowany",
-				"language": "ger",
+				"date": "2020",
+				"language": "pol",
 				"libraryCatalog": "dLibra",
-				"publisher": "Simon Schropp u. Comp.",
-				"rights": "Biblioteka Uniwersytetu Łódzkiego",
-				"shortTitle": "D2. Special Karte von Südpreussen",
+				"publisher": "Sołectwo Świerczów",
+				"rights": "Prawa zastrzeżone - dostęp nieograniczony / Rights Reserved - Free Access",
 				"attachments": [],
-				"tags": [],
+				"tags": [
+					{
+						"tag": "Rada sołecka"
+					},
+					{
+						"tag": "Świerczów"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://cyfrowa.bibliotekakolbuszowa.pl/dlibra/results?action=SearchAction&skipSearch=true&mdirids=1&server%3Atype=both&tempQueryType=-3&encode=false&isExpandable=on&isRemote=off&roleId=-3&queryType=-3&dirids=1&rootid=&query=gazeta&localQueryType=-3&remoteQueryType=-2",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://demo.dl.psnc.pl/dlibra/planned?action=SimpleSearchAction&type=-6&p=0",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://demo.dl.psnc.pl/dlibra/collectiondescription/1",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://demo.dl.psnc.pl/dlibra/results?q=*&action=SimpleSearchAction&type=-6&p=0",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://demo.dl.psnc.pl/dlibra/publication/1502/edition/1243",
+		"items": [
+			{
+				"itemType": "document",
+				"title": "Pan Tadeusz",
+				"creators": [
+					{
+						"firstName": "Adam",
+						"lastName": "Mickiewicz",
+						"creatorType": "author"
+					}
+				],
+				"date": "1882",
+				"abstractNote": "Adam Mickiewicz, Pan Tadeusz, poezja polska, ilustracje: E. M. Andriolli. (Skan POLONA)",
+				"url": "https://demo.dl.psnc.pl/dlibra/publication/1502/edition/1243",
+				"language": "pol",
+				"libraryCatalog": "demo.dl.psnc.pl",
+				"publisher": "Lwów, Nakładem Księgarni F. H. Richtera. (H. Altbenberg)",
+				"rights": "Open Access",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "poezja"
+					},
+					{
+						"tag": "poezja polska"
+					}
+				],
 				"notes": [],
 				"seeAlso": []
 			}
