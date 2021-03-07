e9a39bdc11f7a73f809e4cbd16eb9f182d471806
Sebastian Karcher <karcher@u.northwestern.edu>
2021-03-07 13:41:28 -0500

    Fix date/license

--- Hindawi Publishers.js
+++ Hindawi Publishers.js
@@ -14,7 +14,7 @@
 
 /*
 	Translator
-   Copyright (C) 2012 Sebastian Karcher an Avram Lyon
+   Copyright (C) 2021 Sebastian Karcher
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as published by
