1ded2c28f0b00e37c289c9499bda88d52641a8b4
Corey Runkel <runkelcorey@gmail.com>
2021-03-21 17:58:58 -0400

    Map language names to ISO; map file extensions to item types

--- ypfs.js
+++ ypfs.js
@@ -2,14 +2,14 @@
 	"translatorID": "5e1f3e08-ca5f-4196-a662-6cf46b3cdaa7",
 	"label": "ypfs",
 	"creator": "Corey Runkel",
-	"target": "https://ypfs.som.yale.edu",
+	"target": "^https?://ypfs.som.yale.edu",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-03-17 20:42:32"
+	"lastUpdated": "2021-03-21 21:57:13"
 }
 
 /*
@@ -51,8 +51,8 @@ function detectWeb(doc, url) {
 	var found = false;
 	var rows = doc.querySelectorAll('a[href*="/library/"]');
 	for (let row of rows) {
-		let href = row.href;
-		let title = ZU.trimInternal(row.text);
+		href = row.href;
+		title = ZU.trimInternal(row.text);
 		if (!href || !title) continue;
 		if (checkOnly) return true;
 		found = true;
@@ -60,7 +60,6 @@ function detectWeb(doc, url) {
 	}
 	return found ? items : false;
 }
-
 
 
 function doWeb(doc, url) {
@@ -82,24 +81,82 @@ function doWeb(doc, url) {
 
 
 function scrape(doc, url) {
-	var item = new Zotero.Item('document');
+	var type = itemType(ZU.xpath(doc, '//a[@class="button no-icon"]', '@href').toString());
+	var item = new Zotero.Item(type[0]);
+	var pub = ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Publisher")]/following-sibling::dd[1]');
+	var lang = ZU.trimInternal(ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Language")]/following-sibling::dd[1]'));
+	
 	item.title = ZU.xpathText(doc, '//div[@id="block-ypfs-theme-page-title"]/h1/span');
 	item.date = ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Date")]/following-sibling::dd[1]');
 	item.abstractNote = ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Information")]/following-sibling::dd[1]');
-	item.publisher = ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Publisher")]/following-sibling::dd[1]');
-	item.language = ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Language")]/following-sibling::dd[1]');
+	item.publisher = pub.replace(/.*: | \(.*\)/g, "");
+	item.language = getLocale(lang);
 	item.url = url;
+	item.extra = type[3];
 	
 	item.creators = [];
 	for (i=1; i<=ZU.xpath(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Author")]/following-sibling::dd[1]/a').length; i++) {
-		item.creators.push(ZU.cleanAuthor(ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Author")]/following-sibling::dd[1]/a'.concat("[", i, "]")), "author", true));
+		auth = ZU.xpathText(doc, '//dl[@class="ypfs-case__details"]/dt[contains(., "Author")]/following-sibling::dd[1]/a'.concat("[", i, "]"));
+		item.creators.push(ZU.cleanAuthor(auth.replace(/.*: | \(.*\)/g, ""), "author", true));
 	}
 	
 	item.attachments = [{
 		url: ZU.xpath(doc, '//a[@class="button no-icon"]', '@href').toString(),
-		mimeType: "application/pdf",
-		title: "Full Text PDF"
+		mimeType: type[1],
+		title: type[2]
 	}];
 
 	item.complete();
+}
+
+function itemType(href) {
+	switch (href.split(/\./).pop()) {
+		case "pdf":
+			return ['document', 'application/pdf', 'Full Text PDF'];
+		case "xlsx":
+			return ['document', 'application/vnd.ms-excel', 'Excel Workbook', 'type: dataset'];
+		case "xls":
+			return ['document', 'application/vnd.ms-excel', 'Excel Workbook', 'type: dataset'];
+		case "xlsm":
+			return ['document', 'application/vnd.ms-excel.sheet.macroEnabled.12', 'Macro-enabled Excel Workbook', 'type: dataset'];
+		case "doc":
+			return ['document', 'application/vnd.ms-word', 'Word Document'];
+		case "docx":
+			return ['document', 'application/vnd.ms-word', 'Word Document'];
+		case "pptx":
+			return ['presentation', 'application/vnd.ms-powerpoint', 'Powerpoint'];
+		case "mp3":
+			return ['audioRecording', 'audio/mpeg', 'Audio Recording'];
+		case "mp4":
+			return ['document', 'application/mp4', 'mp4 Recording'];
+		case "wma":
+			return ['audioRecording', 'audio/x-ms-wwma', 'Audio Recording'];
+		case "mov":
+			return ['videoRecording', 'video/quicktime', 'Video Recording'];
+	}
+}
+
+function getLocale(lang) {
+	return {
+		Arabic: 'ar',
+		Bulgarian: 'bg',
+		Danish: 'da',
+		Dutch: 'nl',
+		English: 'en',
+		Finnish: 'fi',
+		French: 'fr',
+		German: 'de',
+		Hungarian: 'hu',
+		Icelandic: 'is',
+		Japanese: 'jp',
+		Korean: 'kr',
+		Mongolian: 'mn',
+		Polish: 'pl',
+		Portugese: 'pt',
+		Slovene: 'sl',
+		Spanish: 'es',
+		Swedish: 'sv',
+		Thai: 'th',
+		Turkish: 'tr',
+	}[lang];
 }
