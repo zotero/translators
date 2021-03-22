90bcee8a52208f1b220a96d0b0e3060c3d759f97
jriedl <johannes.riedl@uni-tuebingen.de>
2021-03-22 17:17:57 +0100

    Address https://github.com/ubtue/DatenProbleme/issues/1423

--- SciELO.js
+++ SciELO.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-03-18 11:00:50"
+	"lastUpdated": "2021-03-22 16:16:40"
 }
 
 /*
@@ -87,19 +87,28 @@ function extractAuthors(doc) {
 
 function scrape(doc, url) {
 	var abstract = ZU.xpathText(doc, '//div[@class="abstract"]');
-	var transAbstract = ZU.xpathText(doc, '//div[@class="trans-abstract"]');
+	var transAbstract = ZU.xpath(doc, '//div[@class="trans-abstract"]');
 	var translator = Zotero.loadTranslator('web');
 	//use Embedded Metadata
 	translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
 	translator.setDocument(doc);
-	const keywordsInAbstractsPrefix = /\n*(Palabras clave|Keywords)\s*:.*/i;
-	const abstractPrefix = /^\s*(ABSTRACT:?|RESUMO:?|RESUMEN:?)/i;
+	const keywordsInAbstractsPrefix = /\n*(Palabras clave|Keywords|Stichworte)\s*:.*/i;
+	const abstractPrefix = /^\s*(ABSTRACT:?|RESUMO:?|RESUMEN:?|ZUSAMMENFASSUNG:?)/i;
 	translator.setHandler('itemDone', function(obj, item) {
 		if (abstract) item.abstractNote = abstract.replace(abstractPrefix, "").replace(/[\n\t]/g, "").
 		                                           replace(keywordsInAbstractsPrefix, "");
-		if (transAbstract) item.notes.push({note: "abs:" + transAbstract.replace(abstractPrefix, "").
-		                                           replace(keywordsInAbstractsPrefix, ""),
-		});
+		if (transAbstract) {
+			if (transAbstract.length == 1)
+		        item.notes.push({note: "abs:" + transAbstract.innerText.replace(abstractPrefix, "").
+		                                           replace(keywordsInAbstractsPrefix, "")});
+		    // we can currently handle one additional language
+		    else {
+		       item.notes.push({note: "abs:" + transAbstract[0].innerText.replace(abstractPrefix, "").
+		                                           replace(keywordsInAbstractsPrefix, "")});
+		       item.notes.push({note: "abs1:" + transAbstract[1].innerText.replace(abstractPrefix, "").
+		                                           replace(keywordsInAbstractsPrefix, "")});
+		    }
+		}
 		//abstract in orginal language
 		if (!abstract && item.ISSN === '0049-3449') {
  			item.abstractNote = text(doc, ' p:nth-child(4)');
