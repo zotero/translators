8e6f474a0f9a9dabf92bb71e618dd73879ebf5d2
Abe Jellinek <jellinek@berkeley.edu>
2021-06-07 16:36:21 -0500

    OSA: Update existing tests

--- Optical Society of America.js
+++ Optical Society of America.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-06-07 21:34:47"
+	"lastUpdated": "2021-06-07 21:36:17"
 }
 
 /*
@@ -162,7 +162,7 @@ var testCases = [
 				"publicationTitle": "JOSA A",
 				"rights": "© 1999 Optical Society of America",
 				"shortTitle": "Lens axicons",
-				"url": "http://www.osapublishing.org/abstract.cfm?uri=josaa-16-1-191",
+				"url": "https://www.osapublishing.org/josaa/abstract.cfm?uri=josaa-16-1-191",
 				"volume": "16",
 				"attachments": [
 					{
@@ -170,13 +170,29 @@ var testCases = [
 						"mimeType": "application/pdf"
 					},
 					{
-						"title": "Snapshot"
+						"title": "Snapshot",
+						"mimeType": "text/html"
 					}
 				],
 				"tags": [
-					"Diffraction",
-					"Lens system design",
-					"Propagation"
+					{
+						"tag": "Axicons"
+					},
+					{
+						"tag": "First order optics"
+					},
+					{
+						"tag": "Geometric optics"
+					},
+					{
+						"tag": "Spherical lenses"
+					},
+					{
+						"tag": "Superlenses"
+					},
+					{
+						"tag": "Wavefronts"
+					}
 				],
 				"notes": [],
 				"seeAlso": []
@@ -204,7 +220,7 @@ var testCases = [
 					}
 				],
 				"date": "2006/03/05",
-				"abstractNote": "Resilient packet ring ( IEEE 802.17) is a metropolitan area network technology for data transfer based on ring configuration.The paper provides guidelines for generation of recommends simulated environments for RPR testing ,discusses ways to test complex areas of RPR( e.g Fairness),provides guidelines for generating standard compliant test suite, and puts forward a strategy for automation of RPR testing.This paper is based on development of a RPR solution based on a Network processor.RPR specifies 39 state machines which implement the functionalities Topology Discovery, Protection, Datapath, OAM, Fairness and Shapers. The specification of the functionalities as well as the interaction between them makes RPR a complex protocol to validate. Lack of RPR test generator and inter dependency of control plane on data plane adds to the challenges of RPR testing. Careful planning, execution of testing in phases, building simulators and identifying the areas of challenges will guarantee success.Table of Contents Test Suite generationSimulators for RPR testingTest Sets for RPR testingTesting of RPR areasAutomation possibilities Test Suite generation Protocol Implementation Conformance Statements (PICs) provide a guidelines but it falls short of complete testing if you want to achieve the 'carrier grade' performance of the RPR. The test suite generation demands complete knowledge of the RPR Standard (IEEE 802.17, 802.17a, 802.17b).Simulators for RPR testing Simulator testing is a crucial part of RPR validation. Two types of simulators are recommended. Control plane simulator and the dataplane simulator The control plane functionality can be tested by building a stand alone simulator which can act as a frame work to exchange packets between the control plane instances.Pipeline integration stage is the integration of different modules of the packet processing modules. Pipeline integration testing is performed in the simulated environment with all the data path components treated as one single block. The packet headers are created and injected to the Receiver block and the packets from the pipeline are captured and analyzed at the transmit block. Most of the Network Processor development workbenches (e.g. transactor of IXP) support packet generators. More than 60% of the test cases can be executed in the pipeline integration stage using packet streams generated.Test Sets for RPR testingNo single test set has features required for RPR testing .The paper compares the capabilities of various test sets including Agilent and Ixia and proposes a combination of test sets for achieving RPR test coverage.Testing of RPR areasThe paper suggests methods to validate the following areas of RPR[1] 255 node testing [2] Fairness and Shaper testing [3] 50 milliseconds protection switch time[4] Testing of strict order frames [5] Jitter measurement [6] Performance monitoring testing[7] RPR-RPR BridgingSpatially Aware Sublayer (IEEE802.17b) introduces new challenge for validation of RPR. The paper discusses the complexities involved for validation of IEEE 802.17b.Automation possibilitiesThe paper discusses the areas of automation for RPR testing and methods for the same. RPR test automation can be achieved for the pipeline integration stage, On board integration and system testing phases",
+				"abstractNote": "Resilient Packet Ring (RPR) is a metropolitan area network technology for data transfer based on ring configuration and is standardized as IEEE 802.17. RPR testing is challenging as it combines best of SONET networks and Data networks. The paper provides guidelines for generation of standard compliant test suite, recommends simulated environments for RPR testing and puts forward a strategy for automation of RPR testing. The paper describes various stages of RPR testing and the challenges considering entire project cycle.",
 				"conferenceName": "Optical Fiber Communication Conference",
 				"language": "EN",
 				"libraryCatalog": "www.osapublishing.org",
@@ -212,18 +228,36 @@ var testCases = [
 				"proceedingsTitle": "Optical Fiber Communication Conference and Exposition and The National Fiber Optic Engineers Conference (2006), paper JThB89",
 				"publisher": "Optical Society of America",
 				"rights": "© 2006 Optical Society of America",
-				"url": "http://www.osapublishing.org/abstract.cfm?uri=OFC-2006-JThB89",
+				"url": "https://www.osapublishing.org/abstract.cfm?uri=OFC-2006-JThB89",
 				"attachments": [
 					{
 						"title": "Full Text PDF",
 						"mimeType": "application/pdf"
 					},
 					{
-						"title": "Snapshot"
+						"title": "Snapshot",
+						"mimeType": "text/html"
 					}
 				],
 				"tags": [
-					"Other topics of general interest"
+					{
+						"tag": "Fluorescence correlation spectroscopy"
+					},
+					{
+						"tag": "Green fluorescent protein"
+					},
+					{
+						"tag": "Networking hardware"
+					},
+					{
+						"tag": "Optical angular momentum"
+					},
+					{
+						"tag": "Optical ethernet"
+					},
+					{
+						"tag": "Refractive index"
+					}
 				],
 				"notes": [],
 				"seeAlso": []
@@ -261,7 +295,7 @@ var testCases = [
 				],
 				"date": "1992/09/10",
 				"DOI": "10.1364/AO.31.005706",
-				"ISSN": "1539-4522",
+				"ISSN": "2155-3165",
 				"abstractNote": "We discuss the realization of highly efficient fan-out elements. Laser-beam writing lithography is available now for fabricating smooth surface relief microstructures. We develop several methods for optimizing microstructure profiles. Only a small number of parameters in the object plane are necessary for determining the kinoform. This simplifies the calculation of M × N arrays also for large M and N. Experimental results for a 9-beam fan-out element are presented.",
 				"issue": "26",
 				"journalAbbreviation": "Appl. Opt., AO",
@@ -270,7 +304,7 @@ var testCases = [
 				"pages": "5706-5711",
 				"publicationTitle": "Applied Optics",
 				"rights": "© 1992 Optical Society of America",
-				"url": "http://www.osapublishing.org/abstract.cfm?uri=ao-31-26-5706",
+				"url": "https://www.osapublishing.org/ao/abstract.cfm?uri=ao-31-26-5706",
 				"volume": "31",
 				"attachments": [
 					{
@@ -278,10 +312,30 @@ var testCases = [
 						"mimeType": "application/pdf"
 					},
 					{
-						"title": "Snapshot"
+						"title": "Snapshot",
+						"mimeType": "text/html"
 					}
 				],
-				"tags": [],
+				"tags": [
+					{
+						"tag": "Dammann gratings"
+					},
+					{
+						"tag": "Diffraction efficiency"
+					},
+					{
+						"tag": "Laser beams"
+					},
+					{
+						"tag": "Light sources"
+					},
+					{
+						"tag": "Lithography"
+					},
+					{
+						"tag": "Total internal reflection"
+					}
+				],
 				"notes": [],
 				"seeAlso": []
 			}
