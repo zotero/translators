286ef204aa45ceb6b473f6f5fbdde7d3495dba3c
Guy Aglionby <GuyAglionby@users.noreply.github.com>
2021-06-28 20:19:39 +0100

    Update ACL translator for HTML change (#2496)

--- ACLWeb.js
+++ ACLWeb.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-05-27 12:04:35"
+	"lastUpdated": "2021-06-28 16:13:13"
 }
 
 /*
@@ -101,7 +101,7 @@ function doWeb(doc, url) {
 }
 
 function scrape(doc) {
-	let bibtex = ZU.xpath(doc, '//button[contains(text(), "Copy BibTeX to Clipboard")]/@data-clipboard-text')[0].value;
+	let bibtex = ZU.xpath(doc, '//pre[@id = "citeBibtexContent"]')[0].textContent;
 	let pdfURL = ZU.xpath(doc, '//a[span[contains(text(), "PDF")]]/@href')[0].value;
 	let translator = Zotero.loadTranslator("import");
 	translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
