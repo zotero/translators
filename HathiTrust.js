56ae3908331db1668d54e683339b7188e6127b45
Abe Jellinek <jellinek@berkeley.edu>
2021-07-01 16:51:11 -0700

    HathiTrust: Extract more metadata, support reader page

--- HathiTrust.js
+++ HathiTrust.js
@@ -1,120 +1,157 @@
 {
 	"translatorID": "31da33ad-b4d9-4e99-b9ea-3e1ddad284d8",
-	"label": "Hathi Trust",
-	"creator": "Sebastian Karcher",
-	"target": "^https?://(catalog|babel)\\.hathitrust\\.org",
+	"label": "HathiTrust",
+	"creator": "Sebastian Karcher and Abe Jellinek",
+	"target": "^https?://(catalog|babel)\\.hathitrust\\.org/",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-11-11 20:05:29"
+	"lastUpdated": "2021-07-01 23:43:42"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
-	
-	Copyright © 2011 Sebastian Karcher and the Center for History and New Media
-					 George Mason University, Fairfax, Virginia, USA
-					 http://zotero.org
-	
+
+	Copyright © 2011-2021 Sebastian Karcher, Abe Jellinek,
+					      and the Center for History and New Media
+					      George Mason University, Fairfax, Virginia, USA
+					      http://zotero.org
+
 	This file is part of Zotero.
-	
+
 	Zotero is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	Zotero is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
-	
+
 	***** END LICENSE BLOCK *****
 */
 
 function detectWeb(doc, url) {
-	if (url.match(/\/Record\/\d+/)) return "book";
+	if (url.match(/\/Record\/\d+/)) {
+		return "book";
+	}
 
-	if ((url.indexOf("/Search/") != -1 || url.indexOf("a=listis;"))
-		&& getSearchResults(doc).length) {
+	if (url.includes('/cgi/pt')) {
+		// viewer page; more handling might be needed here
+		return "book";
+	}
+
+	if ((url.includes("/Search/") || url.includes("a=listis;"))
+		&& getSearchResults(doc, true)) {
 		return "multiple";
 	}
+
+	return false;
 }
 
-function getSearchResults(doc) {
-	//search results
-	var res = ZU.xpath(doc, '//div[@class="resultitem"]\
-					[.//a[@class="cataloglinkhref"][1]/@href]');
-	//collections
-	if (!res.length) res = ZU.xpath(doc, '//div[contains(@class,"row")]/div[.//div[contains(@class, "result")]//a[contains(@class, "cataloglinkhref")][1]/@href]');
-	return res;
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('.record-container');
+	for (let row of rows) {
+		let href = attr(row, 'a[href*="/Record/"]', 'href');
+		let id = (href.match(/\/([0-9]+)/) || [])[1];
+		let title = ZU.trimInternal(row.textContent);
+		if (!id || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[id] = title;
+	}
+	return found ? items : false;
 }
 
-function doWeb(doc, url){
-	if (detectWeb(doc, url) == "multiple") { 
-		var items = {};
-		var rows = getSearchResults(doc);
-		var c=0;
-		for (var i in rows) {
-			var title = ZU.xpathText(rows[i], './/span[@class="title"]') || //search result
-						ZU.xpathText(rows[i], './h4[@class="Title"]/text()[last()]');	//collection item
-			var id = ZU.xpathText(rows[i], './/a[contains(@class, "cataloglinkhref")][1]/@href');
-			//Z.debug(id + ": " + title)
-			if (id) {
-				id = (id.match(/\/(\d+)/) || [])[1];
-				//lists can display the same record, but with different titles
-				//(for different PDF versions), so we add a unique number to each
-				//record so they don't override each other. We strip it off later
-				if (id) id = c++ + '-' + id;
-			}
-			if (title && id) items[id] = title;
-		}
-
-		Zotero.selectItems(items, function (items) {
-			if (!items) {
-				return true;
-			}
-			var articles = [];
-			for (var i in items) {
-				articles.push(i.replace(/^\d+-/,''));
-			}
-			scrape(articles);	
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			if (items) scrape(Object.keys(items));
 		});
-	} else {
-		var itemid = url.match(/\/([0-9]+)/)[1];
-		scrape([itemid]);
+	}
+	else {
+		let id = extractID(url);
+		if (!id) id = extractID(attr(doc, '.bibLinks a[href*="/Record/"]', 'href'));
+		if (!id) throw new Error('Couldn\'t extract ID from URL: ' + url);
+		scrape([id]);
 	}
 }
 
-// help function
-function scrape(ids){
-	//RIS Link
-	var risurl = "http://catalog.hathitrust.org/Search/SearchExport?handpicked="
+function extractID(url) {
+	return (url.match(/\/Record\/([0-9]+)/) || [])[1];
+}
+
+function scrape(ids) {
+	var risURL = "http://catalog.hathitrust.org/Search/SearchExport?handpicked="
 		+ ids.join(',') + "&method=ris";
-	Zotero.Utilities.HTTP.doGet(risurl, function (text) {
-		text = text.replace(/M1  - .+/, ""); //M1 has only garbage like repeated page number info
+	ZU.doGet(risURL, function (text) {
+		// M1 has only garbage like repeated page number info
+		text = text.replace(/^M1 {2}- .+/m, "");
 		var translator = Zotero.loadTranslator("import");
 		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
 		translator.setString(text);
-		translator.setHandler("itemDone", function(obj, item) {
-			item.extra="";
-			if (item.place)	item.place = item.place.replace(/[\[\]]/g, "");
-			if (item.tags.length) item.tags = item.tags.join("/").split("/");
-			if (item.url.substr(0,2)=="//") {
+		translator.setHandler("itemDone", function (obj, item) {
+			item.extra = "";
+
+			if (item.place) {
+				item.place = item.place.replace(/[[\]]/g, "");
+			}
+
+			if (item.publisher) {
+				item.publisher = item.publisher.replace(/[[\]]/g, "");
+			}
+
+			if (item.numPages) {
+				// "3 p.l., 192 p." -> 192
+				let cleanedPages = item.numPages.match(/(\d+)\s*p\.($|[^a-z])/i);
+				if (cleanedPages) {
+					item.numPages = cleanedPages[1];
+				}
+			}
+
+			if (item.tags.length) {
+				item.tags = item.tags.join("/").split("/")
+					.map(s => ZU.trimInternal(s).replace(/\.$/, ''));
+			}
+
+			for (let creator of item.creators) {
+				if (creator.firstName) {
+					creator.firstName = creator.firstName.replace(/(\w{2,})\./, '$1');
+				}
+
+				if (creator.lastName) {
+					creator.lastName = creator.lastName.replace(/\.$/, '');
+				}
+			}
+
+			if (item.url.startsWith("//")) {
 				item.url = "https:" + item.url;
 			}
-			item.attachments = [{url:item.url, title: "Hathi Trust Record", mimeType: "text/html"}];
+
+			// there's no reason to have a snapshot of the record page, but
+			// HathiTrust metadata is pretty bare and it's likely that users
+			// will want to come back
+			item.attachments.push({
+				title: 'Record Page',
+				url: item.url,
+				mimeType: 'text/html',
+				snapshot: false
+			});
+
 			item.complete();
-		});	
+		});
 		translator.translate();
 	});
 }
-
 
 /** BEGIN TEST CASES **/
 var testCases = [
@@ -138,15 +175,16 @@ var testCases = [
 					}
 				],
 				"date": "1940",
-				"libraryCatalog": "Hathi Trust",
-				"numPages": "3 p.l., 192 p.",
+				"libraryCatalog": "HathiTrust",
+				"numPages": "192",
 				"place": "Oxford",
 				"publisher": "The Clarendon press",
 				"url": "https://catalog.hathitrust.org/Record/001050654",
 				"attachments": [
 					{
-						"title": "Hathi Trust Record",
-						"mimeType": "text/html"
+						"title": "Record Page",
+						"mimeType": "text/html",
+						"snapshot": false
 					}
 				],
 				"tags": [],
@@ -159,6 +197,61 @@ var testCases = [
 		"type": "web",
 		"url": "http://babel.hathitrust.org/cgi/mb?a=listis;c=421846824",
 		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://babel.hathitrust.org/cgi/pt?id=uiuo.ark:/13960/t70w4tz8j&view=1up&seq=1",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "Articles of association and by-laws of the Jewish Farmers' Cooperative Credit Unions.",
+				"creators": [
+					{
+						"lastName": "Jewish Farmers' Cooperative Credit Unions",
+						"creatorType": "author",
+						"fieldMode": 1
+					},
+					{
+						"lastName": "Jewish Agricultural and Industrial Aid Society",
+						"creatorType": "author",
+						"fieldMode": 1
+					}
+				],
+				"date": "1911",
+				"libraryCatalog": "HathiTrust",
+				"numPages": "19",
+				"place": "New York City",
+				"publisher": "Jewish Agricultural and Industrial Aid Society",
+				"url": "https://catalog.hathitrust.org/Record/102407867",
+				"attachments": [
+					{
+						"title": "Record Page",
+						"mimeType": "text/html",
+						"snapshot": false
+					}
+				],
+				"tags": [
+					{
+						"tag": "Credit unions"
+					},
+					{
+						"tag": "Jewish farmers"
+					},
+					{
+						"tag": "United States"
+					},
+					{
+						"tag": "United States"
+					}
+				],
+				"notes": [
+					{
+						"note": "<p>English and Yiddish text.</p>"
+					}
+				],
+				"seeAlso": []
+			}
+		]
 	}
 ]
 /** END TEST CASES **/
