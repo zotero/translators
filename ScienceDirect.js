fe9a9eddf495fa165132b79e0d77e343ef7d087f
Abe Jellinek <jellinek@berkeley.edu>
2021-07-01 12:19:18 -0700

    ScienceDirect: Don't detect search pages as journal articles
    detectWeb uses journalArticle as a fallback type after tests for all
    other types have failed (the opposite of the way most translators do it,
    but it makes sense for this site). That was causing empty/not-yet-loaded
    search pages to show as journalArticles, and the toolbar icon wouldn't
    update once loading completed.
    
    Also removed attr/text and added a license block.

--- ScienceDirect.js
+++ ScienceDirect.js
@@ -9,12 +9,31 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-10-27 06:55:27"
+	"lastUpdated": "2021-07-01 19:17:31"
 }
 
-// attr()/text() v2
-// eslint-disable-next-line
-function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;} 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2021 Michael Berkowitz and Aurimas Vinckevicius
+	
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
 
 
 function detectWeb(doc, url) {
@@ -39,8 +58,15 @@ function detectWeb(doc, url) {
 		}
 	}
 
-	if (url.search(/\/search[?/]/) != -1 && getArticleList(doc).length > 0) {
-		return "multiple";
+	if (url.search(/\/search[?/]/) != -1) {
+		if (getArticleList(doc).length > 0) {
+			return "multiple";
+		}
+		else if (doc.querySelector('.LoadingOverlay.show')) {
+			// monitor and update the toolbar icon when results have loaded
+			Z.monitorDOMChanges(doc.querySelector('.results-container'));
+			return false;
+		}
 	}
 	if (!url.includes("pdf")) {
 		// Book sections have the ISBN in the URL
