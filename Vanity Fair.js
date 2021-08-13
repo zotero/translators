2e4c1a3c43c8014a2b90bb39703711adc70dcd65
Abe Jellinek <jellinek@berkeley.edu>
2021-08-13 16:24:12 -0600

    Vanity Fair: Lint, remove attr/text, update tests

--- Vanity Fair.js
+++ Vanity Fair.js
@@ -2,14 +2,14 @@
 	"translatorID": "62f46e1a-4c40-4dbb-82aa-71cdeb14f1bc",
 	"label": "Vanity Fair",
 	"creator": "Sebastian Karcher",
-	"target": "^https?://www\\.vanityfair\\.com",
+	"target": "^https?://www\\.vanityfair\\.com/",
 	"minVersion": "1.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2019-06-17 12:59:13"
+	"lastUpdated": "2021-08-13 22:24:07"
 }
 
 /*
@@ -36,32 +36,20 @@
 	***** END LICENSE BLOCK *****
 */
 
-// attr()/text() v2
-// eslint-disable-next-line
-function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null;}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null;}
 
 function detectWeb(doc, _url) {
-	// if (doc.querySelector('.component-search-results')) return 'multiple';
-	if (doc.querySelector('.content-header__rubric--issue-date')) return 'magazineArticle';
-	if (doc.querySelector('time.content-header__publish-date')) return 'blogPost';
+	if (doc.querySelector('.content-header__rubric--issue-date')) {
+		return 'magazineArticle';
+	}
+	if (doc.querySelector('time.content-header__publish-date')) {
+		return 'blogPost';
+	}
 	return false;
 }
 
 function doWeb(doc, url) {
-	// eslint-disable-next-line:lines-around-comment
-	/* remove multiple scraping - Vanity Fair seems to detect headless browsers and gives a 402 error
-	if (detectWeb(doc, url) === 'multiple') {
-		let items = {};
-		for (let item of doc.querySelectorAll('.component-search-results .hed a')) {
-			if (item.href && item.textContent) items[item.href] = item.textContent;
-		}
-
-		Zotero.selectItems(items, function (items) {
-			ZU.processDocuments(Object.keys(items), scrape);
-		});
-	}
-	else {
-	*/
+	// no multiples - Vanity Fair seems to detect headless browsers and gives
+	// a 402 error
 	scrape(doc, url);
 }
 
@@ -69,28 +57,28 @@ function scrape(doc, url) {
 	let itemType = detectWeb(doc, url);
 	let item = new Zotero.Item(itemType);
 	item.url = url;
-	item.attachments = [{ document: doc, title: 'Snapshot', type: 'text/html' }];
+	item.attachments = [{ document: doc, title: 'Snapshot' }];
 
-	let author = text('.byline__name');
+	let author = text(doc, '.byline__name');
 	if (author) item.creators = [ZU.cleanAuthor(author, 'author')];
 
-	let date = text('time.content-header__publish-date');
+	let date = text(doc, 'time.content-header__publish-date');
 	if (date) item.date = ZU.strToISO(date);
 
 	item.language = "en-US";
 	item.ISSN = "0733-8899";
-	item.title = text('h1');
+	item.title = text(doc, 'h1');
 
 	let issue;
 	switch (itemType) {
 		case 'magazineArticle':
-			item.abstractNote = text('.dek');
+			item.abstractNote = text(doc, '.dek');
 
-			issue = text('.content-header__rubric--issue-date');
+			issue = text(doc, '.content-header__rubric--issue-date');
 			if (issue) item.issue = issue.replace(/\s.*/, '');
 
 			item.publicationTitle = "Vanity Fair";
-			item.tags = attr('meta[name="keywords"]', 'content').split(',').filter(tag => !tag.startsWith('_legacy_/'));
+			item.tags = attr(doc, 'meta[name="keywords"]', 'content').split(',').filter(tag => !tag.startsWith('_legacy_/'));
 			break;
 
 		case 'blogPost':
@@ -108,10 +96,11 @@ function scrape(doc, url) {
 var testCases = [
 	{
 		"type": "web",
-		"url": "http://www.vanityfair.com/online/daily/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
+		"url": "https://www.vanityfair.com/news/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
 		"items": [
 			{
 				"itemType": "blogPost",
+				"title": "Q&A: Michael Lewis on the Future of Europe’s Economy",
 				"creators": [
 					{
 						"firstName": "Jaime",
@@ -119,33 +108,30 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "2011-08-10",
+				"blogTitle": "Vanity Fair Blogs",
+				"language": "en-US",
+				"shortTitle": "Q&A",
+				"url": "https://www.vanityfair.com/news/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
 				"attachments": [
 					{
 						"title": "Snapshot",
-						"type": "text/html"
+						"mimeType": "text/html"
 					}
 				],
-				"url": "https://www.vanityfair.com/news/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
-				"date": "2011-08-10",
-				"ISSN": "0733-8899",
-				"language": "en-US",
-				"publicationTitle": "Vanity Fair Blogs",
-				"title": "Q&A: Michael Lewis on the Future of Europe’s Economy",
-				"libraryCatalog": "Vanity Fair",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"shortTitle": "Q&A"
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
-		"url": "http://www.vanityfair.com/society/features/2011/05/top-one-percent-201105",
+		"url": "https://www.vanityfair.com/news/2011/05/top-one-percent-201105",
 		"items": [
 			{
 				"itemType": "magazineArticle",
+				"title": "Of the 1%, by the 1%, for the 1%",
 				"creators": [
 					{
 						"firstName": "Joseph E.",
@@ -153,40 +139,51 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [
-					{ "tag": "americans" },
-					{ "tag": "archive" },
-					{ "tag": "magazine" },
-					{ "tag": "one percent" },
-					{ "tag": "society" },
-					{ "tag": "wealth" }
-				],
-				"seeAlso": [],
-				"attachments": [
-					{
-						"title": "Snapshot",
-						"type": "text/html"
-					}
-				],
-				"url": "https://www.vanityfair.com/news/2011/05/top-one-percent-201105",
 				"date": "2011-03-31",
 				"ISSN": "0733-8899",
 				"issue": "May",
 				"language": "en-US",
-				"publicationTitle": "Vanity Fair",
-				"title": "Of the 1%, by the 1%, for the 1%",
 				"libraryCatalog": "Vanity Fair",
-				"accessDate": "CURRENT_TIMESTAMP"
+				"publicationTitle": "Vanity Fair",
+				"url": "https://www.vanityfair.com/news/2011/05/top-one-percent-201105",
+				"attachments": [
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "americans"
+					},
+					{
+						"tag": "archive"
+					},
+					{
+						"tag": "magazine"
+					},
+					{
+						"tag": "one percent"
+					},
+					{
+						"tag": "society"
+					},
+					{
+						"tag": "wealth"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	},
 	{
 		"type": "web",
-		"url": "http://www.vanityfair.com/online/daily/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
+		"url": "https://www.vanityfair.com/news/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
 		"items": [
 			{
 				"itemType": "blogPost",
+				"title": "Q&A: Michael Lewis on the Future of Europe’s Economy",
 				"creators": [
 					{
 						"firstName": "Jaime",
@@ -194,24 +191,50 @@ var testCases = [
 						"creatorType": "author"
 					}
 				],
-				"notes": [],
-				"tags": [],
-				"seeAlso": [],
+				"date": "2011-08-10",
+				"blogTitle": "Vanity Fair Blogs",
+				"language": "en-US",
+				"shortTitle": "Q&A",
+				"url": "https://www.vanityfair.com/news/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
 				"attachments": [
 					{
 						"title": "Snapshot",
-						"type": "text/html"
+						"mimeType": "text/html"
 					}
 				],
-				"url": "https://www.vanityfair.com/news/2011/08/qa-michael-lewis-on-future-of-europe-economy-and-germany-italy",
-				"date": "2011-08-10",
-				"ISSN": "0733-8899",
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.vanityfair.com/news/2021/08/how-florida-turned-american-politics-into-a-tabloid-nightmare",
+		"items": [
+			{
+				"itemType": "blogPost",
+				"title": "How Florida Turned American Politics Into a Tabloid Nightmare",
+				"creators": [
+					{
+						"firstName": "Joe",
+						"lastName": "Hagan",
+						"creatorType": "author"
+					}
+				],
+				"date": "2021-08-13",
+				"blogTitle": "Vanity Fair Blogs",
 				"language": "en-US",
-				"publicationTitle": "Vanity Fair Blogs",
-				"title": "Q&A: Michael Lewis on the Future of Europe’s Economy",
-				"libraryCatalog": "Vanity Fair",
-				"accessDate": "CURRENT_TIMESTAMP",
-				"shortTitle": "Q&A"
+				"url": "https://www.vanityfair.com/news/2021/08/how-florida-turned-american-politics-into-a-tabloid-nightmare",
+				"attachments": [
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
 			}
 		]
 	}
