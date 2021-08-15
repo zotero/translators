98bf2c711a8b3a169f5a73fde01e247d47273143
reubot <reuben_p@yahoo.com>
2021-08-15 11:33:07 +1000

    code review changes
    commit a2aa5672ed08d76e11001000daedbe54dca1dcd6
    Date:   Sat Aug 7 17:11:16 2021 +1000
    
        get ris element rather than text
    
    commit 4bfe377ee9ed6b36a32840342d8235f43a9441f6
    Date:   Sat Aug 7 16:02:59 2021 +1000
    
        add license block
    
    commit 7a4f62aa14ee81beb0195c10f05b4999445d36e4
    Date:   Sat Aug 7 15:55:21 2021 +1000
    
        simplify getSearchResults
    
    commit fa777b8f04e1fe624f2b106b7f6fd96e17ec1059
    Date:   Sat Aug 7 15:43:22 2021 +1000
    
        remove old code
    
    commit ff890616063c2aec25c5fb3341fccdbee7d4723d
    Date:   Sat Aug 7 15:39:00 2021 +1000
    
        use document-view__body for notes
    
    commit 99beb7777346c2a3485dd210685de7912b00806e
    Date:   Sat Aug 7 14:53:41 2021 +1000
    
        more code review
    
    commit 4bcc98443dbff6bb31246824c13ef6879d39dfaa
    Date:   Sat Aug 7 13:49:33 2021 +1000
    
        code review changes
    
    commit 964890b3ed3798e6674797f81e91f333be55d8b4
    Date:   Sat Aug 7 13:20:39 2021 +1000
    
        Add test case
    
    commit cecff8d92c66c8c778b87ef24763f9525a4b1ebd
    Date:   Sat Aug 7 11:53:02 2021 +1000
    
        moore lint
    
    commit d92a681bcc21f13426646511f3589e21fe137de2
    Date:   Sat Aug 7 00:06:43 2021 +1000
    
        Expected exception block, space or tab after '//' in comment
    
    commit 6c23b906b2b77597ccc762afdda82c6e7de9589a
    Date:   Sat Aug 7 00:04:06 2021 +1000
    
        more lint
    
    commit b808d7306454f2adb51a15b961dfba2bdb9c3cd0
    Date:   Fri Aug 6 23:57:50 2021 +1000
    
        ci test
    
    commit 0d23d3f82e108c6f9975bdae3d547527e0ef6e96
    Date:   Fri Aug 6 19:27:57 2021 +1000
    
        Fix Lint errors

--- NewsBank.js
+++ NewsBank.js
@@ -9,95 +9,137 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-08-06 07:05:28"
+	"lastUpdated": "2021-08-07 04:32:01"
 }
 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	NewsBank translator Copyright © 2021 Reuben Peterkin
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
 function detectWeb(doc, url) {
-	if (getRISText(doc)) return "newspaperArticle";
-	if (url.indexOf("results") != -1) return "multiple";
-//	Zotero.debug(url.indexOf("results"));
+	if (getRISElement(doc)) return "newspaperArticle";
+	if (url.includes("/results")) return "multiple";
 	return false;
 }
 
 function getSearchResults(doc) {
 	var items = {}, found = false;
-	var rows = doc.getElementById('search-hits-gnus-search-hits-pane');
+	var rows = doc.getElementsByTagName('article');
 	if (!rows) return false;
-	rows = rows.getElementsByTagName('article');
-//	Zotero.debug(rows);
+	//	Zotero.debug(rows);
 
-	for (var i=0; i<rows.length; i++) {
-//		var count = rows[i].getElementsByClassName('count')[0];
-//		if (!count) count = "";
-//		else count = count.textContent.replace(/^\s*(\d+)[\s\S]*/, '$1') + '. ';
-
-		//var title = doc.querySelector('.search-hits__hit__title');
-		var title = rows[i].getElementsByClassName('search-hits__hit__title')[0];
-		var hdl = rows[i].getElementsByTagName('a')[0];
-		var prefix = hdl.getElementsByClassName('element-invisible')[0];
-		if (!hdl) continue;
-
+	for (var i = 0; i < rows.length; i++) {
+		var title = rows[i].querySelector('.search-hits__hit__title');
+		var link = rows[i].querySelector('a');
+		var prefix = link.querySelector('.element-invisible').textContent;
+		if (!title || !link) continue;
 		found = true;
 
-		items[hdl.href] = ZU.trimInternal(title.textContent.replace(prefix.textContent,''));
+		if (!prefix) prefix = '';
 
+		items[link.href] = ZU.trimInternal(title.textContent.replace(prefix, ''));
 	}
-//	Zotero.debug(items);
 
 	return found ? items : false;
 }
 
-function getRISText(doc) {
-	return ZU.xpathText(doc, '//textarea[@id="nbplatform-easybib-export-records"]');
+function getRISElement(doc) {
+	return doc.getElementById('nbplatform-noodletools-export-risdatabyformpost');
 }
 
 function getItem(doc) {
-	//ZU.doGet(getRISLink(doc), function(text) {
-	var text = getRISText(doc);
-	//Z.debug(text);
+	var text = getRISElement(doc).textContent.trim();
+	//	Z.debug(text);
 	var trans = Zotero.loadTranslator('import');
 	// RIS
 	trans.setTranslator('32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7');
 	trans.setString(text);
 	trans.setHandler('itemDone', function (obj, item) {
 		item.url = ZU.xpathText(doc, '//div[@class="actions-bar__urltext"]');
-		//Z.debug
-		//console.log(ZU.xpath(doc,'//script[contains(.,"nbcore_pdf"))]'));
-		var jsontext = (ZU.xpathText(doc ,'//script[contains(.,"nbcore_pdf")]'));//'//script[41]'));
-	jsontext = jsontext.replace("<!--//--><![CDATA[//><!--", "");
-	jsontext = jsontext.replace("jQuery.extend(Drupal.settings,", "");
-	jsontext = jsontext.replace(new RegExp('\\);$', 'm'), "");
-	jsontext = jsontext.replace("//--><!]]>", " ");
-	//Z.debug(jsontext);
-	var pdfJSON = JSON.parse(jsontext);
-	//Z.debug(pdfJSON);
-	var notebody = pdfJSON.nbcore_pdf['nbcore-pdf-ascii-bar'].template_params.body;
-	item.notes.push({ note: ZU.trim(notebody) });
-	//item.attachments.push()
-	item.complete();
+
+		var docBody = doc.getElementsByClassName('document-view__body');
+		for (var i = 0; i < docBody.length; i++) {
+			item.notes.push({ note: docBody[i].innerHTML.trim() });
+		}
+
+		item.complete();
 	});
 	trans.translate();
 }
 
 function doWeb(doc, url) {
-
 	if (detectWeb(doc, url) == "multiple") {
-	var items=getSearchResults(doc);
-//	Zotero.debug(items);
+		var items = getSearchResults(doc);
+		//	Zotero.debug(items);
 
-	Zotero.selectItems(items, function(items) {
-		if(!items) return true;
-		var ids = [];
-
-		for (var i in items) {
-			ids.push(i);
-		}
-		Zotero.debug(ids);
-		ZU.processDocuments(ids,getItem);
-	});
+		Zotero.selectItems(items, function (items) {
+			if (!items) return;
+			ZU.processDocuments(Object.keys(items), getItem);
+		});
 	}
-	else
-	{
+	else {
 		getItem(doc);
 	}
 }
+
+// Test cast modified from "The Times and Sunday Times.js"
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://infoweb.newsbank.com/apps/news/openurl?ctx_ver=z39.88-2004&rft_id=info%3Asid/infoweb.newsbank.com&svc_dat=AWNB&req_dat=3AD092142963457FA426C327101D0723&rft_val_format=info%3Aofi/fmt%3Akev%3Amtx%3Actx&rft_dat=document_id%3Anews%252F16579C8CD2790100",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"title": "Rare animals among body count at Scottish zoos",
+				"creators": [
+					{
+						"firstName": "Mark",
+						"lastName": "Macaskill",
+						"creatorType": "author"
+					}
+				],
+				"date": "July 2, 2017",
+				"pages": "3",
+				"libraryCatalog": "NewsBank",
+				"archive": "Access World News",
+				"publicationTitle": "Sunday Times, The (London, England)",
+				"url": "https://infoweb.newsbank.com/apps/news/openurl?ctx_ver=z39.88-2004&rft_id=info%3Asid/infoweb.newsbank.com&svc_dat=AWNB&req_dat=3AD092142963457FA426C327101D0723&rft_val_format=info%3Aofi/fmt%3Akev%3Amtx%3Actx&rft_dat=document_id%3Anews%252F16579C8CD2790100",
+				"attachments": [],
+				"tags": [],
+				"notes": [
+					{
+						"note": "<p>SQN: 127147919</p>"
+					},
+					{
+						"note": "<p>More than 900 creatures in the care of the Royal Zoological Society of Scotland (RZSS) died in captivity last year, including several hundred rare snails bred for conservation.<br><br>Figures released by the charity, which runs the 82-acre Edinburgh Zoo and a wildlife park in the Scottish Highlands, show that about 25 animals were put down on health grounds.<br><br>Dozens more perished within weeks of birth, among them several animals designated as under threat by the International Union for Conservation of Nature and Natural Resources Red List.<br><br>They included a female socorro dove, which is extinct in the wild; four cottontop tamarins and three visayan warty pigs (both critically endangered species); a barbary macaque and two painted hunting dogs (both endangered species); and a crowned lemur, which is categorised as vulnerable.<br><br>According to the RZSS, a total of 856 animals aged 30 days and more died in 2016. A further 71 failed to survive beyond 30 days of birth. In 2015, about 700 animals died.<br><br>The partula snail, an endangered gastropod which originates on the steep volcanic forested islands of French Polynesia, accounted for the bulk of casualties. The RZSS is part of an international conservation effort to protect the snail and reintroduced more than 600 to Tahiti last year amid fanfare. A less wellknown fact is that a further 749 died in captivity.<br><br>RZSS officials pointed out that it cares for more than 8,000 creatures and insisted that the \"vast majority\" of animals died from natural causes . However, Elisa Allen, director of the UK charity, People for the Ethical Treatment of Animals, said captive animals often died far short of their natural life expectancy.<br><br>\"Even the best zoos can never meet all the unique environmental, nutritional, and social needs of the various species they imprison. The obscene amount of money zoos spend on buying, breeding, and housing exotic animals could benefit so many more animals in the wild and go a long way towards addressing the root causes of animal extinction and endangerment: habitat destruction and poaching. Anyone who cares about helping animals should donate to programmes that protect them in their natural habitats and shun these glorified animal prisons.\"<br><br>Last year's casualties included Edinburgh Zoo's first forest reindeer calf, which was euthanised following \"mobility problems brought on by a spinal infection ... an extensive programme of treatment failed to improve his situation\", said officials.<br><br>Three-year-old Yooranah, the first koala ever to be born in the UK, died at Edinburgh Zoo last June after a \"prolonged period of illness\".<br><br>The plight of partula snails has inspired a huge conservation effort after the tiny gastropod, which is so small it can fit on the edge of a 50p piece, became fodder for the non-native rosy wolf snail, which was introduced to French Polynesia in the 1970s. The partula, which grows to about an inch long, is said to live for up to 10 years and was documented by Captain Cook during his travels in 1769.<br><br>The snails have provided valuable insights into the mechanisms of evolution since different species have developed shell colouring suited to the environments they inhabit.<br><br>The RZSS, which has been involved in the conservation of partula snails since 1984, did not provide full details of how or why so many snails died last year but indicated that many had reached the end of their life span.<br><br>\"It is difficult to establish exact life expectancies, and different subspecies tend to live for very different periods of time. The best estimates we have in captivity is about three to four years for the species we hold,\" said a spokesman.<br><br>Zoo officials added that partula snail mortality is at its highest when young - the snails are thin shelled and delicate and more sensitive to minor changes in their environment. \"The team at RZSS Edinburgh Zoo have been hugely successful in breeding and reintroducing this critically endangered species in the wild,\" added the spokesman.</p>"
+					},
+					{
+						"note": "Caption: Four 'critically endangered' cotton-top tamarins, above, died in captivity, while hundreds of partula snails, left, an endangered gastropod that originated in French Polynesia, accounted for the majority of the lost creatures, which have been successfully bred at Edinburgh Zoo, below"
+					}
+				],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
