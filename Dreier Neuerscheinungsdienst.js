999d1d7609ef8b8110dcdc382d00e9cce2751732
denismaier <maier.de@gmail.com>
2021-08-25 09:43:21 +0200

    add tests

--- Dreier Neuerscheinungsdienst.js
+++ Dreier Neuerscheinungsdienst.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-08-25 07:30:26"
+	"lastUpdated": "2021-08-25 07:42:34"
 }
 
 /*
@@ -111,3 +111,99 @@ function scrape(doc) {
 	item.complete();
 }
 
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://www.dietmardreier.de/de/detail/ISBN-9781792457418/Suehr-Christopher-J./A-Firm-Foundation",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "A Firm Foundation: A Simple Introduction to the Christian Bible",
+				"creators": [
+					{
+						"firstName": "Christopher J.",
+						"lastName": "Suehr",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020",
+				"ISBN": "9781792457418",
+				"abstractNote": "The Bible is big, complicated, and one of the most important books in human history.A Firm Foundation introduces the book that many call sacred, but few have studied.This versatile resource presents the Bible in plain language, with straightforward summaries, questions to facilitate discussion and reflection, and clearly defined terminology. It can stand alone, but works best as a companion to the bible.The material within has been used to teach a variety of students, including nonreligious beginners, clergy-in-training, English-language learners, and followers of other major world religions.Build a stronger understanding with A Firm Foundation.",
+				"libraryCatalog": "Dreier Neuerscheinungsdienst",
+				"numPages": "277 Seiten",
+				"place": "Iowa",
+				"publisher": "Kendall/Hunt Publishing Co ,U.S.Kendall",
+				"shortTitle": "A Firm Foundation",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.dietmardreier.de/de/detail/ISBN-9781626168084/Mosher-Lucinda/A-World-of-Inequalities",
+		"items": [
+			 {
+             "itemType": "book"
+             "creators": [
+               {
+                 "firstName": "Lucinda"
+                 "lastName": "Mosher"
+                 "creatorType": "editor"
+               }
+             ]
+             "notes": []
+             "tags": []
+             "seeAlso": []
+             "attachments": []
+             "title": "A World of Inequalities: Christian and Muslim Perspectives"
+             "ISBN": "9781626168084"
+             "publisher": "Georgetown University Press"
+             "place": "Washington, DC"
+             "date": "2021"
+             "numPages": "253 Seiten"
+             "abstractNote": "In this volume, leading Christian and Muslim scholars respond to the global crisis of inequality by demanding and modeling interreligious dialogue. Essays explore the roots of these realities, how they are treated in Christian and Muslim traditions and texts, and how the two faiths can work together to address inequality."
+             "libraryCatalog": "Dreier Neuerscheinungsdienst"
+             "shortTitle": "A World of Inequalities"
+           }
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.dietmardreier.de/de/detail/ISBN-9783806240580/Nussbaum-Martha/Kosmopolitismus",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "Kosmopolitismus: Revision eines Ideals",
+				"creators": [
+					{
+						"firstName": "Martha",
+						"lastName": "Nussbaum",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Manfred",
+						"lastName": "Weltecke",
+						"creatorType": "translator"
+					}
+				],
+				"date": "2020",
+				"ISBN": "9783806240580",
+				"abstractNote": "Was bedeutet die Würde des Menschen? Ein philosophischer Essay Der kynische Philosoph Diogenes bezeichnete sich selbst einen kosmopolitês, einen \"Bürger der Welt\". Damit war der Kosmopolitismus geboren. Ein Kosmopolit definiert sich als Mensch - unabhängig von seiner Abstammung, seinem Geschlecht oder seiner sozialen Herkunft. Diese Haltung ist die Geburtsstunde der Menschenwürde in der abendländischen Kultur. Die amerikanische Philosophin Martha Nussbaum erörtert in ihrem Essay die kulturelle Tradition des Kosmopolitismus in Europa seit der Antike. Sie erläutert die philosophische Idee grundlegend und zeigt Querverbindungen zu aktuellen gesellschaftlichen Debatten auf. Entwicklung der Idee einer universellen Menschenwürde von der Antike bis in die Gegenwart Ethische Dilemmas aus Pluralismus & Globalisierung: Kosmopolitismus als Lösung? Weiterentwicklung der philosophischen Idee: der Fähigkeitenansatz von Martha Nussbaum Tiefgründig und inspirierend: Die Autorin gilt als einflussreichste Philosophin der Gegenwart Die Grundlagen herausfordern: Viele Wesen, viele Arten von Würde Kann in Zeiten des Globalismus und Pluralismus der Kosmopolitismus eine Lösung für die aktuellen politisch-gesellschaftlichen Herausforderungen sein? Auf der Suche nach der Antwort untersucht Martha Nussbaum in ihrem Sachbuch die philosophische Strömung von den griechischen Stoikern und Cicero über Hugo Grotius, Immanuel Kant bis zu Adam Smith. Sie analysiert so den Einfluss des Kosmopolitismus in der politischen Theorie und benennt konsequent die Stärken und Defizite. Mit dem von ihr mitentwickelten Fähigkeitenansatz erweitert sie zudem den modernen Kosmopolitismus radikal über die menschliche Spezies hinaus. Eine tiefgründige Analyse sowohl für Philosophen als auch für politisch Interessierte: Dieses Buch regt zum Nachdenken an!",
+				"libraryCatalog": "Dreier Neuerscheinungsdienst",
+				"numPages": "352 Seiten",
+				"place": "Darmstadt",
+				"publisher": "wbg Theiss in Wissenschaftliche Buchgesellschaft (WBG)",
+				"shortTitle": "Kosmopolitismus",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
