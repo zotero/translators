8fdc2f10a6e0e89ae4848998916b66f135ef97cd
Abe Jellinek <jellinek@berkeley.edu>
2021-09-14 11:39:10 -0700

    Annual Reviews: Fix tags and multiples, lint/restructure

--- Annual Reviews.js
+++ Annual Reviews.js
@@ -1,7 +1,7 @@
 {
 	"translatorID": "5f22bd25-5b70-11e1-bb1d-c4f24aa18c1e",
 	"label": "Annual Reviews",
-	"creator": "Aurimas Vinckevicius",
+	"creator": "Aurimas Vinckevicius and Abe Jellinek",
 	"target": "^https?://[^/]*annualreviews\\.org(:[\\d]+)?(?=/)[^?]*(/(toc|journal|doi)/|showMost(Read|Cited)Articles|doSearch)",
 	"minVersion": "3.0",
 	"maxVersion": "",
@@ -9,11 +9,11 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-01-01 15:19:19"
+	"lastUpdated": "2021-09-14 18:37:45"
 }
 
 /**
-	Copyright (c) 2012 Aurimas Vinckevicius
+	Copyright (c) 2012-2021 Aurimas Vinckevicius and Abe Jellinek
 
 	This program is free software: you can redistribute it and/or
 	modify it under the terms of the GNU Affero General Public License
@@ -30,35 +30,33 @@
 	<http://www.gnu.org/licenses/>.
 */
 
-//add using BibTex
+// add using BibTex
 function addByBibTex(doi, tags) {
 	var baseUrl = 'http://www.annualreviews.org';
 	var risRequest = baseUrl + '/action/downloadCitation';
-	var articleUrl = baseUrl + '/doi/abs/' + doi;
 	var pdfUrl = baseUrl + '/doi/pdf/' + doi;
 
-	var postData = 'include=abs&direct=on&submit=Download+chapter+metadata&downloadFileName=citation' +
-			'&format=bibtex' +		//bibtex
-			'&doi=' + encodeURIComponent(doi);
+	var postData = 'include=abs&direct=on&submit=Download+chapter+metadata&downloadFileName=citation'
+			+ '&format=bibtex'		// bibtex
+			+ '&doi=' + encodeURIComponent(doi);
 
-	Zotero.Utilities.HTTP.doPost(risRequest, postData, function(text) {
+	Zotero.Utilities.HTTP.doPost(risRequest, postData, function (text) {
 		var translator = Zotero.loadTranslator('import');
-		translator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4');	//bibtex
+		translator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4');	// bibtex
 		translator.setString(text);
 
-		translator.setHandler('itemDone', function(obj, item) {
-			//title is sometimes in all caps
-			if (item.title == item.title.toUpperCase())
-				item.title = ZU.capitalizeTitle(item.title, true);
-			if (item.abstractNote){
-				item.abstractNote = item.abstractNote.replace(/^...?Abstract/, "")
+		translator.setHandler('itemDone', function (obj, item) {
+			// title is sometimes in all caps
+			if (item.title == item.title.toUpperCase()) item.title = ZU.capitalizeTitle(item.title, true);
+			if (item.abstractNote) {
+				item.abstractNote = item.abstractNote.replace(/^...?Abstract/, "");
 			}
-			//add tags
+			// add tags
 			if (tags) {
 				item.tags = tags;
 			}
 
-			//set PDF file
+			// set PDF file
 			item.attachments = [{
 				url: pdfUrl,
 				title: 'Full Text PDF',
@@ -73,60 +71,51 @@ function addByBibTex(doi, tags) {
 }
 
 function detectWeb(doc, url) {
-	var title = doc.title.toLowerCase();
-
-	if ( url.match(/\/doi\/(abs|full|pdf)\//) ) {
-
+	if (url.match(/\/doi\/(abs|full|pdf)\//)) {
 		return 'journalArticle';
-
-	} else if ( title.match('- table of contents -') ||
-		title.match('- most downloaded reviews') ||
-		title.match('- most cited reviews') ||
-		title.match('- forthcoming -') ||
-		title.match('search results') ||
-		url.match('/journal/') ) {		//individual journal home page
-
+	}
+	else if (getSearchResults(doc, true)) {
 		return 'multiple';
 	}
+	return false;
+}
+
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('.articleBoxWrapper');
+	if (!rows.length) rows = doc.querySelectorAll('.teaser');
+	for (let row of rows) {
+		let doi = attr(row, 'input[name="doi"]', 'value');
+		if (!doi) doi = ZU.cleanDOI(attr(row, 'h2 > a', 'href'));
+		let title = ZU.trimInternal(text(row, 'h2 > a'));
+		if (!doi || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[doi] = title;
+	}
+	return found ? items : false;
 }
 
 function doWeb(doc, url) {
-	if ( detectWeb(doc, url) == 'multiple' ) {
-		var articles = Zotero.Utilities.xpath(doc, '//div[@class="articleBoxWrapper"]');
-		var selectList = new Object();
-		var doi, title, article;
-		for ( var i in articles ) {
-			article = articles[i];
-			doi = Zotero.Utilities.xpath(article, './div[@class="articleCheck"]/input');
-			title = Zotero.Utilities.xpathText(article, './div[@class="articleBoxMeta"]/h2/a');
-			if ( doi && doi[0].value && title) {
-				selectList[doi[0].value] = title;
-			}
-		}
-
-		Zotero.selectItems(selectList, function(selectedItems) {
-			if (selectedItems == null) return true;
-			for (var item in selectedItems) {
-				addByBibTex(item);
-			}
+	if (detectWeb(doc, url) == "multiple") {
+		Zotero.selectItems(getSearchResults(doc, false), function (items) {
+			if (items) Object.keys(items).forEach(addByBibTex);
 		});
-	} else {
-		var match = url.match(/\/(?:abs|full|pdf)\/([^?]+)/);
-		if (match) {
-			//get keywords before we leave
-			var tags, keywords = ZU.xpath(doc,
-				'//form[@id="frmQuickSearch"]\
-				/div[@class="pageTitle" and contains(text(), "KEYWORD")]\
-				/following-sibling::div/span[@class="data"]');
-			if (keywords) {
-				tags = new Array();
-				for (var i=0, n=keywords.length; i<n; i++) {
-					tags.push(keywords[i].textContent.trim());
-				}
-			}
+	}
+	else {
+		scrape(doc, url);
+	}
+}
 
-			addByBibTex(match[1], tags);
-		}
+
+function scrape(doc, url) {
+	var match = url.match(/\/(?:abs|full|pdf)\/([^?]+)/);
+	if (match) {
+		let tags = attr(doc, 'meta[name="dc.Subject"]', 'content')
+			.split('; ')
+			.map(tag => ({ tag }));
+		addByBibTex(match[1], tags);
 	}
 }
 
@@ -159,7 +148,7 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "http://www.annualreviews.org/doi/abs/10.1146/annurev.biophys.29.1.545?prevSearch=&searchHistoryKey=",
+		"url": "https://www.annualreviews.org/doi/abs/10.1146/annurev.biophys.29.1.545?prevSearch=&searchHistoryKey=",
 		"items": [
 			{
 				"itemType": "journalArticle",
@@ -190,7 +179,7 @@ var testCases = [
 				"libraryCatalog": "Annual Reviews",
 				"pages": "545-576",
 				"publicationTitle": "Annual Review of Biophysics and Biomolecular Structure",
-				"url": "http://dx.doi.org/10.1146/annurev.biophys.29.1.545",
+				"url": "https://doi.org/10.1146/annurev.biophys.29.1.545",
 				"volume": "29",
 				"attachments": [
 					{
@@ -199,11 +188,21 @@ var testCases = [
 					}
 				],
 				"tags": [
-					"ADF/cofilins",
-					"Arp2/3 complex",
-					"WASp",
-					"cell motility",
-					"profilin"
+					{
+						"tag": "ADF/cofilins"
+					},
+					{
+						"tag": "Arp2/3 complex"
+					},
+					{
+						"tag": "WASp"
+					},
+					{
+						"tag": "cell motility"
+					},
+					{
+						"tag": "profilin"
+					}
 				],
 				"notes": [],
 				"seeAlso": []
@@ -214,6 +213,70 @@ var testCases = [
 		"type": "web",
 		"url": "http://www.annualreviews.org/toc/anchem/5/1",
 		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.annualreviews.org/toc/linguistics/current",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.annualreviews.org/doi/abs/10.1146/annurev-linguistics-081720-111352",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Linguistics Then and Now: Some Personal Reflections",
+				"creators": [
+					{
+						"firstName": "Noam",
+						"lastName": "Chomsky",
+						"creatorType": "author"
+					}
+				],
+				"date": "2021",
+				"DOI": "10.1146/annurev-linguistics-081720-111352",
+				"abstractNote": "By mid-twentieth century, a working consensus had been reached in the linguistics community, based on the great achievements of preceding years. Synchronic linguistics had been established as a science, a “taxonomic” science, with sophisticated procedures of analysis of data. Taxonomic science has limits. It does not ask “why?” The time was ripe to seek explanatory theories, using insights provided by the theory of computation and studies of explanatory depth. That effort became the generative enterprise within the biolinguistics framework. Tensions quickly arose: The elements of explanatory theories (generative grammars) were far beyond the reach of taxonomic procedures. The structuralist principle that language is a matter of training and habit, extended by analogy, was unsustainable. More generally, the mood of “virtually everything is known” became “almost nothing is understood,” a familiar phenomenon in the history of science, opening a new and exciting era for a flourishing discipline.",
+				"issue": "1",
+				"itemID": "doi:10.1146/annurev-linguistics-081720-111352",
+				"libraryCatalog": "Annual Reviews",
+				"pages": "1-11",
+				"publicationTitle": "Annual Review of Linguistics",
+				"shortTitle": "Linguistics Then and Now",
+				"url": "https://doi.org/10.1146/annurev-linguistics-081720-111352",
+				"volume": "7",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"tags": [
+					{
+						"tag": "autobiography"
+					},
+					{
+						"tag": "biolinguistics program"
+					},
+					{
+						"tag": "explanatory linguistic theory"
+					},
+					{
+						"tag": "explanatory theories"
+					},
+					{
+						"tag": "generative enterprise"
+					},
+					{
+						"tag": "history of linguistics"
+					},
+					{
+						"tag": "history of science"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
 	}
 ]
 /** END TEST CASES **/
