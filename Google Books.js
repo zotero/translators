99fd87ce69f4aeb5772c5aeb4d01a1168f3fa5d7
Abe Jellinek <jellinek@berkeley.edu>
2021-10-12 11:14:45 -0700

    Google Books: Fall back on page title if no dc:title

--- Google Books.js
+++ Google Books.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsbv",
-	"lastUpdated": "2019-12-09 13:35:39"
+	"lastUpdated": "2021-10-12 18:13:54"
 }
 
 /*
@@ -129,10 +129,16 @@ function scrape(doc, url) {
 		var canonicalUrl = ZU.xpath(doc, '/html/head/link[@rel="canonical"]')[0].href;
 		id = canonicalUrl.match(/[&?]id=([^&]+)/)[1];
 	}
-	ZU.doGet("//books.google.com/books/feeds/volumes/" + id, parseXML);
+	
+	// somewhat of a hack to recover from a rare situation in which the XML
+	// doesn't have a dc:title tag (bad metadata on Google's end).
+	// the Google Books suffix appears consistent across languages.
+	let fallbackTitle = doc.title.replace(/(\s*-\s*)?Google Books/, '');
+	ZU.doGet("//books.google.com/books/feeds/volumes/" + id,
+		text => parseXML(text, fallbackTitle));
 }
 
-function parseXML(text) {
+function parseXML(text, fallbackTitle) {
 	// Z.debug(text);
 	// Remove xml parse instruction and doctype
 	var parser = new DOMParser();
@@ -180,7 +186,7 @@ function parseXML(text) {
 	newItem.ISBN = ISBN;
 	
 	newItem.publisher = ZU.xpathText(xml, "dc:publisher", ns);
-	newItem.title = ZU.xpathText(xml, "dc:title", ns, ": ");
+	newItem.title = ZU.xpathText(xml, "dc:title", ns, ": ") || fallbackTitle;
 	newItem.language = ZU.xpathText(xml, 'dc:language', ns);
 	newItem.abstractNote = ZU.xpathText(xml, 'dc:description', ns);
 	newItem.date = ZU.xpathText(xml, "dc:date", ns);
@@ -512,6 +518,33 @@ var testCases = [
 						"tag": "Psychology / General"
 					}
 				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.google.com/books/edition/_/IG1qDwAAQBAJ?hl=en",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "Recursive Macroeconomic Theory",
+				"creators": [],
+				"ISBN": "9780262038669",
+				"extra": "Google-Books-ID: IG1qDwAAQBAJ",
+				"language": "en",
+				"libraryCatalog": "Google Books",
+				"numPages": "1477",
+				"publisher": "MIT Press",
+				"attachments": [
+					{
+						"title": "Google Books Link",
+						"snapshot": false,
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
 				"notes": [],
 				"seeAlso": []
 			}
