ef0e08cdc5da22058cd3b1d1cba36f23440f7268
socheres <kim.changhe@googlemail.com>
2021-11-19 12:00:51 +0100

    add orcid for review https://github.com/ubtue/DatenProbleme/issues/1620

--- SAGE Journals.js
+++ SAGE Journals.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-11-18 16:31:17"
+	"lastUpdated": "2021-11-19 11:00:40"
 }
 
 /*
@@ -134,18 +134,21 @@ function scrape(doc, url) {
 				let regexName = /author=.*"/;
 				if(entryHTML.match(regexOrcid)) {
 					item.notes.push({note: "orcid:" + entryHTML.match(regexOrcid)[0] + ' | ' + entryHTML.match(regexName)[0].replace('\"', '')});
+				//scrape ORCID at the bottom of text if css selector ".author-section-div" is empty. E.g. most of cases by reviews https://journals.sagepub.com/doi/10.1177/15423050211028189
+				} 
+				else if (entryHTML.match(regexOrcid) !== null){
+					let ReviewAuthorSectionEntries = doc.querySelectorAll('.NLM_fn p');
+					for (let ReviewAuthorSectionEntry of ReviewAuthorSectionEntries) {
+						let entryInnerText = ReviewAuthorSectionEntry.innerText;
+						let regexOrcid = /\d+-\d+-\d+-\d+x?/i;
+						if(entryInnerText.match(regexOrcid) && entryInnerText.split('\n')[1] != undefined) {
+							let authorEntry = entryInnerText.split('\n')[1].replace(/https:\/\/.*/, '');
+							item.notes.push({note: "orcid:" + entryInnerText.match(regexOrcid)[0] + ' | ' + entryInnerText.match(authorEntry)[0].replace('\"', '')});
+						}				
+					}	
 				}
 			}
-			//scrape ORCID for reviews e.g. https://journals.sagepub.com/doi/10.1177/15423050211028189
-			let ReviewAuthorSectionEntries = doc.querySelectorAll('.NLM_fn p');
-			for (let ReviewAuthorSectionEntry of ReviewAuthorSectionEntries) {
-				let entryInnerText = ReviewAuthorSectionEntry.innerText;
-				let regexOrcid = /\d+-\d+-\d+-\d+x?/i;
-				if(entryInnerText.match(regexOrcid) && entryInnerText.split('\n')[1] != undefined) {
-					let authorEntry = entryInnerText.split('\n')[1].replace(/https:\/\/.*/, '');
-					item.notes.push({note: "orcid:" + entryInnerText.match(regexOrcid)[0] + ' | ' + entryInnerText.match(authorEntry)[0].replace('\"', '')});
-				}				
-			}
+
 			// Workaround to address address weird incorrect multiple extraction by both querySelectorAll and xpath
 			// So, let's deduplicate...
 			item.notes = Array.from(new Set(item.notes.map(JSON.stringify))).map(JSON.parse);
@@ -426,9 +429,6 @@ var testCases = [
 					},
 					{
 						"note": "orcid:0000-0002-0893-3054 | author=Glasdam, Stinne"
-					},
-					{
-						"note": "orcid:0000-0002-0893-3054 | Stinne Glasdam "
 					},
 					{
 						"note": "LF:"
@@ -722,9 +722,6 @@ var testCases = [
 					},
 					{
 						"note": "orcid:0000-0003-2751-3204 | author=Grzymała-Moszczyńska, Halina"
-					},
-					{
-						"note": "orcid:0000-0001-6906-3104 | Adam Anczyk "
 					}
 				],
 				"seeAlso": []
@@ -771,9 +768,6 @@ var testCases = [
 				"notes": [
 					{
 						"note": "<p>doi: 10.1177/15423050211028189</p>"
-					},
-					{
-						"note": "orcid:0000-0002-5423-3796 | Emi Alisa Johnson Brand "
 					}
 				],
 				"seeAlso": []
