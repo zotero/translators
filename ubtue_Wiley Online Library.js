7aab251157ed4f8438cd640099d9b714bcf76185
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-03-23 11:25:02 +0100

    https://github.com/ubtue/zotero-translators/pull/246#discussion_r833017042

--- ubtue_Wiley Online Library.js
+++ ubtue_Wiley Online Library.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-03-17 12:48:09"
+	"lastUpdated": "2022-03-23 10:24:14"
 }
 
 /*
@@ -87,6 +87,10 @@ function addArticleNumber (doc, item) {
 		item.pages = 'article ' + item.pages;	
 	}
 }
+//ubtue: replace '-' with '/' according to guidelines
+function normalizeIssue(doc, item) {
+	if (item.issue != undefined) item.issue = item.issue.replace(/-/g, "/");
+}
 
 function scrapeBook(doc, url) {
 	var title = doc.getElementById('productTitle');
@@ -139,7 +143,7 @@ function scrapeBook(doc, url) {
 			'/h6[normalize-space(text())="About The Product"]',
 			'/following-sibling::p'].join(''), null, "\n") || "");
 	newItem.accessDate = 'CURRENT_TIMESTAMP';
-	if (item.issue != undefined) item.issue = item.issue.replace(/-/g, "/");
+	normalizeIssue(doc, item);
 	newItem.complete();
 }
 
@@ -204,7 +208,7 @@ function scrapeEM(doc, url) {
 				n--;
 			}
 		}
-		if (item.issue != undefined) item.issue = item.issue.replace(/-/g, "/");
+		normalizeIssue(doc, item);
 		item.complete();
 	});
 
@@ -212,7 +216,7 @@ function scrapeEM(doc, url) {
 	addArticleNumber(doc, item);
 	addFreeAccessTag(doc, item);
 	getORCID(doc, item);
-	if (item.issue != undefined) item.issue = item.issue.replace(/-/g, "/");
+	normalizeIssue(doc, item);
 	item.complete();
 
 	translator.getTranslatorObject(function(em) {
@@ -380,7 +384,7 @@ function scrapeBibTeX(doc, url) {
 				item.DOI = item.DOI.replace(/^https:\/\/doi.org\/(.*)/, "$1");
 			addFreeAccessTag(doc, item);
 			getORCID(doc, item);
-			if (item.issue != undefined) item.issue = item.issue.replace(/-/g, "/");
+			normalizeIssue(doc, item);
 			item.complete();
 		});
 
@@ -457,7 +461,7 @@ function scrapeCochraneTrial(doc, url){
 
 	addBookReviewTag(doc, item);
 	addArticleNumber(doc, item);
-	if (item.issue != undefined) item.issue = item.issue.replace(/-/g, "/");
+	normalizeIssue(doc, item);
 	item.complete();
 }
 
