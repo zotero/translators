deb4c3c894eb0e4b2bab541ae0dcbe93cae10f1c
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-07-08 18:03:19 +0200

    ubtue/DatenProbleme/issues/1766

--- ubtue_Project MUSE.js
+++ ubtue_Project MUSE.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-11-09 14:10:56"
+	"lastUpdated": "2022-07-08 16:02:03"
 }
 
 /*
@@ -115,13 +115,20 @@ function scrape(doc) {
 			if (dcType && dcType.match(/Review/i)) {
 				item.tags.push("Book Review");
 			}
-			
+			if (item.pages) {
+				if (item.pages.match(/[ivx]+-[ivx+]/i) != null) {
+					if (item.pages.match(/([ivx]+)-[ivx+]/i)[1] == item.pages.match(/[ivx]+-([ivx+])/i)[1]) {
+						item.pages = item.pages.match(/([ivx]+)-[ivx+]/i)[1];
+					}
+				}
+			}
 			item.notes = [];
 			item.complete();
 		});
 		translator.translate();
 	});
-}/** BEGIN TEST CASES **/
+}
+/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
@@ -140,7 +147,7 @@ var testCases = [
 				"date": "2006",
 				"ISSN": "1477-464X",
 				"issue": "1",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"pages": "121-164",
 				"publicationTitle": "Past & Present",
 				"url": "https://muse.jhu.edu/article/200965",
@@ -174,7 +181,7 @@ var testCases = [
 				"date": "2009",
 				"ISBN": "9780820705057",
 				"abstractNote": "In Writing the Forest in Early Modern England: A Sylvan Pastoral Nation, Jeffrey S. Theis focuses on pastoral literature in early modern England as an emerging form of nature writing. In particular, Theis analyzes what happens when pastoral writing is set in forests — what he terms “sylvan pastoral.”\nDuring the sixteenth and seventeenth centuries, forests and woodlands played an instrumental role in the formation of individual and national identities in England. Although environmentalism as we know it did not yet exist, persistent fears of timber shortages led to a larger anxiety about the status of forests. Perhaps more important, forests were dynamic and contested sites of largely undeveloped spaces where the poor would migrate in a time of rising population when land became scarce. And in addition to being a place where the poor would go, the forest also was a playground for monarchs and aristocrats where they indulged in the symbolically rich sport of hunting.\nConventional pastoral literature, then, transforms when writers use it to represent and define forests and the multiple ways in which English society saw these places. In exploring these themes, authors expose national concerns regarding deforestation and forest law and present views relating to land ownership, nationhood, and the individual’s relationship to nature. Of particular interest are the ways in which cultures turn confusing spaces into known places and how this process is shaped by nature, history, gender, and class.\nTheis examines the playing out of these issues in familiar works by Shakespeare, such as A Midsummer Night’s Dream, The Merry Wives of Windsor, and As You Like It, Andrew Marvell’s “Upon Appleton House,” John Milton’s Mask and Paradise Lost, as well as in lesser known prose works of the English Revolution, such as James Howell’s Dendrologia>/i> and John Evelyn’s Sylva.\nAs a unique ecocritical study of forests in early modern English literature, Writing the Forest makes an important contribution to the growing field of the history of environmentalism, and will be of interest to those working in literary and cultural history as well as philosophers concerned with nature and space theory.",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"place": "Pittsburgh",
 				"publisher": "Duquesne University Press",
 				"shortTitle": "Writing the Forest in Early Modern England",
@@ -205,7 +212,7 @@ var testCases = [
 				"ISSN": "1097-3729",
 				"abstractNote": "This article uses coverage of the fiftieth anniversary of the Pill as an example of what Richard Hirsh describes as the “real world” role of historians of technology. It explores how the presentation of historical topics on the world wide web has complicated how the history of technology is conveyed to the public. The article shows that that the Pill is especially suited to demonstrating the public role of historians of technology because, as the most popular form of reversible birth control, it has touched the lives of millions of Americans. Thus, an exploration of how the Pill’s fiftieth anniversary was covered illustrates how historians can use their expertise to provide a nuanced interpretation of a controversial topic in the history of technology.",
 				"issue": "4",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"pages": "735-745",
 				"publicationTitle": "Technology and Culture",
 				"shortTitle": "The Pill at Fifty",
@@ -237,7 +244,7 @@ var testCases = [
 				"ISSN": "1542-4278",
 				"abstractNote": "This article highlights an important paradox: in Argentina between 2003 and 2013 the center-left Peronist government’s approach to governance mirrors that of the center-right Peronist administration of the 1990s. While the latter centralized authority to pursue neoliberal reforms, the former have centralized authority in the name of expanding government intervention in the economy. In both cases, corruption has tended to go unchecked due to insufficient government accountability. Therefore, although economic policies and political rhetoric have changed dramatically, government corruption remains a constant of the Argentine political system due to the executive branch’s ability to emasculate constitutional checks and balances.",
 				"issue": "2",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"pages": "173-195",
 				"publicationTitle": "Latin American Research Review",
 				"url": "https://muse.jhu.edu/article/551992",
@@ -268,7 +275,7 @@ var testCases = [
 				"ISSN": "1947-8224",
 				"abstractNote": "During the Second Vatican Council, American Jewish community members impacted the drafting of the declaration on the Catholic Church's attitude toward Jews and Judaism. This article explores the American Jewish Committee's reactions to the drafting and promulgation of the Declaration on the Relation of the Church with Non-Christian Religions (Nostra Aetate) and its contribution to establishing interfaith relations. The varied Jewish reactions to the declaration provide insight into the internal Jewish discussions regarding Nostra Aetate, revealing that even though the declaration is assessed positively today, initial Jewish reactions were not enthusiastic.",
 				"issue": "3",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"pages": "25-47",
 				"publicationTitle": "U.S. Catholic Historian",
 				"shortTitle": "American Judaism and the Second Vatican Council",
@@ -341,7 +348,7 @@ var testCases = [
 				"DOI": "10.1353/cro.2021.0019",
 				"ISSN": "1939-3881",
 				"issue": "2",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"pages": "222-225",
 				"publicationTitle": "CrossCurrents",
 				"shortTitle": "Climate Change and the Art of Devotion",
@@ -375,7 +382,7 @@ var testCases = [
 				"date": "2021",
 				"ISSN": "2056-5666",
 				"issue": "3",
-				"libraryCatalog": "Project MUSE",
+				"libraryCatalog": "ubtue_Project MUSE",
 				"pages": "348-351",
 				"publicationTitle": "Christianity & Literature",
 				"shortTitle": "Dostoevsky’s Incarnational Realism",
@@ -387,6 +394,36 @@ var testCases = [
 						"tag": "Book Review"
 					}
 				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://muse.jhu.edu/article/855307",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "About This Issue",
+				"creators": [
+					{
+						"lastName": "Endres",
+						"firstName": "David J.",
+						"creatorType": "author"
+					}
+				],
+				"date": "2022",
+				"DOI": "10.1353/cht.2022.0006",
+				"ISSN": "1947-8224",
+				"issue": "2",
+				"libraryCatalog": "ubtue_Project MUSE",
+				"pages": "i",
+				"publicationTitle": "U.S. Catholic Historian",
+				"url": "https://muse.jhu.edu/article/855307",
+				"volume": "40",
+				"attachments": [],
+				"tags": [],
 				"notes": [],
 				"seeAlso": []
 			}
