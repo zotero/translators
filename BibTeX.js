zotero/translators

commit 76146e54dd3ccb34f291018d29e4de767a9aba94

BibTeX: Don't escape characters if "UTF-8 without BOM" is selected
https://forums.zotero.org/discussion/comment/85557/#Comment_85557

This has apparently been broken for over a decade. The default "UTF-8"
option also isn't actually including a BOM, so we'll have to decide what
to do about that, but we can deal with that separately.

BibTeX.js
@@ -18,7 +18,7 @@
 	},
 	"inRepository": true,
 	"translatorType": 3,
-	"lastUpdated": "2021-09-10 18:17:50"
+	"lastUpdated": "2022-10-31 04:44:38"
 }
 
 /*
@@ -1084,7 +1084,7 @@ function writeField(field, value, isMacro) {
 			value = ZU.XRegExp.replace(value, protectCapsRE, "$1{$2$3}"); // only $2 or $3 will have a value, not both
 		}
 	}
-	if (Zotero.getOption("exportCharset") != "UTF-8") {
+	if (!Zotero.getOption("exportCharset").startsWith("UTF-8")) {
 		value = value.replace(/[\u0080-\uFFFF]/g, mapAccent);
 	}
 	//convert the HTML markup allowed in Zotero for rich text to TeX; excluding doi/url/file shouldn't be necessary, but better to be safe;

commit 0d26b12641666f3393916b04335028a4c9144e5c

BibTeX: Fix error if passed options without `exportCharset`
Fixes an error in the client tests

Regression from 76146e54dd3

BibTeX.js
@@ -18,7 +18,7 @@
 	},
 	"inRepository": true,
 	"translatorType": 3,
-	"lastUpdated": "2022-10-31 04:44:38"
+	"lastUpdated": "2022-10-31 23:11:08"
 }
 
 /*
@@ -1084,7 +1084,8 @@ function writeField(field, value, isMacro) {
 			value = ZU.XRegExp.replace(value, protectCapsRE, "$1{$2$3}"); // only $2 or $3 will have a value, not both
 		}
 	}
-	if (!Zotero.getOption("exportCharset").startsWith("UTF-8")) {
+	var exportCharset = Zotero.getOption("exportCharset");
+	if (exportCharset && !exportCharset.startsWith("UTF-8")) {
 		value = value.replace(/[\u0080-\uFFFF]/g, mapAccent);
 	}
 	//convert the HTML markup allowed in Zotero for rich text to TeX; excluding doi/url/file shouldn't be necessary, but better to be safe;