503c3dbf7fdc72f3b38f6e19781b1c1e29ca623d
Abe Jellinek <jellinek@berkeley.edu>
2023-01-23 12:23:21 -0500

    Fix lint error

--- DOI.js
+++ DOI.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-01-23 17:03:43"
+	"lastUpdated": "2023-01-23 17:23:16"
 }
 
 /*
@@ -220,7 +220,7 @@ function retrieveDOIs(doiOrDOIs) {
 }
 
 function doWeb(doc, url) {
-	let doiOrDOIs = getDOIs(doc, url)
+	let doiOrDOIs = getDOIs(doc, url);
 	Z.debug(doiOrDOIs);
 	retrieveDOIs(doiOrDOIs);
 }
