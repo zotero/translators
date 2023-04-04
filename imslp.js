7dd4861022c8238091b2d535017515444a0a87a9
YohaneWW <20187175+YohaneWW@users.noreply.github.com>
2023-04-05 00:21:34 +0800

    fix compare error

--- imslp.js
+++ imslp.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-04-04 06:54:03"
+	"lastUpdated": "2023-04-04 15:45:17"
 }
 
 /*
@@ -59,10 +59,10 @@ function getSearchResults(doc, url, checkOnly) {
 	}
 	return found ? items : false;
 }
+
 async function doWeb(doc, url) {
 	if (detectWeb(doc, url) == 'multiple') {
 		let items = await Zotero.selectItems(getSearchResults(doc, false));
-		//Z.debug({items})
 		if (!items) return;
 		for (var urls of Object.keys(items)) {
 			await scrape(await requestDocument(urls));
@@ -78,76 +78,92 @@ async function scrape(doc, url) {
 	var item = new Zotero.Item("book");
 	var title = text(doc, '#firstHeading');
 	
-	//getMeta
-	var rows = doc.querySelector('div.wi_body > table').rows;
+	//check metadata
+	var rows = doc.querySelector('.wi_body table').rows;
 	for (var i = 2; i < rows.length; i++) {
-		if (rows[i].cells[0].innerText == "Name Translations") {
-			var names = rows[i].cells[1].innerText;
-			var names_test = "Name Translations";
+		if (rows[i].cells[0].innerText.trim() == "Name Translations") {
+			var names = rows[i].cells[1].innerText.trim();
 		}
-		//!a issue here, when handle multiple, the compare get false.!
-		Z.debug(rows[2].cells[0].innerText);
-		Z.debug(rows[2].cells[0].innerText == "Name Translations");
-		Z.debug(names_test == "Name Translations");
-		if (rows[i].cells[0].innerText == "Composer") {
-			var author = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Composer") {
+			var author = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Opus/Catalogue Number") {
-			var seriesNumber = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Opus/Catalogue Number") {
+			var seriesNumber = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Key") {
-			var key = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Key") {
+			var key = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Movements/Sections") {
-			var movements = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Movements/Sections") {
+			var movements = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Year/Date of Composition") {
-			var date = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Year/Date of Composition") {
+			var date = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "First Performance") {
-			var place = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "First Performance") {
+			var place = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "First Publication") {
-			var publisher = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "First Publication") {
+			var publisher = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Language") {
-			var language = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Language") {
+			var language = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Average Duration") {
-			var duration = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Average Duration") {
+			var duration = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Piece Style") {
-			var style = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Piece Style") {
+			var style = rows[i].cells[1].innerText.trim();
 		}
-		if (rows[i].cells[0].innerText == "Instrumentation") {
-			var instrumentation = rows[i].cells[1].innerText;
+		if (rows[i].cells[0].innerText.trim() == "Instrumentation") {
+			var instrumentation = rows[i].cells[1].innerText.trim();
 		}
 	}
-	//Z.debug(author)
-	//Z.debug(seriesNumber)
-		
+
+	//push metadata
 	item.title = title.replace(/\(.*\)/, "");
-	//The if is only for test to no error
 	if (author) {
 		item.creators.push(ZU.cleanAuthor(author, "author", true));
+		item.tags.push(author.split(',')[0]);
 	}
-	item.abstractNote = names + "\n\n" + movements;
-	item.seriesNumber = seriesNumber;
-	item.place = "First Performance: " + place;
-	item.publisher = publisher;
-	item.date = date;
-	item.language = language;
+	if (names) {
+		item.abstractNote = names.replace(/\[.*\]/, "");
+	}
+	if (movements) {
+		item.notes.push = "<h1>Movement:<h1>\n" + movements;
+	}
+	if (seriesNumber) {
+		item.seriesNumber = seriesNumber;
+	}
+	if (place) {
+		item.place = "First Performance: " + place;
+	}
+	if (publisher) {
+		item.publisher = publisher;
+	}
+	if (date) {
+		item.date = date;
+	}
+	if (language) {
+		item.language = language;
+	}
 	item.url = url;
 	item.libraryCatalog = null;
-	item.extra = "Type: musical_score" + "\n" + "Duration: " + duration;
-	item.tags.push(key);
-	item.tags.push(style);
-	if (instrumentation) {
-		for (var tag of instrumentation.split(',')) {
-			item.tags.push(tag.trim());
-		}
+	if (duration) {
+		item.extra = "Type: musical_score\nDuration: " + duration;
 	}
-	
+	else {
+		item.extra = "Type: musical_score";
+	}
+	if (key) {
+		item.tags.push(key);
+	}
+	if (style) {
+		item.tags.push(style);
+	}
+	if (instrumentation) {
+		item.notes.push = "<h1>Instrumentation:<h1>\n" + instrumentation;
+	}
+
 	item.complete();
 }
 
@@ -169,8 +185,8 @@ var testCases = [
 					}
 				],
 				"date": "1908–09",
-				"abstractNote": "大地之歌; Pieseň o zemi; Песнь о земле; השיר על הארץ; 대지의 노래; [7 more...]\n\n6 movements:\nDas Trinklied vom Jammer der Erde\nDer Einsame im Herbst\nVon der Jugend\nVon der Schönheit\nDer Trunkene im Frühling\nDer Abschied",
-				"extra": "Type: musical_score\nDuration: undefined",
+				"abstractNote": "大地之歌; Pieseň o zemi; Песнь о земле; השיר על הארץ; 대지의 노래;",
+				"extra": "Type: musical_score",
 				"language": "German",
 				"place": "First Performance: 1911-11-20 in Munich, Tonhalle.\nSara Cahier (alto), William Miller (tenor), Orchestra, Bruno Walter (conductor)",
 				"publisher": "1912",
@@ -178,13 +194,10 @@ var testCases = [
 				"attachments": [],
 				"tags": [
 					{
+						"tag": "Mahler"
+					},
+					{
 						"tag": "Romantic"
-					},
-					{
-						"tag": "orchestra"
-					},
-					{
-						"tag": "voices"
 					}
 				],
 				"notes": [],
