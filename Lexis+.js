4e014b544ebe13b24435eb825bacd8b5088dd91a
OCDkirby <59982409+OCDkirby@users.noreply.github.com>
2023-04-07 10:39:56 -0500

    Lexis+: Begin refactoring to new API

--- Lexis+.js
+++ Lexis+.js
@@ -9,12 +9,84 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-04-06 21:58:14"
+	"lastUpdated": "2023-04-07 14:01:13"
 }
+
+/*
+    ***** BEGIN LICENSE BLOCK *****
+
+    Copyright © 2022 YOUR_NAME <- TODO
+
+    This file is part of Zotero.
+
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+    ***** END LICENSE BLOCK *****
+*/
+
+
+// function detectWeb(doc, url) {
+// 	// TODO: adjust the logic here
+// 	if (url.includes('/article/')) {
+// 		return 'newspaperArticle';
+// 	}
+// 	else if (getSearchResults(doc, true)) {
+// 		return 'multiple';
+// 	}
+// 	return false;
+// }
+
+// function getSearchResults(doc, checkOnly) {
+// 	var items = {};
+// 	var found = false;
+// 	// TODO: adjust the CSS selector
+// 	var rows = doc.querySelectorAll('h2 > a.title[href*="/article/"]');
+// 	for (let row of rows) {
+// 		// TODO: check and maybe adjust
+// 		let href = row.href;
+// 		// TODO: check and maybe adjust
+// 		let title = ZU.trimInternal(row.textContent);
+// 		if (!href || !title) continue;
+// 		if (checkOnly) return true;
+// 		found = true;
+// 		items[href] = title;
+// 	}
+// 	return found ? items : false;
+// }
+
+// async function doWeb(doc, url) {
+// 	if (detectWeb(doc, url) == 'multiple') {
+// 		let items = await Zotero.selectItems(getSearchResults(doc, false));
+// 		if (!items) return;
+// 		for (let url of Object.keys(items)) {
+// 			await scrape(await requestDocument(url));
+// 		}
+// 	}
+// 	else {
+// 		await scrape(doc, url);
+// 	}
+// }
+
+// async function scrape(doc, url = doc.location.href) {
+// 	// TODO: implement or add a scrape function template
+// }
+
+
 
 function scrape(doc, url) {
 	var namespace = doc.documentElement.namespaceURI;
-	var nsResolver = namespace ? function(prefix) {
+	var nsResolver = namespace ? function([prefix]) {
 	if (prefix == "x" ) return namespace; else return null;
 	} : null;
 
@@ -23,24 +95,19 @@ function scrape(doc, url) {
 		var newCase = new Zotero.Item("case");
 		newCase.url = doc.location.href;
 		
-		var xPathofTitle = doc.evaluate('//h1[@id="SS_DocumentTitle"]',
-									 doc, nsResolver, XPathResult.ANY_TYPE, null);
-		newCase.title = xPathofTitle.iterateNext().textContent;
+		newCase.title = text(doc, 'h1#SS_DocumentTitle');
 
-		var xPathofCitation = doc.evaluate('//span[@class="active-reporter"]',
-										   doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var citation = xPathofCitation.iterateNext().textContent;
+		var xPathofCitation = ZU.xpath(doc, '//span[@class="active-reporter"]');
+		var citation = xPathofCitation[0].textContent;
 		newCase.reporterVolume = citation.substring(0, citation.indexOf(' '));
 		newCase.reporter = citation.substring(citation.indexOf(' ') + 1, citation.lastIndexOf(' '));
 		newCase.firstPage = citation.substring(citation.lastIndexOf(' ') + 1);
 
-		var xPathofCourt = doc.evaluate('(//p[@class="SS_DocumentInfo"])[1]',
-										doc, nsResolver, XPathResult.ANY_TYPE, null);
-		newCase.court = xPathofCourt.iterateNext().textContent;
+		var xPathofCourt = ZU.xpath(doc, '(//p[@class="SS_DocumentInfo"])[1]', nsResolver);
+		newCase.court = xPathofCourt[0].textContent;
 
-		var xPathofDate = doc.evaluate('//span[@class="date"]',
-									   doc, nsResolver, XPathResult.ANY_TYPE, null);
-		newCase.dateDecided = xPathofDate.iterateNext().textContent;
+		var xPathofDate = ZU.xpath(doc, '//span[@class="date"]', nsResolver);
+		newCase.dateDecided = xPathofDate[0].textContent;
 
 		newCase.complete();
 	}
@@ -49,14 +116,12 @@ function scrape(doc, url) {
 		var newStatute = new Zotero.Item("statute");
 		newStatute.url = doc.location.href;
 
-		var xPathofTitle = doc.evaluate('//h1[@id="SS_DocumentTitle"]',
-									 doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var title = xPathofTitle.iterateNext().textContent;
+		var xPathofTitle = ZU.xpath(doc, '//h1[@id="SS_DocumentTitle"]', nsResolver);
+		var title = xPathofTitle[0].textContent;
 		newStatute.title = title;
 
-		var xPathofInfo = doc.evaluate('//p[@class="SS_DocumentInfo"]',
-										doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var info = xPathofInfo.iterateNext().textContent;
+		var xPathofInfo = ZU.xpath(doc, '//p[@class="SS_DocumentInfo"]', nsResolver);
+		var info = xPathofInfo[0].textContent;
 
 		isolation = info.substring(info.search(
 			/\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)/
@@ -68,11 +133,11 @@ function scrape(doc, url) {
 		{
 			// BB 21st ed. requires parallel cite to Pub. L. No. and Stat. for session laws
 			var statutesAtLarge, publicLawNo;
-			var xPathofActiveReporter = doc.evaluate('//a[@class="SS_ActiveRptr"]',
-									 				 doc, nsResolver, XPathResult.ANY_TYPE, null);
-			var potentialReporter = xPathofActiveReporter.iterateNext();
-			if (potentialReporter) // Sometimes Lexis is weird and doesn't give an ActiveRptr
+			var xPathofActiveReporter = ZU.xpath(doc, '//a[@class="SS_ActiveRptr"]', nsResolver);
+			if (xPathofActiveReporter.length > 0) // Sometimes Lexis is weird and doesn't give an ActiveRptr
 			{
+        var potentialReporter = xPathofActiveReporter[0];
+        Zotero.debug(potentialReporter.textContent);
 				if (potentialReporter.textContent.match(/[sS]tat\./))
 					statutesAtLarge = potentialReporter.textContent;
 				else if (potentialReporter.textContent.match(/[pP]ub\./) ||
@@ -80,16 +145,16 @@ function scrape(doc, url) {
 					publicLawNo = potentialReporter.textContent;
 			}
 
-			var xPathofNonPaginatedReporter = doc.evaluate('//span[@class="SS_NonPaginatedRptr"]',
-									 					   doc, nsResolver, XPathResult.ANY_TYPE, null);
-			var nextReporter;
-			while (nextReporter = xPathofNonPaginatedReporter.iterateNext())
+			var xPathofNonPaginatedReporter = ZU.xpath(doc, '//span[@class="SS_NonPaginatedRptr"]', nsResolver);
+			
+			for (var i = 0; i < xPathofNonPaginatedReporter.length; i++)
 			{
-				if (nextReporter.textContent.match(/[sS]tat\./))
-					statutesAtLarge = nextReporter.textContent;
-				else if (nextReporter.textContent.match(/[pP]ub\./) ||
-						 nextReporter.textContent.match(/[pP]\.[lL]\./))
-					publicLawNo = nextReporter.textContent;
+        var nextReporter = xPathofNonPaginatedReporter[i].textContent;
+				if (nextReporter.match(/[sS]tat\./))
+					statutesAtLarge = nextReporter;
+				else if (nextReporter.match(/[pP]ub\./) ||
+						 nextReporter.match(/[pP]\.[lL]\./))
+					publicLawNo = nextReporter;
 			}
 
 			// Turn publicLawNo into the public law fields
@@ -98,7 +163,7 @@ function scrape(doc, url) {
 				var numPos = publicLawNo.search(/\d+-\d+/)
 				newStatute.publicLawNumber = publicLawNo.substring(
 					numPos,
-					publiclawNo.substring(numPos + 1).indexOf(' ')); // Gets 115-164
+					publicLawNo.substring(numPos + 1).indexOf(' ')); // Gets 115-164
 
 				newStatute.session = newStatute.publicLawNumber.substring(0, newStatute.publicLawNumber.indexOf('-'));
 			}
@@ -144,17 +209,18 @@ function scrape(doc, url) {
 
 function detectWeb(doc, url) {
 	if (doc.title.match(/.*results.*/)) {
-		return "multiple"
+    Zotero.debug("multiple");
+		return "multiple";
 	}
 	else if (doc.title.match(/[a-zA-Z\. ]+\s§\s\d+/) ||
 			 doc.title.match(/[aA][cC][tT]/) ||
 			 doc.title.match(/[pP]\.[lL]\./)) // Match: ... Tex. Bus. & Com. Code § 26.01 ...
 	{
-		return "statute"
+		return "statute";
 	}
 	else if (doc.title.match(/\d+\s[a-zA-Z0-9\. ]+\s\d+/)) // Match: ... 5 U.S. 137 ... 
 	{
-		return "case"
+		return "case";
 	}
 	// TODO secondary sources
 }
@@ -167,30 +233,33 @@ function doWeb(doc, url) {
 
 	var casesOrStatutes = new Array();
 	var items = new Object();
-	var nextTitle;
+	var nextTitle; 
 
 	if (detectWeb(doc, url) == "multiple") {
 		// TODO check what type of element it is (currently only working for 'cases' searches)
-		var titles = doc.evaluate('(//a[@class="titleLink"])',
-								  doc, nsResolver, XPathResult.ANY_TYPE, null);
-		var dates = doc.evaluate('(//span[contains(@class,"metaDataItem")])',
-								 doc, nsResolver, XPathResult.ANY_TYPE, null)
+		var titles = ZU.xpath(doc, '//a[@class="titleLink"]', nsResolver);
+		var dates = ZU.xpath(doc, '(//span[contains(@class,"metaDataItem")])', nsResolver);
 		var nextDate;
-		dates.iterateNext(); // First court name
-		nextDate = dates.iterateNext(); // First date is [2]
-		dates.iterateNext(); // First citation
+    var dateOffset = 1;
+    
+    // dates[0] is first court name
+		nextDate = dates[dateOffset];
+    dateOffset += 3;
+		// dates[2] is first citation
 
-		while (nextTitle = titles.iterateNext()) {
+		for (var i = 0; i < titles.length; i++) {
+      nextTitle = titles[i];
 			items[nextTitle.href] = nextTitle.textContent + "(" + nextDate.textContent + ")";
 			
-			dates.iterateNext(); // Court name
-			nextDate = dates.iterateNext(); // Every 3 items is a date
-			dates.iterateNext(); // Citation
+			// dates[0] is court name
+			nextDate = dates[dateOffset];
+      dateOffset += 3;
+			// dates[2] is a citation
 		}
 
 		items = Zotero.selectItems(items);
-		for(var i in items) {
-			casesOrStatutes.push(i);
+		for(var e in items) {
+			casesOrStatutes.push(e);
 		}
 	}
 	else
