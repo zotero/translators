6dcd97b0a5e455b84ceab0b4a5cd9f52aa1064f7
Sebastian Karcher <karcher@u.northwestern.edu>
2023-04-20 14:11:44 -0400

    Update DV to work properly for datasets

--- Dataverse.js
+++ Dataverse.js
@@ -1,7 +1,7 @@
 {
 	"translatorID": "aedf3fb0-9a50-47b3-ba2f-3206552b82a9",
 	"label": "Dataverse",
-	"creator": "Abe Jellinek",
+	"creator": "Abe Jellinek, Sebastian Karcher",
 	"target": "^https?://(www\\.)?((open|research-|hei)?data|dvn|e?da[td]os|sodha\\.be|repositoriodedados|archive\\.data\\.jhu\\.edu|repositoriopesquisas|darus)",
 	"minVersion": "3.0",
 	"maxVersion": "",
@@ -9,13 +9,13 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-08-11 18:10:33"
+	"lastUpdated": "2023-04-20 18:10:33"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2021 Abe Jellinek
+	Copyright © 2023 Abe Jellinek & Sebastian Karcher
 	
 	This file is part of Zotero.
 
@@ -35,6 +35,9 @@
 	***** END LICENSE BLOCK *****
 */
 
+const datasetType = ZU.fieldIsValidForType('title', 'dataset')
+	? 'dataset'
+	: 'document';
 
 function detectWeb(doc, url) {
 	if (!doc.querySelector('#dataverseHeader')) {
@@ -42,7 +45,7 @@ function detectWeb(doc, url) {
 	}
 	
 	if (url.includes('/dataset.xhtml')) {
-		return "document";
+		return datasetType;
 	}
 	else if (getSearchResults(doc, true)) {
 		return "multiple";
@@ -77,6 +80,15 @@ function doWeb(doc, url) {
 }
 
 function scrape(doc, url) {
+	let jsonLD = text(doc, 'script[type="application/ld+json"]');
+	// Z.debug(jsonLD)
+	let schema = JSON.parse(jsonLD);
+	let license = schema.license;
+	// License can be stored as a string or an object (should be a string)
+	if (typeof (license) == "object") {
+		license = schema.license.text;
+	}
+	let version = schema.version;
 	var translator = Zotero.loadTranslator('web');
 	// Embedded Metadata
 	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
@@ -84,11 +96,15 @@ function scrape(doc, url) {
 	
 	translator.setHandler('itemDone', function (obj, item) {
 		item.libraryCatalog = text(doc, '#breadcrumbLnk0');
+		if (license) {
+			item.rights = ZU.cleanTags(license).trim();
+		}
+		if (version && version > 1) item.versionNumber = version;
 		item.complete();
 	});
 
 	translator.getTranslatorObject(function (trans) {
-		trans.itemType = 'document';
+		trans.itemType = datasetType;
 		trans.doWeb(doc, url);
 	});
 }
@@ -100,7 +116,7 @@ var testCases = [
 		"url": "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/3OUPKS",
 		"items": [
 			{
-				"itemType": "document",
+				"itemType": "dataset",
 				"title": "Survey seed production and seed contract",
 				"creators": [
 					{
@@ -110,11 +126,12 @@ var testCases = [
 					}
 				],
 				"date": "2021-08-04",
+				"DOI": "10.7910/DVN/3OUPKS",
 				"abstractNote": "The data is collected as part of a seed contract experiment conducted in Telengana state, India",
-				"extra": "Type: dataset\nDOI: 10.7910/DVN/3OUPKS",
 				"language": "en",
 				"libraryCatalog": "Harvard Dataverse",
-				"publisher": "Harvard Dataverse",
+				"repository": "Harvard Dataverse",
+				"rights": "CC0",
 				"url": "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/3OUPKS",
 				"attachments": [
 					{
@@ -140,9 +157,10 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "https://datos.uchile.cl/dataset.xhtml?persistentId=doi:10.34691/FK2/UMWJDU",
+		"detectedItemType": "dataset",
 		"items": [
 			{
-				"itemType": "document",
+				"itemType": "dataset",
 				"title": "Replicar los datos para: Genetic diversity in a restricted-dispersal kissing bug: The Center-Periphery Hypothesis halfway",
 				"creators": [
 					{
@@ -172,11 +190,13 @@ var testCases = [
 					}
 				],
 				"date": "2021-07-19",
+				"DOI": "10.34691/FK2/UMWJDU",
 				"abstractNote": "The center-periphery hypothesis (CPH) postulates that populations close to the center of a species’ distribution will exhibit higher genetic diversity and lower genetic differentiation than populations located at the edge of the distribution. The center of a species distribution might represent an optimum for the environmental factors influencing the species absolute fitness and therefore, genetic diversity. In species with wide distribution, the geographical variation of biotic and abiotic variables is crucial to understand the underlying mechanisms of the CPH. We evaluated the CPH and specifically tested which environmental variables better explained the patterns of genetic diversity in the kissing-bug Mepraia spinolai, one of the main wild vectors of Chagas disease in southern South America, distributing across three Mediterranean climatic ecoregions in Chile. We analyzed 2380 neutral Single Nucleotide Polymorphisms (SNP) to estimate genetic diversity. The mean winter temperature, the mean summer temperature, vegetation cover, population abundance, proportion of winged individuals and female abdomen area were measured for each kissing bug population to construct a model. Lower genetic diversity was detected in populations at the edge of the distribution compared to those in the center. However, genetic differentiation was not higher in the periphery. Genetic diversity was related to climatic and biological variables; there was a positive relationship with mean winter temperature and a negative association with mean summer temperature and body size. These results partially support the CPH and identify biotic (abdomen area) and abiotic (winter/summer temperatures) factors that would affect genetic diversity in this restricted-dispersal species of epidemiological relevance.",
-				"extra": "Type: dataset\nDOI: 10.34691/FK2/UMWJDU",
+				"extra": "Type: dataset",
 				"language": "en",
-				"libraryCatalog": "Universidad de Chile",
-				"publisher": "Universidad de Chile",
+				"libraryCatalog": "Repositorio de datos de investigación de la Universidad de Chile",
+				"repository": "Repositorio de datos de investigación de la Universidad de Chile",
+				"rights": "CC0",
 				"shortTitle": "Replicar los datos para",
 				"url": "https://datos.uchile.cl/dataset.xhtml?persistentId=doi:10.34691/FK2/UMWJDU",
 				"attachments": [
@@ -203,9 +223,10 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "https://www.sodha.be/dataset.xhtml?persistentId=doi:10.34934/DVN/HEABHJ",
+		"detectedItemType": "dataset",
 		"items": [
 			{
-				"itemType": "document",
+				"itemType": "dataset",
 				"title": "World Value Surveys 1981",
 				"creators": [
 					{
@@ -215,11 +236,13 @@ var testCases = [
 					}
 				],
 				"date": "2021-07-30",
+				"DOI": "10.34934/DVN/HEABHJ",
 				"abstractNote": "The series is designed to enable a crossnational comparison of values and norms on a wide variety of topics and to monitor changes in values and at...",
-				"extra": "Type: dataset\nDOI: 10.34934/DVN/HEABHJ",
+				"extra": "Type: dataset",
 				"language": "en",
 				"libraryCatalog": "Social Sciences and Digital Humanities Archive – SODHA",
-				"publisher": "Social Sciences and Digital Humanities Archive – SODHA",
+				"repository": "Social Sciences and Digital Humanities Archive – SODHA",
+				"rights": "This work is licensed under a Creative Commons Attribution 4.0 International License (CC-BY).",
 				"url": "https://www.sodha.be/dataset.xhtml?persistentId=doi:10.34934/DVN/HEABHJ",
 				"attachments": [
 					{
@@ -242,7 +265,7 @@ var testCases = [
 		"url": "https://data.worldagroforestry.org/dataset.xhtml?persistentId=doi:10.34725/DVN/NGGXCZ",
 		"items": [
 			{
-				"itemType": "document",
+				"itemType": "dataset",
 				"title": "Replication Data for: Agroforestry governance for operationalising the landscape approach: connecting conservation and farming actors",
 				"creators": [
 					{
@@ -307,11 +330,12 @@ var testCases = [
 					}
 				],
 				"date": "2021-08-05",
+				"DOI": "10.34725/DVN/NGGXCZ",
 				"abstractNote": "The expansion and intensifcation of agriculture as well as the associated land clearing are threatening both biodiversity and human wellbeing in tr...",
-				"extra": "Type: dataset\nDOI: 10.34725/DVN/NGGXCZ",
 				"language": "en",
 				"libraryCatalog": "World Agroforestry - Research Data Repository",
-				"publisher": "World Agroforestry - Research Data Repository",
+				"repository": "World Agroforestry - Research Data Repository",
+				"rights": "This dataset is made available under the Creative Commons Attribution 4.0 International (CC-BY-4.0). The license allows you, the user, to copy and redistribute the material in any medium or format and/or transform, and build upon the material\r\nfor any purpose, even commercially.",
 				"shortTitle": "Replication Data for",
 				"url": "https://data.worldagroforestry.org/dataset.xhtml?persistentId=doi:10.34725/DVN/NGGXCZ",
 				"attachments": [
@@ -326,6 +350,53 @@ var testCases = [
 					},
 					{
 						"tag": "Social Sciences"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://dataverse.no/dataset.xhtml?persistentId=doi:10.18710/GUX2O8",
+		"detectedItemType": "dataset",
+		"items": [
+			{
+				"itemType": "dataset",
+				"title": "Replication Data for: Spatial changes in gas transport and sediment stiffness influenced by regional stress: observations from piezometer data along the Vestnesa Ridge, eastern Fram Strait",
+				"creators": [
+					{
+						"firstName": "Andreia",
+						"lastName": "Plaza-Faverola",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Nabil",
+						"lastName": "Sultan",
+						"creatorType": "author"
+					}
+				],
+				"date": "2023-04-11",
+				"DOI": "10.18710/GUX2O8",
+				"abstractNote": "This database comprises piezometer pore-pressure and temperature data, analyses from Calypso sediment cores (grain sizes and logs) and results from...",
+				"extra": "Type: dataset",
+				"language": "en",
+				"libraryCatalog": "DataverseNO",
+				"repository": "DataverseNO",
+				"rights": "http://creativecommons.org/publicdomain/zero/1.0",
+				"shortTitle": "Replication Data for",
+				"url": "https://dataverse.no/dataset.xhtml?persistentId=doi:10.18710/GUX2O8",
+				"versionNumber": "2",
+				"attachments": [
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Earth and Environmental Sciences"
 					}
 				],
 				"notes": [],
