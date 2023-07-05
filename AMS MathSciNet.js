1ede28e6b28e760bf3e63cb37e9edfb595c8fa6a
Sebastian Karcher <karcher@u.northwestern.edu>
2023-07-05 08:51:43 -0400

    Start fixing MathSciNet

--- AMS MathSciNet.js
+++ AMS MathSciNet.js
@@ -2,44 +2,40 @@
 	"translatorID": "a354331-981b-43de-a61-bc26dd1be3a9",
 	"label": "AMS MathSciNet",
 	"creator": "Simon Kornblith",
-	"target": "^https?://(mathscinet\\.)?ams\\.[^/]*/mathscinet(\\-getitem\\?|/search/(publications\\.html|publdoc\\.html))",
+	"target": "^https?://(mathscinet.)?ams.[^/]*/mathscinet(/article\\?|/publications-search\\?)",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2017-10-20 16:08:00"
+	"lastUpdated": "2023-07-05 03:32:36"
 }
 
 function detectWeb(doc, url) {
-	
 	var itemType;
 	if (getSearchResults(doc, true)) {
 		return "multiple"
-	} else if (itemType = ZU.xpathText(doc, '//div[@class="headlineMenu"]/*[last()-1]')) {
-		switch (itemType.trim().toLowerCase()) {
-			case 'article':
-				return "journalArticle";
-			case 'book':
-				return "book";
-			case 'chapter':
+	} else if (url.includes("article?mr=")) {
+		if (doc.querySelector('div[data-testid="ap-book-isbn"]')) {
+			if (doc.querySelector('div[data-testid="ap-book-collection"]'))
 				return "bookSection";
-		}	
+			else return "book";
+		}
+		else return "journalArticle";
+			
 	}
+	else return false;
 }
-
 
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	//TODO: adjust the xpath
-	var rows = ZU.xpath(doc, '//div[@class="headlineText"]');
-	for (var i=0; i<rows.length; i++) {
-		//TODO: check and maybe adjust
-		var href = ZU.xpathText(rows[i], './a[@class="mrnum"]/@href')
-		//TODO: check and maybe adjust
-		var title = ZU.xpathText(rows[i], './span[@class="title"]')
+	// TODO: adjust the CSS selector
+	var rows = doc.querySelectorAll('.results div.font-weight-bold');
+	for (let row of rows) {
+		let href = attr(row, 'a', 'href');
+		let title = ZU.trimInternal(row.textContent);
 		if (!href || !title) continue;
 		if (checkOnly) return true;
 		found = true;
@@ -48,64 +44,51 @@ function getSearchResults(doc, checkOnly) {
 	return found ? items : false;
 }
 
-
-function doWeb(doc, url) {
-	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
-			if (!items) {
-				return true;
-			}
-			var articles = [];
-			for (var i in items) {
-				articles.push(i);
-			}
-			ZU.processDocuments(articles, scrape);
-		});
-	} else {
-		scrape(doc, url);
+async function doWeb(doc, url) {
+	if (detectWeb(doc, url) == 'multiple') {
+		let items = await Zotero.selectItems(getSearchResults(doc, false));
+		if (!items) return;
+		for (let url of Object.keys(items)) {
+			await scrape(await requestDocument(url));
+		}
+	}
+	else {
+		await scrape(doc, url);
 	}
 }
 
-function scrape(doc, url) {
-	var host = url.match(/^(.+)\/mathscinet/)
-	var pub = host[0] + "/search/publications.html?fmt=bibtex";
-	var MR = ZU.xpathText(doc, '//div[@id="content"]/div[@class="doc"]/div[@class="headline"]/strong[1]');
-	pub += "&pg1=MR&s1="+MR.replace(/^MR0*/, "");
-
-	ZU.doGet(pub, function(text) {
-		var preRE = /<pre>\s*([\s\S]*?)\s*<\/pre>/g;
-		var bibTeXString = "";
-		
-		var m;
-		while (m = preRE.exec(text)) {
-			bibTeXString += m[1] + '\n';
-		}
-		
-		// import using BibTeX
-		var translator = Zotero.loadTranslator("import");
-		translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
-		translator.setString(bibTeXString);
-		translator.setHandler("itemDone", function(obj, item) {
-			// Fix/fetch MR number
-			var mrnumber;
-			if (item.extra) {
-				item.extra = item.extra.replace(/^MR:\s*(?:MR)?(\d+).*/gm,
-					function(m, mr) {
-						mrnumber = mr;
-						return 'MR: ' + mr;
-					});
-			}
-			
-			if (mrnumber) {
-				url = 'https://mathscinet.ams.org/mathscinet-getitem?mr=' + mrnumber;
-			}
-			item.attachments.push({title: "MathSciNet Snapshot", document: doc});
-			item.url = url;
+async function scrape(doc, url = doc.location.href) {
+	Z.debug(url);
+	let id = url.match(/\?mr=(\d+)/);
+	if (!id) {
+		throw("No MR ID, can't proceed")
+	}
+	let bibJSONUrl = '/mathscinet/api/publications/format?formats=bib&ids=' + id[1];
+	Z.debug(bibJSONUrl)
+	let bibJSON = await requestText(bibJSONUrl);
+	Z.debug(bibJSON)
+	// the JSON parser doesn't like newlines or backslashes in the bibtex
+	bibJSON = bibJSON.replace(/\\n/g , "").replace(/\\/g, "");
+	bibJSON = JSON.parse(bibJSON);
+	let bibTex = bibJSON[0].bib;
+	Z.debug(bibTex)
+	let translator = Zotero.loadTranslator("import");
+		translator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4');
+		translator.setString(bibTex);
+		translator.setHandler('itemDone', (_obj, item) => {
+			item.attachments.push({
+				title: 'Snapshot',
+				document: doc
+			});
 			item.complete();
 		});
-		translator.translate();
-	});
+		await translator.translate();
 }
+
+
+
+
+
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
@@ -206,7 +189,7 @@ var testCases = [
 				"seeAlso": []
 			}
 		]
-	},	
+	},
 	{
 		"type": "web",
 		"url": "https://mathscinet.ams.org/mathscinet-getitem?mr=2663710",
