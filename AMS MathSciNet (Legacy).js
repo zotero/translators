e7dddcf0fd02c903112b23d626dcc2597c031c47
Sebastian Karcher <karcher@u.northwestern.edu>
2023-07-06 13:28:36 -0400

    Lint & update legacy code

--- AMS MathSciNet (Legacy).js
+++ AMS MathSciNet (Legacy).js
@@ -1,7 +1,7 @@
 {
 	"translatorID": "a354331-981b-43de-a61-bc26dd1be3a9",
 	"label": "AMS MathSciNet (Legacy)",
-	"creator": "Simon Kornblith",
+	"creator": "Simon Kornblith, Sebastian Karcher",
 	"target": "^https?://(mathscinet\\.)?ams\\.[^/]*/(mathscinet/2006/)?mathscinet(\\-getitem\\?|/search/(publications\\.html|publdoc\\.html))",
 	"minVersion": "3.0",
 	"maxVersion": "",
@@ -13,12 +13,36 @@
 }
 
 
-function detectWeb(doc, url) {
-	
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2015-2023 Simon Kornblith & Sebastian Karcher
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
+function detectWeb(doc, _url) {
 	var itemType;
 	if (getSearchResults(doc, true)) {
-		return "multiple"
-	} else if (itemType = ZU.xpathText(doc, '//div[@class="headlineMenu"]/*[last()-1]')) {
+		return "multiple";
+	}
+	else if (ZU.xpathText(doc, '//div[@class="headlineMenu"]/*[last()-1]')) {
+		itemType = ZU.xpathText(doc, '//div[@class="headlineMenu"]/*[last()-1]');
 		switch (itemType.trim().toLowerCase()) {
 			case 'article':
 				return "journalArticle";
@@ -26,21 +50,19 @@ function detectWeb(doc, url) {
 				return "book";
 			case 'chapter':
 				return "bookSection";
-		}	
+		}
 	}
+	return false;
 }
 
 
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	//TODO: adjust the xpath
 	var rows = ZU.xpath(doc, '//div[@class="headlineText"]');
-	for (var i=0; i<rows.length; i++) {
-		//TODO: check and maybe adjust
-		var href = ZU.xpathText(rows[i], './a[@class="mrnum"]/@href')
-		//TODO: check and maybe adjust
-		var title = ZU.xpathText(rows[i], './span[@class="title"]')
+	for (var i = 0; i < rows.length; i++) {
+		var href = ZU.xpathText(rows[i], './a[@class="mrnum"]/@href');
+		var title = ZU.xpathText(rows[i], './span[@class="title"]');
 		if (!href || !title) continue;
 		if (checkOnly) return true;
 		found = true;
@@ -50,63 +72,62 @@ function getSearchResults(doc, checkOnly) {
 }
 
 
-function doWeb(doc, url) {
-	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
-			if (!items) {
-				return true;
-			}
-			var articles = [];
-			for (var i in items) {
-				articles.push(i);
-			}
-			ZU.processDocuments(articles, scrape);
-		});
-	} else {
-		scrape(doc, url);
+async function doWeb(doc, url) {
+	if (detectWeb(doc, url) == 'multiple') {
+		let items = await Zotero.selectItems(getSearchResults(doc, false));
+		if (!items) return;
+		for (let url of Object.keys(items)) {
+			await scrape(await requestDocument(url));
+		}
+	}
+	else {
+		await scrape(doc, url);
 	}
 }
 
-function scrape(doc, url) {
-	var host = url.match(/^(.+)\/mathscinet/)
+
+async function scrape(doc, url = doc.location.href) {
+	var host = url.match(/^(.+)\/mathscinet/);
 	var pub = host[0] + "/search/publications.html?fmt=bibtex";
 	var MR = ZU.xpathText(doc, '//div[@id="content"]/div[@class="doc"]/div[@class="headline"]/strong[1]');
-	pub += "&pg1=MR&s1="+MR.replace(/^MR0*/, "");
+	pub += "&pg1=MR&s1=" + MR.replace(/^MR0*/, "");
 
-	ZU.doGet(pub, function(text) {
-		var preRE = /<pre>\s*([\s\S]*?)\s*<\/pre>/g;
-		var bibTeXString = "";
+	let text = await requestText(pub);
+	var preRE = /<pre>\s*([\s\S]*?)\s*<\/pre>/g;
+	var bibTeXString = "";
 		
-		var m;
-		while (m = preRE.exec(text)) {
-			bibTeXString += m[1] + '\n';
+	var m;
+	//eslint-disable-next-line no-cond-assign
+	while (m = preRE.exec(text)) {
+		bibTeXString += m[1] + '\n';
+	}
+		
+	// import using BibTeX
+	let translator = Zotero.loadTranslator("import");
+	translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
+	translator.setString(bibTeXString);
+	translator.setHandler("itemDone", function (obj, item) {
+		// Fix/fetch MR number
+		var mrnumber;
+		if (item.extra) {
+			item.extra = item.extra.replace(/^MR:\s*(?:MR)?(\d+).*/gm,
+				function (m, mr) {
+					mrnumber = mr;
+					return 'MR: ' + mr;
+				});
 		}
-		
-		// import using BibTeX
-		var translator = Zotero.loadTranslator("import");
-		translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
-		translator.setString(bibTeXString);
-		translator.setHandler("itemDone", function(obj, item) {
-			// Fix/fetch MR number
-			var mrnumber;
-			if (item.extra) {
-				item.extra = item.extra.replace(/^MR:\s*(?:MR)?(\d+).*/gm,
-					function(m, mr) {
-						mrnumber = mr;
-						return 'MR: ' + mr;
-					});
-			}
 			
-			if (mrnumber) {
-				url = 'https://mathscinet.ams.org/mathscinet-getitem?mr=' + mrnumber;
-			}
-			item.attachments.push({title: "MathSciNet Snapshot", document: doc});
-			item.url = url;
-			item.complete();
-		});
-		translator.translate();
+		if (mrnumber) {
+			url = 'https://mathscinet.ams.org/mathscinet-getitem?mr=' + mrnumber;
+		}
+		item.attachments.push({ title: "MathSciNet Snapshot", document: doc });
+		item.url = url;
+		item.complete();
 	});
-}/** BEGIN TEST CASES **/
+	await translator.translate();
+}
+
+/** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
