2aab8977c1643e1272ed1111e247b2586e116633
zoe-translates <116055375+zoe-translates@users.noreply.github.com>
2023-09-26 18:20:42 +0800

    IEEE Xplore: Don't use export format; save full text PDF
    The "endpoint" for export formats (BibTeX, RIS, etc.) may be unreliable
    unless the referrer is sent, but this is not yet feasible for
    Chromium-family browsers in Connector.
    
    (See: https://forums.zotero.org/discussion/108011/ieee-xplore-falied)
    
    This is changed to a scraper that looks at just the page content,
    therefore avoiding the endpoint call.
    
    The pages are rendered on the client side, so we read the data source as
    JSON directly, without scraping the DOM.
    
    In addition, the following changes are made --
    
    - The full-text PDF file is now saved as an attachment (if the user has
      access to it).
    - The translator now also works on IEEE Xplore's own full-text PDF
      viewer web app. This is done via an additional network request to the
      abstract page where the JSON metadata are hosted. A test case for this
      is added.
    - Tags (keywords) are deduplicated.
    - Only the electronic-media ISBN and ISSN are kept.
    - Other minor bugfixes.

--- IEEE Xplore.js
+++ IEEE Xplore.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-09-24 03:17:24"
+	"lastUpdated": "2023-09-26 10:17:17"
 }
 
 /*
@@ -35,13 +35,18 @@
 	***** END LICENSE BLOCK *****
 */
 
+const BASE_URL = "https://ieeexplore.ieee.org";
+
 function detectWeb(doc, url) {
-	Zotero.monitorDOMChanges(doc.querySelector('.global-content-wrapper'));
+	let globalWrapper = doc.querySelector('.global-content-wrapper');
+	if (globalWrapper) {
+		Zotero.monitorDOMChanges(globalWrapper);
+	}
 	if (doc.defaultView !== null && doc.defaultView !== doc.defaultView.top) return false;
 	
-	if (/[?&]arnumber=(\d+)/i.test(url) || /\/document\/\d+/i.test(url)) {
-		var firstBreadcrumb = ZU.xpathText(doc, '(//div[contains(@class, "breadcrumbs")]//a)[1]');
-		if (firstBreadcrumb == "Conferences") {
+	if (getArticleID(url) !== null) {
+		var firstBreadcrumb = text(doc, ".breadcrumbs > span:first-of-type > a");
+		if (firstBreadcrumb.toLowerCase().includes("conference")) {
 			return "conferencePaper";
 		}
 		return "journalArticle";
@@ -89,28 +94,10 @@ function getSearchResults(doc, checkOnly) {
 		if (!href || !title) continue;
 		if (checkOnly) return true;
 		found = true;
-		items[fixUrl(href)] = title;
+		items[href] = title;
 	}
 	return found ? items : false;
 }
-
-// Some pages don't show the metadata we need (http://forums.zotero.org/discussion/16283)
-// No data: http://ieeexplore.ieee.org/search/srchabstract.jsp?tp=&arnumber=1397982
-// No data: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=1397982
-// Data: http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=1397982
-// Also address issue of saving from PDF itself, I hope
-// URL like http://ieeexplore.ieee.org/ielx4/78/2655/00080767.pdf?tp=&arnumber=80767&isnumber=2655
-// Or: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=1575188&tag=1
-function fixUrl(url) {
-	var arnumber = url.match(/arnumber=(\d+)/);
-	if (arnumber) {
-		return url.replace(/\/(?:search|stamp|ielx[45])\/.*$/, "/xpls/abs_all.jsp?arnumber=" + arnumber[1]);
-	}
-	else {
-		return url;
-	}
-}
-
 
 async function doWeb(doc, url) {
 	if (detectWeb(doc, url) == 'multiple') {
@@ -120,128 +107,151 @@ async function doWeb(doc, url) {
 			await scrape(await requestDocument(url));
 		}
 	}
-	else if (url.includes("/search/") || url.includes("/stamp/") || url.includes("/ielx4/") || url.includes("/ielx5/")) {
-		await scrape(await requestDocument([fixUrl(url)]));
-	}
 	else {
 		await scrape(doc, url);
 	}
 }
 
-
 async function scrape(doc, url = doc.location.href) {
-	var arnumber = (url.match(/arnumber=(\d+)/) || url.match(/\/document\/(\d+)/))[1];
-	var pdf = "/stamp/stamp.jsp?tp=&arnumber=" + arnumber;
-	// Z.debug("arNumber = " + arnumber);
-	
-	var script = ZU.xpathText(doc, '//script[@type="text/javascript" and contains(., "global.document.metadata")]');
-	if (script) {
-		var dataRaw = script.split("global.document.metadata")[1]
-.replace(/^=/, '').replace(/};[\s\S]*$/m, '}');
-		try {
-			var data = JSON.parse(dataRaw);
-		}
-		catch (e) {
-			Z.debug("Error parsing JSON data:");
-			Z.debug(e);
-		}
-	}
-	
-	
-	let bibtexURL = "/rest/search/citation/format?recordIds=" + arnumber + "&fromPage=&citations-format=citation-abstract&download-format=download-bibtex";
-	Z.debug(bibtexURL);
-	// metadata is downloaded in a JSON data field
-	let bibtex = await requestJSON(bibtexURL, { headers: { Referer: url} });
-	bibtex = bibtex.data;
-	bibtex = ZU.unescapeHTML(bibtex.replace(/(&[^\s;]+) and/g, '$1;'));
-	// remove empty tag - we can take this out once empty tags are ignored
-	bibtex = bibtex.replace(/(keywords=\{.+);\}/, "$1}");
-	var earlyaccess = false;
-	if (/^@null/.test(bibtex)) {
-		earlyaccess = true;
-		bibtex = text.replace(/^@null/, "@article");
-	}
-	var translator = Zotero.loadTranslator("import");
-	// Calling the BibTeX translator
-	translator.setTranslator("9cb70025-a888-4a29-a210-93ec52da40d4");
-	translator.setString(bibtex);
-	translator.setHandler("itemDone", function (obj, item) {
-		item.notes = [];
-		var res;
-		// Rearrange titles, per http://forums.zotero.org/discussion/8056
-		// If something has a comma or a period, and the text after comma ends with
-		// "of", "IEEE", or the like, then we switch the parts. Prefer periods.
-		if (item.publicationTitle.includes(".")) {
-			res = item.publicationTitle.trim().match(/^(.*)\.(.*(?:of|on|IEE|IEEE|IET|IRE))$/);
-		}
-		else {
-			res = item.publicationTitle.trim().match(/^(.*),(.*(?:of|on|IEE|IEEE|IET|IRE))$/);
-		}
-		if (res) {
-			item.publicationTitle = res[2] + " " + res[1];
-		}
-		item.proceedingsTitle = item.conferenceName = item.publicationTitle;
-		if (earlyaccess) {
-			item.volume = "Early Access Online";
-			item.issue = "";
-			item.pages = "";
-		}
-			
-		if (data && data.authors && data.authors.length == item.creators.length) {
-			item.creators = [];
-			for (let author of data.authors) {
-				item.creators.push({
-					firstName: author.firstName,
-					lastName: author.lastName,
-					creatorType: "author"
-				});
-			}
-		}
-			
-		if (!item.ISSN && data && data.issn) {
-			item.ISSN = data.issn.map(el => el.value).join(", ");
-		}
-		if (item.ISSN && !ZU.fieldIsValidForType('ISSN', item.itemType)) {
-			item.extra = "ISSN: " + item.ISSN;
-		}
-		item.url = url;
-		item.attachments.push({
-			document: doc,
-			title: "IEEE Xplore Abstract Record"
-		});
-			
-		if (pdf) {
-			ZU.doGet(pdf, function (src) {
-				// Either the PDF is embedded in the page, or (e.g. for iOS)
-				// the page has a redirect to the full-page PDF
-				//
-				// As of 3/2020, embedded PDFs via a web-based proxy are
-				// being served as getPDF.jsp, so support that in addition
-				// to direct .pdf URLs.
-				var m = /<i?frame src="([^"]+\.pdf\b[^"]*|[^"]+\/getPDF\.jsp\b[^"]*)"|<meta HTTP-EQUIV="REFRESH" content="0; url=([^\s"]+\.pdf\b[^\s"]*)"/.exec(src);
-				var pdfUrl = m && (m[1] || m[2]);
-				if (pdfUrl) {
-					item.attachments.unshift({
-						url: pdfUrl,
-						title: "IEEE Xplore Full Text PDF",
-						mimeType: "application/pdf"
-					});
-				}
-				item.complete();
-			}, null);
-		}
-		else {
-			item.complete();
-		}
-	});
+	let articleID = getArticleID(url);
+	let canonicalURL = `${BASE_URL}/document/${articleID}`;
 
-	translator.getTranslatorObject(function (trans) {
-		trans.setKeywordSplitOnSpace(false);
-		trans.setKeywordDelimRe('\\s*;\\s*', '');
-		trans.doImport();
-	});
+	if (/^https:\/\/[^/]+\/stamp\//.test(url)) { // pdf viewer
+		Z.debug(`Input is PDF-view page; article ID ${articleID}`);
+		// get the abstract page
+		doc = await requestDocument(canonicalURL);
+	}
+
+	let metadata = extractJSON(doc);
+	if (!metadata) {
+		throw new Error(`No metadata extract for page at ${url}; article ID ${articleID}`);
+	}
+
+	let type;
+	switch (metadata.contentType) {
+		case "periodicals":
+			type = "journalArticle";
+			break;
+		case "conferences":
+			type = "conferencePaper";
+			break;
+		default:
+			throw new Error(`Unimplemented content type; source: ${metadata.contentType}`);
+	}
+	let item = new Z.Item(type);
+	item.url = canonicalURL;
+
+	// Set doc metadata field as item field
+	function setField(metadataField, itemField = metadataField) {
+		let value = metadata[metadataField];
+		if (value) {
+			item[itemField] = value;
+		}
+	}
+
+	// metadata field -> item field
+	setField("title", "formulaStrippedArticleTitle");
+	if (!item.title) {
+		setField("title");
+	}
+	setField("publicationDate", "date");
+	setField("doi", "DOI");
+	setField("publicationTitle");
+	setField("publisher");
+	setField("volume");
+	setField("issue");
+
+	// abstract
+	if (metadata.abstract) {
+		item.abstractNote = ZU.trimInternal(ZU.unescapeHTML(ZU.cleanTags(metadata.abstract)));
+	}
+
+	// creators
+	for (let authorObj of metadata.authors || []) {
+		item.creators.push({
+			firstName: authorObj.firstName,
+			lastName: authorObj.lastName,
+			creatorType: "author",
+		});
+	}
+
+	// pages
+	let pages = metadata.startPage;
+	if (typeof pages === "string") {
+		if (metadata.endPage) {
+			pages += `-${metadata.endPage}`;
+		}
+		item.pages = pages;
+	}
+
+	// ISSN and ISBN if any, keeping only the ones for electronic media
+	setSN(item, metadata, "ISSN");
+	setSN(item, metadata, "ISBN");
+
+	item.tags.push(...getTags(metadata));
+
+	if (type === "conferencePaper") {
+		setField("confLoc", "place");
+		setField("displayPublicationTitle", "conferenceName");
+		let confDates = metadata.displayPublicationDate;
+		if (confDates) {
+			let extraText = `Conference Dates: ${confDates}`;
+			item.extra = item.extra ? item.extra + `\n${extraText}` : extraText;
+		}
+	}
+
+	item.attachments.push(getFullTextPDF(articleID));
+
+	item.complete();
 }
 
+function getArticleID(url) {
+	let urlObj = new URL(url);
+	let m = urlObj.pathname.match(/\/document\/(\d+)($|\/)/);
+	if (m) {
+		return m[1];
+	}
+	return urlObj.searchParams.get("arnumber");
+}
+
+// Extract form the embedded code the JS object initializer that is the source
+// data for the page rendered in client JS, using regex for simplicity.
+function extractJSON(doc) {
+	for (let elem of doc.querySelectorAll("script[type='text/javascript']")) {
+		if (elem.getAttribute("src")) continue;
+		let m = elem.textContent.match(/;\s*xplGlobal\.document\.metadata\s*=\s*(\{.+?\});\s*$/sm);
+		if (m) return JSON.parse(m[1]);
+	}
+	return null;
+}
+
+function getTags(metadata) {
+	let keywords = new Map();
+	for (let keywordsObj of metadata.keywords || []) {
+		for (let word of keywordsObj.kwd) {
+			let key = word.toLowerCase();
+			if (!keywords.has(key)) keywords.set(key, word);
+		}
+	}
+	return keywords.values();
+}
+
+function setSN(item, metadata, snType) {
+	for (let obj of metadata[snType.toLowerCase()] || []) {
+		if (obj.format === `Electronic ${snType}`) {
+			item[snType] = obj.value;
+			return;
+		}
+	}
+}
+
+function getFullTextPDF(articleID) {
+	// "ref" is the base64-encoded canonical URL of article without the
+	// trailing slash. The encoded URL is always ASCII so btoa() suffices.
+	let ref = btoa(`${BASE_URL}/document/${articleID}`);
+	let url = `${BASE_URL}/stampPDF/getPDF.jsp?tp=&arnumber=${articleID}&ref=${ref}`;
+	return { title: "Full Text PDF", url, mimeType: "application/pdf" };
+}
 
 /** BEGIN TEST CASES **/
 var testCases = [
@@ -1118,6 +1128,133 @@ var testCases = [
 		"url": "https://ieeexplore.ieee.org/xpl/tocresult.jsp?isnumber=10045573&punumber=6221021",
 		"defer": true,
 		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7265050",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Disturbance-Observer-Based Control and Related Methods—An Overview",
+				"creators": [
+					{
+						"firstName": "Wen-Hua",
+						"lastName": "Chen",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Jun",
+						"lastName": "Yang",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Lei",
+						"lastName": "Guo",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Shihua",
+						"lastName": "Li",
+						"creatorType": "author"
+					}
+				],
+				"date": "February 2016",
+				"DOI": "10.1109/TIE.2015.2478397",
+				"ISSN": "1557-9948",
+				"abstractNote": "Disturbance-observer-based control (DOBC) and related methods have been researched and applied in various industrial sectors in the last four decades. This survey, at first time, gives a systematic and comprehensive tutorial and summary on the existing disturbance/uncertainty estimation and attenuation techniques, most notably, DOBC, active disturbance rejection control, disturbance accommodation control, and composite hierarchical antidisturbance control. In all of these methods, disturbance and uncertainty are, in general, lumped together, and an observation mechanism is employed to estimate the total disturbance. This paper first reviews a number of widely used linear and nonlinear disturbance/uncertainty estimation techniques and then discusses and compares various compensation techniques and the procedures of integrating disturbance/uncertainty compensation with a (predesigned) linear/nonlinear controller. It also provides concise tutorials of the main methods in this area with clear descriptions of their features. The application of this group of methods in various industrial sections is reviewed, with emphasis on the commercialization of some algorithms. The survey is ended with the discussion of future directions.",
+				"issue": "2",
+				"libraryCatalog": "IEEE Xplore",
+				"pages": "1083-1095",
+				"publicationTitle": "IEEE Transactions on Industrial Electronics",
+				"url": "https://ieeexplore.ieee.org/document/7265050",
+				"volume": "63",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Attenuation"
+					},
+					{
+						"tag": "Disturbances"
+					},
+					{
+						"tag": "Frequency estimation"
+					},
+					{
+						"tag": "Nonlinear systems"
+					},
+					{
+						"tag": "Observers"
+					},
+					{
+						"tag": "Robustness"
+					},
+					{
+						"tag": "Uncertainty"
+					},
+					{
+						"tag": "active disturbance rejection control"
+					},
+					{
+						"tag": "attenuation techniques"
+					},
+					{
+						"tag": "compensation"
+					},
+					{
+						"tag": "compensation techniques"
+					},
+					{
+						"tag": "composite hierarchical antidisturbance control"
+					},
+					{
+						"tag": "disturbance accommodation control"
+					},
+					{
+						"tag": "disturbance-observer-based control"
+					},
+					{
+						"tag": "disturbance-uncertainty compensation"
+					},
+					{
+						"tag": "estimation"
+					},
+					{
+						"tag": "industrial sections"
+					},
+					{
+						"tag": "linear systems"
+					},
+					{
+						"tag": "linear-nonlinear controller"
+					},
+					{
+						"tag": "motion control"
+					},
+					{
+						"tag": "nonlinear control systems"
+					},
+					{
+						"tag": "nonlinear disturbance-uncertainty estimation"
+					},
+					{
+						"tag": "power system control"
+					},
+					{
+						"tag": "power system faults"
+					},
+					{
+						"tag": "uncertainties"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
 	}
 ]
 /** END TEST CASES **/
