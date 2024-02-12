9a0a729d63539b4420b745f0b26c83d00038c91a
Thaddeus Hetling <158094684+EruditeLying@users.noreply.github.com>
2024-02-12 23:05:59 +0100

    Implement code review suggestions
    As suggested by @AbeJellinek:
    - use relative URLs in search results
    - accept meta tags from anywhere in the page
    - don't set website type
    - use ZU.cleanAuthor to build the creator
    - use Array.from instead of Array.prototype.slice.call
    
    Additionally, call ZU.trimInternal on the abstract, to normalize whitespace
    
    Update test cases accordingly

--- Squarespace.js
+++ Squarespace.js
@@ -51,16 +51,8 @@ function getSearchResults(doc, checkOnly) {
 	var found = false;
 	var rows = doc.querySelectorAll('.sqs-search-container-item');
 	for (let row of rows) {
-		let href = null;
-		try {
-			href = new URL(row.parentElement.attributes['data-url'].value, document.baseURI).href;
-		}
-		catch {
-			continue;
-		}
-
+		let href = row.parentElement.attributes['data-url']?.value;
 		let title = text(row, '.sqs-title');
-
 		if (!href || !title) continue;
 		if (checkOnly) return true;
 		found = true;
@@ -83,7 +75,7 @@ async function doWeb(doc, url) {
 }
 
 function getMetadataItem(doc, prop) {
-	return attr(doc, 'html>head>meta[itemprop="' + prop + '"]', 'content');
+	return attr(doc, 'meta[itemprop="' + prop + '"]', 'content');
 }
 
 async function scrape(doc, url = doc.location.href) {
@@ -93,18 +85,11 @@ async function scrape(doc, url = doc.location.href) {
 	translator.setDocument(doc);
 	
 	translator.setHandler('itemDone', (_obj, item) => {
-		item.websiteType = 'Squarespace blog';
 		item.date = getMetadataItem(doc, 'datePublished');
 		let author = getMetadataItem(doc, 'author');
-		if (author) {
-			item.creators.push({
-				lastName: author,
-				fieldMode: 1,
-				creatorType: 'author'
-			});
-		}
-		if (item.abstractNote) item.abstractNote = ZU.unescapeHTML(item.abstractNote);
-		item.tags = Array.prototype.slice.call(doc.querySelectorAll('.blog-item-tag-wrapper a')).map((tag) => ZU.trim(tag.textContent));
+		if (author) item.creators.push(ZU.cleanAuthor(author, 'author'));
+		if (item.abstractNote) item.abstractNote = ZU.trimInternal(ZU.unescapeHTML(item.abstractNote));
+		item.tags = Array.from(doc.querySelectorAll('.blog-item-tag-wrapper a')).map((tag) => ZU.trim(tag.textContent));
 		item.complete();
 	});
 
@@ -123,8 +108,8 @@ var testCases = [
 				"title": "My Favourite Squarespace CSS Hacks",
 				"creators": [
 					{
-						"lastName": "Karl Patton",
-						"fieldMode": 1,
+						"firstName": "Karl",
+						"lastName": "Patton",
 						"creatorType": "author"
 					}
 				],
@@ -133,7 +118,6 @@ var testCases = [
 				"blogTitle": "Minimist Website Design | Squarespace Expert & Website Designer",
 				"language": "en-CA",
 				"url": "https://www.minimist.ca/articles/my-favourite-squarespace-css-hacks",
-				"websiteType": "Squarespace blog",
 				"attachments": [
 					{
 						"title": "Snapshot",
@@ -155,17 +139,16 @@ var testCases = [
 				"title": "How to reduce spacer block height with CSS in Squarespace (7.0 & 7.1)",
 				"creators": [
 					{
-						"lastName": "The Styled Square",
-						"fieldMode": 1,
+						"firstName": "The Styled",
+						"lastName": "Square",
 						"creatorType": "author"
 					}
 				],
 				"date": "2021-12-10T11:12:50-0800",
-				"abstractNote": "The Spacer Block element is an essential tool for your Squarespace website design, but by default, the Spacer Block has a set height that can be too large for some instances.   In this tutorial, we’re sharing copy & paste CSS to adjust the Spacer Block minimum height for more flexibility whi",
+				"abstractNote": "The Spacer Block element is an essential tool for your Squarespace website design, but by default, the Spacer Block has a set height that can be too large for some instances. In this tutorial, we’re sharing copy & paste CSS to adjust the Spacer Block minimum height for more flexibility whi",
 				"blogTitle": "The Styled Square | Premium Squarespace Website Templates",
 				"language": "en-US",
 				"url": "https://www.thestyledsquare.com/blog-content/how-to-reduce-spacer-block-height-with-css-in-squarespace",
-				"websiteType": "Squarespace blog",
 				"attachments": [
 					{
 						"title": "Snapshot",
@@ -192,8 +175,8 @@ var testCases = [
 				"title": "What are tags on Squarespace and how to add categories",
 				"creators": [
 					{
-						"lastName": "Roshni Kahol",
-						"fieldMode": 1,
+						"firstName": "Roshni",
+						"lastName": "Kahol",
 						"creatorType": "author"
 					}
 				],
@@ -202,7 +185,6 @@ var testCases = [
 				"blogTitle": "Original Box",
 				"language": "en-GB",
 				"url": "https://originalbox.co/blog/tags-squarespace-add-categories",
-				"websiteType": "Squarespace blog",
 				"attachments": [
 					{
 						"title": "Snapshot",
