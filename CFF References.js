ae6fc80028c0465b00cfaa9db124e16546a364ca
Abe Jellinek <jellinek@berkeley.edu>
2024-05-17 15:56:07 -0400

    Rename to match label, fixes
    - Fix writeArray so it works for more than tags
    - Fix regex in writeArray
    - Remove extra newlines at end of some written values
    - Remove link to unrelated localization file
    - "collection_title" maps to proceedingsTitle
    - Fix repository self-assignment
    - Map correspondence authors to "senders" and all primary creator types
      (not just author) to "authors"
    - Fix DOI regex for multiline extra
    - Remove extra special case for authors and keywords
    - Try to avoid outputting invalid YAML for multiline abstracts
    - Fix some lint errors

--- CFF References.js
+++ CFF References.js
@@ -8,7 +8,7 @@
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 2,
-	"lastUpdated": "2024-03-15 12:21:10"
+	"lastUpdated": "2024-05-17 20:02:13"
 }
 
 /*
@@ -37,18 +37,19 @@
 // set a global for spacing purposes under the references key of a citation.cff file
 var referencesSpacing = '    ';
 
-function writeArray(tags) {
-	if (!tags.length) return false;
-	let keywords = "\n";
-	for (let tag of tags) {
-		keywords += referencesSpacing + "  - " + tag.tag + "\n";
+function writeArray(array) {
+	if (!array.length) return false;
+	let output = "\n";
+	for (let elem of array) {
+		if (!elem) continue;
+		output += referencesSpacing + "  - " + elem + "\n";
 	}
-	return keywords.replace(/\\n$/, "");
+	return output.replace(/\n$/, "");
 }
 
 function writeDOI(itemDOI) {
 	if (!itemDOI) return false;
-	let doi = "\n" + referencesSpacing + "  - type: doi\n" + referencesSpacing + "    value: " + itemDOI + "\n";
+	let doi = "\n" + referencesSpacing + "  - type: doi\n" + referencesSpacing + "    value: " + itemDOI;
 	return doi;
 }
 
@@ -70,7 +71,7 @@ function writeCreators(itemCreators, creatorType="") {
 			authors += referencesSpacing + "    given-names: " + author.firstName + "\n";
 		}
 	}
-	return authors;
+	return authors.replace(/\n$/, "");
 }
 
 function doExport() {
@@ -79,8 +80,7 @@ function doExport() {
 	Zotero.write('# This CITATION.cff reference content was generated from Zotero.\n');
 	Zotero.write('references:\n');
 
-	// eslint-disable-next-line no-cond-assign
-	while (item = Zotero.nextItem()) {
+	while ((item = Zotero.nextItem())) {
 		var cff = {};
 		cff.title = ">-\n" + referencesSpacing + "  " + item.title + "\n";
 		cff.abstract = item.abstractNote;
@@ -88,17 +88,13 @@ function doExport() {
 		cff.license = item.rights;
 		cff.version = item.versionNumber;
 
-		// Referenced from:
-		// https://github.com/zotero/zotero/blob/65db66eb339fc2bab4a78f9955f9e5197de3d028/chrome/locale/en-GB/zotero/zotero.properties#L491
-		cff.collection_title = item.collections
+		cff.collection_title = item.proceedingsTitle;
 		cff.conference = item.conferenceName;
 		cff.copyright = item.rights;
+		cff.database = item.libraryCatalog;
 		cff.date_accessed = item.accessDate;
 		cff.edition = item.edition;
-		cff.editors = writeCreators(item.creators, "editor");
 		cff.editors_series = item.series
-		cff.recipients = writeCreators(item.creators, "recipient");
-		cff.translators = writeCreators(item.creators, "translator");
 		cff.format = item.format;
 		cff.institution = item.institution;
 		cff.isbn = item.ISBN;
@@ -106,7 +102,7 @@ function doExport() {
 		cff.issue = item.issue;
 		cff.issue_date = item.issueDate;
 		cff.journal = item.journalAbbreviation;
-		cff.languages = writeArray(item.language)
+		cff.languages = writeArray([item.language]);
 		cff.location = item.archiveLocation;
 		cff.medium = item.medium;
 		cff.number = item.number;
@@ -117,13 +113,22 @@ function doExport() {
 			cff.pmcid = item.extra.match(/pmcid:\s*(\S+)/);
 		}
 		cff.publisher = item.publisher;
-		cff.repository = cff.repository;
+		cff.repository = item.repository;
 		cff.section = item.section;
 		cff.thesis_type = item.thesisType;
 		cff.volume = item.volume;
 		cff.url = item.url;
-		cff.keywords = writeArray(item.tags);
-		cff.authors = writeCreators(item.creators, "author");
+		cff.keywords = writeArray(item.tags.map(tag => tag.tag || tag));
+		if (["letter", "email", "instantMessage"].includes(item.itemType)) {
+			cff.senders = writeCreators(item.creators, "author");
+		}
+		else {
+			cff.authors = writeCreators(item.creators, ZU.getCreatorsForType(item.itemType)[0]);
+		}
+		cff.editors = writeCreators(item.creators, "editor");
+		cff.recipients = writeCreators(item.creators, "recipient");
+		cff.translators = writeCreators(item.creators, "translator");
+
 		if (item.date) {
 			// if we have a dataset or software, use date-released field
 			if (item.itemType == "dataset" || item.itemType == "computerProgram") {
@@ -133,7 +138,7 @@ function doExport() {
 			cff["date-published"] = ZU.strToISO(item.date);
 		}
 		// get DOI from Extra for software; this will stop running automatically once software supports DOI
-		if (!ZU.fieldIsValidForType('DOI', item.itemType) && /^doi:/i.test(item.extra)) {
+		if (!ZU.fieldIsValidForType('DOI', item.itemType) && /^doi:/im.test(item.extra)) {
 			item.DOI = ZU.cleanDOI(item.extra);
 		}
 		cff.identifiers = writeDOI(item.DOI);
@@ -149,11 +154,8 @@ function doExport() {
 				Zotero.write(field + ": " + cff[field]);
 			}
 			else if (field == "abstract") {
-				// special case for abstract, which can sometimes be large
-				Zotero.write(referencesSpacing + field + ": |\n" + referencesSpacing + "  " + cff[field] + "\n");
-			}
-			else if (field == "authors" || field == "keywords") {
-				Zotero.write(referencesSpacing + field + ": " + cff[field]);
+				// multiline
+				Zotero.write(referencesSpacing + field + ": |" + cff[field].replace(/^|\n/g, "\n" + referencesSpacing + "  ") + "\n");
 			}
 			else {
 				Zotero.write(referencesSpacing + field.replace("_", "-") + ": " + cff[field] + "\n");
