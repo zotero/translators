85e2b1c0172d7a0b89994aacd365e4ecf284eb46
sharonhoward <sharon@sharonhoward.org>
2024-09-15 14:18:03 +0100

    added session paper

--- Old Bailey Online.js
+++ Old Bailey Online.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2024-08-31 13:46:15"
+	"lastUpdated": "2024-09-15 13:03:34"
 }
 
 /*
@@ -36,8 +36,10 @@
 */
 
 /* new URLs
-// trial: /record/t18000115-12
-// OA: /record/OA16760705
+// trial report: /record/t18000115-12 [case]
+// OA: /record/OA16760705 [book]
+// session: /record/16740429 [book]
+// other div types /record/[afso - anything else?] [book section might work for these]
 // api: https://www.dhi.ac.uk/api/data/oldbailey_record_single?idkey=t17210419-18
 */
 
@@ -45,26 +47,32 @@ function detectWeb(doc, url) {
 	if (url.includes('/record/t')) {
 		return "case";
 	}
-	else if (url.includes('record/OA')) {
+	else if (url.includes('/record/1')) {
 		return "book";
+	}
+	else if (url.includes('/record/OA')) {
+		return "book";
+
+	// multiples are not doable at present
 	//	} else if (url.includes('/search/') && getSearchResults(doc, true)) {
 	//		return 'multiple';
 	//	}
 	}
-	// multiples  skipped as search isn't working right now...?
 	return false;
 }
 
-/* TODO
+/* may be possible to reinstate this in the longer term but not at present.
 function getSearchResults(doc, checkOnly) {
-	// api endpoint https://www.dhi.ac.uk/api/data/oldbailey_record?(query string)
-	
+	// have to use api endpoint. https://www.dhi.ac.uk/api/data/oldbailey_record?(query string)
+	// several types of search seem not to be accessible via the api (statistics, OAs, associated records)
+	// keyword and name searches need to be able to handle any Proceedings div type, not just trials
+	//	(may be doable, but much more complicated code)
 }*/
 
 async function doWeb(doc, url) {
 	var item = new Zotero.Item();
 
-	let rgx = /(\bt1[^-]+-[0-9]+[a-z]?|OA1[0-9]+[a-z]?)/;
+	let rgx = /(\bt1[^-]+-[0-9]+[a-z]?|OA1[0-9]+[a-z]?|\b1[0-9]{7}[AE]?$)/;
 	let id = url.match(rgx)[0];
 
 	let jsonURL = `https://www.dhi.ac.uk/api/data/oldbailey_record_single?idkey=${id}`;
@@ -74,22 +82,27 @@ async function doWeb(doc, url) {
 	let mParsed = new DOMParser().parseFromString(metadata, 'text/html');
 	let mTable = mParsed.querySelector("table");
 
-	// names for trial or OA
-	let names = ZU.xpathText(mTable, '//tr[contains(th, "Defendants")]/td[1]|//tr[contains(th, "People")]/td[1]');
-	let namesCleaned = ZU.capitalizeTitle(names.toLowerCase(), true);
-	// "Date" works for both t and OA
+	// "Date" works for session, t and OA
 	let niceDate = ZU.xpathText(mTable, '//tr[contains(th, "Date")]/td[1]');
+
+	// text type
+	let textType = ZU.xpathText(mTable, '//tr[contains(th, "Text type")]/td[1]');
 
 	if (url.includes('/record/t1')) {
 		item.itemType = "case";
 		
 		var sessDate = id.substring(1, 5) + "/" + id.substring(5, 7) + "/" + id.substring(7, 9);
 	
+		// names for trial defendants.
+		var names = ZU.xpathText(mTable, '//tr[contains(th, "Defendant")]/td[1]');
+		var namesCleaned = ZU.capitalizeTitle(names.toLowerCase(), true);
+
 		var offencesList = ZU.xpathText(mTable, '//tr[contains(th, "Offences")]/td[1]');
 		var offences = ZU.xpath(mTable, '//tr[contains(th, "Offences")]/td[1]/a');
 		for (let o of offences) {
 			item.tags.push(o.textContent);
 		}
+
 		var verdicts = ZU.xpath(mTable, '//tr[contains(th, "Verdicts")]/td[1]/a');
 		for (let v of verdicts) {
 			item.tags.push(v.textContent);
@@ -111,10 +124,20 @@ async function doWeb(doc, url) {
 		item.itemType = "book";
 		
 		var oaDate = id.substring(2, 6) + "/" + id.substring(6, 8) + "/" + id.substring(8, 10);
-		var oaTitle = "Ordinary's Account, " + niceDate;
+		var oaTitle = textType + ", " + niceDate;
 
 		item.date = ZU.strToISO(oaDate);
 		item.title = oaTitle;
+	}
+
+	else if (url.includes('record/1')) {
+		item.itemType = "book";
+
+		var sessionDate = id.substring(0, 4) + "/" + id.substring(4, 6) + "/" + id.substring(6, 8);
+		var sessionTitle = textType + ", " + niceDate;
+
+		item.date = ZU.strToISO(sessionDate);
+		item.title = sessionTitle;
 	}
 
 	item.extra = "Reference Number: " + json.idkey;
@@ -128,43 +151,6 @@ async function doWeb(doc, url) {
 var testCases = [
 	{
 		"type": "web",
-		"url": "https://www.oldbaileyonline.org/record/t17340911-7",
-		"items": [
-			{
-				"itemType": "case",
-				"caseName": "Trial of Judith Cupid: Theft > Theft from place, 11th September 1734",
-				"creators": [],
-				"dateDecided": "1734-09-11",
-				"court": "Central Criminal Court, London",
-				"docketNumber": "t17340911-7",
-				"extra": "Reference Number: t17340911-7",
-				"shortTitle": "Trial of Judith Cupid",
-				"url": "https://www.oldbaileyonline.org/record/t17340911-7",
-				"attachments": [],
-				"tags": [
-					{
-						"tag": "Guilty"
-					},
-					{
-						"tag": "Theft"
-					},
-					{
-						"tag": "Theft from place"
-					},
-					{
-						"tag": "Theft under 40s"
-					},
-					{
-						"tag": "Transportation"
-					}
-				],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
 		"url": "https://www.oldbaileyonline.org/record/OA17110421",
 		"items": [
 			{
@@ -175,6 +161,25 @@ var testCases = [
 				"extra": "Reference Number: OA17110421",
 				"libraryCatalog": "Old Bailey Online",
 				"url": "https://www.oldbaileyonline.org/record/OA17110421",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.oldbaileyonline.org/record/16740429",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "Sessions paper, 29th April 1674",
+				"creators": [],
+				"date": "1674-04-29",
+				"extra": "Reference Number: 16740429",
+				"libraryCatalog": "Old Bailey Online",
+				"url": "https://www.oldbaileyonline.org/record/16740429",
 				"attachments": [],
 				"tags": [],
 				"notes": [],
@@ -226,6 +231,81 @@ var testCases = [
 						"tag": "Theft"
 					}
 				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.oldbaileyonline.org/record/t17340911-7",
+		"items": [
+			{
+				"itemType": "case",
+				"caseName": "Trial of Judith Cupid: Theft > Theft from place, 11th September 1734",
+				"creators": [],
+				"dateDecided": "1734-09-11",
+				"court": "Central Criminal Court, London",
+				"docketNumber": "t17340911-7",
+				"extra": "Reference Number: t17340911-7",
+				"shortTitle": "Trial of Judith Cupid",
+				"url": "https://www.oldbaileyonline.org/record/t17340911-7",
+				"attachments": [],
+				"tags": [
+					{
+						"tag": "Guilty"
+					},
+					{
+						"tag": "Theft"
+					},
+					{
+						"tag": "Theft from place"
+					},
+					{
+						"tag": "Theft under 40s"
+					},
+					{
+						"tag": "Transportation"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.oldbaileyonline.org/record/16810117A",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "Sessions paper, 17th January 1681",
+				"creators": [],
+				"date": "1681-01-17",
+				"extra": "Reference Number: 16810117A",
+				"libraryCatalog": "Old Bailey Online",
+				"url": "https://www.oldbaileyonline.org/record/16810117A",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.oldbaileyonline.org/record/OA16850506A",
+		"items": [
+			{
+				"itemType": "book",
+				"title": "Ordinary's Account, 6th May 1685",
+				"creators": [],
+				"date": "1685-05-06",
+				"extra": "Reference Number: OA16850506",
+				"libraryCatalog": "Old Bailey Online",
+				"url": "https://www.oldbaileyonline.org/record/OA16850506A",
+				"attachments": [],
+				"tags": [],
 				"notes": [],
 				"seeAlso": []
 			}
