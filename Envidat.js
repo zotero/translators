430f4c3659003b6daa7338c181ce8cd324488ac9
Abe Jellinek <jellinek@berkeley.edu>
2025-04-08 11:34:17 -0400

    Tweaks
    - Use dataset type
    - Update target (regex)
    - Support legacy interface
    - Remove debugs
    - Use requestJSON() and requestDocument()
    - Use text() instead of manually walking tree
    - Fix all-caps tags
    - Update tests and replace broken test

--- Envidat.js
+++ Envidat.js
@@ -2,14 +2,14 @@
 	"translatorID": "0f6f5164-b44b-4ef6-9c5e-e3f39637569b",
 	"label": "Envidat",
 	"creator": "Alain Borel",
-	"target": "^https://(www.)?envidat.ch",
+	"target": "^https://(www\\.)?envidat.ch/",
 	"minVersion": "6.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-12-08 12:13:36"
+	"lastUpdated": "2025-04-08 15:34:08"
 }
 
 /*
@@ -35,10 +35,6 @@
 	***** END LICENSE BLOCK *****
 */
 
-const datasetType = ZU.fieldIsValidForType('title', 'dataset')
-	? 'dataset'
-	: 'document';
-
 // A search query in the frontend will be converted to a query for:
 // all the words in the title or the notes
 // OR all possible single-word queries in the title or notes
@@ -63,10 +59,8 @@ let apiQuery = (arr) => {
 };
 
 function detectWeb(doc, url) {
-	// Zotero.debug(url);
-	if (url.includes('/metadata/')) {
-		// Zotero.debug('This should be a dataset');
-		return datasetType;
+	if (url.includes('/#/metadata/') || url.includes('/dataset/')) {
+		return 'dataset';
 	}
 	else if (url.includes('/#/browse?')) {
 		Zotero.debug('This should be multiple objects');
@@ -84,7 +78,6 @@ async function getSearchResults(doc, checkOnly) {
 	// tags are used at frontend level: https://www.envidat.ch/#/browse?search=rock%20snow&tags=FOREST&isAuthorSearch=false
 	// => same call
 	let queryString = doc.location.href.replace('https://www.envidat.ch/#/browse', '');
-	// Zotero.debug('queryString: ' + queryString);
 	let urlParams = new URLSearchParams(queryString);
 	let query = urlParams.get('search');
 	let tags = urlParams.get('tags');
@@ -94,18 +87,13 @@ async function getSearchResults(doc, checkOnly) {
 	else {
 		tags = [];
 	}
-	// Zotero.debug('querystring: ' + queryString);
-	// Zotero.debug('query: ' + query);
-	// Zotero.debug('tags: ' + tags);
 
 	let termCombinations = possibleCombinations(query);
 	let myQuery = apiQuery(termCombinations);
 
 	let searchApiUrl = 'https://www.envidat.ch/query?' + myQuery;
-	// Zotero.debug(searchApiUrl);
-	let text = await requestText(searchApiUrl);
+	let { response: rsp } = await requestJSON(searchApiUrl);
 	
-	let rsp = JSON.parse(text).response;
 	let href;
 	let title;
 	if (rsp.docs) {
@@ -167,80 +155,41 @@ async function scrape(url) {
 	let dataciteUrl = url.replace('#/metadata/', 'dataset/') + '/export/datacite.xml';
 	// Zotero.debug(dataciteUrl);
 	if (dataciteUrl) {
-		let text = await requestText(dataciteUrl);
-		processMetadata(text);
+		let xmlDoc = await requestDocument(dataciteUrl);
+		processMetadata(xmlDoc);
 	}
 }
 
-function processMetadata(text) {
-	let parser = new DOMParser();
-	let xmlDoc = parser.parseFromString(text, "text/xml");
-	let item = new Zotero.Item(datasetType);
+function processMetadata(xmlDoc) {
+	// TODO: Replace with call to Datacite XML if we ever add a translator for that
+	let item = new Zotero.Item('dataset');
 	item.repository = 'Envidat';
 
-	let titleNode = xmlDoc.getElementsByTagName("title");
-	if (titleNode) {
-		item.title = titleNode[0].childNodes[0].nodeValue;
-	}
-
-	let pubYearNode = xmlDoc.getElementsByTagName("publicationYear");
-	if (pubYearNode) {
-		item.date = pubYearNode[0].childNodes[0].nodeValue;
-	}
-
-	let languageNode = xmlDoc.getElementsByTagName("language");
-	if (languageNode) {
-		item.language = languageNode[0].childNodes[0].nodeValue;
-	}
-
-	let publisherNode = xmlDoc.getElementsByTagName("publisher");
-	if (publisherNode) {
-		item.publisher = publisherNode[0].childNodes[0].nodeValue;
-	}
-
-	let creatorNodes = xmlDoc.getElementsByTagName("creator");
-	for (let creatorNode of creatorNodes) {
-		let givenName = creatorNode.getElementsByTagName("givenName")[0].childNodes[0].nodeValue;
-		let familyName = creatorNode.getElementsByTagName("familyName")[0].childNodes[0].nodeValue;
+	item.title = text(xmlDoc, "title");
+	item.date = text(xmlDoc, "publicationYear");
+	item.language = text(xmlDoc, "language");
+	item.publisher = text(xmlDoc, "publisher");
+	for (let creatorNode of xmlDoc.getElementsByTagName("creator")) {
+		let givenName = text(creatorNode, "givenName");
+		let familyName = text(creatorNode, "familyName");
 		// TODO should we use or map the roles from Datacite?
 		let author = { lastName: familyName, firstName: givenName, creatorType: 'author' };
 		item.creators.push(author);
 	}
 
-	let subjectNodes = xmlDoc.getElementsByTagName("subject");
-	for (let subjectNode of subjectNodes) {
-		let tag = { tag: subjectNode.childNodes[0].nodeValue };
+	for (let subjectNode of xmlDoc.getElementsByTagName("subject")) {
+		let tag = { tag: subjectNode.textContent };
+		if (tag.tag === tag.tag.toUpperCase()) {
+			tag.tag = ZU.capitalizeTitle(tag.tag, true);
+		}
 		item.tags.push(tag);
 	}
 
 	// TODO find an example with a DOI
-	let doiNode = xmlDoc.getElementsByTagName("identifier");
-	if (doiNode) {
-		if (doiNode[0].childNodes == 1) {
-			item.DOI = doiNode[0].childNodes[0].nodeValue;
-		}
-	}
-
-	let alternateIdentifierNodes = xmlDoc.getElementsByTagName("alternateIdentifier");
-	for (let alternateIdentifierNode of alternateIdentifierNodes) {
-		// The URL given in the RIS export is apparently the first one,
-		// but the one with a UUID feels more stable, so let's just use the last one
-		if (alternateIdentifierNode.getAttribute("alternateIdentifierType") == 'URL') {
-			item.url = alternateIdentifierNode.childNodes[0].nodeValue;
-		}
-	}
-
-	let descriptionNodes = xmlDoc.getElementsByTagName("description");
-	for (let descriptionNode of descriptionNodes) {
-		if (descriptionNode.getAttribute("descriptionType") == 'Abstract') {
-			item.abstractNote = descriptionNode.childNodes[0].nodeValue;
-		}
-	}
-
-	let rightNodes = xmlDoc.getElementsByTagName("rights");
-	for (let rightNode of rightNodes) {
-		item.rights = rightNode.childNodes[0].nodeValue;
-	}
+	item.DOI = text(xmlDoc, "Identifier");
+	item.url = text(xmlDoc, "alternateIdentifier[alternateIdentifierType='URL']:last-child");
+	item.abstractNote = text(xmlDoc, "description[descriptionType='Abstract' i]");
+	item.rights = text(xmlDoc, "rights");
 	
 	item.complete();
 }
@@ -250,7 +199,6 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "https://www.envidat.ch/#/metadata/experimental-rockfall-dataset-tschamut-grisons-switzerland",
-		"detectedItemType": "dataset",
 		"items": [
 			{
 				"itemType": "dataset",
@@ -281,40 +229,40 @@ var testCases = [
 				"abstractNote": "Dataset of an experimental campaign of induced rockfall in Tschamut, Grisons, Switzerland. \nThe data archive contains site specific geographical data such as DEM and orthophoto as well as the deposition points of manually induced rockfall by releasing differently shaped boulders with 30–80 kg of mass. Additionally available are all the StoneNode data streams for rocks equipped with a sensor. The data set consists of \n* Deposition points from two series (wet (27/10/2016) and frozen (08/12/2016) ground)  \n* Digital Elevation Model (grid resolution 2 m) obtained via UAV\n* Orthophoto (5 cm resolution) obtained via UAV\n* Digitized rock point clouds (.pts input files for RAMMS::ROCKFALL)\n* StoneNode v1.0 raw data stream for equipped rocks.\nFurther information is found in\n* A. Caviezel et al., _Design and Evaluation of a Low-Power Sensor Device for Induced Rockfall Experiments_, IEEE Transactions on Instrumentation and Measurement, 2018, 67, 767-779, http://ieeexplore.ieee.org/document/8122020/\n*  P. Niklaus et al., _StoneNode: A low-power sensor device for induced rockfall experiments_, 2017 IEEE Sensors Applications Symposium (SAS), 2017, 1-6, http://ieeexplore.ieee.org/document/7894081/",
 				"language": "en",
 				"libraryCatalog": "Envidat",
-				"repository": "WSL Institute for Snow and Avalanche Research SLF",
+				"repository": "EnviDat",
 				"rights": "ODbL with Database Contents License (DbCL)",
 				"url": "https://www.envidat.ch/dataset/5b7a47bf-cbea-42a0-879f-ea2ccd17e82f",
 				"attachments": [],
 				"tags": [
 					{
-						"tag": "DEM"
+						"tag": "Dem"
 					},
 					{
-						"tag": "DEPOSITION POINTS"
+						"tag": "Deposition Points"
 					},
 					{
-						"tag": "INDUCED ROCKFALL"
+						"tag": "Induced Rockfall"
 					},
 					{
-						"tag": "NATURAL HAZARDS"
+						"tag": "Natural Hazards"
 					},
 					{
-						"tag": "ROCKFALL"
+						"tag": "Rockfall"
 					},
 					{
-						"tag": "ROCKFALL EXPERIMENTS"
+						"tag": "Rockfall Experiments"
 					},
 					{
-						"tag": "ROCKFALL RUNOUT"
+						"tag": "Rockfall Runout"
 					},
 					{
-						"tag": "SENSOR STREAM"
+						"tag": "Sensor Stream"
 					},
 					{
-						"tag": "STONENODE"
+						"tag": "Stonenode"
 					},
 					{
-						"tag": "STONENODEDATA"
+						"tag": "Stonenodedata"
 					}
 				],
 				"notes": [],
@@ -324,42 +272,54 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "https://www.envidat.ch/#/metadata/gem-bh",
-		"detectedItemType": "dataset",
+		"url": "https://www.envidat.ch/dataset/10-16904-envidat-28",
 		"items": [
 			{
 				"itemType": "dataset",
-				"title": "Processed permafrost borehole data (2940 m asl), Gemsstock, Switzerland",
+				"title": "Snowfarming data set Davos and Martell 2015",
 				"creators": [
 					{
-						"lastName": "Phillips",
-						"firstName": "Marcia",
+						"lastName": "Grünewald",
+						"firstName": "Thomas",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Wolfsperger",
+						"firstName": "Fabian",
+						"creatorType": "author"
+					},
+					{
+						"lastName": "Lehning",
+						"firstName": "Michael",
 						"creatorType": "author"
 					}
 				],
-				"date": "2016",
-				"abstractNote": "Processed ground temperature measurements at the Gemsstock permafrost borehole in canton Uri, Switzerland. The borehole is located at 2940 m asl on a steep (50&deg;) North-West slope (315&deg;). The surface material is bedrock and borehole depth is 40 m.  Thermistors used YSI 44008. Year of drilling 2006. This borehole is part of the Swiss Permafrost network, PERMOS (www.permos.ch). Contact phillips@slf.ch for details of processing applied.\nPublications\n1. A. Haberkorn, M. Phillips, R. Kenner, H. Rhyner, M. Bavay, S.P. Galos, M. Hoelzle. Thermal regime of rock and its relation to snow cover in steep Alpine rock walls: Gemsstock, central Swiss Alps. 2015. Geografiska Annaler: Series A, Physical Geography. Volume 97. Issue 3. 579–597. http://dx.doi.org/10.1111/geoa.12101. 10.1111/geoa.12101.\n2. R. Kenner, M. Phillips, C. Danioth, C. Denier, P. Thee, A. Zgraggen. Investigation of rock and ice loss in a recently deglaciated mountain rock wall using terrestrial laser scanning: Gemsstock, Swiss Alps. 2011. Cold Regions Science and Technology. Volume 67. Issue 3. 157–164. http://dx.doi.org/10.1016/j.coldregions.2011.04.006. 10.1016/j.coldregions.2011.04.006.",
+				"date": "2018",
+				"abstractNote": "Two data sets obtained for snow farming projects (Fluela, Davos, CH and Martell, IT) in 2015. \nThe data set contains for each site:\n* 10 cm GIS raster of snow depth calculated from terrestrial laserscanning surveys (TLS) in the end of winter season (April/May)\n* 10 cm GIS raster of snow depth calculated from TLS in the end of summer season (October)\nInput files for SNOWPACK model:\n* .sno: snow profile at the end of winter\n* .smet: meteorological data measured by weather stations in the area\nFor more details see  Grünewald, T., Lehning, M., and Wolfsperger, F.: Snow farming: Conserving snow over the summer season, The Cryosphere Discuss., https://doi.org/10.5194/tc-2017-93, in review, 2017.",
 				"language": "en",
 				"libraryCatalog": "Envidat",
-				"repository": "PERMOS",
+				"repository": "EnviDat",
 				"rights": "ODbL with Database Contents License (DbCL)",
-				"url": "https://www.envidat.ch/dataset/1e117ff0-3fd3-4f7f-8593-9313fc79e0ca",
+				"url": "https://www.envidat.ch/dataset/640b09be-3b86-492e-aba2-449329969989",
 				"attachments": [],
 				"tags": [
 					{
-						"tag": "BOREHOLE"
+						"tag": "Snow"
 					},
 					{
-						"tag": "GEMSSTOCK"
+						"tag": "Snow Conservation"
 					},
 					{
-						"tag": "PERMAFROST"
+						"tag": "Snow Farming"
 					},
 					{
-						"tag": "PERMOS"
+						"tag": "Snowpack"
 					},
 					{
-						"tag": "TEMPERATURE"
+						"tag": "Terrestrial Laser Scanning"
+					},
+					{
+						"tag": "Winter Tourism"
 					}
 				],
 				"notes": [],
