7c2724695decc2dbddc5ed9f64d95fca7fb5cb78
Abe Jellinek <jellinek@berkeley.edu>
2025-04-28 14:53:55 -0400

    Add multiples

--- Yandex Books.js
+++ Yandex Books.js
@@ -40,11 +40,38 @@ function detectWeb(doc, url) {
 	if (/\/books\/.+/.test(url) && doc.querySelector('span[data-test-id="CONTENT_TITLE_MAIN"]')) {
 		return 'book';
 	}
+	else if (getSearchResults(doc, true)) {
+		return 'multiple';
+	}
 	return false;
 }
 
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = doc.querySelectorAll('div[data-test-id="SEARCH_ALL_TAB"] a[href*="/books/"]');
+	for (let row of rows) {
+		let href = row.href;
+		let title = ZU.trimInternal(row.textContent);
+		if (!href || !title) continue;
+		if (checkOnly) return true;
+		found = true;
+		items[href] = title;
+	}
+	return found ? items : false;
+}
+
 async function doWeb(doc, url) {
-	await scrape(doc, url);
+	if (detectWeb(doc, url) == 'multiple') {
+		let items = await Zotero.selectItems(getSearchResults(doc, false));
+		if (!items) return;
+		for (let url of Object.keys(items)) {
+			await scrape(await requestDocument(url));
+		}
+	}
+	else {
+		await scrape(doc, url);
+	}
 }
 
 async function scrape(doc, url = doc.location.href) {
@@ -227,6 +254,12 @@ var testCases = [
 				"seeAlso": []
 			}
 		]
+	},
+	{
+		"type": "web",
+		"url": "https://books.yandex.ru/search/all/%D1%81%D0%B1%D0%BE%D1%80%D0%BD%D0%B8%D0%BA",
+		"defer": true,
+		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
