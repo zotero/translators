63f6c116eaf20528ba999040981ca5636a14cd23
SomeoneFighter <92687769+SomeoneFighter@users.noreply.github.com>
2025-06-08 11:20:22 +0300

    add free license

--- Israel National Newspaper Library.js
+++ Israel National Newspaper Library.js
@@ -12,6 +12,29 @@
 	"lastUpdated": "2025-06-08 07:51:10"
 }
 
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2015 Philipp Zumstein
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
 function detectWeb(doc, url) {
 	if (url.includes("/article/")) {
 		return "newspaperArticle";
