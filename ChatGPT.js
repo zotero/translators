3d05846d2bb53073afa707bc2b1f4aa63f883a07
Abe Jellinek <jellinek@berkeley.edu>
2025-10-24 11:21:22 -0400

    Add translator for ChatGPT

---
+++ ChatGPT.js
@@ -0,0 +1,149 @@
+{
+	"translatorID": "d8a83346-164a-467d-8717-eb96d4dcce6f",
+	"label": "ChatGPT",
+	"creator": "Abe Jellinek",
+	"target": "^https://chatgpt\\.com/",
+	"minVersion": "5.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2025-10-24 15:21:11"
+}
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2025 Abe Jellinek
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
+// Simple translator for ChatGPT that grabs the date and model info
+// for private and public chats.
+
+// Please don't take any of the translator authorship advice that ChatGPT
+// gives in the test page. It's mostly wrong.
+
+
+function detectWeb(doc, url) {
+	if (url.includes('/c/') || url.includes('/share/')) {
+		return 'webpage';
+	}
+	return false;
+}
+
+async function doWeb(doc, url) {
+	let item = new Zotero.Item('webpage');
+	item.title = doc.title;
+	item.websiteTitle = 'ChatGPT';
+
+	if (url.includes('/share/')) {
+		item.url = url;
+	}
+	else {
+		item.websiteType = 'Private AI conversation';
+	}
+	
+	item.attachments.push({
+		title: 'Snapshot',
+		document: doc
+	});
+
+	try {
+		await enrichItemWithAPI(doc, url, item);
+	}
+	catch (e) {
+		Zotero.debug(e);
+	}
+
+	item.complete();
+}
+
+async function enrichItemWithAPI(doc, url, item) {
+	let dataScript = [...doc.querySelectorAll('script')]
+		.find(script => script.textContent.startsWith('window.__reactRouterContext.streamController.enqueue('))
+		.textContent;
+	let extract = (key) => {
+		let formattedKey = `\\"${key}\\",\\"`;
+		let keyIndex = dataScript.indexOf(formattedKey);
+		if (keyIndex === -1) return null;
+		return dataScript.substring(keyIndex + formattedKey.length).split('\\"')[0];
+	};
+	
+	let language = extract('locale');
+	let deviceID = extract('WebAnonymousCookieID');
+	let accessToken = extract('accessToken');
+	let clientVersion = doc.documentElement.dataset.build;
+
+	let apiSlug;
+	if (url.includes('/c/')) {
+		apiSlug = 'conversation/' + url.match(/\/c\/([^#?/]+)/)[1];
+	}
+	else {
+		apiSlug = 'share/' + url.match(/\/share\/([^#?/]+)/)[1];
+	}
+
+	let apiURL = '/backend-api/' + apiSlug;
+	let headers = {
+		'OAI-Language': language,
+		'OAI-Device-Id': deviceID,
+		'OAI-Client-Version': clientVersion,
+	};
+	if (accessToken) {
+		headers.Authorization = `Bearer ${accessToken}`;
+	}
+	
+	let json = await requestJSON(apiURL, { headers });
+
+	item.title = json.title;
+	item.date = ZU.strToISO(new Date((json.update_time || json.create_time) * 1000).toISOString());
+	if (json.model) {
+		item.websiteType = `Conversation with ${json.model.title}`;
+	}
+}
+
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://chatgpt.com/share/68fa640b-9fc8-8013-a803-3d5241df6556",
+		"items": [
+			{
+				"itemType": "webpage",
+				"title": "Write Zotero translator",
+				"creators": [],
+				"date": "2025-10-24",
+				"url": "https://chatgpt.com/share/68fa640b-9fc8-8013-a803-3d5241df6556",
+				"websiteTitle": "ChatGPT",
+				"websiteType": "Conversation with GPT-5",
+				"attachments": [
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
