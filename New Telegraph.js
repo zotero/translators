6ef880ddbcea2f18347978623e9575e56ef5211b
vanderwaalforces <173410664+vanderwaalforces@users.noreply.github.com>
2026-02-10 17:42:19 +0100

    Update New Telegraph.js

--- New Telegraph.js
+++ New Telegraph.js
@@ -137,11 +137,6 @@ function detectWeb(doc, url) {
 		return 'newspaperArticle';
 	}
 
-	// 5) Only at this stage, test for listing
-	if (getSearchResults(doc, true)) {
-		return 'multiple';
-	}
-
 	return false;
 }
 
