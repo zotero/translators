202eef147c512a73793cc5acefabe1a97102da5c
robinruff <robin.ruff.98@googlemail.com>
2026-03-06 21:34:21 +0100

    Fix: replace invalid rdf:resource predicate with z:filePath (#3590)

--- RDF.js
+++ RDF.js
@@ -12,7 +12,7 @@
 	},
 	"inRepository": true,
 	"translatorType": 1,
-	"lastUpdated": "2025-08-06 22:48:30"
+	"lastUpdated": "2026-03-06 20:32:45"
 }
 
 /*
@@ -484,7 +484,7 @@ function detectType(newItem, node, ret) {
 				// process as file
 				t.zotero = "attachment";
 
-				var path = getFirstResults(node, [rdf + "resource"]);
+				var path = getFirstResults(node, [n.z + "filePath", rdf + "resource"]);
 				if (path) {
 					newItem.path = Zotero.RDF.getResourceURI(path[0]);
 				}
