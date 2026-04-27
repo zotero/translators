d5f77c8992603e74c53e99eb8f3b3a4221b68214
Tobias Diez <code@tobiasdiez.de>
2026-04-27 17:43:20 +0200

    Refactor regex patterns to use native RegExp (#3595)
    And fix invalid "\p{han}".

--- BibLaTeX.js
+++ BibLaTeX.js
@@ -17,7 +17,7 @@
 	},
 	"inRepository": true,
 	"translatorType": 2,
-	"lastUpdated": "2024-03-25 14:49:42"
+	"lastUpdated": "2026-04-01 18:00:00"
 }
 
 /*
@@ -302,9 +302,8 @@ function writeField(field, value, isMacro, noEscape) {
 
 		// Case of words with uppercase characters in non-initial positions is preserved with braces.
 		// we're looking at all unicode letters
-		var protectCaps = new ZU.XRegExp("\\b\\p{Letter}+\\p{Uppercase_Letter}\\p{Letter}*", 'g');
 		if (field != "pages") {
-			value = ZU.XRegExp.replace(value, protectCaps, "{$0}");
+			value = value.replace(/\b\p{L}+\p{Lu}\p{L}*/gu, "{$&}");
 		}
 
 		// Page ranges should use double dash
